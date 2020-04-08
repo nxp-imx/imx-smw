f5b773581d73259596ef2177d24f425f188e3e65
Bruno De Smet <bruno.de.smet@nxp.com>
2020-04-08 21:42:02 +0200

    SSMW-70 core:init: implement Configuration module with support of HSM
    - Define new ops in struct smw_ops required for:
      - Load configuration
      - Load HSM subsystem
    - Init module adds temporarily DUMMY operation
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- core/init/init.c
+++ core/init/init.c
@@ -3,12 +3,18 @@
  * Copyright 2019-2020 NXP
  */
 
+#include "smw_status.h"
+
+#include "smw_osal.h"
+#include "global.h"
+#include "debug.h"
 #include "utils.h"
+#include "subsystems.h"
+#include "config.h"
 
-struct smw_ctx g_smw_ctx = { .start_count = 0,
-			     .dgb_lvl = SMW_UTILS_DBG_LEVEL_DEFAULT };
+struct smw_ctx g_smw_ctx = { .ops = { 0 }, .start_count = 0 };
 
-static int smw_check_ops(const struct smw_ops *ops)
+static int check_ops(const struct smw_ops *ops)
 {
 	if (!ops)
 		return -1;
@@ -17,16 +23,25 @@ static int smw_check_ops(const struct smw_ops *ops)
 	    (bool)!ops->critical_section_stop)
 		return -1;
 
+	if ((ops->mutex_init || ops->mutex_destroy || ops->mutex_lock ||
+	     ops->mutex_unlock) &&
+	    !(ops->mutex_init && ops->mutex_destroy && ops->mutex_lock &&
+	      ops->mutex_unlock))
+		return -1;
+
+	if (!ops->thread_create || !ops->thread_cancel)
+		return -1;
+
 	return 0;
 }
 
-int smw_start(const struct smw_ops *ops, unsigned char dbg_lvl)
+int smw_init(const struct smw_ops *ops)
 {
 	int status = 0;
 
-	SMW_UTILS_TRACE_FUNCTION_CALL;
+	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	status = smw_check_ops(ops);
+	status = check_ops(ops);
 	if (status)
 		goto end;
 
@@ -35,38 +50,57 @@ int smw_start(const struct smw_ops *ops, unsigned char dbg_lvl)
 
 	if (!g_smw_ctx.start_count) {
 		g_smw_ctx.ops = *ops;
-		g_smw_ctx.dgb_lvl = dbg_lvl;
+		status = smw_config_init();
 	}
 
 	if (!status)
 		g_smw_ctx.start_count++;
 
-	SMW_UTILS_DBG_PRINTF(VERBOSE, "%s - Start count: %d\n", __func__,
-			     g_smw_ctx.start_count);
+	SMW_DBG_PRINTF(VERBOSE, "%s - Start count: %d\n", __func__,
+		       g_smw_ctx.start_count);
 
 	if (ops->critical_section_stop)
 		ops->critical_section_stop();
 
 end:
-	SMW_UTILS_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }
 
-int smw_stop(void)
+int smw_deinit(void)
 {
 	int status = 0;
 
-	SMW_UTILS_TRACE_FUNCTION_CALL;
+	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	SMW_UTILS_CRITICAL_SECTION_START;
 
-	g_smw_ctx.start_count--;
+	if (g_smw_ctx.start_count == 1) {
+		status = smw_config_deinit();
+		if (status)
+			goto end;
+	}
 
-	SMW_UTILS_DBG_PRINTF(VERBOSE, "%s - Start count: %d\n", __func__,
-			     g_smw_ctx.start_count);
+	if (g_smw_ctx.start_count >= 1)
+		g_smw_ctx.start_count--;
 
+	SMW_DBG_PRINTF(VERBOSE, "%s - Start count: %d\n", __func__,
+		       g_smw_ctx.start_count);
+
+end:
 	SMW_UTILS_CRITICAL_SECTION_STOP;
 
-	SMW_UTILS_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
+}
+
+void smw_dummy(void)
+{
+	SMW_DBG_TRACE_FUNCTION_CALL;
+}
+
+struct operation_func *smw_dummy_get_func(void)
+{
+	SMW_DBG_TRACE_FUNCTION_CALL;
+	return NULL;
 }
--- inc/smw_osal.h
+++ inc/smw_osal.h
@@ -10,43 +10,83 @@
  * struct smw_ops - SMW OSAL
  * @critical_section_start: [optional] Start critical section
  * @critical_section_stop: [optional] Stop critical section
- * @thread_self: [optional] Returns the ID of the thread being executed
+ * @mutex_init: [optional] Initialize a mutex
+ * @mutex_destroy: [optional] Destroy a mutex
+ * @mutex_lock: [optional] Lock a mutex
+ * @mutex_unlock: [optional] Unlock a mutex
+ * @thread_create: [mandatory] Create a thread
+ * @thread_cancel: [mandatory] Cancel a thread
+ * @thread_self: [optional] Return the ID of the thread being executed
  *
  * This structure defines the SMW OSAL.
  * Functions pointers marked as [mandatory] must be assigned.
  * Functions pointers marked as [optional] may not be assigned.
+ * mutex_* functions pointers are optional together.
  * critical_* functions pointers are optional together.
  */
 struct smw_ops {
 	void (*critical_section_start)(void);
 	void (*critical_section_stop)(void);
 
+	int (*mutex_init)(void **mutex);
+	int (*mutex_destroy)(void **mutex);
+	int (*mutex_lock)(void *mutex);
+	int (*mutex_unlock)(void *mutex);
+
+	int (*thread_create)(unsigned long *thread,
+			     void *(*start_routine)(void *), void *arg);
+	int (*thread_cancel)(unsigned long thread);
+
 	unsigned long (*thread_self)(void);
 };
 
 /**
- * smw_start() - Start the SMW library.
- * @ops: pointer the structure describing the OSAL.
- * @dbg_lvl: debug level of the SMW library.
+ * smw_init() - Initialize the SMW library.
+ * @ops: pointer to the structure describing the OSAL.
  *
- * This function starts the Security Middleware.
+ * This function initializes the Security Middleware.
  * It verifies that ops is valid and
- * then initializes SMW.
+ * then initializes SMW modules.
  *
  * Return:
  * error code.
  */
-int smw_start(const struct smw_ops *ops, unsigned char dbg_lvl);
+int smw_init(const struct smw_ops *ops);
 
 /**
- * smw_stop() - Stop the SMW library.
+ * smw_deinit() - Deinitialize the SMW library.
  *
- * This function stops the Security Middleware.
+ * This function deinitializes the Security Middleware.
  * It frees all memory dynamically allocated by SMW.
  *
  * Return:
  * error code.
  */
-int smw_stop(void);
+int smw_deinit(void);
+
+/**
+ * smw_config_load() - Load a configuration.
+ * @buffer: pointer to the plaintext configuration.
+ * @size: size of the plaintext configuration
+ *
+ * This function loads a configuration.
+ * The plaintext configuration is parsed and
+ * the content is stored in the Configuration database.
+ *
+ * Return:
+ * error code.
+ */
+int smw_config_load(char *buffer, unsigned int size);
+
+/**
+ * smw_config_unload() - Unload the current configuration.
+ *
+ * This function unloads the current configuration.
+ * It frees all memory dynamically allocated by SMW.
+ *
+ * Return:
+ * none.
+ */
+void smw_config_unload(void);
 
 #endif /* SMW_OSAL_H */

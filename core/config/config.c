5f8f6d09b858d2a6e6eb085b0a2744172bce3725
Cedric Neveux <cedric.neveux@nxp.com>
2020-08-25 07:50:18 +0200

    SSMW-131 core: config: implement key configuration information API
    Add API to get the key information supported in SMW
    function of configuration loaded.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- core/config/config.c
+++ core/config/config.c
@@ -16,7 +16,7 @@
 
 __export int smw_config_subsystem_present(const char *subsystem)
 {
-	unsigned int id = SUBSYSTEM_ID_INVALID;
+	enum subsystem_id id = SUBSYSTEM_ID_INVALID;
 
 	if (!subsystem)
 		return SMW_STATUS_INVALID_PARAM;
@@ -24,12 +24,11 @@ __export int smw_config_subsystem_present(const char *subsystem)
 	return smw_config_get_subsystem_id(subsystem, &id);
 }
 
-__export int smw_config_subsystem_check_digest(const char *subsystem,
-					       const char *algo)
+__export int smw_config_check_digest(const char *subsystem, const char *algo)
 {
 	int status;
-	unsigned int id = SUBSYSTEM_ID_INVALID;
-	unsigned int algo_id;
+	enum subsystem_id id = SUBSYSTEM_ID_INVALID;
+	enum smw_config_hash_algo_id algo_id;
 	struct hash_params *params;
 
 	if (!algo)
@@ -50,6 +49,46 @@ __export int smw_config_subsystem_check_digest(const char *subsystem,
 
 	if (!check_id(algo_id, params->algo_bitmap))
 		return SMW_STATUS_OPERATION_NOT_CONFIGURED;
+
+	return SMW_STATUS_OK;
+}
+
+__export int smw_config_check_generate_key(const char *subsystem,
+					   struct smw_key_info *info)
+{
+	int status;
+	enum subsystem_id id = SUBSYSTEM_ID_INVALID;
+	enum smw_config_key_type_id keytype_id;
+	struct key_operation_params *params;
+
+	if (!info || !info->key_type_name)
+		return SMW_STATUS_INVALID_PARAM;
+
+	status = smw_config_get_subsystem_id(subsystem, &id);
+	if (status != SMW_STATUS_OK)
+		return status;
+
+	status = smw_config_get_key_type_id(info->key_type_name, &keytype_id);
+	if (status != SMW_STATUS_OK)
+		return status;
+
+	status = smw_config_get_subsystem_caps(&id, OPERATION_ID_GENERATE_KEY,
+					       (void **)&params);
+	if (status != SMW_STATUS_OK)
+		return status;
+
+	if (!check_id(keytype_id, params->key_type_bitmap))
+		return SMW_STATUS_OPERATION_NOT_CONFIGURED;
+
+	if (info->security_size) {
+		if (!check_security_size(info->security_size,
+					 params->key_size_min,
+					 params->key_size_max))
+			return SMW_STATUS_OPERATION_NOT_CONFIGURED;
+	} else {
+		info->security_size_min = params->key_size_min;
+		info->security_size_max = params->key_size_max;
+	}
 
 	return SMW_STATUS_OK;
 }
--- public/smw_config.h
+++ public/smw_config.h
@@ -16,15 +16,54 @@
 int smw_config_subsystem_present(const char *subsystem);
 
 /**
- * smw_config_subsystem_check_digest() - Check if a digest @algo is supported
- * @subsystem: Name of the subsystem
+ * smw_config_check_digest() - Check if a digest @algo is supported
+ * @subsystem: Name of the subsystem (if NULL default subsystem)
  * @algo: Digest algorithm name
+ *
+ * Function checks if the digest @algo is supported on the given @subsytem.
+ * If @subsystem is NULL, default subsystem digest capability is checked.
  *
  * Return:
  * SMW_STATUS_OK                       Subsystem is present
  * SMW_STATUS_OPERATION_NOT_CONFIGURED Algorithm not supported
  * error code otherwise
  */
-int smw_config_subsystem_check_digest(const char *subsystem, const char *algo);
+int smw_config_check_digest(const char *subsystem, const char *algo);
+
+/**
+ * struct smw_key_info - Key information
+ * @key_type_name: Key type name
+ * @security_size: Key security size in bits
+ * @security_size_min: Key security size minimum in bits
+ * @security_size_max: Key security size maximum in bits
+ */
+struct smw_key_info {
+	const char *key_type_name;
+	unsigned int security_size;
+	unsigned int security_size_min;
+	unsigned int security_size_max;
+};
+
+/**
+ * smw_config_check_generate_key() - Check generate key type
+ * @subsystem: Name of the subsystem (if NULL default subsystem)
+ * @info: Key information
+ *
+ * Function checks if the key type provided in the @info structure is
+ * supported on the given subsystem.
+ *
+ * If @info's security size field is equal 0, returns the security key
+ * range size in bits supported by the subsystem for the key type. Else
+ * checks if the security size is supported.
+ *
+ * If @subsystem is NULL, default subsystem digest capability is checked.
+ *
+ * Return:
+ * SMW_STATUS_OK                       Subsystem is present
+ * SMW_STATUS_OPERATION_NOT_CONFIGURED Algorithm not supported
+ * error code otherwise
+ */
+int smw_config_check_generate_key(const char *subsystem,
+				  struct smw_key_info *info);
 
 #endif /* __SMW_CONFIG_H__ */

nxp-imx/imx-smw

commit c1dbd26a3cbedbdd181984335e99b2a4ec1d4dc6

SSMW-215 scripts: upload package to test in Nexus
Anonymous right access has been removed in bamboo. Hence, to
be able to run the LAVA CI, the package file has to be uploaded
on Nexus to allow the linux running on board to get it.

Add a CI task to clean up Nexus folder at the end of the PR/branch
verification.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

scripts/bamboo/smw_bamboo_config.sh
@@ -1,0 +1,76 @@
+#!/bin/bash
+# Define all bamboo common share function/macro and constants
+
+# Get bamboo variables
+bamboo_plan="${bamboo_planKey:-"misc"}"
+
+nexus_test_url="${bamboo_nexus_test_url:-"https://nl-nxrm.sw.nxp.com"}"
+nexus_test_url_get="${nexus_test_url}/repository"
+nexus_test_repo="${bamboo_nexus_test_repo:-"mougins-raw-public-artifacts"}"
+nexus_test_top_dir="${bamboo_nexus_test_top_dir:-"SMW_Test"}"
+nexus_test_dir="${nexus_test_repo}/${nexus_test_top_dir}/${bamboo_plan}"
+nexus_test_full_path="${nexus_test_url_get}/${nexus_test_dir}"
+
+job_name=${bamboo_plan}-${bamboo_buildNumber}
+
+gcda_tarball="${job_name}_gcda.tar.gz"
+gcda_tarball="${gcda_tarball// /_}"
+
+#
+# Python virtual environment
+#
+# End vvenv
+function exit_vvenv {
+    deactivate
+}
+
+# Start venv
+function start_vvenv {
+    venv_dir="venv_smw"
+    eval "python3 -m venv ${venv_dir}"
+    eval "source ${venv_dir}/bin/activate"
+}
+
+#
+# Common scripts functions
+#
+function check_url
+{
+    status=$(curl --head --silent "$1" | head -n 1)
+    if echo "$status" | grep -q 404; then
+      printf "File %s doesn't exist\n" "$1"
+      exit 1
+    fi
+}
+
+function nexus_upload_artifacts
+{
+    script_dir=$1
+    src=$2
+    dst=$3
+
+    printf "NEXUS: Uploading folder %s in folder %s\n" "${src}" "${dst}"
+
+    eval "$1/bamboo/bamboo-to-nexus-upload.sh ${src} ${dst} ${nexus_test_url}"
+}
+
+function is_pr
+{
+    # Check if the branch is a PR or not
+   if [[ -z "${bamboo_repository_pr_targetBranch+x}" ]] ; then
+       echo 0
+   else
+       echo 1
+   fi
+}
+
+function is_weekly_build
+{
+    local daytoday=$(date +%w)
+
+    if [[ ${daytoday} == "$bamboo_weekly_day_run" ]]; then
+        echo 1
+    else
+        echo 0
+    fi
+}

scripts/bamboo/smw_bamboo_build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-set -e
+set -ex
 
-function cleanup {
-    #
-    # Stop venv
-    #
-    deactivate
-}
-trap cleanup EXIT
+bash_dir=$(dirname "${BASH_SOURCE[0]}")
+source "${bash_dir}/smw_bamboo_config.sh"
+
+trap exit_vvenv EXIT
 
 function usage()
 {
@@ -76,19 +73,9 @@ build_debug="build.${platform}_deb"
 rm -rf "${build_release}"
 rm -rf "${build_debug}"
 
-PR=0
-daytoday=$(date +%w)
-
-# Check if the branch is a PR
-if [ ! -z "${bamboo_repository_pr_targetBranch+x}" ] ; then
-    PR=1
-fi
-
 # Check if the branch is not a PR and it's daily build
-if [[ ${PR} -eq 0 ]]; then
-    if [[ ${daytoday} == "$bamboo_weekly_day_run" ]]; then
-        git clean -xdf
-    fi
+if [[ $(is_pr) -eq 0 ]] && [[ $(is_weekly_build) -eq 1 ]]; then
+    git clean -xdf
 fi
 
 export="./export"
@@ -103,10 +90,9 @@ fi
 #
 # Start venv
 #
-venv_dir="venv_smw"
-eval "python3 -m venv ${venv_dir}"
-eval "source ${venv_dir}/bin/activate"
+start_vvenv
 
+# Install python package to build OPTEE
 eval "python3 -m pip install pyelftools pycryptodomex"
 
 #
@@ -151,7 +137,7 @@ doc_dir_root="Documentations"
 doc_dir_html="html"
 doc_dir_pdf="latex"
 
-if [[ ${PR} -eq 0 ]]; then
+if [[ $(is_pr) -eq 0 ]]; then
     if [[ ${opt_doc} -eq 1 ]]; then
         echo "Archiving documentation..."
         rm -rf "${mnt_server}/${doc_dir_root}"

scripts/bamboo/smw_clean_nexus.sh
@@ -1,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+bash_dir=$(dirname "${BASH_SOURCE[0]}")
+source "${bash_dir}/smw_bamboo_config.sh"
+
+script_tools_dir="$bamboo_build_working_directory/tools"
+test_dir="${nexus_test_top_dir}/${bamboo_plan}"
+
+printf "NEXUS: Clean folder %s in folder %s\n" "${test_dir}" "${nexus_test_repo}"
+
+eval "${script_tools_dir}/bamboo/nexus-clean-dir-recursively.sh ${nexus_test_repo} ${test_dir}"

scripts/bamboo/smw_code_coverage.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -ex
 
-# Define nexus default url
-nexus_url="${bamboo_nexus_url:-"https://nl-nxrm.sw.nxp.com/repository"}"
-nexus_dir="${bamboo_nexus_lava_dir:-"mougins-raw-public-artifacts/LAVA/smw"}"
-job_name="${bamboo_planKey}-${bamboo_buildNumber}"
-bamboo_plan="${bamboo_planKey:-"misc"}"
+bash_dir=$(dirname "${BASH_SOURCE[0]}")
+source "${bash_dir}/smw_bamboo_config.sh"
+
+trap exit_vvenv EXIT
 
 function usage()
 {
@@ -51,15 +50,6 @@ function get_file_info()
     fi
 }
 
-function check_url
-{
-    status=$(curl -I -s "$1" | head -n 1)
-    if [[ -w ${status} ]]; then
-      printf "File %s doesn't exist\n" "$1"
-      exit 1
-    fi
-}
-
 function tarball_extract
 {
     printf "Untar %s into %s\n" "$1" "$2"
@@ -72,35 +62,30 @@ function gcda_upload_extract
     # Upload gdca tarball and untar it in the
     # build directory
     #
-    local gcda_filename="${platform}_${job_name}.tar.gz"
-    local gcda_url="${nexus_url}/${nexus_dir}/${bamboo_plan}"
+    local gcda_filename="$1"
 
-    check_url "${gcda_url}/${gcda_filename}"
+    check_url "${nexus_test_full_path}/${gcda_filename}"
 
     printf "Get the GCDA tarball file\n"
-    eval "curl -O ${gcda_url}/${gcda_filename}"
+    eval "curl -O ${nexus_test_full_path}/${gcda_filename}"
 
-    tarball_extract "$1" "$2"
-
-    eval "curl -n -X DELETE ${gcda_url}/${gcda_filename}"
+    tarball_extract "${gcda_filename}" "$2"
 }
 
 if [[ $# -lt 1 ]]; then
     usage
 fi
 
-daytoday=$(date +%w)
-PR=0
-# Check if the branch is a PR
-if [ ! -z "${bamboo_repository_pr_targetBranch+x}" ] ; then
-    PR=1
-fi
-
-if [[ ${daytoday} == "$bamboo_weekly_day_run" ]] && [[ ${PR} == 0 ]]; then
-    #
+# Check if the branch is not a PR and it's daily build
+if [[ $(is_pr) -eq 0 ]] && [[ $(is_weekly_build) -eq 1 ]]; then
     # Don't run code coverage
     exit 0
 fi
+
+#
+# Start venv
+#
+start_vvenv
 
 platform=$1
 
@@ -122,9 +107,6 @@ gcov_report="gcov_report_${platform}"
 gcov_out="./${gcov_report}"
 lcov_tool="../lcov_tool"
 
-gcda_tarball="${platform}_${job_name}.tar.gz"
-gcda_tarball="${gcda_tarball// /_}"
-
 #
 # Check if gcno tarball exist and copy it locally
 #
@@ -144,7 +126,7 @@ fi
 eval "mkdir -p ${gcov_data}${root_dir}"
 
 tarball_extract "${gcno_tarball}" "${gcov_data}${root_dir}"
-gcda_upload_extract "${gcda_tarball}" "${gcov_data}"
+gcda_upload_extract "${platform}_${gcda_tarball}" "${gcov_data}"
 
 gcno_dir="${gcov_data}/${root_dir}/${build_dir}"
 gcda_dir="${gcov_data}/${root_dir}/${build_dir}"

scripts/bamboo/smw_lava_run.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -ex
 
-function cleanup {
-    #
-    # Stop venv
-    #
-    deactivate
-}
-trap cleanup EXIT
+bash_dir=$(dirname "${BASH_SOURCE[0]}")
+source "${bash_dir}/smw_bamboo_config.sh"
+
+trap exit_vvenv EXIT
 
 function usage()
 {
@@ -69,32 +66,23 @@ if [[ $# -ne 0 ]]; then
     done
 fi
 
+#
+# Start venv
+#
+start_vvenv
+
 eval "./scripts/smw_squad.sh install"
 
-script_dir="$bamboo_build_working_directory"/lava/bambooIntegrationScripts
+script_lava_dir="$bamboo_build_working_directory/lava/bambooIntegrationScripts"
+script_tools_dir="$bamboo_build_working_directory/tools"
 yaml_dir=./scripts/lava
-
-daytoday=$(date +%w)
-PR=0
-# Check if the branch is a PR
-if [ ! -z "${bamboo_repository_pr_targetBranch+x}" ] ; then
-    PR=1
-fi
 
 # Fetch all reports from LAVA
 rm -rf logs && mkdir logs
 
 squad_id=$(echo "$bamboo_planRepository_branchName" | tr / _)_${bamboo_buildNumber}
-job_id=${bamboo_planKey}-${bamboo_buildNumber}
 
-#
-# Start venv
-#
-venv_dir="venv_smw"
-eval "python3 -m venv ${venv_dir}"
-eval "source ${venv_dir}/bin/activate"
-
-if [[ ${daytoday} == "$bamboo_weekly_day_run" ]] && [[ ${PR} == 0 ]]; then
+if [[ $(is_pr) -eq 0 ]] && [[ $(is_weekly_build) -eq 1 ]]; then
     #
     # If executed on selected weekly day, assume it's a periodic weekly don't
     # check code change neither do a code coverage report
@@ -103,20 +91,33 @@ if [[ ${daytoday} == "$bamboo_weekly_day_run" ]] && [[ ${PR} == 0 ]]; then
     if [[ ! -z ${opt_coverage} ]]; then exit 0; fi
 
     eval "./scripts/smw_squad.sh submit_uuu ${platform} \
-          ${script_dir} ${yaml_dir} ${squad_id} job_name=${job_id}"
+          ${script_lava_dir} ${yaml_dir} ${squad_id} job_name=${job_name}"
 else
-    package_url="https://bamboo1.sw.nxp.com/browse/${job_id}/artifact/shared"
-    plat_package="package_${platform}_${opt_type}"
-
     #
     # Test Release or Debug build and retrieve code coverage result if any
     #
-    eval "./scripts/smw_squad.sh submit_uuu ${platform} ${script_dir} \
-          ${yaml_dir} ${squad_id} ${opt_coverage} \
-          package_url=${package_url}/${plat_package} \
-          job_name=${job_id}"
-fi
+    to_nexus_dir="to_nexus"
+    plat_package="libsmw_package.tar.gz"
+    pkg_name="pkg_${platform}_${opt_type}_${bamboo_buildNumber}.tar.gz"
 
+    eval "mkdir -p ${to_nexus_dir}"
+    eval "cp ./package/${plat_package} ./to_nexus/${pkg_name}"
+
+    # Upload package to be tested in Nexus
+    nexus_upload_artifacts "${script_tools_dir}" "${to_nexus_dir}" "${nexus_test_dir}"
+
+    coverage_url=
+
+    if [[ -n ${opt_coverage} ]]; then
+        coverage_url="${nexus_test_full_path}/${platform}_${gcda_tarball}"
+    fi
+
+    eval "./scripts/smw_squad.sh submit_uuu ${platform} ${script_lava_dir} \
+          ${yaml_dir} ${squad_id} \
+          package_url=${nexus_test_full_path}/${pkg_name} \
+          coverage_url=${coverage_url} \
+          job_name=${job_name}"
+fi
 
 #
 # Wait for LAVA execution results

scripts/lava/smw_package.yaml
@@ -14,5 +14,5 @@
           format: Lava-Test Test Definition 1.0
         run:
           steps:
-          - lava-test-case dl_package --shell 'wget REPLACE_PACKAGE_URL/libsmw_package.tar.gz'
+          - lava-test-case dl_package --shell 'curl REPLACE_PACKAGE_URL -o libsmw_package.tar.gz'
           - lava-test-case install_package --shell 'tar -xvf libsmw_package.tar.gz -C / --warning=no-timestamp'

scripts/smw_squad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -eE
+set -x
+
 trap 'error ${LINENO}' ERR
 
 script_name=$0
@@ -32,12 +34,9 @@ suffix_imx_boot="${bamboo_suffix_boot:-"-sd.bin-flash"}"
 prefix_u_boot="${bamboo_prefix_u_boot:-"u-boot-"}"
 suffix_u_boot="${bamboo_suffix_boot:-"_sd-optee.imx"}"
 image_folder="${bamboo_image_folder:-"fsl-imx-internal-wayland"}"
-nexus_repo="${bamboo_nexus_repo:-"IMX-raw_Linux_Internal_Daily_Build"}"
-nexus_url="${bamboo_nexus_url:-"https://nl-nxrm.sw.nxp.com/repository"}"
-nexus_dir="${bamboo_nexus_lava_dir:-"mougins-raw-public-artifacts/LAVA/smw"}"
-bamboo_plan="${bamboo_planKey:-"misc"}"
+nexus_build_repo="${bamboo_nexus_build_repo:-"Linux_Factory"}"
 
-opt_coverage=0
+opt_coverage_url=
 ctest_label=""
 
 platforms_list=(
@@ -139,8 +138,8 @@ function parse_parameters
           opt_job_name="${arg#*=}"
           ;;
 
-        coverage)
-          opt_coverage=1
+        coverage_url=*)
+          opt_coverage_url="${arg#*=}"
           ;;
 
         *)
@@ -171,8 +170,8 @@ function check_file
 
 function check_url
 {
-    status=$(curl -I -s "$1" | head -n 1)
-    if [[ -w ${status} ]]; then
+    status=$(curl --head --silent "$1" | head -n 1)
+    if echo "$status" | grep -q 404; then
       printf "File %s doesn't exist\n" "$1"
       exit 1
     fi
@@ -244,7 +243,7 @@ function squad_submit
       exit 1
     fi
 
-    nexus_find_args="-l nl -nosdcard -r ${nexus_repo} "
+    nexus_find_args="-l nl -nosdcard -r ${nexus_build_repo} "
     nexus_find_args="${nexus_find_args} -i ${image_type}"
     nexus_find_args="${nexus_find_args} -j ${image_folder}"
     nexus_find_args="${nexus_find_args} -o ${opt_job_name}"
@@ -311,7 +310,7 @@ function squad_submit
 
     cat "${yaml_dir}/${smw_setup_yaml}.yaml" > "${yaml_dir}/${smw_tmp_yaml}"
     if [[ ! -z ${opt_package_url} ]]; then
-      check_url "${opt_package_url}/libsmw_package.tar.gz"
+      check_url "${opt_package_url}"
 
       cat "${yaml_dir}/${smw_package_yaml}" >> "${yaml_dir}/${smw_tmp_yaml}"
     fi
@@ -323,10 +322,7 @@ function squad_submit
         cat "${yaml_dir}/${pkcs11_ctest_yaml}"
     } >> "${yaml_dir}/${smw_tmp_yaml}"
 
-    if [[ ${opt_coverage} -eq 1 ]]; then
-      # If code coverage enabled
-      gcda_tarball="${platform}_${opt_job_name}.tar.gz"
-      gcda_tarball="${gcda_tarball// /_}"
+    if [[ -n ${opt_coverage_url} ]]; then
       cat "${yaml_dir}/${code_coverage_yaml}" >> "${yaml_dir}/${smw_tmp_yaml}"
     fi
 
@@ -342,11 +338,10 @@ function squad_submit
     fi
     sed -i "s|REPLACE_CTEST_LABEL|${ctest_label}|" "${filename_job}"
 
-    if [[ ${opt_coverage} -eq 1 ]]; then
-      upload_url="${nexus_url}/${nexus_dir}"
-      if [[ ! -z ${bamboo_plan} ]]; then
-          upload_url="${upload_url}/${bamboo_plan}"
-      fi
+    if [[ -n ${opt_coverage_url} ]]; then
+      # If code coverage enabled
+      gcda_tarball=$(basename "${opt_coverage_url}")
+      upload_url=$(dirname "${opt_coverage_url}")
       sed -i "s|REPLACE_GCDA_FILE|${gcda_tarball}|" "${filename_job}"
       sed -i "s|REPLACE_UPLOAD_URL|${upload_url}|" "${filename_job}"
     fi

commit 4606855b8053dd79c61f09c4d9880c2949beb634

SSMW-218 scripts: fix python deactivate call
Python virtual environment command 'deactivate' exist only if the
virtual environment has been created.

Hence leaving a script before activating the python virtual
environment will exit in error because of 'deactivate' command
not found and tag the bamboo task as failed.

Fixes: c1dbd26a3cb (SSMW-215 scripts: upload package to test in Nexus)

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

scripts/bamboo/smw_bamboo_config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Define all bamboo common share function/macro and constants
 
+# Flag indicating if python virtual environment activated or not
+python_vvenv_on=0
+
 # Get bamboo variables
 bamboo_plan="${bamboo_planKey:-"misc"}"
 
@@ -21,7 +24,9 @@ gcda_tarball="${gcda_tarball// /_}"
 #
 # End vvenv
 function exit_vvenv {
-    deactivate
+    if [[ ${python_vvenv_on} -eq 1 ]]; then
+        deactivate
+    fi
 }
 
 # Start venv
@@ -29,6 +34,8 @@ function start_vvenv {
     venv_dir="venv_smw"
     eval "python3 -m venv ${venv_dir}"
     eval "source ${venv_dir}/bin/activate"
+
+    python_vvenv_on=1
 }
 
 #
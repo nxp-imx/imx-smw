nxp-imx/imx-smw

commit 9ab0a0d8e96154a0684b80711431977ab13ac6e7

SSMW-233 scripts: Add i.MX8ULP EVK platform
Add i.MX8ULP EVK platform build and test

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

scripts/bamboo/smw_lava_run.sh
@@ -23,6 +23,7 @@ function usage()
 
     $(basename "$0") <platform> <type> coverage
       <platform> : Mandatory platform
+      <deploy>   : [option] board binary deployement (default: 'uuu' or 'uboot')
       <type>     : [option] build type ('release' or 'debug', default: 'release')
       coverage   : [option] enable the code coverage tool
 
@@ -39,6 +40,7 @@ shift
 
 opt_type=rel
 opt_coverage=
+opt_deploy=submit_uuu
 
 if [[ $# -ne 0 ]]; then
     for arg in "$@"
@@ -56,6 +58,13 @@ if [[ $# -ne 0 ]]; then
                 opt_type=rel
                 ;;
 
+            uuu)
+                opt_deploy=submit_uuu
+                ;;
+
+            uboot)
+                opt_deploy=submit
+                ;;
 
             *)
                 echo "Unknown argument \"${arg}\""
@@ -88,7 +97,7 @@ if [[ $(is_pr) -eq 0 ]] && [[ $(is_release_build) -eq 1 ]]; then
     # check code change neither do a code coverage report
     #
 
-    if [[ ! -z ${opt_coverage} ]]; then exit 0; fi
+    if [[ -n ${opt_coverage} ]]; then exit 0; fi
 
     eval "./scripts/smw_squad.sh submit_uuu ${platform} \
           ${script_lava_dir} ${yaml_dir} ${squad_id} job_name=${job_name}"
@@ -112,7 +121,7 @@ else
         coverage_url="${nexus_test_full_path}/${platform}_${gcda_tarball}"
     fi
 
-    eval "./scripts/smw_squad.sh submit_uuu ${platform} ${script_lava_dir} \
+    eval "./scripts/smw_squad.sh ${opt_deploy} ${platform} ${script_lava_dir} \
           ${yaml_dir} ${squad_id} \
           package_url=${nexus_test_full_path}/${pkg_name} \
           coverage_url=${coverage_url} \

scripts/smw_configure.sh
@@ -45,6 +45,10 @@ case ${platform} in
 	opt_seco=1
 	;;
 
+    imx8ulpevk)
+        optee_plat="imx-mx8ulpevk"
+	;;
+
     imx8mmevk)
         optee_plat="imx-mx8mmevk"
 	;;

scripts/smw_squad.sh
@@ -40,6 +40,7 @@ opt_coverage_url=
 ctest_label=""
 
 platforms_list=(
+    imx8ulpevk \
     imx8mmevk \
     imx7dsabresd \
     imx8qxpc0mek)
@@ -184,7 +185,7 @@ function install_lavacli
     #
     parse_parameters "$@"
 
-    if [[ ! -z ${opt_token} ]]; then
+    if [[ -n ${opt_token} ]]; then
       lava_token="{opt_token}"
     fi
 
@@ -234,7 +235,7 @@ function squad_submit
 
     parse_parameters "$@"
 
-    if [[ ! -z ${opt_token} ]]; then
+    if [[ -n ${opt_token} ]]; then
       squad_token="{opt_token}"
     fi
 
@@ -290,6 +291,18 @@ function squad_submit
         uboot_mmc_cnt="0x800"
         ;;
 
+      imx8ulpevk)
+        smw_setup_yaml="${smw_setup_yaml}"
+        bootimage="-m ${prefix_imx_boot}${platform}-sd.bin-flash_singleboot_m33"
+        nexus_find_args="${nexus_find_args} -d imx8ulp-evk.dtb"
+        nexus_find_args="${nexus_find_args} -k ${prefix_image}${platform}.bin"
+        kernel_type="image"
+        nexus_find_args="${nexus_find_args} -b ${prefix_rootfs}${platform}"
+        nexus_find_args="${nexus_find_args} -v imx8ulp-evk-no-usb"
+        uboot_mmc_blk="0x0"
+        uboot_mmc_cnt="0x2000"
+        ;;
+
       *)
         printf "Platform %s not supported\n" "${platform}"
         display_platforms
@@ -304,12 +317,12 @@ function squad_submit
 
     check_file "${script_dir}" "${devops_script}"
 
-    if [[ ! -z ${opt_ctest_label} ]]; then
+    if [[ -n ${opt_ctest_label} ]]; then
       ctest_label="-L ${opt_ctest_label}"
     fi
 
     cat "${yaml_dir}/${smw_setup_yaml}.yaml" > "${yaml_dir}/${smw_tmp_yaml}"
-    if [[ ! -z ${opt_package_url} ]]; then
+    if [[ -n ${opt_package_url} ]]; then
       check_url "${opt_package_url}"
 
       cat "${yaml_dir}/${smw_package_yaml}" >> "${yaml_dir}/${smw_tmp_yaml}"
@@ -333,7 +346,7 @@ function squad_submit
     sed -i "s|REPLACE_UBOOT_MMC_CNT|${uboot_mmc_cnt}|" "${filename_job}"
     sed -i "s|REPLACE_KERNEL_TYPE|${kernel_type}|" "${filename_job}"
 
-    if [[ ! -z ${opt_package_url} ]]; then
+    if [[ -n ${opt_package_url} ]]; then
       sed -i "s|REPLACE_PACKAGE_URL|${opt_package_url}|" "${filename_job}"
     fi
     sed -i "s|REPLACE_CTEST_LABEL|${ctest_label}|" "${filename_job}"
@@ -353,7 +366,7 @@ function squad_submit
 
     eval "${script_dir}/${devops_script} ${nexus_find_args}"
 
-    if [[ ! -z ${opt_package_url} ]]; then
+    if [[ -n ${opt_package_url} ]]; then
         printf "PACKAGE_URL = %s\n" "${opt_package_url}"
     fi
 

commit c870b7a9d82b5cc43f6ed083a03de0bbdd29c4fa

SSMW-241 scripts: Fix weekly lava submit job
Wrong command when submitting a weekly lava job on 8ULP board with
bamboo CI.

Fixes: 9ab0a0d8e96 (SSMW-233 scripts: Add i.MX8ULP EVK platform)

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

scripts/bamboo/smw_lava_run.sh
@@ -99,7 +99,7 @@ if [[ $(is_pr) -eq 0 ]] && [[ $(is_release_build) -eq 1 ]]; then
 
     if [[ -n ${opt_coverage} ]]; then exit 0; fi
 
-    eval "./scripts/smw_squad.sh submit_uuu ${platform} \
+    eval "./scripts/smw_squad.sh ${opt_deploy} ${platform} \
           ${script_lava_dir} ${yaml_dir} ${squad_id} job_name=${job_name}"
 else
     #
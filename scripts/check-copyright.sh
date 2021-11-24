nxp-imx/imx-smw

commit fdc0cb465a0079bfa728d612120e4ba96f77692e

SSMW-236 scripts: Add check copyright header
Add script to check the source copyright header.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

scripts/check-copyright.sh
@@ -1,0 +1,132 @@
+#!/bin/bash
+
+# Check-copyright script
+# Check file NXP copyright and date
+
+set -u
+
+if tty --quiet <&2 ; then
+    W="$(tput bold)$(tput setaf 1)WARNING$(tput sgr0)"
+else
+    W="WARNING"
+fi
+
+: "${CHECK_STYLE_IGNORE:=.check-style.ignore}"
+: "${FILE_EXT:=c|h|cpp|in}"
+
+ret_error=0
+
+function usage() {
+  local myscript
+
+  myscript=$(basename "$0")
+  echo "Usage: $myscript [--working]                 Check working area"
+  echo "       $myscript <commit>                    Check specific commit"
+  echo "       $myscript --diff <commit1> <commit2>  Check diff commit1...commit2"
+  echo "       $myscript --cached                    Check staging area"
+  echo "       $myscript --help                      This help"
+  exit 1
+}
+
+function is_ignored() {
+    while read -r ignore_pattern ; do
+        case $f in
+            $ignore_pattern)
+                return 0
+                ;;
+        esac
+    done < <(grep -Ev '^[[:blank:]]*(#|$)' "${CHECK_STYLE_IGNORE}")
+
+    return 1
+}
+
+function _copyright() {
+    for f in $1 ; do
+        if is_ignored "${f}" ; then
+            continue
+        fi
+
+        # Search copyright in 20 first lines, with current year date
+        head -n 20 "$f" | grep "[Cc]opyright 20..* NXP" | \
+              grep "$(date +%Y)" > /dev/null
+        if [ $? -ne 0 ]; then
+            echo "$W: $f misses correct copyright info." >&2
+            ret_error=1
+        fi
+    done
+}
+
+function checkworking() {
+    files=$(git diff --name-only --diff-filter=ACMR -- . | \
+            grep -E "\.(${FILE_EXT})$")
+    _copyright "$files"
+}
+
+function checkstaging() {
+    files=$(git diff --cached --name-only --diff-filter=ACMR | \
+            grep -E "\.(${FILE_EXT})$")
+    _copyright "$files"
+}
+
+function checkcommit() {
+    files=$(git diff-tree --no-commit-id --name-only --diff-filter=ACMR -r "$1" | \
+            grep -E "\.(${FILE_EXT})$")
+    _copyright "$files"
+
+}
+
+function checkdiff() {
+    for c in $(git rev-list "$1..$2")
+    do
+        echo ""
+        echo "========================================================"
+        checkcommit "$c"
+        echo "========================================================"
+    done
+}
+
+opt_action="${1:---working}"
+
+case "${opt_action}" in
+    --cached)
+        echo "Check staging area"
+        checkstaging
+        ;;
+
+    --diff)
+        echo "Check diff ($2 $3)"
+        checkdiff "$2" "$3"
+        ;;
+
+    --working)
+        echo "Check working area"
+        checkworking
+        ;;
+
+    --help|-h)
+        usage
+        ;;
+
+    *)
+        echo "Check commit $1"
+        checkcommit "$1"
+        ;;
+
+esac
+
+if [ ${ret_error} -eq 1 ] ; then
+    (
+        echo ""
+        echo "Wrong Copyrights detected in some files."
+        echo "The copyright notice should look like:"
+        echo ""
+        echo "   /*"
+        echo "    * Copyright $(date +%Y) NXP"
+        echo "    */"
+        echo ""
+        echo "Refer to https://confluence.sw.nxp.com/display/OSS/File+Header for details"
+        echo ""
+    ) >&2
+fi
+
+exit ${ret_error}

scripts/bamboo/smw_checkcopyright.sh
@@ -1,0 +1,23 @@
+#!/bin/bash
+# Check Copyright script
+set -ex
+
+bash_dir=$(dirname "${BASH_SOURCE[0]}")
+source "${bash_dir}/smw_bamboo_config.sh"
+
+cc_tool="${bash_dir}/../check-copyright.sh"
+
+if [ ! -e "${cc_tool}" ]; then
+    echo "WARNING: Check Copyright not run"
+    exit 0
+fi
+
+# Check if the branch is a PR
+if [[ $(is_pr) -eq 1 ]] ; then
+    head_pr="origin/${bamboo_repository_pr_targetBranch}"
+    head_src="origin/${bamboo_repository_pr_sourceBranch}"
+
+    # Run check copyright on every commit of the PR
+    ${cc_tool} --diff "${head_pr}" "${head_src}"
+fi
+

scripts/git-hooks/pre-commit.d/check-copyright
@@ -1,61 +1,15 @@
-#! /bin/bash
+#!/bin/bash
+set -eu
 
-set -u
-
-if tty --quiet <&2 ; then
-    W="$(tput bold)$(tput setaf 1)WARNING$(tput sgr0)"
-else
-    W="WARNING"
-fi
-
-: "${CHECK_STYLE_IGNORE:=.check-style.ignore}"
-
-function is_ignored() {
-    TEST_FILE="$1"
-    while read -r ignore_pattern ; do
-        case $FILE in
-            $ignore_pattern)
-                return 0
-                ;;
-        esac
-    done < <(grep -Ev '^[[:blank:]]*(#|$)' "${CHECK_STYLE_IGNORE}")
-
-    return 1
+# Get the Git Top level directory
+function get_toplevel() {
+	local topdir
+	topdir=$(git rev-parse --show-toplevel)
+	echo "$topdir"
 }
 
-RETURN=0
+# Get the git root directory
+GIT_TOPLEVEL=$(get_toplevel)
+TOOL_SH="$GIT_TOPLEVEL"/scripts/check-copyright.sh
 
-FILES=$(git diff --cached --name-only --diff-filter=ACMR | grep -E "\.(c|h|cpp)$")
-for FILE in $FILES ; do
-
-    if is_ignored "${FILE}" ; then
-        continue
-    fi
-
-    # search copyright in 20 first lines, with current year date
-    head -n 20 "$FILE" | grep "[Cc]opyright 20..* NXP" | grep $(date +%Y) > /dev/null
-    if [ $? -ne 0 ]; then
-        if [ $RETURN -eq 0 ] ; then
-            echo >&2
-        fi
-        echo "$W: $FILE misses correct copyright info." >&2
-        RETURN=1
-    fi
-done
-
-if [ $RETURN -eq 1 ] ; then
-    (
-        echo ""
-        echo "Wrong Copyrights detected in some files."
-        echo "The copyright notice should look like:"
-        echo ""
-        echo "      /*"
-        echo "       * Copyright $(date +%Y) NXP"
-        echo "       */"
-        echo ""
-        echo "Refer to https://nww.wiki.nxp.com/display/OSS/Copyright+Usage for details"
-        echo ""
-    ) >&2
-fi
-
-exit $RETURN
+$TOOL_SH --cached

commit 0206a87916611c60de62646b2a8e8af59006ce47

SSMW-246 scripts: Fix check copyright script
When a file is deleted, don't check it. Just print a WARNING message.

Fixes: fdc0cb465a0 (SSMW-236 scripts: Add check copyright header)

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

scripts/check-copyright.sh
@@ -46,6 +46,11 @@ function _copyright() {
             continue
         fi
 
+	if [[ ! -e ${f} ]]; then
+	    echo "WARNING: File $f deleted"
+	    continue
+	fi
+
         # Search copyright in 20 first lines, with current year date
         head -n 20 "$f" | grep "[Cc]opyright 20..* NXP" | \
               grep "$(date +%Y)" > /dev/null
@@ -80,6 +85,7 @@ function checkdiff() {
     do
         echo ""
         echo "========================================================"
+	echo " commit $c "
         checkcommit "$c"
         echo "========================================================"
     done
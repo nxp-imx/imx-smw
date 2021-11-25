nxp-imx/imx-smw

commit 01a08c5a1b3e746b3d5a4b04f7e69a3b34971ce6

SSMW-231 core: config: Add support of PSA_DEFAULT tag
- Parse PSA_DEFAULT tag in configuration file
- Store configured value in database

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/config/parser.c
@@ -246,8 +246,8 @@ end:
 int read_range(char **start, char *end, unsigned int *min, unsigned int *max)
 {
 	int status = SMW_STATUS_OK;
-	char *cur = *start;
 
+	char *cur = *start;
 	unsigned int m;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
@@ -342,6 +342,7 @@ int read_params_name(char **start, char *end, char *dest)
 int skip_param(char **start, char *end)
 {
 	int status = SMW_STATUS_OK;
+
 	char *cur = *start;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
@@ -374,8 +375,8 @@ int read_names(char **start, char *end, unsigned long *bitmap,
 	       const char *const array[], unsigned int size)
 {
 	int status = SMW_STATUS_OK;
-	char *cur = *start;
 
+	char *cur = *start;
 	char buffer[SMW_CONFIG_MAX_STRING_LENGTH + 1];
 	unsigned int id;
 
@@ -417,7 +418,6 @@ static bool read_operation(char **start, char *end,
 	bool skip = false;
 
 	int status = SMW_STATUS_OK;
-
 	char *cur = *start;
 	char buffer[SMW_CONFIG_MAX_STRING_LENGTH + 1];
 	enum operation_id operation_id = OPERATION_ID_INVALID;
@@ -490,10 +490,8 @@ static bool read_subsystem(char **start, char *end, int *return_status)
 	bool skip = false;
 
 	int status = SMW_STATUS_OK;
-
 	char *cur = *start;
 	char buffer[SMW_CONFIG_MAX_STRING_LENGTH + 1];
-
 	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
 	enum load_method_id load_method_id = LOAD_METHOD_ID_INVALID;
 
@@ -532,12 +530,14 @@ static bool read_subsystem(char **start, char *end, int *return_status)
 		status = SMW_STATUS_OK;
 	if (status != SMW_STATUS_OK)
 		goto end;
+
 	SMW_DBG_PRINTF(INFO, "Start/stop method name: %s\n", buffer);
 
 	/* Secure Subsystem start/stop method id */
 	status = get_load_method_id(buffer, &load_method_id);
 	if (status != SMW_STATUS_OK)
 		goto end;
+
 	SMW_DBG_PRINTF(DEBUG, "Start/stop method id: %d\n", subsystem_id);
 
 	skip_insignificant_chars(&cur, end);
@@ -580,6 +580,53 @@ end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %s\n", __func__,
 		       skip ? "TRUE" : "FALSE");
 	return skip;
+}
+
+static int get_psa_default_subsystem(char **start, char *end)
+{
+	int status = SMW_STATUS_OK;
+
+	char *cur = *start;
+	char buffer[SMW_CONFIG_MAX_SUBSYSTEM_NAME_LENGTH + 1];
+	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!detect_tag(&cur, end, psa_default_tag))
+		goto end;
+
+	skip_insignificant_chars(&cur, end);
+
+	if (equal != *cur) {
+		status = SMW_STATUS_SYNTAX_ERROR;
+		goto end;
+	}
+	cur++;
+
+	skip_insignificant_chars(&cur, end);
+
+	status = read_string(&cur, end, buffer,
+			     SMW_CONFIG_MAX_SUBSYSTEM_NAME_LENGTH, semicolon);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	SMW_DBG_PRINTF(INFO, "PSA default secure subsystem name: %s\n", buffer);
+
+	/* Secure Subsystem id */
+	status = smw_config_get_subsystem_id(buffer, &subsystem_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	SMW_DBG_PRINTF(DEBUG, "PSA default secure subsystem id: %d\n",
+		       subsystem_id);
+
+	set_psa_default_subsystem(subsystem_id);
+
+	*start = cur;
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
 }
 
 static int verify_version(char **start, char *end)
@@ -647,6 +694,11 @@ int parse(char *buffer, unsigned int size, unsigned int *offset)
 		goto end;
 
 	skip_insignificant_chars(&cur, end);
+
+	/* PSA default subsystem */
+	status = get_psa_default_subsystem(&cur, end);
+	if (status != SMW_STATUS_OK)
+		goto end;
 
 	/* List of Secure Subsystems */
 	while (cur < end) {

core/config/common.h
@@ -239,6 +239,17 @@ int parse(char *buffer, unsigned int size, unsigned int *offset);
 void init_database(bool reset);
 
 /**
+ * set_psa_default_subsystem() - Set the PSA default Secure Subsystem.
+ * @id: Secure Subsystem ID.
+ *
+ * This function sets the PSA default Secure Subsystem.
+ *
+ * Return:
+ * none.
+ */
+void set_psa_default_subsystem(enum subsystem_id id);
+
+/**
  * set_bit() - Set a bit in a bitmap.
  * @bitmap: Bitmap.
  * @size: Size of the bitmap in bits.
@@ -252,10 +263,10 @@ void init_database(bool reset);
 void set_bit(unsigned long *bitmap, unsigned int size, unsigned int offset);
 
 /**
- * set_subsystem_configured() - Set a Secure subsystem as configured.
+ * set_subsystem_configured() - Set a Secure Subsystem as configured.
  * @id: Secure Subsystem ID.
  *
- * This function sets a Secure subsystem as configured.
+ * This function sets a Secure Subsystem as configured.
  *
  * Return:
  * none.
@@ -263,23 +274,23 @@ void set_bit(unsigned long *bitmap, unsigned int size, unsigned int offset);
 void set_subsystem_configured(enum subsystem_id id);
 
 /**
- * is_subsystem_configured() - Tell if a Secure subsystem is configured.
+ * is_subsystem_configured() - Tell if a Secure Subsystem is configured.
  * @id: Secure Subsystem ID.
  *
- * This function tells if Secure subsystem is configured.
+ * This function tells if Secure Subsystem is configured.
  *
  * Return:
- * * true:	- the Secure subsystem is configured.
- * * false:	- the Secure subsystem is not configured.
+ * * true:	- the Secure Subsystem is configured.
+ * * false:	- the Secure Subsystem is not configured.
  */
 bool is_subsystem_configured(enum subsystem_id id);
 
 /**
- * set_subsystem_load_method() - Set a Secure subsystem load method.
+ * set_subsystem_load_method() - Set a Secure Subsystem load method.
  * @id: Secure Subsystem ID.
  * @load_method_id: Load method ID.
  *
- * This function sets a Secure subsystem load method.
+ * This function sets a Secure Subsystem load method if not already set.
  *
  * Return:
  * none.

core/config/database.c
@@ -64,11 +64,22 @@ void init_database(bool reset)
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
+	database.psa_default_subsystem_id = SUBSYSTEM_ID_INVALID;
+
 	for (i = 0; i < SUBSYSTEM_ID_NB; i++)
 		init_subsystem(&database.subsystem[i], reset);
 
 	for (i = 0; i < OPERATION_ID_NB; i++)
 		database.operation[i] = SUBSYSTEM_ID_INVALID;
+}
+
+void set_psa_default_subsystem(enum subsystem_id id)
+{
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	SUBSYSTEM_ID_ASSERT(id);
+
+	database.psa_default_subsystem_id = id;
 }
 
 void set_bit(unsigned long *bitmap, unsigned int size, unsigned int offset)

core/config/database.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020 NXP
+ * Copyright 2020-2021 NXP
  */
 
 #define LOAD_METHOD_ID_DEFAULT LOAD_METHOD_ID_AT_CONFIG_LOAD_UNLOAD
@@ -16,6 +16,7 @@ struct subsystem {
 };
 
 struct database {
+	enum subsystem_id psa_default_subsystem_id;
 	struct subsystem subsystem[SUBSYSTEM_ID_NB];
 	enum subsystem_id operation[OPERATION_ID_NB];
 };

core/config/database_debug.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020 NXP
+ * Copyright 2020-2021 NXP
  */
 
 #include "smw_status.h"
@@ -26,6 +26,9 @@ void print_database(void)
 	unsigned long operations_bitmap;
 	struct smw_utils_list *operations_caps_list;
 
+	SMW_DBG_PRINTF(INFO, "PSA default subsystem: %d\n",
+		       database.psa_default_subsystem_id);
+
 	SMW_DBG_PRINTF(INFO, "Secure subsystems capabilities:\n");
 	for (i = 0; i < SUBSYSTEM_ID_NB; i++) {
 		subsystem = &database.subsystem[i];
@@ -50,6 +53,7 @@ void print_database(void)
 		if (configured)
 			smw_utils_list_print(operations_caps_list);
 	}
+
 	SMW_DBG_PRINTF(INFO, "Default subsystems:\n");
 	for (i = 0; i < OPERATION_ID_NB; i++) {
 		SMW_DBG_PRINTF(INFO, "    [%d] = %d\n", i,

core/config/tag.c
@@ -23,6 +23,7 @@ const char *operation_tag = "[SECURITY_OPERATION]";
 /* Parameters tags */
 const char *version_tag = "VERSION";
 const char *default_tag = "DEFAULT;";
+const char *psa_default_tag = "PSA_DEFAULT";
 const char *key_type_values = "KEY_TYPE_VALUES";
 const char *key_size_range = "KEY_SIZE_RANGE";
 const char *hash_algo_values = "HASH_ALGO_VALUES";

core/config/tag.h
@@ -26,6 +26,7 @@ extern const char *operation_tag;
 /* Parameters tags */
 extern const char *version_tag;
 extern const char *default_tag;
+extern const char *psa_default_tag;
 extern const char *key_type_values;
 extern const char *key_size_range;
 extern const char *hash_algo_values;

commit 00639a3ad2ea4114dc512edc69a503ee09198124

SSMW-247 core: config: Fix parsing of PSA_DEFAULT tag
- Insignificant chars after the next semicolon must be skipped
otherwise next tag is not detected.
- Call skip_insignificant_chars() after get_psa_default_subsystem().

Fixes: 01a08c5a1b3 (SSMW-231 core: config: Add support of PSA_DEFAULT tag)

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/config/parser.c
@@ -711,6 +711,8 @@ int parse(char *buffer, unsigned int size, unsigned int *offset)
 	if (status != SMW_STATUS_OK)
 		goto end;
 
+	skip_insignificant_chars(&cur, end);
+
 	/* List of Secure Subsystems */
 	while (cur < end) {
 		/* Secure Subsystem tag */
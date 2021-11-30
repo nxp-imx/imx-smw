nxp-imx/imx-smw

commit 7c6c0f21b8700d732912056d6949567675d69a74

SSMW-76 core: subsystems: Add support of key operations in TEE
- Implement Normal World side of the TEE subsystem key manager.

- Key operation are: generate key and delete key.

- Software limitation:
	- Generate: ECDSA_BRAINPOOL_R1, ECDSA_BRAINPOOL_T1,
		    DSA_SM2_FP and SM4 are not supported.

Signed-off-by: Remi Koman <remi.koman@nxp.com>

core/subsystems/tee/tee.c
@@ -1,0 +1,137 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2020 NXP
+ */
+
+#include <tee_client_api.h>
+
+#include "operations.h"
+#include "subsystems.h"
+#include "debug.h"
+#include "smw_osal.h"
+#include "global.h"
+#include "utils.h"
+#include "tee.h"
+#include "tee_subsystem.h"
+#include "smw_status.h"
+
+/**
+ * struct tee_subsystem - Context between Normal World and Secure World.
+ * @context: Optee OS TA context.
+ * @session: Optee OS TA session.
+ */
+struct tee_subsystem {
+	TEEC_Context context;
+	TEEC_Session session;
+};
+
+/* SMW-OPTEE OS global context */
+static struct tee_subsystem tee_ctx;
+
+__attribute__((weak)) bool tee_key_handle(enum operation_id operation_id,
+					  void *args, int *status)
+{
+	return false;
+}
+
+/**
+ * load() - Load optee os subsystem.
+ *
+ * Return:
+ * SMW_STATUS_SUBSYSTEM_LOAD_FAILURE	- Failure.
+ * SMW_STATUS_OK			- Success.
+ */
+static int load(void)
+{
+	TEEC_Result tee_res = TEEC_ERROR_GENERIC;
+	TEEC_UUID ta_uuid = SMW_TA_UUID;
+	int status = SMW_STATUS_SUBSYSTEM_LOAD_FAILURE;
+	uint32_t err_origin = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	tee_res = TEEC_InitializeContext(NULL, &tee_ctx.context);
+	if (tee_res != TEEC_SUCCESS) {
+		SMW_DBG_PRINTF(ERROR, "Can't init TEE context: 0x%x\n",
+			       tee_res);
+		goto exit;
+	}
+
+	tee_res = TEEC_OpenSession(&tee_ctx.context, &tee_ctx.session, &ta_uuid,
+				   TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
+	if (tee_res != TEEC_SUCCESS)
+		SMW_DBG_PRINTF(ERROR, "Can't open TEE Session: 0x%x\n",
+			       tee_res);
+	else
+		status = SMW_STATUS_OK;
+
+exit:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+/**
+ * unload() - Unload optee os subsystem.
+ *
+ * Return:
+ * SMW_STATUS_OK	- Success.
+ */
+static int unload(void)
+{
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	TEEC_CloseSession(&tee_ctx.session);
+	TEEC_FinalizeContext(&tee_ctx.context);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, SMW_STATUS_OK);
+	return SMW_STATUS_OK;
+}
+
+/**
+ * execute() - Execute operation.
+ * @op_id: Operation ID.
+ * @args: Operation arguments.
+ *
+ * Return:
+ * SMW_STATUS_OK			- Operation succeeded.
+ * SMW_STATUS_OPERATION_NOT_SUPPORTED	- Operation not supported.
+ * SMW_STATUS_SUBSYSTEM_FAILURE		- Operation failed.
+ */
+static int execute(enum operation_id op_id, void *args)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (tee_key_handle(op_id, args, &status))
+		;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+int execute_tee_cmd(uint32_t cmd_id, TEEC_Operation *op)
+{
+	TEEC_Result tee_res = TEEC_ERROR_GENERIC;
+	int status = SMW_STATUS_SUBSYSTEM_FAILURE;
+	uint32_t err_origin = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	tee_res = TEEC_InvokeCommand(&tee_ctx.session, cmd_id, op, &err_origin);
+
+	if (tee_res == TEEC_SUCCESS)
+		status = SMW_STATUS_OK;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static struct subsystem_func func = { .load = load,
+				      .unload = unload,
+				      .execute = execute };
+
+struct subsystem_func *smw_tee_get_func(void)
+{
+	return &func;
+}

CMakeLists.txt
@@ -32,6 +32,7 @@ if(CMAKE_BUILD_TYPE STREQUAL "Debug")
 endif()
 
 option(ENABLE_HSM "Enable HSM subsystem" ON)
+option(ENABLE_TEE "Enable TEE subsystem" ON)
 
 option(ENABLE_KEYMGR_MODULE "Link the Key manager" ON)
 cmake_dependent_option(ENABLE_HASH "Enable Hash" ON "ENABLE_KEYMGR_MODULE" OFF)

core/subsystems/CMakeLists.txt
@@ -2,5 +2,9 @@ if(ENABLE_HSM)
     add_subsystem(hsm)
 endif()
 
+if(ENABLE_TEE)
+    add_subsystem(tee)
+endif()
+
 set(OBJ_LIST ${OBJ_LIST} PARENT_SCOPE)
 set(EXT_LIBS ${EXT_LIBS} PARENT_SCOPE)

core/subsystems/tee/CMakeLists.txt
@@ -1,0 +1,85 @@
+set(OBJ_LIST ${OBJ_LIST} $<TARGET_OBJECTS:tee> PARENT_SCOPE)
+
+macro(find_teec_lib export_dir found)
+    find_file(TEEC_LIB_FILE ${TEEC_LIB_NAME} ${export_dir}/usr/lib)
+    find_path(TEEC_INCLUDE_PATH tee_client_api.h ${export_dir}/usr/include)
+
+    if(TEEC_LIB_FILE AND TEEC_INCLUDE_PATH)
+        set(${found} True)
+    else()
+        set(${found} False)
+    endif()
+endmacro()
+
+set(TEEC_LIB_NAME "libteec.a")
+
+#
+# As by default TEE subsystem is ON, if OPTEE_CLIENT_EXPORT_DIR is not defined
+# it's set to the path to imx-optee-client source files that should be located
+# at the same level as smw source files.
+# If defined, make path absolute if it's not the case
+#
+if(NOT DEFINED OPTEE_CLIENT_EXPORT_DIR)
+    set(OPTEE_CLIENT_EXPORT_DIR ${CMAKE_SOURCE_DIR}/../imx-optee-client)
+elseif(NOT IS_ABSOLUTE ${OPTEE_CLIENT_EXPORT_DIR})
+        set(OPTEE_CLIENT_EXPORT_DIR
+            "${CMAKE_SOURCE_DIR}/${OPTEE_CLIENT_EXPORT_DIR}")
+endif()
+
+#
+# Look for optee client library and export files
+#
+find_teec_lib(${OPTEE_CLIENT_EXPORT_DIR} TEEC_LIB_FOUND)
+
+#
+# If library and export files not found build imx optee client
+#
+if(NOT TEEC_LIB_FOUND)
+    find_file(TEEC_MAKEFILE Makefile ${OPTEE_CLIENT_EXPORT_DIR})
+
+    if(TEEC_MAKEFILE)
+        set(ENV{CC} ${CMAKE_C_COMPILER})
+        set(ENV{AR} ${CMAKE_AR})
+        set(OUTPUT_DIR ${CMAKE_BINARY_DIR}/optee-client)
+
+        get_filename_component(TEEC_SOURCES ${TEEC_MAKEFILE} DIRECTORY)
+
+        message(STATUS "Building OPTEE Client")
+        execute_process(COMMAND make O=${OUTPUT_DIR}
+                        WORKING_DIRECTORY ${TEEC_SOURCES}
+                        RESULT_VARIABLE TEEC_BUILD_RES)
+
+        if(NOT ${TEEC_BUILD_RES} EQUAL 0)
+            message(FATAL_ERROR "\nFailed to build OPTEE Client from "
+                                "${OPTEE_CLIENT_EXPORT_DIR}\n")
+        endif()
+
+        set(OPTEE_CLIENT_EXPORT_DIR ${OUTPUT_DIR}/export)
+        message(STATUS "OPTEE Client export directory:"
+                       " ${OPTEE_CLIENT_EXPORT_DIR}")
+
+        find_teec_lib(${OPTEE_CLIENT_EXPORT_DIR} TEEC_LIB_FOUND)
+        if(NOT TEEC_LIB_FOUND)
+            message(FATAL_ERROR "\nOPTEE Client export directory not valid\n")
+        endif()
+    else()
+        message(FATAL_ERROR "\nOPTEE Client can't be build, Makefile"
+                            " not found in ${OPTEE_CLIENT_EXPORT_DIR}\n")
+    endif()
+endif()
+
+set(OPTEE_CLIENT_INCLUDE_DIR ${TEEC_INCLUDE_PATH})
+set(TEE_COMMON_INCLUDE ./common)
+set(INCLUDE_DIR ${INCLUDE_DIR} ${TEE_COMMON_INCLUDE}
+                ${OPTEE_CLIENT_INCLUDE_DIR})
+
+add_library(tee OBJECT "")
+set_target_properties(tee PROPERTIES C_VISIBILITY_PRESET hidden)
+target_sources(tee PRIVATE
+               tee.c
+               $<$<BOOL:${ENABLE_KEYMGR_MODULE}>:keymgr.c>)
+target_include_directories(tee PRIVATE ${INCLUDE_DIR})
+
+set(EXT_LIBS ${EXT_LIBS}
+    $<$<BOOL:${TEEC_LIB_FILE}>:${TEEC_LIB_FILE}>
+    PARENT_SCOPE)

core/subsystems/tee/common/tee_subsystem.h
@@ -1,0 +1,31 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2020 NXP
+ */
+
+#ifndef TEE_SUBSYSTEM_H
+#define TEE_SUBSYSTEM_H
+
+#define SMW_TA_UUID                                                            \
+	{                                                                      \
+		0x11b5c4aa, 0x6d20, 0x11ea,                                    \
+		{                                                              \
+			0xbc, 0x55, 0x02, 0x42, 0xac, 0x13, 0x00, 0x03         \
+		}                                                              \
+	}
+
+/* TEE key type */
+enum tee_key_type {
+	TEE_KEY_TYPE_ID_ECDSA,
+	TEE_KEY_TYPE_ID_AES,
+	TEE_KEY_TYPE_ID_DES,
+	TEE_KEY_TYPE_ID_DES3,
+	TEE_KEY_TYPE_ID_NB,
+	TEE_KEY_TYPE_ID_INVALID
+};
+
+/* Key manager commands */
+#define CMD_GENERATE_KEY 0
+#define CMD_DELETE_KEY	 1
+
+#endif /* TEE_SUBSYSTEM_H */

core/subsystems/tee/keymgr.c
@@ -1,0 +1,318 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2020 NXP
+ */
+
+#include <tee_client_api.h>
+
+#include "operations.h"
+#include "subsystems.h"
+#include "debug.h"
+#include "smw_osal.h"
+#include "global.h"
+#include "utils.h"
+#include "config.h"
+#include "keymgr.h"
+#include "tee.h"
+#include "tee_subsystem.h"
+#include "smw_status.h"
+
+#define KEY_ID_MASK UINT32_MAX
+
+/**
+ * struct - Key info
+ * @smw_key_type: SMW key type.
+ * @tee_key_type: TEE key type.
+ * @security_size: Key security size in bits.
+ *
+ * smw_tee_key_info must be ordered from lowest to highest.
+ * Security sizes must be ordered from lowest to highest for one given
+ * key type ID.
+ */
+static struct {
+	enum smw_config_key_type_id smw_key_type;
+	enum tee_key_type tee_key_type;
+	unsigned int security_size;
+} smw_tee_key_info[] = {
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_ECDSA_NIST,
+	  .tee_key_type = TEE_KEY_TYPE_ID_ECDSA,
+	  .security_size = 192 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_ECDSA_NIST,
+	  .tee_key_type = TEE_KEY_TYPE_ID_ECDSA,
+	  .security_size = 224 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_ECDSA_NIST,
+	  .tee_key_type = TEE_KEY_TYPE_ID_ECDSA,
+	  .security_size = 256 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_ECDSA_NIST,
+	  .tee_key_type = TEE_KEY_TYPE_ID_ECDSA,
+	  .security_size = 384 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_ECDSA_NIST,
+	  .tee_key_type = TEE_KEY_TYPE_ID_ECDSA,
+	  .security_size = 521 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_ECDSA_BRAINPOOL_R1,
+	  .tee_key_type = TEE_KEY_TYPE_ID_INVALID },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_ECDSA_BRAINPOOL_T1,
+	  .tee_key_type = TEE_KEY_TYPE_ID_INVALID },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_AES,
+	  .tee_key_type = TEE_KEY_TYPE_ID_AES,
+	  .security_size = 128 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_AES,
+	  .tee_key_type = TEE_KEY_TYPE_ID_AES,
+	  .security_size = 192 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_AES,
+	  .tee_key_type = TEE_KEY_TYPE_ID_AES,
+	  .security_size = 256 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_DES,
+	  .tee_key_type = TEE_KEY_TYPE_ID_DES,
+	  .security_size = 64 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_DES3,
+	  .tee_key_type = TEE_KEY_TYPE_ID_DES3,
+	  .security_size = 128 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_DES3,
+	  .tee_key_type = TEE_KEY_TYPE_ID_DES3,
+	  .security_size = 192 },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_DSA_SM2_FP,
+	  .tee_key_type = TEE_KEY_TYPE_ID_INVALID },
+	{ .smw_key_type = SMW_CONFIG_KEY_TYPE_ID_SM4,
+	  .tee_key_type = TEE_KEY_TYPE_ID_INVALID }
+};
+
+/**
+ * convert_key_type() - Convert SMW key type to TEE key type.
+ * @smw_key_type: SMW key type.
+ * @tee_key_type: TEE key type. Not updated if conversion can't be done.
+ *
+ * Return:
+ * SMW_STATUS_OK			- Success.
+ * SMW_STATUS_OPERATION_NOT_SUPPORTED	- Invalid key type.
+ */
+static int convert_key_type(enum smw_config_key_type_id smw_key_type,
+			    enum tee_key_type *tee_key_type)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+	unsigned int i = 0;
+	unsigned int size = ARRAY_SIZE(smw_tee_key_info);
+	enum tee_key_type tmp_type = TEE_KEY_TYPE_ID_INVALID;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	for (; i < size; i++) {
+		if (smw_tee_key_info[i].smw_key_type == smw_key_type) {
+			tmp_type = smw_tee_key_info[i].tee_key_type;
+			if (tmp_type != TEE_KEY_TYPE_ID_INVALID) {
+				*tee_key_type = tmp_type;
+				status = SMW_STATUS_OK;
+			}
+			break;
+		}
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+/**
+ * check_key_security_size() - Check key security size value.
+ * @key_type: Key type.
+ * @security_size: Key security size in bits.
+ *
+ * Return:
+ * SMW_STATUS_OK			- Size is ok.
+ * SMW_STATUS_OPERATION_NOT_SUPPORTED	- Size is not supported.
+ */
+static int check_key_security_size(enum smw_config_key_type_id key_type,
+				   unsigned int security_size)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+	unsigned int i = 0;
+	unsigned int size = ARRAY_SIZE(smw_tee_key_info);
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	for (; i < size; i++) {
+		if (smw_tee_key_info[i].smw_key_type < key_type)
+			continue;
+		if (smw_tee_key_info[i].smw_key_type > key_type)
+			break;
+		if (smw_tee_key_info[i].security_size < security_size)
+			continue;
+		if (smw_tee_key_info[i].security_size > security_size)
+			break;
+
+		status = SMW_STATUS_OK;
+		break;
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+/**
+ * generate_key() - Generate a key.
+ * @args: Key generation arguments.
+ *
+ * The generated key is stored in tee subsystem storage. It can be transient or
+ * persistent object.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_INVALID_PARAM	- One of the parameters is invalid.
+ * SMW_STATUS_SUBSYSTEM_FAILURE	- Operation failed.
+ * Error code from smw_keymgr_read_attributes().
+ */
+static int generate_key(void *args)
+{
+	TEEC_Operation op = { 0 };
+	int status = SMW_STATUS_INVALID_PARAM;
+	unsigned int attribute_length = 0;
+	unsigned long id = 0;
+	unsigned char *attribute_list = NULL;
+	enum tee_key_type key_type = TEE_KEY_TYPE_ID_INVALID;
+	struct smw_keymgr_attributes attributes = { 0 };
+	struct smw_keymgr_generate_key_args *key_args =
+		(struct smw_keymgr_generate_key_args *)args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!key_args)
+		goto exit;
+
+	/* Convert smw key type to tee key type */
+	status = convert_key_type(key_args->key_type_id, &key_type);
+	if (status != SMW_STATUS_OK) {
+		SMW_DBG_PRINTF(ERROR, "%s: Key type not supported\n", __func__);
+		goto exit;
+	}
+
+	/* Check that key size is supported by optee */
+	status = check_key_security_size(key_args->key_type_id,
+					 key_args->security_size);
+	if (status != SMW_STATUS_OK) {
+		SMW_DBG_PRINTF(ERROR, "%s: Key size not supported\n", __func__);
+		goto exit;
+	}
+
+	/* Parse key attributes */
+	if (key_args->key_attributes_list) {
+		attribute_list = (unsigned char *)key_args->key_attributes_list;
+		attribute_length = key_args->key_attributes_list_length;
+		status = smw_keymgr_read_attributes(attribute_list,
+						    attribute_length,
+						    &attributes);
+		if (status != SMW_STATUS_OK) {
+			SMW_DBG_PRINTF(ERROR, "%s: Undefined key attributes\n",
+				       __func__);
+			goto exit;
+		}
+	}
+
+	/*
+	 * params[0] = Key security size (in bits) and key type
+	 * params[1] = Key ID
+	 * params[2] = Persistent or not
+	 */
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_VALUE_OUTPUT,
+					 TEEC_VALUE_INPUT, TEEC_NONE);
+
+	op.params[0].value.a = key_args->security_size;
+	op.params[0].value.b = key_type;
+	op.params[2].value.a = attributes.persistent_storage;
+
+	/* Invoke TA */
+	status = execute_tee_cmd(CMD_GENERATE_KEY, &op);
+	if (status != SMW_STATUS_OK) {
+		SMW_DBG_PRINTF(ERROR, "%s: Operation failed\n", __func__);
+		goto exit;
+	}
+
+	id = op.params[1].value.a;
+	/* Internal TA ID is 32 bits coded */
+	id |= (unsigned long)SUBSYSTEM_ID_TEE << 32;
+
+	/* Update key_identifier struct */
+	key_args->key_identifier->subsystem_id = SUBSYSTEM_ID_TEE;
+	key_args->key_identifier->key_type_id = key_args->key_type_id;
+	key_args->key_identifier->security_size = key_args->security_size;
+	key_args->key_identifier->is_private = true;
+	key_args->key_identifier->id = id;
+
+	SMW_DBG_PRINTF(DEBUG, "%s: Key #%ld is generated\n", __func__,
+		       key_args->key_identifier->id);
+
+exit:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+/**
+ * delete_key() - Delete a key present in TEE subsystem storage.
+ * @args: Key deletion arguments.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_INVALID_PARAM	- One of the parameters is invalid.
+ * SMW_STATUS_SUBSYSTEM_FAILURE	- Operation failed.
+ */
+static int delete_key(void *args)
+{
+	TEEC_Operation op = { 0 };
+	int status = SMW_STATUS_INVALID_PARAM;
+	uint32_t key_id = 0;
+	struct smw_keymgr_delete_key_args *key_args =
+		(struct smw_keymgr_delete_key_args *)args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!key_args)
+		goto exit;
+
+	/* params[0] = Key ID */
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE, TEEC_NONE,
+					 TEEC_NONE);
+
+	/* Key research is done with Key ID */
+	key_id = (uint32_t)(key_args->key_identifier->id & KEY_ID_MASK);
+	op.params[0].value.a = key_id;
+
+	/* Invoke TA */
+	status = execute_tee_cmd(CMD_DELETE_KEY, &op);
+	if (status != SMW_STATUS_OK)
+		SMW_DBG_PRINTF(ERROR, "%s: Operation failed\n", __func__);
+	else
+		SMW_DBG_PRINTF(DEBUG, "%s: Key #%ld is deleted\n", __func__,
+			       key_args->key_identifier->id);
+
+exit:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+bool tee_key_handle(enum operation_id op_id, void *args, int *status)
+{
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	switch (op_id) {
+	case OPERATION_ID_GENERATE_KEY:
+		*status = generate_key(args);
+		break;
+	case OPERATION_ID_DELETE_KEY:
+		*status = delete_key(args);
+		break;
+	case OPERATION_ID_DERIVE_KEY:
+		*status = SMW_STATUS_OPERATION_NOT_CONFIGURED;
+		break;
+	case OPERATION_ID_UPDATE_KEY:
+		*status = SMW_STATUS_OPERATION_NOT_CONFIGURED;
+		break;
+	case OPERATION_ID_IMPORT_KEY:
+		*status = SMW_STATUS_OPERATION_NOT_CONFIGURED;
+		break;
+	case OPERATION_ID_EXPORT_KEY:
+		*status = SMW_STATUS_OPERATION_NOT_CONFIGURED;
+		break;
+	default:
+		return false;
+	}
+
+	return true;
+}

core/subsystems/tee/tee.h
@@ -1,0 +1,32 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2020 NXP
+ */
+
+#ifndef TEE_H
+#define TEE_H
+
+/**
+ * execute_tee_cmd() - Invoke a command within the SMW TA session.
+ * @cmd_id: ID of the command to execute.
+ * @op: Pointer to the operation structure.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_SUBSYSTEM_FAILURE - Operation failed.
+ */
+int execute_tee_cmd(uint32_t cmd_id, TEEC_Operation *op);
+
+/**
+ * tee_key_handle() - Handle the key operations.
+ * @operation_id: Security Operation ID.
+ * @args: Pointer to a structure of arguments defined by the internal API.
+ * @status: Error code set only if the Security Operation is handled.
+ *
+ * Return:
+ * true		- the Security Operation has been handled.
+ * false	- the Security Operation has not been handled.
+ */
+bool tee_key_handle(enum operation_id operation_id, void *args, int *status);
+
+#endif /* TEE_H */

commit 2523bf85cdf23fe2de31d7a78828f711d6d559d7

SSMW-249 subsystem: tee: Fix unload subsystem
Close the TEE Session and finalize the Context only if respectively
opened and initialized.

Fixes: 7c6c0f21b87 (SSMW-76 core: subsystems: Add support of key
                    operations in TEE)

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/tee/tee.c
@@ -17,11 +17,15 @@
 
 /**
  * struct tee_subsystem - Context between Normal World and Secure World.
+ * @ctx_initialized: Optee OS TA context initialized.
  * @context: Optee OS TA context.
+ * @sess_opened: Optee OS TA session opened.
  * @session: Optee OS TA session.
  */
 struct tee_subsystem {
+	bool ctx_initialized;
 	TEEC_Context context;
+	bool sess_opened;
 	TEEC_Session session;
 };
 
@@ -105,6 +109,9 @@ static int load(void)
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
+	/* Initialize the TEE context */
+	memset(&tee_ctx, 0, sizeof(tee_ctx));
+
 	tee_res = TEEC_InitializeContext(NULL, &tee_ctx.context);
 	if (tee_res != TEEC_SUCCESS) {
 		SMW_DBG_PRINTF(ERROR, "Can't init TEE context: 0x%x\n",
@@ -112,13 +119,18 @@ static int load(void)
 		goto exit;
 	}
 
+	tee_ctx.ctx_initialized = true;
+
 	tee_res = TEEC_OpenSession(&tee_ctx.context, &tee_ctx.session, &ta_uuid,
 				   TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
-	if (tee_res != TEEC_SUCCESS)
+	if (tee_res != TEEC_SUCCESS) {
 		SMW_DBG_PRINTF(ERROR, "Can't open TEE Session: 0x%x\n",
 			       tee_res);
-	else
-		status = SMW_STATUS_OK;
+		goto exit;
+	}
+
+	tee_ctx.sess_opened = true;
+	status = SMW_STATUS_OK;
 
 exit:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
@@ -135,8 +147,11 @@ static int unload(void)
 {
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	TEEC_CloseSession(&tee_ctx.session);
-	TEEC_FinalizeContext(&tee_ctx.context);
+	if (tee_ctx.sess_opened)
+		TEEC_CloseSession(&tee_ctx.session);
+
+	if (tee_ctx.ctx_initialized)
+		TEEC_FinalizeContext(&tee_ctx.context);
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, SMW_STATUS_OK);
 	return SMW_STATUS_OK;
91a838fdd9dacb57f20f2281de13ea0559b07774
Cedric Neveux <cedric.neveux@nxp.com>
2021-12-03 14:24:16 +0100

    SSMW-240 public: Add osal exported APIs
    Add OSAL APIs:
     - smw_osal_lib_init: initialize the library
     - smw_osal_set_subsystem_info: set subsystem information
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- public/smw_osal.h
+++ public/smw_osal.h
@@ -1,10 +1,15 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2021 NXP
+ * Copyright 2021-2022 NXP
  */
 
 #ifndef __SMW_OSAL_H__
 #define __SMW_OSAL_H__
+
+#include <stddef.h>
+
+#include "smw_status.h"
+#include "smw_strings.h"
 
 /**
  * DOC:
@@ -29,4 +34,59 @@
  */
 const char *smw_osal_latest_subsystem_name(void);
 
+/**
+ * smw_osal_lib_init() - Initialize the SMW library
+ *
+ * This function must be the first function called by the application opening
+ * a library instance.
+ * It loads the subsystem configuration set in the linux environment
+ * variable SMW_CONFIG_FILE.
+ *
+ * Return:
+ * SMW_STATUS_OK                   - Library initialization success
+ * SMW_STATUS_LIBRARY_ALREADY_INIT - Library already initialized
+ * otherwise any of the smw status
+ */
+enum smw_status_code smw_osal_lib_init(void);
+
+/* Size of the TEE TA UUID string size including the null terminator */
+#define TEE_TA_UUID_SIZE_MAX 37
+
+/**
+ * struct tee_info - TEE Subsystem information
+ * @ta_uuid: TA UUID
+ */
+struct tee_info {
+	char ta_uuid[TEE_TA_UUID_SIZE_MAX];
+};
+
+/**
+ * struct se_info - Secure Enclave information
+ * @storage_id: Key storage identifier
+ * @storage_nonce: Key storage nonce
+ * @storage_replay: Replay attack counter
+ */
+struct se_info {
+	unsigned int storage_id;
+	unsigned int storage_nonce;
+	unsigned short storage_replay;
+};
+
+/**
+ * smw_osal_set_subsystem_info() - Set the Subsystem configuration information
+ * @subsystem: Subsystem name
+ * @info: Subsystem information
+ * @info_size: Size in bytes of @info parameter
+ *
+ * This function must be called before a subsystem is loaded.
+ *
+ * Return:
+ * SMW_STATUS_OK                - Success
+ * SMW_STATUS_SUBSYSTEM_LOADED  - Subsystem is already loaded
+ * SMW_STATUS_INVALID_PARAM     - Function parameter error
+ * SMW_STATUS_ALLOC_FAILURE     - Allocation failure
+ * SMW_STATUS_UNKNOWN_NAME      - Subsystem unknown
+ */
+enum smw_status_code smw_osal_set_subsystem_info(smw_subsystem_t subsystem,
+						 void *info, size_t info_size);
 #endif /* __SMW_OSAL_H__ */

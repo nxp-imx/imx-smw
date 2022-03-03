nxp-imx/imx-smw

commit a0496312a9c87a8999caf88e04977e86c2def6fd

SSMW-159 subsystem: hsm: Fix key generation
Add transient or persistent info in key_info
bitmap argument.

Define transient and persistent key group ID.

Use strict operation flag when FLUSH_KEY attribute
is set (write keys of the same group in the NVM).

Delete master key info.

Signed-off-by: Remi Koman <remi.koman@nxp.com>

core/subsystems/hsm/keymgr.c
@@ -17,6 +17,15 @@
 
 #include "common.h"
 
+/*
+ * Hardcoded values due to limitation of SMW's HSM subsystem support. Transient
+ * and persistent keys can't be part of the same key group and user can't set
+ * the key group ID.
+ * May have to be change in other version.
+ */
+#define PERSISTENT_KEY_GROUP 0
+#define TRANSIENT_KEY_GROUP  1
+
 /* Key type IDs must be ordered from lowest to highest.
  * Security sizes must be ordered from lowest to highest
  * for 1 given Key type ID.
@@ -143,9 +152,18 @@ static int generate_key(struct hdl *hdl, void *args)
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	op_generate_key_args.key_info = HSM_KEY_INFO_MASTER;
-	if (generate_key_args->key_attributes.persistent_storage)
-		op_generate_key_args.key_info |= HSM_KEY_INFO_PERSISTENT;
+	if (generate_key_args->key_attributes.persistent_storage) {
+		op_generate_key_args.key_info = HSM_KEY_INFO_PERSISTENT;
+		op_generate_key_args.key_group = PERSISTENT_KEY_GROUP;
+	} else {
+		op_generate_key_args.key_info = HSM_KEY_INFO_TRANSIENT;
+		op_generate_key_args.key_group = TRANSIENT_KEY_GROUP;
+	}
+
+	if (generate_key_args->key_attributes.flush_key)
+		op_generate_key_args.flags |=
+			HSM_OP_KEY_GENERATION_FLAGS_STRICT_OPERATION;
+
 	op_generate_key_args.out_key = out_key;
 
 	SMW_DBG_PRINTF(VERBOSE,
@@ -401,6 +419,11 @@ static int delete_key(struct hdl *hdl, void *args)
 			      &manage_key_args.key_type);
 	if (status != SMW_STATUS_OK)
 		goto end;
+
+	if (key_descriptor->identifier.persistent)
+		manage_key_args.key_group = PERSISTENT_KEY_GROUP;
+	else
+		manage_key_args.key_group = TRANSIENT_KEY_GROUP;
 
 	SMW_DBG_PRINTF(VERBOSE,
 		       "[%s (%d)] Call hsm_manage_key()\n"

commit 43f23a4c6d22cf34e00f4d68dfca2c449f5ef1e1

SSMW-284 subsystems: hsm: Fix TLS Key exchange group
Fix missing of the TLS Key group management. The HSM
impose the key shared group to be transient key group.

Fixes: a0496312a9c (SSMW-159 subsystem: hsm: Fix key generation)

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/inc/keymgr.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2021 NXP
+ * Copyright 2020-2022 NXP
  */
 
 #ifndef __KEYMGR_H__
@@ -8,6 +8,15 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+
+/*
+ * Hardcoded values due to limitation of SMW's HSM subsystem support. Transient
+ * and persistent keys can't be part of the same key group and user can't set
+ * the key group ID.
+ * May have to be change in other version.
+ */
+#define PERSISTENT_KEY_GROUP 0
+#define TRANSIENT_KEY_GROUP  1
 
 /* Default RSA public exponent is 65537, which has a length of 3 bytes */
 #define DEFAULT_RSA_PUB_EXP_LEN 3

core/subsystems/hsm/keymgr.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2021 NXP
+ * Copyright 2020-2022 NXP
  */
 
 #include "smw_status.h"
@@ -16,15 +16,6 @@
 #include "keymgr.h"
 
 #include "common.h"
-
-/*
- * Hardcoded values due to limitation of SMW's HSM subsystem support. Transient
- * and persistent keys can't be part of the same key group and user can't set
- * the key group ID.
- * May have to be change in other version.
- */
-#define PERSISTENT_KEY_GROUP 0
-#define TRANSIENT_KEY_GROUP  1
 
 /* Key type IDs must be ordered from lowest to highest.
  * Security sizes must be ordered from lowest to highest

core/subsystems/hsm/keymgr_derive_tls12.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021 NXP
+ * Copyright 2021-2022 NXP
  */
 
 #include "smw_status.h"
@@ -185,6 +185,8 @@ static int build_mac_key_id(unsigned long long *id,
 		identifier.id = hsm_id;
 		identifier.security_size = kdf_info->mac_security_size;
 		identifier.subsystem_id = SUBSYSTEM_ID_HSM;
+		/* Only transient key are generated */
+		identifier.persistent = false;
 
 		*id = smw_keymgr_build_key_id(&identifier);
 	}
@@ -205,6 +207,8 @@ static int build_master_key_id(unsigned long long *id, unsigned int hsm_id)
 		identifier.id = hsm_id;
 		identifier.security_size = TLS12_MASTER_SECRET_SEC_SIZE;
 		identifier.subsystem_id = SUBSYSTEM_ID_HSM;
+		/* Only transient key are generated */
+		identifier.persistent = false;
 
 		*id = smw_keymgr_build_key_id(&identifier);
 	}
@@ -227,6 +231,8 @@ static int build_enc_key_id(unsigned long long *id,
 		identifier.id = hsm_id;
 		identifier.security_size = kdf_info->enc_security_size;
 		identifier.subsystem_id = SUBSYSTEM_ID_HSM;
+		/* Only transient key are generated */
+		identifier.persistent = false;
 
 		*id = smw_keymgr_build_key_id(&identifier);
 	}
@@ -420,7 +426,9 @@ int derive_tls12(struct hdl *hdl, struct smw_keymgr_derive_key_args *args)
 		op_hsm_args.ke_input_size = key_base_len;
 	}
 
+	/* Only Transient keys generated */
 	op_hsm_args.shared_key_info = HSM_KEY_INFO_TRANSIENT;
+	op_hsm_args.shared_key_group = TRANSIENT_KEY_GROUP;
 
 	SMW_DBG_PRINTF(VERBOSE,
 		       "[%s (%d)] Call hsm_key_exchange()\n"
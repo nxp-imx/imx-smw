8db0ae7f29df203516a6041e1d9479da9cb8a158
Bruno De Smet <bruno.de.smet@nxp.com>
2022-04-28 15:48:30 +0200

    SSMW-260 core: Add support to hash one go in PSA API
    - implement functions related to hash one go
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- core/psa/crypto.c
+++ core/psa/crypto.c
@@ -16,6 +16,25 @@
 
 #include "util_status.h"
 
+#define HASH_ALGO(_id, _name, _length, _block_size)                            \
+	{                                                                      \
+		.psa_alg_id = _id, .smw_alg_name = _name, .length = _length,   \
+		.block_size = _block_size                                      \
+	}
+
+static struct hash_algo_info {
+	psa_algorithm_t psa_alg_id;
+	smw_hash_algo_t smw_alg_name;
+	size_t length;
+	size_t block_size;
+} hash_algo_info[] = { HASH_ALGO(PSA_ALG_MD5, "MD5", 16, 64),
+		       HASH_ALGO(PSA_ALG_SHA_1, "SHA1", 20, 64),
+		       HASH_ALGO(PSA_ALG_SHA_224, "SHA224", 28, 64),
+		       HASH_ALGO(PSA_ALG_SHA_256, "SHA256", 32, 64),
+		       HASH_ALGO(PSA_ALG_SHA_384, "SHA384", 48, 128),
+		       HASH_ALGO(PSA_ALG_SHA_512, "SHA512", 64, 128),
+		       HASH_ALGO(PSA_ALG_SM3, "SM3", 32, 64) };
+
 enum smw_status_code call_smw_api(enum smw_status_code (*api)(void *a),
 				  void *args,
 				  struct smw_config_psa_config *config,
@@ -41,6 +60,57 @@ static smw_subsystem_t get_subsystem_name(struct smw_config_psa_config *config)
 		return smw_config_get_subsystem_name(config->subsystem_id);
 
 	return NULL;
+}
+
+static struct hash_algo_info *get_hash_algo_info(psa_algorithm_t alg)
+{
+	unsigned int i;
+	unsigned int size = ARRAY_SIZE(hash_algo_info);
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	for (i = 0; i < size; i++) {
+		if (hash_algo_info[i].psa_alg_id == alg)
+			return &hash_algo_info[i];
+	}
+
+	return NULL;
+}
+
+static smw_hash_algo_t get_hash_algo_name(psa_algorithm_t alg)
+{
+	struct hash_algo_info *info = get_hash_algo_info(alg);
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!info)
+		return NULL;
+
+	return info->smw_alg_name;
+}
+
+__export size_t psa_hash_block_length(psa_algorithm_t alg)
+{
+	struct hash_algo_info *info = get_hash_algo_info(alg);
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!info)
+		return 0;
+
+	return info->block_size;
+}
+
+__export size_t psa_hash_length(psa_algorithm_t alg)
+{
+	struct hash_algo_info *info = get_hash_algo_info(alg);
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!info)
+		return 0;
+
+	return info->length;
 }
 
 __export psa_status_t psa_aead_abort(psa_aead_operation_t *operation)
@@ -538,15 +608,28 @@ __export psa_status_t psa_hash_compare(psa_algorithm_t alg,
 				       size_t input_length, const uint8_t *hash,
 				       size_t hash_length)
 {
-	(void)alg;
-	(void)input;
-	(void)input_length;
-	(void)hash;
-	(void)hash_length;
+	psa_status_t psa_status;
+	uint8_t hash_computed[PSA_HASH_MAX_SIZE];
+	size_t hash_computed_length = 0;
+	unsigned int i;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	return PSA_ERROR_NOT_SUPPORTED;
+	psa_status =
+		psa_hash_compute(alg, input, input_length, hash_computed,
+				 sizeof(hash_computed), &hash_computed_length);
+
+	if (psa_status != PSA_SUCCESS)
+		return psa_status;
+
+	if (hash_computed_length != hash_length)
+		return PSA_ERROR_INVALID_SIGNATURE;
+
+	for (i = 0; i < hash_length; i++)
+		if (hash[i] != hash_computed[i])
+			return PSA_ERROR_INVALID_SIGNATURE;
+
+	return PSA_SUCCESS;
 }
 
 __export psa_status_t psa_hash_compute(psa_algorithm_t alg,
@@ -554,16 +637,33 @@ __export psa_status_t psa_hash_compute(psa_algorithm_t alg,
 				       size_t input_length, uint8_t *hash,
 				       size_t hash_size, size_t *hash_length)
 {
-	(void)alg;
-	(void)input;
-	(void)input_length;
-	(void)hash;
-	(void)hash_size;
-	(void)hash_length;
+	enum smw_status_code status = SMW_STATUS_OK;
+	struct smw_hash_args args = { 0 };
+	struct smw_config_psa_config config;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	return PSA_ERROR_NOT_SUPPORTED;
+	if (!smw_utils_is_lib_initialized())
+		return PSA_ERROR_BAD_STATE;
+
+	args.algo_name = get_hash_algo_name(alg);
+	if (!args.algo_name)
+		return PSA_ERROR_NOT_SUPPORTED;
+
+	smw_config_get_psa_config(&config);
+
+	args.subsystem_name = get_subsystem_name(&config);
+	args.input = (unsigned char *)input;
+	args.input_length = input_length;
+	args.output = hash;
+	args.output_length = hash_size;
+
+	status = call_smw_api((enum smw_status_code(*)(void *))smw_hash, &args,
+			      &config, &args.subsystem_name);
+
+	*hash_length = args.output_length;
+
+	return util_smw_to_psa_status(status);
 }
 
 __export psa_status_t psa_hash_finish(psa_hash_operation_t *operation,

nxp-imx/imx-smw

commit dcbfa29b69c8c19cff674c46386ec165112a56fb

SSMW-151 subsystem: hsm: Add cipher operation
Add support of Cipher ONESHOT operation

Signed-off-by: Remi Koman <remi.koman@nxp.com>

core/subsystems/tee/cipher.c
@@ -85,21 +85,6 @@ static unsigned int get_nb_key_id(struct smw_crypto_cipher_args *args)
 	return nb_ids;
 }
 
-static unsigned int get_nb_key_buffer(struct smw_crypto_cipher_args *args)
-{
-	unsigned int i;
-	unsigned int nb_buffers = 0;
-
-	/* Key is defined as buffer if ID is not set and buffer set */
-	for (i = 0; i < args->nb_keys; i++) {
-		if (!args->keys_desc[i]->identifier.id &&
-		    smw_crypto_get_cipher_key(args, i))
-			nb_buffers++;
-	}
-
-	return nb_buffers;
-}
-
 static int import_key_buffer(struct smw_crypto_cipher_args *args,
 			     enum smw_config_key_type_id key_type,
 			     uint32_t **key_ids, unsigned int *nb_ids)
@@ -112,7 +97,7 @@ static int import_key_buffer(struct smw_crypto_cipher_args *args,
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	*nb_ids = get_nb_key_buffer(args);
+	*nb_ids = smw_crypto_get_cipher_nb_key_buffer(args);
 
 	/* No key is defined as buffer, no key to import */
 	if (!*nb_ids)

core/crypto/cipher.c
@@ -587,3 +587,19 @@ smw_crypto_set_cipher_init_handle(struct smw_crypto_cipher_args *args,
 	if (args && args->init_pub && args->init_pub->context)
 		args->init_pub->context->handle = handle;
 }
+
+unsigned int
+smw_crypto_get_cipher_nb_key_buffer(struct smw_crypto_cipher_args *args)
+{
+	unsigned int i;
+	unsigned int nb_buffers = 0;
+
+	/* Key is defined as buffer if ID is not set and buffer set */
+	for (i = 0; i < args->nb_keys; i++) {
+		if (!args->keys_desc[i]->identifier.id &&
+		    smw_crypto_get_cipher_key(args, i))
+			nb_buffers++;
+	}
+
+	return nb_buffers;
+}

core/subsystems/hsm/cipher.c
@@ -1,0 +1,187 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2021 NXP
+ */
+
+#include "hsm_api.h"
+
+#include "smw_status.h"
+
+#include "smw_osal.h"
+#include "global.h"
+#include "debug.h"
+#include "utils.h"
+#include "operations.h"
+#include "subsystems.h"
+#include "cipher.h"
+
+#include "common.h"
+
+#define CIPHER_ONE_GO_ALGO_ID(_key_type, _cipher_mode)                         \
+	{                                                                      \
+		.key_type = SMW_CONFIG_KEY_TYPE_ID_##_key_type,                \
+		.cipher_mode = SMW_CONFIG_CIPHER_MODE_ID_##_cipher_mode,       \
+		.hsm_algo =                                                    \
+			HSM_CIPHER_ONE_GO_ALGO_##_key_type##_##_cipher_mode    \
+	}
+
+static const struct {
+	enum smw_config_key_type_id key_type;
+	enum smw_config_cipher_mode_id cipher_mode;
+	hsm_op_cipher_one_go_algo_t hsm_algo;
+} CIPHER_ONE_GO_ALGO_ID[] = { CIPHER_ONE_GO_ALGO_ID(AES, CBC),
+			      CIPHER_ONE_GO_ALGO_ID(AES, ECB) };
+
+static int get_cipher_one_go_algo(enum smw_config_key_type_id key_type,
+				  enum smw_config_cipher_mode_id cipher_mode,
+				  hsm_op_cipher_one_go_algo_t *hsm_algo)
+{
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_SIZE(CIPHER_ONE_GO_ALGO_ID); i++) {
+		if (key_type == CIPHER_ONE_GO_ALGO_ID[i].key_type &&
+		    cipher_mode == CIPHER_ONE_GO_ALGO_ID[i].cipher_mode) {
+			*hsm_algo = CIPHER_ONE_GO_ALGO_ID[i].hsm_algo;
+			return SMW_STATUS_OK;
+		}
+	}
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
+static int get_cipher_one_go_flags(enum smw_config_cipher_op_type_id smw_op,
+				   hsm_op_cipher_one_go_flags_t *hsm_op)
+{
+	switch (smw_op) {
+	case SMW_CONFIG_CIPHER_OP_ID_ENCRYPT:
+		*hsm_op = HSM_CIPHER_ONE_GO_FLAGS_ENCRYPT;
+		break;
+
+	case SMW_CONFIG_CIPHER_OP_ID_DECRYPT:
+		*hsm_op = HSM_CIPHER_ONE_GO_FLAGS_DECRYPT;
+		break;
+
+	default:
+		return SMW_STATUS_INVALID_PARAM;
+	}
+
+	return SMW_STATUS_OK;
+}
+
+static int cipher(struct hdl *hdl, void *args)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+	hsm_err_t err = HSM_NO_ERROR;
+	op_cipher_one_go_args_t op_cipher_args = { 0 };
+	enum smw_config_key_type_id key_type;
+	struct smw_crypto_cipher_args *cipher_args = args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (smw_crypto_get_cipher_nb_key_buffer(cipher_args)) {
+		SMW_DBG_PRINTF(ERROR, "%s: HSM doesn't support keys buffer\n",
+			       __func__);
+		goto end;
+	}
+
+	/* Get 1st key type as reference */
+	key_type = cipher_args->keys_desc[0]->identifier.type_id;
+
+	/* Get HSM algorithm */
+	status = get_cipher_one_go_algo(key_type, cipher_args->mode_id,
+					&op_cipher_args.cipher_algo);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	/* Get HSM operation */
+	status = get_cipher_one_go_flags(cipher_args->op_id,
+					 &op_cipher_args.flags);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	op_cipher_args.output = smw_crypto_get_cipher_output(cipher_args);
+	op_cipher_args.input_size =
+		smw_crypto_get_cipher_input_len(cipher_args);
+
+	/* Get output length feature */
+	if (!op_cipher_args.output) {
+		/* Cipher output length is equal to input length */
+		smw_crypto_set_cipher_output_len(cipher_args,
+						 op_cipher_args.input_size);
+		goto end;
+	}
+
+	/*
+	 * If output length is too short, update is done here (not supported by
+	 * HSM)
+	 */
+	if (smw_crypto_get_cipher_output_len(cipher_args) <
+	    op_cipher_args.input_size) {
+		status = SMW_STATUS_OUTPUT_TOO_SHORT;
+		smw_crypto_set_cipher_output_len(cipher_args,
+						 op_cipher_args.input_size);
+		goto end;
+	}
+
+	op_cipher_args.key_identifier =
+		smw_crypto_get_cipher_key_id(cipher_args, 0);
+	op_cipher_args.iv = smw_crypto_get_cipher_iv(cipher_args);
+	op_cipher_args.iv_size = smw_crypto_get_cipher_iv_len(cipher_args);
+	op_cipher_args.input = smw_crypto_get_cipher_input(cipher_args);
+
+	/*
+	 * If output length is too big HSM returns HSM_INVALID_PARAM, which
+	 * doesn't match SMW API behavior. Then set HSM argument to the correct
+	 * value.
+	 */
+	op_cipher_args.output_size = op_cipher_args.input_size;
+
+	SMW_DBG_PRINTF(VERBOSE,
+		       "[%s (%d)] Call hsm_cipher_one_go()\n"
+		       "cipher_hdl: %d\n"
+		       "op_cipher_one_go_args_t\n"
+		       "    key_identifier: %d\n"
+		       "    iv: %p\n"
+		       "    iv_size: %d\n"
+		       "    cipher_algo: %d\n"
+		       "    flags: %d\n"
+		       "    input: %p\n"
+		       "    output: %p\n"
+		       "    input_size: %d\n"
+		       "    output_size: %d\n",
+		       __func__, __LINE__, hdl->cipher,
+		       op_cipher_args.key_identifier, op_cipher_args.iv,
+		       op_cipher_args.iv_size, op_cipher_args.cipher_algo,
+		       op_cipher_args.flags, op_cipher_args.input,
+		       op_cipher_args.output, op_cipher_args.input_size,
+		       op_cipher_args.output_size);
+
+	err = hsm_cipher_one_go(hdl->cipher, &op_cipher_args);
+	if (err != HSM_NO_ERROR) {
+		SMW_DBG_PRINTF(DEBUG, "hsm_cipher_one_go returned %d\n", err);
+		status = SMW_STATUS_SUBSYSTEM_FAILURE;
+	}
+
+	/* Update output length */
+	smw_crypto_set_cipher_output_len(cipher_args,
+					 op_cipher_args.output_size);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+bool hsm_cipher_handle(struct hdl *hdl, enum operation_id operation_id,
+		       void *args, int *status)
+{
+	switch (operation_id) {
+	case OPERATION_ID_CIPHER:
+		*status = cipher(hdl, args);
+		break;
+
+	default:
+		return false;
+	}
+
+	return true;
+}

core/inc/cipher.h
@@ -196,4 +196,14 @@ void smw_crypto_set_cipher_ctx_reserved(struct smw_crypto_cipher_args *args,
 void smw_crypto_set_cipher_init_handle(struct smw_crypto_cipher_args *args,
 				       void *handle);
 
+/**
+ * smw_crypto_get_cipher_nb_key_buffer() - Get number of keys defined as buffer
+ * @args: Pointer to internal cipher arguments.
+ *
+ * Return:
+ * Number of keys defined as buffer
+ */
+unsigned int
+smw_crypto_get_cipher_nb_key_buffer(struct smw_crypto_cipher_args *args);
+
 #endif /* __CIPHER_H__ */

core/subsystems/hsm/CMakeLists.txt
@@ -26,7 +26,8 @@ target_sources(hsm PRIVATE
                $<$<BOOL:${ENABLE_KEYMGR_MODULE}>:keymgr.c>
                $<$<BOOL:${ENABLE_HASH}>:hash.c>
                $<$<BOOL:${ENABLE_SIGN_VERIFY}>:sign_verify.c>
-               $<$<BOOL:${ENABLE_RNG}>:rng.c>)
+               $<$<BOOL:${ENABLE_RNG}>:rng.c>
+               $<$<BOOL:${ENABLE_CIPHER}>:cipher.c>)
 
 target_include_directories(hsm PRIVATE ${INCLUDE_DIR}
                            ${SECO_INCLUDE_DIRS})

core/subsystems/hsm/common.h
@@ -12,6 +12,7 @@
  * @signature_ver: Signature verification service flow handle
  * @hash: Hash service flow handle
  * @rng: RNG service flow handle
+ * @cipher: Cipher service flow handle
  *
  * This structure stores the HSM handles managed by the SMW library.
  */
@@ -23,6 +24,7 @@ struct hdl {
 	hsm_hdl_t signature_ver;
 	hsm_hdl_t hash;
 	hsm_hdl_t rng;
+	hsm_hdl_t cipher;
 };
 
 /**

core/subsystems/hsm/entry.c
@@ -32,7 +32,8 @@ static struct {
 		   .signature_gen = 0,
 		   .signature_ver = 0,
 		   .hash = 0,
-		   .rng = 0 },
+		   .rng = 0,
+		   .cipher = 0 },
 	  .nvm_status = NVM_STATUS_UNDEF,
 	  .tid = 0,
 	  .mutex = NULL };
@@ -291,12 +292,49 @@ static void close_rng_service(hsm_hdl_t rng_hdl)
 	SMW_DBG_PRINTF(DEBUG, "%s - returned: %d\n", __func__, err);
 }
 
+static int open_cipher_service(hsm_hdl_t key_store_hdl, hsm_hdl_t *cipher_hdl)
+{
+	int status = SMW_STATUS_OK;
+
+	hsm_err_t err = HSM_NO_ERROR;
+	open_svc_cipher_args_t open_svc_cipher_args = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	err = hsm_open_cipher_service(key_store_hdl, &open_svc_cipher_args,
+				      cipher_hdl);
+	if (err != HSM_NO_ERROR) {
+		SMW_DBG_PRINTF(DEBUG, "%s - err: %d\n", __func__, err);
+		status = SMW_STATUS_SUBSYSTEM_FAILURE;
+		goto end;
+	}
+
+	SMW_DBG_PRINTF(DEBUG, "cipher_hdl: %d\n", *cipher_hdl);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static void close_cipher_service(hsm_hdl_t cipher_hdl)
+{
+	hsm_err_t __maybe_unused err;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	SMW_DBG_PRINTF(DEBUG, "cipher_hdl: %d\n", cipher_hdl);
+	err = hsm_close_cipher_service(cipher_hdl);
+	SMW_DBG_PRINTF(DEBUG, "%s - returned: %d\n", __func__, err);
+}
+
 static void reset_handles(void)
 {
 	struct hdl *hdl = &ctx.hdl;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
+	if (hdl->cipher)
+		close_cipher_service(hdl->cipher);
 	if (hdl->rng)
 		close_rng_service(hdl->rng);
 	if (hdl->hash)
@@ -319,6 +357,7 @@ static void reset_handles(void)
 	hdl->signature_ver = 0;
 	hdl->hash = 0;
 	hdl->rng = 0;
+	hdl->cipher = 0;
 }
 
 static void *storage_thread(void *arg)
@@ -473,6 +512,10 @@ int load(void)
 		goto err;
 
 	status = open_rng_service(hdl->session, &hdl->rng);
+	if (status != SMW_STATUS_OK)
+		goto err;
+
+	status = open_cipher_service(hdl->key_store, &hdl->cipher);
 
 err:
 	smw_utils_mutex_unlock(ctx.mutex);
@@ -535,6 +578,17 @@ __weak bool hsm_rng_handle(struct hdl *hdl, enum operation_id operation_id,
 	return false;
 }
 
+__weak bool hsm_cipher_handle(struct hdl *hdl, enum operation_id operation_id,
+			      void *args, int *status)
+{
+	(void)hdl;
+	(void)operation_id;
+	(void)args;
+	(void)status;
+
+	return false;
+}
+
 static int execute(enum operation_id operation_id, void *args)
 {
 	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
@@ -549,8 +603,10 @@ static int execute(enum operation_id operation_id, void *args)
 		goto end;
 	else if (hsm_sign_verify_handle(hdl, operation_id, args, &status))
 		goto end;
+	else if (hsm_rng_handle(hdl, operation_id, args, &status))
+		goto end;
 
-	hsm_rng_handle(hdl, operation_id, args, &status);
+	hsm_cipher_handle(hdl, operation_id, args, &status);
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);

commit 32dbc711c436e548e9a03787d8bd9e057a990707

SSMW-300 subsystem: tee: Fix cipher key import
Fix the key importation in cipher operation when 2 keys are used where
first key is an key id and second key is a buffer (temporary key
importation).

Fixes: dcbfa29b69c (SSMW-151 subsystem: hsm: Add cipher operation)

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/tee/cipher.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021 NXP
+ * Copyright 2021-2022 NXP
  */
 
 #include <tee_client_api.h>
@@ -71,48 +71,18 @@ static int get_tee_cipher_operation(enum smw_config_cipher_op_type_id smw_op,
 	return SMW_STATUS_OK;
 }
 
-static unsigned int get_nb_key_id(struct smw_crypto_cipher_args *args)
-{
-	unsigned int i;
-	unsigned int nb_ids = 0;
-
-	for (i = 0; i < args->nb_keys; i++) {
-		if (args->keys_desc[i]->identifier.id)
-			nb_ids++;
-	}
-
-	return nb_ids;
-}
-
-static int import_key_buffer(struct smw_crypto_cipher_args *args,
-			     enum smw_config_key_type_id key_type,
-			     uint32_t **key_ids, unsigned int *nb_ids)
+static int import_key_buffer(struct smw_keymgr_descriptor *key,
+			     unsigned int *key_id)
 {
 	TEEC_Operation op = { 0 };
 	int status = SMW_STATUS_OK;
-	unsigned int i;
 	struct keymgr_shared_params import_shared_params = { 0 };
-	uint32_t *ids_array = NULL;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	*nb_ids = smw_crypto_get_cipher_nb_key_buffer(args);
-
-	/* No key is defined as buffer, no key to import */
-	if (!*nb_ids)
-		goto end;
-
-	/* Caller must free this memory */
-	ids_array = SMW_UTILS_MALLOC(*nb_ids * sizeof(uint32_t));
-	if (!ids_array) {
-		status = SMW_STATUS_ALLOC_FAILURE;
-		goto end;
-	}
-
-	*key_ids = ids_array;
-
 	/* Key type is common for all keys */
-	status = tee_convert_key_type(key_type, &import_shared_params.key_type);
+	status = tee_convert_key_type(key->identifier.type_id,
+				      &import_shared_params.key_type);
 	if (status != SMW_STATUS_OK)
 		goto end;
 
@@ -128,51 +98,43 @@ static int import_key_buffer(struct smw_crypto_cipher_args *args,
 	op.params[0].tmpref.buffer = &import_shared_params;
 	op.params[0].tmpref.size = sizeof(import_shared_params);
 
-	for (i = 0; i < *nb_ids; i++) {
-		import_shared_params.security_size =
-			args->keys_desc[i]->identifier.security_size;
+	import_shared_params.security_size = key->identifier.security_size;
 
-		op.params[1].tmpref.buffer = smw_crypto_get_cipher_key(args, i);
-		op.params[1].tmpref.size =
-			smw_crypto_get_cipher_key_len(args, i);
+	op.params[1].tmpref.buffer = smw_keymgr_get_private_data(key);
+	op.params[1].tmpref.size = smw_keymgr_get_private_length(key);
 
-		/* Invoke TA */
-		status = execute_tee_cmd(CMD_IMPORT_KEY, &op);
-		if (status != SMW_STATUS_OK) {
-			SMW_DBG_PRINTF(ERROR, "%s: Operation failed\n",
-				       __func__);
-			goto end;
-		}
-
-		ids_array[i] = import_shared_params.id;
+	/* Invoke TA */
+	status = execute_tee_cmd(CMD_IMPORT_KEY, &op);
+	if (status != SMW_STATUS_OK) {
+		SMW_DBG_PRINTF(ERROR, "%s: Operation failed\n", __func__);
+		goto end;
 	}
+
+	*key_id = import_shared_params.id;
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }
 
-static int delete_imported_keys(uint32_t *key_ids, unsigned int nb_keys)
+static int delete_imported_keys(unsigned int key_id)
 {
 	TEEC_Operation op = { 0 };
 	int status = SMW_STATUS_OK;
-	unsigned int i;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	if (!key_ids)
+	if (key_id == INVALID_KEY_ID)
 		goto end;
 
 	/* params[0] = Key ID */
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE, TEEC_NONE,
 					 TEEC_NONE);
 
-	for (i = 0; i < nb_keys; i++) {
-		op.params[0].value.a = key_ids[i];
+	op.params[0].value.a = key_id;
 
-		/* Invoke TA */
-		status = execute_tee_cmd(CMD_DELETE_KEY, &op);
-	}
+	/* Invoke TA */
+	status = execute_tee_cmd(CMD_DELETE_KEY, &op);
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
@@ -182,18 +144,28 @@ end:
 static int cipher_init(struct smw_crypto_cipher_args *args)
 {
 	TEEC_Operation op = { 0 };
-	int status;
-	unsigned int nb_key_ids;
-	unsigned int nb_ephemeral_ids = 0;
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+	int res;
+	unsigned int key_idx;
+	unsigned int key_id;
 	uint32_t param2_type = TEEC_NONE;
-	uint32_t *ephemeral_key_ids = NULL;
 	enum smw_config_key_type_id key_type;
 	struct shared_context context = { 0 };
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
+	if (args->nb_keys > 2)
+		goto end;
+
 	/* Get 1st key type as reference */
 	key_type = args->keys_desc[0]->identifier.type_id;
+
+	/*
+	 * params[0] = TEE Algo ID, TEE Operation
+	 * params[1] = Key ids as integer or as integer array
+	 * params[2] = IV or none
+	 * params[3] = Operation handle
+	 */
 
 	/* Get OPTEE algorithm */
 	status = get_tee_cipher_algo_id(key_type, args->mode_id,
@@ -206,44 +178,32 @@ static int cipher_init(struct smw_crypto_cipher_args *args)
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	/* If some keys are defined as buffer import them */
-	status = import_key_buffer(args, key_type, &ephemeral_key_ids,
-				   &nb_ephemeral_ids);
-	if (status != SMW_STATUS_OK)
-		goto end;
+	for (key_idx = 0; key_idx < args->nb_keys; key_idx++) {
+		key_id = smw_crypto_get_cipher_key_id(args, key_idx);
 
-	/* Get number of keys defined as key ID */
-	nb_key_ids = get_nb_key_id(args);
-
-	/*
-	 * params[0] = TEE Algo ID, TEE Operation
-	 * params[1] = Key ids as integer or as integer array
-	 * params[2] = IV or none
-	 * params[3] = Operation handle
-	 */
-
-	/*
-	 * For one or two key, IDs are shared as integer.
-	 * If in the future, more keys are used this parameter can be used as a
-	 * key IDs array.
-	 */
-	if (args->nb_keys <= 2) {
-		if (nb_ephemeral_ids) {
-			op.params[1].value.a = ephemeral_key_ids[0];
-
-			if (nb_ephemeral_ids == 2)
-				op.params[1].value.b = ephemeral_key_ids[1];
+		/*
+		 * If the key id is not valid, import the key first in
+		 * TEE, then imported key is removed before leaving.
+		 */
+		if (key_id == INVALID_KEY_ID) {
+			/* If some keys are defined as buffer import them */
+			status = import_key_buffer(args->keys_desc[key_idx],
+						   &key_id);
+			if (status != SMW_STATUS_OK)
+				goto end;
 		}
 
-		if (nb_key_ids) {
-			if (!nb_ephemeral_ids)
-				op.params[1].value.a =
-					smw_crypto_get_cipher_key_id(args, 0);
-
-			if (nb_key_ids == 2)
-				op.params[1].value.b =
-					smw_crypto_get_cipher_key_id(args, 1);
-		}
+		/*
+		 * Only 2 keys maximum can be handled in the TEE TA Cipher
+		 * operation.
+		 * The op.params[1].value (a or b) are re-used to delete
+		 * imported key. As this TA operation parameter is an
+		 * input, it can't be overwritten.
+		 */
+		if (key_idx)
+			op.params[1].value.b = key_id;
+		else
+			op.params[1].value.a = key_id;
 	}
 
 	if (smw_crypto_get_cipher_iv(args)) {
@@ -263,22 +223,30 @@ static int cipher_init(struct smw_crypto_cipher_args *args)
 	SMW_DBG_PRINTF_COND(ERROR, status != SMW_STATUS_OK,
 			    "%s: Operation failed\n", __func__);
 
-	/* Delete imported ephemeral keys and update operation context */
-	if (status != SMW_STATUS_OK) {
-		/* TA cipher initialization error code is returned */
-		(void)delete_imported_keys(ephemeral_key_ids, nb_ephemeral_ids);
-	} else {
+	if (status == SMW_STATUS_OK) {
 		smw_crypto_set_cipher_init_handle(args, context.handle);
 		smw_crypto_set_cipher_ctx_reserved(args, tee_get_ctx_ops());
+	}
 
-		status = delete_imported_keys(ephemeral_key_ids,
-					      nb_ephemeral_ids);
+	/* Delete imported ephemeral keys and update operation context */
+	for (key_idx = 0; key_idx < args->nb_keys; key_idx++) {
+		key_id = smw_crypto_get_cipher_key_id(args, key_idx);
+		if (key_id == INVALID_KEY_ID) {
+			/*
+			 * The op.params[1].value (a or b) are re-used to get
+			 * imported key id.
+			 */
+			if (key_idx)
+				key_id = op.params[1].value.b;
+			else
+				key_id = op.params[1].value.a;
+
+			res = delete_imported_keys(key_id);
+			status = (status == SMW_STATUS_OK) ? res : status;
+		}
 	}
 
 end:
-	if (ephemeral_key_ids)
-		SMW_UTILS_FREE(ephemeral_key_ids);
-
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }
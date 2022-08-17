b908f30afa82de2375256ee94ea3bc487f2526a1
Cedric Neveux <cedric.neveux@nxp.com>
2022-08-17 16:25:26 +0200

    SSMW-292 tests: Remove tests not passing because of ELE FW or Library issues.
    - U_ELE_Cipher_001
    - U_ELE_Sign_Verify_001
    - F_ELE_Keymgr_001
    - F_ELE_Thread_001
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- tests/cmake/ele_tests.cmake
+++ tests/cmake/ele_tests.cmake
@@ -6,10 +6,13 @@ set(CMD_SUSPEND ${TEST_KEYMGR_SUSPEND_RESUME_CMD} ${CFG_FILE})
 # Get all test definition files except those in multiple parts
 file(GLOB TESTS ${TEST_DEF_SRC_DIR}/*_${GROUP}_*_???.json)
 
+# Remove failing test due to ELE Library or FW issues
+list(REMOVE_ITEM TESTS ${TEST_DEF_SRC_DIR}/F_ELE_Thread_001.json)
+
 set(CFG_FILES ${SMW_CONFIG_SRC_DIR}/${CFG_FILE})
 add_and_install_tests("${TESTS}" "${CFG_FILES}" "${CMD}")
 
 # Create suspend resume test definition files list and install test
-file(GLOB TESTS ${TEST_DEF_SRC_DIR}/F_${GROUP}_Keymgr_001.?.json)
-
-add_and_install_tests("${TESTS}" "${CFG_FILES}" "${CMD_SUSPEND}")
+#file(GLOB TESTS ${TEST_DEF_SRC_DIR}/F_${GROUP}_Keymgr_001.?.json)
+#
+#add_and_install_tests("${TESTS}" "${CFG_FILES}" "${CMD_SUSPEND}")

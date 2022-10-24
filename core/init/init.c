5c94859c0519cefb1f0e96c07b9fa05852a99b56
Bruno De Smet <bruno.de.smet@nxp.com>
2022-10-24 17:08:02 +0200

    SSMW-325 init: SMW context dynamically allocated
    - SMW context is dyamically allocated when library is initialized
    and freed when library is deinitialized.
    - Implement get_smw_ctx() to get access to the SMW context.
    - Implement get_smw_ops() to get access to the OSAL ops.
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- core/init/init.c
+++ core/init/init.c
@@ -12,7 +12,19 @@
 #include "subsystems.h"
 #include "config.h"
 
-struct smw_ctx g_smw_ctx = { .ops = { 0 }, .start_count = 0 };
+static struct smw_ctx *smw_ctx;
+
+inline struct smw_ctx *get_smw_ctx(void)
+{
+	return smw_ctx;
+}
+
+inline struct smw_ops *get_smw_ops(void)
+{
+	struct smw_ctx *ctx = get_smw_ctx();
+
+	return ctx ? &ctx->ops : NULL;
+}
 
 static int check_ops(const struct smw_ops *ops)
 {
@@ -23,10 +35,8 @@ static int check_ops(const struct smw_ops *ops)
 	    (bool)!ops->critical_section_stop)
 		return -1;
 
-	if ((ops->mutex_init || ops->mutex_destroy || ops->mutex_lock ||
-	     ops->mutex_unlock) &&
-	    !(ops->mutex_init && ops->mutex_destroy && ops->mutex_lock &&
-	      ops->mutex_unlock))
+	if (!ops->mutex_init || !ops->mutex_destroy || !ops->mutex_lock ||
+	    !ops->mutex_unlock)
 		return -1;
 
 	if (!ops->thread_create || !ops->thread_cancel)
@@ -59,16 +69,29 @@ enum smw_status_code smw_init(const struct smw_ops *ops)
 	if (ops->critical_section_start)
 		ops->critical_section_start();
 
-	if (!g_smw_ctx.start_count) {
-		g_smw_ctx.ops = *ops;
+	if (!smw_ctx) {
+		smw_ctx = SMW_UTILS_CALLOC(1, sizeof(struct smw_ctx));
+		if (!smw_ctx) {
+			status = SMW_STATUS_ALLOC_FAILURE;
+			if (ops->critical_section_stop)
+				ops->critical_section_stop();
+			goto end;
+		}
+
+		SMW_DBG_PRINTF(DEBUG, "SMW context allocation\n");
+	}
+
+	if (!smw_ctx->start_count) {
+		smw_ctx->ops = *ops;
+
 		status = smw_config_init();
 	}
 
 	if (status == SMW_STATUS_OK)
-		g_smw_ctx.start_count++;
+		smw_ctx->start_count++;
 
 	SMW_DBG_PRINTF(VERBOSE, "%s - Start count: %d\n", __func__,
-		       g_smw_ctx.start_count);
+		       smw_ctx->start_count);
 
 	if (ops->critical_section_stop)
 		ops->critical_section_stop();
@@ -82,21 +105,34 @@ enum smw_status_code smw_deinit(void)
 {
 	enum smw_status_code status = SMW_STATUS_OK;
 
+	struct smw_ctx *ctx;
+
 	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	ctx = get_smw_ctx();
+
+	if (!ctx)
+		return SMW_STATUS_INVALID_LIBRARY_CONTEXT;
 
 	SMW_UTILS_CRITICAL_SECTION_START;
 
-	if (g_smw_ctx.start_count == 1) {
+	if (ctx->start_count == 1) {
 		status = smw_config_deinit();
 		if (status != SMW_STATUS_OK)
 			goto end;
 	}
 
-	if (g_smw_ctx.start_count >= 1)
-		g_smw_ctx.start_count--;
+	if (ctx->start_count >= 1)
+		ctx->start_count--;
 
 	SMW_DBG_PRINTF(VERBOSE, "%s - Start count: %d\n", __func__,
-		       g_smw_ctx.start_count);
+		       ctx->start_count);
+
+	if (ctx->start_count == 0) {
+		SMW_DBG_PRINTF(DEBUG, "SMW context free\n");
+		SMW_UTILS_FREE(smw_ctx);
+		smw_ctx = NULL;
+	}
 
 end:
 	SMW_UTILS_CRITICAL_SECTION_STOP;

nxp-imx/imx-smw

commit 806e20c1f76db206f2088096b1e8d3befec4a6b4

SSMW-174 scripts: add gcov, coverity and bamboo scripts
Add gcov script to generate code coverage report.
Add coverity script used to build project in coverity tool.
Add bamboo scripts to build, run coverity, run lava and generate
code coverage.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

scripts/smw_coverity_build.sh
@@ -1,0 +1,33 @@
+#!/bin/bash
+set -e
+
+function usage()
+{
+    cat << EOF
+
+    ******************************************************
+    * Usage of Security Middleware Coverity build script *
+    ******************************************************
+
+    Script is cleaning first the SMW targets and then
+    rebuild all targets configured in given directory.
+
+    It is assumed that projects have been configured before calling
+    this script and dependencies built as well.
+
+    $(basename "$0") <dir>
+      <dir> : Mandatory putput build directory
+
+EOF
+    exit 1
+}
+
+if [[ $# -ne 1 ]]; then
+    usage
+fi
+
+# Clean build targets first
+eval "cd $1 && make clean && cd .."
+
+# Rebuild targets
+eval "./scripts/smw_build.sh build out=$1"

scripts/bamboo/smw_bamboo_build.sh
@@ -1,0 +1,82 @@
+#!/bin/bash
+set -e
+
+function usage()
+{
+    cat << EOF
+
+    ****************************************************
+    * Usage of Security Middleware Bamboo build script *
+    ****************************************************
+
+    Script is building the overall Security Middleware project
+    in Bamboo environment.
+    SMW library, PKCS11 and all tests are built in Release and Debug
+    mode.
+
+    Then if build is done for a pull-request validation, packages
+    for each build mode, all files used to run tests including
+    built libraries.
+
+    $(basename "$0") <arch>
+      <arch> : Mandatory architecture aarch32 or aarch64
+
+EOF
+    exit 1
+}
+
+if [[ $# -ne 1 ]]; then
+    usage
+fi
+
+arch="$1"
+build_release="build.${arch}_rel"
+build_debug="build.${arch}_deb"
+
+cd "$bamboo_build_working_directory"/smw
+
+# Delete previous builds
+rm -rf "${build_release}"
+rm -rf "${build_debug}"
+
+daytoday=$(date +%w)
+
+# Check if the branch is a PR and it's daily build
+if [ -z "${bamboo_repository_pr_targetBranch+x}" ]; then
+    if [[ ${daytoday} == "$bamboo_weekly_day_run" ]]; then
+        git clean -xdf
+    fi
+fi
+
+export="./export"
+
+# Delete seco export in case there is a change
+if [[ -e "${export}/usr/include/hsm/" ]]; then
+    rm -f "${export}/usr/include/seco_nvm.h"
+    rm -f "${export}/usr/include/hsm/*.*"
+    rm -f "${export}/usr/lib/seco_nvm_manager.a"
+    rm -f "${export}/usr/lib/hsm_lib.a"
+fi
+
+#
+# Configure, build and package Release build of all targets
+#
+eval "./scripts/smw_configure.sh ${build_release} ${arch}"
+eval "./scripts/smw_build.sh build out=${build_release}"
+eval "./scripts/smw_build.sh package out=${build_release}"
+
+#
+# Configure, build and package Debug build of all targets
+# Enable code coverage
+#
+eval "./scripts/smw_configure.sh ${build_debug} ${arch}"
+eval "./scripts/smw_build.sh configure out=${build_debug} \
+      coverage debug verbose=4"
+eval "./scripts/smw_build.sh build out=${build_debug}"
+eval "./scripts/smw_build.sh package out=${build_debug}"
+
+#
+# Create text file with build information
+#
+echo "ROOT_DIR=$PWD" >> gcno_build_info.txt
+echo "BUILD_DIR=${build_debug}" >> gcno_build_info.txt

scripts/bamboo/smw_code_coverage.sh
@@ -1,0 +1,135 @@
+#!/bin/bash
+set -ex
+
+# Define nexus default url
+nexus_url="${bamboo_nexus_url:-"https://nl-nxrm.sw.nxp.com/repository"}"
+nexus_dir="${bamboo_nexus_lava_dir:-"mougins-raw-public-artifacts/LAVA/smw"}"
+job_name="${bamboo_planKey}-${bamboo_buildNumber}"
+bamboo_plan="${bamboo_planKey:-"misc"}"
+
+function usage()
+{
+    cat << EOF
+
+    *****************************************************
+    * Usage of Security Middleware Code Coverage script *
+    *****************************************************
+
+    Script is generating the GCOV code coverage report
+    in Bamboo environment.
+
+    It uploads the tarball file of the gcda data file
+    generated during the test executing.
+
+    $(basename "$0") <platform>
+      <platform> : Mandatory platform
+
+EOF
+    exit 1
+}
+
+function get_file_info()
+{
+    local pattern="$2"
+    local filename="$3"
+
+    while read -r line
+    do
+        case $line in
+            ${pattern}*)
+                IFS='=' read -ra split_line <<< "$line"
+                break
+                ;;
+        esac
+    done < "${filename}"
+
+    if [[ ${#split_line[@]} -ge 2 ]]; then
+        eval "$1=${split_line[1]}"
+    else
+        printf "${pattern} not found in %s\n" "${filename}"
+        exit 1
+    fi
+}
+
+function check_url
+{
+    status=$(curl -I -s "$1" | head -n 1)
+    if [[ -w ${status} ]]; then
+      printf "File %s doesn't exist\n" "$1"
+      exit 1
+    fi
+}
+
+function gcda_upload_extract
+{
+    #
+    # Upload gdca tarball and untar it in the
+    # build directory
+    #
+    local gcda_filename="${platform}_${job_name}.tar.gz"
+    local gcda_url="${nexus_url}/${nexus_dir}/${bamboo_plan}"
+
+    check_url "${gcda_url}/${gcda_filename}"
+
+    printf "Get the GCDA tarball file\n"
+    eval "curl -O ${gcda_url}/${gcda_filename}"
+
+    printf "Untar %s into %s\n" "$1" "$2"
+    eval "tar -xvmf $1 -C $2"
+
+    eval "curl -n -X DELETE ${gcda_url}/${gcda_filename}"
+}
+
+if [[ $# -lt 1 ]]; then
+    usage
+fi
+
+platform=$1
+
+#
+# Read the gcno build file info
+#
+root_dir=
+build_dir=
+get_file_info root_dir "ROOT_DIR" "gcno_build_info.txt"
+get_file_info build_dir "BUILD_DIR" "gcno_build_info.txt"
+
+#
+# Define code coverage directories
+#
+gcov_report="gcov_report"
+gcov_out="./${gcov_report}"
+gcno_dir="${root_dir}/${build_dir}"
+gcda_dir="${root_dir}/${build_dir}"
+
+lcov_tool="../lcov_tool"
+gcda_tarball="${platform}_${job_name}.tar.gz"
+gcda_tarball="${gcda_tarball// /_}"
+
+# Go to the bamboo build directory where are the gcno files
+eval "cd ${root_dir}"
+
+gcda_upload_extract "${gcda_tarball}" "/"
+
+#
+# Install lcov tool
+#
+eval "./scripts/gcov-gen.sh install lcov=${lcov_tool}"
+
+#
+# Find where the toolchain is installed
+#
+toolchain_path=
+get_file_info toolchain_path "TOOLCHAIN_BIN_PATH" "${build_dir}/CMakeCache.txt"
+
+#
+# Generate gcov report and tar it
+#
+eval "./scripts/gcov-gen.sh report gcno=${gcno_dir} gcda=${gcda_dir} \
+       src=. nomerge out=${gcov_out} conf=./scripts/lcov.rc \
+       lcov=${lcov_tool} gcov=${toolchain_path} title=\"SMW Code Coverage\""
+
+#
+# Copy generated report in current plan to publish artifact
+#
+eval "cp ${gcov_report}.tar.gz $bamboo_build_working_directory"

scripts/bamboo/smw_coverity.sh
@@ -1,0 +1,68 @@
+#!/bin/bash
+set -e
+
+function usage()
+{
+    cat << EOF
+
+    ************************************************
+    * Usage of Security Middleware Coverity script *
+    ************************************************
+
+    Script is preparing the Security Middelware project targets
+    to be analyzed by coverity (built in debug mode).
+    No need to build the project coverity will do it.
+    Then it call coverity script to do the code analysis.
+
+    $(basename "$0") <arch>
+      <arch> : Mandatory architecture aarch32 or aarch64
+
+EOF
+    exit 1
+}
+
+if [[ $# -ne 1 ]]; then
+    usage
+fi
+
+arch="$1"
+
+# Define local variable used in coverity script and yaml
+compiler="${arch}-none-linux-gnu-gcc"
+coverity_yaml="./tools/coverity/config/stec/smw_${arch}.yml"
+
+cd "$bamboo_build_working_directory"/smw
+
+build_debug=build.${arch}_deb
+
+# Delete previous builds
+rm -rf "${build_debug}"
+
+#
+# Configure, build and package Debug build of all targets
+#
+eval "./scripts/smw_configure.sh ${build_debug} ${arch}"
+eval "./scripts/smw_build.sh configure out=${build_debug} debug verbose=4"
+
+cd "$bamboo_build_working_directory"
+
+#
+# Create a copy of the Coverity Yaml script and replace the
+# compiler and build output directory
+#
+
+eval "cp ./tools/coverity/config/stec/smw.yml ${coverity_yaml}"
+
+sed -i "s|REPLACE_COMPILER|${compiler}|" "${coverity_yaml}"
+sed -i "s|REPLACE_BUILD_DIR|${build_debug}|" "${coverity_yaml}"
+
+#
+# Set environment variable used by coverity scripts
+#
+BASE_DIR=$PWD
+
+eval "./tools/coverity-agent/coverity_scan4.sh \
+    --config ${coverity_yaml} \
+    --coverity-dir $bamboo_capability_coveritydir_201909 \
+    --source-path ./smw \
+    --commands-path ./smw"

scripts/bamboo/smw_lava_run.sh
@@ -1,0 +1,81 @@
+#!/bin/bash
+set -ex
+
+function usage()
+{
+    cat << EOF
+
+    *****************************************************
+    * Usage of Security Middleware LAVA test run script *
+    *****************************************************
+
+    Script is preparing the LAVA environment and script
+    to run LAVA test on a given platform.
+    If it's a PR verification, run Release and Debug
+    tests based on the PR built package.
+    Else if it's not a PR run tests using Linux root
+    file system libraries and tests.
+
+    $(basename "$0") <platform>
+      <platform> : Mandatory platform
+
+EOF
+    exit 1
+}
+
+if [[ $# -ne 1 ]]; then
+    usage
+fi
+
+platform="$1"
+
+cd "$bamboo_build_working_directory"/smw
+
+eval "./scripts/smw_squad.sh install"
+
+script_dir="$bamboo_build_working_directory"/lava/bambooIntegrationScripts
+yaml_dir=./tests/lava
+
+daytoday=$(date +%w)
+PR=0
+# Check if the branch is a PR
+if [ ! -z "${bamboo_repository_pr_targetBranch+x}" ] ; then
+    PR=1
+fi
+
+# Fetch all reports from LAVA
+rm -rf logs && mkdir logs
+
+squad_id=$(echo "$bamboo_planRepository_branchName" | tr / _)_${bamboo_buildNumber}
+job_id=${bamboo_planKey}-${bamboo_buildNumber}
+
+if [[ ${daytoday} == "$bamboo_weekly_day_run" ]] && [[ ${PR} == 0 ]]; then
+    #
+    # If executed on Sunday, assume it's a periodic weekly check not a code change
+    #
+    eval "./scripts/smw_squad.sh submit_uuu ${platform} \
+          ${script_dir} ${yaml_dir} ${squad_id} job_name=${job_id}"
+else
+    package_url="https://bamboo1.sw.nxp.com/browse/${job_id}/artifact/shared"
+
+    #
+    # Test Release build
+    #
+    eval "./scripts/smw_squad.sh submit_uuu ${platform} ${script_dir} \
+           ${yaml_dir} ${squad_id} package_url=${package_url}/package_rel \
+           job_name=${job_id}"
+
+    #
+    # Test Debug build and retreive code coverage result
+    #
+    eval "./scripts/smw_squad.sh submit_uuu ${platform} ${script_dir} \
+          ${yaml_dir} ${squad_id} coverage \
+          package_url=${package_url}/package_deb \
+          job_name=${job_id}"
+fi
+
+
+#
+# Wait for LAVA execution results
+#
+eval "./scripts/smw_squad.sh result"

scripts/common_toolchain.cmake
@@ -30,7 +30,7 @@ endif()
 
 set(TOOLCHAIN_PREFIX ${TOOLCHAIN_NAME}-)
 
-get_filename_component(TOOLCHAIN_BIN_PATH ${GCC_BINTOOL} DIRECTORY)
+get_filename_component(TOOLCHAIN_BIN_PATH ${GCC_BINTOOL} DIRECTORY CACHE)
 
 set(CMAKE_C_COMPILER ${TOOLCHAIN_BIN_PATH}/${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})

scripts/gcov-gen.sh
@@ -1,0 +1,449 @@
+#!/bin/bash
+set -eE
+trap 'error ${LINENO}' ERR
+
+#
+# Copy gcno and gcda in the same temporary directory
+# Generate the analyze data file
+# Generate the html report
+#
+
+# Definition of the tools name
+lcov_tool="lcov"
+gcov_tool="gcov"
+genhtml_tool="genhtml"
+
+lcov_git="https://github.com/linux-test-project/lcov.git"
+
+script_name=$0
+opt_gcno_dir=
+opt_gcda_dir=
+opt_merge_dir=
+opt_merge=1
+out_dir="code-coverage"
+config_rc=""
+opt_lcov=
+opt_html=
+opt_gcov_path=
+opt_lcov_path=
+opt_lcov_ver="1.15"
+git_toplevel=
+
+#
+# Return function value (because of set -e trapping)
+# 1 = ok
+# 0 = error
+ret_func=0
+
+# Get the Git Top level directory
+function get_toplevel() {
+    local topdir
+    topdir=$(git rev-parse --show-toplevel)
+    echo "$topdir"
+}
+
+function usage_install()
+{
+    git_toplevel=$(get_toplevel)
+
+    printf "\n"
+    printf "To install the lcov tools\n"
+    printf " %s install lcov=<path> lcov_ver=<ver>\n" "${script_name}"
+    printf "\n"
+    printf "  Install lcov tool from git repo %s\n" "lcov_git"
+    printf "    lcov=     Full path where to install "
+    printf "(default %s/.tmp)\n" "${git_toplevel}"
+    printf "    lcov_ver= LCOV minimal version (default %s)\n" "${opt_lcov_ver}"
+    printf "\n"
+}
+
+function usage_report()
+{
+    printf "\n"
+    printf "To generate code coverage report\n"
+    printf " %s report gcno=<path> gcda=<path> src=<path> [options]\n" "${script_name}"
+    printf "\n"
+    printf "  Merge gcov data files gcno and gcda in temporary directory\n"
+    printf "  Generate the lcov report in HTML format\n"
+    printf "\n"
+    printf "  Mandatory"
+    printf "    gcno=   Base directory where the *.gcno files are\n"
+    printf "    gcda=   Base directory where the *.gcda files are\n"
+    printf "    src=    Base directory where the source (*.h, *.c) files are\n"
+    printf "\n"
+    printf "  Misc:\n"
+    printf "    help    Print this help\n"
+    printf "\n"
+    printf "  Options:\n"
+    printf "    nomerge   If defined, gcno and gcda path must be the same"
+    printf "              \"tmp\" option must not be defined if this option set\n"
+    printf "              (default gcno and gcda are merger in \"tmp\" directory\n"
+    printf "    tmp=      Temporary directory where to merge *.gcno *.gcda"
+    printf "              (default ./tmp)\n"
+    printf "              \"nomerge\" must not be set if this option set\n"
+    printf "    title=    Title of the HTML report add \" \" around string"
+    printf "              (e.g. \"string\")\n"
+    printf "    out=      HMTL report output directory\n"
+    printf "    conf=     HMTL configuration file (*.rc)\n"
+    printf "    gcov=     Full path and name of the gcov tool\n"
+    printf "    lcov=     Full path to lcov tools (lcov/genhtml)\n"
+    printf "    lcov_ver= LCOV minimal version (default %s)\n" "${opt_lcov_ver}"
+    printf "\n"
+}
+
+function usage()
+{
+    printf "\n"
+    printf "*****************************************\n"
+    printf " Usage of Code Coverage GCOV/LCOV script \n"
+    printf "*****************************************\n"
+    usage_install
+    usage_report
+
+    exit 1
+}
+
+
+function parse_param()
+{
+    opt_mandatory=0
+    exp_mandatory=3
+
+    if [[ ${opt_action} == "install" ]]; then
+        exp_mandatory=0
+    fi
+
+    for arg in "$@"
+    do
+        case ${arg} in
+            help)
+                usage
+                ;;
+            gcno=*)
+                opt_gcno_dir="${arg#*=}"
+                opt_mandatory=$((opt_mandatory + 1))
+                ;;
+            gcda=*)
+                opt_gcda_dir="${arg#*=}"
+                opt_mandatory=$((opt_mandatory + 1))
+                ;;
+            src=*)
+                opt_mandatory=$((opt_mandatory + 1))
+                opt_lcov="${opt_lcov} -b ${arg#*=}"
+                ;;
+            tmp=*)
+                opt_merge_dir="${arg#*=}"
+                if [[ ${opt_merge} -eq 0 ]]; then
+                    usage
+                fi
+                ;;
+            nomerge)
+                opt_merge=0
+                if [[ ! -z ${opt_merge_dir} ]]; then
+                    usage
+                fi
+                ;;
+            title=*)
+                opt_html="${opt_html} -t \"${arg#*=}\""
+                ;;
+            out=*)
+                out_dir="${arg#*=}"
+                ;;
+            conf=*)
+                config_rc="${arg#*=}"
+                opt_html="${opt_html} --config-file ${config_rc}"
+                ;;
+            gcov=*)
+                opt_gcov_path="${arg#*=}"
+                ;;
+            lcov=*)
+                opt_lcov_path="${arg#*=}"
+                ;;
+            lcov_ver=*)
+                opt_lcov_ver="${arg#*=}"
+                ;;
+            *)
+                echo "Invalid option: ${arg}"
+                usage
+                ;;
+        esac
+        shift
+    done
+
+    if [[ ${opt_mandatory} -lt ${exp_mandatory} ]]; then
+        usage
+    fi
+
+}
+
+function check_directory()
+{
+    declare -n mydir=$1
+    mydir="${mydir/\~/$HOME}"
+
+    if [[ ! -d "${mydir}" ]]; then
+        printf "%s is not a directory\n" "${mydir}"
+        ret_func=0
+    else
+        ret_func=1
+    fi
+}
+
+function find_file()
+{
+    declare -n retfile=$1
+    local dir=$2
+    local found=""
+    local tofind="${retfile}"
+    local lists
+
+    if [[ $# -eq 3 ]];  then
+        tofind="$3""${tofind}"
+    fi
+
+    lists=$(find "${dir}" -type f -name "${tofind}" -print)
+    for file in "${lists[@]}"
+    do
+        if [[ -f "${file}" ]]; then
+            found="${file}"
+            break
+        fi
+    done
+
+    if [[ -z "${found}" ]]; then
+        printf "%s not found in %s\n" "${tofind}" "${dir}"
+        ret_func=0
+    else
+        retfile="${found}"
+        ret_func=1
+    fi
+}
+
+function install_lcov()
+{
+    local cur_path=$PWD
+
+    if [[ -z "${opt_lcov_path}" ]]; then
+        git_toplevel=$(get_toplevel)
+        opt_lcov_path="${git_toplevel}/.tmp"
+    fi
+
+    check_lcov_tools
+    if [[ ${ret_func} -eq 1 ]]; then
+        printf "LCOV tool already installed\n"
+        return
+    fi
+
+    printf "Installation of lcov in %s\n" "${opt_lcov_path}"
+
+    # Get lcov from git and set to requested tag version
+    eval "mkdir -p ${opt_lcov_path}"
+    eval "cd ${opt_lcov_path}"
+    if [[ -d "lcov" ]]; then
+        printf "Directory already exist. Please check.\n"
+        exit 1
+    fi
+
+    eval "git clone ${lcov_git}"
+    eval "cd ${opt_lcov_path}/lcov"
+    eval "git checkout v${opt_lcov_ver}"
+
+    eval "cd ${cur_path}"
+    find_file lcov_tool "${opt_lcov_path}"
+    if [[ ${ret_func} -eq 0 ]]; then exit 1; fi
+    find_file genhtml_tool "${opt_lcov_path}"
+    if [[ ${ret_func} -eq 0 ]]; then exit 1; fi
+}
+
+function check_lcov_version()
+{
+    #
+    # Check if lcov is installed and version is v1.15 minimum
+    #
+    if ! hash ${lcov_tool} 2>/dev/null; then
+        printf "lcov not installed\n"
+        install_lcov
+    fi
+
+    # Get the current lcov version installed
+    lcov_ver=$(${lcov_tool} -ver 2>&1 | grep -Eo "LCOV version [0-9]\.[0-9]+")
+    lcov_ver=$(echo "${lcov_ver}" | grep -Eo '[0-9]\.[0-9]+')
+
+    IFS='.' read -ra alcov_ver <<< "${lcov_ver}"
+    IFS='.' read -ra aexp_ver <<< "${opt_lcov_ver}"
+
+    match=${#aexp_ver[@]}
+    if [[ ${#alcov_ver[@]} -eq ${#aexp_ver[@]} ]]; then
+        for (( i=0; i<=match; i++ )); do
+            if [[ ${alcov_ver[$i]} -lt ${aexp_ver[$i]} ]]; then
+                break;
+            fi
+            match=$((match-1))
+        done
+    fi
+
+    if [[ ${match} -ne 0 ]]; then
+        printf "Bad version got %s expected %s\n" "${lcov_ver}" "${opt_lcov_ver}"
+        ret_func=0
+    else
+        ret_func=1
+    fi
+}
+
+function check_lcov_tools()
+{
+    if [[ ! -z "${opt_lcov_path}" ]]; then
+        check_directory opt_lcov_path
+        if [[ ${ret_func} -eq 0 ]]; then return; fi
+
+        find_file lcov_tool "${opt_lcov_path}"
+        if [[ ${ret_func} -eq 0 ]]; then return; fi
+        find_file genhtml_tool "${opt_lcov_path}"
+        if [[ ${ret_func} -eq 0 ]]; then return; fi
+    fi
+
+    check_lcov_version
+}
+
+function check_gcov_tools()
+{
+    if [[ ! -z "${opt_gcov_path}" ]]; then
+        check_directory opt_gcov_path
+        if [[ ${ret_func} -eq 0 ]]; then exit 1; fi
+
+        find_file gcov_tool "${opt_gcov_path}" "*"
+        if [[ ${ret_func} -eq 1 ]]; then
+            opt_html="${opt_html} --rc geninfo_gcov_tool=${gcov_tool}"
+        fi
+    fi
+}
+
+function copy_file()
+{
+    eval "cp -R $1 $2"
+}
+
+function lcov_generate()
+{
+    eval "${lcov_tool} --gcov-tool ${gcov_tool} -c ${opt_lcov} \
+           -d ${opt_merge_dir} -o code_cover.info"
+}
+
+function html_generate()
+{
+    if [[ -d "${out_dir}" ]]; then
+        eval "rm -rf ${out_dir}"
+    fi
+
+    eval "${genhtml_tool} ${opt_html} --output-directory ${out_dir} \
+          \"code_cover.info\""
+}
+
+function tar_report()
+{
+    printf "\n"
+    printf "TAR report\n"
+    eval "tar -cvzf ${out_dir}.tar.gz ${out_dir}"
+}
+
+function merge_gcda_gcno()
+{
+    # Set default merge dir if not defined
+    if [[ -z ${opt_merge_dir} ]]; then
+        opt_merge_dir="tmp"
+    fi
+
+    # Make the temporary directory
+    if [[ -d "${opt_merge_dir}" ]]; then
+        eval "rm -rf ${opt_merge_dir}"
+    fi
+
+    eval "mkdir -p ${opt_merge_dir}"
+
+    #
+    # Copy *.gcno to temporary directory
+    copy_file "${opt_gcno_dir}" "${opt_merge_dir}"
+
+    #
+    # Copy *.gcda to temporary directory
+    copy_file "${opt_gcda_dir}" "${opt_merge_dir}"
+}
+
+function check_gcda_gcno()
+{
+    if [[ "${opt_gcda_dir}" != "${opt_gcno_dir}" ]]; then
+        usage
+    fi
+
+    opt_merge_dir=${opt_gcda_dir}
+    check_directory opt_merge_dir
+    if [[ ${ret_func} -eq 0 ]]; then exit 1; fi
+}
+
+function generate_report()
+{
+    #
+    # Check if lcov tools are installed or present
+    # in given lcov path option
+    #
+    check_lcov_tools
+    if [[ ${ret_func} -eq 0 ]]; then exit 1; fi
+
+    #
+    # Check if gcov tools are installed or present
+    # in given gcov path option
+    #
+    check_gcov_tools
+
+    if [[ ${opt_merge} -eq 1 ]]; then
+        merge_gcda_gcno
+    else
+        check_gcda_gcno
+    fi
+
+    #
+    # Generate data coverage info file
+    lcov_generate
+
+    #
+    # Generate HTML report file
+    html_generate
+
+    #
+    # Tar report
+    tar_report
+
+    if [[ ${opt_merge} -eq 1 ]]; then
+        if [[ -d "${opt_merge_dir}" ]]; then
+            eval "rm -rf ${opt_merge_dir}"
+        fi
+    fi
+
+    if [[ -e "code_cover.info" ]]; then
+        eval "rm code_cover.info"
+    fi
+}
+
+if [[ $# -eq 0 ]]; then
+    usage
+fi
+
+opt_action="$1"
+shift
+
+parse_param "$@"
+
+case ${opt_action} in
+    install)
+        install_lcov
+        ;;
+
+    report)
+        generate_report
+        ;;
+
+    *)
+        printf "Unknown action %s\n" "$1"
+        usage
+        ;;
+esac

scripts/lcov.rc
@@ -1,0 +1,160 @@
+#
+# /etc/lcovrc - system-wide defaults for LCOV
+#
+# To change settings for a single user, place a customized copy of this file
+# at location ~/.lcovrc
+#
+
+# Specify an external style sheet file (same as --css-file option of genhtml)
+#genhtml_css_file = gcov.css
+
+# Specify coverage rate limits (in %) for classifying file entries
+# HI:   hi_limit <= rate <= 100         graph color: green
+# MED: med_limit <= rate <  hi_limit    graph color: orange
+# LO:         0  <= rate <  med_limit   graph color: red
+genhtml_hi_limit = 80
+genhtml_med_limit = 65
+
+# Width of line coverage field in source code view
+genhtml_line_field_width = 12
+
+# Width of branch coverage field in source code view
+genhtml_branch_field_width = 16
+
+# Width of overview image (used by --frames option of genhtml)
+genhtml_overview_width = 80
+
+# Resolution of overview navigation: this number specifies the maximum
+# difference in lines between the position a user selected from the overview
+# and the position the source code window is scrolled to (used by --frames
+# option of genhtml)
+genhtml_nav_resolution = 4
+
+# Clicking a line in the overview image should show the source code view at
+# a position a bit further up so that the requested line is not the first
+# line in the window. This number specifies that offset in lines (used by
+# --frames option of genhtml)
+genhtml_nav_offset = 10
+
+# Do not remove unused test descriptions if non-zero (same as
+# --keep-descriptions option of genhtml)
+genhtml_keep_descriptions = 0
+
+# Do not remove prefix from directory names if non-zero (same as --no-prefix
+# option of genhtml)
+genhtml_no_prefix = 0
+
+# Do not create source code view if non-zero (same as --no-source option of
+# genhtml)
+genhtml_no_source = 0
+
+# Replace tabs with number of spaces in source view (same as --num-spaces
+# option of genhtml)
+genhtml_num_spaces = 8
+
+# Highlight lines with converted-only data if non-zero (same as --highlight
+# option of genhtml)
+genhtml_highlight = 0
+
+# Include color legend in HTML output if non-zero (same as --legend option of
+# genhtml)
+genhtml_legend = 0
+
+# Use FILE as HTML prolog for generated pages (same as --html-prolog option of
+# genhtml)
+#genhtml_html_prolog = FILE
+
+# Use FILE as HTML epilog for generated pages (same as --html-epilog option of
+# genhtml)
+#genhtml_html_epilog = FILE
+
+# Use custom filename extension for pages (same as --html-extension option of
+# genhtml)
+#genhtml_html_extension = html
+
+# Compress all generated html files with gzip.
+genhtml_html_gzip = 0
+
+# Include sorted overview pages (can be disabled by the --no-sort option of
+# genhtml)
+genhtml_sort = 1
+
+# Include function coverage data display (can be disabled by the
+# --no-func-coverage option of genhtml)
+#genhtml_function_coverage = 1
+
+# Include branch coverage data display (can be disabled by the
+# --no-branch-coverage option of genhtml)
+#genhtml_branch_coverage = 1
+
+# Specify the character set of all generated HTML pages
+genhtml_charset=UTF-8
+
+# Allow HTML markup in test case description text if non-zero
+genhtml_desc_html=0
+
+# Specify the precision for coverage rates
+#genhtml_precision=1
+
+# Show missed counts instead of hit counts
+#genhtml_missed=1
+
+# Demangle C++ symbols
+#genhtml_demangle_cpp=1
+
+# Location of the gcov tool (same as --gcov-info option of geninfo)
+#geninfo_gcov_tool=gcov
+
+# Adjust test names to include operating system information if non-zero
+#geninfo_adjust_testname = 0
+
+# Calculate checksum for each source code line if non-zero (same as --checksum
+# option of geninfo if non-zero, same as --no-checksum if zero)
+#geninfo_checksum = 1
+
+# Specify whether to capture coverage data for external source files (can
+# be overridden by the --external and --no-external options of geninfo/lcov)
+#geninfo_external = 1
+
+# Enable libtool compatibility mode if non-zero (same as --compat-libtool option
+# of geninfo if non-zero, same as --no-compat-libtool if zero)
+#geninfo_compat_libtool = 0
+
+# Use gcov's --all-blocks option if non-zero
+#geninfo_gcov_all_blocks = 1
+
+# Specify compatiblity modes (same as --compat option of geninfo).
+#geninfo_compat = libtool=on, hammer=auto, split_crc=auto
+
+# Adjust path to source files by removing or changing path components that
+# match the specified pattern (Perl regular expression format)
+#geninfo_adjust_src_path = /tmp/build => /usr/src
+
+# Specify if geninfo should try to automatically determine the base-directory
+# when collecting coverage data.
+geninfo_auto_base = 1
+
+# Directory containing gcov kernel files
+# lcov_gcov_dir = /proc/gcov
+
+# Location for temporary directories
+lcov_tmp_dir = /tmp
+
+# Show full paths during list operation if non-zero (same as --list-full-path
+# option of lcov)
+lcov_list_full_path = 0
+
+# Specify the maximum width for list output. This value is ignored when
+# lcov_list_full_path is non-zero.
+lcov_list_width = 80
+
+# Specify the maximum percentage of file names which may be truncated when
+# choosing a directory prefix in list output. This value is ignored when
+# lcov_list_full_path is non-zero.
+lcov_list_truncate_max = 20
+
+# Specify if function coverage data should be collected and processed.
+lcov_function_coverage = 1
+
+# Specify if branch coverage data should be collected and processed.
+lcov_branch_coverage = 0

commit 6b4310209aa879687075ce7af3affe34603fbd1b

SSMW-334 scripts: Fix typo
Fixes: 806e20c1f76d ("SSMW-174 scripts: add gcov, coverity and bamboo scripts")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

scripts/smw_coverity_build.sh
@@ -16,7 +16,7 @@ function usage()
     this script and dependencies built as well.
 
     $(basename "$0") <dir>
-      <dir> : Mandatory putput build directory
+      <dir> : Mandatory build directory
 
 EOF
     exit 1
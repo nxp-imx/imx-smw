nxp-imx/imx-smw

commit 02174ea7ee6311c36ff355731a08f58724bf50c0

SSMW-121 pkcs11: implement SMW device
Add SMW device to use SMW default subsystem for the operation when
compilation option SMW_DEVICE_ONLY is set (default value).
Else if option disabled all SMW's Secure Subsystem are listed as
slot/token in plus of the SMW default subsytem.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/ifsmw/CMakeLists.txt
@@ -1,4 +1,46 @@
 file(GLOB ifsmw_src *.c)
 
+option(SMW_DEVICE_ONLY "SMW Device only" ON)
+message(STATUS "    Abstract SMW's Secure Subsystems: ${SMW_DEVICE_ONLY}")
+
+set(NB_IFSWM_DEV 1)
+list(APPEND list_ifsmw "smw")
+
+if (NOT SMW_DEVICE_ONLY)
+foreach(src IN LISTS ifsmw_src)
+  get_filename_component(filename ${src} NAME)
+  if(NOT ${filename} MATCHES "smw_dev.c" AND ${filename} MATCHES "_dev.c$")
+     string(REGEX REPLACE "_dev.c$" "" ifsmw_name ${filename})
+     list(APPEND list_ifsmw ${ifsmw_name})
+     MATH(EXPR NB_IFSWM_DEV "${NB_IFSWM_DEV} + 1")
+  endif()
+endforeach()
+endif()
+message(STATUS "    SMW devices: ${list_ifsmw}")
+message(STATUS "    Nb SMW devices: ${NB_IFSWM_DEV}")
+
+#
+# For each defined devices (SMW's Secure Subsytem name)
+# Build:
+#    - list of device information
+#    - list of cryptoki mechanism information function
+string(APPEND DEV_INFO "\n")
+string(APPEND DEV_MECH_INFO "\n")
+foreach(dev IN LISTS list_ifsmw)
+  string(APPEND DECLARE_DEV_INFO "extern const struct libdev ${dev}_info;\n")
+  string(APPEND DECLARE_DEV_MECH_INFO "FUNC_DEV_MECH_INFO(${dev}_mech_info);\n")
+  string(APPEND DEV_INFO "{ .dev = &${dev}_info }, \n")
+  string(APPEND DEV_MECH_INFO "${dev}_mech_info, \n")
+endforeach()
+
+configure_file(dev_config.c.in dev_config.c)
+configure_file(dev_config.h.in dev_config.h)
+
+list(APPEND ifsmw_src ${CMAKE_CURRENT_BINARY_DIR}/dev_config.c)
+
 target_sources(${PROJECT_NAME} PRIVATE ${ifsmw_src})
+
+target_include_directories(${PROJECT_NAME} PRIVATE
+	${CMAKE_CURRENT_BINARY_DIR}
+        ${CMAKE_CURRENT_SOURCE_DIR})
 

pkcs11/src/ifsmw/device.c
@@ -5,25 +5,15 @@
 #include "smw_config.h"
 #include "smw_status.h"
 
+#include "dev_config.h"
 #include "lib_device.h"
 #include "util.h"
 
-#include "local.h"
-
-/**
- * struct ifdev - SMW device definition
- * @dev: Constant information
- */
-struct ifdev {
-	const struct libdev *dev;
-};
-
-const struct ifdev smw_devices[] = { { .dev = &hsm_info },
-				     { .dev = &optee_info } };
+#include "trace.h"
 
 const struct libdev *libdev_get_devinfo(CK_SLOT_ID slotid)
 {
-	if (slotid < ARRAY_SIZE(smw_devices))
+	if (slotid < NB_IFSWM_DEV)
 		return smw_devices[slotid].dev;
 
 	return NULL;
@@ -31,7 +21,7 @@ const struct libdev *libdev_get_devinfo(CK_SLOT_ID slotid)
 
 unsigned int libdev_get_nb_devinfo(void)
 {
-	return ARRAY_SIZE(smw_devices);
+	return NB_IFSWM_DEV;
 }
 
 void libdev_set_present(struct libdevice *devices)
@@ -43,7 +33,13 @@ void libdev_set_present(struct libdevice *devices)
 
 	for (idx = 0; idx < nb_devices; idx++) {
 		devinfo = libdev_get_devinfo(idx);
-		status = smw_config_subsystem_present(devinfo->name);
+		if (devinfo->name)
+			status = smw_config_subsystem_present(devinfo->name);
+		else
+			status = SMW_STATUS_OK;
+
+		DBG_TRACE("SMW subsytem (%u) [%s] present returned %d", idx,
+			  devinfo->name, status);
 		if (status == SMW_STATUS_OK)
 			SET_BITS(devices[idx].slot.flags, CKF_TOKEN_PRESENT);
 		else

pkcs11/src/ifsmw/dev_config.c.in
@@ -1,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2020 NXP
+ */
+
+#include "dev_config.h"
+
+const struct ifdev smw_devices[NB_IFSWM_DEV] = { @DEV_INFO@ };
+
+FUNC_DEV_MECH_INFO_PTR(dev_mech_info[NB_IFSWM_DEV]) = { @DEV_MECH_INFO@ };
+

pkcs11/src/ifsmw/dev_config.h.in
@@ -1,0 +1,45 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2020 NXP
+ */
+
+#ifndef __DEV_CONFIG_H__
+#define __DEV_CONFIG_H__
+
+#include "pkcs11smw.h"
+
+/*
+ * Define the number of devices interface
+ */
+#define NB_IFSWM_DEV @NB_IFSWM_DEV@
+
+/**
+ * struct ifdev - SMW device definition
+ * @dev: Constant information
+ */
+struct ifdev {
+	const struct libdev *dev;
+};
+
+extern const struct ifdev smw_devices[NB_IFSWM_DEV];
+
+/*
+ * Definition of the device's mechanism information function prototype
+ */
+#define FUNC_DEV_MECH_INFO(name)                                               \
+	CK_RV(name)(CK_MECHANISM_TYPE type, CK_MECHANISM_INFO_PTR info)
+#define FUNC_DEV_MECH_INFO_PTR(name) FUNC_DEV_MECH_INFO(*name)
+
+extern FUNC_DEV_MECH_INFO_PTR(dev_mech_info[NB_IFSWM_DEV]);
+
+/*
+ * Declaration of the device information structures
+ */
+${DECLARE_DEV_INFO}
+
+/*
+ * Declaration of the device mechanism information functions
+ */
+${DECLARE_DEV_MECH_INFO}
+
+#endif /* __DEV_CONFIG_H__ */

pkcs11/src/ifsmw/local.h
@@ -1,24 +1,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
-/*
- * Copyright 2020 NXP
- */
-
-#ifndef __LOCAL_H__
-#define __LOCAL_H__
-
-#include "pkcs11smw.h"
-
-/*
- * Definition of the device's mechanism information function prototype
- */
-#define FUNC_MECH_INFO(name)                                                   \
-	CK_RV(name)(CK_MECHANISM_TYPE type, CK_MECHANISM_INFO_PTR info)
-#define FUNC_MECH_INFO_PTR(name) FUNC_MECH_INFO(*(name))
-
-FUNC_MECH_INFO(hsm_info_mdigest);
-FUNC_MECH_INFO(optee_info_mdigest);
-
-extern const struct libdev hsm_info;
-extern const struct libdev optee_info;
-
-#endif /* __LOCAL_H__ */

pkcs11/src/ifsmw/mechanims.c
@@ -5,26 +5,19 @@
 #include "smw_config.h"
 #include "smw_status.h"
 
+#include "dev_config.h"
 #include "lib_context.h"
 #include "lib_device.h"
 #include "pkcs11smw.h"
 
 #include "trace.h"
 
-#include "local.h"
-
 struct mgroup;
 
-/*
- * Function prototype to check if all mechanism of a group
- * are supported by a SMW subsystem
- */
-#define FUNC_MECH_CHECK(name)                                                  \
-	void(name)(CK_SLOT_ID slotid, const char *subsystem,                   \
-		   struct mgroup *mgroup)
-#define FUNC_MECH_CHECK_PTR(name) FUNC_MECH_CHECK(*(name))
-
-static FUNC_MECH_CHECK(check_mdigest);
+static void check_mdigest(CK_SLOT_ID slotid, const char *subsystem,
+			  struct mgroup *mgroup);
+static CK_RV info_mdigest(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			  CK_MECHANISM_INFO_PTR info);
 
 /**
  * struct mentry - Definition of a mechanism supported by each device
@@ -49,8 +42,10 @@ struct mgroup {
 	unsigned int number;
 	struct mentry *mechanism;
 
-	FUNC_MECH_CHECK_PTR(check);
-	FUNC_MECH_INFO_PTR(*info);
+	void (*check)(CK_SLOT_ID slotid, const char *subsystem,
+		      struct mgroup *mgroup);
+	CK_RV(*info)
+	(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type, CK_MECHANISM_INFO_PTR info);
 };
 
 #define M_INIT(name, id)                                                       \
@@ -71,8 +66,6 @@ static struct mentry mdigest[] = {
 	M_INIT(SHA1, SHA_1),	M_INIT(SHA224, SHA224), M_INIT(SHA256, SHA256),
 	M_INIT(SHA384, SHA384), M_INIT(SHA512, SHA512),
 };
-
-FUNC_MECH_INFO_PTR(info_mdigest[]) = { hsm_info_mdigest, optee_info_mdigest };
 
 /*
  * All SMW mechanisms
@@ -181,7 +174,7 @@ CK_RV libdev_get_mechanism_info(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
 					return CKR_MECHANISM_INVALID;
 				}
 
-				return group->info[slotid](type, info);
+				return group->info(slotid, type, info);
 			}
 		}
 	}
@@ -189,7 +182,8 @@ CK_RV libdev_get_mechanism_info(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
 	return CKR_MECHANISM_INVALID;
 }
 
-static FUNC_MECH_CHECK(check_mdigest)
+static void check_mdigest(CK_SLOT_ID slotid, const char *subsystem,
+			  struct mgroup *mgroup)
 {
 	int status;
 	unsigned int idx;
@@ -205,6 +199,30 @@ static FUNC_MECH_CHECK(check_mdigest)
 		if (status == SMW_STATUS_OK)
 			SET_BITS(entry->slot_flag, slot_flag);
 	}
+}
+
+static CK_RV info_mdigest(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			  CK_MECHANISM_INFO_PTR info)
+{
+	CK_RV ret = CKR_OK;
+
+	DBG_TRACE("Return info of %lu digest mechanism", type);
+
+	/*
+	 * Digest global settings.
+	 */
+	info->ulMaxKeySize = 0;
+	info->ulMinKeySize = 0;
+	info->flags = CKF_DIGEST;
+
+	/*
+	 * Call specific device mechanism information function
+	 * to complete the global setting.
+	 */
+	if (dev_mech_info[slotid])
+		ret = dev_mech_info[slotid](type, info);
+
+	return ret;
 }
 
 CK_RV libdev_mechanisms_init(CK_SLOT_ID slotid)

pkcs11/src/ifsmw/optee_dev.c
@@ -3,8 +3,7 @@
  * Copyright 2020 NXP
  */
 
-#include "local.h"
-
+#include "dev_config.h"
 #include "lib_device.h"
 #include "pkcs11smw_config.h"
 
@@ -24,14 +23,12 @@ const struct libdev optee_info = {
 	.flags_token = 0,
 };
 
-FUNC_MECH_INFO(optee_info_mdigest)
+FUNC_DEV_MECH_INFO(optee_mech_info)
 {
 	(void)(type);
+	(void)(info);
 
 	DBG_TRACE("Return info of %lu digset mechanism", type);
 
-	info->ulMaxKeySize = 0;
-	info->ulMinKeySize = 0;
-	info->flags = CKF_DIGEST;
 	return CKR_OK;
 }

pkcs11/src/ifsmw/smw_dev.c
@@ -2,6 +2,7 @@
 /*
  * Copyright 2020 NXP
  */
+
 #include "dev_config.h"
 #include "lib_device.h"
 #include "pkcs11smw_config.h"
@@ -9,26 +10,27 @@
 #include "trace.h"
 
 /*
- * Define HSM Security Middleware library Secure Subsystem.
+ * Define Security Middleware library generic device.
+ * Use default Secure Subsystem operation defined in
+ * the configuration file.
  */
-const struct libdev hsm_info = {
-	.name = "HSM",
-	.description = "Hardware Secure Module",
+const struct libdev smw_info = {
+	.name = NULL,
+	.description = "Security Middleware Abstraction",
 	.manufacturer = MANUFACTURER_ID,
 	.model = "",
 	.serial = "",
 	.version = { 0, 0 },
-	.flags_slot = CKF_HW_SLOT,
+	.flags_slot = 0,
 	.flags_token = 0,
 };
 
-FUNC_DEV_MECH_INFO(hsm_mech_info)
+FUNC_DEV_MECH_INFO(smw_mech_info)
 {
 	(void)(type);
+	(void)(info);
 
 	DBG_TRACE("Return info of %lu digest mechanism", type);
-	if (info->flags & CKF_DIGEST)
-		info->flags |= CKF_HW;
 
 	return CKR_OK;
 }

commit 487410cd89754c681b20e1d790af8b34147abd33

SSMW-334 pkcs11: Fix typo
Fixes: 02174ea7ee63 ("SSMW-121 pkcs11: implement SMW device")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

pkcs11/src/ifsmw/CMakeLists.txt
@@ -20,7 +20,7 @@ message(STATUS "    SMW devices: ${list_ifsmw}")
 message(STATUS "    Nb SMW devices: ${NB_IFSWM_DEV}")
 
 #
-# For each defined devices (SMW's Secure Subsytem name)
+# For each defined devices (SMW's Secure Subsystem name)
 # Build:
 #    - list of device information
 #    - list of cryptoki mechanism information function

pkcs11/src/ifsmw/device.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2021 NXP
+ * Copyright 2020-2022 NXP
  */
 #include "smw_config.h"
 #include "smw_status.h"
@@ -38,7 +38,7 @@ void libdev_set_present(struct libdevice *devices)
 		else
 			status = SMW_STATUS_OK;
 
-		DBG_TRACE("SMW subsytem (%u) [%s] present returned %d", idx,
+		DBG_TRACE("SMW subsystem (%u) [%s] present returned %d", idx,
 			  devinfo->name ? devinfo->name : "NULL", status);
 		if (status == SMW_STATUS_OK)
 			SET_BITS(devices[idx].slot.flags, CKF_TOKEN_PRESENT);
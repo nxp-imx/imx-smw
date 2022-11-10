nxp-imx/imx-smw

commit a18dc07e848f7ae14ae6fe78f4aa763e26ed5080

SSMW-71 core: utils: Implement smw_utils_execute_operation()
- Implement smw_utils_execute_operation() called by all Security modules
  to execute a Security Operation
- Add SMW_DBG_HEX_DUMP() debug macro

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/inc/exec.h
@@ -1,0 +1,21 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2020 NXP
+ */
+
+/**
+ * smw_utils_execute_operation() - Execute a Security Operation.
+ * @operation_id: Security Operation ID.
+ * @args: Pointer to a structure of arguments defined by the internal API.
+ * @subsystem_id: Secure Subsystem ID.
+ *
+ * This function calls the API of a Secure Subsytem to perform
+ * the required Security Operation.
+ * The Secure Subsystem is either an argument or the default Secure Subsystem
+ * configured for this Security Operation.
+ *
+ * Return:
+ * error code.
+ */
+int smw_utils_execute_operation(enum operation_id operation_id, void *args,
+				enum subsystem_id subsystem_id);

core/inc/debug.h
@@ -43,11 +43,33 @@
 			SMW_PRINTF(__VA_ARGS__);                               \
 	} while (0)
 
-#define SMW_DBG_PRINTF_COND(cond, level, ...)                                  \
+#define SMW_DBG_PRINTF_COND(level, cond, ...)                                  \
 	do {                                                                   \
 		if (SMW_DBG_LEVEL_##level <= SMW_DBG_LEVEL)                    \
 			if (cond)                                              \
 				SMW_PRINTF(__VA_ARGS__);                       \
+	} while (0)
+
+#define SMW_DBG_HEX_DUMP(level, addr, size, align)                             \
+	do {                                                                   \
+		if (SMW_DBG_LEVEL_##level > SMW_DBG_LEVEL)                     \
+			break;                                                 \
+		int i;                                                         \
+		unsigned char *_addr = addr;                                   \
+		unsigned int _size = (unsigned int)(size);                     \
+		unsigned int _align = (unsigned int)(align);                   \
+		_size--;                                                       \
+		if (_align > 4)                                                \
+			_align = 4;                                            \
+		if (_align)                                                    \
+			_align = 1 << _align;                                  \
+		if (g_smw_ctx.ops.thread_self)                                 \
+			printf("(%lx)\n", g_smw_ctx.ops.thread_self());        \
+		for (i = 0; i < _size; i++) {                                  \
+			printf("%.2x%s", (_addr)[i],                           \
+			       (i + 1) & (_align - 1) ? " " : "\n");           \
+		}                                                              \
+		printf("%.2x\n", (_addr)[_size]);                              \
 	} while (0)
 
 #else /* ENABLE_TRACE */
@@ -55,7 +77,8 @@
 #define SMW_PRINTF(...)
 #define SMW_DBG_TRACE_FUNCTION_CALL
 #define SMW_DBG_PRINTF(level, ...)
-#define SMW_DBG_PRINTF_COND(cond, level, ...)
+#define SMW_DBG_PRINTF_COND(level, cond, ...)
+#define SMW_DBG_HEX_DUMP(level, addr, size, align)
 
 #endif /* ENABLE_TRACE */
 

core/inc/utils.h
@@ -10,6 +10,12 @@
 #include <string.h>
 #include <limits.h>
 
+#define SMW_ALL_ONES (-1)
+
+#if !defined ARRAY_SIZE
+#define ARRAY_SIZE(array) (sizeof(array) / sizeof((array)[0]))
+#endif
+
 #define SMW_UTILS_MALLOC  malloc
 #define SMW_UTILS_FREE	  free
 #define SMW_UTILS_MEMCPY  memcpy

core/utils/CMakeLists.txt
@@ -1,7 +1,7 @@
 set(OBJ_LIST ${OBJ_LIST} $<TARGET_OBJECTS:utils> PARENT_SCOPE)
 
 add_library(utils OBJECT "" ${INCLUDE_DIR})
-target_sources(utils PRIVATE list.c common.h)
+target_sources(utils PRIVATE list.c exec.c common.h)
 if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND VERBOSE AND NOT VERBOSE EQUAL 0)
 target_sources(utils PRIVATE list_debug.c)
 endif()

core/utils/exec.c
@@ -1,0 +1,67 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2020 NXP
+ */
+
+#include "smw_status.h"
+
+#include "smw_osal.h"
+#include "global.h"
+#include "debug.h"
+#include "utils.h"
+#include "operations.h"
+#include "subsystems.h"
+#include "config.h"
+
+int smw_utils_execute_operation(enum operation_id operation_id, void *args,
+				enum subsystem_id subsystem_id)
+{
+	int status = SMW_STATUS_OK;
+
+	void *params = NULL;
+	struct operation_func *operation_func;
+	struct subsystem_func *subsystem_func;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	SMW_DBG_PRINTF(INFO, "Execute Security Operation: %s (%d)\n",
+		       smw_config_get_operation_name(operation_id),
+		       operation_id);
+
+	operation_func = smw_config_get_operation_func(operation_id);
+
+	SMW_DBG_ASSERT(operation_func);
+	SMW_DBG_ASSERT(operation_func->check_subsystem_caps);
+
+	status = smw_config_get_subsystem_caps(&subsystem_id, operation_id,
+					       &params);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = operation_func->check_subsystem_caps(args, params);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = smw_config_load_subsystem(subsystem_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	SMW_DBG_PRINTF(INFO, "Secure Subsystem: %s (%d)\n",
+		       smw_config_get_subsystem_name(subsystem_id),
+		       subsystem_id);
+
+	subsystem_func = smw_config_get_subsystem_func(subsystem_id);
+
+	SMW_DBG_ASSERT(subsystem_func);
+	SMW_DBG_ASSERT(subsystem_func->execute);
+
+	status = subsystem_func->execute(operation_id, args);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = smw_config_unload_subsystem(subsystem_id);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}

commit e1f9d10d7b85c9c0793705a00af61d6adfbc0a7b

SSMW-334 core: utils: Fix typo
Fixes: a18dc07e848f ("SSMW-71 core: utils: Implement smw_utils_execute_operation()")
Fixes: 2cc2b9550d84 ("SSMW-149 core: crypto: Add support of cipher")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/inc/exec.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2021 NXP
+ * Copyright 2020-2022 NXP
  */
 
 enum smw_op_step {
@@ -16,7 +16,7 @@ enum smw_op_step {
  * @args: Pointer to a structure of arguments defined by the internal API.
  * @subsystem_id: Secure Subsystem ID.
  *
- * This function calls the API of a Secure Subsytem to perform
+ * This function calls the API of a Secure Subsystem to perform
  * the required Security Operation.
  * The Secure Subsystem is either an argument or the default Secure Subsystem
  * configured for this Security Operation.
@@ -33,7 +33,7 @@ int smw_utils_execute_operation(enum operation_id operation_id, void *args,
  * @args: Pointer to a structure of arguments defined by the internal API.
  * @subsystem_id: Secure Subsystem ID.
  *
- * This function calls the API of a Secure Subsytem to perform
+ * This function calls the API of a Secure Subsystem to perform
  * the required initialization Security Operation.
  * The Secure Subsystem is either an argument or the default Secure Subsystem
  * configured for this Security Operation.
@@ -50,7 +50,7 @@ int smw_utils_execute_init(enum operation_id operation_id, void *args,
  * @args: Pointer to a structure of arguments defined by the internal API.
  * @subsystem_id: Secure Subsystem ID.
  *
- * This function calls the API of a Secure Subsytem to perform
+ * This function calls the API of a Secure Subsystem to perform
  * the required update Security Operation.
  * The Secure Subsystem is either an argument or the default Secure Subsystem
  * configured for this Security Operation.
@@ -67,7 +67,7 @@ int smw_utils_execute_update(enum operation_id operation_id, void *args,
  * @args: Pointer to a structure of arguments defined by the internal API.
  * @subsystem_id: Secure Subsystem ID.
  *
- * This function calls the API of a Secure Subsytem to perform
+ * This function calls the API of a Secure Subsystem to perform
  * the required final Security Operation.
  * The Secure Subsystem is either an argument or the default Secure Subsystem
  * configured for this Security Operation.
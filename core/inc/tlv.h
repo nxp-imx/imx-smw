nxp-imx/imx-smw

commit 143f57dd59dd37d4fec1247622651406fb151bc5

SSMW-88 core: keymgr: Add "Persistent" attribute support
Add support of "persistent" attribute in key manager

Signed-off-by: Remi Koman <remi.koman@nxp.com>

core/inc/tlv.h
@@ -1,0 +1,73 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2020 NXP
+ */
+
+/* TLV defines */
+#define SMW_TLV_LENGTH_FIELD_SIZE 2 /* TLV length encoded with 2 bytes */
+
+/**
+ * smw_tlv_read_element() - Read one Type-Length-Value encoded element.
+ * @attribute: [in/out] pointer to the element to read and pointing to the next
+ *                      element when function returns.
+ * @end: Pointer to @attribute end address.
+ * @type: Pointer to type buffer.
+ * @value: Pointer to value buffer.
+ * @value_size: Pointer to value buffer size (in bytes).
+ *
+ * This function reads one TLV encoded buffer.
+ * TLV format is:
+ *  - Type: String null terminated.
+ *  - Length: Size of value in bytes (encoded as a number on 2 bytes).
+ *  - Value: Byte stream.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_INVALID_PARAM	- Failed to read buffer.
+ */
+int smw_tlv_read_element(const unsigned char **attribute,
+			 const unsigned char *end, unsigned char **type,
+			 unsigned char **value, unsigned int *value_size);
+
+/**
+ * smw_tlv_verify_boolean() - Verify that TLV length and value correspond to
+ *                            boolean type.
+ * @length: Length of @value in bytes.
+ * @value: Value buffer.
+ *
+ * Length must be 0 and value must be NULL.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_INVALID_PARAM	- One of the parameter is invalid.
+ */
+int smw_tlv_verify_boolean(unsigned int length, unsigned char *value);
+
+/**
+ * smw_tlv_verify_string() - Verify that TLV length and value correspond to
+ *                           string type.
+ * @length: Length of @value in bytes.
+ * @value: Value buffer.
+ *
+ * Value must be a null-terminated string.
+ * Length must be the length of @value including the null character.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_INVALID_PARAM	- One of the parameter is invalid.
+ */
+int smw_tlv_verify_string(unsigned int length, unsigned char *value);
+
+/**
+ * smw_tlv_verify_enumeration() - Verify that TLV length and value correspond to
+ *                                enumeration type.
+ * @length: Length of @value in bytes.
+ * @value: Value buffer.
+ *
+ * Enumeration type is treated as string type.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_INVALID_PARAM	- One of the parameter is invalid.
+ */
+int smw_tlv_verify_enumeration(unsigned int length, unsigned char *value);

core/utils/tlv.c
@@ -1,0 +1,116 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2020 NXP
+ */
+
+#include "tlv.h"
+#include "smw_status.h"
+#include "smw_osal.h"
+#include "global.h"
+#include "debug.h"
+#include "utils.h"
+
+int smw_tlv_read_element(const unsigned char **attribute,
+			 const unsigned char *end, unsigned char **type,
+			 unsigned char **value, unsigned int *value_size)
+{
+	int status = SMW_STATUS_OK;
+	unsigned int j = 1;
+	unsigned char *p = (unsigned char *)*attribute;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	SMW_DBG_ASSERT(attribute && end && type && value && value_size);
+
+	/* Parse type */
+	while ((p < end) && (*p != '\0'))
+		p++;
+
+	if (p >= end) {
+		SMW_DBG_PRINTF(ERROR, "%s: Can't parse type field\n", __func__);
+		status = SMW_STATUS_INVALID_PARAM;
+		goto exit;
+	}
+
+	*type = (unsigned char *)*attribute;
+	p++;
+
+	if ((end - p) < SMW_TLV_LENGTH_FIELD_SIZE) {
+		SMW_DBG_PRINTF(ERROR,
+			       "%s (%d): attributes_length is too small\n",
+			       __func__, __LINE__);
+		status = SMW_STATUS_INVALID_PARAM;
+		goto exit;
+	}
+
+	/* Parse length */
+	*value_size = *p++;
+	for (; j < SMW_TLV_LENGTH_FIELD_SIZE; j++) {
+		*value_size <<= 8;
+		*value_size |= *p++;
+	}
+
+	if (!*value_size) {
+		SMW_DBG_PRINTF(DEBUG, "%s: Attribute is boolean\n", __func__);
+		*value = NULL;
+		/* Update attribute pointer to next element to handle */
+		*attribute = p + SMW_TLV_LENGTH_FIELD_SIZE;
+		goto exit;
+	}
+
+	if ((end - p) < *value_size) {
+		SMW_DBG_PRINTF(ERROR,
+			       "%s (%d): attributes_length is too small\n",
+			       __func__, __LINE__);
+		status = SMW_STATUS_INVALID_PARAM;
+		goto exit;
+	}
+
+	/* Parse value */
+	*value = p;
+
+	/* Update attribute pointer to next element to handle */
+	*attribute = p + *value_size;
+
+exit:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+int smw_tlv_verify_boolean(unsigned int length, unsigned char *value)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!length && !value)
+		status = SMW_STATUS_OK;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+int smw_tlv_verify_string(unsigned int length, unsigned char *value)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (*(value + length - 1) == '\0')
+		status = SMW_STATUS_OK;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+int smw_tlv_verify_enumeration(unsigned int length, unsigned char *value)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	status = smw_tlv_verify_string(length, value);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}

core/inc/keymgr.h
@@ -118,3 +118,36 @@ struct smw_keymgr_delete_key_args {
 	/* Inputs */
 	struct smw_key_identifier *key_identifier;
 };
+
+/**
+ * struct smw_keymgr_attributes - Key attributes list.
+ * @persistent_storage: Use persistent subsystem storage or not.
+ *
+ */
+struct smw_keymgr_attributes {
+	bool persistent_storage;
+};
+
+/**
+ * smw_keymgr_read_attributes() - Read key_attributes_list buffer.
+ * @attributes_list: List of attributes buffer to read.
+ * @attributes_length: Buffer size (bytes).
+ * @key_attributes: Pointer to smw_keymgr_attributes structure to fill.
+ *
+ * This function reads a list of attributes parsed by smw_tlv_read_element()
+ * function and fill smw_keymgr_attributes structure using fill_key_attributes()
+ * function.
+ * @attributes_list is encoded with TLV encoding scheme:
+ * The ‘Type’ field is encoded as an ASCII string terminated with the null
+ * character.
+ * The ‘Length’ field is encoded with two bytes.
+ * The ‘Value’ field is a byte stream that contains the data.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_INVALID_PARAM	- One of the parameter is invalid.
+ * SMW_STATUS_ALLOC_FAILURE	- Memory allocation failed.
+ */
+int smw_keymgr_read_attributes(const unsigned char *attributes_list,
+			       unsigned int attributes_length,
+			       struct smw_keymgr_attributes *key_attributes);

core/keymgr/keymgr.c
@@ -15,6 +15,38 @@
 #include "config.h"
 #include "keymgr.h"
 #include "exec.h"
+#include "tlv.h"
+
+/**
+ * smw_keymgr_store_persistent() - Store persistent storage info.
+ * @key_attributes: Pointer to smw_keymgr_attributes structure to fill.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_INVALID_PARAM	- key_attributes is NULL.
+ */
+static int
+smw_keymgr_store_persistent(struct smw_keymgr_attributes *key_attributes);
+
+/**
+ * struct smw_keymgr_attributes_tlv - Key manager attribute handler.
+ * @type: Attribute type.
+ * @verify: Verification function appropriate to the attribute type.
+ * @store: Store function appropriate to the attribute type.
+ *
+ * For an attribute type related to key manager module, this structure provides
+ * functions to verify the kind of type (boolean, enumeration, string, numeral)
+ * and store the value.
+ */
+static struct smw_keymgr_attributes_tlv {
+	const unsigned char *type;
+	int (*verify)(unsigned int length, unsigned char *value);
+	int (*store)(struct smw_keymgr_attributes *key_attributes);
+} smw_keymgr_attributes_tlv_array[] = {
+	{ .type = (const unsigned char *)"PERSISTENT",
+	  .verify = smw_tlv_verify_boolean,
+	  .store = smw_keymgr_store_persistent }
+};
 
 static int
 generate_key_convert_args(struct smw_generate_key_args *args,
@@ -195,6 +227,70 @@ delete_key_convert_args(struct smw_delete_key_args *args,
 	*subsystem_id = converted_args->key_identifier->subsystem_id;
 
 end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+/**
+ * fill_key_attributes() - Fill a smw_keymgr_attributes structure.
+ * @type: Attribute type.
+ * @value: Attribute value.
+ * @value_size: Length of @value in bytes.
+ * @key_attributes: Pointer the key attributes structure to fill.
+ *
+ * Finds the attribute @type into the key attribute TLV list and if found,
+ * verify that value is correct.
+ * Then store the attribute value into the @key_attributes.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_INVALID_PARAM	- One of the parameter is invalid.
+ */
+static int fill_key_attributes(unsigned char *type, unsigned char *value,
+			       unsigned int value_size,
+			       struct smw_keymgr_attributes *key_attributes)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+	unsigned int i = 0;
+	unsigned int size = ARRAY_SIZE(smw_keymgr_attributes_tlv_array);
+	struct smw_keymgr_attributes_tlv *array =
+		smw_keymgr_attributes_tlv_array;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!type || !key_attributes)
+		goto end;
+
+	for (i = 0; i < size; i++) {
+		if (!SMW_UTILS_STRCMP((char *)type, (char *)array[i].type)) {
+			status = array[i].verify(value_size, value);
+			if (status != SMW_STATUS_OK)
+				goto end;
+
+			status = array[i].store(key_attributes);
+			break;
+		}
+	}
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int
+smw_keymgr_store_persistent(struct smw_keymgr_attributes *key_attributes)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!key_attributes)
+		goto exit;
+
+	key_attributes->persistent_storage = true;
+	status = SMW_STATUS_OK;
+
+exit:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }
@@ -387,6 +483,49 @@ int smw_delete_key(struct smw_delete_key_args *args)
 	status = smw_utils_execute_operation(OPERATION_ID_DELETE_KEY,
 					     &delete_key_args, subsystem_id);
 end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+int smw_keymgr_read_attributes(const unsigned char *attributes_list,
+			       unsigned int attributes_length,
+			       struct smw_keymgr_attributes *key_attributes)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+	unsigned int value_size = 0;
+	unsigned char *type = NULL;
+	unsigned char *value = NULL;
+	const unsigned char *p = attributes_list;
+	const unsigned char *end = attributes_list + attributes_length;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!attributes_list || !key_attributes) {
+		status = SMW_STATUS_INVALID_PARAM;
+		goto exit;
+	}
+
+	/* Initialize key_attributes parameter to default values */
+	key_attributes->persistent_storage = false;
+
+	while (p < end) {
+		/* Parse attribute */
+		status = smw_tlv_read_element(&p, end, &type, &value,
+					      &value_size);
+		if (status != SMW_STATUS_OK) {
+			SMW_DBG_PRINTF(ERROR, "%s: Parsing attribute failed\n",
+				       __func__);
+			goto exit;
+		}
+
+		/* Fill smw_keymgr_attributes struct */
+		status = fill_key_attributes(type, value, value_size,
+					     key_attributes);
+		if (status != SMW_STATUS_OK)
+			SMW_DBG_PRINTF(ERROR, "%s: Bad attribute\n", __func__);
+	}
+
+exit:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }

core/utils/CMakeLists.txt
@@ -2,7 +2,7 @@ set(OBJ_LIST ${OBJ_LIST} $<TARGET_OBJECTS:utils> PARENT_SCOPE)
 
 add_library(utils OBJECT "" ${INCLUDE_DIR})
 set_target_properties(utils PROPERTIES C_VISIBILITY_PRESET hidden)
-target_sources(utils PRIVATE list.c exec.c common.h)
+target_sources(utils PRIVATE list.c exec.c tlv.c common.h)
 if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND VERBOSE AND NOT VERBOSE EQUAL 0)
 target_sources(utils PRIVATE list_debug.c)
 endif()

commit 062ef26a100ec3c958853036157334827fbcfb69

SSMW-334 core: utils: Fix wrong debug printf
Fixes: 143f57dd59dd ("SSMW-88 core: keymgr: Add "Persistent" attribute support")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/inc/tlv.h
@@ -16,9 +16,9 @@
 
 /**
  * smw_tlv_read_element() - Read one Type-Length-Value encoded element.
- * @attribute: [in/out] pointer to the element to read and pointing to the next
+ * @element: [in/out] pointer to the element to read and pointing to the next
  *                      element when function returns.
- * @end: Pointer to @attribute end address.
+ * @end: Pointer to @element end address.
  * @type: Pointer to type buffer.
  * @value: Pointer to value buffer.
  * @value_size: Pointer to value buffer size (in bytes).
@@ -33,7 +33,7 @@
  * SMW_STATUS_OK		- Success.
  * SMW_STATUS_INVALID_PARAM	- Failed to read buffer.
  */
-int smw_tlv_read_element(const unsigned char **attribute,
+int smw_tlv_read_element(const unsigned char **element,
 			 const unsigned char *end, unsigned char **type,
 			 unsigned char **value, unsigned int *value_size);
 

core/utils/tlv.c
@@ -10,17 +10,17 @@
 #include "utils.h"
 #include "tlv.h"
 
-int smw_tlv_read_element(const unsigned char **attribute,
+int smw_tlv_read_element(const unsigned char **element,
 			 const unsigned char *end, unsigned char **type,
 			 unsigned char **value, unsigned int *value_size)
 {
 	int status = SMW_STATUS_OK;
 	unsigned int j = 1;
-	unsigned char *p = (unsigned char *)*attribute;
+	unsigned char *p = (unsigned char *)*element;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	SMW_DBG_ASSERT(attribute && end && type && value && value_size);
+	SMW_DBG_ASSERT(element && end && type && value && value_size);
 
 	/* Parse type */
 	while ((p < end) && (*p != '\0'))
@@ -32,12 +32,11 @@ int smw_tlv_read_element(const unsigned char **attribute,
 		goto exit;
 	}
 
-	*type = (unsigned char *)*attribute;
+	*type = (unsigned char *)*element;
 	p++;
 
 	if ((end - p) < SMW_TLV_LENGTH_FIELD_SIZE) {
-		SMW_DBG_PRINTF(ERROR,
-			       "%s (%d): attributes_length is too small\n",
+		SMW_DBG_PRINTF(ERROR, "%s (%d): Buffer is too small\n",
 			       __func__, __LINE__);
 		status = SMW_STATUS_INVALID_PARAM;
 		goto exit;
@@ -51,15 +50,14 @@ int smw_tlv_read_element(const unsigned char **attribute,
 	}
 
 	if (!*value_size) {
-		SMW_DBG_PRINTF(DEBUG, "%s: Attribute is boolean\n", __func__);
+		SMW_DBG_PRINTF(DEBUG, "%s: Element length is 0\n", __func__);
 		*value = NULL;
-		*attribute = p;
+		*element = p;
 		goto exit;
 	}
 
 	if ((unsigned int)(end - p) < *value_size) {
-		SMW_DBG_PRINTF(ERROR,
-			       "%s (%d): attributes_length is too small\n",
+		SMW_DBG_PRINTF(ERROR, "%s (%d): Buffer is too small\n",
 			       __func__, __LINE__);
 		status = SMW_STATUS_INVALID_PARAM;
 		goto exit;
@@ -68,8 +66,8 @@ int smw_tlv_read_element(const unsigned char **attribute,
 	/* Parse value */
 	*value = p;
 
-	/* Update attribute pointer to next element to handle */
-	*attribute = p + *value_size;
+	/* Update element pointer to next element to handle */
+	*element = p + *value_size;
 
 exit:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
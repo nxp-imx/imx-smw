dd5a57bb2daa533f3a1686798df4ca363a7fbfc3
Bruno De Smet <bruno.de.smet@nxp.com>
2023-03-03 17:24:55 +0100

    SSMW-401 tests: psa: Add support to dynamic test list
    Pass custom test list file to the test executable.
    Test executable copies the content of this file into a buffer.
    This buffer is passed to VAL dispatcher using PAL API.
    Only tests listed in the custom test list are executed.
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- tests/psa/main.c
+++ tests/psa/main.c
@@ -1,18 +1,95 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2022 NXP
+ * Copyright 2022-2023 NXP
  */
 
-#include <stdint.h>
+#include <stdarg.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <string.h>
+
+#include <getopt.h>
 
 #include <smw_status.h>
 #include <smw_osal.h>
 
+#include "psa_arch_tests.h"
+
 #define DEFAULT_KEY_DB "/var/tmp/key_db_smw_psa_test.dat"
 
-int32_t val_entry(void);
+static void read_custom_test_list_file(char **buffer, char *file_name)
+{
+	FILE *f = NULL;
+	long err = 0;
+	unsigned long fsize = 0;
+
+	if (!file_name)
+		return;
+
+	f = fopen(file_name, "r");
+	if (!f)
+		goto end;
+
+	if (fseek(f, 0, SEEK_END)) {
+		if (ferror(f))
+			perror("fseek() SEEK_END");
+		goto end;
+	}
+
+	err = ftell(f);
+	if (err < 0) {
+		if (ferror(f))
+			perror("ftell()");
+		goto end;
+	}
+
+	fsize = err;
+
+	if (fseek(f, 0, SEEK_SET)) {
+		if (ferror(f))
+			perror("fseek() SEEK_SET");
+		goto end;
+	}
+
+	*buffer = malloc(fsize + 1);
+	if (!*buffer)
+		goto end;
+
+	if (fsize != fread(*buffer, sizeof **buffer, fsize, f)) {
+		if (ferror(f))
+			perror("fread()");
+
+		free(*buffer);
+		*buffer = NULL;
+
+		goto end;
+	}
+
+	*(*buffer + fsize) = '\0';
+
+end:
+	if (f && fclose(f))
+		perror("fclose()");
+}
+
+/**
+ * usage - Print program usage.
+ * @progname: Program name.
+ *
+ * Return:
+ * None.
+ */
+static void usage(char *progname)
+{
+	printf("Usage: %s <options>\n", progname);
+	printf("\n");
+	printf("options:\n");
+	printf("\t-h    This help\n");
+	printf("\t-t    Custom test list file\n");
+	printf("\n");
+}
 
 /**
  * main() - PSA Architecture Testsuite main function.
@@ -24,11 +101,12 @@ int32_t val_entry(void);
  */
 int main(int argc, char **argv)
 {
+	uint32_t ret;
 	enum smw_status_code status;
 	char *filepath = DEFAULT_KEY_DB;
-
-	(void)argc;
-	(void)argv;
+	int option = 0;
+	char *custom_test_list = NULL;
+	char *custom_test_list_file_name = NULL;
 
 	struct se_info {
 		unsigned int storage_id;
@@ -39,6 +117,30 @@ int main(int argc, char **argv)
 	struct tee_info {
 		char ta_uuid[37];
 	} tee_default_info = { { "1682dada-20de-4b02-9eaa-284776931233" } };
+
+	if (argc > 1) {
+		/* Parse command line argument to get the options. */
+		do {
+			option = getopt(argc, argv, "h:t:");
+
+			switch (option) {
+			case -1:
+				break;
+
+			case 't':
+				custom_test_list_file_name = optarg;
+				break;
+
+			case 'h':
+				usage(argv[0]);
+				return 0;
+
+			default:
+				usage(argv[0]);
+				return -1;
+			}
+		} while (option != -1);
+	}
 
 	status = smw_osal_set_subsystem_info("ELE", &se_default_info,
 					     sizeof(se_default_info));
@@ -59,5 +161,15 @@ int main(int argc, char **argv)
 	    status != SMW_STATUS_LIBRARY_ALREADY_INIT)
 		return -1;
 
-	return val_entry();
+	read_custom_test_list_file(&custom_test_list,
+				   custom_test_list_file_name);
+
+	pal_set_custom_test_list(custom_test_list);
+
+	ret = val_entry();
+
+	if (custom_test_list)
+		free(custom_test_list);
+
+	return ret;
 }
---
+++ tests/psa/psa_arch_tests.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef __PSA_ARCH_TESTS_H__
+#define __PSA_ARCH_TESTS_H__
+
+#include <stdint.h>
+
+int32_t val_entry(void);
+void pal_set_custom_test_list(char *custom_test_list);
+
+#endif /* __PSA_ARCH_TESTS_H__ */

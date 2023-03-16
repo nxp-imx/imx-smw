nxp-imx/imx-smw

commit 73580a1a15a49900abb7bcac06c99e1b9067c7e9

SSMW-257 core: psa: PSA key manager
- Implement functions related to key mamager
  - psa_destroy_key()
  - psa_export_key()
  - psa_generate_key()
  - psa_import_key()
  - psa_reset_key_attributes()
- Move all functions related to key manager to core/psa/keymgr.c
- Add definitions related to key policy in core/inc/keymgr.h

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/psa/common.c
@@ -1,0 +1,38 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2022 NXP
+ */
+
+#include "smw_status.h"
+#include "smw_strings.h"
+
+#include "debug.h"
+#include "subsystems.h"
+#include "config.h"
+
+enum smw_status_code call_smw_api(enum smw_status_code (*api)(void *a),
+				  void *args,
+				  struct smw_config_psa_config *config,
+				  smw_subsystem_t *subsystem_name)
+{
+	enum smw_status_code status;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	status = api(args);
+	if (config->alt && status == SMW_STATUS_OPERATION_NOT_SUPPORTED &&
+	    subsystem_name && *subsystem_name) {
+		*subsystem_name = NULL;
+		status = api(args);
+	}
+
+	return status;
+}
+
+smw_subsystem_t get_subsystem_name(struct smw_config_psa_config *config)
+{
+	if (config->subsystem_id != SUBSYSTEM_ID_INVALID)
+		return smw_config_get_subsystem_name(config->subsystem_id);
+
+	return NULL;
+}

core/psa/common.h
@@ -1,0 +1,43 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2022 NXP
+ */
+
+#ifndef __COMMON__H__
+#define __COMMON__H__
+
+#include "config.h"
+
+/**
+ * call_smw_api() - Call SMW API.
+ * @api: SMW API.
+ * @args: Arguments of the SMW API.
+ * @config: PSA configuration.
+ * @subsystem_name: Pointer to the subsystem name in the arguments structure.
+ *
+ * This function first calls the SMW API with the requested subsystem.
+ * If the operation is not supported and the subsystem fallback mechanism is configured,
+ * the SMW API is called another time with the default subsystem configured if different
+ * from the requested subsystem.
+ *
+ * Return:
+ * See &enum smw_status_code.
+ */
+enum smw_status_code call_smw_api(enum smw_status_code (*api)(void *a),
+				  void *args,
+				  struct smw_config_psa_config *config,
+				  smw_subsystem_t *subsystem_name);
+
+/**
+ * get_subsystem_name() - Get the configured PSA subsystem name.
+ * @config: PSA configuration.
+ *
+ * This function returns the default subsystem to be used for PSA operations
+ * if one has been configured, NULL otherwise.
+ *
+ * Return:
+ * See &typedef smw_subsystem_t.
+ */
+smw_subsystem_t get_subsystem_name(struct smw_config_psa_config *config);
+
+#endif /* __COMMON__H__ */

core/psa/crypto.c
@@ -3,17 +3,15 @@
  * Copyright 2022 NXP
  */
 
-#include "psa/crypto.h"
-
 #include "smw_crypto.h"
+
+#include "psa/crypto.h"
 
 #include "compiler.h"
 #include "debug.h"
 #include "utils.h"
-#include "operations.h"
-#include "subsystems.h"
-#include "config.h"
 
+#include "common.h"
 #include "util_status.h"
 
 #define HASH_ALGO(_id, _name, _length, _block_size)                            \
@@ -34,33 +32,6 @@ static struct hash_algo_info {
 		       HASH_ALGO(PSA_ALG_SHA_384, "SHA384", 48, 128),
 		       HASH_ALGO(PSA_ALG_SHA_512, "SHA512", 64, 128),
 		       HASH_ALGO(PSA_ALG_SM3, "SM3", 32, 64) };
-
-enum smw_status_code call_smw_api(enum smw_status_code (*api)(void *a),
-				  void *args,
-				  struct smw_config_psa_config *config,
-				  smw_subsystem_t *subsystem_name)
-{
-	enum smw_status_code status;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	status = api(args);
-	if (config->alt && status == SMW_STATUS_OPERATION_NOT_SUPPORTED &&
-	    subsystem_name && *subsystem_name) {
-		*subsystem_name = NULL;
-		status = api(args);
-	}
-
-	return status;
-}
-
-static smw_subsystem_t get_subsystem_name(struct smw_config_psa_config *config)
-{
-	if (config->subsystem_id != SUBSYSTEM_ID_INVALID)
-		return smw_config_get_subsystem_name(config->subsystem_id);
-
-	return NULL;
-}
 
 static struct hash_algo_info *get_hash_algo_info(psa_algorithm_t alg)
 {
@@ -477,19 +448,6 @@ __export psa_status_t psa_cipher_update(psa_cipher_operation_t *operation,
 	return PSA_ERROR_NOT_SUPPORTED;
 }
 
-__export psa_status_t psa_copy_key(psa_key_id_t source_key,
-				   const psa_key_attributes_t *attributes,
-				   psa_key_id_t *target_key)
-{
-	(void)source_key;
-	(void)attributes;
-	(void)target_key;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
 __export psa_status_t psa_crypto_init(void)
 {
 	SMW_DBG_TRACE_FUNCTION_CALL;
@@ -498,53 +456,6 @@ __export psa_status_t psa_crypto_init(void)
 		return PSA_ERROR_GENERIC_ERROR;
 
 	return PSA_SUCCESS;
-}
-
-__export psa_status_t psa_destroy_key(psa_key_id_t key)
-{
-	(void)key;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t psa_export_key(psa_key_id_t key, uint8_t *data,
-				     size_t data_size, size_t *data_length)
-{
-	(void)key;
-	(void)data;
-	(void)data_size;
-	(void)data_length;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t psa_export_public_key(psa_key_id_t key, uint8_t *data,
-					    size_t data_size,
-					    size_t *data_length)
-{
-	(void)key;
-	(void)data;
-	(void)data_size;
-	(void)data_length;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t psa_generate_key(const psa_key_attributes_t *attributes,
-				       psa_key_id_t *key)
-{
-	(void)attributes;
-	(void)key;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
 }
 
 __export psa_status_t psa_generate_random(uint8_t *output, size_t output_size)
@@ -569,17 +480,6 @@ __export psa_status_t psa_generate_random(uint8_t *output, size_t output_size)
 				      &args, &config, &args.subsystem_name);
 
 	return util_smw_to_psa_status(status);
-}
-
-__export psa_status_t psa_get_key_attributes(psa_key_id_t key,
-					     psa_key_attributes_t *attributes)
-{
-	(void)key;
-	(void)attributes;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
 }
 
 __export psa_status_t psa_hash_abort(psa_hash_operation_t *operation)
@@ -743,145 +643,6 @@ __export psa_status_t psa_hash_verify(psa_hash_operation_t *operation,
 	return PSA_ERROR_NOT_SUPPORTED;
 }
 
-__export psa_status_t psa_import_key(const psa_key_attributes_t *attributes,
-				     const uint8_t *data, size_t data_length,
-				     psa_key_id_t *key)
-{
-	(void)attributes;
-	(void)data;
-	(void)data_length;
-	(void)key;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t
-psa_key_derivation_abort(psa_key_derivation_operation_t *operation)
-{
-	(void)operation;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t
-/* Without this comment clang-format does not meet the checkpatch requirement. */
-psa_key_derivation_get_capacity(const psa_key_derivation_operation_t *operation,
-				size_t *capacity)
-{
-	(void)operation;
-	(void)capacity;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t
-/* Without this comment clang-format does not meet the checkpatch requirement. */
-psa_key_derivation_input_bytes(psa_key_derivation_operation_t *operation,
-			       psa_key_derivation_step_t step,
-			       const uint8_t *data, size_t data_length)
-{
-	(void)operation;
-	(void)step;
-	(void)data;
-	(void)data_length;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t
-psa_key_derivation_input_key(psa_key_derivation_operation_t *operation,
-			     psa_key_derivation_step_t step, psa_key_id_t key)
-{
-	(void)operation;
-	(void)step;
-	(void)key;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t
-/* Without this comment clang-format does not meet the checkpatch requirement. */
-psa_key_derivation_key_agreement(psa_key_derivation_operation_t *operation,
-				 psa_key_derivation_step_t step,
-				 psa_key_id_t private_key,
-				 const uint8_t *peer_key,
-				 size_t peer_key_length)
-{
-	(void)operation;
-	(void)step;
-	(void)private_key;
-	(void)peer_key;
-	(void)peer_key_length;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t
-psa_key_derivation_output_bytes(psa_key_derivation_operation_t *operation,
-				uint8_t *output, size_t output_length)
-{
-	(void)operation;
-	(void)output;
-	(void)output_length;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t
-/* Without this comment clang-format does not meet the checkpatch requirement. */
-psa_key_derivation_output_key(const psa_key_attributes_t *attributes,
-			      psa_key_derivation_operation_t *operation,
-			      psa_key_id_t *key)
-{
-	(void)attributes;
-	(void)operation;
-	(void)key;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t
-/* Without this comment clang-format does not meet the checkpatch requirement. */
-psa_key_derivation_set_capacity(psa_key_derivation_operation_t *operation,
-				size_t capacity)
-{
-	(void)operation;
-	(void)capacity;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export psa_status_t
-/* Without this comment clang-format does not meet the checkpatch requirement. */
-psa_key_derivation_setup(psa_key_derivation_operation_t *operation,
-			 psa_algorithm_t alg)
-{
-	(void)operation;
-	(void)alg;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	return PSA_ERROR_NOT_SUPPORTED;
-}
-
 __export psa_status_t psa_mac_abort(psa_mac_operation_t *operation)
 {
 	(void)operation;
@@ -1016,13 +777,6 @@ __export psa_status_t psa_raw_key_agreement(psa_algorithm_t alg,
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	return PSA_ERROR_NOT_SUPPORTED;
-}
-
-__export void psa_reset_key_attributes(psa_key_attributes_t *attributes)
-{
-	(void)attributes;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
 }
 
 __export psa_status_t psa_sign_hash(psa_key_id_t key, psa_algorithm_t alg,

core/inc/keymgr.h
@@ -42,25 +42,60 @@
 #define DERIVE_STR	   "DERIVE"
 #define ALGO_STR	   "ALGO"
 #define HASH_STR	   "HASH"
+#define KDF_STR		   "KDF"
 #define LENGTH_STR	   "LENGTH"
 #define MIN_LENGTH_STR	   "MIN_LENGTH"
 
-#define ANY_STR		   "ANY"
-#define ALL_CIPHER_STR	   "ALL_CIPHER"
-#define HMAC_STR	   "HMAC"
-#define CMAC_STR	   "CMAC"
-#define CTR_STR		   "CTR"
-#define ECB_NO_PADDING_STR "ECB_NO_PADDING"
-#define CBC_NO_PADDING_STR "CBC_NO_PADDING"
-#define CCM_STR		   "CCM"
-#define RSA_PKCS1V15_STR   "RSA_PKCS1V15"
-#define RSA_PSS_STR	   "RSA_PSS"
-#define ECDSA_STR	   "ECDSA"
-#define SHA_1_STR	   "SHA1"
-#define SHA_224_STR	   "SHA224"
-#define SHA_256_STR	   "SHA256"
-#define SHA_384_STR	   "SHA384"
-#define SHA_512_STR	   "SHA512"
+#define ANY_STR			    "ANY"
+#define ALL_CIPHER_STR		    "ALL_CIPHER"
+#define HMAC_STR		    "HMAC"
+#define CBC_MAC_STR		    "CBC_MAC"
+#define CMAC_STR		    "CMAC"
+#define STREAM_CIPHER_STR	    "STREAM_CIPHER"
+#define CTR_STR			    "CTR"
+#define CFB_STR			    "CFB"
+#define OFB_STR			    "OFB"
+#define XTS_STR			    "XTS"
+#define ECB_NO_PADDING_STR	    "ECB_NO_PADDING"
+#define CBC_NO_PADDING_STR	    "CBC_NO_PADDING"
+#define CBC_PKCS7_STR		    "CBC_PKCS7"
+#define CCM_STR			    "CCM"
+#define GCM_STR			    "GCM"
+#define CHACHA20_POLY1305_STR	    "CHACHA20_POLY1305"
+#define HKDF_STR		    "HKDF"
+#define TLS12_PRF_STR		    "TLS12_PRF"
+#define TLS12_PSK_TO_MS_STR	    "TLS12_PSK_TO_MS"
+#define PBKDF2_HMAC_STR		    "PBKDF2_HMAC"
+#define PBKDF2_AES_CMAC_PRF_128_STR "PBKDF2_AES_CMAC_PRF_128"
+#define RSA_PKCS1V15_STR	    "RSA_PKCS1V15"
+#define RSA_PSS_STR		    "RSA_PSS"
+#define RSA_PSS_ANY_SALT_STR	    "RSA_PSS_ANY_SALT"
+#define ECDSA_STR		    "ECDSA"
+#define DETERMINISTIC_ECDSA_STR	    "DETERMINISTIC_ECDSA"
+#define PURE_EDDSA_STR		    "PURE_EDDSA"
+#define ED25519PH_STR		    "ED25519PH"
+#define ED448PH_STR		    "ED448PH"
+#define RSA_PKCS1V15_CRYPT_STR	    "RSA_PKCS1V15_CRYPT"
+#define RSA_OAEP_STR		    "RSA_OAEP"
+#define ECDH_STR		    "ECDH"
+#define FFDH_STR		    "FFDH"
+#define MD2_STR			    "MD2"
+#define MD4_STR			    "MD4"
+#define MD5_STR			    "MD5"
+#define RIPEMD160_STR		    "RIPEMD160"
+#define SHA_1_STR		    "SHA1"
+#define SHA_224_STR		    "SHA224"
+#define SHA_256_STR		    "SHA256"
+#define SHA_384_STR		    "SHA384"
+#define SHA_512_STR		    "SHA512"
+#define SHA_512_224_STR		    "SHA512_224"
+#define SHA_512_256_STR		    "SHA512_256"
+#define SHA3_224_STR		    "SHA3_224"
+#define SHA3_256_STR		    "SHA3_256"
+#define SHA3_384_STR		    "SHA3_384"
+#define SHA3_512_STR		    "SHA3_512"
+#define SHAKE256_512_STR	    "SHAKE256_512"
+#define SM3_STR			    "SM3"
 
 enum smw_keymgr_privacy_id {
 	/* Key privacy */
@@ -216,8 +251,8 @@ struct smw_keymgr_delete_key_args {
 /**
  * smw_keymgr_alloc_keypair_buffer() - Allocate a keypair object.
  * @descriptor: Pointer to the internal Key descriptor structure.
- * @public_length: Length of the public Key buffer
- * @private_length: Length of the private Key buffer
+ * @public_length: Length of the public Key buffer.
+ * @private_length: Length of the private Key buffer.
  *
  * This function allocates a keypair buffer object and
  * the keys buffers (public/private) if corresponding lengths are set.

core/psa/CMakeLists.txt
@@ -3,9 +3,11 @@ set(OBJ_LIST ${OBJ_LIST} $<TARGET_OBJECTS:psa> PARENT_SCOPE)
 add_library(psa OBJECT "")
 set_target_properties(psa PROPERTIES C_VISIBILITY_PRESET hidden)
 target_sources(psa PRIVATE
+               common.c
                crypto.c
                initial_attestation.c
                internal_trusted_storage.c
                protected_storage.c
+               keymgr.c
                util_status.c)
 target_include_directories(psa PRIVATE ${INCLUDE_DIR})

core/psa/util_status.c
@@ -13,11 +13,12 @@ psa_status_t util_smw_to_psa_status(enum smw_status_code status)
 
 	switch (status) {
 	case SMW_STATUS_OK:
+	case SMW_STATUS_KEY_POLICY_WARNING_IGNORED:
 		psa_status = PSA_SUCCESS;
 		break;
 
 	case SMW_STATUS_UNKNOWN_ID:
-		psa_status = PSA_ERROR_DOES_NOT_EXIST;
+		psa_status = PSA_ERROR_INVALID_HANDLE;
 		break;
 
 	case SMW_STATUS_ALLOC_FAILURE:

commit 2b9608ca6e12bbe62fb45dbb58d9f264c8411ecd

SSMW-359 core: psa: Simplify PSA configuration management
- PSA configuration is managed and used in one single function.
- Functions in core/psa shall return PSA error code

Fixes: 73580a1a15a4 ("SSMW-257 core: psa: PSA key manager")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/psa/common.c
@@ -6,33 +6,34 @@
 #include "smw_status.h"
 #include "smw_strings.h"
 
+#include "psa/error.h"
+
 #include "debug.h"
 #include "subsystems.h"
 #include "config.h"
 
-enum smw_status_code call_smw_api(enum smw_status_code (*api)(void *a),
-				  void *args,
-				  struct smw_config_psa_config *config,
-				  smw_subsystem_t *subsystem_name)
+#include "util_status.h"
+
+psa_status_t call_smw_api(enum smw_status_code (*api)(void *a), void *args,
+			  smw_subsystem_t *subsystem_name)
 {
 	enum smw_status_code status;
+	struct smw_config_psa_config config;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
+	if (!subsystem_name || *subsystem_name)
+		return PSA_ERROR_INVALID_ARGUMENT;
+
+	smw_config_get_psa_config(&config);
+	*subsystem_name = smw_config_get_subsystem_name(config.subsystem_id);
+
 	status = api(args);
-	if (config->alt && status == SMW_STATUS_OPERATION_NOT_SUPPORTED &&
-	    subsystem_name && *subsystem_name) {
+	if (config.alt && status == SMW_STATUS_OPERATION_NOT_SUPPORTED &&
+	    *subsystem_name) {
 		*subsystem_name = NULL;
 		status = api(args);
 	}
 
-	return status;
-}
-
-smw_subsystem_t get_subsystem_name(struct smw_config_psa_config *config)
-{
-	if (config->subsystem_id != SUBSYSTEM_ID_INVALID)
-		return smw_config_get_subsystem_name(config->subsystem_id);
-
-	return NULL;
+	return util_smw_to_psa_status(status);
 }

core/psa/common.h
@@ -14,32 +14,18 @@
  * call_smw_api() - Call SMW API.
  * @api: SMW API.
  * @args: Arguments of the SMW API.
- * @config: PSA configuration.
  * @subsystem_name: Pointer to the subsystem name in the arguments structure.
  *
- * This function first calls the SMW API with the requested subsystem.
+ * This function sets the subsystem to be invoked in the arguments structure
+ * as the PSA default subsystem, and then calls the SMW API.
  * If the operation is not supported and the subsystem fallback mechanism is configured,
- * the SMW API is called another time with the default subsystem configured if different
- * from the requested subsystem.
+ * the SMW API is called another time and invokes the default subsystem configured
+ * for the operation.
  *
  * Return:
- * See &enum smw_status_code.
+ * PSA error code.
  */
-enum smw_status_code call_smw_api(enum smw_status_code (*api)(void *a),
-				  void *args,
-				  struct smw_config_psa_config *config,
-				  smw_subsystem_t *subsystem_name);
-
-/**
- * get_subsystem_name() - Get the configured PSA subsystem name.
- * @config: PSA configuration.
- *
- * This function returns the default subsystem to be used for PSA operations
- * if one has been configured, NULL otherwise.
- *
- * Return:
- * See &typedef smw_subsystem_t.
- */
-smw_subsystem_t get_subsystem_name(struct smw_config_psa_config *config);
+psa_status_t call_smw_api(enum smw_status_code (*api)(void *a), void *args,
+			  smw_subsystem_t *subsystem_name);
 
 #endif /* __COMMON__H__ */

core/psa/crypto.c
@@ -12,7 +12,6 @@
 #include "utils.h"
 
 #include "common.h"
-#include "util_status.h"
 
 #define HASH_ALGO(_id, _name, _length, _block_size)                            \
 	{                                                                      \
@@ -460,26 +459,21 @@ __export psa_status_t psa_crypto_init(void)
 
 __export psa_status_t psa_generate_random(uint8_t *output, size_t output_size)
 {
-	enum smw_status_code status = SMW_STATUS_OK;
 	struct smw_rng_args args = { 0 };
-	struct smw_config_psa_config config;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	if (!smw_utils_is_lib_initialized())
 		return PSA_ERROR_BAD_STATE;
 
-	smw_config_get_psa_config(&config);
-
-	args.subsystem_name = get_subsystem_name(&config);
 	args.output = output;
 	args.output_length = output_size;
 
 	if (output_size)
-		status = call_smw_api((enum smw_status_code(*)(void *))smw_rng,
-				      &args, &config, &args.subsystem_name);
+		return call_smw_api((enum smw_status_code(*)(void *))smw_rng,
+				    &args, &args.subsystem_name);
 
-	return util_smw_to_psa_status(status);
+	return PSA_SUCCESS;
 }
 
 __export psa_status_t psa_hash_abort(psa_hash_operation_t *operation)
@@ -537,9 +531,8 @@ __export psa_status_t psa_hash_compute(psa_algorithm_t alg,
 				       size_t input_length, uint8_t *hash,
 				       size_t hash_size, size_t *hash_length)
 {
-	enum smw_status_code status = SMW_STATUS_OK;
+	psa_status_t psa_status;
 	struct smw_hash_args args = { 0 };
-	struct smw_config_psa_config config;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -550,20 +543,17 @@ __export psa_status_t psa_hash_compute(psa_algorithm_t alg,
 	if (!args.algo_name)
 		return PSA_ERROR_NOT_SUPPORTED;
 
-	smw_config_get_psa_config(&config);
-
-	args.subsystem_name = get_subsystem_name(&config);
 	args.input = (unsigned char *)input;
 	args.input_length = input_length;
 	args.output = hash;
 	args.output_length = hash_size;
 
-	status = call_smw_api((enum smw_status_code(*)(void *))smw_hash, &args,
-			      &config, &args.subsystem_name);
+	psa_status = call_smw_api((enum smw_status_code(*)(void *))smw_hash,
+				  &args, &args.subsystem_name);
 
 	*hash_length = args.output_length;
 
-	return util_smw_to_psa_status(status);
+	return psa_status;
 }
 
 __export psa_status_t psa_hash_finish(psa_hash_operation_t *operation,

core/psa/keymgr.c
@@ -11,7 +11,6 @@
 #include "debug.h"
 #include "utils.h"
 #include "tlv.h"
-#include "config.h"
 #include "keymgr.h"
 
 #include "asn1.h"
@@ -530,11 +529,9 @@ static void get_alg_name(psa_algorithm_t alg, const char **alg_str,
 	}
 }
 
-static int set_key_algo(psa_algorithm_t alg, unsigned char **tlv,
-			unsigned int *tlv_length)
+static psa_status_t set_key_algo(psa_algorithm_t alg, unsigned char **tlv,
+				 unsigned int *tlv_length)
 {
-	int status = SMW_STATUS_OK;
-
 	const char *alg_str = NULL;
 	const char *hash_str = NULL;
 	const char *kdf_str = NULL;
@@ -552,17 +549,15 @@ static int set_key_algo(psa_algorithm_t alg, unsigned char **tlv,
 	*tlv_length = 0;
 
 	if (!alg)
-		goto end;
+		return PSA_SUCCESS;
 
 	get_alg_name(alg, &alg_str, &hash_str, &kdf_str, &length, &min_length);
 
 	if (alg_str)
 		alg_len = SMW_UTILS_STRLEN(alg_str);
 
-	if (!alg_str || !alg_len) {
-		status = SMW_STATUS_INVALID_PARAM;
-		goto end;
-	}
+	if (!alg_str || !alg_len)
+		return PSA_ERROR_INVALID_ARGUMENT;
 
 	if (hash_str)
 		hash_len = SMW_UTILS_STRLEN(hash_str);
@@ -587,10 +582,8 @@ static int set_key_algo(psa_algorithm_t alg, unsigned char **tlv,
 						      sizeof(min_length));
 
 	*tlv = SMW_UTILS_MALLOC(*tlv_length);
-	if (!*tlv) {
-		status = SMW_STATUS_ALLOC_FAILURE;
-		goto end;
-	}
+	if (!*tlv)
+		return PSA_ERROR_INSUFFICIENT_MEMORY;
 
 	p = *tlv;
 	SMW_UTILS_MEMCPY(p, alg_str, alg_len + 1);
@@ -613,9 +606,7 @@ static int set_key_algo(psa_algorithm_t alg, unsigned char **tlv,
 	if (kdf_len)
 		smw_tlv_set_length(kdf_tlv, p);
 
-end:
-	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
-	return status;
+	return PSA_SUCCESS;
 }
 
 static unsigned int get_usage_tlv_length(psa_key_usage_t usage_flags,
@@ -642,12 +633,12 @@ static unsigned int get_usage_tlv_length(psa_key_usage_t usage_flags,
 	return usage_tlv_length;
 }
 
-static int set_key_attributes_list(const psa_key_attributes_t *attributes,
-				   unsigned char **key_attributes_list,
-				   unsigned int *key_attributes_list_length)
+static psa_status_t
+set_key_attributes_list(const psa_key_attributes_t *attributes,
+			unsigned char **key_attributes_list,
+			unsigned int *key_attributes_list_length)
 {
-	int status = SMW_STATUS_OK;
-
+	psa_status_t psa_status;
 	unsigned char *p;
 	unsigned int i;
 	psa_key_usage_t usage_flags;
@@ -663,17 +654,15 @@ static int set_key_attributes_list(const psa_key_attributes_t *attributes,
 	*key_attributes_list = NULL;
 	*key_attributes_list_length = 0;
 
-	if (!attributes) {
-		status = SMW_STATUS_INVALID_PARAM;
-		goto end;
-	}
+	if (!attributes)
+		return PSA_ERROR_INVALID_ARGUMENT;
 
 	usage_flags = psa_get_key_usage_flags(attributes);
 
-	status = set_key_algo(psa_get_key_algorithm(attributes), &algo_v,
-			      &algo_v_length);
-	if (status != SMW_STATUS_OK)
-		goto end;
+	psa_status = set_key_algo(psa_get_key_algorithm(attributes), &algo_v,
+				  &algo_v_length);
+	if (psa_status != PSA_SUCCESS)
+		return psa_status;
 
 	if (algo_v_length)
 		algo_tlv_length =
@@ -690,7 +679,7 @@ static int set_key_attributes_list(const psa_key_attributes_t *attributes,
 
 	*key_attributes_list = SMW_UTILS_MALLOC(*key_attributes_list_length);
 	if (!*key_attributes_list) {
-		status = SMW_STATUS_ALLOC_FAILURE;
+		psa_status = PSA_ERROR_INSUFFICIENT_MEMORY;
 		goto end;
 	}
 
@@ -722,7 +711,7 @@ static int set_key_attributes_list(const psa_key_attributes_t *attributes,
 		       (unsigned int)(p - *key_attributes_list));
 
 end:
-	if (status != SMW_STATUS_OK) {
+	if (psa_status != PSA_SUCCESS) {
 		if (*key_attributes_list)
 			SMW_UTILS_FREE(*key_attributes_list);
 	}
@@ -730,8 +719,7 @@ end:
 	if (algo_v)
 		SMW_UTILS_FREE(algo_v);
 
-	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
-	return status;
+	return psa_status;
 }
 
 static psa_status_t
@@ -868,7 +856,7 @@ static psa_status_t export_gen_public_key(uint8_t *data, size_t data_size,
 static psa_status_t export_key_common(psa_key_id_t key, uint8_t *data,
 				      size_t data_size, size_t *data_length)
 {
-	enum smw_status_code status = SMW_STATUS_OK;
+	enum smw_status_code status;
 	struct smw_export_key_args args = { 0 };
 	struct smw_key_descriptor key_descriptor = { 0 };
 	struct smw_keypair_buffer keypair_buffer = { 0 };
@@ -933,7 +921,7 @@ __export psa_status_t psa_copy_key(psa_key_id_t source_key,
 
 __export psa_status_t psa_destroy_key(psa_key_id_t key)
 {
-	enum smw_status_code status = SMW_STATUS_OK;
+	enum smw_status_code status;
 	struct smw_delete_key_args args = { 0 };
 	struct smw_key_descriptor key_descriptor = { 0 };
 
@@ -969,10 +957,9 @@ __export psa_status_t psa_export_public_key(psa_key_id_t key, uint8_t *data,
 __export psa_status_t psa_generate_key(const psa_key_attributes_t *attributes,
 				       psa_key_id_t *key)
 {
-	enum smw_status_code status = SMW_STATUS_OK;
+	psa_status_t psa_status;
 	struct smw_generate_key_args args = { 0 };
 	struct smw_key_descriptor key_descriptor = { 0 };
-	struct smw_config_psa_config config;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -982,36 +969,33 @@ __export psa_status_t psa_generate_key(const psa_key_attributes_t *attributes,
 	if (!attributes || !key)
 		return PSA_ERROR_INVALID_ARGUMENT;
 
-	smw_config_get_psa_config(&config);
-
 	key_descriptor.type_name = get_key_type(attributes);
 	if (!key_descriptor.type_name)
 		return PSA_ERROR_NOT_SUPPORTED;
 
 	key_descriptor.security_size = psa_get_key_bits(attributes);
 
-	args.subsystem_name = get_subsystem_name(&config);
-	status = set_key_attributes_list(attributes, &args.key_attributes_list,
-					 &args.key_attributes_list_length);
-	if (status != SMW_STATUS_OK)
-		goto end;
+	psa_status =
+		set_key_attributes_list(attributes, &args.key_attributes_list,
+					&args.key_attributes_list_length);
+	if (psa_status != PSA_SUCCESS)
+		return psa_status;
 
 	args.key_descriptor = &key_descriptor;
 
-	status = call_smw_api((enum smw_status_code(*)(void *))smw_generate_key,
-			      &args, &config, &args.subsystem_name);
+	psa_status =
+		call_smw_api((enum smw_status_code(*)(void *))smw_generate_key,
+			     &args, &args.subsystem_name);
 
-	if (status == SMW_STATUS_OK ||
-	    status == SMW_STATUS_KEY_POLICY_WARNING_IGNORED)
+	if (psa_status == PSA_SUCCESS)
 		*key = key_descriptor.id;
 	else
 		*key = PSA_KEY_ID_NULL;
 
-end:
 	if (args.key_attributes_list)
 		SMW_UTILS_FREE(args.key_attributes_list);
 
-	return util_smw_to_psa_status(status);
+	return psa_status;
 }
 
 __export psa_status_t psa_get_key_attributes(psa_key_id_t key,
@@ -1030,12 +1014,10 @@ __export psa_status_t psa_import_key(const psa_key_attributes_t *attributes,
 				     psa_key_id_t *key)
 {
 	psa_status_t psa_status;
-	enum smw_status_code status = SMW_STATUS_OK;
 	struct smw_import_key_args args = { 0 };
 	struct smw_key_descriptor key_descriptor = { 0 };
 	struct smw_keypair_buffer keypair_buffer = { 0 };
 	struct smw_keypair_gen *keypair_gen;
-	struct smw_config_psa_config config;
 	psa_key_type_t key_type;
 	unsigned int security_size;
 
@@ -1046,8 +1028,6 @@ __export psa_status_t psa_import_key(const psa_key_attributes_t *attributes,
 
 	if (!attributes || !data || !data_length || !key)
 		return PSA_ERROR_INVALID_ARGUMENT;
-
-	smw_config_get_psa_config(&config);
 
 	key_descriptor.type_name = get_key_type(attributes);
 	if (!key_descriptor.type_name)
@@ -1091,28 +1071,27 @@ __export psa_status_t psa_import_key(const psa_key_attributes_t *attributes,
 	if (!key_descriptor.security_size)
 		key_descriptor.security_size = security_size;
 
-	args.subsystem_name = get_subsystem_name(&config);
-	status = set_key_attributes_list(attributes, &args.key_attributes_list,
-					 &args.key_attributes_list_length);
-	if (status != SMW_STATUS_OK)
-		goto end;
+	psa_status =
+		set_key_attributes_list(attributes, &args.key_attributes_list,
+					&args.key_attributes_list_length);
+	if (psa_status != PSA_SUCCESS)
+		return psa_status;
 
 	args.key_descriptor = &key_descriptor;
 
-	status = call_smw_api((enum smw_status_code(*)(void *))smw_import_key,
-			      &args, &config, &args.subsystem_name);
+	psa_status =
+		call_smw_api((enum smw_status_code(*)(void *))smw_import_key,
+			     &args, &args.subsystem_name);
 
-	if (status == SMW_STATUS_OK ||
-	    status == SMW_STATUS_KEY_POLICY_WARNING_IGNORED)
+	if (psa_status == PSA_SUCCESS)
 		*key = key_descriptor.id;
 	else
 		*key = PSA_KEY_ID_NULL;
 
-end:
 	if (args.key_attributes_list)
 		SMW_UTILS_FREE(args.key_attributes_list);
 
-	return util_smw_to_psa_status(status);
+	return psa_status;
 }
 
 __export psa_status_t
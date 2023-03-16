nxp-imx/imx-smw

commit 8335fded5104af109c743ecd30483a44a4e52932

SSMW-240 subsystem: tee: Create a common library
In order to manage key storage per application, each application must
load one TA with a UUID.
OPTEE handles key storage per TA UUID.

Rework original TA to create a library to share common function and
load a TA per application.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/tee/lib_ta/core/cipher.c
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021 NXP
+ * Copyright 2021-2022 NXP
  */
 
 #include <util.h>
 #include <tee_internal_api.h>
 
 #include "tee_subsystem.h"
-#include "ta_cipher.h"
-#include "ta_keymgr.h"
+#include "cipher.h"
+#include "keymgr.h"
 
 #define MAX_CIPHER_KEYS 2
 

core/subsystems/tee/lib_ta/core/hash.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2021 NXP
+ * Copyright 2020-2022 NXP
  */
 
 #include <util.h>
@@ -9,7 +9,7 @@
 #include <tee_internal_api.h>
 
 #include "tee_subsystem.h"
-#include "ta_hash.h"
+#include "hash.h"
 
 #define ALGORITHM_INFO(_algo)                                                  \
 	{                                                                      \

core/subsystems/tee/lib_ta/core/inc/hash.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2021 NXP
+ * Copyright 2020-2022 NXP
  */
 
 #ifndef TA_HASH_H

core/subsystems/tee/lib_ta/core/keymgr.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2021 NXP
+ * Copyright 2020-2022 NXP
  */
 
 #include <util.h>
@@ -9,7 +9,7 @@
 #include <tee_internal_api_extensions.h>
 
 #include "tee_subsystem.h"
-#include "ta_keymgr.h"
+#include "keymgr.h"
 
 /* Number of attributes switch key type */
 #define NB_ATTR_ECDSA_PUB_KEY 3

core/subsystems/tee/lib_ta/core/sign_verify.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021 NXP
+ * Copyright 2021-2022 NXP
  */
 
 #include <util.h>
@@ -9,9 +9,9 @@
 #include <tee_internal_api_extensions.h>
 
 #include "tee_subsystem.h"
-#include "ta_keymgr.h"
-#include "ta_hash.h"
-#include "ta_sign_verify.h"
+#include "keymgr.h"
+#include "hash.h"
+#include "sign_verify.h"
 
 #define ALGORITHM_ID(_key_type_id, _security_size)                             \
 	{                                                                      \

core/subsystems/tee/CMakeLists.txt
@@ -50,35 +50,48 @@ target_include_directories(tee SYSTEM PRIVATE ${TEEC_INCLUDE_DIR})
 
 set(EXT_LIBS ${EXT_LIBS} ${TEEC_LIBRARY} PARENT_SCOPE)
 
-# Invoke TA Makefile
-set(TA_MAKEFILE_PATH ${CMAKE_SOURCE_DIR}/core/subsystems/tee/ta)
-
-set(TA_BUILD_DIR ${CMAKE_BINARY_DIR}/ta)
-
 # Add TA Build directory in the clean list
 set_property(DIRECTORY APPEND PROPERTY
              ADDITIONAL_MAKE_CLEAN_FILES ${TA_BUILD_DIR})
 
 string(REGEX REPLACE "-gcc$" "-" CROSS_COMPILE  ${CMAKE_C_COMPILER})
 
-set(TA_CMD make -C ${TA_MAKEFILE_PATH}
+set(TA_LIB_CMD make -C ${CMAKE_CURRENT_SOURCE_DIR}/lib_ta
+		TA_DEV_KIT_DIR=${TA_DEV_KIT_DIR}
+                CROSS_COMPILE=${CROSS_COMPILE}
+		CFG_TEE_TA_LOG_LEVEL=${VERBOSE}
+		O=${CMAKE_BINARY_DIR}/lib_ta
+                )
+
+
+set(TA_CMD make -C ${CMAKE_CURRENT_SOURCE_DIR}/ta
                 BINARY=${TA_BINARY}
-                TA_EXPORT_DIR=${TA_DEV_KIT_DIR}
-                TA_CROSS_COMPILE=${CROSS_COMPILE}
-                TA_LOG_LEVEL=${VERBOSE}
-                O=${TA_BUILD_DIR}
+		TA_DEV_KIT_DIR=${TA_DEV_KIT_DIR}
+                CROSS_COMPILE=${CROSS_COMPILE}
+		CFG_TEE_TA_LOG_LEVEL=${VERBOSE}
+                O=${CMAKE_BINARY_DIR}/ta
                 )
 
 include(ExternalProject)
-ExternalProject_Add(smw_tee_ta
+ExternalProject_Add(lib_ta
 	CONFIGURE_COMMAND ""
 	INSTALL_COMMAND ""
 	BUILD_ALWAYS True
-	SOURCE_DIR ${CMAKE_SOURCE_DIR}/core/subsystems/tee/ta
+	SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/lib_ta
+	BINARY_DIR ${CMAKE_BINARY_DIR}/lib_ta
+	TMP_DIR ${CMAKE_BINARY_DIR}/lib_ta
+	BUILD_COMMAND ${TA_LIB_CMD})
+
+ExternalProject_Add(smw_ta
+	CONFIGURE_COMMAND ""
+	INSTALL_COMMAND ""
+	DEPENDS lib_ta
+	BUILD_ALWAYS True
+	SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/ta
 	BINARY_DIR ${CMAKE_BINARY_DIR}/ta
 	TMP_DIR ${CMAKE_BINARY_DIR}/ta
 	BUILD_COMMAND ${TA_CMD})
 
 include(GNUInstallDirs)
-install(FILES ${TA_BUILD_DIR}/${TA_FILE}
+install(FILES ${CMAKE_BINARY_DIR}/ta/${TA_FILE}
        	DESTINATION ${DESTDIR}/${CMAKE_INSTALL_LIBDIR}/optee_armtz)

core/subsystems/tee/lib_ta/Makefile
@@ -1,0 +1,11 @@
+LIBNAME = libsmw_ta
+CPPFLAGS += -DCFG_TEE_TA_LOG_LEVEL=$(CFG_TEE_TA_LOG_LEVEL)
+
+include ${TA_DEV_KIT_DIR}/mk/ta_dev_kit.mk
+
+all: move
+
+move:
+	@echo "Move target $(libname).a to ${O}"
+	@echo $(link-out-dir)
+	$(q)mv $(libname).a ${O}

core/subsystems/tee/lib_ta/core/entry.c
@@ -1,0 +1,100 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2022 NXP
+ */
+
+#include <tee_internal_api.h>
+#include <tee_subsystem.h>
+
+#include <libsmw_ta.h>
+
+#include "keymgr.h"
+#include "hash.h"
+#include "sign_verify.h"
+#include "hmac.h"
+#include "rng.h"
+#include "cipher.h"
+#include "operation_context.h"
+
+TEE_Result libsmw_detach(void)
+{
+	TEE_Result res;
+
+	res = clear_key_linked_list();
+	if (res) {
+		EMSG("Error 0x%" PRIx32 " while cleaning key linked list", res);
+		res = TEE_ERROR_GENERIC;
+	}
+
+	return res;
+}
+
+TEE_Result libsmw_dispatcher(uint32_t cmd_id, uint32_t param_types,
+			     TEE_Param params[TEE_NUM_PARAMS])
+{
+	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
+
+	FMSG("Executing %s", __func__);
+
+	switch (cmd_id) {
+	case CMD_GENERATE_KEY:
+		res = generate_key(param_types, params);
+		break;
+
+	case CMD_DELETE_KEY:
+		res = delete_key(param_types, params);
+		break;
+
+	case CMD_IMPORT_KEY:
+		res = import_key(param_types, params);
+		break;
+
+	case CMD_EXPORT_KEY:
+		res = export_key(param_types, params);
+		break;
+
+	case CMD_HASH:
+		res = hash(param_types, params);
+		break;
+
+	case CMD_SIGN:
+	case CMD_VERIFY:
+		res = sign_verify(param_types, params, cmd_id);
+		break;
+
+	case CMD_HMAC:
+		res = hmac(param_types, params);
+		break;
+
+	case CMD_RNG:
+		res = rng(param_types, params);
+		break;
+
+	case CMD_CIPHER_INIT:
+		res = cipher_init(param_types, params);
+		break;
+
+	case CMD_CIPHER_UPDATE:
+		res = cipher_update(param_types, params);
+		break;
+
+	case CMD_CIPHER_FINAL:
+		res = cipher_final(param_types, params);
+		break;
+
+	case CMD_CANCEL_OP:
+		res = cancel_operation(param_types, params);
+		break;
+
+	case CMD_COPY_CTX:
+		res = copy_context(param_types, params);
+		break;
+
+	default:
+		break;
+	}
+
+	FMSG("%s command %u returned 0x%" PRIx32, __func__, cmd_id, res);
+
+	return res;
+}

commit 264070dbbd4a6da0ca491931a58bbd07a46d70e5

SSMW-386 subsystems: tee: Fix TEE TA compilation warnings
Fixes TA warnings.

Fixes: 8335fded5104 ("SSMW-240 subsystem: tee: Create a common library")
Fixes: aa10810b3b1e ("SSMW-371 subsystems: tee: Implements MAC operation")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/tee/lib_ta/core/cipher.c
@@ -84,7 +84,7 @@ TEE_Result cipher_init(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS])
 		goto end;
 	}
 
-	max_key_size = key_info[0].maxKeySize;
+	max_key_size = key_info[0].maxObjectSize;
 
 	if (nb_ids == MAX_CIPHER_KEYS) {
 		res = ta_get_key_handle(&keys_handle[1].handle,
@@ -101,7 +101,7 @@ TEE_Result cipher_init(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS])
 			goto end;
 		}
 
-		max_key_size = MAX(max_key_size, key_info[1].maxKeySize);
+		max_key_size = MAX(max_key_size, key_info[1].maxObjectSize);
 		max_key_size *= 2;
 	}
 

core/subsystems/tee/lib_ta/core/common.c
@@ -12,7 +12,7 @@ TEE_Result check_operation_keys_usage(TEE_OperationHandle op,
 	TEE_Result res = TEE_SUCCESS;
 	TEE_OperationInfoMultiple *op_info = NULL;
 	TEE_OperationInfoKey *op_key_info = NULL;
-	uint32_t op_info_size;
+	size_t op_info_size;
 
 	if (!key_info && nb_keys) {
 		res = TEE_ERROR_BAD_PARAMETERS;

core/subsystems/tee/lib_ta/core/hash.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2022 NXP
+ * Copyright 2020-2023 NXP
  */
 
 #include <util.h>
@@ -21,7 +21,7 @@
 static const struct algorithm_info {
 	enum tee_algorithm_id ca_id;
 	uint32_t ta_id;
-	uint32_t length;
+	size_t length;
 } algorithm_infos[] = { ALGORITHM_INFO(MD5),	ALGORITHM_INFO(SHA1),
 			ALGORITHM_INFO(SHA224), ALGORITHM_INFO(SHA256),
 			ALGORITHM_INFO(SHA384), ALGORITHM_INFO(SHA512),
@@ -52,7 +52,7 @@ static TEE_Result get_algorithm_info(enum tee_algorithm_id ca_id,
 }
 
 TEE_Result ta_get_digest_length(enum tee_algorithm_id tee_algorithm_id,
-				uint32_t *digest_len)
+				size_t *digest_len)
 {
 	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
 	const struct algorithm_info *info = NULL;
@@ -94,7 +94,7 @@ TEE_Result ta_get_hash_ca_id(uint32_t digest_len, enum tee_algorithm_id *ca_id)
 
 TEE_Result ta_compute_digest(enum tee_algorithm_id tee_algorithm_id,
 			     const void *chunk, uint32_t chunk_len, void *hash,
-			     uint32_t *hash_len)
+			     size_t *hash_len)
 {
 	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
 	TEE_OperationHandle operation = TEE_HANDLE_NULL;

core/subsystems/tee/lib_ta/core/inc/hash.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2022 NXP
+ * Copyright 2020-2023 NXP
  */
 
 #ifndef TA_HASH_H
@@ -29,7 +29,7 @@ TEE_Result hash(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS]);
  * TEE_ERROR_NOT_SUPPORTED	- Hash algorithm is not supported.
  */
 TEE_Result ta_get_digest_length(enum tee_algorithm_id tee_algorithm_id,
-				uint32_t *digest_len);
+				size_t *digest_len);
 
 /**
  * ta_get_hash_ca_id() - Get CA hash ID from digest length.
@@ -58,6 +58,6 @@ TEE_Result ta_get_hash_ca_id(uint32_t digest_len, enum tee_algorithm_id *ca_id);
  */
 TEE_Result ta_compute_digest(enum tee_algorithm_id tee_algorithm_id,
 			     const void *chunk, uint32_t chunk_len, void *hash,
-			     uint32_t *hash_len);
+			     size_t *hash_len);
 
 #endif /* TA_HASH_H */

core/subsystems/tee/lib_ta/core/keymgr.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2022 NXP
+ * Copyright 2020-2023 NXP
  */
 
 #include <util.h>
@@ -561,11 +561,11 @@ static void shift_public_key(unsigned int key_size, unsigned int size,
 static TEE_Result export_pub_key_ecc(TEE_ObjectHandle handle,
 				     unsigned int security_size,
 				     unsigned char *pub_key,
-				     unsigned int *pub_key_size)
+				     size_t *pub_key_size)
 {
 	TEE_Result res = TEE_ERROR_NO_DATA;
-	unsigned int key_size_bytes = 0;
-	unsigned int size = 0;
+	size_t key_size_bytes = 0;
+	size_t size = 0;
 
 	FMSG("Executing %s", __func__);
 
@@ -576,7 +576,7 @@ static TEE_Result export_pub_key_ecc(TEE_ObjectHandle handle,
 
 	/* Public key size is twice private key size */
 	if (*pub_key_size != 2 * key_size_bytes) {
-		EMSG("Invalid pub key size: %d (%d expected)", *pub_key_size,
+		EMSG("Invalid pub key size: %zu (%zu expected)", *pub_key_size,
 		     2 * key_size_bytes);
 		return res;
 	}
@@ -989,9 +989,9 @@ static TEE_Result set_key_rsa_attribute(unsigned char *pub_exp,
  */
 static TEE_Result export_pub_key_rsa(TEE_ObjectHandle handle,
 				     unsigned char *modulus,
-				     unsigned int *modulus_len,
+				     size_t *modulus_len,
 				     unsigned char *pub_exp,
-				     unsigned int *pub_exp_len)
+				     size_t *pub_exp_len)
 {
 	TEE_Result res = TEE_ERROR_NO_DATA;
 
@@ -1030,8 +1030,8 @@ TEE_Result generate_key(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS])
 	unsigned char *pub_key = NULL;
 	unsigned char *modulus = NULL;
 	unsigned char *rsa_pub_exp_attr = NULL;
-	uint32_t *pub_key_size = NULL;
-	uint32_t *modulus_size = NULL;
+	size_t *pub_key_size = NULL;
+	size_t *modulus_size = NULL;
 	size_t rsa_pub_exp_attr_len = 0;
 	bool persistent = false;
 	struct key_data *key_data = NULL;
@@ -1513,8 +1513,8 @@ TEE_Result export_key(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS])
 	TEE_ObjectHandle key_handle = TEE_HANDLE_NULL;
 	TEE_ObjectInfo obj_info = { 0 };
 	bool persistent = false;
-	uint32_t *modulus_len = NULL;
-	uint32_t *pub_len = NULL;
+	size_t *modulus_len = NULL;
+	size_t *pub_len = NULL;
 	unsigned char *modulus = NULL;
 	unsigned char *pub_data = NULL;
 

core/subsystems/tee/lib_ta/core/mac.c
@@ -64,7 +64,7 @@ static TEE_Result mac_operate(uint32_t param_types,
 	void *message;
 	uint32_t message_len;
 	void *mac;
-	uint32_t mac_len;
+	size_t mac_len;
 	const struct algo_info *alg = NULL;
 
 	bool persistent = false;

core/subsystems/tee/lib_ta/core/sign_verify.c
@@ -238,7 +238,7 @@ TEE_Result sign_verify(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS],
 	uint32_t mode;
 	uint32_t algorithm_id = 0;
 	void *digest = NULL;
-	uint32_t digest_len = 0;
+	size_t digest_len = 0;
 	bool persistent = false;
 	uint32_t attr_count = 0;
 	struct sign_verify_shared_params *shared_params = NULL;
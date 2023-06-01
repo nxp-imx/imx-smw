nxp-imx/imx-smw

commit 0c51cf5d2cee694eb5f3fbfb68b1d4c10e3bfaba

SSMW-380 subsystems: ele: Implements get key attributes
Implementation of the ELE get key attributes operation.
Retrieve the key attributes and convert key policy (usage/algorithm) in
policy TLV list.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/lifecycle.c
@@ -1,0 +1,113 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ */
+
+#include "debug.h"
+#include "tlv.h"
+#include "utils.h"
+
+#include "common.h"
+
+#define LIFECYCLE(_lifecycle)                                                  \
+	{                                                                      \
+		.str = LC_##_lifecycle##_STR,                                  \
+		.ele = HSM_KEY_LIFECYCLE_##_lifecycle                          \
+	}
+
+static const struct lifecycle {
+	const char *str;
+	hsm_key_lifecycle_t ele;
+} lifecycles[] = { LIFECYCLE(OPEN), LIFECYCLE(CLOSED),
+		   LIFECYCLE(CLOSED_LOCKED) };
+
+static int lifecycle_to_string(unsigned char **str, int str_length,
+			       hsm_key_lifecycle_t lifecycle)
+{
+	int out_len = 0;
+	size_t i = 0;
+
+	if (lifecycle) {
+		for (; i < ARRAY_SIZE(lifecycles); i++) {
+			if (!(lifecycle & lifecycles[i].ele))
+				continue;
+
+			SMW_DBG_PRINTF(DEBUG, "%s(%d) %s\n", __func__, __LINE__,
+				       lifecycles[i].str);
+
+			out_len = SMW_UTILS_STRLEN(lifecycles[i].str) + 1;
+
+			if (*str) {
+				if (str_length >= out_len) {
+					SMW_UTILS_MEMCPY(*str,
+							 lifecycles[i].str,
+							 out_len);
+					*str += out_len;
+				} else {
+					out_len = -1;
+				}
+			}
+
+			break;
+		}
+	}
+
+	return out_len;
+}
+
+int ele_get_lifecycle(unsigned char **lifecycle, unsigned int *lifecycle_len,
+		      hsm_key_lifecycle_t ele_lifecycle)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	int lc_str_len = 0;
+	unsigned char *p = NULL;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!lifecycle || !lifecycle_len)
+		goto exit;
+
+	/* Get the expected lifecycle(s) string length */
+	lc_str_len = lifecycle_to_string(&p, lc_str_len, ele_lifecycle);
+	if (lc_str_len == -1) {
+		status = SMW_STATUS_OPERATION_FAILURE;
+		goto exit;
+	}
+
+	/* Calculate lifecyle length and allocate the lifecycle string */
+	*lifecycle_len = SMW_TLV_ELEMENT_LENGTH(LIFECYCLE_STR, lc_str_len);
+
+	*lifecycle = SMW_UTILS_CALLOC(1, *lifecycle_len);
+	if (!*lifecycle) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		goto exit;
+	}
+
+	p = *lifecycle;
+	smw_tlv_set_type(&p, LIFECYCLE_STR);
+
+	/* Get the expected lifecycle(s) string */
+	lc_str_len = lifecycle_to_string(&p, lc_str_len, ele_lifecycle);
+	if (lc_str_len == -1) {
+		status = SMW_STATUS_OPERATION_FAILURE;
+	} else {
+		smw_tlv_set_length(*lifecycle, p);
+		status = SMW_STATUS_OK;
+	}
+
+exit:
+	if (status != SMW_STATUS_OK) {
+		if (lifecycle && *lifecycle) {
+			SMW_UTILS_FREE(*lifecycle);
+
+			*lifecycle = NULL;
+		}
+
+		if (lifecycle_len)
+			*lifecycle_len = 0;
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}

core/subsystems/ele/CMakeLists.txt
@@ -20,7 +20,8 @@ set_target_properties(ele PROPERTIES C_VISIBILITY_PRESET hidden)
 
 target_sources(ele PRIVATE
                entry.c
-               $<$<BOOL:${ENABLE_KEYMGR_MODULE}>:keymgr.c keymgr_derive.c key_policy.c>
+               $<$<BOOL:${ENABLE_KEYMGR_MODULE}>:keymgr.c keymgr_derive.c
+                    key_policy.c lifecycle.c>
                $<$<BOOL:${ENABLE_HASH}>:hash.c>
                $<$<BOOL:${ENABLE_MAC}>:mac.c>
                $<$<BOOL:${ENABLE_SIGN_VERIFY}>:sign_verify.c>

core/subsystems/ele/common.h
@@ -195,6 +195,26 @@ int ele_set_key_policy(const unsigned char *policy, unsigned int policy_len,
 		       unsigned int *actual_policy_len);
 
 /**
+ * ele_get_key_policy() - Convert the ELE key policy to user key policy
+ * @policy: Pointer to the key policy
+ * @policy_len: Length of @policy
+ * @ele_usage: ELE key usage(s) bit mask
+ * @ele_algo: ELE key permitted algorithm
+ *
+ * On success, the function allocates the @policy buffer and returns its length
+ * in the @policy_len.
+ *
+ * Return:
+ * SMW_STATUS_OK                         - Success
+ * SMW_STATUS_INVALID_PARAM              - Invalid parameters
+ * SMW_STATUS_ALLOC_FAILURE              - Memory allocation failure
+ * SMW_STATUS_OPERATION_FAILURE          - Unexpected operation failure
+ */
+int ele_get_key_policy(unsigned char **policy, unsigned int *policy_len,
+		       hsm_key_usage_t ele_usage,
+		       hsm_permitted_algo_t ele_algo);
+
+/**
  * ele_export_public_key() - Export the ELE public key
  * @hdl: Pointer to the ELE handles structure.
  * @key_desc: Key descriptor
@@ -214,5 +234,23 @@ int ele_set_key_policy(const unsigned char *policy, unsigned int policy_len,
  */
 int ele_export_public_key(struct hdl *hdl,
 			  struct smw_keymgr_descriptor *key_desc);
+
+/**
+ * ele_get_lifecycle() - Convert the ELE lifecycle to user lifecycle
+ * @lifecycle: Pointer to the lifecycle string
+ * @lifecycle_len: Length of @lifecycle
+ * @ele_lifecycle: ELE lifecycle(s) bit mask
+ *
+ * On success, the function allocates the @lifecycle buffer and returns its
+ * length in the @lifecycle_len.
+ *
+ * Return:
+ * SMW_STATUS_OK                         - Success
+ * SMW_STATUS_INVALID_PARAM              - Invalid parameters
+ * SMW_STATUS_ALLOC_FAILURE              - Memory allocation failure
+ * SMW_STATUS_OPERATION_FAILURE          - Unexpected operation failure
+ */
+int ele_get_lifecycle(unsigned char **lifecycle, unsigned int *lifecycle_len,
+		      hsm_key_lifecycle_t ele_lifecycle);
 
 #endif /* __COMMON_H__ */

core/subsystems/ele/key_policy.c
@@ -14,27 +14,38 @@ struct perm_algo_param {
 	int shift;
 	int mask;
 	int min_bit;
-	int (*encode)(hsm_permitted_algo_t *algo, const unsigned char *value,
-		      unsigned int value_size,
+	int (*to_value)(hsm_permitted_algo_t *algo, const unsigned char *value,
+			unsigned int value_size,
+			const struct perm_algo_param *param);
+	int (*to_str)(unsigned char **str, int str_length,
+		      hsm_permitted_algo_t algo,
 		      const struct perm_algo_param *param);
 };
 
-#define KEY_USAGE(_smw, _ele)                                                  \
+#define KEY_USAGE(_smw, _ele, _restricted)                                     \
 	{                                                                      \
-		.str = _smw##_STR, .ele = HSM_KEY_USAGE_##_ele                 \
+		.str = _smw##_STR, .ele = HSM_KEY_USAGE_##_ele,                \
+		.restricted = _restricted                                      \
 	}
 
+/**
+ * struct - Key usage
+ * @str: SMW usage name used for TLV encoding.
+ * @ele: ELE key usage value.
+ * @restricted: Is usage restricted to an algorithm.
+ */
 static const struct {
 	const char *str;
 	hsm_key_usage_t ele;
-} key_usage[] = { KEY_USAGE(EXPORT, EXPORT),
-		  KEY_USAGE(ENCRYPT, ENCRYPT),
-		  KEY_USAGE(DECRYPT, DECRYPT),
-		  KEY_USAGE(SIGN_MESSAGE, SIGN_MSG),
-		  KEY_USAGE(VERIFY_MESSAGE, VERIFY_MSG),
-		  KEY_USAGE(SIGN_HASH, SIGN_HASH),
-		  KEY_USAGE(VERIFY_HASH, VERIFY_HASH),
-		  KEY_USAGE(DERIVE, DERIVE) };
+	bool restricted;
+} key_usage[] = { KEY_USAGE(EXPORT, EXPORT, false),
+		  KEY_USAGE(ENCRYPT, ENCRYPT, true),
+		  KEY_USAGE(DECRYPT, DECRYPT, true),
+		  KEY_USAGE(SIGN_MESSAGE, SIGN_MSG, true),
+		  KEY_USAGE(VERIFY_MESSAGE, VERIFY_MSG, true),
+		  KEY_USAGE(SIGN_HASH, SIGN_HASH, true),
+		  KEY_USAGE(VERIFY_HASH, VERIFY_HASH, true),
+		  KEY_USAGE(DERIVE, DERIVE, true) };
 
 #define KEY_ALGO(_smw, _ele)                                                   \
 	{                                                                      \
@@ -48,18 +59,18 @@ static const struct {
 		   KEY_ALGO(SHA_256, 0x9), KEY_ALGO(SHA_384, 0xA),
 		   KEY_ALGO(SHA_512, 0xB), KEY_ALGO(ANY, 0xFF) };
 
-static int permitted_algo_hash(hsm_permitted_algo_t *algo,
-			       const unsigned char *value,
-			       unsigned int value_size,
-			       const struct perm_algo_param *param)
+static int perm_algo_hash_val(hsm_permitted_algo_t *algo,
+			      const unsigned char *value,
+			      unsigned int value_size,
+			      const struct perm_algo_param *param)
 {
 	(void)value_size;
 
-	size_t i;
+	size_t i = 0;
 
 	SMW_DBG_PRINTF(DEBUG, "%s(%d) HASH=%s\n", __func__, __LINE__, value);
 
-	for (i = 0; i < ARRAY_SIZE(hash_algos); i++) {
+	for (; i < ARRAY_SIZE(hash_algos); i++) {
 		if (SMW_UTILS_STRCMP(hash_algos[i].str, (char *)value))
 			continue;
 
@@ -79,12 +90,12 @@ static int permitted_algo_hash(hsm_permitted_algo_t *algo,
 	return false;
 }
 
-static int permitted_algo_length(hsm_permitted_algo_t *algo,
-				 const unsigned char *value,
-				 unsigned int value_size,
-				 const struct perm_algo_param *param)
+static int perm_algo_length_val(hsm_permitted_algo_t *algo,
+				const unsigned char *value,
+				unsigned int value_size,
+				const struct perm_algo_param *param)
 {
-	int len;
+	int len = 0;
 
 	len = smw_tlv_convert_numeral(value_size, (unsigned char *)value);
 	if (len > param->mask)
@@ -93,12 +104,106 @@ static int permitted_algo_length(hsm_permitted_algo_t *algo,
 	*algo = SET_CLEAR_MASK(*algo, len << param->shift,
 			       param->mask << param->shift);
 
-	*algo |= param->min_bit;
-
 	SMW_DBG_PRINTF(DEBUG, "%s(%d) algo=0x%08X\n", __func__, __LINE__,
 		       *algo);
 
 	return true;
+}
+
+static int perm_algo_minlength_val(hsm_permitted_algo_t *algo,
+				   const unsigned char *value,
+				   unsigned int value_size,
+				   const struct perm_algo_param *param)
+{
+	if (perm_algo_length_val(algo, value, value_size, param)) {
+		*algo |= param->min_bit;
+
+		SMW_DBG_PRINTF(DEBUG, "%s(%d) algo=0x%08X\n", __func__,
+			       __LINE__, *algo);
+		return true;
+	}
+
+	return false;
+}
+
+static int perm_algo_hash_str(unsigned char **str, int str_length,
+			      hsm_permitted_algo_t algo,
+			      const struct perm_algo_param *param)
+{
+	int out_len = 0;
+	size_t i = 0;
+	int hash_algo = 0;
+
+	hash_algo = (algo >> param->shift) & param->mask;
+
+	if (hash_algo) {
+		for (; i < ARRAY_SIZE(hash_algos); i++) {
+			if (hash_algos[i].value != hash_algo)
+				continue;
+
+			SMW_DBG_PRINTF(DEBUG, "%s(%d) %s=%s\n", __func__,
+				       __LINE__, param->str, hash_algos[i].str);
+
+			out_len = SMW_UTILS_STRLEN(hash_algos[i].str) + 1;
+			out_len = SMW_TLV_ELEMENT_LENGTH(param->str, out_len);
+
+			if (*str) {
+				if (str_length >= out_len)
+					smw_tlv_set_string(str, param->str,
+							   hash_algos[i].str);
+				else
+					out_len = -1;
+			}
+
+			break;
+		}
+	}
+
+	return out_len;
+}
+
+static int perm_algo_length_str(unsigned char **str, int str_length,
+				hsm_permitted_algo_t algo,
+				const struct perm_algo_param *param)
+{
+	uint64_t algo_length = 0;
+	int out_len = 0;
+
+	if (algo & param->min_bit)
+		return out_len;
+
+	algo_length = (algo >> param->shift) & param->mask;
+
+	if (algo_length) {
+		out_len = smw_tlv_numeral_length(algo_length);
+		out_len = SMW_TLV_ELEMENT_LENGTH(param->str, out_len);
+
+		SMW_DBG_PRINTF(DEBUG, "%s(%d) %s=%lu\n", __func__, __LINE__,
+			       param->str, algo_length);
+
+		if (*str) {
+			if (str_length >= out_len)
+				smw_tlv_set_numeral(str, param->str,
+						    algo_length);
+			else
+				out_len = -1;
+		}
+	}
+
+	return out_len;
+}
+
+static int perm_algo_minlength_str(unsigned char **str, int str_length,
+				   hsm_permitted_algo_t algo,
+				   const struct perm_algo_param *param)
+{
+	int out_len = 0;
+
+	if (algo & param->min_bit)
+		out_len = perm_algo_length_str(str, str_length,
+					       algo & ~param->min_bit, param);
+
+	return out_len;
 }
 
 #define PERMITTED_ALGO_HMAC_ANY_HASH		   (PERMITTED_ALGO_HMAC_SHA256 | 0xFF)
@@ -106,52 +211,59 @@ static int permitted_algo_length(hsm_permitted_algo_t *algo,
 #define PERMITTED_ALGO_RSA_PKCS1_V15_ANY_HASH	   (0x060002FF)
 #define PERMITTED_ALGO_RSA_PKCS1_PSS_MGF1_ANY_HASH (0x060003FF)
 
-#define PERM_ALGO_PARAM(_name, _shift, _mask, _min_bit, _encode)               \
+#define PERM_ALGO_PARAM(_name, _shift, _mask, _min_bit, _to_value, _to_str)    \
 	{                                                                      \
 		.str = _name##_STR, .shift = _shift, .mask = _mask,            \
-		.min_bit = _min_bit, .encode = _encode                         \
+		.min_bit = _min_bit, .to_value = _to_value, .to_str = _to_str  \
 	}
 
 static const struct perm_algo_param perm_hmac_algo[] = {
-	PERM_ALGO_PARAM(HASH, 0, 0xFF, 0, &permitted_algo_hash),
-	PERM_ALGO_PARAM(LENGTH, 16, 0x3F, 0, &permitted_algo_length),
-	PERM_ALGO_PARAM(MIN_LENGTH, 16, 0x3F, BIT(15), &permitted_algo_length),
+	PERM_ALGO_PARAM(HASH, 0, 0xFF, 0, &perm_algo_hash_val,
+			&perm_algo_hash_str),
+	PERM_ALGO_PARAM(LENGTH, 16, 0x3F, BIT(15), &perm_algo_length_val,
+			&perm_algo_length_str),
+	PERM_ALGO_PARAM(MIN_LENGTH, 16, 0x3F, BIT(15), &perm_algo_minlength_val,
+			&perm_algo_minlength_str),
 	{ 0 }
 };
 
 static const struct perm_algo_param perm_cmac_algo[] = {
-	PERM_ALGO_PARAM(LENGTH, 16, 0x3F, 0, &permitted_algo_length),
-	PERM_ALGO_PARAM(MIN_LENGTH, 16, 0x3F, BIT(15), &permitted_algo_length),
+	PERM_ALGO_PARAM(LENGTH, 16, 0x3F, BIT(15), &perm_algo_length_val,
+			&perm_algo_length_str),
+	PERM_ALGO_PARAM(MIN_LENGTH, 16, 0x3F, BIT(15), &perm_algo_minlength_val,
+			&perm_algo_minlength_str),
 	{ 0 }
 };
 
 static const struct perm_algo_param perm_hash_algo[] = {
-	PERM_ALGO_PARAM(HASH, 0, 0xFF, 0, &permitted_algo_hash),
+	PERM_ALGO_PARAM(HASH, 0, 0xFF, 0, &perm_algo_hash_val,
+			&perm_algo_hash_str),
 	{ 0 }
 };
 
-#define PERM_ALGO(_name, _base, _params)                                       \
+#define PERM_ALGO(_name, _base, _length_mask, _params)                         \
 	{                                                                      \
 		.str = _name##_STR, .algo_base = PERMITTED_ALGO_##_base,       \
-		.params = _params                                              \
+		.length_mask = _length_mask, .params = _params                 \
 	}
 
 static const struct {
 	const char *str;
 	int algo_base;
+	int length_mask;
 	const struct perm_algo_param *params;
 } perm_algos[] = {
-	PERM_ALGO(HMAC, HMAC_ANY_HASH, perm_hmac_algo),
-	PERM_ALGO(CMAC, CMAC, perm_cmac_algo),
-	PERM_ALGO(CTR, CTR, NULL),
-	PERM_ALGO(ECB_NO_PADDING, ECB_NO_PADDING, NULL),
-	PERM_ALGO(CBC_NO_PADDING, CBC_NO_PADDING, NULL),
-	PERM_ALGO(ALL_CIPHER, ALL_CIPHER, NULL),
-	PERM_ALGO(CCM, CCM, NULL),
-	PERM_ALGO(RSA_PKCS1V15, RSA_PKCS1_V15_ANY_HASH, perm_hash_algo),
-	PERM_ALGO(RSA_PSS, RSA_PKCS1_PSS_MGF1_ANY_HASH, perm_hash_algo),
-	PERM_ALGO(ECDSA, ECDSA_ANY_HASH, perm_hash_algo),
-	PERM_ALGO(ALL_AEAD, ALL_AEAD, NULL)
+	PERM_ALGO(HMAC, HMAC_ANY_HASH, (0x3F << 16) | BIT(15), perm_hmac_algo),
+	PERM_ALGO(CMAC, CMAC, (0x3F << 16) | BIT(15), perm_cmac_algo),
+	PERM_ALGO(CTR, CTR, 0, NULL),
+	PERM_ALGO(ECB_NO_PADDING, ECB_NO_PADDING, 0, NULL),
+	PERM_ALGO(CBC_NO_PADDING, CBC_NO_PADDING, 0, NULL),
+	PERM_ALGO(ALL_CIPHER, ALL_CIPHER, 0, NULL),
+	PERM_ALGO(CCM, CCM, 0, NULL),
+	PERM_ALGO(RSA_PKCS1V15, RSA_PKCS1_V15_ANY_HASH, 0, perm_hash_algo),
+	PERM_ALGO(RSA_PSS, RSA_PKCS1_PSS_MGF1_ANY_HASH, 0, perm_hash_algo),
+	PERM_ALGO(ECDSA, ECDSA_ANY_HASH, 0, perm_hash_algo),
+	PERM_ALGO(ALL_AEAD, ALL_AEAD, 0, NULL)
 };
 
 static int convert_algo_param(hsm_permitted_algo_t *algo,
@@ -166,7 +278,7 @@ static int convert_algo_param(hsm_permitted_algo_t *algo,
 			SMW_DBG_PRINTF(DEBUG, "%s(%d) Param=%s\n", __func__,
 				       __LINE__, type);
 
-			return param->encode(algo, value, value_size, param);
+			return param->to_value(algo, value, value_size, param);
 		}
 
 		param++;
@@ -296,14 +408,14 @@ exit:
 }
 
 /**
- * convert_usage() - Convert SMW to ELE key usage value
+ * convert_usage_to_value() - Convert SMW to ELE key usage value
  * @value: String value to convert
  * @usage: ELE key usage bitmask
  *
  * Return:
  * True if value supported, otherwise false.
  */
-static bool convert_usage(const char *value, hsm_key_usage_t *usage)
+static bool convert_usage_to_value(const char *value, hsm_key_usage_t *usage)
 {
 	unsigned int i;
 
@@ -316,6 +428,154 @@ static bool convert_usage(const char *value, hsm_key_usage_t *usage)
 	}
 
 	return false;
+}
+
+/**
+ * key_algo_to_string() - Convert ELE key algorithm value(s) to SMW's policy
+ *                        string
+ * @str: Pointer to string value of @algo bitmask
+ * @length: Maximum length of the @str
+ * @algo: ELE key algo bitmask
+ *
+ * Return:
+ * If @str is NULL, the expected length of @str
+ * If @str is not NULL, the length of @str built
+ * -1 in case of error (@str's length too small
+ */
+static int key_algo_to_string(unsigned char **str, int length,
+			      hsm_permitted_algo_t algo)
+{
+	const struct perm_algo_param *algo_params = NULL;
+	const char *algo_str = NULL;
+	unsigned char *p = *str;
+	int out_len = 0;
+	int rem_len = length;
+	int algo_len = 0;
+	unsigned int i = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	for (i = 0; i < ARRAY_SIZE(perm_algos); i++) {
+		if ((algo & perm_algos[i].algo_base) ==
+		    (algo & ~perm_algos[i].length_mask)) {
+			algo_str = perm_algos[i].str;
+			algo_params = perm_algos[i].params;
+			break;
+		}
+	}
+
+	if (!algo_str)
+		goto exit;
+
+	SMW_DBG_PRINTF(DEBUG, "Key algo %s\n", algo_str);
+
+	algo_len = SMW_UTILS_STRLEN(algo_str) + 1;
+	algo_len = SMW_TLV_ELEMENT_LENGTH(ALGO_STR, algo_len);
+
+	if (p) {
+		if (rem_len >= algo_len) {
+			smw_tlv_set_string(&p, ALGO_STR, algo_str);
+			rem_len -= algo_len;
+		} else {
+			out_len = -1;
+			goto exit;
+		}
+	}
+
+	out_len += algo_len;
+
+	while (algo_params && algo_params->str) {
+		algo_len = algo_params->to_str(&p, rem_len, algo, algo_params);
+
+		if (algo_len == -1) {
+			out_len = -1;
+			goto exit;
+		} else {
+			out_len += algo_len;
+		}
+
+		algo_params++;
+	}
+
+	if (p)
+		smw_tlv_set_length(p - out_len, p);
+
+exit:
+	if (out_len != -1 && *str)
+		*str = p;
+
+	SMW_DBG_PRINTF(DEBUG, "%s return length %u\n", __func__, out_len);
+	return out_len;
+}
+
+/**
+ * key_usage_algo_to_string() - Convert ELE key usage and algorithm value(s) to
+ *                              SMW's policy string
+ * @str: Pointer to string value of @usage and @algo bitmask
+ * @length: Maximum length of the @str
+ * @usage: ELE key usage bitmask
+ * @algo: ELE key algo bitmask
+ *
+ * Return:
+ * If @str is NULL, the expected length of @str
+ * If @str is not NULL, the length of @str built
+ * -1 in case of error (@str's length too small
+ */
+static int key_usage_algo_to_string(unsigned char **str, int length,
+				    hsm_key_usage_t usage,
+				    hsm_permitted_algo_t algo)
+{
+	unsigned char *p = *str;
+	int out_len = 0;
+	int rem_len = length;
+	int usage_len = 0;
+	int algo_len = 0;
+	unsigned int i = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	for (i = 0; i < ARRAY_SIZE(key_usage); i++) {
+		if (!(usage & key_usage[i].ele))
+			continue;
+
+		SMW_DBG_PRINTF(DEBUG, "Key usage: %s\n", key_usage[i].str);
+
+		usage_len = SMW_UTILS_STRLEN(key_usage[i].str) + 1;
+		usage_len = SMW_TLV_ELEMENT_LENGTH(USAGE_STR, usage_len);
+
+		if (p) {
+			if (rem_len >= usage_len) {
+				smw_tlv_set_string(&p, USAGE_STR,
+						   key_usage[i].str);
+				rem_len -= usage_len;
+			} else {
+				out_len = -1;
+				break;
+			}
+		}
+
+		if (key_usage[i].restricted) {
+			/* Get the expected algo string length */
+			algo_len = key_algo_to_string(&p, rem_len, algo);
+			if (algo_len == -1)
+				break;
+
+			usage_len += algo_len;
+
+			if (p) {
+				rem_len -= algo_len;
+				smw_tlv_set_length(p - usage_len, p);
+			}
+		}
+
+		out_len += usage_len;
+	}
+
+	if (out_len != 1 && *str)
+		*str = p;
+
+	SMW_DBG_PRINTF(DEBUG, "%s return length %u\n", __func__, out_len);
+	return out_len;
 }
 
 /**
@@ -479,7 +739,7 @@ int ele_set_key_policy(const unsigned char *policy, unsigned int policy_len,
 
 		usage_str = (char *)value;
 
-		if (!convert_usage(usage_str, ele_usage)) {
+		if (!convert_usage_to_value(usage_str, ele_usage)) {
 			ignored++;
 			continue;
 		}
@@ -536,6 +796,65 @@ exit:
 		}
 
 		*actual_policy_len = 0;
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+int ele_get_key_policy(unsigned char **policy, unsigned int *policy_len,
+		       hsm_key_usage_t ele_usage, hsm_permitted_algo_t ele_algo)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	int usage_str_len = 0;
+	unsigned char *p = NULL;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!policy || !policy_len)
+		goto exit;
+
+	/* Get the expected usage(s) and algo string length */
+	usage_str_len = key_usage_algo_to_string(&p, usage_str_len, ele_usage,
+						 ele_algo);
+	if (usage_str_len == -1) {
+		status = SMW_STATUS_OPERATION_FAILURE;
+		goto exit;
+	}
+
+	/* Calculate policy length and allocate the policy string */
+	*policy_len = SMW_TLV_ELEMENT_LENGTH(POLICY_STR, usage_str_len);
+
+	*policy = SMW_UTILS_CALLOC(1, *policy_len);
+	if (!*policy) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		goto exit;
+	}
+
+	p = *policy;
+	smw_tlv_set_type(&p, POLICY_STR);
+
+	/* Get the expected usage(s) and algo string */
+	usage_str_len = key_usage_algo_to_string(&p, usage_str_len, ele_usage,
+						 ele_algo);
+	if (usage_str_len == -1) {
+		status = SMW_STATUS_OPERATION_FAILURE;
+	} else {
+		smw_tlv_set_length(*policy, p);
+		status = SMW_STATUS_OK;
+	}
+
+exit:
+	if (status != SMW_STATUS_OK) {
+		if (policy && *policy) {
+			SMW_UTILS_FREE(*policy);
+
+			*policy = NULL;
+		}
+
+		if (policy_len)
+			*policy_len = 0;
 	}
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);

core/subsystems/ele/keymgr.c
@@ -29,6 +29,22 @@ static int ecc_public_key_length(unsigned int security_size);
 #define ELE_RAW_KEY_TYPE_MASK	      BIT(12)
 
 /*
+ * Key lifetime encoding
+ */
+#define ELE_KEY_TRANSIENT  0x0
+#define ELE_KEY_PERSISTENT 0x1
+#define ELE_KEY_PERMANENT  0xFF
+
+#define ELE_KEY_LIFETIME_PERSISTENCE_MASK 0xFF
+#define ELE_KEY_LIFETIME_PERSISTENCE_GET(val)                                  \
+	((val) & (ELE_KEY_LIFETIME_PERSISTENCE_MASK))
+#define ELE_KEY_LIFETIME_LOCATION_MASK	0xFFFFFF
+#define ELE_KEY_LIFETIME_LOCATION_SHIFT 8
+#define ELE_KEY_LIFETIME_LOCATION_GET(val)                                     \
+	(((val) >> ELE_KEY_LIFETIME_LOCATION_SHIFT) &                          \
+	 ELE_KEY_LIFETIME_LOCATION_MASK)
+
+/*
  * Macro setting the ELE key type category
  */
 #define ELE_ASYM_PUBLIC_KEY_TYPE(type)                                         \
@@ -120,6 +136,19 @@ static const struct ele_key_def *get_key_def_by_ele_type(unsigned int key_type)
 	return ret_key;
 }
 
+static enum smw_keymgr_privacy_id
+get_key_privacy_by_ele_type(unsigned int key_type)
+{
+	enum smw_keymgr_privacy_id privacy = SMW_KEYMGR_PRIVACY_ID_PRIVATE;
+
+	if (key_type & ELE_ASYM_KEYPAIR_TYPE_MASK)
+		privacy = SMW_KEYMGR_PRIVACY_ID_PAIR;
+	else if (key_type & ELE_ASYM_PUBLIC_KEY_TYPE_MASK)
+		privacy = SMW_KEYMGR_PRIVACY_ID_PUBLIC;
+
+	return privacy;
+}
+
 static int get_full_ele_key_type(enum smw_config_key_type_id key_type_id,
 				 hsm_key_type_t *ele_type)
 {
@@ -137,6 +166,32 @@ static int get_full_ele_key_type(enum smw_config_key_type_id key_type_id,
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
+}
+
+static enum smw_keymgr_persistence_id
+get_key_persistence(hsm_key_lifetime_t lifetime)
+{
+	enum smw_keymgr_persistence_id persistence =
+		SMW_KEYMGR_PERSISTENCE_ID_INVALID;
+
+	switch (ELE_KEY_LIFETIME_PERSISTENCE_GET(lifetime)) {
+	case ELE_KEY_TRANSIENT:
+		persistence = SMW_KEYMGR_PERSISTENCE_ID_TRANSIENT;
+		break;
+
+	case ELE_KEY_PERSISTENT:
+		persistence = SMW_KEYMGR_PERSISTENCE_ID_PERSISTENT;
+		break;
+
+	case ELE_KEY_PERMANENT:
+		persistence = SMW_KEYMGR_PERSISTENCE_ID_PERMANENT;
+		break;
+
+	default:
+		break;
+	}
+
+	return persistence;
 }
 
 /**
@@ -421,7 +476,8 @@ static int generate_key(struct hdl *hdl, void *args)
 		goto end;
 	}
 
-	if (key_args->key_attributes.persistent_storage) {
+	if (key_args->key_attributes.persistence ==
+	    SMW_KEYMGR_PERSISTENCE_ID_PERSISTENT) {
 		op_args.key_lifetime = HSM_SE_KEY_STORAGE_PERSISTENT;
 		op_args.key_group = PERSISTENT_KEY_GROUP;
 	} else {
@@ -601,6 +657,70 @@ static int get_key_lengths(struct hdl *hdl, void *args)
 	return status;
 }
 
+static int get_key_attributes(struct hdl *hdl, void *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_keymgr_get_key_attributes_args *key_attrs = NULL;
+	op_get_key_attr_args_t op_key_attrs = { 0 };
+	const struct ele_key_def *key_def = NULL;
+	unsigned char *policy_list = NULL;
+	unsigned char *lifecycle_list = NULL;
+	unsigned int policy_list_length = 0;
+	unsigned int lifecycle_list_length = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	key_attrs = args;
+
+	op_key_attrs.key_identifier = key_attrs->identifier.id;
+
+	status = get_key_attributes_operation(hdl, &op_key_attrs);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	key_def = get_key_def_by_ele_type(op_key_attrs.key_type);
+	if (!key_def) {
+		status = SMW_STATUS_KEY_INVALID;
+		goto end;
+	}
+
+	key_attrs->identifier.type_id = key_def->smw_key_type;
+	key_attrs->identifier.security_size = op_key_attrs.bit_key_sz;
+	key_attrs->identifier.privacy_id =
+		get_key_privacy_by_ele_type(op_key_attrs.key_type);
+	key_attrs->identifier.persistence_id =
+		get_key_persistence(op_key_attrs.key_lifetime);
+	key_attrs->identifier.storage_id =
+		ELE_KEY_LIFETIME_LOCATION_GET(op_key_attrs.key_lifetime);
+
+	status = ele_get_key_policy(&policy_list, &policy_list_length,
+				    op_key_attrs.key_usage,
+				    op_key_attrs.permitted_algo);
+
+	if (status == SMW_STATUS_OK)
+		status = ele_get_lifecycle(&lifecycle_list,
+					   &lifecycle_list_length,
+					   op_key_attrs.lifecycle);
+
+	if (status == SMW_STATUS_OK) {
+		smw_keymgr_set_policy(key_attrs, policy_list,
+				      policy_list_length);
+		smw_keymgr_set_lifecycle(key_attrs, lifecycle_list,
+					 lifecycle_list_length);
+	} else {
+		if (policy_list)
+			SMW_UTILS_FREE(policy_list);
+
+		if (lifecycle_list)
+			SMW_UTILS_FREE(lifecycle_list);
+	}
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
 int ele_set_pubkey_type(enum smw_config_key_type_id key_type_id,
 			hsm_pubkey_type_t *ele_type)
 {
@@ -708,6 +828,9 @@ bool ele_key_handle(struct hdl *hdl, enum operation_id operation_id, void *args,
 		break;
 	case OPERATION_ID_GET_KEY_LENGTHS:
 		*status = get_key_lengths(hdl, args);
+		break;
+	case OPERATION_ID_GET_KEY_ATTRIBUTES:
+		*status = get_key_attributes(hdl, args);
 		break;
 	default:
 		return false;

commit 1318f61400b3e356e098465fd5f2692c45300dec

SSMW-416 subsystems: ele: Fix key lifecycle decoding
Fix the key lifecycle decoding bit mask.

Fixes: 0c51cf5d2cee ("SSMW-380 subsystems: ele: Implements get key attributes")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/lifecycle.c
@@ -47,8 +47,6 @@ static int lifecycle_to_string(unsigned char **str, int str_length,
 					out_len = -1;
 				}
 			}
-
-			break;
 		}
 	}
 
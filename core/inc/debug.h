nxp-imx/imx-smw

commit 44a4dc4745e3878ee3014bd023809a126dc4b1f1

SSMW-70 core:utils: implement Configuration module with support of HSM
- Refactor Utils module

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/inc/debug.h
@@ -1,0 +1,70 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2020 NXP
+ */
+
+/* Debug levels */
+#define SMW_DBG_LEVEL_NONE    0 /* No trace */
+#define SMW_DBG_LEVEL_ERROR   1 /* Failures of which the user must be aware */
+#define SMW_DBG_LEVEL_INFO    2 /* Traces which could interest the user */
+#define SMW_DBG_LEVEL_DEBUG   3 /* First level of debugging information */
+#define SMW_DBG_LEVEL_VERBOSE 4 /* Maximum level of debugging information */
+
+#if defined(ENABLE_DEBUG)
+
+#define SMW_ABORT abort
+
+#else /* ENABLE_DEBUG */
+
+#define SMW_ABORT(...)
+
+#endif /* ENABLE_DEBUG */
+
+#if defined(ENABLE_TRACE)
+
+#define SMW_DBG_LEVEL TRACE_LEVEL
+
+#define SMW_PRINTF(...)                                                        \
+	do {                                                                   \
+		if (g_smw_ctx.ops.thread_self)                                 \
+			printf("(%lx) ", g_smw_ctx.ops.thread_self());         \
+		printf(__VA_ARGS__);                                           \
+	} while (0)
+
+#define SMW_DBG_TRACE_FUNCTION_CALL                                            \
+	do {                                                                   \
+		if (SMW_DBG_LEVEL_VERBOSE <= SMW_DBG_LEVEL)                    \
+			SMW_PRINTF("Executing %s\n", __func__);                \
+	} while (0)
+
+#define SMW_DBG_PRINTF(level, ...)                                             \
+	do {                                                                   \
+		if (SMW_DBG_LEVEL_##level <= SMW_DBG_LEVEL)                    \
+			SMW_PRINTF(__VA_ARGS__);                               \
+	} while (0)
+
+#define SMW_DBG_PRINTF_COND(cond, level, ...)                                  \
+	do {                                                                   \
+		if (SMW_DBG_LEVEL_##level <= SMW_DBG_LEVEL)                    \
+			if (cond)                                              \
+				SMW_PRINTF(__VA_ARGS__);                       \
+	} while (0)
+
+#else /* ENABLE_TRACE */
+
+#define SMW_PRINTF(...)
+#define SMW_DBG_TRACE_FUNCTION_CALL
+#define SMW_DBG_PRINTF(level, ...)
+#define SMW_DBG_PRINTF_COND(cond, level, ...)
+
+#endif /* ENABLE_TRACE */
+
+#define SMW_DBG_ASSERT(exp)                                                    \
+	do {                                                                   \
+		if ((exp))                                                     \
+			break;                                                 \
+		SMW_PRINTF("Assertion \"%s\" failed: file \"%s\","             \
+			   "line %d\n",                                        \
+			   #exp, __FILE__, __LINE__);                          \
+		SMW_ABORT();                                                   \
+	} while (0)

core/inc/global.h
@@ -1,0 +1,18 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2020 NXP
+ */
+
+/* SMW globals */
+/**
+ * struct smw_ctx - SMW context
+ * @ops: Structure containing the OSAL primitives
+ * @start_count: Number of threads/applications that started the SMW library
+ *
+ */
+struct smw_ctx {
+	struct smw_ops ops;
+	int start_count;
+};
+
+extern struct smw_ctx g_smw_ctx;

core/inc/list.h
@@ -1,0 +1,89 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2020 NXP
+ */
+
+/* List management */
+
+struct node;
+/**
+ * struct smw_utils_list - Linked list
+ * @length: Current number of nodes
+ * @first: Pointer to the first node of the list
+ * @last: Pointer to the last node of the list
+ *
+ */
+struct smw_utils_list {
+	struct node *first;
+	struct node *last;
+};
+
+/**
+ * smw_utils_list_init() - Initialize a linked list.
+ * @list: Pointer to a linked list.
+ *
+ * This function initializes a linked list.
+ *
+ * Return:
+ * none.
+ */
+void smw_utils_list_init(struct smw_utils_list *list);
+
+/**
+ * smw_utils_list_destroy() - Destroy a linked list.
+ * @list: Pointer to a linked list.
+ *
+ * This function destroys a linked list.
+ * All modes are destroyed.
+ * All memory dynamically allocated is freed.
+ *
+ * Return:
+ * none.
+ */
+void smw_utils_list_destroy(struct smw_utils_list *list);
+
+/**
+ * smw_utils_list_append_data() - Append data to the linked list.
+ * @list: Pointer to a linked list.
+ * @data: Pointer to the data to be stored.
+ * @destructor: Pointer to a dedicated function to destroy the data.
+ * @print: Pointer to a dedicated function to print the data.
+ *
+ * This function creates a node containing the data,
+ * and links the node after the last node of the list.
+ *
+ * Return:
+ * * true	- the operation is successful.
+ * * false	- the operation has failed.
+ */
+bool smw_utils_list_append_data(struct smw_utils_list *list, void *data,
+				void (*destructor)(void *),
+				void (*print)(void *));
+
+/**
+ * smw_utils_list_find_data() - Find data from the linked list.
+ * @list: Pointer to a linked list.
+ * @filter: Pointer to criteria to find a node in the list.
+ * @match: Pointer to a dedicated function that matches
+ *         the data contained in a node and the criteria pointed by filter.
+ *
+ * This function returns the pointer to the data
+ * contained in the node matching the criteria if any.
+ *
+ * Return:
+ * * pointer to the data if found
+ * * NULL
+ */
+void *smw_utils_list_find_data(struct smw_utils_list *list, void *filter,
+			       bool (*match)(void *, void *));
+
+/**
+ * smw_utils_list_print() - Print the linked list.
+ * @list: Pointer to a linked list.
+ *
+ * This function prints the linked list.
+ *
+ * Return:
+ * none.
+ */
+void smw_utils_list_print(struct smw_utils_list *list);

core/inc/utils.h
@@ -10,51 +10,12 @@
 #include <string.h>
 #include <limits.h>
 
-#include <smw_osal.h>
-#include <smw_debug.h>
-
-extern struct smw_ctx g_smw_ctx;
-
-#define SMW_UTILS_DBG_LEVEL_DEFAULT SMW_DBG_LEVEL_NONE
-
-/* SMW globals */
-/**
- * struct smw_ctx - SMW context
- * @ops: Structure containing the OSAL primitives
- * @start_count: Number of threads/applications that started the SMW library
- * @dbg_lvl: Current debug level
- *
- */
-struct smw_ctx {
-	struct smw_ops ops;
-	int start_count;
-	unsigned char dgb_lvl;
-};
-
-#define SMW_UTILS_PRINTF(...)                                                  \
-	do {                                                                   \
-		if (g_smw_ctx.ops.thread_self)                                 \
-			printf("(%lx) ", g_smw_ctx.ops.thread_self());         \
-		printf(__VA_ARGS__);                                           \
-	} while (0)
-
-#if defined(ENABLE_DEBUG)
-#define SMW_UTILS_TRACE_FUNCTION_CALL                                          \
-	do {                                                                   \
-		if (g_smw_ctx.dgb_lvl >= SMW_DBG_LEVEL_VERBOSE)                \
-			SMW_UTILS_PRINTF("Executing %s\n", __func__);          \
-	} while (0)
-
-#define SMW_UTILS_DBG_PRINTF(level, ...)                                       \
-	do {                                                                   \
-		if (g_smw_ctx.dgb_lvl >= SMW_DBG_LEVEL_##level)                \
-			SMW_UTILS_PRINTF(__VA_ARGS__);                         \
-	} while (0)
-
-#else
-#define SMW_UTILS_TRACE_FUNCTION_CALL
-#define SMW_UTILS_DBG_PRINTF(level, ...)
-#endif /* ENABLE_DEBUG */
+#define SMW_UTILS_MALLOC  malloc
+#define SMW_UTILS_FREE	  free
+#define SMW_UTILS_MEMCPY  memcpy
+#define SMW_UTILS_STRLEN  strlen
+#define SMW_UTILS_STRCMP  strcmp
+#define SMW_UTILS_STRNCMP strncmp
 
 #define SMW_UTILS_CRITICAL_SECTION_START                                       \
 	do {                                                                   \
@@ -67,3 +28,59 @@ struct smw_ctx {
 		if (g_smw_ctx.ops.critical_section_stop)                       \
 			g_smw_ctx.ops.critical_section_stop();                 \
 	} while (0)
+
+static inline int smw_utils_mutex_init(void **mutex)
+{
+	int err = 0;
+
+	SMW_DBG_ASSERT(mutex);
+	if (g_smw_ctx.ops.mutex_init)
+		err = g_smw_ctx.ops.mutex_init(mutex);
+
+	return err;
+}
+
+static inline int smw_utils_mutex_destroy(void **mutex)
+{
+	int err = 0;
+
+	SMW_DBG_ASSERT(mutex);
+	if (g_smw_ctx.ops.mutex_destroy)
+		err = g_smw_ctx.ops.mutex_destroy(mutex);
+
+	return err;
+}
+
+static inline void smw_utils_mutex_lock(void *mutex)
+{
+	if (g_smw_ctx.ops.mutex_lock)
+		SMW_DBG_ASSERT(!g_smw_ctx.ops.mutex_lock(mutex));
+}
+
+static inline void smw_utils_mutex_unlock(void *mutex)
+{
+	if (g_smw_ctx.ops.mutex_unlock)
+		SMW_DBG_ASSERT(!g_smw_ctx.ops.mutex_unlock(mutex));
+}
+
+static inline int smw_utils_thread_create(unsigned long *thread,
+					  void *(*start_routine)(void *arg),
+					  void *arg)
+{
+	int err = 0;
+
+	if (g_smw_ctx.ops.thread_create)
+		err = g_smw_ctx.ops.thread_create(thread, start_routine, arg);
+
+	return err;
+}
+
+static inline int smw_utils_thread_cancel(unsigned long thread)
+{
+	int err = 0;
+
+	if (g_smw_ctx.ops.thread_cancel)
+		err = g_smw_ctx.ops.thread_cancel(thread);
+
+	return err;
+}

core/utils/CMakeLists.txt
@@ -1,0 +1,8 @@
+set(OBJ_LIST ${OBJ_LIST} $<TARGET_OBJECTS:utils> PARENT_SCOPE)
+
+add_library(utils OBJECT "" ${INCLUDE_DIR})
+target_sources(utils PRIVATE list.c common.h)
+if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND VERBOSE AND NOT VERBOSE EQUAL 0)
+target_sources(utils PRIVATE list_debug.c)
+endif()
+target_include_directories(utils PRIVATE ${INCLUDE_DIR})

core/utils/common.h
@@ -1,0 +1,21 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2020 NXP
+ */
+
+/**
+ * struct node - Linked list node
+ * @prev: Previous node
+ * @next: Next node
+ * @data: Pointer to the data contained in the node
+ * @destructor: Pointer to a dedicated function to destroy the data
+ * @printer: Pointer to a dedicated function to print the data
+ *
+ */
+struct node {
+	struct node *prev;
+	struct node *next;
+	void *data;
+	void (*destructor)(void *params);
+	void (*printer)(void *params);
+};

core/utils/list.c
@@ -1,0 +1,122 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2020 NXP
+ */
+
+#include "smw_osal.h"
+#include "global.h"
+#include "debug.h"
+#include "utils.h"
+#include "list.h"
+
+#include "common.h"
+
+static struct node *create_node(void *data, void (*destructor)(void *),
+				void (*printer)(void *))
+{
+	struct node *node = SMW_UTILS_MALLOC(sizeof(struct node));
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (node) {
+		node->prev = NULL;
+		node->next = NULL;
+
+		node->destructor = destructor;
+		node->printer = printer;
+		node->data = data;
+	}
+
+	return node;
+}
+
+static void destroy_node(struct node *node)
+{
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (node->destructor)
+		node->destructor(node->data);
+
+	SMW_UTILS_FREE(node);
+}
+
+void smw_utils_list_init(struct smw_utils_list *list)
+{
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	list->first = NULL;
+	list->last = NULL;
+}
+
+void smw_utils_list_destroy(struct smw_utils_list *list)
+{
+	struct node *next, *node = list->first;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	while (node) {
+		next = node->next;
+		destroy_node(node);
+		node = next;
+	}
+
+	smw_utils_list_init(list);
+}
+
+static struct node *find_node(struct smw_utils_list *list, void *filter,
+			      bool (*match)(void *, void *))
+{
+	struct node *node = list->first;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	while (node) {
+		if (match(node->data, filter))
+			return node;
+
+		node = node->next;
+	}
+
+	return NULL;
+}
+
+bool smw_utils_list_append_data(struct smw_utils_list *list, void *data,
+				void (*destructor)(void *),
+				void (*printer)(void *))
+{
+	struct node *node = create_node(data, destructor, printer);
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!node)
+		return false;
+
+	if (list->last) {
+		list->last->next = node;
+		node->prev = list->last;
+		list->last = node;
+	} else {
+		SMW_DBG_ASSERT(!list->first);
+		list->first = node;
+		list->last = node;
+	}
+
+	return true;
+}
+
+void *smw_utils_list_find_data(struct smw_utils_list *list, void *filter,
+			       bool (*match)(void *, void *))
+{
+	struct node *node = find_node(list, filter, match);
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (node)
+		return node->data;
+
+	return NULL;
+}
+
+__attribute__((weak)) void smw_utils_list_print(struct smw_utils_list *list)
+{
+}

core/utils/list_debug.c
@@ -1,0 +1,43 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2020 NXP
+ */
+
+#include "smw_osal.h"
+#include "global.h"
+#include "debug.h"
+#include "utils.h"
+#include "list.h"
+
+#include "common.h"
+
+static void print_node(struct node *node)
+{
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	SMW_DBG_PRINTF(DEBUG, "Data: %p\n", node->data);
+	if (node->printer)
+		node->printer(node->data);
+}
+
+void smw_utils_list_print(struct smw_utils_list *list)
+{
+	struct node *node = list->first;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (SMW_DBG_LEVEL < SMW_DBG_LEVEL_DEBUG)
+		return;
+
+	SMW_DBG_PRINTF(DEBUG,
+		       "Print list: %p,\n"
+		       "    first : %p,\n"
+		       "    last  : %p\n",
+		       list, list->first, list->last);
+
+	while (node) {
+		print_node(node);
+
+		node = node->next;
+	}
+}

commit 5a417fd4685f81ec81420c8113019060f12b4842

SSMW-464 core: debug: Fix SMW_DBG_ASSERT exit
Fix the SMW_DBG_ASSERT() macro to exit properly in release and debug
compilation.
Removed no more used SMW_ABORT() macro.

Fixes: 44a4dc4745e3 ("SSMW-70 core:utils: implement Configuration module with support of HSM")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/inc/debug.h
@@ -19,12 +19,6 @@
 #define SMW_DBG_LEVEL_DEBUG   3 /* First level of debugging information */
 #define SMW_DBG_LEVEL_VERBOSE 4 /* Maximum level of debugging information */
 
-#if defined(ENABLE_DEBUG)
-#define SMW_ABORT abort
-#else /* ENABLE_DEBUG */
-#define SMW_ABORT(...)
-#endif /* ENABLE_DEBUG */
-
 #if defined(ENABLE_TRACE)
 
 #define SMW_DBG_LEVEL TRACE_LEVEL
@@ -103,7 +97,8 @@ static inline void dbg_hex_dump(const unsigned char *addr, unsigned int size,
 			   "line %d\n",                                        \
 			   #exp, __FILE__, __LINE__);                          \
 		SMW_FFLUSH(stdout);                                            \
-		SMW_ABORT();                                                   \
+		/* Exit in error properly flushing/closing streams */          \
+		exit(EXIT_FAILURE);                                            \
 	} while (0)
 
 #endif /* __DEBUG_H__ */
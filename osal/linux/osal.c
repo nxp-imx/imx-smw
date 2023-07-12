nxp-imx/imx-smw

commit b4be8b538b4ef900cd969308aeb1236998eb9b29

SSMW-454 osal: Fix coverity and coding rules
Fix the coverity findings and coding rules.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

osal/linux/osal.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2019-2022 NXP
+ * Copyright 2019-2023 NXP
  */
 
 #include "local.h"
@@ -118,10 +118,10 @@ static void vprint(const char *format, va_list arg)
 static int fill_dbg_buffer(char *out, size_t size, unsigned int *off,
 			   const char *fmt, size_t fmt_size, char c)
 {
-	int ret;
+	int ret = -1;
 
 	if (*off >= size)
-		return -1;
+		return ret;
 
 	if (fmt_size >= size)
 		printf(fmt, c);
@@ -143,10 +143,11 @@ static int fill_dbg_buffer(char *out, size_t size, unsigned int *off,
 static void hex_dump(const unsigned char *addr, unsigned int size,
 		     unsigned int align)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	/* Size of out must be at least equal to 3. */
 	char out[256] = { 0 };
 	unsigned int off = 0;
+	unsigned int align_mask = 0;
 
 	printf("(%d) [0x%lx] (%p-%u)\n", getpid(), pthread_self(), addr, size);
 
@@ -156,16 +157,19 @@ static void hex_dump(const unsigned char *addr, unsigned int size,
 	}
 
 	if (align > 4)
-		align = 4;
-	if (align)
-		align = 1 << align;
+		align_mask = BIT(4);
+	else if (align)
+		align_mask = BIT(align) & 0xF;
+
+	if (align_mask)
+		align_mask -= 1;
 
 	for (i = 0; i < size; i++) {
 		if (fill_dbg_buffer(out, sizeof(out), &off, "%.2x ", 3,
 				    addr[i]))
 			break;
 
-		if (!((i + 1) & (align - 1))) {
+		if (!((i + 1) & align_mask)) {
 			if (fill_dbg_buffer(out, sizeof(out), &off, "%c", 1,
 					    '\n'))
 				break;
@@ -319,7 +323,7 @@ static bool is_lib_initialized(void)
 static int get_default_config(char **buffer, unsigned int *size)
 {
 	int status = SMW_STATUS_NO_CONFIG_LOADED;
-	long fsize;
+	long fsize = 0;
 
 	FILE *f = NULL;
 	const char *file_name = getenv("SMW_CONFIG_FILE");
@@ -427,7 +431,7 @@ __export enum smw_status_code
 smw_osal_set_subsystem_info(smw_subsystem_t subsystem, void *info,
 			    size_t info_size)
 {
-	enum smw_status_code status;
+	enum smw_status_code status = SMW_STATUS_OK;
 
 	TRACE_FUNCTION_CALL;
 
@@ -456,7 +460,7 @@ smw_osal_set_subsystem_info(smw_subsystem_t subsystem, void *info,
 __export enum smw_status_code smw_osal_open_key_db(const char *file,
 						   size_t len __maybe_unused)
 {
-	enum smw_status_code status;
+	enum smw_status_code status = SMW_STATUS_OK;
 
 	TRACE_FUNCTION_CALL;
 
@@ -476,9 +480,9 @@ __export enum smw_status_code smw_osal_open_key_db(const char *file,
 
 __export enum smw_status_code smw_osal_lib_init(void)
 {
-	enum smw_status_code status;
+	enum smw_status_code status = SMW_STATUS_OK;
 
-	struct osal_ctx *ctx;
+	struct osal_ctx *ctx = NULL;
 	struct smw_ops ops = { 0 };
 	char *buffer = NULL;
 	unsigned int size = 0;

osal/linux/key_db.c
@@ -34,7 +34,7 @@ __weak void dbg_get_lock_file(int fp)
 
 static int lock_db(struct key_db_obj *db)
 {
-	int ret;
+	int ret = 0;
 	struct flock lock = { 0 };
 
 	ret = mutex_lock(db->mutex);
@@ -49,6 +49,7 @@ static int lock_db(struct key_db_obj *db)
 		ret = -1;
 	}
 
+	// coverity[missing_unlock]
 	return ret;
 }
 
@@ -76,15 +77,20 @@ static int unlock_db(struct key_db_obj *db)
 static void find_db_key_id(struct key_db_obj *db, unsigned int id,
 			   struct key_entry *key, long *pos)
 {
-	ssize_t off = 0;
+	off_t off = 0;
+	ssize_t nb_bytes = 0;
+	size_t inc = 0;
 
 	*pos = -1;
 
-	while (pread(db->fp, key, sizeof(*key), off) == sizeof(*key)) {
+	while ((nb_bytes = pread(db->fp, key, sizeof(*key), off)) > 0 &&
+	       nb_bytes == sizeof(*key)) {
 		DBG_PRINTF(DEBUG, "%s ID=%u vs %u\n", __func__, key->id, id);
 		if (key->id != id) {
 			/* Go to the next entry */
-			off += sizeof(*key) + key->info_size;
+			if (ADD_OVERFLOW(sizeof(*key), key->info_size, &inc) ||
+			    ADD_OVERFLOW(off, inc, &off))
+				break;
 			continue;
 		}
 
@@ -111,13 +117,16 @@ static void find_db_key_id(struct key_db_obj *db, unsigned int id,
 static void find_db_key_free(struct key_db_obj *db, struct osal_key *key,
 			     unsigned int *free_id, long *pos)
 {
-	ssize_t off = 0;
-	struct key_entry rd_key;
+	off_t off = 0;
+	off_t inc_off = 0;
+	ssize_t nb_bytes = 0;
+	struct key_entry rd_key = { 0 };
 	unsigned int last_id = 0;
 
 	*pos = -1;
 
-	while (pread(db->fp, &rd_key, sizeof(rd_key), off) == sizeof(rd_key)) {
+	while ((nb_bytes = pread(db->fp, &rd_key, sizeof(rd_key), off)) > 0 &&
+	       nb_bytes == sizeof(rd_key)) {
 		/*
 		 * If the key id read is the requested range
 		 * set the last_id value
@@ -128,7 +137,12 @@ static void find_db_key_free(struct key_db_obj *db, struct osal_key *key,
 		if (last_id != rd_key.id || rd_key.flags != ENTRY_FREE ||
 		    rd_key.info_size < key->info_size) {
 			/* Go to the next entry */
-			off += sizeof(rd_key) + rd_key.info_size;
+			if (ADD_OVERFLOW(rd_key.info_size, sizeof(rd_key),
+					 &inc_off))
+				return;
+			if (ADD_OVERFLOW(inc_off, off, &off))
+				return;
+
 			continue;
 		}
 
@@ -160,6 +174,7 @@ static int write_key_db(struct key_db_obj *db, struct key_entry *key,
 	int err = -1;
 	off_t off = pos;
 	struct stat f_stat = { 0 };
+	ssize_t nb_bytes = 0;
 
 	DBG_PRINTF(DEBUG, "%s (%d) pos = %ld\n", __func__, __LINE__, pos);
 
@@ -174,7 +189,8 @@ static int write_key_db(struct key_db_obj *db, struct key_entry *key,
 		off = f_stat.st_size;
 	}
 
-	if (pwrite(db->fp, key, sizeof(*key), off) != sizeof(*key)) {
+	nb_bytes = pwrite(db->fp, key, sizeof(*key), off);
+	if (nb_bytes < 0 || nb_bytes != (ssize_t)sizeof(*key)) {
 		DBG_PRINTF(ERROR, "%s (%d) DB write error\n", __func__,
 			   __LINE__);
 		goto end;
@@ -184,8 +200,8 @@ static int write_key_db(struct key_db_obj *db, struct key_entry *key,
 		dbg_entry_info(info, key->info_size);
 
 		off += sizeof(*key);
-		if (pwrite(db->fp, info, key->info_size, off) !=
-		    (ssize_t)key->info_size) {
+		nb_bytes = pwrite(db->fp, info, key->info_size, off);
+		if (nb_bytes < 0 || (size_t)nb_bytes != key->info_size) {
 			DBG_PRINTF(ERROR, "%s (%d) DB write error\n", __func__,
 				   __LINE__);
 			goto end;
@@ -217,11 +233,12 @@ static void close_db_file(struct key_db_obj *db)
 
 int key_db_open(const char *key_db)
 {
+	int ret = -1;
 	struct osal_ctx *ctx = get_osal_ctx();
-	struct key_db_obj *db;
+	struct key_db_obj *db = NULL;
 
 	if (!ctx)
-		return -1;
+		return ret;
 
 	db = ctx->key_db_obj;
 
@@ -232,14 +249,13 @@ int key_db_open(const char *key_db)
 		db = calloc(1, sizeof(*db));
 		if (!db) {
 			DBG_PRINTF(ERROR, "Key database allocation error\n");
-			return -1;
+			goto end;
 		}
 
 		ctx->key_db_obj = db;
-	}
-
-	if (db->fp)
+	} else if (db->fp) {
 		close_db_file(db);
+	}
 
 	/*
 	 * Open the application key database file.
@@ -250,20 +266,28 @@ int key_db_open(const char *key_db)
 	if (db->fp < 0) {
 		DBG_PRINTF(ERROR, "%s (%d): %s\n", __func__, __LINE__,
 			   get_strerr());
+		db->fp = 0;
+		goto end;
+	}
+
+	ret = mutex_init(&db->mutex);
+
+end:
+	if (ret && db) {
+		close_db_file(db);
+		(void)mutex_destroy(&db->mutex);
 
 		free(db);
 		ctx->key_db_obj = NULL;
-
-		return -1;
 	}
 
-	return mutex_init(&db->mutex);
+	return ret;
 }
 
 void key_db_close(void)
 {
 	struct osal_ctx *ctx = get_osal_ctx();
-	struct key_db_obj *db;
+	struct key_db_obj *db = NULL;
 
 	if (!ctx)
 		return;
@@ -283,9 +307,11 @@ void key_db_close(void)
 int key_db_get_info(struct osal_key *key)
 {
 	int ret = -1;
+	ssize_t nb_bytes = 0;
 	long pos = -1;
+	off_t offset = 0;
 	struct osal_ctx *ctx = get_osal_ctx();
-	struct key_db_obj *db;
+	struct key_db_obj *db = NULL;
 	struct key_entry entry = { 0 };
 
 	if (!ctx)
@@ -330,8 +356,11 @@ int key_db_get_info(struct osal_key *key)
 	}
 
 	/* Read the key information and exit */
-	if (pread(db->fp, key->info, entry.info_size, pos + sizeof(entry)) ==
-	    (ssize_t)entry.info_size) {
+	if (ADD_OVERFLOW(pos, sizeof(entry), &offset))
+		goto end;
+
+	nb_bytes = pread(db->fp, key->info, entry.info_size, offset);
+	if (nb_bytes > 0 || nb_bytes == (ssize_t)entry.info_size) {
 		dbg_entry_info(key->info, entry.info_size);
 		ret = 0;
 	} else {
@@ -348,7 +377,7 @@ end:
 int key_db_add(struct osal_key *key)
 {
 	struct osal_ctx *ctx = get_osal_ctx();
-	struct key_db_obj *db;
+	struct key_db_obj *db = NULL;
 	int ret = -1;
 	long pos = -1;
 	unsigned int free_id = 0;
@@ -409,7 +438,7 @@ int key_db_update(struct osal_key *key)
 	int ret = -1;
 	long pos = -1;
 	struct osal_ctx *ctx = get_osal_ctx();
-	struct key_db_obj *db;
+	struct key_db_obj *db = NULL;
 	struct key_entry entry = { 0 };
 
 	if (!ctx)
@@ -463,7 +492,7 @@ int key_db_delete(struct osal_key *key)
 	int ret = -1;
 	long pos = -1;
 	struct osal_ctx *ctx = get_osal_ctx();
-	struct key_db_obj *db;
+	struct key_db_obj *db = NULL;
 	struct key_entry entry = { 0 };
 
 	if (!ctx)

osal/linux/local.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2021-2022 NXP
+ * Copyright 2021-2023 NXP
  */
 
 #ifndef __LOCAL_H__
@@ -16,6 +16,7 @@
 
 #include "smw_osal.h"
 
+#include "builtin_macros.h"
 #include "compiler.h"
 #include "osal.h"
 
@@ -44,10 +45,6 @@
 #define DBG_PRINTF(level, ...)
 #define TRACE_FUNCTION_CALL
 #endif /* ENABLE_TRACE */
-
-#ifndef BIT
-#define BIT(bit) (1 << (bit))
-#endif /* BIT */
 
 /*
  * Define the configuration flags ids

osal/linux/osal_trace.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2022 NXP
+ * Copyright 2022-2023 NXP
  */
 
 #include <errno.h>
@@ -26,9 +26,10 @@ void dbg_entry(struct key_entry *entry __maybe_unused)
 
 void dbg_entry_info(void *buf, size_t len)
 {
-	size_t idx;
-	char out[256];
-	int off = 0;
+	size_t idx = 0;
+	char out[256] = { 0 };
+	unsigned long off = 0;
+	int nb_char = 0;
 
 	if (DBG_LEVEL_DEBUG > DBG_LEVEL)
 		return;
@@ -36,12 +37,17 @@ void dbg_entry_info(void *buf, size_t len)
 	printf("Entry Info: (%p-%zu)\n", buf, len);
 
 	for (idx = 0; idx < len; idx++) {
-		if (((idx % 16) == 0) && idx > 0) {
+		if ((!(idx % 16) && idx > 0) || off == (sizeof(out) - 1)) {
 			printf("%s\n", out);
 			off = 0;
 		}
-		off += snprintf(out + off, (sizeof(out) - off), "%02X ",
-				((char *)buf)[idx]);
+
+		nb_char = snprintf(out + off, (sizeof(out) - off), "%02X ",
+				   ((char *)buf)[idx]);
+		if (nb_char < 0)
+			break;
+
+		off += nb_char;
 	}
 
 	if (off > 0)

commit ecd2fce9a00338cadba3b2f1549b1815f24c784a

SSMW-467 osal: Fix buffer dump function
Fix the buffer dump function when alignment = 4.

Fixes: b4be8b538b4e ("SSMW-454 osal: Fix coverity and coding rules")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

osal/linux/osal.c
@@ -159,7 +159,7 @@ static void hex_dump(const unsigned char *addr, unsigned int size,
 	if (align > 4)
 		align_mask = BIT(4);
 	else if (align)
-		align_mask = BIT(align) & 0xF;
+		align_mask = BIT(align) & 0x1F;
 
 	if (align_mask)
 		align_mask -= 1;
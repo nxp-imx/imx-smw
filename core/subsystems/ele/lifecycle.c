nxp-imx/imx-smw

commit 3fd930ec00fdf07c833f6597cd1c007a690d8dc7

SSMW-348 subsystems: ele: Add support of the data storage operations
Add support of the data storage operations by ELE subsystem

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/subsystems/ele/lifecycle.c
@@ -12,11 +12,13 @@
 #define LIFECYCLE(_lifecycle)                                                  \
 	{                                                                      \
 		.str = LC_##_lifecycle##_STR,                                  \
+		.smw = SMW_LIFECYCLE_##_lifecycle,                             \
 		.ele = HSM_KEY_LIFECYCLE_##_lifecycle                          \
 	}
 
 static const struct lifecycle {
 	const char *str;
+	unsigned long smw;
 	hsm_key_lifecycle_t ele;
 } lifecycles[] = { LIFECYCLE(OPEN), LIFECYCLE(CLOSED),
 		   LIFECYCLE(CLOSED_LOCKED) };
@@ -35,7 +37,13 @@ static size_t lifecycle_to_string(unsigned char **str, size_t str_length,
 			SMW_DBG_PRINTF(DEBUG, "%s(%d) %s\n", __func__, __LINE__,
 				       lifecycles[i].str);
 
-			out_len = SMW_UTILS_STRLEN(lifecycles[i].str) + 1;
+			if (ADD_OVERFLOW(out_len,
+					 SMW_UTILS_STRLEN(lifecycles[i].str) +
+						 1,
+					 &out_len)) {
+				out_len = SIZE_MAX;
+				break;
+			}
 
 			if (*str) {
 				if (str_length >= out_len) {
@@ -45,6 +53,7 @@ static size_t lifecycle_to_string(unsigned char **str, size_t str_length,
 					*str += out_len;
 				} else {
 					out_len = SIZE_MAX;
+					break;
 				}
 			}
 		}
@@ -111,4 +120,16 @@ exit:
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
+}
+
+void ele_set_lifecycle_flags(unsigned long smw_flags, uint16_t *ele_flags)
+{
+	unsigned int i = 0;
+
+	*ele_flags = 0;
+
+	for (; i < ARRAY_SIZE(lifecycles); i++) {
+		if (smw_flags & lifecycles[i].smw)
+			*ele_flags |= lifecycles[i].ele;
+	}
 }

core/subsystems/ele/CMakeLists.txt
@@ -27,7 +27,8 @@ target_sources(ele PRIVATE
                $<$<BOOL:${ENABLE_SIGN_VERIFY}>:sign_verify.c>
                $<$<BOOL:${ENABLE_RNG}>:rng.c>
                $<$<BOOL:${ENABLE_CIPHER}>:cipher.c>
-               $<$<BOOL:${ENABLE_DEVMGR_MODULE}>:devmgr.c>)
+               $<$<BOOL:${ENABLE_DEVMGR_MODULE}>:devmgr.c>
+               $<$<BOOL:${ENABLE_STORAGE_MODULE}>:storage.c>)
 
 target_include_directories(ele PRIVATE
                            ${INCLUDE_DIR}

core/subsystems/ele/cipher.c
@@ -17,22 +17,22 @@
 	{                                                                      \
 		.key_type_id = SMW_CONFIG_KEY_TYPE_ID_##_key_type_id,          \
 		.cipher_mode_id = SMW_CONFIG_CIPHER_MODE_ID_##_cipher_mode_id, \
-		.hsm_algo = HSM_CIPHER_ONE_GO_ALGO_##_cipher_mode_id           \
+		.cipher_algo = HSM_CIPHER_ONE_GO_ALGO_##_cipher_mode_id        \
 	}
 
 static const struct {
 	enum smw_config_key_type_id key_type_id;
 	enum smw_config_cipher_mode_id cipher_mode_id;
-	hsm_op_cipher_one_go_algo_t hsm_algo;
+	hsm_op_cipher_one_go_algo_t cipher_algo;
 } cipher_algos[] = {
 	CIPHER_ALGO(AES, CBC),
 	CIPHER_ALGO(AES, ECB),
 	CIPHER_ALGO(AES, CTR),
 };
 
-static int set_cipher_algo(enum smw_config_key_type_id key_type_id,
-			   enum smw_config_cipher_mode_id cipher_mode_id,
-			   hsm_op_cipher_one_go_algo_t *hsm_algo)
+int ele_set_cipher_algo(enum smw_config_key_type_id key_type_id,
+			enum smw_config_cipher_mode_id cipher_mode_id,
+			hsm_op_cipher_one_go_algo_t *cipher_algo)
 {
 	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
 	unsigned int i = 0;
@@ -40,7 +40,7 @@ static int set_cipher_algo(enum smw_config_key_type_id key_type_id,
 	for (; i < ARRAY_SIZE(cipher_algos); i++) {
 		if (key_type_id == cipher_algos[i].key_type_id &&
 		    cipher_mode_id == cipher_algos[i].cipher_mode_id) {
-			*hsm_algo = cipher_algos[i].hsm_algo;
+			*cipher_algo = cipher_algos[i].cipher_algo;
 			status = SMW_STATUS_OK;
 			break;
 		}
@@ -100,8 +100,8 @@ static int cipher(struct hdl *hdl, void *args)
 	key_type_id = cipher_args->keys_desc[0]->identifier.type_id;
 
 	/* Get ELE algorithm */
-	status = set_cipher_algo(key_type_id, cipher_args->mode_id,
-				 &op_args.cipher_algo);
+	status = ele_set_cipher_algo(key_type_id, cipher_args->mode_id,
+				     &op_args.cipher_algo);
 	if (status != SMW_STATUS_OK)
 		goto end;
 

core/subsystems/ele/common.h
@@ -9,6 +9,7 @@
 #define PSA_COMPLIANT
 #include <hsm_api.h>
 
+#include "constants.h"
 #include "keymgr_derive.h"
 
 /**
@@ -269,5 +270,27 @@ int ele_export_public_key(struct hdl *hdl,
  */
 int ele_get_lifecycle(unsigned char **lifecycle, unsigned int *lifecycle_len,
 		      hsm_key_lifecycle_t ele_lifecycle);
+
+/**
+ * ele_set_lifecycle_flags() - Convert the SMW lifecycle flags to ELE flags
+ * @smw_flags: SMW lifecycle flags
+ * @ele_flags: ELE lifecycle flags
+ */
+
+void ele_set_lifecycle_flags(unsigned long smw_flags, uint16_t *ele_flags);
+
+/**
+ * ele_set_cipher_algo() - Set the ELE cipher algorithm
+ * @key_type_id: SMW Key type id
+ * @cipher_mode_id: SMW cipher mode ID
+ * @cipher_algo: ELE cipher algorithm ID
+ *
+ * Return:
+ * SMW_STATUS_OK                       - Success
+ * SMW_STATUS_OPERATION_NOT_SUPPORTED  - Cipher mode not supported
+ */
+int ele_set_cipher_algo(enum smw_config_key_type_id key_type_id,
+			enum smw_config_cipher_mode_id cipher_mode_id,
+			hsm_op_cipher_one_go_algo_t *cipher_algo);
 
 #endif /* __COMMON_H__ */

core/subsystems/ele/entry.c
@@ -231,6 +231,17 @@ __weak bool ele_device_handle(struct hdl *hdl, enum operation_id operation_id,
 	return false;
 }
 
+__weak bool ele_storage_handle(struct hdl *hdl, enum operation_id operation_id,
+			       void *args, int *status)
+{
+	(void)hdl;
+	(void)operation_id;
+	(void)args;
+	(void)status;
+
+	return false;
+}
+
 static int execute(enum operation_id operation_id, void *args)
 {
 	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
@@ -251,8 +262,10 @@ static int execute(enum operation_id operation_id, void *args)
 		goto end;
 	else if (ele_mac_handle(hdl, operation_id, args, &status))
 		goto end;
+	else if (ele_device_handle(hdl, operation_id, args, &status))
+		goto end;
 
-	ele_device_handle(hdl, operation_id, args, &status);
+	ele_storage_handle(hdl, operation_id, args, &status);
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
@@ -325,10 +338,17 @@ int ele_convert_err(hsm_err_t err)
 		status = SMW_STATUS_SIGNATURE_INVALID;
 		break;
 
+	case HSM_DATA_ALREADY_RETRIEVED:
+		status = SMW_STATUS_DATA_ALREADY_RETRIEVED;
+		break;
+
+	case HSM_INVALID_LIFECYCLE:
+		status = SMW_STATUS_INVALID_LIFECYCLE;
+		break;
+
 	default:
 		/*
 		 * status = SMW_STATUS_SUBSYSTEM_FAILURE
-		 * HSM_INVALID_LIFECYCLE
 		 * HSM_SELF_TEST_FAILURE
 		 * HSM_FATAL_FAILURE
 		 * HSM_GENERAL_ERROR

core/subsystems/ele/keymgr.c
@@ -653,12 +653,39 @@ exit:
 static int import_el2go_data(struct hdl *hdl,
 			     struct smw_keymgr_descriptor *key_desc)
 {
-	(void)hdl;
-	(void)key_desc;
+	int status = SMW_STATUS_OK;
 
-	SMW_DBG_PRINTF(ERROR, "EdgeLock 2GO data import not supported");
+	hsm_err_t err = HSM_NO_ERROR;
 
-	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+	op_data_storage_args_t op_args = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	op_args.data_id = key_desc->identifier.id;
+	op_args.data = smw_keymgr_get_private_data(key_desc);
+	op_args.data_size = smw_keymgr_get_private_length(key_desc);
+	op_args.flags = HSM_OP_DATA_STORAGE_FLAGS_STORE |
+			HSM_OP_DATA_STORAGE_FLAGS_EL2GO;
+
+	SMW_DBG_PRINTF(VERBOSE,
+		       "[%s (%d)] Call hsm_data_ops()\n"
+		       "  op_data_storage_args_t\n"
+		       "    Data\n"
+		       "      - id: %d\n"
+		       "      - buffer: %p\n"
+		       "      - size: %d\n"
+		       "    flags: 0x%X\n",
+		       __func__, __LINE__, op_args.data_id, op_args.data,
+		       op_args.data_size, op_args.flags);
+
+	err = hsm_data_ops(hdl->key_store, &op_args);
+
+	SMW_DBG_PRINTF(DEBUG, "hsm_data_ops returned %d\n", err);
+
+	status = ele_convert_err(err);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
 }
 
 static int import_key(struct hdl *hdl, void *args)

core/subsystems/ele/storage.c
@@ -1,0 +1,233 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ */
+
+#include "debug.h"
+#include "cipher.h"
+#include "utils.h"
+#include "storage.h"
+
+#include "common.h"
+
+static int get_mac_algo(struct smw_storage_sign_args *args,
+			unsigned int *ele_id)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	switch (args->algo_id) {
+	case SMW_CONFIG_MAC_ALGO_ID_CMAC:
+		*ele_id = PERMITTED_ALGO_CMAC;
+		status = SMW_STATUS_OK;
+		break;
+
+	default:
+		break;
+	}
+
+	return status;
+}
+
+static int store_data_raw(struct hdl *hdl,
+			  struct smw_storage_store_data_args *args)
+{
+	int status = SMW_STATUS_OK;
+
+	hsm_err_t err = HSM_NO_ERROR;
+
+	op_data_storage_args_t op_args = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	op_args.data_id =
+		smw_storage_get_data_identifier(&args->data_descriptor);
+	op_args.data = smw_storage_get_data(&args->data_descriptor);
+	op_args.data_size = smw_storage_get_data_length(&args->data_descriptor);
+	op_args.flags = HSM_OP_DATA_STORAGE_FLAGS_STORE;
+
+	SMW_DBG_PRINTF(VERBOSE,
+		       "[%s (%d)] Call hsm_data_ops()\n"
+		       "  op_data_storage_args_t\n"
+		       "    Data\n"
+		       "      - id: %d\n"
+		       "      - buffer: %p\n"
+		       "      - size: %d\n"
+		       "    flags: 0x%X\n",
+		       __func__, __LINE__, op_args.data_id, op_args.data,
+		       op_args.data_size, op_args.flags);
+
+	err = hsm_data_ops(hdl->key_store, &op_args);
+
+	SMW_DBG_PRINTF(DEBUG, "hsm_data_ops returned %d\n", err);
+
+	status = ele_convert_err(err);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int store_data_encrypted(struct hdl *hdl,
+				struct smw_storage_store_data_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	hsm_err_t err = HSM_NO_ERROR;
+
+	op_enc_data_storage_args_t op_args = { 0 };
+	struct smw_storage_data_descriptor *data_descriptor =
+		&args->data_descriptor;
+	struct smw_storage_enc_args *enc_args = &args->enc_args;
+	struct smw_storage_sign_args *sign_args = &args->sign_args;
+	struct smw_keymgr_identifier *key_identifier =
+		&enc_args->keys_desc[0]->identifier;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	op_args.data_id = smw_storage_get_data_identifier(data_descriptor);
+	op_args.data = smw_storage_get_data(data_descriptor);
+	op_args.data_size = smw_storage_get_data_length(data_descriptor);
+	op_args.iv = smw_storage_get_iv(enc_args);
+
+	if (SET_OVERFLOW(smw_storage_get_iv_length(enc_args), op_args.iv_size))
+		goto end;
+
+	if (smw_crypto_cipher_iv_required(args->enc_args.mode_id) &&
+	    !op_args.iv && !op_args.iv_size)
+		op_args.flags |= HSM_OP_ENC_DATA_STORAGE_FLAGS_RANDOM_IV;
+
+	if (data_descriptor->attributes.rw_flags & SMW_STORAGE_READ_ONCE)
+		op_args.flags |= HSM_OP_ENC_DATA_STORAGE_FLAGS_READ_ONCE;
+
+	op_args.enc_key_id = key_identifier->id;
+
+	status = ele_set_cipher_algo(key_identifier->type_id, enc_args->mode_id,
+				     &op_args.enc_algo);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	key_identifier = &sign_args->key_descriptor.identifier;
+	op_args.sign_key_id = key_identifier->id;
+
+	status = get_mac_algo(sign_args, &op_args.sign_algo);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	ele_set_lifecycle_flags(data_descriptor->attributes.lifecycle_flags,
+				&op_args.lifecycle);
+
+	SMW_DBG_PRINTF(VERBOSE,
+		       "[%s (%d)] Call hsm_enc_data_ops()\n"
+		       "  op_enc_data_storage_args_t\n"
+		       "    Data\n"
+		       "      - id: 0x%08X\n"
+		       "      - buffer: %p\n"
+		       "      - size: %d\n"
+		       "    IV\n"
+		       "      - buffer: %p\n"
+		       "      - size: %d\n"
+		       "    Encryption\n"
+		       "      - Key ID: 0x%08X\n"
+		       "      - Algorithm: 0x%08X\n"
+		       "    Signature\n"
+		       "      - Key ID: 0x%08X\n"
+		       "      - Algorithm: 0x%08X\n"
+		       "    flags: 0x%X\n"
+		       "    lifecyle: 0x%04X\n",
+		       __func__, __LINE__, op_args.data_id, op_args.data,
+		       op_args.data_size, op_args.iv, op_args.iv_size,
+		       op_args.enc_key_id, op_args.enc_algo,
+		       op_args.sign_key_id, op_args.sign_algo, op_args.flags,
+		       op_args.lifecycle);
+
+	err = hsm_enc_data_ops(hdl->key_store, &op_args);
+
+	SMW_DBG_PRINTF(DEBUG, "hsm_enc_data_ops returned %d\n", err);
+
+	status = ele_convert_err(err);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int storage_store(struct hdl *hdl, void *args)
+{
+	int status = SMW_STATUS_OK;
+
+	struct smw_storage_store_data_args *store_args = args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (store_args->enc_args.mode_id == SMW_CONFIG_CIPHER_MODE_ID_INVALID &&
+	    store_args->sign_args.algo_id == SMW_CONFIG_MAC_ALGO_ID_INVALID) {
+		status = store_data_raw(hdl, args);
+	} else {
+		status = store_data_encrypted(hdl, args);
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int storage_retrieve(struct hdl *hdl,
+			    struct smw_storage_retrieve_data_args *args)
+{
+	int status = SMW_STATUS_OK;
+
+	hsm_err_t err = HSM_NO_ERROR;
+
+	op_data_storage_args_t op_args = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	op_args.data_id =
+		smw_storage_get_data_identifier(&args->data_descriptor);
+	op_args.data = smw_storage_get_data(&args->data_descriptor);
+	op_args.data_size = smw_storage_get_data_length(&args->data_descriptor);
+	op_args.flags = HSM_OP_DATA_STORAGE_FLAGS_RETRIEVE;
+
+	SMW_DBG_PRINTF(VERBOSE,
+		       "[%s (%d)] Call hsm_data_ops()\n"
+		       "  op_data_storage_args_t\n"
+		       "    Data\n"
+		       "      - id: %d\n"
+		       "      - buffer: %p\n"
+		       "      - size: %d\n"
+		       "    flags: 0x%X\n"
+		       "    svc_flags: 0x%X\n",
+		       __func__, __LINE__, op_args.data_id, op_args.data,
+		       op_args.data_size, op_args.flags, op_args.svc_flags);
+
+	err = hsm_data_ops(hdl->key_store, &op_args);
+	SMW_DBG_PRINTF(DEBUG, "hsm_data_ops returned %d\n", err);
+
+	status = ele_convert_err(err);
+
+	smw_storage_set_data_length(&args->data_descriptor,
+				    op_args.exp_output_size);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+bool ele_storage_handle(struct hdl *hdl, enum operation_id operation_id,
+			void *args, int *status)
+{
+	SMW_DBG_ASSERT(args);
+
+	switch (operation_id) {
+	case OPERATION_ID_STORAGE_STORE:
+		*status = storage_store(hdl, args);
+		break;
+
+	case OPERATION_ID_STORAGE_RETRIEVE:
+		*status = storage_retrieve(hdl, args);
+		break;
+
+	case OPERATION_ID_STORAGE_DELETE:
+	default:
+		return false;
+	}
+
+	return true;
+}

commit 92f2172dcec424b7e1bb4c6ac8299ba1a34a14b4

SSMW-476 subsystems: ele: Fix lifecycle string overflow
Fix the lifecycle string length overflow.

Fixes: 3fd930ec00fd ("SSMW-348 subsystems: ele: Add support of the data storage operations")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/lifecycle.c
@@ -37,9 +37,7 @@ static size_t lifecycle_to_string(unsigned char **str, size_t str_length,
 			SMW_DBG_PRINTF(DEBUG, "%s(%d) %s\n", __func__, __LINE__,
 				       lifecycles[i].str);
 
-			if (ADD_OVERFLOW(out_len,
-					 SMW_UTILS_STRLEN(lifecycles[i].str) +
-						 1,
+			if (ADD_OVERFLOW(SMW_UTILS_STRLEN(lifecycles[i].str), 1,
 					 &out_len)) {
 				out_len = SIZE_MAX;
 				break;
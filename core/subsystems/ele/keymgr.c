nxp-imx/imx-smw

commit 8f4b86669897e97e0945375e603c74cf5b2ff523

SSMW-399 subsystem: ele: Force flag strict operation
Force flag strict operation in case of persistent or permanent key.

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/subsystems/ele/keymgr.c
@@ -486,6 +486,12 @@ static int generate_key(struct hdl *hdl, void *args)
 		op_args.key_group = TRANSIENT_KEY_GROUP;
 	}
 
+	if (key_args->key_attributes.persistence ==
+		    SMW_KEYMGR_PERSISTENCE_ID_PERSISTENT ||
+	    key_args->key_attributes.persistence ==
+		    SMW_KEYMGR_PERSISTENCE_ID_PERMANENT)
+		key_args->key_attributes.flush_key = true;
+
 	if (key_args->key_attributes.flush_key)
 		op_args.flags |= HSM_OP_KEY_GENERATION_FLAGS_STRICT_OPERATION;
 

commit 12f14a223d9081d9bb62950c93f19e77956c8ec4

SSMW-463 subsystems: ele: Missing strict operation flag.
The strict operation flag must be set when a persistent/permanent key is
deleted.

Fixes: 8f4b86669897 ("SSMW-399 subsystem: ele: Force flag strict operation")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/keymgr.c
@@ -275,7 +275,8 @@ static int close_key_mgt_service(hsm_hdl_t key_management_hdl)
 	return ele_convert_err(err);
 }
 
-static int delete_key_operation(hsm_hdl_t key_mgt_hdl, unsigned int key_id)
+static int delete_key_operation(hsm_hdl_t key_mgt_hdl,
+				struct smw_keymgr_identifier *key_identifier)
 {
 	int status = SMW_STATUS_OK;
 
@@ -284,7 +285,12 @@ static int delete_key_operation(hsm_hdl_t key_mgt_hdl, unsigned int key_id)
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	op_args.key_identifier = key_id;
+	op_args.key_identifier = key_identifier->id;
+	if (key_identifier->persistence_id ==
+		    SMW_KEYMGR_PERSISTENCE_ID_PERSISTENT ||
+	    key_identifier->persistence_id ==
+		    SMW_KEYMGR_PERSISTENCE_ID_PERMANENT)
+		op_args.flags = HSM_OP_DEL_KEY_FLAGS_STRICT_OPERATION;
 
 	SMW_DBG_PRINTF(VERBOSE,
 		       "[%s (%d)] Call hsm_delete_key()\n"
@@ -579,7 +585,7 @@ static int generate_key(struct hdl *hdl, void *args)
 			 * Delete the key in subsystem as smw_generate_key()
 			 * is going to remove it from the key database
 			 */
-			(void)delete_key_operation(key_mgt_hdl, key_id);
+			(void)delete_key_operation(key_mgt_hdl, key_identifier);
 		}
 	}
 
@@ -760,7 +766,7 @@ static int delete_key(struct hdl *hdl, void *args)
 	status = open_key_mgmt_service(hdl, &key_mgt_hdl);
 	if (status == SMW_STATUS_OK) {
 		status = delete_key_operation(key_mgt_hdl,
-					      key_desc->identifier.id);
+					      &key_desc->identifier);
 
 		tmp_status = close_key_mgt_service(key_mgt_hdl);
 		if (status == SMW_STATUS_OK)
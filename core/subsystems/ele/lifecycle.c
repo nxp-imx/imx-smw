nxp-imx/imx-smw

commit 92f2172dcec424b7e1bb4c6ac8299ba1a34a14b4

SSMW-476 subsystems: ele: Fix lifecycle string overflow
Fix the lifecycle string length overflow.

Fixes: 3fd930ec00fd ("SSMW-348 subsystems: ele: Add support of the data storage operations")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/lifecycle.c
@@ -37,9 +37,7 @@ static size_t lifecycle_to_string(unsigned char **str, size_t str_length,
 			SMW_DBG_PRINTF(DEBUG, "%s(%d) %s\n", __func__, __LINE__,
 				       lifecycles[i].str);
 
-			if (ADD_OVERFLOW(out_len,
-					 SMW_UTILS_STRLEN(lifecycles[i].str) +
-						 1,
+			if (ADD_OVERFLOW(SMW_UTILS_STRLEN(lifecycles[i].str), 1,
 					 &out_len)) {
 				out_len = SIZE_MAX;
 				break;

commit 99268985c2391d0e136aa263d76552959695c79d

SSMW-481 subsystems: ele: Fix lifecycle string calculation
Fix the lifecycle string calculation in case multiple lifecycles are
supported.

Fixes: 92f2172dcec4 ("SSMW-476 subsystems: ele: Fix lifecycle string overflow")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/lifecycle.c
@@ -37,8 +37,10 @@ static size_t lifecycle_to_string(unsigned char **str, size_t str_length,
 			SMW_DBG_PRINTF(DEBUG, "%s(%d) %s\n", __func__, __LINE__,
 				       lifecycles[i].str);
 
-			if (ADD_OVERFLOW(SMW_UTILS_STRLEN(lifecycles[i].str), 1,
-					 &out_len)) {
+			if (ADD_OVERFLOW(out_len,
+					 SMW_UTILS_STRLEN(lifecycles[i].str),
+					 &out_len) ||
+			    INC_OVERFLOW(out_len, 1)) {
 				out_len = SIZE_MAX;
 				break;
 			}
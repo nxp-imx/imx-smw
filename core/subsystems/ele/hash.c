42ee4af071d6502b92de7b96d5027ebc651ac411
Cedric Neveux <cedric.neveux@nxp.com>
2023-09-08 07:17:35 +0000

    SSMW-466 subsystems: ele: Allow disabling HASH
    Fix the ELE subsystem to handle option ENABLE_HASH=OFF
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- core/subsystems/ele/hash.c
+++ core/subsystems/ele/hash.c
@@ -9,21 +9,9 @@
 #include "debug.h"
 #include "utils.h"
 #include "subsystems.h"
-#include "config.h"
 #include "hash.h"
 
 #include "common.h"
-
-#define HASH_ALGO(_id, _hsm_id, _length)                                       \
-	{                                                                      \
-		.algo_id = SMW_CONFIG_HASH_ALGO_ID_##_id,                      \
-		.ele_algo = HSM_HASH_ALGO_##_hsm_id, .length = _length         \
-	}
-
-static const struct ele_hash_algo hash_algos[] = {
-	HASH_ALGO(SHA224, SHA_224, 28), HASH_ALGO(SHA256, SHA_256, 32),
-	HASH_ALGO(SHA384, SHA_384, 48), HASH_ALGO(SHA512, SHA_512, 64)
-};
 
 static int hash(struct hdl *hdl, void *args)
 {
@@ -81,24 +69,6 @@ static int hash(struct hdl *hdl, void *args)
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
-}
-
-const struct ele_hash_algo *
-ele_get_hash_algo(enum smw_config_hash_algo_id algo_id)
-{
-	const struct ele_hash_algo *hash_algo = NULL;
-	unsigned int i;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	for (i = 0; i < ARRAY_SIZE(hash_algos); i++) {
-		if (hash_algos[i].algo_id == algo_id) {
-			hash_algo = &hash_algos[i];
-			break;
-		}
-	}
-
-	return hash_algo;
 }
 
 bool ele_hash_handle(struct hdl *hdl, enum operation_id operation_id,

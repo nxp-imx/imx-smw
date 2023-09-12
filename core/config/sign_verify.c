93ad048ca20c0ee2d71504035015523bcfc3b41b
Cedric Neveux <cedric.neveux@nxp.com>
2023-09-12 09:44:08 +0000

    SSMW-466 core: Allow disabling SIGN_VERIFY
    Fix the core code to handle the option ENABLE_SIGN_VERIFY=OFF
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- core/config/sign_verify.c
+++ core/config/sign_verify.c
@@ -4,6 +4,7 @@
  */
 
 #include "smw_status.h"
+#include "smw_config.h"
 #include "smw_crypto.h"
 
 #include "compiler.h"
@@ -191,6 +192,64 @@ static int verify_check_subsystem_caps(void *args, void *params)
 	return check_subsystem_caps(args, params);
 }
 
+static int check_sign_verify_common(smw_subsystem_t subsystem,
+				    struct smw_signature_info *info,
+				    enum operation_id op_id)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+	enum subsystem_id id = SUBSYSTEM_ID_INVALID;
+	enum smw_config_key_type_id key_type_id =
+		SMW_CONFIG_KEY_TYPE_ID_INVALID;
+	enum smw_config_hash_algo_id algo_id = SMW_CONFIG_HASH_ALGO_ID_INVALID;
+	enum smw_config_sign_type_id sign_type_id =
+		SMW_CONFIG_SIGN_TYPE_ID_INVALID;
+	struct sign_verify_params params = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!info || !info->key_type_name)
+		return status;
+
+	status = smw_config_get_subsystem_id(subsystem, &id);
+	if (status != SMW_STATUS_OK)
+		return status;
+
+	status = smw_config_get_key_type_id(info->key_type_name, &key_type_id);
+	if (status != SMW_STATUS_OK)
+		return status;
+
+	status = get_operation_params(op_id, id, &params);
+	if (status != SMW_STATUS_OK)
+		return status;
+
+	/* Check key type */
+	if (!check_id(key_type_id, params.key.type_bitmap))
+		return SMW_STATUS_OPERATION_NOT_CONFIGURED;
+
+	/* Check hash algorithm if set */
+	if (info->hash_algo) {
+		status = smw_utils_get_hash_algo_id(info->hash_algo, &algo_id);
+		if (status != SMW_STATUS_OK)
+			return status;
+
+		if (!check_id(algo_id, params.algo_bitmap))
+			return SMW_STATUS_OPERATION_NOT_CONFIGURED;
+	}
+
+	/* Check signature type if set */
+	if (info->signature_type) {
+		status = smw_config_get_signature_type_id(info->signature_type,
+							  &sign_type_id);
+		if (status != SMW_STATUS_OK)
+			return status;
+
+		if (!check_id(sign_type_id, params.sign_type_bitmap))
+			return SMW_STATUS_OPERATION_NOT_CONFIGURED;
+	}
+
+	return SMW_STATUS_OK;
+}
+
 DEFINE_CONFIG_OPERATION_FUNC(sign);
 DEFINE_CONFIG_OPERATION_FUNC(verify);
 
@@ -229,4 +288,18 @@ int smw_config_get_tls_label_id(const char *name,
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
+}
+
+__export enum smw_status_code
+smw_config_check_sign(smw_subsystem_t subsystem,
+		      struct smw_signature_info *info)
+{
+	return check_sign_verify_common(subsystem, info, OPERATION_ID_SIGN);
+}
+
+__export enum smw_status_code
+smw_config_check_verify(smw_subsystem_t subsystem,
+			struct smw_signature_info *info)
+{
+	return check_sign_verify_common(subsystem, info, OPERATION_ID_VERIFY);
 }

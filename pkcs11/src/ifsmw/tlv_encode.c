nxp-imx/imx-smw

commit 9a79b14aae2d8fb0c5f4d7bff2fad723a2ecf5db

SSMW-455 pkcs11: Fix coverity and coding rules
Fix the coverity findings and coding rules.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/ifsmw/tlv_encode.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021-2022 NXP
+ * Copyright 2021-2023 NXP
  */
 
 #include <stdlib.h>
@@ -12,18 +12,38 @@
 
 #define TLV_LENGTH_SIZE 2
 
+static size_t tlv_string_size(const char *type, size_t value_length)
+{
+	size_t size = 0;
+
+	size = strlen(type);
+	if (!INC_OVERFLOW(size, 1) && !INC_OVERFLOW(size, TLV_LENGTH_SIZE) &&
+	    !INC_OVERFLOW(size, value_length))
+		return size;
+
+	return 0;
+}
+
 static CK_RV tlv_realloc(struct smw_tlv *tlv, size_t length)
 {
-	size_t add_length;
+	size_t add_length = 0;
 
-	add_length = tlv->length_max - tlv->length + length;
+	if (SUB_OVERFLOW(tlv->length_max, tlv->length, &add_length))
+		return CKR_ARGUMENTS_BAD;
+
+	if (INC_OVERFLOW(add_length, length))
+		return CKR_ARGUMENTS_BAD;
 
 	if (!tlv->string) {
 		tlv->string = malloc(length);
 		if (!tlv->string)
 			return CKR_HOST_MEMORY;
 
-		tlv->length_max = length;
+		if (SET_OVERFLOW(length, tlv->length_max)) {
+			free(tlv->string);
+			tlv->string = NULL;
+			return CKR_ARGUMENTS_BAD;
+		}
 	} else if (add_length) {
 		tlv->string =
 			realloc(tlv->string, tlv->length_max + add_length);
@@ -36,33 +56,61 @@ static CK_RV tlv_realloc(struct smw_tlv *tlv, size_t length)
 	return CKR_OK;
 }
 
-static void set_tlv_type(struct smw_tlv *tlv, const char *type)
+static CK_RV set_tlv_type(struct smw_tlv *tlv, const char *type)
 {
-	/* Add the Type @type to current tlv string */
-	memcpy(&tlv->string[tlv->length], type, strlen(type));
-	tlv->length += strlen(type);
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	size_t length = 0;
+	unsigned int index = tlv->length;
 
-	/* Set the Type null terminated character */
-	tlv->string[tlv->length++] = 0;
+	length = strlen(type) + 1;
+
+	if (!INC_OVERFLOW(tlv->length, length)) {
+		/* Add the Type @type to current tlv string */
+		memcpy(&tlv->string[index], type, length);
+
+		ret = CKR_OK;
+	}
+
+	return ret;
 }
 
-static void set_tlv_length(struct smw_tlv *tlv, size_t length)
+static CK_RV set_tlv_length(struct smw_tlv *tlv, size_t length)
 {
-	tlv->string[tlv->length++] = GET_BYTE(length, 1);
-	tlv->string[tlv->length++] = GET_BYTE(length, 0);
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	uint8_t byte = 0;
+	char *tlv_string = &tlv->string[tlv->length];
+
+	if (!INC_OVERFLOW(tlv->length, 2)) {
+		byte = GET_BYTE(length, 1);
+		(void)SET_OVERFLOW(byte, *tlv_string);
+
+		byte = GET_BYTE(length, 0);
+		(void)SET_OVERFLOW(byte, *(++tlv_string));
+
+		ret = CKR_OK;
+	}
+
+	return ret;
 }
 
-static void set_tlv_value(struct smw_tlv *tlv, const void *value,
-			  size_t value_len)
+static CK_RV set_tlv_value(struct smw_tlv *tlv, const void *value,
+			   size_t value_len)
 {
-	memcpy(&tlv->string[tlv->length], value, value_len);
-	tlv->length += value_len;
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	unsigned int index = tlv->length;
+
+	if (!INC_OVERFLOW(tlv->length, value_len)) {
+		memcpy(&tlv->string[index], value, value_len);
+		ret = CKR_OK;
+	}
+
+	return ret;
 }
 
 CK_RV tlv_encode_boolean(struct smw_tlv *tlv, const char *type)
 {
-	CK_RV ret;
-	size_t len_add;
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	size_t len_add = 0;
 
 	/*
 	 * Boolean is TLV encoded with:
@@ -70,24 +118,27 @@ CK_RV tlv_encode_boolean(struct smw_tlv *tlv, const char *type)
 	 *   - Length = 2 bytes of 0
 	 *   - Value is not set
 	 */
-	len_add = strlen(type) + 1 + TLV_LENGTH_SIZE;
+	len_add = tlv_string_size(type, 0);
+	if (!len_add)
+		return ret;
+
 	ret = tlv_realloc(tlv, len_add);
 	if (ret != CKR_OK)
 		return ret;
 
-	set_tlv_type(tlv, type);
+	ret = set_tlv_type(tlv, type);
+	if (ret == CKR_OK)
+		/* Set the 2 bytes of Length = 0 */
+		ret = set_tlv_length(tlv, 0);
 
-	/* Set the 2 bytes of Length = 0 */
-	set_tlv_length(tlv, 0);
-
-	return CKR_OK;
+	return ret;
 }
 
 CK_RV tlv_encode_large_numeral(struct smw_tlv *tlv, const char *type,
 			       struct libbignumber *bignum)
 {
-	CK_RV ret;
-	size_t len_add;
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	size_t len_add = 0;
 
 	/*
 	 * Large numeral is TLV encoded with:
@@ -95,28 +146,33 @@ CK_RV tlv_encode_large_numeral(struct smw_tlv *tlv, const char *type,
 	 *   - Length = 2 bytes (numeral byte length)
 	 *   - Value = numeral value
 	 */
-	len_add = strlen(type) + 1 + TLV_LENGTH_SIZE + bignum->length;
+	len_add = tlv_string_size(type, bignum->length);
+	if (!len_add)
+		return ret;
+
 	ret = tlv_realloc(tlv, len_add);
 	if (ret != CKR_OK)
 		return ret;
 
-	set_tlv_type(tlv, type);
+	ret = set_tlv_type(tlv, type);
+	if (ret != CKR_OK)
+		return ret;
 
 	/* Set the 2 bytes of Length */
-	set_tlv_length(tlv, bignum->length);
+	ret = set_tlv_length(tlv, bignum->length);
+	if (ret == CKR_OK)
+		/* Set value */
+		ret = set_tlv_value(tlv, bignum->value, bignum->length);
 
-	/* Set value */
-	set_tlv_value(tlv, bignum->value, bignum->length);
-
-	return CKR_OK;
+	return ret;
 }
 
 CK_RV tlv_encode_string(struct smw_tlv *tlv, const char *type,
 			const char *value)
 {
-	CK_RV ret;
-	size_t len_add;
-	size_t value_length = strlen(value) + 1;
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	size_t len_add = 0;
+	size_t value_length = 0;
 
 	/*
 	 * String is TLV encoded with:
@@ -124,22 +180,30 @@ CK_RV tlv_encode_string(struct smw_tlv *tlv, const char *type,
 	 *   - Length = 2 bytes (@value length, null character included)
 	 *   - Value = null terminated string @value
 	 */
+	value_length = strlen(value);
+	if (INC_OVERFLOW(value_length, 1))
+		return ret;
 
-	len_add = strlen(type) + 1 + TLV_LENGTH_SIZE + value_length;
+	len_add = tlv_string_size(type, value_length);
+	if (!len_add)
+		return ret;
+
 	ret = tlv_realloc(tlv, len_add);
 	if (ret != CKR_OK)
 		return ret;
 
 	/* Set type */
-	set_tlv_type(tlv, type);
+	ret = set_tlv_type(tlv, type);
+	if (ret != CKR_OK)
+		return ret;
 
 	/* Set length */
-	set_tlv_length(tlv, value_length);
+	ret = set_tlv_length(tlv, value_length);
+	if (ret == CKR_OK)
+		/* Set value */
+		ret = set_tlv_value(tlv, value, value_length);
 
-	/* Set value */
-	set_tlv_value(tlv, value, value_length);
-
-	return CKR_OK;
+	return ret;
 }
 
 CK_RV tlv_encode_enum(struct smw_tlv *tlv, const char *type, const char *value)
@@ -156,10 +220,11 @@ CK_RV tlv_encode_enum(struct smw_tlv *tlv, const char *type, const char *value)
 
 CK_RV tlv_encode_numeral(struct smw_tlv *tlv, const char *type, long long num)
 {
-	CK_RV ret;
-	size_t len_add;
-	int i;
-	unsigned int nb_bytes;
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	size_t len_add = 0;
+	int i = 0;
+	unsigned int nb_bytes = 0;
+	uint8_t byte = 0;
 
 	/* Get number of bytes needed to store @num */
 	if (num >> 32)
@@ -171,30 +236,44 @@ CK_RV tlv_encode_numeral(struct smw_tlv *tlv, const char *type, long long num)
 	else
 		nb_bytes = 1;
 
-	len_add = strlen(type) + 3 + nb_bytes;
+	len_add = tlv_string_size(type, nb_bytes);
+	if (!len_add)
+		return ret;
+
 	ret = tlv_realloc(tlv, len_add);
 	if (ret != CKR_OK)
 		return ret;
 
 	/* Set type */
-	set_tlv_type(tlv, type);
+	ret = set_tlv_type(tlv, type);
+	if (ret != CKR_OK)
+		return ret;
 
 	/* Set length */
-	set_tlv_length(tlv, nb_bytes);
+	ret = set_tlv_length(tlv, nb_bytes);
+	if (ret != CKR_OK)
+		return ret;
 
-	for (i = nb_bytes - 1; i >= 0; i--)
-		tlv->string[tlv->length++] = GET_BYTE(num, i);
+	for (i = nb_bytes - 1; i >= 0; i--) {
+		if (INC_OVERFLOW(tlv->length, 1)) {
+			ret = CKR_ARGUMENTS_BAD;
+			break;
+		}
 
-	return CKR_OK;
+		byte = GET_BYTE(num, i);
+		(void)SET_OVERFLOW(byte, tlv->string[tlv->length]);
+	}
+
+	return ret;
 }
 
 CK_RV tlv_encode_concat_string(struct smw_tlv *tlv, const char *type,
 			       const char *value, struct smw_tlv *concat_tlv)
 {
-	CK_RV ret;
-	size_t len_add;
-	size_t value_length = strlen(value) + 1;
-	size_t tlv_length;
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	size_t len_add = 0;
+	size_t value_length = 0;
+	size_t tlv_length = 0;
 
 	/*
 	 * Concat string is TLV encoded with:
@@ -205,33 +284,50 @@ CK_RV tlv_encode_concat_string(struct smw_tlv *tlv, const char *type,
 	 *   - String value to add after field Value
 	 */
 
-	len_add = strlen(type) + 1 + TLV_LENGTH_SIZE + value_length;
-	tlv_length = value_length + concat_tlv->length_max;
-	ret = tlv_realloc(tlv, len_add + concat_tlv->length_max);
+	value_length = strlen(value);
+	if (INC_OVERFLOW(value_length, 1))
+		return ret;
+
+	len_add = tlv_string_size(type, value_length);
+	if (!len_add)
+		return ret;
+
+	if (INC_OVERFLOW(len_add, concat_tlv->length_max))
+		return ret;
+
+	if (ADD_OVERFLOW(value_length, concat_tlv->length_max, &tlv_length))
+		return ret;
+
+	ret = tlv_realloc(tlv, len_add);
 	if (ret != CKR_OK)
 		return ret;
 
 	/* Set type */
-	set_tlv_type(tlv, type);
+	ret = set_tlv_type(tlv, type);
+	if (ret != CKR_OK)
+		return ret;
 
 	/* Set length */
-	set_tlv_length(tlv, tlv_length);
+	ret = set_tlv_length(tlv, tlv_length);
+	if (ret != CKR_OK)
+		return ret;
 
 	/* Set value - Input @value */
-	set_tlv_value(tlv, value, value_length);
+	ret = set_tlv_value(tlv, value, value_length);
 
 	/* Concatenate the string value */
-	if (concat_tlv->string)
-		set_tlv_value(tlv, concat_tlv->string, concat_tlv->length_max);
+	if (ret == CKR_OK && concat_tlv->string)
+		ret = set_tlv_value(tlv, concat_tlv->string,
+				    concat_tlv->length_max);
 
-	return CKR_OK;
+	return ret;
 }
 
 CK_RV tlv_encode_tlv(struct smw_tlv *tlv, const char *type,
 		     struct smw_tlv *tlv_value)
 {
-	CK_RV ret;
-	size_t len_add;
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	size_t len_add = 0;
 
 	/*
 	 * Build a TLV of TLV is TLV encoded with:
@@ -240,22 +336,28 @@ CK_RV tlv_encode_tlv(struct smw_tlv *tlv, const char *type,
 	 *   - Value = @tlv_value->string
 	 */
 
-	len_add = strlen(type) + 1 + TLV_LENGTH_SIZE;
-	ret = tlv_realloc(tlv, len_add + tlv_value->length_max);
+	len_add = tlv_string_size(type, tlv_value->length_max);
+	if (!len_add)
+		return ret;
+
+	ret = tlv_realloc(tlv, len_add);
 	if (ret != CKR_OK)
 		return ret;
 
 	/* Set type */
-	set_tlv_type(tlv, type);
+	ret = set_tlv_type(tlv, type);
+	if (ret != CKR_OK)
+		return ret;
 
 	/* Set length */
-	set_tlv_length(tlv, tlv_value->length_max);
+	ret = set_tlv_length(tlv, tlv_value->length_max);
 
 	/* Concatenate the string value */
-	if (tlv_value->string)
-		set_tlv_value(tlv, tlv_value->string, tlv_value->length_max);
+	if (ret == CKR_OK && tlv_value->string)
+		ret = set_tlv_value(tlv, tlv_value->string,
+				    tlv_value->length_max);
 
-	return CKR_OK;
+	return ret;
 }
 
 void tlv_encode_free(struct smw_tlv *tlv)

pkcs11/src/objects/lib_object.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2022 NXP
+ * Copyright 2020-2023 NXP
  */
 
 #include <stdlib.h>
@@ -74,9 +74,9 @@ const struct template_attr attr_obj_storage[] = {
 static CK_RV obj_add_to_list(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 			     bool token)
 {
-	CK_RV ret;
-	struct libdevice *dev;
-	struct libobj_list *objects;
+	CK_RV ret = CKR_OK;
+	struct libdevice *dev = NULL;
+	struct libobj_list *objects = NULL;
 
 	DBG_TRACE("Add object (%p) in session %lu list", obj, hsession);
 
@@ -121,9 +121,9 @@ static CK_RV obj_add_to_list(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 static CK_RV find_lock_object(CK_SESSION_HANDLE hsession,
 			      struct libobj_obj *obj, struct libobj_list **list)
 {
-	CK_RV ret;
-	struct libdevice *dev;
-	struct libobj_list *objects;
+	CK_RV ret = CKR_OK;
+	struct libdevice *dev = NULL;
+	struct libobj_list *objects = NULL;
 	struct libobj_obj *fobj = NULL;
 
 	DBG_TRACE("Find and lock object %p", obj);
@@ -177,6 +177,7 @@ static CK_RV find_lock_object(CK_SESSION_HANDLE hsession,
 			*list = objects;
 	}
 
+	// coverity[missing_unlock]
 	return ret;
 }
 
@@ -194,12 +195,12 @@ static CK_RV find_lock_object(CK_SESSION_HANDLE hsession,
  */
 static CK_RV set_unique_id(struct libobj_obj *obj)
 {
-	CK_RV ret = CKR_FUNCTION_FAILED;
+	CK_RV ret = CKR_GENERAL_ERROR;
 	struct librfc2279 *unique_id = NULL;
-	struct libbytes id = {};
+	struct libbytes id = { 0 };
 
 	if (!obj)
-		return CKR_GENERAL_ERROR;
+		return ret;
 
 	DBG_TRACE("Class %lx", obj->class);
 
@@ -268,8 +269,8 @@ end:
 static CK_RV get_session_obj_access(CK_SESSION_HANDLE hsession,
 				    unsigned int *access)
 {
-	CK_RV ret;
-	CK_SESSION_INFO sinfo;
+	CK_RV ret = CKR_OK;
+	CK_SESSION_INFO sinfo = { 0 };
 
 	ret = libsess_get_info(hsession, &sinfo);
 	if (ret != CKR_OK)
@@ -302,8 +303,8 @@ static CK_RV get_session_obj_access(CK_SESSION_HANDLE hsession,
 static CK_RV get_token_obj_access(CK_SESSION_HANDLE hsession,
 				  unsigned int *access)
 {
-	CK_RV ret;
-	CK_SESSION_INFO sinfo;
+	CK_RV ret = CKR_OK;
+	CK_SESSION_INFO sinfo = { 0 };
 
 	ret = libsess_get_info(hsession, &sinfo);
 	if (ret != CKR_OK)
@@ -339,9 +340,9 @@ static CK_RV get_token_obj_access(CK_SESSION_HANDLE hsession,
 static CK_RV obj_is_destroyable(CK_SESSION_HANDLE hsession,
 				struct libobj_obj *obj)
 {
-	CK_RV ret;
-	unsigned int access;
-	bool is_destroyable = true;
+	CK_RV ret = CKR_FUNCTION_FAILED;
+	unsigned int access = OBJ_RO;
+	bool is_destroyable = false;
 	bool is_private = false;
 
 	switch (obj->class) {
@@ -356,7 +357,7 @@ static CK_RV obj_is_destroyable(CK_SESSION_HANDLE hsession,
 		break;
 
 	default:
-		return CKR_FUNCTION_FAILED;
+		return ret;
 	}
 
 	if (!is_destroyable)
@@ -388,9 +389,9 @@ static CK_RV obj_is_destroyable(CK_SESSION_HANDLE hsession,
 static CK_RV obj_is_modifiable(CK_SESSION_HANDLE hsession,
 			       struct libobj_obj *obj)
 {
-	CK_RV ret;
-	unsigned int access;
-	bool is_modifiable = true;
+	CK_RV ret = CKR_FUNCTION_FAILED;
+	unsigned int access = OBJ_RO;
+	bool is_modifiable = false;
 	bool is_token = false;
 
 	switch (obj->class) {
@@ -404,7 +405,7 @@ static CK_RV obj_is_modifiable(CK_SESSION_HANDLE hsession,
 		break;
 
 	default:
-		return CKR_FUNCTION_FAILED;
+		return ret;
 	}
 
 	if (!is_modifiable)
@@ -434,29 +435,33 @@ static CK_RV obj_is_modifiable(CK_SESSION_HANDLE hsession,
  */
 static CK_RV obj_allocate(struct libobj_obj **obj)
 {
-	CK_RV ret;
-	struct libobj_obj *newobj;
+	CK_RV ret = CKR_HOST_MEMORY;
+	struct libobj_obj *newobj = NULL;
 
 	newobj = malloc(sizeof(*newobj));
 	if (!newobj)
-		return CKR_HOST_MEMORY;
+		goto end;
 
 	ret = libmutex_create(&newobj->lock);
-	if (ret != CKR_OK) {
-		free(newobj);
-		return ret;
-	}
+	if (ret != CKR_OK)
+		goto end;
 
 	newobj->class = 0;
 	newobj->object = NULL;
 	newobj->prev = NULL;
 	newobj->next = NULL;
 
+end:
+	if (ret != CKR_OK) {
+		free(newobj);
+		newobj = NULL;
+	}
+
 	*obj = newobj;
 
 	DBG_TRACE("Allocated a new object (%p)", *obj);
 
-	return CKR_OK;
+	return ret;
 }
 
 /**
@@ -469,7 +474,7 @@ static CK_RV obj_allocate(struct libobj_obj **obj)
  */
 static CK_RV obj_storage_allocate(struct libobj_storage **obj)
 {
-	struct libobj_storage *newobj;
+	struct libobj_storage *newobj = NULL;
 
 	newobj = malloc(sizeof(*newobj));
 	if (!newobj)
@@ -503,7 +508,7 @@ static CK_RV obj_storage_allocate(struct libobj_storage **obj)
  */
 static void obj_storage_free(struct libobj_obj *obj)
 {
-	struct libobj_storage *objstorage;
+	struct libobj_storage *objstorage = NULL;
 
 	if (!obj)
 		return;
@@ -601,8 +606,8 @@ static void obj_free(struct libobj_obj *obj, struct libobj_list *list)
 static CK_RV obj_storage_new(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 			     struct libattr_list *attrs)
 {
-	CK_RV ret;
-	unsigned int access;
+	CK_RV ret = CKR_OK;
+	unsigned int access = OBJ_RO;
 	struct libobj_storage *newobj = NULL;
 
 	ret = obj_storage_allocate(&newobj);
@@ -685,7 +690,7 @@ static CK_RV obj_storage_new(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 static CK_RV class_get_attribute(CK_ATTRIBUTE_PTR attr,
 				 const struct libobj_obj *libobj)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_FUNCTION_FAILED;
 
 	DBG_TRACE("Get attribute type=%#lx", attr->type);
 	switch (libobj->class) {
@@ -720,7 +725,7 @@ static CK_RV class_get_attribute(CK_ATTRIBUTE_PTR attr,
 		break;
 
 	default:
-		ret = CKR_FUNCTION_FAILED;
+		break;
 	}
 
 	DBG_TRACE("Get attribute type=%#lx ret %ld", attr->type, ret);
@@ -746,7 +751,7 @@ static CK_RV class_get_attribute(CK_ATTRIBUTE_PTR attr,
 static CK_RV class_modify_attribute(CK_ATTRIBUTE_PTR attr,
 				    struct libobj_obj *libobj)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_FUNCTION_FAILED;
 
 	DBG_TRACE("Modify attribute type=%#lx", attr->type);
 	switch (libobj->class) {
@@ -781,7 +786,7 @@ static CK_RV class_modify_attribute(CK_ATTRIBUTE_PTR attr,
 		break;
 
 	default:
-		ret = CKR_FUNCTION_FAILED;
+		break;
 	}
 
 	DBG_TRACE("Modify attribute type=%#lx ret %ld", attr->type, ret);
@@ -791,7 +796,7 @@ static CK_RV class_modify_attribute(CK_ATTRIBUTE_PTR attr,
 CK_RV libobj_create(CK_SESSION_HANDLE hsession, CK_ATTRIBUTE_PTR attrs,
 		    CK_ULONG nb_attrs, CK_OBJECT_HANDLE_PTR hobj)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_OK;
 	struct libobj_obj *newobj = NULL;
 	struct libattr_list attrs_list = { .attr = attrs, .number = nb_attrs };
 
@@ -855,9 +860,9 @@ end:
 
 CK_RV libobj_destroy(CK_SESSION_HANDLE hsession, CK_OBJECT_HANDLE hobject)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_OK;
 	struct libobj_obj *obj = (struct libobj_obj *)hobject;
-	struct libobj_list *objects;
+	struct libobj_list *objects = NULL;
 
 	DBG_TRACE("Destroy object (%p) of session %lu", obj, hsession);
 
@@ -880,10 +885,10 @@ CK_RV libobj_destroy(CK_SESSION_HANDLE hsession, CK_OBJECT_HANDLE hobject)
 CK_RV libobj_get_attribute(CK_SESSION_HANDLE hsession, CK_OBJECT_HANDLE hobject,
 			   CK_ATTRIBUTE_PTR attrs, CK_ULONG nb_attrs)
 {
-	CK_RV ret;
-	CK_RV status;
+	CK_RV ret = CKR_OK;
+	CK_RV status = CKR_OK;
 	struct libobj_obj *libobj = (struct libobj_obj *)hobject;
-	CK_ULONG idx;
+	CK_ULONG idx = 0;
 
 	DBG_TRACE("Get attribute(s) of object (%p) in session %lu", libobj,
 		  hsession);
@@ -893,7 +898,7 @@ CK_RV libobj_get_attribute(CK_SESSION_HANDLE hsession, CK_OBJECT_HANDLE hobject,
 		goto end;
 
 	DBG_TRACE("Get %lu attribute(s)", nb_attrs);
-	for (idx = 0; idx < nb_attrs; idx++) {
+	for (; idx < nb_attrs; idx++) {
 		DBG_TRACE("Get attribute %lu type=%#lx", idx, attrs[idx].type);
 		status =
 			attr_get_obj_value(&attrs[idx], attr_obj_common,
@@ -938,9 +943,9 @@ CK_RV libobj_modify_attribute(CK_SESSION_HANDLE hsession,
 			      CK_OBJECT_HANDLE hobject, CK_ATTRIBUTE_PTR attrs,
 			      CK_ULONG nb_attrs)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_OK;
 	struct libobj_obj *libobj = (struct libobj_obj *)hobject;
-	CK_ULONG idx;
+	CK_ULONG idx = 0;
 
 	DBG_TRACE("Modify attribute(s) of object (%p) in session %lu", libobj,
 		  hsession);
@@ -952,7 +957,7 @@ CK_RV libobj_modify_attribute(CK_SESSION_HANDLE hsession,
 	ret = obj_is_modifiable(hsession, libobj);
 
 	DBG_TRACE("Modify %lu attribute(s)", nb_attrs);
-	for (idx = 0; idx < nb_attrs && ret == CKR_OK; idx++) {
+	for (; idx < nb_attrs && ret == CKR_OK; idx++) {
 		DBG_TRACE("Modify attribute %lu type=%#lx", idx,
 			  attrs[idx].type);
 		ret = attr_modify_obj_value(&attrs[idx], attr_obj_common,
@@ -983,7 +988,7 @@ CK_RV libobj_generate_keypair(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
 			      CK_ULONG nb_priv_attrs, CK_OBJECT_HANDLE_PTR hpub,
 			      CK_OBJECT_HANDLE_PTR hpriv)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_OK;
 	struct libobj_obj *pub_key = NULL;
 	struct libobj_obj *priv_key = NULL;
 	struct libattr_list pub_attrs_list = { .attr = pub_attrs,
@@ -1094,7 +1099,7 @@ CK_RV libobj_generate_key(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
 			  CK_ATTRIBUTE_PTR attrs, CK_ULONG nb_attrs,
 			  CK_OBJECT_HANDLE_PTR hkey)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_OK;
 	struct libobj_obj *key = NULL;
 	struct libattr_list attrs_list = { .attr = attrs, .number = nb_attrs };
 
@@ -1170,10 +1175,10 @@ static CK_RV object_match(CK_SESSION_HANDLE hsession,
 			  CK_ATTRIBUTE_PTR attrs_tmp, CK_ULONG nb_attrs)
 {
 	CK_RV ret = CKR_OK;
-	struct libobj_obj *obj;
+	struct libobj_obj *obj = NULL;
 	struct libobj_handles *obj_match = NULL;
-	CK_ULONG idx;
-	bool match;
+	CK_ULONG idx = 0;
+	bool match = false;
 
 	for (obj = LIST_FIRST(list); obj; obj = LIST_NEXT(obj)) {
 		DBG_TRACE("Check if object %p match", obj);
@@ -1219,8 +1224,8 @@ static CK_RV object_match(CK_SESSION_HANDLE hsession,
 
 static void destroy_query_list(struct libobj_query *query)
 {
-	struct libobj_handles *obj;
-	struct libobj_handles *next;
+	struct libobj_handles *obj = NULL;
+	struct libobj_handles *next = NULL;
 
 	if (query) {
 		obj = LIST_FIRST(&query->objects);
@@ -1237,11 +1242,11 @@ static void destroy_query_list(struct libobj_query *query)
 CK_RV libobj_find_init(CK_SESSION_HANDLE hsession, CK_ATTRIBUTE_PTR attrs,
 		       CK_ULONG nb_attrs)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_OK;
 	struct libobj_query *query = NULL;
-	struct libobj_list *objects;
-	struct libdevice *dev;
-	CK_ULONG idx;
+	struct libobj_list *objects = NULL;
+	struct libdevice *dev = NULL;
+	CK_ULONG idx = 0;
 	CK_ATTRIBUTE_PTR attrs_tmp = NULL;
 
 	DBG_TRACE("Start Find Object Query on session %lu", hsession);
@@ -1335,10 +1340,10 @@ end:
 CK_RV libobj_find(CK_SESSION_HANDLE hsession, CK_OBJECT_HANDLE_PTR pobjs,
 		  CK_ULONG nb_objs_max, CK_ULONG_PTR pnb_objs_found)
 {
-	CK_RV ret;
-	struct libobj_query *query;
-	struct libobj_handles *obj;
-	struct libobj_handles *next;
+	CK_RV ret = CKR_OK;
+	struct libobj_query *query = NULL;
+	struct libobj_handles *obj = NULL;
+	struct libobj_handles *next = NULL;
 	CK_ULONG rem_objs = nb_objs_max;
 	CK_OBJECT_HANDLE_PTR out_objs = pobjs;
 
@@ -1354,7 +1359,10 @@ CK_RV libobj_find(CK_SESSION_HANDLE hsession, CK_OBJECT_HANDLE_PTR pobjs,
 		LIST_REMOVE(&query->objects, obj);
 		free(obj);
 		obj = next;
-		(*pnb_objs_found)++;
+
+		if (INC_OVERFLOW(*pnb_objs_found, 1))
+			return CKR_GENERAL_ERROR;
+
 		out_objs++;
 	}
 
@@ -1367,8 +1375,8 @@ end:
 
 CK_RV libobj_find_final(CK_SESSION_HANDLE hsession)
 {
-	CK_RV ret;
-	struct libobj_query *query;
+	CK_RV ret = CKR_OK;
+	struct libobj_query *query = NULL;
 
 	DBG_TRACE("Final Find Object Query on session %lu", hsession);
 	ret = libsess_get_query(hsession, &query);
@@ -1386,14 +1394,14 @@ CK_RV libobj_find_final(CK_SESSION_HANDLE hsession)
 
 CK_RV libobj_list_destroy(struct libobj_list *list)
 {
-	CK_RV ret;
-	struct libobj_obj *obj;
-	struct libobj_obj *next;
+	CK_RV ret = CKR_GENERAL_ERROR;
+	struct libobj_obj *obj = NULL;
+	struct libobj_obj *next = NULL;
 
 	DBG_TRACE("Destroy all objects from list %p", list);
 
 	if (!list)
-		return CKR_GENERAL_ERROR;
+		return ret;
 
 	/* Lock the list until the end of the destruction */
 	ret = LLIST_LOCK(list);

pkcs11/src/CMakeLists.txt
@@ -12,7 +12,8 @@ configure_file(./include/pkcs11smw_config.h.in ./include/pkcs11smw_config.h)
 
 target_include_directories(${PROJECT_NAME} PRIVATE
 	./include
-	${CMAKE_CURRENT_BINARY_DIR}/include)
+	${CMAKE_CURRENT_BINARY_DIR}/include
+	${CMAKE_SOURCE_DIR}/inc)
 
 add_subdirectory(utils)
 add_subdirectory(ifsmw)

pkcs11/src/digest.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2021 NXP
+ * Copyright 2020-2021, 2023 NXP
  */
 
 #include "pkcs11smw.h"
@@ -13,12 +13,12 @@ static CK_RV digest(CK_SESSION_HANDLE hSession, CK_BYTE_PTR pData,
 		    CK_ULONG ulDataLen, CK_BYTE_PTR pDigest,
 		    CK_ULONG_PTR pulDigestLen)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_SESSION_HANDLE_INVALID;
 	CK_MECHANISM mechanism = { 0 };
-	struct libdig_params params;
+	struct libdig_params params = { 0 };
 
 	if (!hSession)
-		return CKR_SESSION_HANDLE_INVALID;
+		return ret;
 
 	if ((pDigest && (!pData != !ulDataLen)) || !pulDigestLen) {
 		ret = CKR_ARGUMENTS_BAD;
@@ -47,10 +47,10 @@ end:
 
 CK_RV C_DigestInit(CK_SESSION_HANDLE hSession, CK_MECHANISM_PTR pMechanism)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_SESSION_HANDLE_INVALID;
 
 	if (!hSession)
-		return CKR_SESSION_HANDLE_INVALID;
+		return ret;
 
 	if (pMechanism) {
 		ret = libsess_validate_mechanism(hSession, pMechanism,

pkcs11/src/entry.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020 NXP
+ * Copyright 2020, 2023 NXP
  */
 
 #include "lib_context.h"
@@ -10,9 +10,9 @@
 
 CK_RV C_Initialize(CK_VOID_PTR pInitArgs)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_OK;
 	CK_C_INITIALIZE_ARGS_PTR pinit = pInitArgs;
-	struct libcaps *libcaps;
+	struct libcaps *libcaps = NULL;
 
 	ret = libctx_create();
 	if (ret != CKR_OK)
@@ -115,12 +115,10 @@ end_check:
 
 CK_RV C_Finalize(CK_VOID_PTR pReserved)
 {
-	CK_RV ret;
+	CK_RV ret = CKR_ARGUMENTS_BAD;
 
-	if (pReserved)
-		return CKR_ARGUMENTS_BAD;
-
-	ret = libctx_destroy();
+	if (!pReserved)
+		ret = libctx_destroy();
 
 	return ret;
 }

pkcs11/src/get_info.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020 NXP
+ * Copyright 2020, 2023 NXP
  */
 
 #include <string.h>
@@ -212,7 +212,8 @@ static struct CK_INTERFACE pkcs11smw_interfaces[] = {
 
 CK_RV C_GetInfo(CK_INFO_PTR pInfo)
 {
-	size_t len;
+	size_t len = 0;
+	size_t memset_len = 0;
 
 	if (!pInfo)
 		return CKR_ARGUMENTS_BAD;
@@ -224,14 +225,16 @@ CK_RV C_GetInfo(CK_INFO_PTR pInfo)
 	/* Pad manufacturerID and LibraryDescription with blank */
 	len = strlen((const char *)pkcs11smw_info.manufacturerID);
 	DBG_TRACE("Manufacturer (%zu) bytes: %s", len, pInfo->manufacturerID);
-	memset(pInfo->manufacturerID + len, ' ',
-	       sizeof(pInfo->manufacturerID) - len);
+
+	if (!SUB_OVERFLOW(sizeof(pInfo->manufacturerID), len, &memset_len))
+		memset(pInfo->manufacturerID + len, ' ', memset_len);
 
 	len = strlen((const char *)pkcs11smw_info.libraryDescription);
 	DBG_TRACE("Lib Description (%zu) bytes: %s", len,
 		  pInfo->libraryDescription);
-	memset(pInfo->libraryDescription + len, ' ',
-	       sizeof(pInfo->libraryDescription) - len);
+
+	if (!SUB_OVERFLOW(sizeof(pInfo->libraryDescription), len, &memset_len))
+		memset(pInfo->libraryDescription + len, ' ', memset_len);
 
 	return CKR_OK;
 }
@@ -249,7 +252,7 @@ CK_RV C_GetFunctionList(CK_FUNCTION_LIST_PTR_PTR ppFunctionList)
 CK_RV C_GetInterfaceList(CK_INTERFACE_PTR pInterfacesList,
 			 CK_ULONG_PTR pulCount)
 {
-	CK_ULONG nb_entries;
+	CK_ULONG nb_entries = 0;
 
 	if (!pulCount)
 		return CKR_ARGUMENTS_BAD;
@@ -274,7 +277,7 @@ CK_RV C_GetInterface(CK_UTF8CHAR_PTR pInterfaceName, CK_VERSION_PTR pVersion,
 {
 	CK_RV ret = CKR_ARGUMENTS_BAD;
 	CK_INTERFACE_PTR entry = pkcs11smw_interfaces;
-	CK_FUNCTION_LIST_PTR func_list;
+	CK_FUNCTION_LIST_PTR func_list = NULL;
 
 	if (!ppInterface)
 		goto end;

commit 907cc96ce39df2f7b3b371cd36998dad0508ad98

SSMW-528 pkcs11: Fix TLV overflow and object's mutex destroy
Fix TLV numeral construction generating a buffer overflow.
Fix object's mutex destroyed. Mutex was never destroyed.

Fixes: 9a79b14aae2d ("SSMW-455 pkcs11: Fix coverity and coding rules")
Fixes: 305aab3e784e ("SSMW-130 pkcs11: add support token object")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/ifsmw/tlv_encode.c
@@ -222,7 +222,6 @@ CK_RV tlv_encode_numeral(struct smw_tlv *tlv, const char *type, long long num)
 {
 	CK_RV ret = CKR_ARGUMENTS_BAD;
 	size_t len_add = 0;
-	int i = 0;
 	unsigned int nb_bytes = 0;
 	uint8_t byte = 0;
 
@@ -254,14 +253,14 @@ CK_RV tlv_encode_numeral(struct smw_tlv *tlv, const char *type, long long num)
 	if (ret != CKR_OK)
 		return ret;
 
-	for (i = nb_bytes - 1; i >= 0; i--) {
+	for (; nb_bytes; nb_bytes--) {
+		byte = GET_BYTE(num, nb_bytes - 1);
+		(void)SET_OVERFLOW(byte, tlv->string[tlv->length]);
+
 		if (INC_OVERFLOW(tlv->length, 1)) {
 			ret = CKR_ARGUMENTS_BAD;
 			break;
 		}
-
-		byte = GET_BYTE(num, i);
-		(void)SET_OVERFLOW(byte, tlv->string[tlv->length]);
 	}
 
 	return ret;

pkcs11/src/objects/lib_object.c
@@ -575,8 +575,7 @@ static void obj_free(struct libobj_obj *obj, struct libobj_list *list)
 		LIST_REMOVE(list, obj);
 	}
 
-	libmutex_unlock(obj->lock);
-	libmutex_destroy(obj->lock);
+	libmutex_destroy(&obj->lock);
 
 	free(obj);
 }
@@ -870,11 +869,12 @@ CK_RV libobj_destroy(CK_SESSION_HANDLE hsession, CK_OBJECT_HANDLE hobject)
 	if (ret == CKR_OK) {
 		/* Check if the object can be destroyed */
 		ret = obj_is_destroyable(hsession, obj);
+
+		libmutex_unlock(obj->lock);
+
 		if (ret == CKR_OK) {
 			obj_free(obj, objects);
 			obj = NULL;
-		} else {
-			libmutex_unlock(obj->lock);
 		}
 	}
 
@@ -1418,6 +1418,7 @@ CK_RV libobj_list_destroy(struct libobj_list *list)
 			return ret;
 		}
 
+		libmutex_unlock(obj->lock);
 		obj_free(obj, list);
 		obj = next;
 	}
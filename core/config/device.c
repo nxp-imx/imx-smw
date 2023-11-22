72fb991827ba51c26f415adbb89be6511b42ee50
Cedric Neveux <cedric.neveux@nxp.com>
2023-11-22 14:09:16 +0100

    SSMW-498 core: config: Add device storage reprovisioning
    Add device storage reprovisioning operation.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- core/config/device.c
+++ core/config/device.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2023 NXP
+ * Copyright 2023-2024 NXP
  */
 
 #include "smw_status.h"
@@ -110,3 +110,54 @@ static int device_lifecycle_check_subsystem_caps(void *args, void *params)
 }
 
 DEFINE_CONFIG_OPERATION_FUNC(device_lifecycle);
+
+static int device_reprovision_read_params(char **start, char *end,
+					  void **params)
+{
+	(void)params;
+
+	int status = SMW_STATUS_OK;
+	char *cur = *start;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	while ((cur < end) && (open_square_bracket != *cur)) {
+		status = skip_param(&cur, end);
+		if (status != SMW_STATUS_OK)
+			goto end;
+
+		skip_insignificant_chars(&cur, end);
+	}
+
+	*start = cur;
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static void device_reprovision_merge_params(void *caps, void *params)
+{
+	(void)caps;
+	(void)params;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+}
+
+static void device_reprovision_print_params(void *params)
+{
+	(void)params;
+}
+
+static int device_reprovision_check_subsystem_caps(void *args, void *params)
+{
+	(void)args;
+	(void)params;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, SMW_STATUS_OK);
+	return SMW_STATUS_OK;
+}
+
+DEFINE_CONFIG_OPERATION_FUNC(device_reprovision);

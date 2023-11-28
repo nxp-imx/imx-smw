b180f6984b72f64693090c492cfbd84db3eedfb5
Cedric Neveux <cedric.neveux@nxp.com>
2023-11-28 11:03:34 +0100

    SSMW-536 tests: Fix coverity
    Fix test engine thread wait condition.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- tests/engine/utils/util_cond.c
+++ tests/engine/utils/util_cond.c
@@ -6,36 +6,53 @@
 #include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "util.h"
 #include "util_cond.h"
 #include "util_mutex.h"
 
+/**
+ * struct thr_cond - Thread waiting condition
+ * @active: True if thread waiting condition still active
+ * @cond: System thread condition variable
+ */
+struct thr_cond {
+	int active;
+	pthread_cond_t wait_cond;
+};
+
 void *util_cond_create(void)
 {
-	void *cond = NULL;
+	struct thr_cond *thr_cond = NULL;
 
-	cond = calloc(1, sizeof(pthread_cond_t));
-	if (cond) {
-		if (pthread_cond_init(cond, NULL)) {
-			free(cond);
-			cond = NULL;
+	thr_cond = calloc(1, sizeof(*thr_cond));
+	if (thr_cond) {
+		if (pthread_cond_init(&thr_cond->wait_cond, NULL)) {
+			free(thr_cond);
+			thr_cond = NULL;
 		}
 	}
 
-	return cond;
+	return thr_cond;
 }
 
 int util_cond_destroy(void **cond)
 {
+	struct thr_cond *thr_cond = NULL;
+
 	if (!cond)
 		return ERR_CODE(BAD_ARGS);
 
-	if (*cond) {
-		if (pthread_cond_destroy(*cond))
+	thr_cond = *cond;
+
+	if (thr_cond) {
+		thr_cond->active = false;
+
+		if (pthread_cond_destroy(&thr_cond->wait_cond))
 			return ERR_CODE(COND_DESTROY);
 
-		free(*cond);
+		free(thr_cond);
 
 		*cond = NULL;
 	}
@@ -45,13 +62,17 @@ int util_cond_destroy(void **cond)
 
 int util_cond_signal(void *cond)
 {
-	if (!cond) {
+	struct thr_cond *thr_cond = cond;
+
+	if (!thr_cond) {
 		DBG_PRINT_BAD_ARGS();
 		return ERR_CODE(BAD_ARGS);
 	}
 
-	if (pthread_cond_signal(cond)) {
-		DBG_PRINT("Signal (%p) failed: %s", cond, util_get_strerr());
+	thr_cond->active = false;
+	if (pthread_cond_signal(&thr_cond->wait_cond)) {
+		DBG_PRINT("Signal (%p) failed: %s", thr_cond->wait_cond,
+			  util_get_strerr());
 		return ERR_CODE(FAILED);
 	}
 
@@ -60,13 +81,14 @@ int util_cond_signal(void *cond)
 
 int util_cond_wait(void *cond, void *mutex, unsigned int timeout)
 {
-	int res = ERR_CODE(FAILED);
+	int res = ERR_CODE(BAD_ARGS);
 	int err = 0;
 	struct timespec ts = { 0 };
+	struct thr_cond *thr_cond = cond;
 
-	if (!cond || !mutex || !timeout) {
+	if (!thr_cond || !mutex || !timeout) {
 		DBG_PRINT_BAD_ARGS();
-		return ERR_CODE(BAD_ARGS);
+		return res;
 	}
 
 	util_mutex_lock(mutex);
@@ -79,18 +101,26 @@ int util_cond_wait(void *cond, void *mutex, unsigned int timeout)
 
 	ts.tv_sec += timeout;
 
-	err = pthread_cond_timedwait(cond, mutex, &ts);
-	if (err) {
-		if (err == ETIMEDOUT) {
-			DBG_PRINT("Wait (%p) failed: Timeout", cond);
-			res = ERR_CODE(TIMEOUT);
-		} else {
-			DBG_PRINT("Wait (%p) failed %d", cond, err);
+	thr_cond->active = true;
+
+	while (thr_cond->active) {
+		err = pthread_cond_timedwait(&thr_cond->wait_cond, mutex, &ts);
+		if (err) {
+			if (err == ETIMEDOUT) {
+				DBG_PRINT("Wait (%p) failed: Timeout",
+					  thr_cond->wait_cond);
+				res = ERR_CODE(TIMEOUT);
+			} else {
+				DBG_PRINT("Wait (%p) failed %d",
+					  thr_cond->wait_cond, err);
+				res = ERR_CODE(FAILED);
+			}
+
+			goto exit;
 		}
-	} else {
-		res = ERR_CODE(PASSED);
 	}
 
+	res = ERR_CODE(PASSED);
 exit:
 	util_mutex_unlock(mutex);
 

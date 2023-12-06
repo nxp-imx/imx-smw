4efc6f4d61e192c596c6b42f5c738d350f985b96
Bruno De Smet <bruno.de.smet@nxp.com>
2023-12-06 23:27:15 +0100

    SSMW-543 core: config: Remove duplicate Hash algorithm definitioms
    Use enum smw_config_hash_algo_id instead of enum smw_config_hmac_algo_id
    and smw_utils_get_hash_algo_id() isntead of smw_utils_get_hmac_algo_id()
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- core/config/hmac.c
+++ core/config/hmac.c
@@ -19,22 +19,6 @@
 #include "common.h"
 #include "tag.h"
 
-static const char *const hmac_algo_names[] = {
-	[SMW_CONFIG_HMAC_ALGO_ID_MD5] = "MD5",
-	[SMW_CONFIG_HMAC_ALGO_ID_SHA1] = "SHA1",
-	[SMW_CONFIG_HMAC_ALGO_ID_SHA224] = "SHA224",
-	[SMW_CONFIG_HMAC_ALGO_ID_SHA256] = "SHA256",
-	[SMW_CONFIG_HMAC_ALGO_ID_SHA384] = "SHA384",
-	[SMW_CONFIG_HMAC_ALGO_ID_SHA512] = "SHA512",
-	[SMW_CONFIG_HMAC_ALGO_ID_SM3] = "SM3"
-};
-
-int read_hmac_algo_names(char **start, char *end, unsigned long *bitmap)
-{
-	return smw_config_read_names(start, end, bitmap, hmac_algo_names,
-				     SMW_CONFIG_HMAC_ALGO_ID_NB);
-}
-
 static int hmac_read_params(char **start, char *end, void **params)
 {
 	int status = SMW_STATUS_OK;
@@ -66,8 +50,8 @@ static int hmac_read_params(char **start, char *end, void **params)
 		skip_insignificant_chars(&cur, end);
 
 		if (!SMW_UTILS_STRNCMP(buffer, hmac_algo_values, length)) {
-			status = read_hmac_algo_names(&cur, end,
-						      &p->algo_bitmap);
+			status = smw_utils_hash_algo_names(&cur, end,
+							   &p->algo_bitmap);
 			if (status != SMW_STATUS_OK)
 				goto end;
 		} else if (read_key(buffer, length, &cur, end,

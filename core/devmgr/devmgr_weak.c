2157daa8750251363b3133015d6d29be698dfede
Cedric Neveux <cedric.neveux@nxp.com>
2023-12-13 11:24:51 +0100

    SSMW-498 core: devmgr: Move lifecycle weak functions
    Move the lifecycle weak functions into the device manager common
    weak file.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- core/devmgr/devmgr_weak.c
+++ core/devmgr/devmgr_weak.c
@@ -53,6 +53,26 @@ smw_device_get_uuid(struct smw_device_uuid_args *args)
 }
 
 __weak enum smw_status_code
+smw_device_get_lifecycle(struct smw_device_lifecycle_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
+__weak enum smw_status_code
+smw_device_set_lifecycle(struct smw_device_lifecycle_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
+__weak enum smw_status_code
 smw_device_reprovision_prepare(struct smw_device_reprovision_args *args)
 {
 	(void)args;

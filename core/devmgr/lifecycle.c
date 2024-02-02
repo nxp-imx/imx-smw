nxp-imx/imx-smw

commit f8c926e407e1b17cb537b311d57a7c31cd10a55f

SSMW-345 core: devmgr: Add device lifecycle operations
Add the operations to manage the device lifecycle.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/devmgr/lifecycle.c
@@ -1,0 +1,142 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ */
+
+#include "smw_device.h"
+
+#include "subsystems.h"
+
+#include "config.h"
+#include "constants.h"
+#include "devmgr.h"
+#include "debug.h"
+#include "exec.h"
+#include "tlv_strings.h"
+#include "utils.h"
+
+#define LIFECYCLE(_name)                                                       \
+	{                                                                      \
+		.lifecycle_str = LC_##_name##_STR,                             \
+		.lifecycle = SMW_LIFECYCLE_##_name,                            \
+	}
+
+/**
+ * struct - Lifecycle
+ * @lifecycle_str: Lifecycle name used for TLV encoding.
+ * @lifecycle: Lifecycle id.
+ */
+static const struct {
+	const char *lifecycle_str;
+	unsigned int lifecycle;
+} lifecycle_info[] = { LIFECYCLE(OPEN), LIFECYCLE(CLOSED),
+		       LIFECYCLE(CLOSED_LOCKED), LIFECYCLE(OEM_RETURN),
+		       LIFECYCLE(NXP_RETURN) };
+
+static int get_lifecycle(const char *name, unsigned int *id)
+{
+	int status = SMW_STATUS_INVALID_LIFECYCLE;
+	unsigned int i = 0;
+
+	if (!name)
+		return status;
+
+	for (; i < ARRAY_SIZE(lifecycle_info); i++) {
+		if (!SMW_UTILS_STRCMP(name, lifecycle_info[i].lifecycle_str)) {
+			*id = lifecycle_info[i].lifecycle;
+			status = SMW_STATUS_OK;
+			break;
+		}
+	}
+
+	return status;
+}
+
+static int set_lifecycle(unsigned int id, const char **name)
+{
+	int status = SMW_STATUS_INVALID_LIFECYCLE;
+	unsigned int i = 0;
+
+	for (; i < ARRAY_SIZE(lifecycle_info); i++) {
+		if (id == lifecycle_info[i].lifecycle) {
+			*name = lifecycle_info[i].lifecycle_str;
+			status = SMW_STATUS_OK;
+			break;
+		}
+	}
+
+	return status;
+}
+
+enum smw_status_code
+smw_device_set_lifecycle(struct smw_device_lifecycle_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_devmgr_lifecycle_args lc_args = { 0 };
+	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!args)
+		goto end;
+
+	if (args->version != 0) {
+		status = SMW_STATUS_INVALID_VERSION;
+		goto end;
+	}
+
+	status = smw_config_get_subsystem_id(args->subsystem_name,
+					     &subsystem_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = get_lifecycle(args->lifecycle_name, &lc_args.lifecycle_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	lc_args.op = SMW_OP_DEVMGR_SET_LIFECYCLE;
+
+	status = smw_utils_execute_operation(OPERATION_ID_DEVICE_LIFECYCLE,
+					     &lc_args, subsystem_id);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+enum smw_status_code
+smw_device_get_lifecycle(struct smw_device_lifecycle_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_devmgr_lifecycle_args lc_args = { 0 };
+	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!args)
+		goto end;
+
+	if (args->version != 0) {
+		status = SMW_STATUS_INVALID_VERSION;
+		goto end;
+	}
+
+	status = smw_config_get_subsystem_id(args->subsystem_name,
+					     &subsystem_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	lc_args.op = SMW_OP_DEVMGR_GET_LIFECYCLE;
+
+	status = smw_utils_execute_operation(OPERATION_ID_DEVICE_LIFECYCLE,
+					     &lc_args, subsystem_id);
+	if (status == SMW_STATUS_OK)
+		status = set_lifecycle(lc_args.lifecycle_id,
+				       &args->lifecycle_name);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}

core/devmgr/CMakeLists.txt
@@ -9,11 +9,17 @@ if(ENABLE_DEVICE_ATTESTATION)
    add_operation(DEVICE_ATTESTATION)
 endif()
 
+if(ENABLE_DEVICE_LIFECYCLE)
+   add_operation(DEVICE_LIFECYCLE)
+endif()
+
 add_library(devmgr OBJECT "")
 target_sources(devmgr PRIVATE
                devmgr_weak.c
+               lifecycle_weak.c
                $<$<BOOL:${ENABLE_DEVMGR_MODULE}>:devmgr.c>
-               $<$<BOOL:${ENABLE_DEVICE_ATTESTATION}>:attestation.c>)
+               $<$<BOOL:${ENABLE_DEVICE_ATTESTATION}>:attestation.c>
+               $<$<BOOL:${ENABLE_DEVICE_LIFECYCLE}>:lifecycle.c>)
 
 target_include_directories(devmgr PRIVATE ${INCLUDE_DIR})
 

core/devmgr/lifecycle_weak.c
@@ -1,0 +1,29 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ */
+
+#include "smw_device.h"
+
+#include "compiler.h"
+#include "debug.h"
+
+__weak enum smw_status_code
+smw_device_get_lifecycle(struct smw_device_lifecycle_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
+__weak enum smw_status_code
+smw_device_set_lifecycle(struct smw_device_lifecycle_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}

core/inc/constants.h
@@ -16,5 +16,7 @@
 #define SMW_LIFECYCLE_OPEN	    BIT(1)
 #define SMW_LIFECYCLE_CLOSED	    BIT(2)
 #define SMW_LIFECYCLE_CLOSED_LOCKED BIT(3)
+#define SMW_LIFECYCLE_OEM_RETURN    BIT(4)
+#define SMW_LIFECYCLE_NXP_RETURN    BIT(5)
 
 #endif /* __CONSTANTS_H__ */

core/inc/devmgr.h
@@ -8,7 +8,12 @@
 
 #include "smw_device.h"
 
-enum smw_op_devmgr { SMW_OP_DEVMGR_ATTESTATION, SMW_OP_DEVMGR_UUID };
+enum smw_op_devmgr {
+	SMW_OP_DEVMGR_ATTESTATION,
+	SMW_OP_DEVMGR_UUID,
+	SMW_OP_DEVMGR_SET_LIFECYCLE,
+	SMW_OP_DEVMGR_GET_LIFECYCLE
+};
 
 /**
  * struct smw_devmgr_args - Device manager arguments
@@ -22,6 +27,15 @@ struct smw_devmgr_args {
 		struct smw_device_attestation_args *attestation;
 		struct smw_device_uuid_args *uuid;
 	} pub;
+};
+
+/**
+ * struct smw_devmgr_lifecycle_args - Device lifecycle arguments
+ * @lifecycle_id: Internal device lifecycle value
+ */
+struct smw_devmgr_lifecycle_args {
+	enum smw_op_devmgr op;
+	unsigned int lifecycle_id;
 };
 
 /**

core/inc/tlv_strings.h
@@ -32,6 +32,8 @@
 #define LC_CLOSED_STR	     "CLOSED"
 #define LC_CLOSED_LOCKED_STR "CLOSED_LOCKED"
 #define LC_CURRENT_STR	     "CURRENT"
+#define LC_OEM_RETURN_STR    "OEM_RETURN"
+#define LC_NXP_RETURN_STR    "NXP_RETURN"
 
 #define READ_ONLY_STR  "READ_ONLY"
 #define READ_ONCE_STR  "READ_ONCE"

commit 2ef6c9a88886c8189fcbbfb2e50e3f9571a7a30f

SSMW-542 core: devmgr: Return correct status code
If the arguments version is not supported,
the device manager returns SMW_STATUS_VERSION_NOT_SUPPORTED.

Fixes: f8c926e407e1 ("SSMW-345 core: devmgr: Add device lifecycle operations")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/devmgr/lifecycle.c
@@ -82,7 +82,7 @@ smw_device_set_lifecycle(struct smw_device_lifecycle_args *args)
 		goto end;
 
 	if (args->version != 0) {
-		status = SMW_STATUS_INVALID_VERSION;
+		status = SMW_STATUS_VERSION_NOT_SUPPORTED;
 		goto end;
 	}
 
@@ -119,7 +119,7 @@ smw_device_get_lifecycle(struct smw_device_lifecycle_args *args)
 		goto end;
 
 	if (args->version != 0) {
-		status = SMW_STATUS_INVALID_VERSION;
+		status = SMW_STATUS_VERSION_NOT_SUPPORTED;
 		goto end;
 	}
 
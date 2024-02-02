nxp-imx/imx-smw

commit 8bd9311a776579b6a863e8a5ffc8de089b42dac1

SSMW-266 core: OSAL key data base becomes OSAL object database
- Use new SMW OSAL operations related to database
- Implement smw_object_db_xxx() APIs
- smw_keybmgr_db_xxx() use smw_object_db_xxx() APIs
- Remove useless param of smw_keymgr_db_delete()

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/inc/storage.h
@@ -11,20 +11,34 @@
 
 /**
  * struct smw_storage_data_attributes - Storage attributes.
- * @rw_flags: Data access flags.
- * @lifecycle_flags: Device lifecycles where data is accessible.
+ * @rw_flags: Data access flags
+ * @lifecycle_flags: Device lifecycles where data is accessible
+ * @persistence_id: Persistence ID
  */
 struct smw_storage_data_attributes {
 	unsigned long rw_flags;
 	unsigned long lifecycle_flags;
+	enum smw_object_persistence_id persistence_id;
+};
+
+/**
+ * struct smw_storage_data_info - Data information stored in object database.
+ * @subsystem_id: Secure Subsystem ID
+ * @size: Data size
+ * @attributes: Data attributes
+ */
+struct smw_storage_data_info {
+	enum subsystem_id subsystem_id;
+	unsigned int size;
+	struct smw_storage_data_attributes attributes;
 };
 
 /**
  * struct smw_storage_enc_args - Encryption arguments.
- * @keys_desc: Pointer to an array of internal key descriptor structure pointer.
- * @nb_keys: Number of entries of the array key descriptor pointer.
+ * @keys_desc: Pointer to an array of internal key descriptor structure pointer
+ * @nb_keys: Number of entries of the array key descriptor pointer
  * @mode_id: Cipher mode ID
- * @pub: Encryption arguments from the public API.
+ * @pub: Encryption arguments from the public API
  */
 struct smw_storage_enc_args {
 	struct smw_keymgr_descriptor **keys_desc;
@@ -35,8 +49,8 @@ struct smw_storage_enc_args {
 
 /**
  * struct smw_storage_data_descriptor - Data descriptor
- * @attributes: Data attributes.
- * @pub: Data descriptor from the public API.
+ * @attributes: Data attributes
+ * @pub: Data descriptor from the public API
  */
 struct smw_storage_data_descriptor {
 	struct smw_storage_data_attributes attributes;
@@ -58,9 +72,9 @@ struct smw_storage_sign_args {
 
 /**
  * struct smw_storage_store_data_args - Internal store data arguments structure
- * @data_descriptor: Data descriptor.
- * @enc_args: Encryption arguments.
- * @sign_args: Sign arguments.
+ * @data_descriptor: Data descriptor
+ * @enc_args: Encryption arguments
+ * @sign_args: Sign arguments
  */
 struct smw_storage_store_data_args {
 	struct smw_storage_data_descriptor data_descriptor;
@@ -70,7 +84,7 @@ struct smw_storage_store_data_args {
 
 /**
  * struct smw_storage_retrieve_data_args - Internal retrieve data arguments structure
- * @data_descriptor: Data descriptor.
+ * @data_descriptor: Data descriptor
  */
 struct smw_storage_retrieve_data_args {
 	struct smw_storage_data_descriptor data_descriptor;
@@ -78,7 +92,7 @@ struct smw_storage_retrieve_data_args {
 
 /**
  * struct smw_storage_delete_data_args - Internal delete data arguments structure
- * @data_descriptor: Data descriptor.
+ * @data_descriptor: Data descriptor
  */
 struct smw_storage_delete_data_args {
 	struct smw_storage_data_descriptor data_descriptor;
@@ -129,7 +143,7 @@ smw_storage_get_data_length(struct smw_storage_data_descriptor *descriptor);
 /**
  * smw_storage_set_data_length() - Set the length of the data.
  * @descriptor: Pointer to the internal data descriptor structure.
- * @length: Length of the data
+ * @length: Length of the data.
  *
  * This function sets the length of the data.
  * If the @descriptor field @pub is NULL, the function returns with no action.

core/storage/storage.c
@@ -11,6 +11,7 @@
 #include "exec.h"
 #include "tlv.h"
 #include "attr.h"
+#include "object_db.h"
 #include "storage.h"
 
 /**
@@ -65,6 +66,19 @@ static int store_write_only(void *attributes, unsigned char *value,
 static int store_lifecycle(void *attributes, unsigned char *value,
 			   unsigned int length);
 
+/**
+ * store_persistent() - Store persistent storage info.
+ * @attributes: Pointer to attribute structure to fill.
+ * @value: Unused.
+ * @length: Unused.
+ *
+ * Return:
+ * SMW_STATUS_OK		- Success.
+ * SMW_STATUS_INVALID_PARAM	- @attributes is NULL.
+ */
+static int store_persistent(void *attributes, unsigned char *value,
+			    unsigned int length);
+
 static const struct attribute_tlv data_attributes_tlv_array[] = {
 	{ .type = (const unsigned char *)READ_ONLY_STR,
 	  .verify = smw_tlv_verify_boolean,
@@ -77,7 +91,10 @@ static const struct attribute_tlv data_attributes_tlv_array[] = {
 	  .store = store_write_only },
 	{ .type = (const unsigned char *)LIFECYCLE_STR,
 	  .verify = smw_tlv_verify_variable_length_list,
-	  .store = store_lifecycle }
+	  .store = store_lifecycle },
+	{ .type = (const unsigned char *)PERSISTENT_STR,
+	  .verify = smw_tlv_verify_boolean,
+	  .store = store_persistent }
 };
 
 #define LIFECYCLE(_name)                                                       \
@@ -208,15 +225,38 @@ end:
 	return status;
 }
 
+static int store_persistent(void *attributes, unsigned char *value,
+			    unsigned int length)
+{
+	(void)value;
+	(void)length;
+
+	int status = SMW_STATUS_INVALID_PARAM;
+	struct smw_storage_data_attributes *attr = attributes;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (attr) {
+		attr->persistence_id = SMW_OBJECT_PERSISTENCE_ID_PERSISTENT;
+		status = SMW_STATUS_OK;
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
 static void
 set_default_attributes(struct smw_storage_data_attributes *data_attributes)
 {
 	data_attributes->rw_flags = 0;
 	data_attributes->lifecycle_flags = 0;
+	data_attributes->persistence_id = SMW_OBJECT_PERSISTENCE_ID_TRANSIENT;
 }
 
 static int convert_data_descriptor(struct smw_data_descriptor *in,
-				   struct smw_storage_data_descriptor *out)
+				   struct smw_storage_data_descriptor *out,
+				   enum subsystem_id *subsystem_id,
+				   union smw_object_db_info *info)
 {
 	int status = SMW_STATUS_OK;
 
@@ -229,9 +269,26 @@ static int convert_data_descriptor(struct smw_data_descriptor *in,
 		read_attributes(in->attributes_list, in->attributes_list_length,
 				&out->attributes, data_attributes_tlv_array,
 				ARRAY_SIZE(data_attributes_tlv_array));
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = smw_object_db_get_info(in->identifier,
+					out->attributes.persistence_id, info);
+	if (status == SMW_STATUS_OK) {
+		if (*subsystem_id != SUBSYSTEM_ID_INVALID &&
+		    info->data_info.subsystem_id != *subsystem_id) {
+			status = SMW_STATUS_INVALID_PARAM;
+			goto end;
+		}
+
+		*subsystem_id = info->data_info.subsystem_id;
+	} else if (status != SMW_STATUS_UNKNOWN_ID) {
+		goto end;
+	}
 
 	out->pub = in;
 
+end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }
@@ -318,7 +375,8 @@ end:
 static int
 store_data_convert_args(struct smw_store_data_args *args,
 			struct smw_storage_store_data_args *converted_args,
-			enum subsystem_id *subsystem_id)
+			enum subsystem_id *subsystem_id,
+			union smw_object_db_info *info)
 {
 	int status = SMW_STATUS_VERSION_NOT_SUPPORTED;
 
@@ -329,11 +387,6 @@ store_data_convert_args(struct smw_store_data_args *args,
 
 	status =
 		smw_config_get_subsystem_id(args->subsystem_name, subsystem_id);
-	if (status != SMW_STATUS_OK)
-		goto end;
-
-	status = convert_data_descriptor(args->data_descriptor,
-					 &converted_args->data_descriptor);
 	if (status != SMW_STATUS_OK)
 		goto end;
 
@@ -345,7 +398,12 @@ store_data_convert_args(struct smw_store_data_args *args,
 
 	status = convert_sign_args(args->sign_args, &converted_args->sign_args,
 				   *subsystem_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
 
+	status = convert_data_descriptor(args->data_descriptor,
+					 &converted_args->data_descriptor,
+					 subsystem_id, info);
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
@@ -354,7 +412,8 @@ end:
 static int
 retrieve_data_convert_args(struct smw_retrieve_data_args *args,
 			   struct smw_storage_retrieve_data_args *conv_args,
-			   enum subsystem_id *subsystem_id)
+			   enum subsystem_id *subsystem_id,
+			   union smw_object_db_info *info)
 {
 	int status = SMW_STATUS_VERSION_NOT_SUPPORTED;
 
@@ -369,7 +428,8 @@ retrieve_data_convert_args(struct smw_retrieve_data_args *args,
 		goto end;
 
 	status = convert_data_descriptor(args->data_descriptor,
-					 &conv_args->data_descriptor);
+					 &conv_args->data_descriptor,
+					 subsystem_id, info);
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
@@ -379,7 +439,8 @@ end:
 static int
 delete_data_convert_args(struct smw_delete_data_args *args,
 			 struct smw_storage_delete_data_args *converted_args,
-			 enum subsystem_id *subsystem_id)
+			 enum subsystem_id *subsystem_id,
+			 union smw_object_db_info *info)
 {
 	int status = SMW_STATUS_VERSION_NOT_SUPPORTED;
 
@@ -394,9 +455,8 @@ delete_data_convert_args(struct smw_delete_data_args *args,
 		goto end;
 
 	status = convert_data_descriptor(args->data_descriptor,
-					 &converted_args->data_descriptor);
-	if (status != SMW_STATUS_OK)
-		goto end;
+					 &converted_args->data_descriptor,
+					 subsystem_id, info);
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
@@ -468,8 +528,12 @@ smw_storage_get_iv_length(struct smw_storage_enc_args *enc_args)
 enum smw_status_code smw_store_data(struct smw_store_data_args *args)
 {
 	int status = SMW_STATUS_OK;
+
 	struct smw_storage_store_data_args store_data_args = { 0 };
 	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
+	union smw_object_db_info info = { 0 };
+	enum smw_object_persistence_id persistence_id =
+		SMW_OBJECT_PERSISTENCE_ID_TRANSIENT;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -484,12 +548,37 @@ enum smw_status_code smw_store_data(struct smw_store_data_args *args)
 		goto end;
 	}
 
-	status = store_data_convert_args(args, &store_data_args, &subsystem_id);
-	if (status != SMW_STATUS_OK)
+	status = store_data_convert_args(args, &store_data_args, &subsystem_id,
+					 &info);
+
+	persistence_id =
+		store_data_args.data_descriptor.attributes.persistence_id;
+
+	if (status == SMW_STATUS_OK) {
+		if (info.data_info.attributes.rw_flags &
+		    SMW_STORAGE_READ_ONLY) {
+			status = SMW_STATUS_INVALID_PARAM;
+			goto end;
+		}
+	} else if (status == SMW_STATUS_UNKNOWN_ID) {
+		status =
+			smw_object_db_create(&args->data_descriptor->identifier,
+					     persistence_id, &info);
+		if (status != SMW_STATUS_OK)
+			goto end;
+	} else {
 		goto end;
+	}
 
 	status = smw_utils_execute_operation(OPERATION_ID_STORAGE_STORE,
 					     &store_data_args, subsystem_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	info.data_info.size = store_data_args.data_descriptor.pub->length;
+	info.data_info.attributes = store_data_args.data_descriptor.attributes;
+	status = smw_object_db_update(args->data_descriptor->identifier,
+				      persistence_id, &info);
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
@@ -499,8 +588,14 @@ end:
 enum smw_status_code smw_retrieve_data(struct smw_retrieve_data_args *args)
 {
 	int status = SMW_STATUS_OK;
+	int tmp_status = SMW_STATUS_OK;
+
 	struct smw_storage_retrieve_data_args retrieve_data_args = { 0 };
 	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
+	union smw_object_db_info info = { 0 };
+	bool obj_not_present = false;
+	enum smw_object_persistence_id persistence_id =
+		SMW_OBJECT_PERSISTENCE_ID_TRANSIENT;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -515,12 +610,37 @@ enum smw_status_code smw_retrieve_data(struct smw_retrieve_data_args *args)
 	}
 
 	status = retrieve_data_convert_args(args, &retrieve_data_args,
-					    &subsystem_id);
-	if (status != SMW_STATUS_OK)
+					    &subsystem_id, &info);
+	if (status == SMW_STATUS_UNKNOWN_ID) {
+		if (subsystem_id == SUBSYSTEM_ID_INVALID)
+			goto end;
+
+		obj_not_present = true;
+	} else if (status != SMW_STATUS_OK) {
 		goto end;
+	}
 
 	status = smw_utils_execute_operation(OPERATION_ID_STORAGE_RETRIEVE,
 					     &retrieve_data_args, subsystem_id);
+	if (status != SMW_STATUS_OK && status != SMW_STATUS_UNKNOWN_ID)
+		goto end;
+
+	if (obj_not_present)
+		goto end;
+
+	persistence_id = info.data_info.attributes.persistence_id;
+
+	if (status != SMW_STATUS_OK ||
+	    info.data_info.attributes.rw_flags & SMW_STORAGE_READ_ONCE) {
+		tmp_status =
+			smw_object_db_delete(args->data_descriptor->identifier,
+					     persistence_id);
+
+		if (status == SMW_STATUS_OK ||
+		    info.data_info.attributes.persistence_id !=
+			    SMW_OBJECT_PERSISTENCE_ID_TRANSIENT)
+			status = tmp_status;
+	}
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
@@ -530,8 +650,13 @@ end:
 enum smw_status_code smw_delete_data(struct smw_delete_data_args *args)
 {
 	int status = SMW_STATUS_OK;
+	int tmp_status = SMW_STATUS_OK;
+	enum smw_object_persistence_id persistence_id =
+		SMW_OBJECT_PERSISTENCE_ID_TRANSIENT;
+
 	struct smw_storage_delete_data_args delete_data_args = { 0 };
 	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
+	union smw_object_db_info info = { 0 };
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -546,12 +671,24 @@ enum smw_status_code smw_delete_data(struct smw_delete_data_args *args)
 	}
 
 	status = delete_data_convert_args(args, &delete_data_args,
-					  &subsystem_id);
+					  &subsystem_id, &info);
 	if (status != SMW_STATUS_OK)
 		goto end;
 
+	persistence_id = info.data_info.attributes.persistence_id;
+
 	status = smw_utils_execute_operation(OPERATION_ID_STORAGE_DELETE,
 					     &delete_data_args, subsystem_id);
+	if (status != SMW_STATUS_OK && status != SMW_STATUS_UNKNOWN_ID)
+		goto end;
+
+	tmp_status = smw_object_db_delete(args->data_descriptor->identifier,
+					  persistence_id);
+
+	if (status == SMW_STATUS_OK ||
+	    info.data_info.attributes.persistence_id !=
+		    SMW_OBJECT_PERSISTENCE_ID_TRANSIENT)
+		status = tmp_status;
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);

core/inc/constants.h
@@ -8,6 +8,9 @@
 
 #include "builtin_macros.h"
 
+/* Define invalid object identifier */
+#define INVALID_OBJ_ID 0
+
 #define SMW_STORAGE_READ_ONLY  BIT(0)
 #define SMW_STORAGE_READ_ONCE  BIT(1)
 #define SMW_STORAGE_WRITE_ONLY BIT(2)

core/inc/keymgr.h
@@ -9,10 +9,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#include "constants.h"
 #include "config.h"
+#include "object.h"
 
 /* Define invalid key identifier */
-#define INVALID_KEY_ID 0
+#define INVALID_KEY_ID INVALID_OBJ_ID
 
 /* Default RSA public exponent is 65537, which has a length of 3 bytes */
 #define DEFAULT_RSA_PUB_EXP_LEN 3
@@ -32,15 +34,6 @@ enum smw_keymgr_format_id {
 	SMW_KEYMGR_FORMAT_ID_BASE64,
 	SMW_KEYMGR_FORMAT_ID_NB,
 	SMW_KEYMGR_FORMAT_ID_INVALID
-};
-
-enum smw_keymgr_persistence_id {
-	/* Key persistence */
-	SMW_KEYMGR_PERSISTENCE_ID_TRANSIENT,
-	SMW_KEYMGR_PERSISTENCE_ID_PERSISTENT,
-	SMW_KEYMGR_PERSISTENCE_ID_PERMANENT,
-	SMW_KEYMGR_PERSISTENCE_ID_NB,
-	SMW_KEYMGR_PERSISTENCE_ID_INVALID
 };
 
 /**
@@ -65,7 +58,7 @@ struct smw_keymgr_identifier {
 	unsigned int attribute;
 	unsigned int security_size;
 	uint32_t id;
-	enum smw_keymgr_persistence_id persistence_id;
+	enum smw_object_persistence_id persistence_id;
 	uint32_t storage_id;
 	uint16_t group;
 };
@@ -112,18 +105,18 @@ struct smw_keymgr_descriptor {
 
 /**
  * struct smw_keymgr_attributes - Key attributes list.
- * @persistence: Define key persistence.
- * @rsa_pub_exp: Pointer to rsa public exponent.
- * @rsa_pub_exp_len: @rsa_pub_exp length in bytes.
+ * @persistence_id: Key persistence ID
+ * @rsa_pub_exp: Pointer to rsa public exponent
+ * @rsa_pub_exp_len: @rsa_pub_exp length in bytes
  * @flush_key: Flush persistent key(s)
- * @policy: Key policy encoded as variable-length list TLV.
- * @policy_len: @policy length in bytes.
+ * @policy: Key policy encoded as variable-length list TLV
+ * @policy_len: @policy length in bytes
  * @pub_key_attributes_list: Key attributes list from the public API
  * @pub_key_attributes_list_length: Length of @pub_key_attributes_list
- * @storage_id: Key storage ID.
+ * @storage_id: Key storage ID
  */
 struct smw_keymgr_attributes {
-	enum smw_keymgr_persistence_id persistence;
+	enum smw_object_persistence_id persistence_id;
 	unsigned char *rsa_pub_exp;
 	unsigned int rsa_pub_exp_len;
 	bool flush_key;

core/inc/keymgr_db.h
@@ -13,8 +13,8 @@
  * @id: New key identifier created in the database
  * @identifier: Internal Key identifier object
  *
- * Function creates a new key in the OSAL Key database. The
- * given @identifier is stored in the key entry.
+ * Function creates a new key in the OSAL object database. The
+ * given @identifier is stored in the object entry.
  * Objective of the function is to ensure that key can be
  * created in the database before creating/importing key in
  * the subsystem.

core/inc/object.h
@@ -1,0 +1,30 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef __OBJECT_H__
+#define __OBJECT_H__
+
+enum smw_object_persistence_id {
+	/* Object persistence */
+	SMW_OBJECT_PERSISTENCE_ID_TRANSIENT,
+	SMW_OBJECT_PERSISTENCE_ID_PERSISTENT,
+	SMW_OBJECT_PERSISTENCE_ID_PERMANENT,
+	SMW_OBJECT_PERSISTENCE_ID_NB,
+	SMW_OBJECT_PERSISTENCE_ID_INVALID
+};
+
+/**
+ * smw_object_get_persistence_name() - Get the persistence name.
+ * @persistence_id: Persistence ID.
+ *
+ * This function gets the name of a persistence.
+ *
+ * Return:
+ * pointer to the string that is the persistence name.
+ */
+const char *
+smw_object_get_persistence_name(enum smw_object_persistence_id persistence_id);
+
+#endif /* __OBJECT_H__ */

core/inc/object_db.h
@@ -1,0 +1,86 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef __OBJECT_DB_H__
+#define __OBJECT_DB_H__
+
+#include "object.h"
+#include "keymgr.h"
+#include "storage.h"
+
+union smw_object_db_info {
+	struct smw_keymgr_identifier key_identifier;
+	struct smw_storage_data_info data_info;
+};
+
+/**
+ * smw_object_db_create() - Create an object in the database
+ * @id: New object identifier created in the database
+ * @persistence_id: Object persistence ID
+ * @info: Object information
+ *
+ * Function creates a new object in the OSAL object database. The
+ * given @info is stored in the object entry.
+ * Objective of the function is to ensure that the object can be
+ * created in the database before storing the obecjt in the
+ * subsystem.
+ *
+ * Return:
+ * SMW_STATUS_OK                - Success
+ * SMW_STATUS_OPS_INVALID       - OSAL operation invalid
+ * SMW_STATUS_OBJ_DB_CREATE     - Object creation error
+ */
+int smw_object_db_create(unsigned int *id,
+			 enum smw_object_persistence_id persistence_id,
+			 union smw_object_db_info *info);
+
+/**
+ * smw_object_db_update() - Update an object in the database
+ * @id: Object identifier to update in the database
+ * @persistence_id: Object persistence ID
+ * @info: Object information
+ *
+ * Function updates an object in the database. The given @identifier
+ * replaces the key entry's data.
+ *
+ * Return:
+ * SMW_STATUS_OK                - Success
+ * SMW_STATUS_OPS_INVALID       - OSAL operation invalid
+ * SMW_STATUS_OBJ_DB_UPDATE     - Object update error
+ */
+int smw_object_db_update(unsigned int id,
+			 enum smw_object_persistence_id persistence_id,
+			 union smw_object_db_info *info);
+
+/**
+ * smw_object_db_delete() - Delete an object in the database
+ * @id: Object identifier to delete in the database
+ * @persistence_id: Object persistence ID
+ *
+ * Return:
+ * SMW_STATUS_OK                - Success
+ * SMW_STATUS_OPS_INVALID       - OSAL operation invalid
+ * SMW_STATUS_OBJ_DB_DELETE     - Object delete error
+ */
+int smw_object_db_delete(unsigned int id,
+			 enum smw_object_persistence_id persistence_id);
+
+/**
+ * smw_object_db_get_info() - Retrieve the object information from the database
+ * @id: Object identifier in the database
+ * @persistence_id: Object persistence ID
+ * @info: Object information
+ *
+ * Return:
+ * SMW_STATUS_OK                - Success
+ * SMW_STATUS_OPS_INVALID       - OSAL operation invalid
+ * SMW_STATUS_OBJ_DB_GET_INFO   - Object get information error
+ * SMW_STATUS_UNKNOWN_ID        - Object ID is unknown
+ */
+int smw_object_db_get_info(unsigned int id,
+			   enum smw_object_persistence_id persistence_id,
+			   union smw_object_db_info *info);
+
+#endif /* __OBJECT_DB_H__ */

core/inc/utils.h
@@ -16,6 +16,7 @@
 #include "builtin_macros.h"
 
 #include "global.h"
+#include "debug.h"
 
 #define SMW_ALL_ONES (-1UL)
 

commit 084c86fcbd28bdb97d099428f2b9c25718e0e84d

SSMW-499 core: storage: Fix object database
Store the ID of the subsystem used to store data
in the object database.

Fixes: 8bd9311a7765 ("SSMW-266 core: OSAL key data base becomes OSAL object database")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/inc/storage.h
@@ -49,10 +49,12 @@ struct smw_storage_enc_args {
 
 /**
  * struct smw_storage_data_descriptor - Data descriptor
+ * @subsystem_id: Secure Subsystem ID
  * @attributes: Data attributes
  * @pub: Data descriptor from the public API
  */
 struct smw_storage_data_descriptor {
+	enum subsystem_id subsystem_id;
 	struct smw_storage_data_attributes attributes;
 	struct smw_data_descriptor *pub;
 };

core/storage/storage.c
@@ -539,6 +539,8 @@ enum smw_status_code smw_store_data(struct smw_store_data_args *args)
 	if (status != SMW_STATUS_OK)
 		goto end;
 
+	info.data_info.subsystem_id =
+		store_data_args.data_descriptor.subsystem_id;
 	info.data_info.size = store_data_args.data_descriptor.pub->length;
 	info.data_info.attributes = store_data_args.data_descriptor.attributes;
 	status = smw_object_db_update(args->data_descriptor->identifier,
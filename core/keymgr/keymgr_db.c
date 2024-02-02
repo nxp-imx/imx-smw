nxp-imx/imx-smw

commit 8bd9311a776579b6a863e8a5ffc8de089b42dac1

SSMW-266 core: OSAL key data base becomes OSAL object database
- Use new SMW OSAL operations related to database
- Implement smw_object_db_xxx() APIs
- smw_keybmgr_db_xxx() use smw_object_db_xxx() APIs
- Remove useless param of smw_keymgr_db_delete()

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/keymgr/keymgr_db.c
@@ -5,121 +5,48 @@
 
 #include "smw_status.h"
 
-#include "debug.h"
-#include "global.h"
-#include "keymgr_db.h"
-
-static void prepare_osal_key(struct smw_keymgr_identifier *identifier,
-			     struct osal_key *key)
-{
-	key->id = identifier->id;
-	key->persistent = identifier->persistence_id;
-	key->info = identifier;
-	key->info_size = sizeof(*identifier);
-
-	/* If key id is known, there is no key id range */
-	if (key->id == INVALID_KEY_ID) {
-		key->range.min = 1;
-		key->range.max = UINT32_MAX;
-	} else {
-		key->range.min = key->id;
-		key->range.max = key->id;
-	}
-}
+#include "utils.h"
+#include "keymgr.h"
+#include "object_db.h"
 
 int smw_keymgr_db_create(unsigned int *id,
 			 struct smw_keymgr_identifier *identifier)
 {
-	int ret = SMW_STATUS_KEY_DB_CREATE;
-	struct smw_ops *ops = get_smw_ops();
-	struct osal_key key = { 0 };
+	union smw_object_db_info info = { 0 };
 
-	SMW_DBG_TRACE_FUNCTION_CALL;
+	*id = identifier->id;
+	SMW_UTILS_MEMCPY(&info, identifier, sizeof(*identifier));
 
-	if (!id || !identifier)
-		return SMW_STATUS_INVALID_PARAM;
-
-	if (!ops || !ops->add_key_info)
-		return SMW_STATUS_OPS_INVALID;
-
-	prepare_osal_key(identifier, &key);
-
-	if (!ops->add_key_info(&key) && key.id != INVALID_KEY_ID) {
-		*id = key.id;
-		ret = SMW_STATUS_OK;
-	}
-
-	return ret;
+	return smw_object_db_create(id, identifier->persistence_id, &info);
 }
 
 int smw_keymgr_db_update(unsigned int id,
 			 struct smw_keymgr_identifier *identifier)
 {
-	int ret = SMW_STATUS_KEY_DB_UPDATE;
-	struct smw_ops *ops = get_smw_ops();
-	struct osal_key key = { 0 };
+	union smw_object_db_info info = { 0 };
 
-	SMW_DBG_TRACE_FUNCTION_CALL;
+	SMW_UTILS_MEMCPY(&info, identifier, sizeof(*identifier));
 
-	if (!identifier)
-		return SMW_STATUS_INVALID_PARAM;
-
-	if (!ops || !ops->update_key_info)
-		return SMW_STATUS_OPS_INVALID;
-
-	prepare_osal_key(identifier, &key);
-	key.id = id;
-
-	if (!ops->update_key_info(&key))
-		ret = SMW_STATUS_OK;
-
-	return ret;
+	return smw_object_db_update(id, identifier->persistence_id, &info);
 }
 
 int smw_keymgr_db_delete(unsigned int id,
 			 struct smw_keymgr_identifier *identifier)
 
 {
-	int ret = SMW_STATUS_KEY_DB_DELETE;
-	struct smw_ops *ops = get_smw_ops();
-	struct osal_key key = { 0 };
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	if (!ops || !ops->delete_key_info)
-		return SMW_STATUS_OPS_INVALID;
-
-	prepare_osal_key(identifier, &key);
-	key.id = id;
-
-	if (!ops->delete_key_info(&key))
-		ret = SMW_STATUS_OK;
-
-	return ret;
+	return smw_object_db_delete(id, identifier->persistence_id);
 }
 
 int smw_keymgr_db_get_info(unsigned int id,
 			   struct smw_keymgr_identifier *identifier)
 {
-	int ret = SMW_STATUS_KEY_DB_GET_INFO;
-	struct smw_ops *ops = get_smw_ops();
-	struct osal_key key = { 0 };
+	int ret = SMW_STATUS_OK;
+	union smw_object_db_info info = { 0 };
 
-	SMW_DBG_TRACE_FUNCTION_CALL;
+	ret = smw_object_db_get_info(id, identifier->persistence_id, &info);
 
-	if (!identifier)
-		return SMW_STATUS_INVALID_PARAM;
-
-	if (!ops || !ops->get_key_info)
-		return SMW_STATUS_OPS_INVALID;
-
-	prepare_osal_key(identifier, &key);
-	key.id = id;
-
-	if (!ops->get_key_info(&key))
-		ret = SMW_STATUS_OK;
-	else if (key.id == INVALID_KEY_ID)
-		ret = SMW_STATUS_UNKNOWN_ID;
+	if (ret == SMW_STATUS_OK)
+		SMW_UTILS_MEMCPY(identifier, &info, sizeof(*identifier));
 
 	return ret;
 }

core/inc/constants.h
@@ -8,6 +8,9 @@
 
 #include "builtin_macros.h"
 
+/* Define invalid object identifier */
+#define INVALID_OBJ_ID 0
+
 #define SMW_STORAGE_READ_ONLY  BIT(0)
 #define SMW_STORAGE_READ_ONCE  BIT(1)
 #define SMW_STORAGE_WRITE_ONLY BIT(2)

core/inc/keymgr.h
@@ -9,10 +9,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#include "constants.h"
 #include "config.h"
+#include "object.h"
 
 /* Define invalid key identifier */
-#define INVALID_KEY_ID 0
+#define INVALID_KEY_ID INVALID_OBJ_ID
 
 /* Default RSA public exponent is 65537, which has a length of 3 bytes */
 #define DEFAULT_RSA_PUB_EXP_LEN 3
@@ -32,15 +34,6 @@ enum smw_keymgr_format_id {
 	SMW_KEYMGR_FORMAT_ID_BASE64,
 	SMW_KEYMGR_FORMAT_ID_NB,
 	SMW_KEYMGR_FORMAT_ID_INVALID
-};
-
-enum smw_keymgr_persistence_id {
-	/* Key persistence */
-	SMW_KEYMGR_PERSISTENCE_ID_TRANSIENT,
-	SMW_KEYMGR_PERSISTENCE_ID_PERSISTENT,
-	SMW_KEYMGR_PERSISTENCE_ID_PERMANENT,
-	SMW_KEYMGR_PERSISTENCE_ID_NB,
-	SMW_KEYMGR_PERSISTENCE_ID_INVALID
 };
 
 /**
@@ -65,7 +58,7 @@ struct smw_keymgr_identifier {
 	unsigned int attribute;
 	unsigned int security_size;
 	uint32_t id;
-	enum smw_keymgr_persistence_id persistence_id;
+	enum smw_object_persistence_id persistence_id;
 	uint32_t storage_id;
 	uint16_t group;
 };
@@ -112,18 +105,18 @@ struct smw_keymgr_descriptor {
 
 /**
  * struct smw_keymgr_attributes - Key attributes list.
- * @persistence: Define key persistence.
- * @rsa_pub_exp: Pointer to rsa public exponent.
- * @rsa_pub_exp_len: @rsa_pub_exp length in bytes.
+ * @persistence_id: Key persistence ID
+ * @rsa_pub_exp: Pointer to rsa public exponent
+ * @rsa_pub_exp_len: @rsa_pub_exp length in bytes
  * @flush_key: Flush persistent key(s)
- * @policy: Key policy encoded as variable-length list TLV.
- * @policy_len: @policy length in bytes.
+ * @policy: Key policy encoded as variable-length list TLV
+ * @policy_len: @policy length in bytes
  * @pub_key_attributes_list: Key attributes list from the public API
  * @pub_key_attributes_list_length: Length of @pub_key_attributes_list
- * @storage_id: Key storage ID.
+ * @storage_id: Key storage ID
  */
 struct smw_keymgr_attributes {
-	enum smw_keymgr_persistence_id persistence;
+	enum smw_object_persistence_id persistence_id;
 	unsigned char *rsa_pub_exp;
 	unsigned int rsa_pub_exp_len;
 	bool flush_key;

core/inc/keymgr_db.h
@@ -13,8 +13,8 @@
  * @id: New key identifier created in the database
  * @identifier: Internal Key identifier object
  *
- * Function creates a new key in the OSAL Key database. The
- * given @identifier is stored in the key entry.
+ * Function creates a new key in the OSAL object database. The
+ * given @identifier is stored in the object entry.
  * Objective of the function is to ensure that key can be
  * created in the database before creating/importing key in
  * the subsystem.

core/inc/object.h
@@ -1,0 +1,30 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef __OBJECT_H__
+#define __OBJECT_H__
+
+enum smw_object_persistence_id {
+	/* Object persistence */
+	SMW_OBJECT_PERSISTENCE_ID_TRANSIENT,
+	SMW_OBJECT_PERSISTENCE_ID_PERSISTENT,
+	SMW_OBJECT_PERSISTENCE_ID_PERMANENT,
+	SMW_OBJECT_PERSISTENCE_ID_NB,
+	SMW_OBJECT_PERSISTENCE_ID_INVALID
+};
+
+/**
+ * smw_object_get_persistence_name() - Get the persistence name.
+ * @persistence_id: Persistence ID.
+ *
+ * This function gets the name of a persistence.
+ *
+ * Return:
+ * pointer to the string that is the persistence name.
+ */
+const char *
+smw_object_get_persistence_name(enum smw_object_persistence_id persistence_id);
+
+#endif /* __OBJECT_H__ */

core/inc/object_db.h
@@ -1,0 +1,86 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef __OBJECT_DB_H__
+#define __OBJECT_DB_H__
+
+#include "object.h"
+#include "keymgr.h"
+#include "storage.h"
+
+union smw_object_db_info {
+	struct smw_keymgr_identifier key_identifier;
+	struct smw_storage_data_info data_info;
+};
+
+/**
+ * smw_object_db_create() - Create an object in the database
+ * @id: New object identifier created in the database
+ * @persistence_id: Object persistence ID
+ * @info: Object information
+ *
+ * Function creates a new object in the OSAL object database. The
+ * given @info is stored in the object entry.
+ * Objective of the function is to ensure that the object can be
+ * created in the database before storing the obecjt in the
+ * subsystem.
+ *
+ * Return:
+ * SMW_STATUS_OK                - Success
+ * SMW_STATUS_OPS_INVALID       - OSAL operation invalid
+ * SMW_STATUS_OBJ_DB_CREATE     - Object creation error
+ */
+int smw_object_db_create(unsigned int *id,
+			 enum smw_object_persistence_id persistence_id,
+			 union smw_object_db_info *info);
+
+/**
+ * smw_object_db_update() - Update an object in the database
+ * @id: Object identifier to update in the database
+ * @persistence_id: Object persistence ID
+ * @info: Object information
+ *
+ * Function updates an object in the database. The given @identifier
+ * replaces the key entry's data.
+ *
+ * Return:
+ * SMW_STATUS_OK                - Success
+ * SMW_STATUS_OPS_INVALID       - OSAL operation invalid
+ * SMW_STATUS_OBJ_DB_UPDATE     - Object update error
+ */
+int smw_object_db_update(unsigned int id,
+			 enum smw_object_persistence_id persistence_id,
+			 union smw_object_db_info *info);
+
+/**
+ * smw_object_db_delete() - Delete an object in the database
+ * @id: Object identifier to delete in the database
+ * @persistence_id: Object persistence ID
+ *
+ * Return:
+ * SMW_STATUS_OK                - Success
+ * SMW_STATUS_OPS_INVALID       - OSAL operation invalid
+ * SMW_STATUS_OBJ_DB_DELETE     - Object delete error
+ */
+int smw_object_db_delete(unsigned int id,
+			 enum smw_object_persistence_id persistence_id);
+
+/**
+ * smw_object_db_get_info() - Retrieve the object information from the database
+ * @id: Object identifier in the database
+ * @persistence_id: Object persistence ID
+ * @info: Object information
+ *
+ * Return:
+ * SMW_STATUS_OK                - Success
+ * SMW_STATUS_OPS_INVALID       - OSAL operation invalid
+ * SMW_STATUS_OBJ_DB_GET_INFO   - Object get information error
+ * SMW_STATUS_UNKNOWN_ID        - Object ID is unknown
+ */
+int smw_object_db_get_info(unsigned int id,
+			   enum smw_object_persistence_id persistence_id,
+			   union smw_object_db_info *info);
+
+#endif /* __OBJECT_DB_H__ */

core/inc/storage.h
@@ -11,20 +11,34 @@
 
 /**
  * struct smw_storage_data_attributes - Storage attributes.
- * @rw_flags: Data access flags.
- * @lifecycle_flags: Device lifecycles where data is accessible.
+ * @rw_flags: Data access flags
+ * @lifecycle_flags: Device lifecycles where data is accessible
+ * @persistence_id: Persistence ID
  */
 struct smw_storage_data_attributes {
 	unsigned long rw_flags;
 	unsigned long lifecycle_flags;
+	enum smw_object_persistence_id persistence_id;
+};
+
+/**
+ * struct smw_storage_data_info - Data information stored in object database.
+ * @subsystem_id: Secure Subsystem ID
+ * @size: Data size
+ * @attributes: Data attributes
+ */
+struct smw_storage_data_info {
+	enum subsystem_id subsystem_id;
+	unsigned int size;
+	struct smw_storage_data_attributes attributes;
 };
 
 /**
  * struct smw_storage_enc_args - Encryption arguments.
- * @keys_desc: Pointer to an array of internal key descriptor structure pointer.
- * @nb_keys: Number of entries of the array key descriptor pointer.
+ * @keys_desc: Pointer to an array of internal key descriptor structure pointer
+ * @nb_keys: Number of entries of the array key descriptor pointer
  * @mode_id: Cipher mode ID
- * @pub: Encryption arguments from the public API.
+ * @pub: Encryption arguments from the public API
  */
 struct smw_storage_enc_args {
 	struct smw_keymgr_descriptor **keys_desc;
@@ -35,8 +49,8 @@ struct smw_storage_enc_args {
 
 /**
  * struct smw_storage_data_descriptor - Data descriptor
- * @attributes: Data attributes.
- * @pub: Data descriptor from the public API.
+ * @attributes: Data attributes
+ * @pub: Data descriptor from the public API
  */
 struct smw_storage_data_descriptor {
 	struct smw_storage_data_attributes attributes;
@@ -58,9 +72,9 @@ struct smw_storage_sign_args {
 
 /**
  * struct smw_storage_store_data_args - Internal store data arguments structure
- * @data_descriptor: Data descriptor.
- * @enc_args: Encryption arguments.
- * @sign_args: Sign arguments.
+ * @data_descriptor: Data descriptor
+ * @enc_args: Encryption arguments
+ * @sign_args: Sign arguments
  */
 struct smw_storage_store_data_args {
 	struct smw_storage_data_descriptor data_descriptor;
@@ -70,7 +84,7 @@ struct smw_storage_store_data_args {
 
 /**
  * struct smw_storage_retrieve_data_args - Internal retrieve data arguments structure
- * @data_descriptor: Data descriptor.
+ * @data_descriptor: Data descriptor
  */
 struct smw_storage_retrieve_data_args {
 	struct smw_storage_data_descriptor data_descriptor;
@@ -78,7 +92,7 @@ struct smw_storage_retrieve_data_args {
 
 /**
  * struct smw_storage_delete_data_args - Internal delete data arguments structure
- * @data_descriptor: Data descriptor.
+ * @data_descriptor: Data descriptor
  */
 struct smw_storage_delete_data_args {
 	struct smw_storage_data_descriptor data_descriptor;
@@ -129,7 +143,7 @@ smw_storage_get_data_length(struct smw_storage_data_descriptor *descriptor);
 /**
  * smw_storage_set_data_length() - Set the length of the data.
  * @descriptor: Pointer to the internal data descriptor structure.
- * @length: Length of the data
+ * @length: Length of the data.
  *
  * This function sets the length of the data.
  * If the @descriptor field @pub is NULL, the function returns with no action.

core/inc/utils.h
@@ -16,6 +16,7 @@
 #include "builtin_macros.h"
 
 #include "global.h"
+#include "debug.h"
 
 #define SMW_ALL_ONES (-1UL)
 

commit f9ffe9b64c2a15fecd1dcd24ee43aa94d7672033

SSMW-531 core: keymgr: Rework key info storage in key database
Remove unnecessary calls to SMW_UTILS_MEMCPY().

Fixes: 8bd9311a7765 ("SSMW-266 core: OSAL key data base becomes OSAL object database")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/keymgr/keymgr_db.c
@@ -15,7 +15,7 @@ int smw_keymgr_db_create(unsigned int *id,
 	union smw_object_db_info info = { 0 };
 
 	*id = identifier->id;
-	SMW_UTILS_MEMCPY(&info, identifier, sizeof(*identifier));
+	info.key_identifier = *identifier;
 
 	return smw_object_db_create(id, identifier->persistence_id, &info);
 }
@@ -25,7 +25,7 @@ int smw_keymgr_db_update(unsigned int id,
 {
 	union smw_object_db_info info = { 0 };
 
-	SMW_UTILS_MEMCPY(&info, identifier, sizeof(*identifier));
+	info.key_identifier = *identifier;
 
 	return smw_object_db_update(id, identifier->persistence_id, &info);
 }
@@ -46,7 +46,7 @@ int smw_keymgr_db_get_info(unsigned int id,
 	ret = smw_object_db_get_info(id, identifier->persistence_id, &info);
 
 	if (ret == SMW_STATUS_OK)
-		SMW_UTILS_MEMCPY(identifier, &info, sizeof(*identifier));
+		*identifier = info.key_identifier;
 
 	return ret;
 }
nxp-imx/imx-smw

commit 6d44fca17f31740f40d9dbc50cd56ca3c3b30cfc

SSMW-349 core: devmgr: Add device manager module
Add a device manager module implementing the device attestation
operation.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/devmgr/attestation.c
@@ -1,0 +1,74 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ */
+
+#include "smw_device.h"
+
+#include "subsystems.h"
+
+#include "config.h"
+#include "devmgr.h"
+#include "debug.h"
+#include "exec.h"
+
+unsigned char *smw_devmgr_get_challenge_data(struct smw_devmgr_args *args)
+{
+	unsigned char *buffer = NULL;
+
+	if (args->op == SMW_OP_DEVMGR_ATTESTATION && args->pub.attestation)
+		buffer = args->pub.attestation->challenge;
+
+	return buffer;
+}
+
+unsigned int smw_devmgr_get_challenge_length(struct smw_devmgr_args *args)
+{
+	unsigned int length = 0;
+
+	if (args->op == SMW_OP_DEVMGR_ATTESTATION && args->pub.attestation)
+		length = args->pub.attestation->challenge_length;
+
+	return length;
+}
+
+void smw_devmgr_set_certificate_length(struct smw_devmgr_args *args,
+				       unsigned int length)
+{
+	if (args->op == SMW_OP_DEVMGR_ATTESTATION && args->pub.attestation)
+		args->pub.attestation->certificate_length = length;
+}
+
+enum smw_status_code
+smw_device_attestation(struct smw_device_attestation_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_devmgr_args attest_args = { 0 };
+	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!args)
+		goto end;
+
+	if (args->version != 0) {
+		status = SMW_STATUS_INVALID_VERSION;
+		goto end;
+	}
+
+	status = smw_config_get_subsystem_id(args->subsystem_name,
+					     &subsystem_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	attest_args.op = SMW_OP_DEVMGR_ATTESTATION;
+	attest_args.pub.attestation = args;
+
+	status = smw_utils_execute_operation(OPERATION_ID_DEVICE_ATTESTATION,
+					     &attest_args, subsystem_id);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}

core/devmgr/devmgr.c
@@ -1,0 +1,117 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ */
+
+#include "smw_device.h"
+
+#include "subsystems.h"
+
+#include "config.h"
+#include "debug.h"
+#include "devmgr.h"
+#include "exec.h"
+
+unsigned char *smw_devmgr_get_certificate_data(struct smw_devmgr_args *args)
+{
+	unsigned char *buffer = NULL;
+
+	switch (args->op) {
+	case SMW_OP_DEVMGR_ATTESTATION:
+		if (args->pub.attestation)
+			buffer = args->pub.attestation->certificate;
+		break;
+
+	case SMW_OP_DEVMGR_UUID:
+		if (args->pub.uuid)
+			buffer = args->pub.uuid->certificate;
+		break;
+
+	default:
+		break;
+	}
+
+	return buffer;
+}
+
+unsigned int smw_devmgr_get_certificate_length(struct smw_devmgr_args *args)
+{
+	unsigned int length = 0;
+
+	switch (args->op) {
+	case SMW_OP_DEVMGR_ATTESTATION:
+		if (args->pub.attestation)
+			length = args->pub.attestation->certificate_length;
+		break;
+
+	case SMW_OP_DEVMGR_UUID:
+		if (args->pub.uuid)
+			length = args->pub.uuid->certificate_length;
+		break;
+
+	default:
+		break;
+	}
+
+	return length;
+}
+
+unsigned char *smw_devmgr_get_uuid_data(struct smw_devmgr_args *args)
+{
+	unsigned char *buffer = NULL;
+
+	if (args->op == SMW_OP_DEVMGR_UUID && args->pub.uuid)
+		buffer = args->pub.uuid->uuid;
+
+	return buffer;
+}
+
+unsigned int smw_devmgr_get_uuid_length(struct smw_devmgr_args *args)
+{
+	unsigned int length = 0;
+
+	if (args->op == SMW_OP_DEVMGR_UUID && args->pub.uuid)
+		length = args->pub.uuid->uuid_length;
+
+	return length;
+}
+
+void smw_devmgr_set_uuid_length(struct smw_devmgr_args *args,
+				unsigned int length)
+{
+	if (args->op == SMW_OP_DEVMGR_UUID && args->pub.uuid)
+		args->pub.uuid->uuid_length = length;
+}
+
+enum smw_status_code smw_device_get_uuid(struct smw_device_uuid_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_devmgr_args uuid_args = { 0 };
+	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!args)
+		goto end;
+
+	if (args->version != 0) {
+		status = SMW_STATUS_INVALID_VERSION;
+		goto end;
+	}
+
+	status = smw_config_get_subsystem_id(args->subsystem_name,
+					     &subsystem_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	uuid_args.op = SMW_OP_DEVMGR_UUID;
+	uuid_args.pub.uuid = args;
+
+	status = smw_utils_execute_implicit(OPERATION_ID_DEVICE_GET_UUID,
+					    &uuid_args, subsystem_id);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}

core/CMakeLists.txt
@@ -97,6 +97,7 @@ add_subdirectory(subsystems/tee)
 add_subdirectory(keymgr)
 add_subdirectory(crypto)
 add_subdirectory(psa)
+add_subdirectory(devmgr)
 
 get_property(LIST_SUBSYSTEM_ID GLOBAL PROPERTY PROP_SUBSYSTEM_ID)
 get_property(LIST_SUBSYSTEM_EXPORT GLOBAL PROPERTY PROP_SUBSYSTEM_EXPORT)

core/devmgr/CMakeLists.txt
@@ -1,0 +1,19 @@
+set(OBJ_LIST ${OBJ_LIST} $<TARGET_OBJECTS:devmgr> PARENT_SCOPE)
+
+
+if(ENABLE_DEVMGR_MODULE)
+   add_operation_implicit(DEVICE_GET_UUID)
+endif()
+
+if(ENABLE_DEVICE_ATTESTATION)
+   add_operation(DEVICE_ATTESTATION)
+endif()
+
+add_library(devmgr OBJECT "")
+target_sources(devmgr PRIVATE
+               devmgr_weak.c
+               $<$<BOOL:${ENABLE_DEVMGR_MODULE}>:devmgr.c>
+               $<$<BOOL:${ENABLE_DEVICE_ATTESTATION}>:attestation.c>)
+
+target_include_directories(devmgr PRIVATE ${INCLUDE_DIR})
+

core/devmgr/devmgr_weak.c
@@ -1,0 +1,43 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ */
+
+#include "smw_device.h"
+
+#include "compiler.h"
+#include "debug.h"
+#include "devmgr.h"
+
+__weak unsigned char *
+smw_devmgr_get_challenge_data(struct smw_devmgr_args *args)
+{
+	(void)args;
+
+	return NULL;
+}
+
+__weak unsigned int
+smw_devmgr_get_challenge_length(struct smw_devmgr_args *args)
+{
+	(void)args;
+
+	return 0;
+}
+
+__weak void smw_devmgr_set_certificate_length(struct smw_devmgr_args *args,
+					      unsigned int length)
+{
+	(void)args;
+	(void)length;
+}
+
+__weak enum smw_status_code
+smw_device_attestation(struct smw_device_attestation_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}

core/inc/devmgr.h
@@ -1,0 +1,129 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef __DEVMGR_H___
+#define __DEVMGR_H___
+
+#include "smw_device.h"
+
+enum smw_op_devmgr { SMW_OP_DEVMGR_ATTESTATION, SMW_OP_DEVMGR_UUID };
+
+/**
+ * struct smw_devmgr_args - Device manager arguments
+ * @op: Device manager operation
+ * @pub: Pointer to the public API arguments structure
+ *
+ */
+struct smw_devmgr_args {
+	enum smw_op_devmgr op;
+	union {
+		struct smw_device_attestation_args *attestation;
+		struct smw_device_uuid_args *uuid;
+	} pub;
+};
+
+/**
+ * smw_devmgr_get_challenge_data() - Return the address of the challenge
+ *                                   buffer.
+ * @args: Pointer to the internal device args structure.
+ *
+ * This function returns the address of the challenge buffer.
+ *
+ * Return:
+ * NULL
+ * address of the challenge buffer
+ */
+unsigned char *smw_devmgr_get_challenge_data(struct smw_devmgr_args *args);
+
+/**
+ * smw_devmgr_get_challenge_length() - Return the length of the challenge
+ *                                     buffer.
+ * @args: Pointer to the internal device args structure.
+ *
+ * This function returns the length of the challenge buffer.
+ *
+ * Return:
+ * 0
+ * length of the challenge buffer.
+ */
+unsigned int smw_devmgr_get_challenge_length(struct smw_devmgr_args *args);
+
+/**
+ * smw_devmgr_get_certificate_data() - Return the address of the certificate
+ *                                     buffer.
+ * @args: Pointer to the internal device args structure.
+ *
+ * This function returns the address of the certificate buffer.
+ *
+ * Return:
+ * NULL
+ * address of the certificate buffer
+ */
+unsigned char *smw_devmgr_get_certificate_data(struct smw_devmgr_args *args);
+
+/**
+ * smw_devmgr_get_certificate_length() - Return the length of the certificate
+ *                                       buffer.
+ * @args: Pointer to the internal device args structure.
+ *
+ * This function returns the length of the certificate buffer.
+ *
+ * Return:
+ * 0
+ * length of the certificate buffer.
+ */
+unsigned int smw_devmgr_get_certificate_length(struct smw_devmgr_args *args);
+
+/**
+ * smw_devmgr_set_certificate_length() - Set the length of the certificate
+ *                                       buffer.
+ * @args: Pointer to the internal device args structure.
+ * @length: Length of the certificate buffer.
+ *
+ * This function sets the length of the certificate buffer.
+ *
+ * Return:
+ * none.
+ */
+void smw_devmgr_set_certificate_length(struct smw_devmgr_args *args,
+				       unsigned int length);
+
+/**
+ * smw_devmgr_get_uuid_data() - Return the address of the device UUID buffer.
+ * @args: Pointer to the internal device args structure.
+ *
+ * This function returns the address of the device UUID buffer.
+ *
+ * Return:
+ * NULL
+ * address of the device UUID buffer
+ */
+unsigned char *smw_devmgr_get_uuid_data(struct smw_devmgr_args *args);
+
+/**
+ * smw_devmgr_get_uuid_length() - Return the length of the device UUID buffer.
+ * @args: Pointer to the internal device args structure.
+ *
+ * This function returns the length of the device UUID buffer.
+ *
+ * Return:
+ * 0
+ * length of the device UUID buffer.
+ */
+unsigned int smw_devmgr_get_uuid_length(struct smw_devmgr_args *args);
+
+/**
+ * smw_devmgr_set_uuid_length() - Set the length of the device UUID buffer.
+ * @args: Pointer to the internal device args structure.
+ * @length: Length of the device UUID buffer.
+ *
+ * This function sets the length of the device UUID buffer.
+ *
+ * Return:
+ * none.
+ */
+void smw_devmgr_set_uuid_length(struct smw_devmgr_args *args,
+				unsigned int length);
+#endif /* __DEVMGR_H___ */

commit 401a34de54fd2d08e905e0f29fd5a3360baee056

SSMW-542 core: devmgr: Return correct status code
If the arguments version is not supported,
the device manager returns SMW_STATUS_VERSION_NOT_SUPPORTED.

Fixes: 6d44fca17f31 ("SSMW-349 core: devmgr: Add device manager module")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/devmgr/attestation.c
@@ -53,7 +53,7 @@ smw_device_attestation(struct smw_device_attestation_args *args)
 		goto end;
 
 	if (args->version != 0) {
-		status = SMW_STATUS_INVALID_VERSION;
+		status = SMW_STATUS_VERSION_NOT_SUPPORTED;
 		goto end;
 	}
 

core/devmgr/devmgr.c
@@ -96,7 +96,7 @@ enum smw_status_code smw_device_get_uuid(struct smw_device_uuid_args *args)
 		goto end;
 
 	if (args->version != 0) {
-		status = SMW_STATUS_INVALID_VERSION;
+		status = SMW_STATUS_VERSION_NOT_SUPPORTED;
 		goto end;
 	}
 
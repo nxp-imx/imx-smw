5e651f1d8aaa279c6b8856c4a34831470e9067c2
Bruno De Smet <bruno.de.smet@nxp.com>
2024-03-01 01:27:57 +0100

    SSMW-564 subsystems: ele: Unlock key group mutex
    Fix coverity finding.
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- core/subsystems/ele/keymgr.c
+++ core/subsystems/ele/keymgr.c
@@ -609,7 +609,8 @@ static int append_key_group(struct smw_utils_list *key_grp_list,
 static int get_key_group(struct subsystem_context *ele_ctx, bool persistent,
 			 unsigned int *out_grp)
 {
-	int status = SMW_STATUS_MUTEX_LOCK_FAILURE;
+	int status = SMW_STATUS_OPERATION_FAILURE;
+	int status_mutex = SMW_STATUS_OK;
 
 	struct node *node = NULL;
 	struct key_group *key_grp = NULL;
@@ -619,21 +620,20 @@ static int get_key_group(struct subsystem_context *ele_ctx, bool persistent,
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	if (smw_utils_mutex_lock(ele_ctx->key_grp_mutex))
+	if (smw_utils_mutex_lock(ele_ctx->key_grp_mutex)) {
+		status_mutex = SMW_STATUS_MUTEX_LOCK_FAILURE;
 		goto end;
+	}
 
 	if (persistent)
 		last_grp = ELE_LAST_PERSISTENT_KEY_GROUP;
 
-	status = SMW_STATUS_OPERATION_FAILURE;
 	for (grp = first_grp; grp <= last_grp; grp++) {
 		node = smw_utils_list_find_first(&ele_ctx->key_grp_list, &grp);
 		if (node) {
 			key_grp = smw_utils_list_get_data(node);
-			if (!key_grp) {
-				status = SMW_STATUS_OPERATION_FAILURE;
+			if (!key_grp)
 				break;
-			}
 
 			if (key_grp->persistent == persistent &&
 			    !key_grp->full) {
@@ -649,11 +649,13 @@ static int get_key_group(struct subsystem_context *ele_ctx, bool persistent,
 		}
 	}
 
-	if (smw_utils_mutex_unlock(ele_ctx->key_grp_mutex) &&
-	    status == SMW_STATUS_OK)
-		status = SMW_STATUS_MUTEX_UNLOCK_FAILURE;
+	if (smw_utils_mutex_unlock(ele_ctx->key_grp_mutex))
+		status_mutex = SMW_STATUS_MUTEX_UNLOCK_FAILURE;
 
 end:
+	if (status == SMW_STATUS_OK)
+		status = status_mutex;
+
 	SMW_DBG_PRINTF(VERBOSE, "%s key group #%d returned %d\n", __func__,
 		       *out_grp, status);
 	return status;
@@ -662,22 +664,24 @@ end:
 static int set_key_group_state(struct subsystem_context *ele_ctx,
 			       unsigned int grp, bool persistent, bool full)
 {
-	int status = SMW_STATUS_MUTEX_LOCK_FAILURE;
+	int status = SMW_STATUS_OK;
+	int status_mutex = SMW_STATUS_OK;
 
 	struct node *node = NULL;
 	struct key_group *key_grp = NULL;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	if (smw_utils_mutex_lock(ele_ctx->key_grp_mutex))
+	if (smw_utils_mutex_lock(ele_ctx->key_grp_mutex)) {
+		status_mutex = SMW_STATUS_MUTEX_LOCK_FAILURE;
 		goto end;
+	}
 
 	node = smw_utils_list_find_first(&ele_ctx->key_grp_list, &grp);
 	if (node) {
 		key_grp = smw_utils_list_get_data(node);
 		if (key_grp && key_grp->persistent == persistent) {
 			key_grp->full = full;
-			status = SMW_STATUS_OK;
 		} else {
 			SMW_DBG_PRINTF(ERROR,
 				       "%s: key group %u list data error (%p)",
@@ -691,11 +695,13 @@ static int set_key_group_state(struct subsystem_context *ele_ctx,
 					  persistent, full);
 	}
 
-	if (smw_utils_mutex_unlock(ele_ctx->key_grp_mutex) &&
-	    status == SMW_STATUS_OK)
-		status = SMW_STATUS_MUTEX_UNLOCK_FAILURE;
+	if (smw_utils_mutex_unlock(ele_ctx->key_grp_mutex))
+		status_mutex = SMW_STATUS_MUTEX_UNLOCK_FAILURE;
 
 end:
+	if (status == SMW_STATUS_OK)
+		status = status_mutex;
+
 	SMW_DBG_PRINTF(VERBOSE, "%s key group #%d returned %d\n", __func__, grp,
 		       status);
 	return status;

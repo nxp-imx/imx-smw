d3c878b5a8a0e27a9df06a212e34604f4ec00a35
Ilie Halip <ilie.halip@nxp.com>
2024-06-26 13:20:28 +0300

    SSMW-665 tests: utils: fix memory read error
    vprintf() and vfprintf() modify the va_list parameter, so its value is
    undefined after the first function call.
    
    Use va_copy() to create a va_list for the second function call.
    
    Fixes: 1782eb16cf47 ("SSMW-250 tests: Make log application file thread safe")
    
    Signed-off-by: Ilie Halip <ilie.halip@nxp.com>

--- tests/engine/utils/util_log.c
+++ tests/engine/utils/util_log.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2022-2023 NXP
+ * Copyright 2022-2024 NXP
  */
 
 #include <stdarg.h>
@@ -45,22 +45,24 @@ exit:
 void util_log_status(struct test_data *test, const char *fmt, ...)
 {
 	int nb_char = 0;
-	va_list args = { 0 };
+	va_list args1 = { 0 };
+	va_list args2 = { 0 };
 
 	if (!test) {
 		DBG_PRINT_BAD_ARGS();
 		return;
 	}
 
-	va_start(args, fmt);
+	va_start(args1, fmt);
+	va_copy(args2, args1);
 
 	/* Lock and print status on stdout */
 	util_mutex_lock(test->lock_dbg);
-	(void)vprintf(fmt, args);
+	(void)vprintf(fmt, args1);
 	util_mutex_unlock(test->lock_dbg);
 
 	util_mutex_lock(test->lock_log);
-	nb_char = vfprintf(test->log, fmt, args);
+	nb_char = vfprintf(test->log, fmt, args2);
 	if (nb_char < 0)
 		DBG_PRINT("Log error %s", util_get_strerr());
 
@@ -69,5 +71,6 @@ void util_log_status(struct test_data *test, const char *fmt, ...)
 
 	util_mutex_unlock(test->lock_log);
 
-	va_end(args);
+	va_end(args1);
+	va_end(args2);
 }

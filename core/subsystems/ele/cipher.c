nxp-imx/imx-smw

commit 63a377264b10cf05af633a1ad41014ea125509b6

SSMW-292 subsystem: ele: Add ELE subsystem
Add ELE (EdgeLock Enclave) secure subsystem. This subsystem is enabled
on the i.MX8ULP and i.MX9x platforms.

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>
Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/cipher.c
@@ -1,0 +1,167 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2022 NXP
+ */
+
+#include "smw_status.h"
+
+#include "global.h"
+#include "debug.h"
+#include "utils.h"
+#include "subsystems.h"
+#include "cipher.h"
+
+#include "common.h"
+
+#define CIPHER_ONE_GO_ALGO_ID(_key_type, _cipher_mode)                         \
+	{                                                                      \
+		.key_type = SMW_CONFIG_KEY_TYPE_ID_##_key_type,                \
+		.cipher_mode = SMW_CONFIG_CIPHER_MODE_ID_##_cipher_mode,       \
+		.hsm_algo = HSM_CIPHER_ONE_GO_ALGO_##_cipher_mode              \
+	}
+
+static struct {
+	enum smw_config_key_type_id key_type;
+	enum smw_config_cipher_mode_id cipher_mode;
+	hsm_op_cipher_one_go_algo_t hsm_algo;
+} cipher_algo[] = {
+	CIPHER_ONE_GO_ALGO_ID(AES, CBC),
+	CIPHER_ONE_GO_ALGO_ID(AES, ECB),
+	CIPHER_ONE_GO_ALGO_ID(AES, CTR),
+};
+
+static int get_cipher_one_go_algo(enum smw_config_key_type_id key_type,
+				  enum smw_config_cipher_mode_id cipher_mode,
+				  hsm_op_cipher_one_go_algo_t *hsm_algo)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_SIZE(cipher_algo); i++) {
+		if (key_type == cipher_algo[i].key_type &&
+		    cipher_mode == cipher_algo[i].cipher_mode) {
+			*hsm_algo = cipher_algo[i].hsm_algo;
+			status = SMW_STATUS_OK;
+			break;
+		}
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int get_cipher_one_go_flags(enum smw_config_cipher_op_type_id smw_op,
+				   hsm_op_cipher_one_go_flags_t *hsm_op)
+{
+	switch (smw_op) {
+	case SMW_CONFIG_CIPHER_OP_ID_ENCRYPT:
+		*hsm_op = HSM_CIPHER_ONE_GO_FLAGS_ENCRYPT;
+		break;
+
+	case SMW_CONFIG_CIPHER_OP_ID_DECRYPT:
+		*hsm_op = HSM_CIPHER_ONE_GO_FLAGS_DECRYPT;
+		break;
+
+	default:
+		return SMW_STATUS_INVALID_PARAM;
+	}
+
+	return SMW_STATUS_OK;
+}
+
+static int cipher(struct hdl *hdl, void *args)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	hsm_err_t err;
+	op_cipher_one_go_args_t op_args = { 0 };
+	enum smw_config_key_type_id key_type;
+	struct smw_crypto_cipher_args *cipher_args = args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (smw_crypto_get_cipher_nb_key_buffer(cipher_args)) {
+		SMW_DBG_PRINTF(ERROR, "%s: ELE doesn't support keys buffer\n",
+			       __func__);
+		goto end;
+	}
+
+	/* Get 1st key type as reference */
+	key_type = cipher_args->keys_desc[0]->identifier.type_id;
+
+	/* Get ELE algorithm */
+	status = get_cipher_one_go_algo(key_type, cipher_args->mode_id,
+					&op_args.cipher_algo);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	/* Get ELE operation */
+	status = get_cipher_one_go_flags(cipher_args->op_id, &op_args.flags);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	op_args.output = smw_crypto_get_cipher_output(cipher_args);
+
+	/* Get output length feature */
+	if (!op_args.output) {
+		/* Cipher output length is equal to input length */
+		smw_crypto_set_cipher_output_len(cipher_args,
+						 op_args.input_size);
+		status = SMW_STATUS_OK;
+		goto end;
+	}
+
+	op_args.key_identifier = smw_crypto_get_cipher_key_id(cipher_args, 0);
+	op_args.output_size = smw_crypto_get_cipher_output_len(cipher_args);
+	op_args.input = smw_crypto_get_cipher_input(cipher_args);
+	op_args.input_size = smw_crypto_get_cipher_input_len(cipher_args);
+	op_args.iv = smw_crypto_get_cipher_iv(cipher_args);
+	op_args.iv_size = smw_crypto_get_cipher_iv_len(cipher_args);
+
+	SMW_DBG_PRINTF(VERBOSE,
+		       "[%s (%d)] Call hsm_cipher_one_go()\n"
+		       "op_cipher_one_go_args_t\n"
+		       "    key_identifier: 0x%08X\n"
+		       "    algo: 0x%08X\n"
+		       "    flags: 0x%X\n"
+		       "    IV\n"
+		       "      - buffer: %p\n"
+		       "      - size: %d\n"
+		       "    Input\n"
+		       "       - buffer: %p\n"
+		       "       - size: %d\n"
+		       "    Output\n"
+		       "       - buffer: %p\n"
+		       "       - size: %d\n",
+		       __func__, __LINE__, op_args.key_identifier,
+		       op_args.cipher_algo, op_args.flags, op_args.iv,
+		       op_args.iv_size, op_args.input, op_args.input_size,
+		       op_args.output, op_args.output_size);
+
+	err = hsm_do_cipher(hdl->key_store, &op_args);
+	SMW_DBG_PRINTF(DEBUG, "hsm_cipher_one_go returned %d\n", err);
+
+	status = ele_convert_err(err);
+
+	/* Update the output length  */
+	smw_crypto_set_cipher_output_len(cipher_args, op_args.output_size);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+bool ele_cipher_handle(struct hdl *hdl, enum operation_id operation_id,
+		       void *args, int *status)
+{
+	switch (operation_id) {
+	case OPERATION_ID_CIPHER:
+		*status = cipher(hdl, args);
+		break;
+
+	default:
+		return false;
+	}
+
+	return true;
+}

core/CMakeLists.txt
@@ -81,6 +81,7 @@ configure_file(${CORE_SRC_INC}/info.h.in ${CORE_BIN_INC}/info.h)
 
 add_subdirectory(utils)
 add_subdirectory(init)
+add_subdirectory(subsystems/ele)
 add_subdirectory(subsystems/hsm)
 add_subdirectory(subsystems/tee)
 add_subdirectory(keymgr)

core/subsystems/ele/CMakeLists.txt
@@ -1,0 +1,37 @@
+if(NOT DEFINED ELE_ROOT)
+    message(STATUS "ELE (EdgeLock Enclave) not enabled")
+    return()
+endif()
+
+find_package(ZLIBLight REQUIRED)
+find_package(Ele REQUIRED)
+
+#
+# Add ELE subsystem
+#
+add_subsystem(ele)
+
+set(OBJ_LIST ${OBJ_LIST} $<TARGET_OBJECTS:ele> PARENT_SCOPE)
+
+message(STATUS "    ELE  Libraries: ${ELE_LIBRARIES}")
+message(STATUS "    ELE  Headers  : ${ELE_INCLUDE_DIRS}")
+message(STATUS "    ZLIB Library  : ${ZLIB_LIBRARY}")
+message(STATUS "    ZLIB Header   : ${ZLIB_INCLUDE_DIR}")
+
+add_library(ele OBJECT "")
+set_target_properties(ele PROPERTIES C_VISIBILITY_PRESET hidden)
+
+target_sources(ele PRIVATE
+               entry.c
+               $<$<BOOL:${ENABLE_KEYMGR_MODULE}>:keymgr.c keymgr_derive.c key_policy.c>
+               $<$<BOOL:${ENABLE_HASH}>:hash.c>
+               $<$<BOOL:${ENABLE_HMAC}>:hmac.c>
+               $<$<BOOL:${ENABLE_SIGN_VERIFY}>:sign_verify.c>
+               $<$<BOOL:${ENABLE_RNG}>:rng.c>
+               $<$<BOOL:${ENABLE_CIPHER}>:cipher.c>)
+
+target_include_directories(ele PRIVATE
+                           ${INCLUDE_DIR}
+                           ${ELE_INCLUDE_DIRS})
+
+set(EXT_LIBS ${EXT_LIBS} ${ELE_LIBRARIES} ${ZLIB_LIBRARY} PARENT_SCOPE)

core/subsystems/ele/common.h
@@ -1,0 +1,181 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2022 NXP
+ */
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define PSA_COMPLIANT
+#include <hsm_api.h>
+
+#include "keymgr_derive.h"
+
+/**
+ * struct hdl - ELE handles
+ * @session: Session handle
+ * @key_store: Key store service flow handle
+ *
+ * This structure stores the ELE handles managed by the SMW library.
+ */
+struct hdl {
+	hsm_hdl_t session;
+	hsm_hdl_t key_store;
+};
+
+/**
+ * ele_key_handle() - Handle the Key operations.
+ * @hdl: Pointer to the ELE handles structure.
+ * @operation_id: Security Operation ID.
+ * @args: Pointer to a structure of arguments defined by the internal API.
+ * @status: Error code set only if the Security Operation is handled.
+ *
+ * This function handles the Key operations.
+ * @status is set only if the function returns true.
+ *
+ * Return:
+ * * true:	- the Security Operation has been handled.
+ * * false:	- the Security Operation has not been handled.
+ */
+bool ele_key_handle(struct hdl *hdl, enum operation_id operation_id, void *args,
+		    int *status);
+
+/**
+ * ele_hash_handle() - Handle the Hash operation.
+ * @hdl: Pointer to the ELE handles structure.
+ * @operation_id: Security Operation ID.
+ * @args: Pointer to a structure of arguments defined by the internal API.
+ * @status: Error code set only if the Security Operation is handled.
+ *
+ * This function handles the Hash operation.
+ * @status is set only if the function returns true.
+ *
+ * Return:
+ * * true:	- the Security Operation has been handled.
+ * * false:	- the Security Operation has not been handled.
+ */
+bool ele_hash_handle(struct hdl *hdl, enum operation_id operation_id,
+		     void *args, int *status);
+
+/**
+ * ele_hmac_handle() - Handle the HMAC operation.
+ * @hdl: Pointer to the ELE handles structure.
+ * @operation_id: Security Operation ID.
+ * @args: Pointer to a structure of arguments defined by the internal API.
+ * @status: Error code set only if the Security Operation is handled.
+ *
+ * This function handles the HMAC operation.
+ * @status is set only if the function returns true.
+ *
+ * Return:
+ * * true:	- the Security Operation has been handled.
+ * * false:	- the Security Operation has not been handled.
+ */
+bool ele_hmac_handle(struct hdl *hdl, enum operation_id operation_id,
+		     void *args, int *status);
+
+/**
+ * ele_sign_verify_handle() - Handle the Sign and Verify operation.
+ * @hdl: Pointer to the ELE handles structure.
+ * @operation_id: Security Operation ID.
+ * @args: Pointer to a structure of arguments defined by the internal API.
+ * @status: Error code set only if the Security Operation is handled.
+ *
+ * This function handles the signarture generation and verification operation.
+ * @status is set only if the function returns true.
+ *
+ * Return:
+ * * true:	- the Security Operation has been handled.
+ * * false:	- the Security Operation has not been handled.
+ */
+bool ele_sign_verify_handle(struct hdl *hdl, enum operation_id operation_id,
+			    void *args, int *status);
+
+/**
+ * ele_rng_handle() - Handle the random generation operation.
+ * @hdl: Pointer to the ELE handles structure.
+ * @operation_id: Security Operation ID.
+ * @args: Pointer to a structure of arguments defined by the internal API.
+ * @status: Error code set only if the Security Operation is handled.
+ *
+ * This function handles the random number generation operation.
+ * @status is set only if the function returns true.
+ *
+ * Return:
+ * * true:	- the Security Operation has been handled.
+ * * false:	- the Security Operation has not been handled.
+ */
+bool ele_rng_handle(struct hdl *hdl, enum operation_id operation_id, void *args,
+		    int *status);
+
+/**
+ * ele_cipher_handle() - Handle the cipher encryption/decryption operation.
+ * @hdl: Pointer to the ELE handles structure.
+ * @operation_id: Security Operation ID.
+ * @args: Pointer to a structure of arguments defined by the internal API.
+ * @status: Error code set only if the Security Operation is handled.
+ *
+ * This function handles the cipher encryption/decrytion operation.
+ * @status is set only if the function returns true.
+ *
+ * Return:
+ * * true:	- the Security Operation has been handled.
+ * * false:	- the Security Operation has not been handled.
+ */
+bool ele_cipher_handle(struct hdl *hdl, enum operation_id operation_id,
+		       void *args, int *status);
+
+/**
+ * ele_derive_key() - ELE key derivation operation.
+ * @hdl: Pointer to the ELE handles structure.
+ * @args: Pointer to the derive key arguments.
+ *
+ * Return:
+ * SMW status
+ */
+int ele_derive_key(struct hdl *hdl, struct smw_keymgr_derive_key_args *args);
+
+/**
+ * ele_convert_err() - Convert ELE error into SMW status.
+ * @err: ELE error code.
+ *
+ * Return:
+ * SMW status
+ */
+int ele_convert_err(hsm_err_t err);
+
+/**
+ * ele_set_key_type() - Set the ELE key type
+ * @key_type_id: SMW Key type id
+ * @security_size: Key security size
+ * @ele_type: ELE key type corresponding
+ *
+ * Return:
+ * SMW_STATUS_OK                       - Success
+ * SMW_STATUS_OPERATION_NOT_SUPPORTED  - Key type not supported
+ */
+int ele_set_key_type(enum smw_config_key_type_id key_type_id,
+		     unsigned short security_size, hsm_key_type_t *ele_type);
+
+/**
+ * ele_set_key_policy() - Convert the user key policy to ELE key policy
+ * @policy: Pointer to the key policy
+ * @policy_len: Length of @policy
+ * @ele_usage: ELE key usage(s) bit mask
+ * @ele_algo: ELE key permitted algorithm (first algorithm defined)
+ * @actual_policy: Key attributes policy used
+ * @actual_policy_len: Length of key attributes policy used
+ *
+ * Return:
+ * SMW_STATUS_OK                         - Success
+ * SMW_STATUS_KEY_POLICY_ERROR           - User key policy definition error
+ * SMW_STATUS_KEY_POLICY_WARNING_IGNORED - One of the user key policy is ignored
+ * Other SMW status error.
+ */
+int ele_set_key_policy(const unsigned char *policy, unsigned int policy_len,
+		       hsm_key_usage_t *ele_usage,
+		       hsm_permitted_algo_t *ele_algo,
+		       unsigned char **actual_policy,
+		       unsigned int *actual_policy_len);
+
+#endif /* __COMMON_H__ */

core/subsystems/ele/entry.c
@@ -1,0 +1,317 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2022 NXP
+ */
+
+#include "smw_osal.h"
+
+#include "compiler.h"
+#include "global.h"
+#include "debug.h"
+#include "utils.h"
+#include "subsystems.h"
+#include "config.h"
+
+#include "common.h"
+
+static struct {
+	struct hdl hdl;
+	unsigned long tid;
+} ctx = {
+	.hdl = { .session = 0, .key_store = 0 },
+	.tid = 0,
+};
+
+static int open_session(hsm_hdl_t *session_hdl)
+{
+	int status = SMW_STATUS_SUBSYSTEM_FAILURE;
+
+	hsm_err_t err;
+	open_session_args_t open_session_args = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	err = hsm_open_session(&open_session_args, session_hdl);
+	status = ele_convert_err(err);
+
+	SMW_DBG_PRINTF(DEBUG, "%s - err: %d\n", __func__, err);
+	SMW_DBG_PRINTF(DEBUG, "session_hdl: %u\n", *session_hdl);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static void close_session(hsm_hdl_t session_hdl)
+{
+	hsm_err_t __maybe_unused err;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	SMW_DBG_PRINTF(DEBUG, "session_hdl: %u\n", session_hdl);
+	err = hsm_close_session(session_hdl);
+	SMW_DBG_PRINTF(DEBUG, "%s - returned: %d\n", __func__, err);
+}
+
+static int open_key_store_service(hsm_hdl_t session_hdl,
+				  hsm_hdl_t *key_store_hdl)
+{
+	int status = SMW_STATUS_SUBSYSTEM_FAILURE;
+
+	hsm_err_t err;
+	open_svc_key_store_args_t open_svc_key_store_args = { 0 };
+	const char *subsystem_name;
+	struct se_info info;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+	subsystem_name = smw_config_get_subsystem_name(SUBSYSTEM_ID_ELE);
+
+	status = smw_utils_get_subsystem_info(subsystem_name, &info);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	open_svc_key_store_args.key_store_identifier = info.storage_id;
+	open_svc_key_store_args.authentication_nonce = info.storage_nonce;
+	open_svc_key_store_args.max_updates_number = info.storage_replay;
+
+	/* Key store may not exists. Try to create it */
+	open_svc_key_store_args.flags = HSM_SVC_KEY_STORE_FLAGS_CREATE;
+	err = hsm_open_key_store_service(session_hdl, &open_svc_key_store_args,
+					 key_store_hdl);
+	if (err == HSM_ID_CONFLICT || err == HSM_KEY_STORE_CONFLICT) {
+		/* Key store already exists. Do not try to create it */
+		open_svc_key_store_args.flags = 0;
+		err = hsm_open_key_store_service(session_hdl,
+						 &open_svc_key_store_args,
+						 key_store_hdl);
+	}
+
+	status = ele_convert_err(err);
+	SMW_DBG_PRINTF(DEBUG, "%s - err: %d\n", __func__, err);
+
+	SMW_DBG_PRINTF(DEBUG, "key_store_hdl: %u\n", *key_store_hdl);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static void close_key_store_service(hsm_hdl_t key_store_hdl)
+{
+	hsm_err_t __maybe_unused err;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	SMW_DBG_PRINTF(DEBUG, "key_store_hdl: %u\n", key_store_hdl);
+	err = hsm_close_key_store_service(key_store_hdl);
+	SMW_DBG_PRINTF(DEBUG, "%s - returned: %d\n", __func__, err);
+}
+
+static void reset_handles(void)
+{
+	struct hdl *hdl = &ctx.hdl;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (hdl->key_store)
+		close_key_store_service(hdl->key_store);
+	if (hdl->session)
+		close_session(hdl->session);
+
+	hdl->session = 0;
+	hdl->key_store = 0;
+}
+
+static int unload(void)
+{
+	int status = SMW_STATUS_OK;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	reset_handles();
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int load(void)
+{
+	int status = SMW_STATUS_SUBSYSTEM_LOAD_FAILURE;
+
+	struct hdl *hdl = &ctx.hdl;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	status = open_session(&hdl->session);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = open_key_store_service(hdl->session, &hdl->key_store);
+
+end:
+	if (status != SMW_STATUS_OK)
+		(void)unload();
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+__weak bool ele_key_handle(struct hdl *hdl, enum operation_id operation_id,
+			   void *args, int *status)
+{
+	(void)hdl;
+	(void)operation_id;
+	(void)args;
+	(void)status;
+
+	return false;
+}
+
+__weak bool ele_hash_handle(struct hdl *hdl, enum operation_id operation_id,
+			    void *args, int *status)
+{
+	(void)hdl;
+	(void)operation_id;
+	(void)args;
+	(void)status;
+
+	return false;
+}
+
+__weak bool ele_sign_verify_handle(struct hdl *hdl,
+				   enum operation_id operation_id, void *args,
+				   int *status)
+{
+	(void)hdl;
+	(void)operation_id;
+	(void)args;
+	(void)status;
+
+	return false;
+}
+
+__weak bool ele_rng_handle(struct hdl *hdl, enum operation_id operation_id,
+			   void *args, int *status)
+{
+	(void)hdl;
+	(void)operation_id;
+	(void)args;
+	(void)status;
+
+	return false;
+}
+
+__weak bool ele_cipher_handle(struct hdl *hdl, enum operation_id operation_id,
+			      void *args, int *status)
+{
+	(void)hdl;
+	(void)operation_id;
+	(void)args;
+	(void)status;
+
+	return false;
+}
+
+__weak bool ele_hmac_handle(struct hdl *hdl, enum operation_id operation_id,
+			    void *args, int *status)
+{
+	(void)hdl;
+	(void)operation_id;
+	(void)args;
+	(void)status;
+
+	return false;
+}
+
+static int execute(enum operation_id operation_id, void *args)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	struct hdl *hdl = &ctx.hdl;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (ele_key_handle(hdl, operation_id, args, &status))
+		goto end;
+	else if (ele_hash_handle(hdl, operation_id, args, &status))
+		goto end;
+	else if (ele_sign_verify_handle(hdl, operation_id, args, &status))
+		goto end;
+	else if (ele_rng_handle(hdl, operation_id, args, &status))
+		goto end;
+	else if (ele_cipher_handle(hdl, operation_id, args, &status))
+		goto end;
+
+	ele_hmac_handle(hdl, operation_id, args, &status);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static struct subsystem_func func = { .load = load,
+				      .unload = unload,
+				      .execute = execute };
+
+struct subsystem_func *smw_ele_get_func(void)
+{
+	return &func;
+}
+
+int ele_convert_err(hsm_err_t err)
+{
+	int status = SMW_STATUS_SUBSYSTEM_FAILURE;
+
+	switch (err) {
+	case HSM_NO_ERROR:
+		status = SMW_STATUS_OK;
+		break;
+
+	case HSM_INVALID_PARAM:
+	case HSM_INVALID_MESSAGE:
+	case HSM_INVALID_ADDRESS:
+	case HSM_UNKNOWN_HANDLE:
+	case HSM_UNKNOWN_KEY_STORE:
+	case HSM_ID_CONFLICT:
+		status = SMW_STATUS_INVALID_PARAM;
+		break;
+
+	case HSM_OUT_OF_MEMORY:
+		status = SMW_STATUS_SUBSYSTEM_OUT_OF_MEMORY;
+		break;
+
+	case HSM_UNKNOWN_ID:
+		status = SMW_STATUS_UNKNOWN_ID;
+		break;
+
+	case HSM_FEATURE_NOT_SUPPORTED:
+	case HSM_FEATURE_DISABLED:
+	case HSM_CMD_NOT_SUPPORTED:
+		status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+		break;
+
+	case HSM_NOT_READY_RATING:
+	case HSM_RNG_NOT_STARTED:
+	case HSM_KEY_STORE_COUNTER:
+		status = SMW_STATUS_OPERATION_FAILURE;
+		break;
+
+	case HSM_NVM_ERROR:
+	case HSM_KEY_STORE_ERROR:
+	case HSM_KEY_STORE_CONFLICT:
+	case HSM_KEY_STORE_AUTH:
+		status = SMW_STATUS_SUBSYSTEM_STORAGE_ERROR;
+		break;
+
+	default:
+		/*
+		 * status = SMW_STATUS_SUBSYSTEM_FAILURE
+		 * HSM_INVALID_LIFECYCLE
+		 * HSM_SELF_TEST_FAILURE
+		 * HSM_FATAL_FAILURE
+		 * HSM_GENERAL_ERROR
+		 */
+		break;
+	}
+
+	return status;
+}

core/subsystems/ele/hash.c
@@ -1,0 +1,118 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2022 NXP
+ */
+
+#include "smw_status.h"
+
+#include "global.h"
+#include "debug.h"
+#include "utils.h"
+#include "subsystems.h"
+#include "config.h"
+#include "hash.h"
+
+#include "common.h"
+
+#define HASH_ALGO(_id, _hsm_id, _length)                                       \
+	{                                                                      \
+		.algo_id = SMW_CONFIG_HASH_ALGO_ID_##_id,                      \
+		.hsm_hash_algo = HSM_HASH_ALGO_##_hsm_id, .length = _length    \
+	}
+
+static struct hash_algo {
+	enum smw_config_hash_algo_id algo_id;
+	hsm_hash_algo_t hsm_hash_algo;
+	uint32_t length;
+} hash_algos[] = { HASH_ALGO(SHA224, SHA_224, 28),
+		   HASH_ALGO(SHA256, SHA_256, 32),
+		   HASH_ALGO(SHA384, SHA_384, 48),
+		   HASH_ALGO(SHA512, SHA_512, 64) };
+
+static struct hash_algo *get_hash_algo(enum smw_config_hash_algo_id algo_id)
+{
+	struct hash_algo *hash_algo = NULL;
+	unsigned int i;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	for (i = 0; i < ARRAY_SIZE(hash_algos); i++) {
+		if (hash_algos[i].algo_id == algo_id) {
+			hash_algo = &hash_algos[i];
+			break;
+		}
+	}
+
+	return hash_algo;
+}
+
+static int hash(struct hdl *hdl, void *args)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	hsm_err_t err;
+	op_hash_one_go_args_t op_args = { 0 };
+
+	struct smw_crypto_hash_args *hash_args = args;
+	struct hash_algo *hash_algo = NULL;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	hash_algo = get_hash_algo(hash_args->algo_id);
+	if (!hash_algo)
+		goto end;
+
+	op_args.input = smw_crypto_get_hash_input_data(hash_args);
+	op_args.output = smw_crypto_get_hash_output_data(hash_args);
+	op_args.input_size = smw_crypto_get_hash_input_length(hash_args);
+	op_args.output_size = smw_crypto_get_hash_output_length(hash_args);
+	op_args.algo = hash_algo->hsm_hash_algo;
+
+	/* Get output length feature */
+	if (!op_args.output) {
+		smw_crypto_set_hash_output_length(hash_args, hash_algo->length);
+		status = SMW_STATUS_OK;
+		goto end;
+	}
+
+	SMW_DBG_PRINTF(VERBOSE,
+		       "[%s (%d)] Call hsm_do_hash()\n"
+		       "op_args_t\n"
+		       "    algo: 0x%08X\n"
+		       "    flags: 0x%X\n"
+		       "    Input\n"
+		       "      - buffer: %p\n"
+		       "      - size: %d\n"
+		       "    Output\n"
+		       "      - buffer: %p\n"
+		       "      - size: %d\n",
+		       __func__, __LINE__, op_args.algo, op_args.flags,
+		       op_args.input, op_args.input_size, op_args.output,
+		       op_args.output_size);
+
+	err = hsm_do_hash(hdl->session, &op_args);
+	SMW_DBG_PRINTF(DEBUG, "hsm_do_hash returned %d\n", err);
+
+	status = ele_convert_err(err);
+
+	/* Update Digest size */
+	smw_crypto_set_hash_output_length(hash_args, op_args.output_size);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+bool ele_hash_handle(struct hdl *hdl, enum operation_id operation_id,
+		     void *args, int *status)
+{
+	switch (operation_id) {
+	case OPERATION_ID_HASH:
+		*status = hash(hdl, args);
+		break;
+	default:
+		return false;
+	}
+
+	return true;
+}

core/subsystems/ele/hmac.c
@@ -1,0 +1,129 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2022 NXP
+ */
+
+#include "smw_status.h"
+
+#include "debug.h"
+#include "keymgr.h"
+#include "hmac.h"
+#include "subsystems.h"
+#include "utils.h"
+
+#include "common.h"
+
+#define HMAC_ALGO(_id, _hsm_id, _length)                                       \
+	{                                                                      \
+		.algo_id = SMW_CONFIG_HMAC_ALGO_ID_##_id,                      \
+		.hsm_algo = PERMITTED_ALGO_HMAC_##_hsm_id, .length = _length   \
+	}
+
+static struct hmac_algo {
+	enum smw_config_hmac_algo_id algo_id;
+	hsm_op_mac_one_go_algo_t hsm_algo;
+	uint32_t length;
+} hmac_algos[] = {
+	HMAC_ALGO(SHA256, SHA256, 32),
+	HMAC_ALGO(SHA384, SHA384, 48),
+};
+
+static struct hmac_algo *get_hmac_algo(enum smw_config_hmac_algo_id algo_id)
+{
+	struct hmac_algo *info = NULL;
+	unsigned int i;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	for (i = 0; i < ARRAY_SIZE(hmac_algos); i++) {
+		if (hmac_algos[i].algo_id == algo_id) {
+			info = &hmac_algos[i];
+			break;
+		}
+	}
+
+	return info;
+}
+
+static int hmac(struct hdl *hdl, void *args)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	hsm_err_t err = HSM_NO_ERROR;
+	op_mac_one_go_args_t op_args = { 0 };
+
+	struct smw_crypto_hmac_args *hmac_args = args;
+	struct hmac_algo *hmac_algo;
+	struct smw_keymgr_descriptor *key_descriptor;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	SMW_DBG_ASSERT(hmac_args);
+
+	hmac_algo = get_hmac_algo(hmac_args->algo_id);
+	if (!hmac_algo)
+		goto end;
+
+	key_descriptor = &hmac_args->key_descriptor;
+
+	if (key_descriptor->format_id != SMW_KEYMGR_FORMAT_ID_INVALID)
+		//TODO: first import key, then generate mac
+		//      for now import is not supported by ELE
+		goto end;
+
+	op_args.key_identifier = key_descriptor->identifier.id;
+	op_args.algorithm = hmac_algo->hsm_algo;
+	op_args.flags = HSM_OP_MAC_ONE_GO_FLAGS_MAC_GENERATION;
+	op_args.payload = smw_hmac_get_input_data(hmac_args);
+	op_args.payload_size = smw_hmac_get_input_length(hmac_args);
+	op_args.mac = smw_hmac_get_output_data(hmac_args);
+
+	if (!op_args.mac) {
+		smw_hmac_set_output_length(hmac_args, hmac_algo->length);
+		status = SMW_STATUS_OK;
+		goto end;
+	}
+
+	op_args.mac_size = smw_hmac_get_output_length(hmac_args);
+
+	SMW_DBG_PRINTF(VERBOSE,
+		       "[%s (%d)] Call hsm_hmac_one_go()\n"
+		       "op_hmac_one_go_args_t\n"
+		       "    key_identifier: 0x%X\n"
+		       "    algo: 0x%08X\n"
+		       "    flags: 0x%X\n"
+		       "    Payload\n"
+		       "      - buffer: %p\n"
+		       "      - size: %d\n"
+		       "    Mac\n"
+		       "      - buffer: %p\n"
+		       "      - size: %d\n",
+		       __func__, __LINE__, op_args.key_identifier,
+		       op_args.algorithm, op_args.flags, op_args.payload,
+		       op_args.payload_size, op_args.mac, op_args.mac_size);
+
+	err = hsm_do_mac(hdl->key_store, &op_args);
+	SMW_DBG_PRINTF(DEBUG, "%s hsm_mac_one_go returned %d\n", __func__, err);
+
+	status = ele_convert_err(err);
+
+	smw_hmac_set_output_length(hmac_args, op_args.mac_size);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+bool ele_hmac_handle(struct hdl *hdl, enum operation_id operation_id,
+		     void *args, int *status)
+{
+	switch (operation_id) {
+	case OPERATION_ID_HMAC:
+		*status = hmac(hdl, args);
+		break;
+	default:
+		return false;
+	}
+
+	return true;
+}

commit 8fa20b214f1048ecd32c29d90cc09b779116eaeb

SSMW-672 subsystems: ele: output_len correctly set when output is NULL
Set output len to input len when output buffer is NULL

Fixes: 63a377264b10 ("SSMW-292 subsystem: ele: Add ELE subsystem")

Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

core/subsystems/ele/cipher.c
@@ -74,6 +74,7 @@ static int cipher(struct hdl *hdl, void *args)
 		goto end;
 
 	op_args.output = smw_crypto_get_cipher_output(cipher_args);
+	op_args.input_size = smw_crypto_get_cipher_input_len(cipher_args);
 
 	/* Get output length feature */
 	if (!op_args.output) {
@@ -87,7 +88,6 @@ static int cipher(struct hdl *hdl, void *args)
 	op_args.key_identifier = smw_crypto_get_cipher_key_id(cipher_args, 0);
 	op_args.output_size = smw_crypto_get_cipher_output_len(cipher_args);
 	op_args.input = smw_crypto_get_cipher_input(cipher_args);
-	op_args.input_size = smw_crypto_get_cipher_input_len(cipher_args);
 	op_args.iv = smw_crypto_get_cipher_iv(cipher_args);
 
 	if (SET_OVERFLOW(smw_crypto_get_cipher_iv_len(cipher_args),
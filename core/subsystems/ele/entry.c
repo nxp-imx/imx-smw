046a850bc0c06837fff65f5ebdaae99394d48c32
Bruno De Smet <bruno.de.smet@nxp.com>
2024-07-15 11:57:10 +0200

    SSMW-673 subsystems: ele: Change smw_subsystem_t to enum type
    In the SMW public API, the Secure Subsystem name
    is not a string anymore but an enum.
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- core/subsystems/ele/entry.c
+++ core/subsystems/ele/entry.c
@@ -55,13 +55,11 @@ static int open_key_store_service(hsm_hdl_t session_hdl,
 
 	hsm_err_t err = HSM_NO_ERROR;
 	open_svc_key_store_args_t open_svc_key_store_args = { 0 };
-	const char *subsystem_name = NULL;
 	struct se_info info = { 0 };
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
-	subsystem_name = smw_config_get_subsystem_name(SUBSYSTEM_ID_ELE);
 
-	if (smw_utils_get_subsystem_info(subsystem_name, &info))
+	if (smw_utils_get_subsystem_info(SMW_SUBSYSTEM_NAME_ELE, &info))
 		goto end;
 
 	open_svc_key_store_args.key_store_identifier = info.storage_id;

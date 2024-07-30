b1f8d12276dd83861f7dd971dcc2e1822c2e8bf1
Bruno De Smet <bruno.de.smet@nxp.com>
2024-07-30 14:03:09 +0200

    SSMW-676 core: utils: Change smw_hash_algo_t to enum type
    In the SMW public API, the hash algorithm name
    is not a string anymore but an enum.
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- core/utils/hash.c
+++ core/utils/hash.c
@@ -7,49 +7,35 @@
 
 #include "config.h"
 #include "debug.h"
-#include "name.h"
+#include "utils.h"
 
-static const char *const hash_algo_names[] = {
-	[SMW_CONFIG_HASH_ALGO_ID_MD5] = "MD5",
-	[SMW_CONFIG_HASH_ALGO_ID_SHA1] = "SHA1",
-	[SMW_CONFIG_HASH_ALGO_ID_SHA224] = "SHA224",
-	[SMW_CONFIG_HASH_ALGO_ID_SHA256] = "SHA256",
-	[SMW_CONFIG_HASH_ALGO_ID_SHA384] = "SHA384",
-	[SMW_CONFIG_HASH_ALGO_ID_SHA512] = "SHA512",
-	[SMW_CONFIG_HASH_ALGO_ID_SHA3_224] = "SHA3_224",
-	[SMW_CONFIG_HASH_ALGO_ID_SHA3_256] = "SHA3_256",
-	[SMW_CONFIG_HASH_ALGO_ID_SHA3_384] = "SHA3_384",
-	[SMW_CONFIG_HASH_ALGO_ID_SHA3_512] = "SHA3_512",
-	[SMW_CONFIG_HASH_ALGO_ID_SM3] = "SM3"
-};
+/*
+ * Ordering must be the same for internal values and public values.
+ * This way the offset between the internal values and the public values
+ * can be used for conversion, and no conversion table is required.
+ *
+ * The offset between the internal values and the public values is
+ * given by the first public value.
+ */
 
-int smw_utils_hash_algo_names(char **start, char *end, unsigned long *bitmap)
-{
-	int status =
-		smw_config_read_strings(start, end, bitmap, hash_algo_names,
-					SMW_CONFIG_HASH_ALGO_ID_NB);
-	if (status == SMW_STATUS_UNKNOWN_NAME)
-		status = SMW_STATUS_UNKNOWN_ALGO_NAME;
+#define SMW_CONFIG_HASH_ALGO_ID_OFFSET                                         \
+	(SMW_HASH_ALGO_NAME_MD5 - SMW_CONFIG_HASH_ALGO_ID_MD5)
 
-	return status;
-}
-
-int smw_utils_get_hash_algo_id(const char *name,
+int smw_utils_get_hash_algo_id(smw_hash_algo_t name,
 			       enum smw_config_hash_algo_id *id)
 {
-	int status = SMW_STATUS_OK;
+	int status = SMW_STATUS_UNKNOWN_ALGO_NAME;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	if (!name)
+	if (name == SMW_HASH_ALGO_NAME_NONE) {
 		*id = SMW_CONFIG_HASH_ALGO_ID_INVALID;
-	else
-		status = smw_utils_get_string_index(name, hash_algo_names,
-						    SMW_CONFIG_HASH_ALGO_ID_NB,
-						    id);
-
-	if (status == SMW_STATUS_UNKNOWN_NAME)
-		status = SMW_STATUS_UNKNOWN_ALGO_NAME;
+		status = SMW_STATUS_OK;
+	} else if (name < SMW_HASH_ALGO_NAME_NB) {
+		if (!SUB_OVERFLOW(name, SMW_CONFIG_HASH_ALGO_ID_OFFSET,
+				  (int *)id))
+			status = SMW_STATUS_OK;
+	}
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;

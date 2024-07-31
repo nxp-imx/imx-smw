6f1566d45a505aa1aa0df38a700e598cd9f5e953
Bruno De Smet <bruno.de.smet@nxp.com>
2024-07-31 03:48:33 +0200

    SSMW-679 core: utils: Change smw_aead_mode_t to enum type
    In the SMW public API, the AEAD mode name
    is not a string anymore but an enum.
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- core/utils/aead.c
+++ core/utils/aead.c
@@ -8,17 +8,44 @@
 #include "config.h"
 #include "debug.h"
 #include "name.h"
-
-static const char *const aead_mode_names[] = {
-	[SMW_CONFIG_AEAD_MODE_ID_CCM] = "CCM",
-	[SMW_CONFIG_AEAD_MODE_ID_GCM] = "GCM",
-	[SMW_CONFIG_AEAD_MODE_ID_CHACHA20_POLY1305] = "CHACHA20_POLY1305"
-};
+#include "utils.h"
 
 static const char *const aead_op_type_names[] = {
 	[SMW_CONFIG_AEAD_OP_ID_ENCRYPT] = "ENCRYPT",
 	[SMW_CONFIG_AEAD_OP_ID_DECRYPT] = "DECRYPT"
 };
+
+/*
+ * Ordering must be the same for internal values and public values.
+ * This way the offset between the internal values and the public values
+ * can be used for conversion, and no conversion table is required.
+ *
+ * The offset between the internal values and the public values is
+ * given by the first public value.
+ */
+
+#define SMW_CONFIG_AEAD_MODE_ID_OFFSET                                         \
+	(SMW_AEAD_MODE_NAME_CCM - SMW_CONFIG_AEAD_MODE_ID_CCM)
+
+int smw_utils_get_aead_mode_id(smw_aead_mode_t name,
+			       enum smw_config_aead_mode_id *id)
+{
+	int status = SMW_STATUS_UNKNOWN_MODE_NAME;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (name == SMW_AEAD_MODE_NAME_NONE) {
+		*id = SMW_CONFIG_AEAD_MODE_ID_INVALID;
+		status = SMW_STATUS_OK;
+	} else if (name < SMW_AEAD_MODE_NAME_NB) {
+		if (!SUB_OVERFLOW(name, SMW_CONFIG_AEAD_MODE_ID_OFFSET,
+				  (int *)id))
+			status = SMW_STATUS_OK;
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
 
 int smw_utils_aead_op_type_names(char **start, char *end, unsigned long *bitmap)
 {
@@ -28,38 +55,6 @@ int smw_utils_aead_op_type_names(char **start, char *end, unsigned long *bitmap)
 	if (status == SMW_STATUS_UNKNOWN_NAME)
 		status = SMW_STATUS_UNKNOWN_OP_TYPE_NAME;
 
-	return status;
-}
-
-int smw_utils_aead_mode_names(char **start, char *end, unsigned long *bitmap)
-{
-	int status =
-		smw_config_read_strings(start, end, bitmap, aead_mode_names,
-					SMW_CONFIG_AEAD_MODE_ID_NB);
-	if (status == SMW_STATUS_UNKNOWN_NAME)
-		status = SMW_STATUS_UNKNOWN_MODE_NAME;
-
-	return status;
-}
-
-int smw_utils_get_aead_mode_id(const char *name,
-			       enum smw_config_aead_mode_id *id)
-{
-	int status = SMW_STATUS_OK;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	if (!name)
-		*id = SMW_CONFIG_AEAD_MODE_ID_INVALID;
-	else
-		status = smw_utils_get_string_index(name, aead_mode_names,
-						    SMW_CONFIG_AEAD_MODE_ID_NB,
-						    id);
-
-	if (status == SMW_STATUS_UNKNOWN_NAME)
-		status = SMW_STATUS_UNKNOWN_MODE_NAME;
-
-	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }
 

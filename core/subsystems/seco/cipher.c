d60d3c944379172b12b3b4fd312213941392e071
Bruno De Smet <bruno.de.smet@nxp.com>
2024-07-31 06:04:48 +0200

    SSMW-680 subsystems: seco: Change smw_cipher_operation_t to enum type
    In the SMW public API, the cipher operation type name
    is not a string anymore but an enum.
    smw_cipher_operation_t is renamed as smw_cipher_op_type_t.
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- core/subsystems/seco/cipher.c
+++ core/subsystems/seco/cipher.c
@@ -49,7 +49,7 @@ static int set_cipher_algo(enum smw_config_key_type_id key_type_id,
 
 #define CIPHER_FLAG(_op_type_id)                                               \
 	{                                                                      \
-		.smw_op_type_id = SMW_CONFIG_CIPHER_OP_ID_##_op_type_id,       \
+		.smw_op_type_id = SMW_CONFIG_CIPHER_OP_TYPE_ID_##_op_type_id,  \
 		.flags = HSM_CIPHER_ONE_GO_FLAGS_##_op_type_id                 \
 	}
 
@@ -103,7 +103,7 @@ static int cipher(struct hdl *hdl, void *args)
 		goto end;
 
 	/* Set flags */
-	status = set_cipher_flags(cipher_args->op_id, &op_args.flags);
+	status = set_cipher_flags(cipher_args->op_type_id, &op_args.flags);
 	if (status != SMW_STATUS_OK)
 		goto end;
 

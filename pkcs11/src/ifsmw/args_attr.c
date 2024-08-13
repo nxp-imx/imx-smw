nxp-imx/imx-smw

commit 9cb327b21e3d13564c4c936714bc238d2c6750fa

SSMW-616 pkcs11: Remove TLV attributes list
Implement the new format for signature, key and data attributes.

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

pkcs11/src/ifsmw/args_attr.c
@@ -3,261 +3,132 @@
  * Copyright 2021-2024 NXP
  */
 
+#include "smw/attr.h"
+
 #include "libobj_types.h"
 
 #include "args_attr.h"
-#include "tlv_encode.h"
 
-#include "trace.h"
-
-static CK_RV set_sign_usage(struct smw_tlv *policy,
-			    struct smw_tlv *allowed_algos)
+static void set_sign_usage(smw_attr_usage_t *usage_flags)
 {
-	CK_RV ret = CKR_OK;
-
-	ret = tlv_encode_concat_string(policy, SMW_ATTR_USAGE,
-				       SMW_ATTR_USAGE_SIGN_MSG, allowed_algos);
-	if (ret == CKR_OK)
-		ret = tlv_encode_concat_string(policy, SMW_ATTR_USAGE,
-					       SMW_ATTR_USAGE_SIGN_HASH,
-					       allowed_algos);
-
-	return ret;
+	SMW_ATTR_USAGE_SET_SIGN_MESSAGE(*usage_flags);
+	SMW_ATTR_USAGE_SET_SIGN_HASH(*usage_flags);
 }
 
-static CK_RV set_verify_usage(struct smw_tlv *policy,
-			      struct smw_tlv *allowed_algos)
+static void set_verify_usage(smw_attr_usage_t *usage_flags)
 {
-	CK_RV ret = CKR_OK;
-
-	ret = tlv_encode_concat_string(policy, SMW_ATTR_USAGE,
-				       SMW_ATTR_USAGE_VERIFY_MSG,
-				       allowed_algos);
-	if (ret == CKR_OK)
-		ret = tlv_encode_concat_string(policy, SMW_ATTR_USAGE,
-					       SMW_ATTR_USAGE_VERIFY_HASH,
-					       allowed_algos);
-
-	return ret;
+	SMW_ATTR_USAGE_SET_VERIFY_MESSAGE(*usage_flags);
+	SMW_ATTR_USAGE_SET_VERIFY_HASH(*usage_flags);
 }
 
-static CK_RV set_common_key_usage(struct smw_tlv *policy,
-				  struct libobj_obj *obj,
-				  struct smw_tlv *allowed_algos)
+static void set_common_key_usage(smw_attr_usage_t *usage_flags,
+				 struct libobj_obj *obj)
 {
-	CK_RV ret = CKR_OK;
-
 	if (is_copyable_obj(obj, storage))
-		ret = tlv_encode_string(policy, SMW_ATTR_USAGE,
-					SMW_ATTR_USAGE_COPY);
+		SMW_ATTR_USAGE_SET_COPY(*usage_flags);
 
-	if (ret == CKR_OK && is_derive_key(obj))
-		ret = tlv_encode_concat_string(policy, SMW_ATTR_USAGE,
-					       SMW_ATTR_USAGE_DERIVE,
-					       allowed_algos);
-
-	return ret;
+	if (is_derive_key(obj))
+		SMW_ATTR_USAGE_SET_DERIVE(*usage_flags);
 }
 
-static CK_RV set_public_key_usage(struct smw_tlv *policy,
-				  struct libobj_obj *obj,
-				  struct smw_tlv *allowed_algos)
+static void set_public_key_usage(smw_attr_usage_t *usage_flags,
+				 struct libobj_obj *obj)
 {
-	CK_RV ret = CKR_OK;
 	struct libobj_key_public *key = get_key_from(obj);
 
 	if (key->encrypt)
-		ret = tlv_encode_concat_string(policy, SMW_ATTR_USAGE,
-					       SMW_ATTR_USAGE_ENCRYPT,
-					       allowed_algos);
+		SMW_ATTR_USAGE_SET_ENCRYPT(*usage_flags);
 
-	if (ret == CKR_OK && key->verify)
-		ret = set_verify_usage(policy, allowed_algos);
-
-	return ret;
+	if (key->verify)
+		set_verify_usage(usage_flags);
 }
 
-static CK_RV set_private_key_usage(struct smw_tlv *policy,
-				   struct libobj_obj *obj,
-				   struct smw_tlv *allowed_algos)
+static void set_private_key_usage(smw_attr_usage_t *usage_flags,
+				  struct libobj_obj *obj)
 {
-	CK_RV ret = CKR_OK;
 	struct libobj_key_private *key = get_key_from(obj);
 
 	if (key->decrypt)
-		ret = tlv_encode_concat_string(policy, SMW_ATTR_USAGE,
-					       SMW_ATTR_USAGE_DECRYPT,
-					       allowed_algos);
+		SMW_ATTR_USAGE_SET_DECRYPT(*usage_flags);
 
-	if (ret == CKR_OK && key->sign)
-		ret = set_sign_usage(policy, allowed_algos);
+	if (key->sign)
+		set_sign_usage(usage_flags);
 
-	if (ret == CKR_OK && key->extractable && !key->sensitive)
-		ret = tlv_encode_string(policy, SMW_ATTR_USAGE,
-					SMW_ATTR_USAGE_EXPORT);
-
-	return ret;
+	if (key->extractable && !key->sensitive)
+		SMW_ATTR_USAGE_SET_EXPORT(*usage_flags);
 }
 
-static CK_RV set_secret_key_usage(struct smw_tlv *policy,
-				  struct libobj_obj *obj,
-				  struct smw_tlv *allowed_algos)
+static void set_secret_key_usage(smw_attr_usage_t *usage_flags,
+				 struct libobj_obj *obj)
 {
-	CK_RV ret = CKR_OK;
 	struct libobj_key_secret *key = get_key_from(obj);
 
 	if (key->encrypt)
-		ret = tlv_encode_concat_string(policy, SMW_ATTR_USAGE,
-					       SMW_ATTR_USAGE_ENCRYPT,
-					       allowed_algos);
+		SMW_ATTR_USAGE_SET_ENCRYPT(*usage_flags);
 
-	if (ret == CKR_OK && key->decrypt)
-		ret = tlv_encode_concat_string(policy, SMW_ATTR_USAGE,
-					       SMW_ATTR_USAGE_DECRYPT,
-					       allowed_algos);
+	if (key->decrypt)
+		SMW_ATTR_USAGE_SET_DECRYPT(*usage_flags);
 
-	if (ret == CKR_OK && key->sign)
-		ret = set_sign_usage(policy, allowed_algos);
+	if (key->sign)
+		set_sign_usage(usage_flags);
 
-	if (ret == CKR_OK && key->verify)
-		ret = set_verify_usage(policy, allowed_algos);
+	if (key->verify)
+		set_verify_usage(usage_flags);
 
-	if (ret == CKR_OK && key->extractable && !key->sensitive)
-		ret = tlv_encode_concat_string(policy, SMW_ATTR_USAGE,
-					       SMW_ATTR_USAGE_EXPORT,
-					       allowed_algos);
-
-	return ret;
+	if (key->extractable && !key->sensitive)
+		SMW_ATTR_USAGE_SET_EXPORT(*usage_flags);
 }
 
-static CK_RV set_ec_key_usage(struct smw_tlv *policy, struct libobj_obj *obj,
-			      struct smw_tlv *allowed_algos)
+static void set_ec_key_usage(smw_attr_usage_t *usage_flags,
+			     struct libobj_obj *obj)
 {
-	CK_RV ret = CKR_OK;
 	struct libobj_key_ec_pair *key = get_subkey_from(obj);
 
 	switch (key->type) {
 	case LIBOBJ_KEY_PUBLIC:
-		ret = set_public_key_usage(policy, obj, allowed_algos);
+		set_public_key_usage(usage_flags, obj);
 		break;
 
 	case LIBOBJ_KEY_PRIVATE:
-		ret = set_private_key_usage(policy, obj, allowed_algos);
+		set_private_key_usage(usage_flags, obj);
 		break;
 
 	default:
-		ret = set_private_key_usage(policy, obj, allowed_algos);
-		if (ret == CKR_OK && key->pub_obj)
-			ret = set_public_key_usage(policy, key->pub_obj,
-						   allowed_algos);
+		set_private_key_usage(usage_flags, obj);
+
+		if (key->pub_obj)
+			set_public_key_usage(usage_flags, key->pub_obj);
+
 		break;
 	}
-
-	return ret;
 }
 
-static CK_RV set_rsa_key_usage(struct smw_tlv *policy, struct libobj_obj *obj,
-			       struct smw_tlv *allowed_algos)
+static void set_rsa_key_usage(smw_attr_usage_t *usage_flags,
+			      struct libobj_obj *obj)
 {
-	CK_RV ret = CKR_OK;
 	struct libobj_key_rsa_pair *key = get_subkey_from(obj);
 
 	switch (key->type) {
 	case LIBOBJ_KEY_PUBLIC:
-		ret = set_public_key_usage(policy, obj, allowed_algos);
+		set_public_key_usage(usage_flags, obj);
 		break;
 
 	case LIBOBJ_KEY_PRIVATE:
-		ret = set_private_key_usage(policy, obj, allowed_algos);
+		set_private_key_usage(usage_flags, obj);
 		break;
 
 	default:
-		ret = set_private_key_usage(policy, obj, allowed_algos);
-		if (ret == CKR_OK && key->pub_obj)
-			ret = set_public_key_usage(policy, key->pub_obj,
-						   allowed_algos);
+		set_private_key_usage(usage_flags, obj);
+
+		if (key->pub_obj)
+			set_public_key_usage(usage_flags, key->pub_obj);
+
 		break;
 	}
-
-	return ret;
 }
 
-static CK_RV rsa_key_attr(struct smw_tlv *attr, struct libobj_obj *obj)
+void args_attrs_key_usage(smw_attr_usage_t *usage_flags, struct libobj_obj *obj)
 {
-	CK_RV ret = CKR_OK;
-	struct libobj_key_rsa_pair *key = get_subkey_from(obj);
-
-	/*
-	 * The public exponent might be present and define
-	 * the RSA public exponent attribute.
-	 */
-	if (key->pub_exp.value) {
-		DBG_TRACE("Build RSA public exponent attribute");
-		ret = tlv_encode_large_numeral(attr, "RSA_PUB_EXP",
-					       &key->pub_exp);
-	}
-
-	return ret;
-}
-
-CK_RV args_attr_generate_key(struct smw_tlv *attr, struct libobj_obj *obj)
-{
-	CK_RV ret = CKR_OK;
-
-	if (is_token_obj(obj, storage)) {
-		DBG_TRACE("Generate Persistent Key");
-		ret = tlv_encode_boolean(attr, "PERSISTENT");
-		if (ret != CKR_OK)
-			return ret;
-	}
-
-	switch (get_key_type(obj)) {
-	case CKK_RSA:
-		ret = rsa_key_attr(attr, obj);
-		break;
-
-	default:
-		break;
-	}
-
-	return ret;
-}
-
-CK_RV args_attr_import_key(struct smw_tlv *attr, struct libobj_obj *obj)
-{
-	CK_RV ret = CKR_OK;
-
-	if (is_token_obj(obj, storage)) {
-		DBG_TRACE("Import Persistent Key");
-		ret = tlv_encode_boolean(attr, "PERSISTENT");
-	}
-
-	return ret;
-}
-
-CK_RV args_attr_sign_verify(struct smw_tlv *attr, const char *signature_type,
-			    CK_ULONG salt_len)
-{
-	CK_RV ret = CKR_OK;
-
-	if (signature_type) {
-		ret = tlv_encode_enum(attr, "SIGNATURE_TYPE", signature_type);
-		if (ret != CKR_OK)
-			return ret;
-	}
-
-	if (salt_len)
-		ret = tlv_encode_numeral(attr, "SALT_LEN", salt_len);
-
-	return ret;
-}
-
-CK_RV args_attrs_key_policy(struct smw_tlv *attr, struct libobj_obj *obj,
-			    struct smw_tlv *allowed_algos)
-{
-	CK_RV ret = CKR_FUNCTION_FAILED;
-	struct smw_tlv policy = { 0 };
-
 	switch (get_key_type(obj)) {
 	case CKK_AES:
 	case CKK_DES:
@@ -273,38 +144,32 @@ CK_RV args_attrs_key_policy(struct smw_tlv *attr, struct libobj_obj *obj,
 	case CKK_SHA3_256_HMAC:
 	case CKK_SHA3_384_HMAC:
 	case CKK_SHA3_512_HMAC:
-		ret = set_secret_key_usage(&policy, obj, allowed_algos);
+		set_secret_key_usage(usage_flags, obj);
 		break;
 
 	case CKK_EC:
-		ret = set_ec_key_usage(&policy, obj, allowed_algos);
+		set_ec_key_usage(usage_flags, obj);
 		break;
 
 	case CKK_RSA:
-		ret = set_rsa_key_usage(&policy, obj, allowed_algos);
+		set_rsa_key_usage(usage_flags, obj);
 		break;
 
 	default:
 		break;
 	}
 
-	if (ret == CKR_OK)
-		ret = set_common_key_usage(&policy, obj, allowed_algos);
-
-	if (ret == CKR_OK && policy.string)
-		ret = tlv_encode_tlv(attr, SMW_ATTR_POLICY, &policy);
-
-	tlv_encode_free(&policy);
-
-	return ret;
+	set_common_key_usage(usage_flags, obj);
 }
 
-CK_RV args_attrs_store_data(struct smw_tlv *attr, struct libobj_obj *obj)
+void args_attr_key_storage(smw_attr_attributes_t *attr, struct libobj_obj *obj)
 {
-	CK_RV ret = CKR_OK;
+	if (is_token_obj(obj, storage))
+		*attr = SMW_ATTR_SET_PERSISTENT(*attr);
+}
 
+void args_attr_data_storage(smw_attr_attributes_t *attr, struct libobj_obj *obj)
+{
 	if (!is_modifiable_obj(obj, storage))
-		ret = tlv_encode_boolean(attr, "READ_ONLY");
-
-	return ret;
+		*attr = SMW_ATTR_SET_READ_ONLY(*attr);
 }

pkcs11/src/ifsmw/args_attr.h
@@ -8,84 +8,36 @@
 #include "pkcs11smw.h"
 #include "types.h"
 
-#include "tlv_encode.h"
-
-/*
- * Definition of the SMW key policy TLV's attribute type
- */
-#define SMW_ATTR_ALGO		   "ALGO"
-#define SMW_ATTR_USAGE		   "USAGE"
-#define SMW_ATTR_POLICY		   "POLICY"
-#define SMW_ATTR_HASH		   "HASH"
-#define SMW_ATTR_USAGE_COPY	   "COPY"
-#define SMW_ATTR_USAGE_DERIVE	   "DERIVE"
-#define SMW_ATTR_USAGE_ENCRYPT	   "ENCRYPT"
-#define SMW_ATTR_USAGE_DECRYPT	   "DECRYPT"
-#define SMW_ATTR_USAGE_SIGN_MSG	   "SIGN_MESSAGE"
-#define SMW_ATTR_USAGE_SIGN_HASH   "SIGN_HASH"
-#define SMW_ATTR_USAGE_VERIFY_MSG  "VERIFY_MESSAGE"
-#define SMW_ATTR_USAGE_VERIFY_HASH "VERIFY_HASH"
-#define SMW_ATTR_USAGE_EXPORT	   "EXPORT"
-
 /**
- * args_attr_generate_key() - Build the key generate attribute list
- * @attr: Generate key attribute list
+ * args_attrs_key_usage() - Build the key usage flags
+ * @usage_flags: Usage flags
  * @obj: Key object
  *
  * Return:
- * CKR_HOST_MEMORY               - Out of memory
- * CKR_OK                        - Success
+ * None.
  */
-CK_RV args_attr_generate_key(struct smw_tlv *attr, struct libobj_obj *obj);
+void args_attrs_key_usage(smw_attr_usage_t *usage_flags,
+			  struct libobj_obj *obj);
 
 /**
- * args_attr_import_key() - Build the key import attribute list
- * @attr: Import key attribute list
- * @obj: Key object
+ * args_attr_key_storage() - Set the key storage attributes
+ * @attr: Attributes
+ * @obj: Object
  *
  * Return:
- * CKR_HOST_MEMORY               - Out of memory
- * CKR_OK                        - Success
+ * None.
  */
-CK_RV args_attr_import_key(struct smw_tlv *attr, struct libobj_obj *obj);
+void args_attr_key_storage(smw_attr_attributes_t *attr, struct libobj_obj *obj);
 
 /**
- * args_attr_sign_verify() - Build the sign or verify attribute list
- * @attr: Sign or verify attribute list
- * @signature_type: Signature type attribute
- * @salt_len: Salt length attribute
+ * args_attr_data_storage() - Set the data storage attributes
+ * @attr: Attributes
+ * @obj: Object
  *
  * Return:
- * CKR_HOST_MEMORY               - Out of memory
- * CKR_OK                        - Success
+ * None.
  */
-CK_RV args_attr_sign_verify(struct smw_tlv *attr, const char *signature_type,
-			    CK_ULONG salt_len);
-
-/**
- * args_attrs_key_policy() - Build the key policy attribute list
- * @attr: Attribute list
- * @obj: Key object
- * @allowed_algos: Allowed key's algorithms
- *
- * Return:
- * CKR_FUNCTION_FAILED           - Operation failed
- * CKR_HOST_MEMORY               - Out of memory
- * CKR_OK                        - Success
- */
-CK_RV args_attrs_key_policy(struct smw_tlv *attr, struct libobj_obj *obj,
-			    struct smw_tlv *allowed_algos);
-
-/**
- * args_attrs_store_data() - Build the store data attribute list
- * @attr: Attribute list
- * @obj: Data object
- *
- * Return:
- * CKR_HOST_MEMORY               - Out of memory
- * CKR_ARGUMENTS_BAD             - Bad arguments
- * CKR_OK                        - Success
- */
-CK_RV args_attrs_store_data(struct smw_tlv *attr, struct libobj_obj *obj);
+void args_attr_data_storage(smw_attr_attributes_t *attr,
+			    struct libobj_obj *obj);
 
 #endif /* __ARGS_ATTR_H__ */

pkcs11/src/ifsmw/data.c
@@ -17,7 +17,6 @@
 #include "libobj_types.h"
 
 #include "args_attr.h"
-#include "tlv_encode.h"
 
 #define DATA_LABEL(name)                                                       \
 	{                                                                      \
@@ -136,7 +135,7 @@ static int store_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
 	struct smw_store_data_args args = { 0 };
 	struct smw_data_descriptor data_descriptor = { 0 };
 	struct libobj_data *data = get_subobj_from(obj, storage);
-	struct smw_tlv data_attr = { 0 };
+	struct smw_data_attributes data_attr = { 0 };
 
 	ret = libsess_get_slotid(hsession, &slotid);
 	if (ret != CKR_OK)
@@ -150,17 +149,13 @@ static int store_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
 	if (ret != CKR_OK)
 		return ret;
 
-	ret = args_attrs_store_data(&data_attr, obj);
-	if (ret != CKR_OK)
-		return ret;
-
 	data_descriptor.data = data->value.array;
 
 	if (SET_OVERFLOW(data->value.number, data_descriptor.length))
 		return CKR_FUNCTION_FAILED;
 
-	data_descriptor.attributes_list = (unsigned char *)data_attr.string;
-	data_descriptor.attributes_list_length = data_attr.length;
+	args_attr_data_storage(&data_attr.attributes, obj);
+	data_descriptor.data_attributes = &data_attr;
 
 	args.subsystem_name = devinfo->name;
 	args.data_descriptor = &data_descriptor;
@@ -170,8 +165,6 @@ static int store_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
 		ret = CKR_OK;
 	else
 		ret = CKR_FUNCTION_FAILED;
-
-	tlv_encode_free(&data_attr);
 
 	return ret;
 }

pkcs11/src/ifsmw/key_desc.c
@@ -297,6 +297,11 @@ static CK_RV rsa_key_desc(struct smw_key_descriptor *desc,
 		smw_key->private_data = key->priv_exp.value;
 		if (SET_OVERFLOW(key->priv_exp.length, smw_key->private_length))
 			return CKR_ARGUMENTS_BAD;
+
+		smw_key->public_exponent = key->pub_exp.value;
+		if (SET_OVERFLOW(key->pub_exp.length,
+				 smw_key->public_exponent_length))
+			return CKR_ARGUMENTS_BAD;
 	}
 
 	return CKR_OK;

pkcs11/src/ifsmw/tlv_encode.c
@@ -1,370 +1,0 @@
-// SPDX-License-Identifier: BSD-3-Clause
-/*
- * Copyright 2021-2023 NXP
- */
-
-#include <stdlib.h>
-#include <string.h>
-
-#include "util.h"
-
-#include "tlv_encode.h"
-
-#define TLV_LENGTH_SIZE 2
-
-static size_t tlv_string_size(const char *type, size_t value_length)
-{
-	size_t size = 0;
-
-	size = strlen(type);
-	if (!INC_OVERFLOW(size, 1) && !INC_OVERFLOW(size, TLV_LENGTH_SIZE) &&
-	    !INC_OVERFLOW(size, value_length))
-		return size;
-
-	return 0;
-}
-
-static CK_RV tlv_realloc(struct smw_tlv *tlv, size_t length)
-{
-	size_t add_length = 0;
-
-	if (SUB_OVERFLOW(tlv->length_max, tlv->length, &add_length))
-		return CKR_ARGUMENTS_BAD;
-
-	if (INC_OVERFLOW(add_length, length))
-		return CKR_ARGUMENTS_BAD;
-
-	if (!tlv->string) {
-		tlv->string = malloc(length);
-		if (!tlv->string)
-			return CKR_HOST_MEMORY;
-
-		if (SET_OVERFLOW(length, tlv->length_max)) {
-			free(tlv->string);
-			tlv->string = NULL;
-			return CKR_ARGUMENTS_BAD;
-		}
-	} else if (add_length) {
-		tlv->string =
-			realloc(tlv->string, tlv->length_max + add_length);
-		if (!tlv->string)
-			return CKR_HOST_MEMORY;
-
-		tlv->length_max += add_length;
-	}
-
-	return CKR_OK;
-}
-
-static CK_RV set_tlv_type(struct smw_tlv *tlv, const char *type)
-{
-	CK_RV ret = CKR_ARGUMENTS_BAD;
-	size_t length = 0;
-	unsigned int index = tlv->length;
-
-	length = strlen(type) + 1;
-
-	if (!INC_OVERFLOW(tlv->length, length)) {
-		/* Add the Type @type to current tlv string */
-		memcpy(&tlv->string[index], type, length);
-
-		ret = CKR_OK;
-	}
-
-	return ret;
-}
-
-static CK_RV set_tlv_length(struct smw_tlv *tlv, size_t length)
-{
-	CK_RV ret = CKR_ARGUMENTS_BAD;
-	uint8_t byte = 0;
-	char *tlv_string = &tlv->string[tlv->length];
-
-	if (!INC_OVERFLOW(tlv->length, 2)) {
-		byte = GET_BYTE(length, 1);
-		(void)SET_OVERFLOW(byte, *tlv_string);
-
-		byte = GET_BYTE(length, 0);
-		(void)SET_OVERFLOW(byte, *(++tlv_string));
-
-		ret = CKR_OK;
-	}
-
-	return ret;
-}
-
-static CK_RV set_tlv_value(struct smw_tlv *tlv, const void *value,
-			   size_t value_len)
-{
-	CK_RV ret = CKR_ARGUMENTS_BAD;
-	unsigned int index = tlv->length;
-
-	if (!INC_OVERFLOW(tlv->length, value_len)) {
-		memcpy(&tlv->string[index], value, value_len);
-		ret = CKR_OK;
-	}
-
-	return ret;
-}
-
-CK_RV tlv_encode_boolean(struct smw_tlv *tlv, const char *type)
-{
-	CK_RV ret = CKR_ARGUMENTS_BAD;
-	size_t len_add = 0;
-
-	/*
-	 * Boolean is TLV encoded with:
-	 *   - Type = null terminated string @type
-	 *   - Length = 2 bytes of 0
-	 *   - Value is not set
-	 */
-	len_add = tlv_string_size(type, 0);
-	if (!len_add)
-		return ret;
-
-	ret = tlv_realloc(tlv, len_add);
-	if (ret != CKR_OK)
-		return ret;
-
-	ret = set_tlv_type(tlv, type);
-	if (ret == CKR_OK)
-		/* Set the 2 bytes of Length = 0 */
-		ret = set_tlv_length(tlv, 0);
-
-	return ret;
-}
-
-CK_RV tlv_encode_large_numeral(struct smw_tlv *tlv, const char *type,
-			       struct libbignumber *bignum)
-{
-	CK_RV ret = CKR_ARGUMENTS_BAD;
-	size_t len_add = 0;
-
-	/*
-	 * Large numeral is TLV encoded with:
-	 *   - Type = null terminated string @type
-	 *   - Length = 2 bytes (numeral byte length)
-	 *   - Value = numeral value
-	 */
-	len_add = tlv_string_size(type, bignum->length);
-	if (!len_add)
-		return ret;
-
-	ret = tlv_realloc(tlv, len_add);
-	if (ret != CKR_OK)
-		return ret;
-
-	ret = set_tlv_type(tlv, type);
-	if (ret != CKR_OK)
-		return ret;
-
-	/* Set the 2 bytes of Length */
-	ret = set_tlv_length(tlv, bignum->length);
-	if (ret == CKR_OK)
-		/* Set value */
-		ret = set_tlv_value(tlv, bignum->value, bignum->length);
-
-	return ret;
-}
-
-CK_RV tlv_encode_string(struct smw_tlv *tlv, const char *type,
-			const char *value)
-{
-	CK_RV ret = CKR_ARGUMENTS_BAD;
-	size_t len_add = 0;
-	size_t value_length = 0;
-
-	/*
-	 * String is TLV encoded with:
-	 *   - Type = null terminated string @type
-	 *   - Length = 2 bytes (@value length, null character included)
-	 *   - Value = null terminated string @value
-	 */
-	value_length = strlen(value);
-	if (INC_OVERFLOW(value_length, 1))
-		return ret;
-
-	len_add = tlv_string_size(type, value_length);
-	if (!len_add)
-		return ret;
-
-	ret = tlv_realloc(tlv, len_add);
-	if (ret != CKR_OK)
-		return ret;
-
-	/* Set type */
-	ret = set_tlv_type(tlv, type);
-	if (ret != CKR_OK)
-		return ret;
-
-	/* Set length */
-	ret = set_tlv_length(tlv, value_length);
-	if (ret == CKR_OK)
-		/* Set value */
-		ret = set_tlv_value(tlv, value, value_length);
-
-	return ret;
-}
-
-CK_RV tlv_encode_enum(struct smw_tlv *tlv, const char *type, const char *value)
-{
-	/*
-	 * Enumeration is TLV encoded with:
-	 *   - Type = null terminated string @type
-	 *   - Length = 2 bytes (@value length, null character included)
-	 *   - Value = null terminated string @value
-	 */
-
-	return tlv_encode_string(tlv, type, value);
-}
-
-CK_RV tlv_encode_numeral(struct smw_tlv *tlv, const char *type, long long num)
-{
-	CK_RV ret = CKR_ARGUMENTS_BAD;
-	size_t len_add = 0;
-	unsigned int nb_bytes = 0;
-	uint8_t byte = 0;
-
-	/* Get number of bytes needed to store @num */
-	if (num >> 32)
-		nb_bytes = 8;
-	else if (num >> 16)
-		nb_bytes = 4;
-	else if (num >> 8)
-		nb_bytes = 2;
-	else
-		nb_bytes = 1;
-
-	len_add = tlv_string_size(type, nb_bytes);
-	if (!len_add)
-		return ret;
-
-	ret = tlv_realloc(tlv, len_add);
-	if (ret != CKR_OK)
-		return ret;
-
-	/* Set type */
-	ret = set_tlv_type(tlv, type);
-	if (ret != CKR_OK)
-		return ret;
-
-	/* Set length */
-	ret = set_tlv_length(tlv, nb_bytes);
-	if (ret != CKR_OK)
-		return ret;
-
-	for (; nb_bytes; nb_bytes--) {
-		byte = GET_BYTE(num, nb_bytes - 1);
-		(void)SET_OVERFLOW(byte, tlv->string[tlv->length]);
-
-		if (INC_OVERFLOW(tlv->length, 1)) {
-			ret = CKR_ARGUMENTS_BAD;
-			break;
-		}
-	}
-
-	return ret;
-}
-
-CK_RV tlv_encode_concat_string(struct smw_tlv *tlv, const char *type,
-			       const char *value, struct smw_tlv *concat_tlv)
-{
-	CK_RV ret = CKR_ARGUMENTS_BAD;
-	size_t len_add = 0;
-	size_t value_length = 0;
-	size_t tlv_length = 0;
-
-	/*
-	 * Concat string is TLV encoded with:
-	 *   - Type = null terminated string @type
-	 *   - Length = 2 bytes (@value length, null character included) +
-	 *              length of the string value to concatenate
-	 *   - Value = null terminated string @value
-	 *   - String value to add after field Value
-	 */
-
-	value_length = strlen(value);
-	if (INC_OVERFLOW(value_length, 1))
-		return ret;
-
-	len_add = tlv_string_size(type, value_length);
-	if (!len_add)
-		return ret;
-
-	if (INC_OVERFLOW(len_add, concat_tlv->length_max))
-		return ret;
-
-	if (ADD_OVERFLOW(value_length, concat_tlv->length_max, &tlv_length))
-		return ret;
-
-	ret = tlv_realloc(tlv, len_add);
-	if (ret != CKR_OK)
-		return ret;
-
-	/* Set type */
-	ret = set_tlv_type(tlv, type);
-	if (ret != CKR_OK)
-		return ret;
-
-	/* Set length */
-	ret = set_tlv_length(tlv, tlv_length);
-	if (ret != CKR_OK)
-		return ret;
-
-	/* Set value - Input @value */
-	ret = set_tlv_value(tlv, value, value_length);
-
-	/* Concatenate the string value */
-	if (ret == CKR_OK && concat_tlv->string)
-		ret = set_tlv_value(tlv, concat_tlv->string,
-				    concat_tlv->length_max);
-
-	return ret;
-}
-
-CK_RV tlv_encode_tlv(struct smw_tlv *tlv, const char *type,
-		     struct smw_tlv *tlv_value)
-{
-	CK_RV ret = CKR_ARGUMENTS_BAD;
-	size_t len_add = 0;
-
-	/*
-	 * Build a TLV of TLV is TLV encoded with:
-	 *   - Type = null terminated string @type
-	 *   - Length = 2 bytes (@tlv_value->length_max)
-	 *   - Value = @tlv_value->string
-	 */
-
-	len_add = tlv_string_size(type, tlv_value->length_max);
-	if (!len_add)
-		return ret;
-
-	ret = tlv_realloc(tlv, len_add);
-	if (ret != CKR_OK)
-		return ret;
-
-	/* Set type */
-	ret = set_tlv_type(tlv, type);
-	if (ret != CKR_OK)
-		return ret;
-
-	/* Set length */
-	ret = set_tlv_length(tlv, tlv_value->length_max);
-
-	/* Concatenate the string value */
-	if (ret == CKR_OK && tlv_value->string)
-		ret = set_tlv_value(tlv, tlv_value->string,
-				    tlv_value->length_max);
-
-	return ret;
-}
-
-void tlv_encode_free(struct smw_tlv *tlv)
-{
-	if (tlv->string)
-		free(tlv->string);
-
-	tlv->string = NULL;
-	tlv->length = 0;
-	tlv->length_max = 0;
-}

commit c34860dbaf1df247e288a8fa161fe2745401ba80

SSMW-694 pkcs11: ensure usage and permitted algorithm set
Ensure that the key usage and permitted algorithm are set as expected
by the SMW library.

Fixes: 9cb327b21e3d ("SSMW-616 pkcs11: Remove TLV attributes list")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/ifsmw/args_attr.c
@@ -9,18 +9,6 @@
 
 #include "args_attr.h"
 
-static void set_sign_usage(smw_attr_usage_t *usage_flags)
-{
-	SMW_ATTR_USAGE_SET_SIGN_MESSAGE(*usage_flags);
-	SMW_ATTR_USAGE_SET_SIGN_HASH(*usage_flags);
-}
-
-static void set_verify_usage(smw_attr_usage_t *usage_flags)
-{
-	SMW_ATTR_USAGE_SET_VERIFY_MESSAGE(*usage_flags);
-	SMW_ATTR_USAGE_SET_VERIFY_HASH(*usage_flags);
-}
-
 static void set_common_key_usage(smw_attr_usage_t *usage_flags,
 				 struct libobj_obj *obj)
 {
@@ -39,8 +27,10 @@ static void set_public_key_usage(smw_attr_usage_t *usage_flags,
 	if (key->encrypt)
 		SMW_ATTR_USAGE_SET_ENCRYPT(*usage_flags);
 
-	if (key->verify)
-		set_verify_usage(usage_flags);
+	if (key->verify) {
+		SMW_ATTR_USAGE_SET_VERIFY_MESSAGE(*usage_flags);
+		SMW_ATTR_USAGE_SET_VERIFY_HASH(*usage_flags);
+	}
 }
 
 static void set_private_key_usage(smw_attr_usage_t *usage_flags,
@@ -51,8 +41,10 @@ static void set_private_key_usage(smw_attr_usage_t *usage_flags,
 	if (key->decrypt)
 		SMW_ATTR_USAGE_SET_DECRYPT(*usage_flags);
 
-	if (key->sign)
-		set_sign_usage(usage_flags);
+	if (key->sign) {
+		SMW_ATTR_USAGE_SET_SIGN_MESSAGE(*usage_flags);
+		SMW_ATTR_USAGE_SET_SIGN_HASH(*usage_flags);
+	}
 
 	if (key->extractable && !key->sensitive)
 		SMW_ATTR_USAGE_SET_EXPORT(*usage_flags);
@@ -70,10 +62,10 @@ static void set_secret_key_usage(smw_attr_usage_t *usage_flags,
 		SMW_ATTR_USAGE_SET_DECRYPT(*usage_flags);
 
 	if (key->sign)
-		set_sign_usage(usage_flags);
+		SMW_ATTR_USAGE_SET_SIGN_MESSAGE(*usage_flags);
 
 	if (key->verify)
-		set_verify_usage(usage_flags);
+		SMW_ATTR_USAGE_SET_VERIFY_MESSAGE(*usage_flags);
 
 	if (key->extractable && !key->sensitive)
 		SMW_ATTR_USAGE_SET_EXPORT(*usage_flags);

pkcs11/src/ifsmw/mechanism.c
@@ -222,7 +222,7 @@ struct mgroup {
 	M_ALGO(_algo, SMW_HASH_ALGO_NAME_NONE, SMW_MAC_ALGO_NAME_NONE,         \
 	       SMW_CIPHER_MODE_NAME_##_mode, SMW_AEAD_MODE_NAME_NONE,          \
 	       SMW_SIGNATURE_ALGO_NAME_NONE, SMW_SIGNATURE_TYPE_NAME_NONE,     \
-	       SMW_ATTR_ALGO_SYMMETRIC_ENCRYPTION(SMW_ATTR_ALGO_##_algo,       \
+	       SMW_ATTR_ALGO_SYMMETRIC_ENCRYPTION(SMW_ATTR_ALGO_DEFAULT,       \
 						  SMW_ATTR_MODE_##_mode_id),   \
 	       _id)
 
@@ -230,8 +230,8 @@ struct mgroup {
 	M_ALGO(_algo, SMW_HASH_ALGO_NAME_NONE, SMW_MAC_ALGO_NAME_NONE,         \
 	       SMW_CIPHER_MODE_NAME_NONE, SMW_AEAD_MODE_NAME_##_mode,          \
 	       SMW_SIGNATURE_ALGO_NAME_NONE, SMW_SIGNATURE_TYPE_NAME_NONE,     \
-	       SMW_ATTR_ALGO_SYMMETRIC_ENCRYPTION(SMW_ATTR_ALGO_##_algo,       \
-						  SMW_ATTR_MODE_##_mode_id),   \
+	       SMW_ATTR_ALGO_AEAD(SMW_ATTR_ALGO_##_algo,                       \
+				  SMW_ATTR_MODE_##_mode_id, 0),                \
 	       _id)
 
 #define M_MAC(_algo, _mac, _mode_id, _id)                                      \
@@ -420,6 +420,13 @@ static CK_RV smw_status_to_ck_rv(enum smw_status_code status)
 	case SMW_STATUS_INVALID_IV_SIZE:
 		return CKR_MECHANISM_PARAM_INVALID;
 
+	case SMW_STATUS_KEY_INVALID:
+		return CKR_KEY_FUNCTION_NOT_PERMITTED;
+
+	case SMW_STATUS_OPERATION_NOT_CONFIGURED:
+	case SMW_STATUS_OPERATION_NOT_SUPPORTED:
+		return CKR_FUNCTION_NOT_SUPPORTED;
+
 	default:
 		return CKR_DEVICE_ERROR;
 	}
@@ -515,11 +522,11 @@ static CK_RV get_key_permitted_algo(smw_attr_algo_t *permitted_algo,
 		if (ret != CKR_OK) {
 			DBG_TRACE("Key allowed mechanism 0x%lx error %ld",
 				  mech_list->mech[0], ret);
+		} else {
+			DBG_TRACE("Key permitted algorithm 0x%" PRIx64,
+				  entry->smw_algo_id);
+			*permitted_algo = entry->smw_algo_id;
 		}
-
-		DBG_TRACE("Key permitted algorithm 0x%" PRIx64,
-			  entry->smw_algo_id);
-		*permitted_algo = entry->smw_algo_id;
 	}
 
 	return ret;
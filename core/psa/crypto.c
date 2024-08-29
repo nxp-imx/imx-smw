31e460f66b1346ba5a9876e8cad4d43472260a2e
Bruno De Smet <bruno.de.smet@nxp.com>
2024-08-29 10:25:23 +0200

    SSMW-594 core: psa: Implement psa_key_agreement()
    This function is not supported yet and returns PSA_ERROR_NOT_SUPPORTED.
    
    Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

--- core/psa/crypto.c
+++ core/psa/crypto.c
@@ -1401,6 +1401,24 @@ __export psa_status_t psa_purge_key(psa_key_id_t key)
 	return PSA_ERROR_NOT_SUPPORTED;
 }
 
+psa_status_t psa_key_agreement(psa_key_id_t private_key,
+			       const uint8_t *peer_key, size_t peer_key_length,
+			       psa_algorithm_t alg,
+			       const psa_key_attributes_t *attributes,
+			       psa_key_id_t *key)
+{
+	(void)private_key;
+	(void)peer_key;
+	(void)peer_key_length;
+	(void)alg;
+	(void)attributes;
+	(void)key;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	return PSA_ERROR_NOT_SUPPORTED;
+}
+
 __export psa_status_t psa_raw_key_agreement(psa_algorithm_t alg,
 					    psa_key_id_t private_key,
 					    const uint8_t *peer_key,

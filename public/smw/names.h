77cefcfa60d28b0e4c6fca76d565ea1ffc7eeb3e
Rakshata Nayak <rakshata.nayak@nxp.com>
2024-09-29 13:55:49 +0530

    SSMW-742 public: Add a new key type SMW_KEY_TYPE_NAME_HKDF_IKM
    Add new a key type SMW_KEY_TYPE_NAME_HKDF_IKM.
    
    Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

--- public/smw/names.h
+++ public/smw/names.h
@@ -138,6 +138,7 @@ typedef enum {
  * * SMW_KEY_TYPE_NAME_TLS_MASTER: TLS master key
  * * SMW_KEY_TYPE_NAME_RAW: Raw key
  * * SMW_KEY_TYPE_NAME_DERIVE: Derived key
+ * * SMW_KEY_TYPE_NAME_HKDF_IKM: HKDF IKM Key
  * * SMW_KEY_TYPE_NAME_NB: Number of key types
  */
 typedef enum {
@@ -157,6 +158,7 @@ typedef enum {
 	SMW_KEY_TYPE_NAME_TLS_MASTER,
 	SMW_KEY_TYPE_NAME_RAW,
 	SMW_KEY_TYPE_NAME_DERIVE,
+	SMW_KEY_TYPE_NAME_HKDF_IKM,
 	SMW_KEY_TYPE_NAME_NB
 } smw_key_type_t;
 

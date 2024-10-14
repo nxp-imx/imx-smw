nxp-imx/imx-smw

commit c6b5661b7e37d8aa173a1af0ba9202f2a9856ff1

SSMW-501 pkcs11: Data storage API
Enable data storage with:
- C_CreateObject
- C_DestroyObject
- C_GetAttributeValue

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

pkcs11/src/ifsmw/data.c
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2022-2023 NXP
+ * Copyright 2022-2024 NXP
  */
+
+#include <stdlib.h>
 #include <string.h>
 
 #include "smw_osal.h"
 #include "smw_status.h"
+#include "smw_storage.h"
 
+#include "util.h"
+
+#include "lib_session.h"
 #include "lib_device.h"
 #include "libobj_types.h"
-#include "util.h"
+
+#include "args_attr.h"
+#include "tlv_encode.h"
 
 #define DATA_LABEL(name)                                                       \
 	{                                                                      \
@@ -83,6 +91,167 @@ static int set_obj_db(struct libobj_obj *obj)
 
 	return ret;
 }
+
+static int set_data_identifier(unsigned int *identifier,
+			       const struct libobj_obj *obj)
+{
+	int ret = CKR_OK;
+
+	struct librfc2279 *unique_id = get_unique_id_obj(obj, storage);
+	struct libbytes data_id = { 0 };
+
+	data_id.number =
+		util_rfc2279_to_byte_len(unique_id->string, unique_id->length);
+	if (!data_id.number)
+		return CKR_FUNCTION_FAILED;
+
+	data_id.array = malloc(data_id.number);
+	if (!data_id.array)
+		return CKR_HOST_MEMORY;
+
+	if (util_rfc2279_to_byte(data_id.array, data_id.number,
+				 unique_id->string,
+				 unique_id->length) != unique_id->length) {
+		ret = CKR_FUNCTION_FAILED;
+		goto end;
+	}
+
+	if (TO_INT(*identifier, &data_id.array[sizeof(obj->class)],
+		   sizeof(*identifier)))
+		ret = CKR_ATTRIBUTE_VALUE_INVALID;
+
+end:
+	free(data_id.array);
+
+	return ret;
+}
+
+static int store_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
+{
+	int ret = CKR_OK;
+
+	enum smw_status_code status = SMW_STATUS_OK;
+	CK_SLOT_ID slotid = 0;
+	const struct libdev *devinfo = NULL;
+	struct smw_store_data_args args = { 0 };
+	struct smw_data_descriptor data_descriptor = { 0 };
+	struct libobj_data *data = get_subobj_from(obj, storage);
+	struct smw_tlv data_attr = { 0 };
+
+	ret = libsess_get_slotid(hsession, &slotid);
+	if (ret != CKR_OK)
+		return ret;
+
+	devinfo = libdev_get_devinfo(slotid);
+	if (!devinfo)
+		return CKR_SLOT_ID_INVALID;
+
+	ret = set_data_identifier(&data_descriptor.identifier, obj);
+	if (ret != CKR_OK)
+		return ret;
+
+	ret = args_attrs_store_data(&data_attr, obj);
+	if (ret != CKR_OK)
+		return ret;
+
+	data_descriptor.data = data->value.array;
+
+	if (SET_OVERFLOW(data->value.number, data_descriptor.length))
+		return CKR_FUNCTION_FAILED;
+
+	data_descriptor.attributes_list = (unsigned char *)data_attr.string;
+	data_descriptor.attributes_list_length = data_attr.length;
+
+	args.subsystem_name = devinfo->name;
+	args.data_descriptor = &data_descriptor;
+
+	status = smw_store_data(&args);
+	if (status == SMW_STATUS_OK)
+		ret = CKR_OK;
+	else
+		ret = CKR_FUNCTION_FAILED;
+
+	tlv_encode_free(&data_attr);
+
+	return ret;
+}
+
+static int retrieve_data(const struct libobj_obj *obj)
+{
+	int ret = CKR_OK;
+
+	enum smw_status_code status = SMW_STATUS_OK;
+	struct smw_retrieve_data_args args = { 0 };
+	struct smw_data_descriptor data_descriptor = { 0 };
+	struct libobj_data *data = get_subobj_from(obj, storage);
+	unsigned char *buffer = NULL;
+
+	ret = set_data_identifier(&data_descriptor.identifier, obj);
+	if (ret != CKR_OK)
+		return ret;
+
+	args.data_descriptor = &data_descriptor;
+
+	status = smw_retrieve_data(&args);
+
+	if (status == SMW_STATUS_OUTPUT_TOO_SHORT && data_descriptor.length) {
+		if (data->value.number < data_descriptor.length) {
+			buffer = malloc(data_descriptor.length);
+			if (!buffer)
+				return CKR_HOST_MEMORY;
+
+			data_descriptor.data = buffer;
+		} else {
+			data_descriptor.data = data->value.array;
+		}
+
+		status = smw_retrieve_data(&args);
+	}
+
+	if (status != SMW_STATUS_OK) {
+		ret = CKR_FUNCTION_FAILED;
+		goto end;
+	}
+
+	if (buffer) {
+		if (data->value.array)
+			free(data->value.array);
+
+		data->value.array = buffer;
+	}
+
+	data->value.number = data_descriptor.length;
+
+end:
+	if (ret != CKR_OK && buffer)
+		free(buffer);
+
+	return ret;
+}
+
+static int delete_data(struct libobj_obj *obj)
+{
+	int ret = CKR_OK;
+
+	enum smw_status_code status = SMW_STATUS_OK;
+	struct smw_delete_data_args args = { 0 };
+	struct smw_data_descriptor data_descriptor = { 0 };
+
+	ret = set_data_identifier(&data_descriptor.identifier, obj);
+	if (ret != CKR_OK)
+		return ret;
+
+	args.data_descriptor = &data_descriptor;
+
+	status = smw_delete_data(&args);
+	if (status == SMW_STATUS_OK)
+		ret = CKR_OK;
+	else
+		ret = CKR_FUNCTION_FAILED;
+
+	return ret;
+}
+
 static const struct data_op {
 	struct librfc2279 label;
 	int (*set)(struct libobj_obj *obj);
@@ -93,10 +262,10 @@ static const struct data_op {
 	{ DATA_LABEL("Object DB"), .set = &set_obj_db },
 };
 
+struct librfc2279 data_label = DATA_LABEL("Data");
+
 CK_RV libdev_create_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
 {
-	(void)hsession;
-
 	struct libobj_storage *objstorage = NULL;
 	const struct data_op *op = data_op;
 	size_t index = 0;
@@ -105,6 +274,11 @@ CK_RV libdev_create_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
 	if (!objstorage)
 		return CKR_ARGUMENTS_BAD;
 
+	if (objstorage->label.length == data_label.length &&
+	    !memcmp(objstorage->label.string, data_label.string,
+		    data_label.length))
+		return store_data(hsession, obj);
+
 	for (; index < ARRAY_SIZE(data_op); index++, op++) {
 		if (objstorage->label.length == op->label.length &&
 		    !memcmp(objstorage->label.string, op->label.string,
@@ -112,6 +286,38 @@ CK_RV libdev_create_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
 			return op->set(obj);
 		}
 	}
+
+	return CKR_FUNCTION_FAILED;
+}
+
+CK_RV libdev_retrieve_data(const struct libobj_obj *obj)
+{
+	struct libobj_storage *objstorage = NULL;
+
+	objstorage = get_object_from(obj);
+	if (!objstorage)
+		return CKR_ARGUMENTS_BAD;
+
+	if (objstorage->label.length == data_label.length &&
+	    !memcmp(objstorage->label.string, data_label.string,
+		    data_label.length))
+		return retrieve_data(obj);
+
+	return CKR_FUNCTION_FAILED;
+}
+
+CK_RV libdev_delete_data(struct libobj_obj *obj)
+{
+	struct libobj_storage *objstorage = NULL;
+
+	objstorage = get_object_from(obj);
+	if (!objstorage)
+		return CKR_ARGUMENTS_BAD;
+
+	if (objstorage->label.length == data_label.length &&
+	    !memcmp(objstorage->label.string, data_label.string,
+		    data_label.length))
+		return delete_data(obj);
 
 	return CKR_FUNCTION_FAILED;
 }

pkcs11/src/objects/data.c
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2022-2023 NXP
+ * Copyright 2022-2024 NXP
  */
 
 #include <stdlib.h>
+#include <limits.h>
 
 #include "attributes.h"
 #include "data.h"
@@ -53,6 +54,8 @@ void data_free(struct libobj_obj *obj)
 
 	if (!data)
 		return;
+
+	(void)libdev_delete_data(obj);
 
 	DBG_TRACE("Free data object (%p)", data);
 
@@ -113,9 +116,12 @@ CK_RV data_get_attribute(CK_ATTRIBUTE_PTR attr, const struct libobj_obj *obj)
 
 	DBG_TRACE("Get attribute type=%#lx", attr->type);
 
-	/* Get attribute from the data attribute */
-	ret = attr_get_obj_value(attr, attr_data, ARRAY_SIZE(attr_data),
-				 get_subobj_from(obj, storage));
+	if (is_token_obj(obj, storage) && attr->type == CKA_VALUE)
+		ret = libdev_retrieve_data(obj);
+
+	if (ret == CKR_OK || ret == CKR_BUFFER_TOO_SMALL)
+		ret = attr_get_obj_value(attr, attr_data, ARRAY_SIZE(attr_data),
+					 get_subobj_from(obj, storage));
 
 	DBG_TRACE("Get attribute type=%#lx ret %ld", attr->type, ret);
 	return ret;
@@ -133,4 +139,26 @@ CK_RV data_modify_attribute(CK_ATTRIBUTE_PTR attr, struct libobj_obj *obj)
 
 	DBG_TRACE("Modify attribute type=%#lx ret %ld", attr->type, ret);
 	return ret;
+}
+
+CK_RV data_get_id(struct libbytes *data_id, struct libobj_obj *obj,
+		  size_t prefix_len)
+{
+	unsigned int id = 0;
+
+	if (!obj || !data_id)
+		return CKR_GENERAL_ERROR;
+
+	id = (uintptr_t)obj & UINT_MAX;
+
+	data_id->number = prefix_len + sizeof(id);
+	data_id->array = malloc(data_id->number);
+	if (!data_id->array)
+		return CKR_HOST_MEMORY;
+
+	DBG_TRACE("Data ID 0x%X", id);
+
+	TO_CK_BYTES(&data_id->array[prefix_len], id);
+
+	return CKR_OK;
 }

pkcs11/src/ifsmw/args_attr.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021-2023 NXP
+ * Copyright 2021-2024 NXP
  */
 
 #include "libobj_types.h"
@@ -284,6 +284,16 @@ CK_RV args_attrs_key_policy(struct smw_tlv *attr, struct libobj_obj *obj,
 		ret = tlv_encode_tlv(attr, SMW_ATTR_POLICY, &policy);
 
 	tlv_encode_free(&policy);
+
+	return ret;
+}
+
+CK_RV args_attrs_store_data(struct smw_tlv *attr, struct libobj_obj *obj)
+{
+	CK_RV ret = CKR_OK;
+
+	if (!is_modifiable_obj(obj, storage))
+		ret = tlv_encode_boolean(attr, "READ_ONLY");
 
 	return ret;
 }

pkcs11/src/ifsmw/args_attr.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2021-2022 NXP
+ * Copyright 2021-2024 NXP
  */
 #ifndef __ARGS_ATTR_H__
 #define __ARGS_ATTR_H__
@@ -65,7 +65,7 @@ CK_RV args_attr_sign_verify(struct smw_tlv *attr, const char *signature_type,
 /**
  * args_attrs_key_policy() - Build the key policy attribute list
  * @attr: Attribute list
- * @obj: key object
+ * @obj: Key object
  * @allowed_algos: Allowed key's algorithms
  *
  * Return:
@@ -75,5 +75,17 @@ CK_RV args_attr_sign_verify(struct smw_tlv *attr, const char *signature_type,
  */
 CK_RV args_attrs_key_policy(struct smw_tlv *attr, struct libobj_obj *obj,
 			    struct smw_tlv *allowed_algos);
+
+/**
+ * args_attrs_store_data() - Build the store data attribute list
+ * @attr: Attribute list
+ * @obj: Data object
+ *
+ * Return:
+ * CKR_HOST_MEMORY               - Out of memory
+ * CKR_ARGUMENTS_BAD             - Bad arguments
+ * CKR_OK                        - Success
+ */
+CK_RV args_attrs_store_data(struct smw_tlv *attr, struct libobj_obj *obj);
 
 #endif /* __ARGS_ATTR_H__ */

pkcs11/src/include/lib_device.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2023 NXP
+ * Copyright 2020-2024 NXP
  */
 
 #ifndef __LIB_DEVICE_H__
@@ -301,6 +301,7 @@ CK_RV libdev_mechanisms_init(CK_SLOT_ID slotid);
 
 /**
  * libdev_rng() - Call SMW random number generator API
+ * @hsession: Session handle
  * @pRandomData: Location that receives the random data
  * @ulRandomLen: Length in bytes of the random data
  *
@@ -330,6 +331,36 @@ CK_RV libdev_rng(CK_SESSION_HANDLE hsession, CK_BYTE_PTR pRandomData,
  * CKR_OK                        - Success
  */
 CK_RV libdev_create_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj);
+
+/**
+ * libdev_retrieve_data() - Retrieve SMW data
+ * @obj: Data object
+ *
+ * Function calls the SMW API to retrieve data if data label
+ * is supported.
+ *
+ * Return:
+ * CKR_ARGUMENTS_BAD             - Bad arguments
+ * CKR_DEVICE_ERROR              - Device failure
+ * CKR_FUNCTION_FAILED           - Operation failed
+ * CKR_OK                        - Success
+ */
+CK_RV libdev_retrieve_data(const struct libobj_obj *obj);
+
+/**
+ * libdev_delete_data() - Delete SMW data
+ * @obj: Data object
+ *
+ * Function calls the SMW API to delete data if data label
+ * is supported.
+ *
+ * Return:
+ * CKR_ARGUMENTS_BAD             - Bad arguments
+ * CKR_DEVICE_ERROR              - Device failure
+ * CKR_FUNCTION_FAILED           - Operation failed
+ * CKR_OK                        - Success
+ */
+CK_RV libdev_delete_data(struct libobj_obj *obj);
 
 /**
  * libdev_cancel_operation() - Cancel an on-going cryptographic multi-part operation.

pkcs11/src/include/util.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020, 2023 NXP
+ * Copyright 2020, 2023-2024 NXP
  */
 #ifndef __UTIL_H__
 #define __UTIL_H__
@@ -17,6 +17,25 @@
 		__typeof__(out) _out = (out);                                  \
 		for (size_t i = 0; i < sizeof(val); i++)                       \
 			_out[i] = GET_BYTE(val, i);                            \
+	})
+
+#define TO_INT(out, buf, len)                                                  \
+	({                                                                     \
+		__typeof__(out) _out = 0;                                      \
+		__typeof__(buf) _buf = (buf);                                  \
+		size_t i = len;                                                \
+		int ret = 0;                                                   \
+		if (i > sizeof(_out)) {                                        \
+			ret = 1;                                               \
+		} else {                                                       \
+			_out = _buf[--i] & UINT8_MAX;                          \
+			for (; i; i--) {                                       \
+				_out <<= 8;                                    \
+				_out |= _buf[i - 1] & UINT8_MAX;               \
+			}                                                      \
+			out = _out;                                            \
+		}                                                              \
+		ret;                                                           \
 	})
 
 /**
@@ -50,50 +69,50 @@ void util_copy_str_to_utf8(CK_UTF8CHAR_PTR dst, size_t len_dst,
 			   const char *src);
 
 /**
- * util_byte_to_utf8_len() - Get the UTF8 string length of a byte array
+ * util_byte_to_rfc2279_len() - Get the RFC2279 string length of a byte array
  * @src: Byte array
  * @len_src: Length of the @src array
  *
  * Return:
- * UTF8 length of the byte array
+ * RFC2279 length of the byte array
  */
-size_t util_byte_to_utf8_len(const CK_BYTE_PTR src, size_t len_src);
+size_t util_byte_to_rfc2279_len(const CK_BYTE_PTR src, size_t len_src);
 
 /**
- * util_byte_to_utf8() - Convert a byte array to an UTF8 string
- * @dst: UTF8 string output
- * @len_dst: UTF8 string length maximum
+ * util_byte_to_rfc2279() - Convert a byte array to an RFC2279 string
+ * @dst: RFC2279 string output
+ * @len_dst: RFC2279 string length maximum
  * @src: Byte array to convert
  * @len_src: Length of the @src array
  *
  * Return:
  * The number of source byte converted
  */
-size_t util_byte_to_utf8(CK_UTF8CHAR_PTR dst, size_t len_dst,
-			 const CK_BYTE_PTR src, size_t len_src);
+size_t util_byte_to_rfc2279(CK_UTF8CHAR_PTR dst, size_t len_dst,
+			    const CK_BYTE_PTR src, size_t len_src);
 
 /**
- * util_utf8_to_byte_len() - Get the byte array length of an UTF8 string
- * @src: UTF8 string
+ * util_rfc2279_to_byte_len() - Get the byte array length of an RFC2279 string
+ * @src: RFC2279 string
  * @len_src: Length of the @src string
  *
  * Return:
- * byte array length of the UTF8 string
+ * byte array length of the RFC2279 string
  */
-size_t util_utf8_to_byte_len(const CK_UTF8CHAR_PTR src, size_t len_src);
+size_t util_rfc2279_to_byte_len(const CK_UTF8CHAR_PTR src, size_t len_src);
 
 /**
- * util_utf8_to_byte() - Convert an UTF8 string to an array of byte
+ * util_rfc2279_to_byte() - Convert an RFC2279 string to an array of byte
  * @dst: Byte array to convert
  * @len_dst: Length of the @src array
- * @src: UTF8 string output
- * @len_src: UTF8 string length maximum
+ * @src: RFC2279 string output
+ * @len_src: RFC2279 string length maximum
  *
  * Return:
- * The length of UTF8 string converted
+ * The length of RFC2279 string converted
  */
-size_t util_utf8_to_byte(CK_BYTE_PTR dst, size_t len_dst,
-			 const CK_UTF8CHAR_PTR src, size_t len_src);
+size_t util_rfc2279_to_byte(CK_BYTE_PTR dst, size_t len_dst,
+			    const CK_UTF8CHAR_PTR src, size_t len_src);
 
 /**
  * util_get_bignum_bits() - Get the number of bits of a big number

pkcs11/src/objects/data.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2022 NXP
+ * Copyright 2022, 2024 NXP
  */
 
 #ifndef __DATA_H__
@@ -65,5 +65,22 @@ CK_RV data_get_attribute(CK_ATTRIBUTE_PTR attr, const struct libobj_obj *obj);
  * CKR_OK                        - Success
  */
 CK_RV data_modify_attribute(CK_ATTRIBUTE_PTR attr, struct libobj_obj *obj);
+
+/*
+ * data_get_id() - Generate the data ID
+ * @id: Byte buffer of the data ID
+ * @obj: Data object
+ * @prefix_len: Byte length of id prefix
+ *
+ * Generate a unique data ID.
+ *
+ * return:
+ * CKR_HOST_MEMORY               - Allocation error
+ * CKR_GENERAL_ERROR             - General error defined
+ * CKR_FUNCTION_FAILED           - Function failure
+ * CKR_OK                        - Success
+ */
+CK_RV data_get_id(struct libbytes *id, struct libobj_obj *obj,
+		  size_t prefix_len);
 
 #endif /* __DATA_H__ */

pkcs11/src/objects/lib_object.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2023 NXP
+ * Copyright 2020-2024 NXP
  */
 
 #include <stdlib.h>
@@ -212,6 +212,11 @@ static CK_RV set_unique_id(struct libobj_obj *obj)
 		ret = key_get_id(&id, obj, sizeof(obj->class));
 		break;
 
+	case CKO_DATA:
+		unique_id = get_unique_id_obj(obj, storage);
+		ret = data_get_id(&id, obj, sizeof(obj->class));
+		break;
+
 	default:
 		return CKR_FUNCTION_FAILED;
 	}
@@ -221,8 +226,8 @@ static CK_RV set_unique_id(struct libobj_obj *obj)
 
 	TO_CK_BYTES(id.array, obj->class);
 
-	/* Get UTF8 length and allocate UTF8 string */
-	unique_id->length = util_byte_to_utf8_len(id.array, id.number);
+	/* Get RFC2279 length and allocate RFC2279 string */
+	unique_id->length = util_byte_to_rfc2279_len(id.array, id.number);
 	if (!unique_id->length) {
 		ret = CKR_FUNCTION_FAILED;
 		goto end;
@@ -235,8 +240,8 @@ static CK_RV set_unique_id(struct libobj_obj *obj)
 	}
 
 	ret = CKR_FUNCTION_FAILED;
-	if (util_byte_to_utf8(unique_id->string, unique_id->length, id.array,
-			      id.number) == id.number)
+	if (util_byte_to_rfc2279(unique_id->string, unique_id->length, id.array,
+				 id.number) == id.number)
 		ret = CKR_OK;
 
 end:
@@ -830,6 +835,10 @@ CK_RV libobj_create(CK_SESSION_HANDLE hsession, CK_ATTRIBUTE_PTR attrs,
 
 	case CKO_DATA:
 		ret = obj_storage_new(hsession, newobj, &attrs_list);
+		if (ret != CKR_OK)
+			break;
+
+		ret = set_unique_id(newobj);
 		if (ret != CKR_OK)
 			break;
 

commit 11b4b5dcfd0713612942cc56ce37ea70794c132a

SSMW-743 pkcs11: Fix data storage label validation
Remove the fact that a data label must be "Data" to store/retrieve/data
in SMW's subsystems.
Remove the hardcoded data label used to configure SMW library (use
the system configuration file instead - smw.conf)

Fixes: c6b5661b7e37 ("SSMW-501 pkcs11: Data storage API")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/ifsmw/data.c
@@ -20,64 +20,6 @@
 #include "args_attr.h"
 #include "ifsmw_utils.h"
 
-#define DATA_LABEL(name)                                                       \
-	{                                                                      \
-		.string = (CK_UTF8CHAR *)name, .length = sizeof(name) - 1,     \
-	}
-
-static int set_tee_info(struct libobj_obj *obj)
-{
-	enum smw_status_code status = SMW_STATUS_OK;
-	struct libobj_data *data = get_subobj_from(obj, storage);
-
-	DBG_TRACE("Set TEE info (%d)", SMW_SUBSYSTEM_NAME_TEE);
-
-	status = smw_osal_set_subsystem_info(SMW_SUBSYSTEM_NAME_TEE,
-					     data->value.array,
-					     data->value.number);
-
-	return smw_status_to_ck_rv(status);
-}
-
-static int set_seco_info(struct libobj_obj *obj)
-{
-	enum smw_status_code status = SMW_STATUS_OK;
-	struct libobj_data *data = get_subobj_from(obj, storage);
-
-	DBG_TRACE("Set SECO info (%d)", SMW_SUBSYSTEM_NAME_SECO);
-
-	status = smw_osal_set_subsystem_info(SMW_SUBSYSTEM_NAME_SECO,
-					     data->value.array,
-					     data->value.number);
-
-	return smw_status_to_ck_rv(status);
-}
-
-static int set_ele_info(struct libobj_obj *obj)
-{
-	enum smw_status_code status = SMW_STATUS_OK;
-	struct libobj_data *data = get_subobj_from(obj, storage);
-
-	DBG_TRACE("Set ELE info (%d)", SMW_SUBSYSTEM_NAME_ELE);
-
-	status = smw_osal_set_subsystem_info(SMW_SUBSYSTEM_NAME_ELE,
-					     data->value.array,
-					     data->value.number);
-
-	return smw_status_to_ck_rv(status);
-}
-
-static int set_obj_db(struct libobj_obj *obj)
-{
-	enum smw_status_code status = SMW_STATUS_OK;
-	struct libobj_data *data = get_subobj_from(obj, storage);
-
-	status = smw_osal_open_obj_db((const char *)data->value.array,
-				      data->value.number);
-
-	return smw_status_to_ck_rv(status);
-}
-
 static int set_data_identifier(unsigned int *identifier,
 			       const struct libobj_obj *obj)
 {
@@ -219,42 +161,15 @@ static int delete_data(struct libobj_obj *obj)
 	return smw_status_to_ck_rv(status);
 }
 
-static const struct data_op {
-	struct librfc2279 label;
-	int (*set)(struct libobj_obj *obj);
-} data_op[] = {
-	{ DATA_LABEL("TEE Info"), .set = &set_tee_info },
-	{ DATA_LABEL("SECO Info"), .set = &set_seco_info },
-	{ DATA_LABEL("ELE Info"), .set = &set_ele_info },
-	{ DATA_LABEL("Object DB"), .set = &set_obj_db },
-};
-
-struct librfc2279 data_label = DATA_LABEL("Data");
-
 CK_RV libdev_create_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
 {
 	struct libobj_storage *objstorage = NULL;
-	const struct data_op *op = data_op;
-	size_t index = 0;
 
 	objstorage = get_object_from(obj);
 	if (!objstorage)
 		return CKR_ARGUMENTS_BAD;
 
-	if (objstorage->label.length == data_label.length &&
-	    !memcmp(objstorage->label.string, data_label.string,
-		    data_label.length))
-		return store_data(hsession, obj);
-
-	for (; index < ARRAY_SIZE(data_op); index++, op++) {
-		if (objstorage->label.length == op->label.length &&
-		    !memcmp(objstorage->label.string, op->label.string,
-			    op->label.length)) {
-			return op->set(obj);
-		}
-	}
-
-	return CKR_FUNCTION_FAILED;
+	return store_data(hsession, obj);
 }
 
 CK_RV libdev_retrieve_data(const struct libobj_obj *obj)
@@ -265,12 +180,7 @@ CK_RV libdev_retrieve_data(const struct libobj_obj *obj)
 	if (!objstorage)
 		return CKR_ARGUMENTS_BAD;
 
-	if (objstorage->label.length == data_label.length &&
-	    !memcmp(objstorage->label.string, data_label.string,
-		    data_label.length))
-		return retrieve_data(obj);
-
-	return CKR_FUNCTION_FAILED;
+	return retrieve_data(obj);
 }
 
 CK_RV libdev_delete_data(struct libobj_obj *obj)
@@ -281,10 +191,5 @@ CK_RV libdev_delete_data(struct libobj_obj *obj)
 	if (!objstorage)
 		return CKR_ARGUMENTS_BAD;
 
-	if (objstorage->label.length == data_label.length &&
-	    !memcmp(objstorage->label.string, data_label.string,
-		    data_label.length))
-		return delete_data(obj);
-
-	return CKR_FUNCTION_FAILED;
+	return delete_data(obj);
 }
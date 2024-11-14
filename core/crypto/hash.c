nxp-imx/imx-smw

commit 90e8cf6651b967aedbdade10d182e4aead1ba22f

SSMW-510 core: crypto: Add support of hash multi-part
- Implement APIs related to hash multi-part operation
- Simplify cryptographic operation context structure

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/crypto/hash.c
@@ -8,15 +8,16 @@
 
 #include "global.h"
 #include "debug.h"
+#include "utils.h"
 #include "operations.h"
 #include "subsystems.h"
 #include "config.h"
 #include "hash.h"
 #include "exec.h"
 
-static int hash_convert_args(struct smw_hash_args *args,
-			     struct smw_crypto_hash_args *converted_args,
-			     enum subsystem_id *subsystem_id)
+static int convert_args(struct smw_hash_args *args,
+			struct smw_crypto_hash_args *converted_args,
+			enum subsystem_id *subsystem_id)
 {
 	int status = SMW_STATUS_OK;
 
@@ -26,6 +27,8 @@ static int hash_convert_args(struct smw_hash_args *args,
 		status = SMW_STATUS_VERSION_NOT_SUPPORTED;
 		goto end;
 	}
+
+	converted_args->op_step = SMW_OP_STEP_ONESHOT;
 
 	status =
 		smw_config_get_subsystem_id(args->subsystem_name, subsystem_id);
@@ -37,7 +40,75 @@ static int hash_convert_args(struct smw_hash_args *args,
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	converted_args->pub = args;
+	converted_args->oneshot_pub = args;
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int convert_init_args(struct smw_hash_init_args *args,
+			     struct smw_crypto_hash_args *converted_args)
+{
+	int status = SMW_STATUS_OK;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (args->version != 0) {
+		status = SMW_STATUS_VERSION_NOT_SUPPORTED;
+		goto end;
+	}
+
+	converted_args->op_step = SMW_OP_STEP_INIT;
+
+	status = smw_utils_get_hash_algo_id(args->algo_name,
+					    &converted_args->algo_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	converted_args->init_pub = args;
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int convert_update_args(struct smw_hash_update_args *args,
+			       struct smw_crypto_hash_args *converted_args)
+{
+	int status = SMW_STATUS_OK;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (args->version != 0) {
+		status = SMW_STATUS_VERSION_NOT_SUPPORTED;
+		goto end;
+	}
+
+	converted_args->op_step = SMW_OP_STEP_UPDATE;
+
+	converted_args->update_pub = args;
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int convert_final_args(struct smw_hash_final_args *args,
+			      struct smw_crypto_hash_args *converted_args)
+{
+	int status = SMW_STATUS_OK;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (args->version != 0) {
+		status = SMW_STATUS_VERSION_NOT_SUPPORTED;
+		goto end;
+	}
+
+	converted_args->op_step = SMW_OP_STEP_FINAL;
+
+	converted_args->final_pub = args;
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
@@ -48,8 +119,30 @@ unsigned char *smw_crypto_get_hash_input_data(struct smw_crypto_hash_args *args)
 {
 	unsigned char *input_data = NULL;
 
-	if (args->pub)
-		input_data = args->pub->input;
+	switch (args->op_step) {
+	case SMW_OP_STEP_ONESHOT:
+		if (args->oneshot_pub)
+			input_data = args->oneshot_pub->input;
+		break;
+
+	case SMW_OP_STEP_INIT:
+		if (args->init_pub)
+			input_data = args->init_pub->input;
+		break;
+
+	case SMW_OP_STEP_UPDATE:
+		if (args->update_pub)
+			input_data = args->update_pub->input;
+		break;
+
+	case SMW_OP_STEP_FINAL:
+		if (args->final_pub)
+			input_data = args->final_pub->input;
+		break;
+
+	default:
+		break;
+	}
 
 	return input_data;
 }
@@ -58,8 +151,30 @@ unsigned int smw_crypto_get_hash_input_length(struct smw_crypto_hash_args *args)
 {
 	unsigned int input_length = 0;
 
-	if (args->pub)
-		input_length = args->pub->input_length;
+	switch (args->op_step) {
+	case SMW_OP_STEP_ONESHOT:
+		if (args->oneshot_pub)
+			input_length = args->oneshot_pub->input_length;
+		break;
+
+	case SMW_OP_STEP_INIT:
+		if (args->init_pub)
+			input_length = args->init_pub->input_length;
+		break;
+
+	case SMW_OP_STEP_UPDATE:
+		if (args->update_pub)
+			input_length = args->update_pub->input_length;
+		break;
+
+	case SMW_OP_STEP_FINAL:
+		if (args->final_pub)
+			input_length = args->final_pub->input_length;
+		break;
+
+	default:
+		break;
+	}
 
 	return input_length;
 }
@@ -69,8 +184,20 @@ smw_crypto_get_hash_output_data(struct smw_crypto_hash_args *args)
 {
 	unsigned char *output_data = NULL;
 
-	if (args->pub)
-		output_data = args->pub->output;
+	switch (args->op_step) {
+	case SMW_OP_STEP_ONESHOT:
+		if (args->oneshot_pub)
+			output_data = args->oneshot_pub->output;
+		break;
+
+	case SMW_OP_STEP_FINAL:
+		if (args->final_pub)
+			output_data = args->final_pub->output;
+		break;
+
+	default:
+		break;
+	}
 
 	return output_data;
 }
@@ -80,8 +207,20 @@ smw_crypto_get_hash_output_length(struct smw_crypto_hash_args *args)
 {
 	unsigned int output_length = 0;
 
-	if (args->pub)
-		output_length = args->pub->output_length;
+	switch (args->op_step) {
+	case SMW_OP_STEP_ONESHOT:
+		if (args->oneshot_pub)
+			output_length = args->oneshot_pub->output_length;
+		break;
+
+	case SMW_OP_STEP_FINAL:
+		if (args->final_pub)
+			output_length = args->final_pub->output_length;
+		break;
+
+	default:
+		break;
+	}
 
 	return output_length;
 }
@@ -89,8 +228,54 @@ smw_crypto_get_hash_output_length(struct smw_crypto_hash_args *args)
 void smw_crypto_set_hash_output_length(struct smw_crypto_hash_args *args,
 				       unsigned int output_length)
 {
-	if (args->pub)
-		args->pub->output_length = output_length;
+	switch (args->op_step) {
+	case SMW_OP_STEP_ONESHOT:
+		if (args->oneshot_pub)
+			args->oneshot_pub->output_length = output_length;
+		break;
+
+	case SMW_OP_STEP_FINAL:
+		if (args->final_pub)
+			args->final_pub->output_length = output_length;
+		break;
+
+	default:
+		break;
+	}
+}
+
+struct smw_op_context *
+smw_crypto_get_hash_op_context(struct smw_crypto_hash_args *args)
+{
+	struct smw_op_context *ctx = NULL;
+
+	if (!args)
+		return ctx;
+
+	switch (args->op_step) {
+	case SMW_OP_STEP_INIT:
+		if (args->init_pub)
+			ctx = args->init_pub->context;
+
+		break;
+
+	case SMW_OP_STEP_UPDATE:
+		if (args->update_pub)
+			ctx = args->update_pub->context;
+
+		break;
+
+	case SMW_OP_STEP_FINAL:
+		if (args->final_pub)
+			ctx = args->final_pub->context;
+
+		break;
+
+	default:
+		break;
+	}
+
+	return ctx;
 }
 
 enum smw_status_code smw_hash(struct smw_hash_args *args)
@@ -108,12 +293,115 @@ enum smw_status_code smw_hash(struct smw_hash_args *args)
 		goto end;
 	}
 
-	status = hash_convert_args(args, &hash_args, &subsystem_id);
+	status = convert_args(args, &hash_args, &subsystem_id);
 	if (status != SMW_STATUS_OK)
 		goto end;
 
 	status = smw_utils_execute_operation(OPERATION_ID_HASH, &hash_args,
 					     subsystem_id);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+enum smw_status_code smw_hash_init(struct smw_hash_init_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+	struct smw_crypto_hash_args hash_args = { 0 };
+
+	SMW_DBG_TRACE_API_CALL;
+
+	if (!args || !args->context || !args->input != !args->input_length)
+		goto end;
+
+	status = convert_init_args(args, &hash_args);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status =
+		smw_utils_execute_init(OPERATION_ID_HASH_MULTI_PART, &hash_args,
+				       args->context->subsystem_id);
+	/*
+	 * Release the context if the init operation has returned any status
+	 * code except SMW_STATUS_OK and SMW_STATUS_INVALID_PARAM.
+	 */
+	if (status != SMW_STATUS_OK && status != SMW_STATUS_INVALID_PARAM)
+		(void)smw_utils_free_context(&args->context);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+enum smw_status_code smw_hash_update(struct smw_hash_update_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+	struct smw_crypto_hash_args hash_args = { 0 };
+
+	SMW_DBG_TRACE_API_CALL;
+
+	if (!args || !args->context || !args->input || !args->input_length)
+		goto end;
+
+	status = convert_update_args(args, &hash_args);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = smw_utils_execute_update(OPERATION_ID_HASH_MULTI_PART,
+					  &hash_args,
+					  args->context->subsystem_id);
+
+	/*
+	 * Release the operation context if the final operation has returned any
+	 * status code except SMW_STATUS_OK and SMW_STATUS_INVALID_PARAM.
+	 */
+	if (status != SMW_STATUS_OK && status != SMW_STATUS_INVALID_PARAM)
+		(void)smw_utils_free_context(&args->context);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+enum smw_status_code smw_hash_final(struct smw_hash_final_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+	int tmp_status = SMW_STATUS_OK;
+	struct smw_crypto_hash_args hash_args = { 0 };
+
+	SMW_DBG_TRACE_API_CALL;
+
+	if (!args || !args->context || (args->input && !args->input_length) ||
+	    (args->output && !args->output_length))
+		goto end;
+
+	status = convert_final_args(args, &hash_args);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = smw_utils_execute_final(OPERATION_ID_HASH_MULTI_PART,
+					 &hash_args,
+					 args->context->subsystem_id);
+
+	/*
+	 * Release the context if the final operation has returned any status code
+	 * except SMW_STATUS_OUTPUT_TOO_SHORT or SMW_STATUS_INVALID_PARAM.
+	 */
+	if (status != SMW_STATUS_OUTPUT_TOO_SHORT &&
+	    status != SMW_STATUS_INVALID_PARAM) {
+		tmp_status = smw_utils_free_context(&args->context);
+		if (status == SMW_STATUS_OK)
+			status = tmp_status;
+	}
+
+	/*
+	 * SMW_STATUS_OUTPUT_TOO_SHORT is the expected internal status if the
+	 * 'get output buffer length' feature succeed and must be convert to
+	 * SMW_STATUS_OK
+	 */
+	if (status == SMW_STATUS_OUTPUT_TOO_SHORT && !args->output)
+		status = SMW_STATUS_OK;
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);

core/crypto/CMakeLists.txt
@@ -2,6 +2,7 @@ set(OBJ_LIST ${OBJ_LIST} $<TARGET_OBJECTS:crypto> PARENT_SCOPE)
 
 if(ENABLE_HASH)
 add_operation(HASH)
+add_operation(HASH_MULTI_PART)
 endif()
 
 if(ENABLE_SIGN_VERIFY)
@@ -32,7 +33,7 @@ endif()
 # If at least one of the operation that supports MULTI_PART is enabled, context
 # features are enabled
 #
-if (ENABLE_CIPHER OR ENABLE_AEAD)
+if (ENABLE_CIPHER OR ENABLE_AEAD OR ENABLE_HASH)
 set(ENABLE_CONTEXT ON)
 endif()
 

core/crypto/crypto_weak.c
@@ -21,6 +21,33 @@ __weak enum smw_status_code smw_hash(struct smw_hash_args *args)
 	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
 }
 
+__weak enum smw_status_code smw_hash_init(struct smw_hash_init_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_API_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
+__weak enum smw_status_code smw_hash_update(struct smw_hash_update_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_API_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
+__weak enum smw_status_code smw_hash_final(struct smw_hash_final_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_API_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
 __weak enum smw_status_code smw_sign(struct smw_sign_verify_args *args)
 {
 	(void)args;

core/crypto/operation_context.c
@@ -25,7 +25,6 @@ void smw_crypto_copy_ctx_members(struct smw_op_context *dst_context,
 	dst_context->op_state = src_context->op_state;
 	dst_context->subsystem_id = src_context->subsystem_id;
 	dst_context->op_id = src_context->op_id;
-	dst_context->op_type_id = src_context->op_type_id;
 }
 
 enum smw_status_code smw_allocate_context(struct smw_context_args *args)

core/inc/aead.h
@@ -6,7 +6,6 @@
 #ifndef __AEAD_H__
 #define __AEAD_H__
 
-#include "subsystems.h"
 #include "config.h"
 #include "keymgr.h"
 #include "exec.h"

core/inc/exec.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2023 NXP
+ * Copyright 2020-2024 NXP
  */
 
 #ifndef __EXEC_H___
@@ -9,10 +9,10 @@
 #include "subsystems.h"
 
 enum smw_op_step {
+	SMW_OP_STEP_ONESHOT = 0,
 	SMW_OP_STEP_INIT,
 	SMW_OP_STEP_UPDATE,
-	SMW_OP_STEP_FINAL,
-	SMW_OP_STEP_ONESHOT
+	SMW_OP_STEP_FINAL
 };
 
 /**

core/inc/hash.h
@@ -1,23 +1,35 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2021, 2023 NXP
+ * Copyright 2020-2021, 2023-2024 NXP
  */
 
 #ifndef __HASH_H__
 #define __HASH_H__
 
 #include "config.h"
+#include "exec.h"
+#include "operation_context.h"
 
 /**
  * struct smw_crypto_hash_args - Hash arguments
  * @algo_id: Algorithm ID
- * @pub: Pointer to the public API arguments structure
+ * @op_step: Multi-part operation step
+ * @one_shot_pub: Pointer to the public API one-shot arguments structure
+ * @init_pub: Pointer to the public API initialization arguments structure
+ * @update_pub: Pointer to the public API update arguments structure
+ * @final_pub: Pointer to the public API final arguments structure
  *
  */
 struct smw_crypto_hash_args {
 	/* Inputs */
 	enum smw_config_hash_algo_id algo_id;
-	struct smw_hash_args *pub;
+	enum smw_op_step op_step;
+	union {
+		struct smw_hash_args *oneshot_pub;
+		struct smw_hash_init_args *init_pub;
+		struct smw_hash_update_args *update_pub;
+		struct smw_hash_final_args *final_pub;
+	};
 };
 
 /**
@@ -84,5 +96,15 @@ smw_crypto_get_hash_output_length(struct smw_crypto_hash_args *args);
  */
 void smw_crypto_set_hash_output_length(struct smw_crypto_hash_args *args,
 				       unsigned int output_length);
+
+/**
+ * smw_crypto_get_hash_op_context() - Get Hash operation context pointer
+ * @args: Pointer to internal Hash arguments.
+ *
+ * Return:
+ * Address of Hash operation context structure
+ */
+struct smw_op_context *
+smw_crypto_get_hash_op_context(struct smw_crypto_hash_args *args);
 
 #endif /* __HASH_H__ */

core/inc/operation_context.h
@@ -12,15 +12,9 @@
 enum smw_crypto_op_id {
 	SMW_CRYPTO_OP_ID_AEAD_MULTI_PART,
 	SMW_CRYPTO_OP_ID_CIPHER_MULTI_PART,
+	SMW_CRYPTO_OP_ID_HASH_MULTI_PART,
 	SMW_CRYPTO_OP_ID_NB,
 	SMW_CRYPTO_OP_ID_INVALID
-};
-
-enum smw_crypto_op_type_id {
-	SMW_CRYPTO_OP_TYPE_ID_ENCRYPT,
-	SMW_CRYPTO_OP_TYPE_ID_DECRYPT,
-	SMW_CRYPTO_OP_TYPE_ID_NB,
-	SMW_CRYPTO_OP_TYPE_ID_INVALID
 };
 
 enum ctx_op_state {
@@ -35,7 +29,6 @@ enum ctx_op_state {
  * struct smw_op_context - Cryptographic operation context structure
  * @subsystem_id: Subsystem ID
  * @op_id: Operation ID
- * @op_type_id: Operation type ID
  * @op_state: Context operation state
  * @subsystem_context: Pointer to subsystem specific context
  *
@@ -44,7 +37,6 @@ enum ctx_op_state {
 struct smw_op_context {
 	enum subsystem_id subsystem_id;
 	enum smw_crypto_op_id op_id;
-	enum smw_crypto_op_type_id op_type_id;
 	enum ctx_op_state op_state;
 	void *subsystem_context;
 };
@@ -59,7 +51,7 @@ struct smw_crypto_context_ops {
 	int (*cancel)(struct smw_op_context *ctx);
 	int (*copy)(struct smw_op_context *src_ctx,
 		    struct smw_op_context *dst_ctx);
-	void (*free)(struct smw_op_context **ctx);
+	void (*free)(struct smw_op_context *ctx);
 };
 
 /**

commit f5559d70328f7a4a63894a6dc7efecb69c7ca073

SSMW-657 core: crypto: Get output length feature with hash multi-part
smw_hash_final() shall not free the hash multi-part context if the user
has requested the digest length by setting the pointer to the output
buffer to NULL.

Fixes: 90e8cf6651b9 ("SSMW-510 core: crypto: Add support of hash multi-part")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/crypto/hash.c
@@ -383,25 +383,14 @@ enum smw_status_code smw_hash_final(struct smw_hash_final_args *args)
 	status = smw_utils_execute_final(OPERATION_ID_HASH_MULTI_PART,
 					 &hash_args,
 					 args->context->subsystem_id);
+	if (status == SMW_STATUS_OUTPUT_TOO_SHORT ||
+	    status == SMW_STATUS_INVALID_PARAM ||
+	    (status == SMW_STATUS_OK && !args->output))
+		goto end;
 
-	/*
-	 * Release the context if the final operation has returned any status code
-	 * except SMW_STATUS_OUTPUT_TOO_SHORT or SMW_STATUS_INVALID_PARAM.
-	 */
-	if (status != SMW_STATUS_OUTPUT_TOO_SHORT &&
-	    status != SMW_STATUS_INVALID_PARAM) {
-		tmp_status = smw_utils_free_context(&args->context);
-		if (status == SMW_STATUS_OK)
-			status = tmp_status;
-	}
-
-	/*
-	 * SMW_STATUS_OUTPUT_TOO_SHORT is the expected internal status if the
-	 * 'get output buffer length' feature succeed and must be convert to
-	 * SMW_STATUS_OK
-	 */
-	if (status == SMW_STATUS_OUTPUT_TOO_SHORT && !args->output)
-		status = SMW_STATUS_OK;
+	tmp_status = smw_utils_free_context(&args->context);
+	if (status == SMW_STATUS_OK)
+		status = tmp_status;
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
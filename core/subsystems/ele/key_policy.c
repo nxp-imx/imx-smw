nxp-imx/imx-smw

commit 8422ee8251592392395aaf11c00a0c50cd79ee4b

SSMW-576 subsystems: ele: Add support for HKDF(full, extract & expand)
Add support for key derivation using HKDF (Full, Extract step
and Expand step).

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

core/subsystems/ele/key_policy.c
@@ -105,6 +105,14 @@ static const struct {
 	PERMITTED_ALGO(ALL_AEAD, DEFAULT, ANY, NONE, AEAD),
 	PERMITTED_ALGO(ECDH_HKDF_SHA256, ECDH, NONE, SHA256, KEY_DERIVATION),
 	PERMITTED_ALGO(ECDH_HKDF_SHA384, ECDH, NONE, SHA384, KEY_DERIVATION),
+	PERMITTED_ALGO(HKDF_EXTRACT_SHA256, HKDF_EXTRACT, NONE, SHA256,
+		       KEY_DERIVATION),
+	PERMITTED_ALGO(HKDF_EXTRACT_SHA384, HKDF_EXTRACT, NONE, SHA384,
+		       KEY_DERIVATION),
+	PERMITTED_ALGO(HKDF_EXPAND_SHA256, HKDF_EXPAND, NONE, SHA256,
+		       KEY_DERIVATION),
+	PERMITTED_ALGO(HKDF_EXPAND_SHA384, HKDF_EXPAND, NONE, SHA384,
+		       KEY_DERIVATION),
 	PERMITTED_ALGO(ATTEST_CMAC, DEFAULT, CMAC, NONE, KEY_ATTESTATION),
 	PERMITTED_ALGO_CURVE(ATTEST_ECDSA_SHA224, ECDSA, NONE, SHA224,
 			     KEY_ATTESTATION),

core/subsystems/ele/common.h
@@ -432,4 +432,25 @@ int ele_is_oem_srkh_fused(struct subsystem_context *ele_ctx, bool *fused);
  */
 void *ele_get_ctx_ops(void);
 
+/**
+ * open_key_mgmt_service() - Open a key management service flow
+ * @hdl: Pointer to subsystem context handlers
+ * @key_management_hdl: Return the key manager service handler
+ *
+ * Return:
+ * SMW_STATUS_OK                   - Success
+ * SMW_STATUS_SUBSYSTEM_FAILURE    - Subsystem failure
+ */
+int open_key_mgmt_service(struct hdl *hdl, hsm_hdl_t *key_management_hdl);
+
+/**
+ * close_key_mgt_service() - Close the key management service flow
+ * @key_management_hdl: Key manager service handler to close
+ *
+ * Return:
+ * SMW_STATUS_OK                   - Success
+ * SMW_STATUS_SUBSYSTEM_FAILURE    - Subsystem failure
+ */
+int close_key_mgt_service(hsm_hdl_t key_management_hdl);
+
 #endif /* __COMMON_H__ */

core/subsystems/ele/keymgr.c
@@ -315,7 +315,7 @@ static int check_export_key_config(struct smw_keymgr_descriptor *key_descriptor)
 	return status;
 }
 
-static int open_key_mgmt_service(struct hdl *hdl, hsm_hdl_t *key_management_hdl)
+int open_key_mgmt_service(struct hdl *hdl, hsm_hdl_t *key_management_hdl)
 {
 	hsm_err_t err = HSM_NO_ERROR;
 	open_svc_key_management_args_t open_svc_key_management_args = { 0 };
@@ -332,7 +332,7 @@ static int open_key_mgmt_service(struct hdl *hdl, hsm_hdl_t *key_management_hdl)
 	return ele_convert_err(err);
 }
 
-static int close_key_mgt_service(hsm_hdl_t key_management_hdl)
+int close_key_mgt_service(hsm_hdl_t key_management_hdl)
 {
 	hsm_err_t err = HSM_NO_ERROR;
 

core/subsystems/ele/keymgr_derive_hkdf.h
@@ -1,0 +1,76 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2024 NXP
+ */
+
+#ifndef __KEYMGR_DERIVE_HKDF_H__
+#define __KEYMGR_DERIVE_HKDF_H__
+
+#include "common.h"
+
+/**
+ * struct hkdf_ele_op_payload - Key Exchange input content (HKDF FULL/HKDF
+ *                               Extract/HKDF Expand) member argument
+ * @ver: Version of this structure
+ * @rsv: Reserved bits
+ * @keystore_id: Key store ID
+ * @hkdf_algo: HKDF algo to be used for Key derivation process
+ * @key_id: Base Key ID
+ * @key_type: Derived Key type
+ * @key_size: Derived Key size in bits
+ * @key_lifetime: Derived Key lifetime
+ * @key_usage: Derived Key usage
+ * @key_permit_algo: Derived Key permitted algo
+ * @key_lifecycle:  Derived Key lifecycle
+ * @derived_key_id: Derived Key ID
+ * @buffer_len: Buffer length
+ *
+ * In the ELE SE library, the in_content member of
+ * struct op_key_exchange_args_t defined as a uint8_t pointer, is provided,
+ * with no information on what it should point to.
+ * This is why I defined this structure.
+ *
+ * The in_content member points to a memory block that contains
+ * struct hkdf_ele_op_payload followed by the
+ *  - PRK buffer if set for HKDF Expand step.
+ *  - Salt buffer if set for HKDF Full and Extract step.
+ *
+ * @ver supported is 1.
+ *
+ * @buffer_len - It should hold the salt buffer length for HKDF Full/Extract
+ * step. For HKDF Expand step, it should hold PRK buffer length if PRK is
+ * exported.
+ *
+ * @key_id - For HKDF Full/Extract step, it should hold the private key ID to be
+ * used along with peer public key to generate ECDH IKM.
+ * For HKDF Expand step, it should hold the PRK ID, if PRK is already stored
+ * in the ELE storage.
+ *
+ * HKDF Extract:
+ * - @key_type, @key_lifetime, @key_usage, @key_permit_algo, @key_lifecycle are
+ *   not used and ignored if the PRK to be exported.
+ * - @key_lifetime and @key_lifecycle should be set if PRK to be stored in ELE.
+ *
+ * HKDF Full/Expand:
+ * - @key_type, @key_lifetime, @key_usage, @key_permit_algo, @key_lifecycle are
+ *   not used and ignored if the derived key to be exported. Otherwise, these
+ *   parameters must be set.
+ */
+
+struct hkdf_ele_op_payload {
+	uint16_t ver;
+	uint16_t rsv;
+	uint32_t keystore_id;
+	uint32_t hkdf_algo;
+	uint32_t key_id;
+	uint16_t key_type;
+	uint16_t key_size;
+	uint32_t key_lifetime;
+	uint32_t key_usage;
+	uint32_t key_permit_algo;
+	uint32_t key_lifecycle;
+	uint32_t derived_key_id;
+	uint32_t buffer_len;
+};
+
+#endif /* __KEYMGR_DERIVE_HKDF_H__ */

commit 2ee7c5ed86042b0b4316769f1c37cb02a52d5345

SSMW-144 subsystems: ele: Add ELE permitted algo for SMW_ATTR_ALGO_HKDF
Add ELE permitted algo for SMW_ATTR_ALGO_HKDF.

Fixes: 8422ee825159 ("SSMW-576 subsystems: ele: Add support for HKDF(full, extract & expand)")

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

core/subsystems/ele/key_policy.c
@@ -105,6 +105,8 @@ static const struct {
 	PERMITTED_ALGO(ALL_AEAD, DEFAULT, ANY, NONE, AEAD),
 	PERMITTED_ALGO(ECDH_HKDF_SHA256, ECDH, NONE, SHA256, KEY_DERIVATION),
 	PERMITTED_ALGO(ECDH_HKDF_SHA384, ECDH, NONE, SHA384, KEY_DERIVATION),
+	PERMITTED_ALGO(HKDF_EXTRACT_SHA256, HKDF, NONE, SHA256, KEY_DERIVATION),
+	PERMITTED_ALGO(HKDF_EXTRACT_SHA384, HKDF, NONE, SHA384, KEY_DERIVATION),
 	PERMITTED_ALGO(HKDF_EXTRACT_SHA256, HKDF_EXTRACT, NONE, SHA256,
 		       KEY_DERIVATION),
 	PERMITTED_ALGO(HKDF_EXTRACT_SHA384, HKDF_EXTRACT, NONE, SHA384,
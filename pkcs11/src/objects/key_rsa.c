nxp-imx/imx-smw

commit a1f19fbc802c5d360f44dda69ff9752e1420a144

SSMW-187 pkcs11: add RSA key support
Add the RSA key support in library

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/objects/key_rsa.c
@@ -1,0 +1,467 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2021 NXP
+ */
+
+#include <stdlib.h>
+
+#include "attributes.h"
+#include "key_rsa.h"
+
+#include "lib_device.h"
+#include "libobj_types.h"
+#include "util.h"
+
+#include "trace.h"
+
+#define RSA_KEY_PUBLIC	BIT(0)
+#define RSA_KEY_PRIVATE BIT(1)
+#define RSA_KEY_PAIR	(RSA_KEY_PUBLIC | RSA_KEY_PRIVATE)
+
+enum attr_key_rsa_public_list {
+	PUB_MODULUS = 0,
+	PUB_MODULUS_LEN,
+	PUB_PUB_EXP,
+};
+
+const struct template_attr attr_key_rsa_public[] = {
+	[PUB_MODULUS] =
+		TATTR(key_rsa_pair, modulus, MODULUS, 0, MUST, bignumber),
+	[PUB_MODULUS_LEN] = TATTR(key_rsa_pair, modulus_length, MODULUS_BITS, 0,
+				  MUST_NOT, ulong),
+	[PUB_PUB_EXP] = TATTR(key_rsa_pair, pub_exp, PUBLIC_EXPONENT, 0, MUST,
+			      bignumber),
+};
+
+enum attr_key_rsa_private_list {
+	PRIV_MODULUS = 0,
+	PRIV_PUB_EXP,
+	PRIV_PRIV_EXP,
+	PRIV_PRIME_P,
+	PRIV_PRIME_Q,
+	PRIV_EXP_DP,
+	PRIV_EXP_DQ,
+	PRIV_COEFF,
+};
+
+const struct template_attr attr_key_rsa_private[] = {
+	[PRIV_MODULUS] =
+		TATTR(key_rsa_pair, modulus, MODULUS, 0, MUST, bignumber),
+	[PRIV_PUB_EXP] = TATTR(key_rsa_pair, pub_exp, PUBLIC_EXPONENT, 0,
+			       OPTIONAL, bignumber),
+	[PRIV_PRIV_EXP] = TATTR_P(key_rsa_pair, priv_exp, PRIVATE_EXPONENT, 0,
+				  MUST, bignumber),
+	[PRIV_PRIME_P] =
+		TATTR_P(key_rsa_pair, prime_p, PRIME_1, 0, OPTIONAL, bignumber),
+	[PRIV_PRIME_Q] =
+		TATTR_P(key_rsa_pair, prime_q, PRIME_2, 0, OPTIONAL, bignumber),
+	[PRIV_EXP_DP] = TATTR_P(key_rsa_pair, exp_dp, EXPONENT_1, 0, OPTIONAL,
+				bignumber),
+	[PRIV_EXP_DQ] = TATTR_P(key_rsa_pair, exp_dq, EXPONENT_2, 0, OPTIONAL,
+				bignumber),
+	[PRIV_COEFF] = TATTR_P(key_rsa_pair, coeff, COEFFICIENT, 0, OPTIONAL,
+			       bignumber),
+};
+
+/**
+ * key_rsa_allocate() - Allocate and initialize RSA keypair
+ * @pub_obj: RSA Public key object
+ * @priv_obj: RSA Private key object
+ * @type: Type of Key to allocate
+ *
+ * Allocation and set the @type of key to allocate which is:
+ *   RSA_KEY_PUBLIC
+ *   RSA_KEY_PRIVATE
+ *   RSA_KEY_PAIR
+ *
+ * return:
+ * Key allocated if success
+ * NULL otherwise
+ */
+static struct libobj_key_rsa_pair *key_rsa_allocate(struct libobj_obj *pub_obj,
+						    struct libobj_obj *priv_obj,
+						    unsigned int type)
+{
+	struct libobj_key_rsa_pair *key = NULL;
+
+	key = calloc(1, sizeof(*key));
+	if (key) {
+		key->type = type;
+
+		if (type & RSA_KEY_PUBLIC)
+			set_subkey_to(pub_obj, key);
+
+		if (type & RSA_KEY_PRIVATE)
+			set_subkey_to(priv_obj, key);
+	}
+
+	DBG_TRACE("Allocated a new RSA key (%p) of type %d", key, type);
+
+	return key;
+}
+
+/**
+ * key_rsa_free() - Free private or public key
+ * @obj: RSA Keypair object
+ * @type: Type of key private/public to free
+ *
+ * Free the key's field related to the request @type.
+ *
+ * Then, if the requested key @type to free is the same of the @key type:
+ *    - Delete the key from SMW subsystem if key'id set
+ *    - Free the keypair common fields
+ *    - Free the keypair object itself
+ *
+ * Else key is a keypair, hence switch the key type to the remaining
+ * key type part not freed.
+ */
+static void key_rsa_free(struct libobj_obj *obj, unsigned int type)
+{
+	struct libobj_key_rsa_pair *key = get_subkey_from(obj);
+
+	if (!key)
+		return;
+
+	switch (type) {
+	case RSA_KEY_PUBLIC:
+		break;
+
+	case RSA_KEY_PRIVATE:
+		if (key->priv_exp.value) {
+			free(key->priv_exp.value);
+			key->priv_exp.value = NULL;
+		}
+		if (key->prime_p.value) {
+			free(key->prime_p.value);
+			key->prime_p.value = NULL;
+		}
+		if (key->prime_q.value) {
+			free(key->prime_q.value);
+			key->prime_q.value = NULL;
+		}
+		if (key->exp_dp.value) {
+			free(key->exp_dp.value);
+			key->exp_dp.value = NULL;
+		}
+		if (key->exp_dq.value) {
+			free(key->exp_dq.value);
+			key->exp_dq.value = NULL;
+		}
+		if (key->coeff.value) {
+			free(key->coeff.value);
+			key->coeff.value = NULL;
+		}
+		break;
+
+	default:
+		return;
+	}
+
+	if (key->type == type) {
+		if (key->modulus.value)
+			free(key->modulus.value);
+		if (key->pub_exp.value)
+			free(key->pub_exp.value);
+
+		(void)libdev_delete_key(key->key_id);
+
+		free(key);
+	} else {
+		key->type &= ~type;
+	}
+
+	set_subkey_to(obj, NULL);
+}
+
+void key_rsa_public_free(struct libobj_obj *obj)
+{
+	key_rsa_free(obj, RSA_KEY_PUBLIC);
+}
+
+void key_rsa_private_free(struct libobj_obj *obj)
+{
+	key_rsa_free(obj, RSA_KEY_PRIVATE);
+}
+
+CK_RV key_rsa_public_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
+			    struct libattr_list *attrs)
+{
+	CK_RV ret;
+	struct libobj_key_rsa_pair *new_key;
+
+	new_key = key_rsa_allocate(obj, NULL, RSA_KEY_PUBLIC);
+	if (!new_key)
+		return CKR_HOST_MEMORY;
+
+	DBG_TRACE("Create a new RSA public key (%p)", new_key);
+
+	ret = attr_get_value(new_key, &attr_key_rsa_public[PUB_MODULUS], attrs,
+			     NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(new_key, &attr_key_rsa_public[PUB_MODULUS_LEN],
+			     attrs, NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(new_key, &attr_key_rsa_public[PUB_PUB_EXP], attrs,
+			     NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = libdev_import_key(hsession, obj);
+	DBG_TRACE("Public Key ID 0x%llX", new_key->key_id);
+
+end:
+	if (ret != CKR_OK)
+		key_rsa_public_free(obj);
+
+	return ret;
+}
+
+CK_RV key_rsa_public_get_attribute(CK_ATTRIBUTE_PTR attr,
+				   const struct libobj_obj *obj)
+{
+	CK_RV ret;
+
+	DBG_TRACE("Get attribute type=%#lx", attr->type);
+
+	ret = attr_get_obj_value(attr, attr_key_rsa_public,
+				 ARRAY_SIZE(attr_key_rsa_public),
+				 get_subkey_from(obj));
+	if (ret == CKR_ATTRIBUTE_TYPE_INVALID)
+		attr->ulValueLen = CK_UNAVAILABLE_INFORMATION;
+
+	DBG_TRACE("Get attribute type=%#lx ret %ld", attr->type, ret);
+	return ret;
+}
+
+CK_RV key_rsa_public_modify_attribute(CK_ATTRIBUTE_PTR attr,
+				      struct libobj_obj *obj)
+{
+	CK_RV ret;
+
+	DBG_TRACE("Modify attribute type=%#lx", attr->type);
+
+	ret = attr_modify_obj_value(attr, attr_key_rsa_public,
+				    ARRAY_SIZE(attr_key_rsa_public),
+				    get_subkey_from(obj));
+
+	DBG_TRACE("Modify attribute type=%#lx ret %ld", attr->type, ret);
+	return ret;
+}
+
+CK_RV key_rsa_private_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
+			     struct libattr_list *attrs)
+{
+	CK_RV ret;
+	struct libobj_key_rsa_pair *new_key;
+
+	new_key = key_rsa_allocate(NULL, obj, RSA_KEY_PRIVATE);
+	if (!new_key)
+		return CKR_HOST_MEMORY;
+
+	DBG_TRACE("Create a new RSA private key (%p)", new_key);
+
+	ret = attr_get_value(new_key, &attr_key_rsa_private[PRIV_MODULUS],
+			     attrs, NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(new_key, &attr_key_rsa_private[PRIV_MODULUS],
+			     attrs, NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	/*
+	 * Private key public exponent can be omitted, in this case
+	 * the token must use the default value 65537. otherwise it
+	 * must return an error.
+	 */
+	ret = attr_get_value(new_key, &attr_key_rsa_private[PRIV_PUB_EXP],
+			     attrs, NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(new_key, &attr_key_rsa_private[PRIV_PRIV_EXP],
+			     attrs, NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(new_key, &attr_key_rsa_private[PRIV_PRIME_P],
+			     attrs, NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(new_key, &attr_key_rsa_private[PRIV_PRIME_Q],
+			     attrs, NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(new_key, &attr_key_rsa_private[PRIV_EXP_DP], attrs,
+			     NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(new_key, &attr_key_rsa_private[PRIV_EXP_DQ], attrs,
+			     NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(new_key, &attr_key_rsa_private[PRIV_COEFF], attrs,
+			     NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = libdev_import_key(hsession, obj);
+	DBG_TRACE("Private Key ID 0x%llX", new_key->key_id);
+
+end:
+	if (ret != CKR_OK)
+		key_rsa_private_free(obj);
+
+	return ret;
+}
+
+CK_RV key_rsa_private_get_attribute(CK_ATTRIBUTE_PTR attr,
+				    const struct libobj_obj *obj, bool protect)
+{
+	CK_RV ret;
+
+	DBG_TRACE("Get attribute type=%#lx protected=%s", attr->type,
+		  protect ? "YES" : "NO");
+
+	ret = attr_get_obj_prot_value(attr, attr_key_rsa_private,
+				      ARRAY_SIZE(attr_key_rsa_private),
+				      get_subkey_from(obj), protect);
+	if (ret == CKR_ATTRIBUTE_TYPE_INVALID)
+		attr->ulValueLen = CK_UNAVAILABLE_INFORMATION;
+
+	DBG_TRACE("Get attribute type=%#lx ret %ld", attr->type, ret);
+	return ret;
+}
+
+CK_RV key_rsa_private_modify_attribute(CK_ATTRIBUTE_PTR attr,
+				       struct libobj_obj *obj)
+{
+	CK_RV ret;
+
+	DBG_TRACE("Modify attribute type=%#lx", attr->type);
+
+	ret = attr_modify_obj_value(attr, attr_key_rsa_private,
+				    ARRAY_SIZE(attr_key_rsa_private),
+				    get_subkey_from(obj));
+
+	DBG_TRACE("Modify attribute type=%#lx ret %ld", attr->type, ret);
+	return ret;
+}
+
+CK_RV key_rsa_keypair_generate(CK_SESSION_HANDLE hsession,
+			       CK_MECHANISM_PTR mech,
+			       struct libobj_obj *pub_obj,
+			       struct libattr_list *pub_attrs,
+			       struct libobj_obj *priv_obj,
+			       struct libattr_list *priv_attrs)
+{
+	(void)hsession;
+	(void)mech;
+	CK_RV ret;
+	struct libobj_key_rsa_pair *keypair;
+
+	keypair = key_rsa_allocate(pub_obj, priv_obj, RSA_KEY_PAIR);
+	if (!keypair)
+		return CKR_HOST_MEMORY;
+
+	DBG_TRACE("Generate a RSA keypair (%p)", keypair);
+
+	/* Verify the public key attributes */
+	ret = attr_get_value(keypair, &attr_key_rsa_public[PUB_MODULUS],
+			     pub_attrs, MUST_NOT);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(keypair, &attr_key_rsa_public[PUB_MODULUS_LEN],
+			     pub_attrs, MUST);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(keypair, &attr_key_rsa_public[PUB_PUB_EXP],
+			     pub_attrs, OPTIONAL);
+	if (ret != CKR_OK)
+		goto end;
+
+	/* Verify the private key attributes */
+	ret = attr_get_value(keypair, &attr_key_rsa_private[PRIV_MODULUS],
+			     priv_attrs, MUST_NOT);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(keypair, &attr_key_rsa_private[PRIV_PUB_EXP],
+			     priv_attrs, MUST_NOT);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(keypair, &attr_key_rsa_private[PRIV_PRIV_EXP],
+			     priv_attrs, MUST_NOT);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(keypair, &attr_key_rsa_private[PRIV_PRIME_P],
+			     priv_attrs, MUST_NOT);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(keypair, &attr_key_rsa_private[PRIV_PRIME_Q],
+			     priv_attrs, MUST_NOT);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(keypair, &attr_key_rsa_private[PRIV_EXP_DP],
+			     priv_attrs, MUST_NOT);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(keypair, &attr_key_rsa_private[PRIV_EXP_DQ],
+			     priv_attrs, MUST_NOT);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(keypair, &attr_key_rsa_private[PRIV_COEFF],
+			     priv_attrs, MUST_NOT);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = libdev_operate_mechanism(hsession, mech, priv_obj);
+	DBG_TRACE("Key Pair ID 0x%llX", keypair->key_id);
+
+end:
+	if (ret != CKR_OK) {
+		key_rsa_public_free(pub_obj);
+		key_rsa_private_free(priv_obj);
+	}
+
+	return ret;
+}
+
+CK_RV key_rsa_get_id(struct libbytes *id, struct libobj_obj *obj,
+		     size_t prefix_len)
+{
+	struct libobj_key_rsa_pair *keypair;
+
+	if (!obj || !id)
+		return CKR_GENERAL_ERROR;
+
+	keypair = get_subkey_from(obj);
+
+	id->number = prefix_len + sizeof(keypair->key_id);
+	id->array = malloc(id->number);
+	if (!id->array)
+		return CKR_HOST_MEMORY;
+
+	DBG_TRACE("RSA Key ID 0x%llX", keypair->key_id);
+
+	TO_CK_BYTES(&id->array[prefix_len], keypair->key_id);
+
+	return CKR_OK;
+}

pkcs11/src/ifsmw/args_attr.c
@@ -1,0 +1,63 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2021 NXP
+ */
+
+#include "libobj_types.h"
+
+#include "args_attr.h"
+
+#include "trace.h"
+
+static CK_RV rsa_key_attr(struct smw_tlv *attr, struct libobj_obj *obj)
+{
+	CK_RV ret = CKR_OK;
+	struct libobj_key_rsa_pair *key = get_subkey_from(obj);
+
+	/*
+	 * The public exponent might be present and define
+	 * the RSA public exponent attribute.
+	 */
+	if (key->pub_exp.value) {
+		DBG_TRACE("Build RSA public exponent attribute");
+		ret = tlv_encode_large_numeral(attr, "RSA_PUB_EXP",
+					       &key->pub_exp);
+	}
+
+	return ret;
+}
+
+CK_RV args_attr_generate_key(struct smw_tlv *attr, struct libobj_obj *obj)
+{
+	CK_RV ret;
+
+	if (is_token_obj(obj, storage)) {
+		DBG_TRACE("Generate Persistent Key");
+		ret = tlv_encode_boolean(attr, "PERSISTENT");
+		if (ret != CKR_OK)
+			return ret;
+	}
+
+	switch (get_key_type(obj)) {
+	case CKK_RSA:
+		ret = rsa_key_attr(attr, obj);
+		break;
+
+	default:
+		ret = CKR_OK;
+	}
+
+	return ret;
+}
+
+CK_RV args_attr_import_key(struct smw_tlv *attr, struct libobj_obj *obj)
+{
+	CK_RV ret = CKR_OK;
+
+	if (is_token_obj(obj, storage)) {
+		DBG_TRACE("Import Persistent Key");
+		ret = tlv_encode_boolean(attr, "PERSISTENT");
+	}
+
+	return ret;
+}

pkcs11/src/ifsmw/args_attr.h
@@ -1,0 +1,35 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2021 NXP
+ */
+#ifndef __ARGS_ATTR_H__
+#define __ARGS_ATTR_H__
+
+#include "pkcs11smw.h"
+#include "types.h"
+
+#include "tlv_encode.h"
+
+/**
+ * args_attr_generate_key() - Build the key generate attribute list
+ * @attr: Generate key attribute list
+ * @obj: Key object
+ *
+ * Return:
+ * CKR_HOST_MEMORY               - Out of memory
+ * CKR_OK                        - Success
+ */
+CK_RV args_attr_generate_key(struct smw_tlv *attr, struct libobj_obj *obj);
+
+/**
+ * args_attr_import_key() - Build the key import attribute list
+ * @attr: Import key attribute list
+ * @obj: Key object
+ *
+ * Return:
+ * CKR_HOST_MEMORY               - Out of memory
+ * CKR_OK                        - Success
+ */
+CK_RV args_attr_import_key(struct smw_tlv *attr, struct libobj_obj *obj);
+
+#endif /* __ARGS_ATTR_H__ */

pkcs11/src/ifsmw/key_desc.c
@@ -5,6 +5,7 @@
 
 #include "asn1_ec_curve.h"
 #include "libobj_types.h"
+#include "util.h"
 #include "util_asn1.h"
 
 #include "key_desc.h"
@@ -205,6 +206,45 @@ static CK_RV cipher_key_desc(struct smw_key_descriptor *desc,
 	return CKR_OK;
 }
 
+static CK_RV rsa_key_desc(struct smw_key_descriptor *desc,
+			  struct libobj_obj *obj)
+{
+	struct smw_keypair_rsa *smw_key;
+	struct libobj_key_rsa_pair *key = get_subkey_from(obj);
+
+	desc->type_name = "RSA";
+
+	/*
+	 * Modulus length defines the RSA security size
+	 * If key field modulus_length is defined, this is
+	 * by definition a key generation.
+	 * Else, the object key modulus length buffer defines
+	 * the key security size
+	 */
+	if (key->modulus_length)
+		desc->security_size = key->modulus_length;
+	else if (key->modulus.value)
+		desc->security_size = util_get_bignum_bits(&key->modulus);
+	else
+		return CKR_ATTRIBUTE_VALUE_INVALID;
+
+	/*
+	 * If SMW key's descriptor buffer field is set, setup it
+	 * with the RSA key object's buffer
+	 */
+	if (desc->buffer) {
+		smw_key = &desc->buffer->rsa;
+		smw_key->modulus = key->modulus.value;
+		smw_key->modulus_length = key->modulus.length;
+		smw_key->public_data = key->pub_exp.value;
+		smw_key->public_length = key->pub_exp.length;
+		smw_key->private_data = key->priv_exp.value;
+		smw_key->private_length = key->priv_exp.length;
+	}
+
+	return CKR_OK;
+}
+
 CK_RV key_desc_setup(struct smw_key_descriptor *desc, struct libobj_obj *obj)
 {
 	CK_RV ret;
@@ -218,6 +258,10 @@ CK_RV key_desc_setup(struct smw_key_descriptor *desc, struct libobj_obj *obj)
 
 	case CKK_EC:
 		ret = ec_key_desc(desc, obj);
+		break;
+
+	case CKK_RSA:
+		ret = rsa_key_desc(desc, obj);
 		break;
 
 	default:
@@ -239,8 +283,16 @@ void key_desc_copy_key_id(struct libobj_obj *obj,
 		key->key_id = desc->id;
 		break;
 	}
+
 	case CKK_EC: {
 		struct libobj_key_ec_pair *key = get_subkey_from(obj);
+
+		key->key_id = desc->id;
+		break;
+	}
+
+	case CKK_RSA: {
+		struct libobj_key_rsa_pair *key = get_subkey_from(obj);
 
 		key->key_id = desc->id;
 		break;

pkcs11/src/ifsmw/mechanims.c
@@ -16,6 +16,7 @@
 #include "pkcs11smw.h"
 #include "types.h"
 
+#include "args_attr.h"
 #include "key_desc.h"
 #include "tlv_encode.h"
 
@@ -116,12 +117,14 @@ static struct mentry meckeygen[] = {
 };
 
 /*
- * AES Key Generate mechanism
+ * Key Generate mechanism
+ * Cipher and RSA keys
  */
 static struct mentry mkeygen[] = {
 	M_ALGO_SINGLE(AES, AES_KEY_GEN),
 	M_ALGO_SINGLE(DES, DES_KEY_GEN),
 	M_ALGO_SINGLE(DES3, DES3_KEY_GEN),
+	M_ALGO_SINGLE(RSA, RSA_PKCS_KEY_PAIR_GEN),
 };
 
 /*
@@ -400,16 +403,12 @@ static CK_RV op_keygen_common(CK_SLOT_ID slotid, struct libobj_obj *obj)
 	gen_args.subsystem_name = devinfo->name;
 	gen_args.key_descriptor = &key;
 
-	if (is_token_obj(obj, storage)) {
-		DBG_TRACE("Generate Persistent Key");
-		ret = tlv_encode_boolean(&key_attr, "PERSISTENT");
-		if (ret != CKR_OK)
-			goto end;
+	ret = args_attr_generate_key(&key_attr, obj);
+	if (ret != CKR_OK)
+		goto end;
 
-		gen_args.key_attributes_list =
-			(const unsigned char *)key_attr.string;
-		gen_args.key_attributes_list_length = key_attr.length;
-	}
+	gen_args.key_attributes_list = (const unsigned char *)key_attr.string;
+	gen_args.key_attributes_list_length = key_attr.length;
 
 	status = smw_generate_key(&gen_args);
 
@@ -566,16 +565,12 @@ CK_RV libdev_import_key(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
 	imp_args.subsystem_name = devinfo->name;
 	imp_args.key_descriptor = &key;
 
-	if (is_token_obj(obj, storage)) {
-		DBG_TRACE("Import Persistent Key");
-		ret = tlv_encode_boolean(&key_attr, "PERSISTENT");
-		if (ret != CKR_OK)
-			goto end;
+	ret = args_attr_import_key(&key_attr, obj);
+	if (ret != CKR_OK)
+		goto end;
 
-		imp_args.key_attributes_list =
-			(const unsigned char *)key_attr.string;
-		imp_args.key_attributes_list_length = key_attr.length;
-	}
+	imp_args.key_attributes_list = (const unsigned char *)key_attr.string;
+	imp_args.key_attributes_list_length = key_attr.length;
 
 	status = smw_import_key(&imp_args);
 	ret = smw_status_to_ck_rv(status);

pkcs11/src/ifsmw/tlv_encode.c
@@ -6,43 +6,93 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "util.h"
+
 #include "tlv_encode.h"
 
-CK_RV tlv_encode_boolean(struct smw_tlv *tlv, const char *type)
+static CK_RV tlv_realloc(struct smw_tlv *tlv, size_t length)
 {
-	size_t len_add;
-
-	/*
-	 * Boolean is TLV encode with:
-	 *   - Type = null terminated string @type
-	 *   - Length = 2 bytes of 0
-	 *   - Value is not set
-	 */
-	len_add = strlen(type) + 3;
 	if (!tlv->string) {
-		tlv->string = malloc(len_add);
+		tlv->string = malloc(length);
 		if (!tlv->string)
 			return CKR_HOST_MEMORY;
 
-		tlv->length_max = len_add;
-	} else if (tlv->length_max < tlv->length + len_add) {
-		tlv->string = realloc(tlv->string, tlv->length_max + len_add);
+		tlv->length_max = length;
+	} else if (tlv->length_max < tlv->length + length) {
+		tlv->string = realloc(tlv->string, tlv->length_max + length);
 		if (!tlv->string)
 			return CKR_HOST_MEMORY;
 
-		tlv->length_max += len_add;
+		tlv->length_max += length;
 	}
 
+	return CKR_OK;
+}
+
+static void set_tlv_type(struct smw_tlv *tlv, const char *type)
+{
 	/* Add the Type @type to current tlv string */
 	memcpy(&tlv->string[tlv->length], type, strlen(type));
 	tlv->length += strlen(type);
 
 	/* Set the Type null terminated character */
 	tlv->string[tlv->length++] = 0;
+}
+
+static void set_tlv_length(struct smw_tlv *tlv, size_t length)
+{
+	tlv->string[tlv->length++] = GET_BYTE(length, 1);
+	tlv->string[tlv->length++] = GET_BYTE(length, 0);
+}
+
+CK_RV tlv_encode_boolean(struct smw_tlv *tlv, const char *type)
+{
+	CK_RV ret;
+	size_t len_add;
+
+	/*
+	 * Boolean is TLV encoded with:
+	 *   - Type = null terminated string @type
+	 *   - Length = 2 bytes of 0
+	 *   - Value is not set
+	 */
+	len_add = strlen(type) + 3;
+	ret = tlv_realloc(tlv, len_add);
+	if (ret != CKR_OK)
+		return ret;
+
+	set_tlv_type(tlv, type);
 
 	/* Set the 2 bytes of Length = 0 */
-	tlv->string[tlv->length++] = 0;
-	tlv->string[tlv->length++] = 0;
+	set_tlv_length(tlv, 0);
+
+	return CKR_OK;
+}
+
+CK_RV tlv_encode_large_numeral(struct smw_tlv *tlv, const char *type,
+			       struct libbignumber *bignum)
+{
+	CK_RV ret;
+	size_t len_add;
+
+	/*
+	 * Large numeral is TLV encoded with:
+	 *   - Type = null terminated string @type
+	 *   - Length = 2 bytes (numeral byte length)
+	 *   - Value = numeral value
+	 */
+	len_add = strlen(type) + 3 + bignum->length;
+	ret = tlv_realloc(tlv, len_add);
+	if (ret != CKR_OK)
+		return ret;
+
+	set_tlv_type(tlv, type);
+
+	/* Set the 2 bytes of Length */
+	set_tlv_length(tlv, bignum->length);
+
+	memcpy(&tlv->string[tlv->length], bignum->value, bignum->length);
+	tlv->length += bignum->length;
 
 	return CKR_OK;
 }

pkcs11/src/ifsmw/tlv_encode.h
@@ -5,6 +5,7 @@
 #ifndef __TLV_ENCODE_H__
 #define __TLV_ENCODE_H__
 
+#include "libobj_types.h"
 #include "pkcs11smw.h"
 
 struct smw_tlv {
@@ -21,13 +22,31 @@ struct smw_tlv {
  * Function encodes a TLV's boolean of @type and add it to the
  * @tlv's string.
  * If current @tlv's string is too small, reallocate it to contain the
- * new boolean TLS's string.
+ * new boolean TLV's string.
  *
  * return:
  * CKR_HOST_MEMORY               - Out of memory
  * CKR_OK                        - Success
  */
 CK_RV tlv_encode_boolean(struct smw_tlv *tlv, const char *type);
+
+/**
+ * tlv_encode_large_numeral() - Add a TLV's large numeral @type to TLV string
+ * @tlv: TLV's string to be updated
+ * @type: Large numeral string to add
+ * @bignum: Bignumber to encode
+ *
+ * Function encodes a TLV's large numeral of @type and add it to the
+ * @tlv's string.
+ * If current @tlv's string is too small, reallocate it to contain the
+ * new large numeral TLV's string.
+ *
+ * return:
+ * CKR_HOST_MEMORY               - Out of memory
+ * CKR_OK                        - Success
+ */
+CK_RV tlv_encode_large_numeral(struct smw_tlv *tlv, const char *type,
+			       struct libbignumber *bignum);
 
 /**
  * tlv_encode_free() - Free the TLV's string

pkcs11/src/include/libobj_types.h
@@ -151,6 +151,20 @@ struct libobj_key_ec_pair {
 	struct libbignumber value_d; // Secure Key scalar
 };
 
+struct libobj_key_rsa_pair {
+	unsigned long long key_id;
+	unsigned int type;
+	struct libbignumber modulus;  // Modulus n
+	CK_ULONG modulus_length;      // Modulus length in bits
+	struct libbignumber pub_exp;  // Public Exponent e
+	struct libbignumber priv_exp; // Private Exponent d
+	struct libbignumber prime_p;  // Private Prime p
+	struct libbignumber prime_q;  // Private Prime q
+	struct libbignumber exp_dp;   // Private exponent d modulo p-1
+	struct libbignumber exp_dq;   // Private exponent d modulo q-1
+	struct libbignumber coeff;    // Private CRT coefficient q^-1 mod p
+};
+
 struct libobj_key_cipher {
 	unsigned long long key_id;
 	struct libbytes value;

commit 80ccfa1ee5d847064d3e6f4a517f833961976911

SSMW-749 pkcs11: Import key to subsystem only if CKA_TOKEN is true
Import only token keys to the subsystem when C_CreateObject() is called.

Fixes: a1f19fbc802c ("SSMW-187 pkcs11: add RSA key support")

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

pkcs11/src/objects/key_rsa.c
@@ -221,8 +221,11 @@ CK_RV key_rsa_public_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 	if (ret != CKR_OK)
 		goto end;
 
-	ret = libdev_import_key(hsession, obj);
-	DBG_TRACE("Public Key ID 0x%X", new_key->key_id);
+	/* Import only the token key to the subsystem */
+	if (is_token_obj(obj, storage)) {
+		ret = libdev_import_key(hsession, obj);
+		DBG_TRACE("Public Key ID 0x%X", new_key->key_id);
+	}
 
 end:
 	if (ret != CKR_OK)
@@ -356,8 +359,11 @@ CK_RV key_rsa_private_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 	if (ret != CKR_OK)
 		goto end;
 
-	ret = libdev_import_key(hsession, obj);
-	DBG_TRACE("Private Key ID 0x%X", new_key->key_id);
+	/* Import only the token key to the subsystem */
+	if (is_token_obj(obj, storage)) {
+		ret = libdev_import_key(hsession, obj);
+		DBG_TRACE("Private Key ID 0x%X", new_key->key_id);
+	}
 
 end:
 	if (ret != CKR_OK)
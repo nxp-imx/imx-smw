80ccfa1ee5d847064d3e6f4a517f833961976911
Rakshata Nayak <rakshata.nayak@nxp.com>
2024-10-18 08:59:28 +0530

    SSMW-749 pkcs11: Import key to subsystem only if CKA_TOKEN is true
    Import only token keys to the subsystem when C_CreateObject() is called.
    
    Fixes: a1f19fbc802c ("SSMW-187 pkcs11: add RSA key support")
    
    Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

--- pkcs11/src/objects/key_rsa.c
+++ pkcs11/src/objects/key_rsa.c
@@ -221,8 +221,11 @@ CK_RV key_rsa_public_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 	if (ret != CKR_OK)
 		goto end;
 
-	ret = libdev_import_key(hsession, obj);
-	DBG_TRACE("Public Key ID 0x%X", new_key->key_id);
+	/* Import only the token key to the subsystem */
+	if (is_token_obj(obj, storage)) {
+		ret = libdev_import_key(hsession, obj);
+		DBG_TRACE("Public Key ID 0x%X", new_key->key_id);
+	}
 
 end:
 	if (ret != CKR_OK)
@@ -356,8 +359,11 @@ CK_RV key_rsa_private_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 	if (ret != CKR_OK)
 		goto end;
 
-	ret = libdev_import_key(hsession, obj);
-	DBG_TRACE("Private Key ID 0x%X", new_key->key_id);
+	/* Import only the token key to the subsystem */
+	if (is_token_obj(obj, storage)) {
+		ret = libdev_import_key(hsession, obj);
+		DBG_TRACE("Private Key ID 0x%X", new_key->key_id);
+	}
 
 end:
 	if (ret != CKR_OK)

nxp-imx/imx-smw

commit 695a852704782a926b3ce6fcb5b967271db30f93

SSMW-130 pkcs11: add SMW's import key when key created
Key object creation corresponds to a key import SMW.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/objects/key_cipher.c
@@ -59,9 +59,12 @@ void key_cipher_free(struct libobj_obj *obj)
 		free(key->value.array);
 
 	free(key);
+
+	set_subkey_to(obj, NULL);
 }
 
-CK_RV key_cipher_create(struct libobj_obj *obj, struct libattr_list *attrs)
+CK_RV key_cipher_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
+			struct libattr_list *attrs)
 {
 	CK_RV ret;
 	struct libobj_key_cipher *new_key;
@@ -75,15 +78,26 @@ CK_RV key_cipher_create(struct libobj_obj *obj, struct libattr_list *attrs)
 	ret = attr_get_value(&new_key->value, &attr_key_cipher[SEC_VALUE],
 			     attrs, NO_OVERWRITE);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
 	/* Verify the key size value is not defined */
 	ret = attr_get_value(NULL, &attr_key_cipher[SEC_VALUE_LEN], attrs,
 			     NO_OVERWRITE);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
-	return CKR_OK;
+	/* Set the Key object value length equal to the buffer value length */
+	new_key->value_len = new_key->value.number;
+
+	/* Import the secret key in the SMW library session's subsystem */
+	ret = libdev_import_key(hsession, obj);
+	DBG_TRACE("Cipher Key ID 0x%llX", new_key->key_id);
+
+end:
+	if (ret != CKR_OK)
+		key_cipher_free(obj);
+
+	return ret;
 }
 
 CK_RV key_cipher_generate(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
@@ -102,7 +116,7 @@ CK_RV key_cipher_generate(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
 	ret = attr_get_value(&key->value, &attr_key_cipher[SEC_VALUE], attrs,
 			     MUST_NOT);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
 	if (get_key_type(obj) == CKK_AES)
 		ret = attr_get_value(&key->value_len,
@@ -114,11 +128,15 @@ CK_RV key_cipher_generate(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
 				     NO_OVERWRITE);
 
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
 	/* Generate the secret key with SMW library */
 	ret = libdev_operate_mechanism(hsession, mech, obj);
 	DBG_TRACE("Cipher Key ID 0x%llX", key->key_id);
+
+end:
+	if (ret != CKR_OK)
+		key_cipher_free(obj);
 
 	return ret;
 }

pkcs11/src/objects/key_ec.c
@@ -31,11 +31,15 @@ const struct template_attr attr_key_ec_public[] = {
 enum attr_key_ec_private_list {
 	PRIV_PARAMS = 0,
 	PRIV_VALUE,
+	PRIV_PUB_POINT,
 };
 
 const struct template_attr attr_key_ec_private[] = {
-	[PRIV_PARAMS] = { CKA_EC_PARAMS, 0, MUST, attr_to_byte_array },
-	[PRIV_VALUE] = { CKA_VALUE, 0, MUST, attr_to_bignumber },
+	[PRIV_PARAMS] =
+		TATTR(key_ec_pair, params, EC_PARAMS, 0, MUST, byte_array),
+	[PRIV_VALUE] = TATTR_P(key_ec_pair, value_d, VALUE, 0, MUST, bignumber),
+	[PRIV_PUB_POINT] =
+		TATTR(key_ec_pair, point_q, EC_POINT, 0, MUST, byte_array),
 };
 
 /**
@@ -99,13 +103,21 @@ static void key_ec_free(struct libobj_obj *obj, unsigned int type)
 
 	switch (type) {
 	case EC_KEY_PUBLIC:
-		if (key->point_q.array)
+		if (key->point_q.array) {
 			free(key->point_q.array);
+			key->point_q.array = NULL;
+		}
 		break;
 
 	case EC_KEY_PRIVATE:
-		if (key->value_d.value)
+		if (key->value_d.value) {
 			free(key->value_d.value);
+			key->value_d.value = NULL;
+		}
+		if (key->type == EC_KEY_PRIVATE && key->point_q.array) {
+			free(key->point_q.array);
+			key->point_q.array = NULL;
+		}
 		break;
 	default:
 		return;
@@ -121,6 +133,8 @@ static void key_ec_free(struct libobj_obj *obj, unsigned int type)
 	} else {
 		key->type &= ~type;
 	}
+
+	set_subkey_to(obj, NULL);
 }
 
 void key_ec_public_free(struct libobj_obj *obj)
@@ -133,7 +147,8 @@ void key_ec_private_free(struct libobj_obj *obj)
 	key_ec_free(obj, EC_KEY_PRIVATE);
 }
 
-CK_RV key_ec_public_create(struct libobj_obj *obj, struct libattr_list *attrs)
+CK_RV key_ec_public_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
+			   struct libattr_list *attrs)
 {
 	CK_RV ret;
 	struct libobj_key_ec_pair *new_key;
@@ -147,15 +162,25 @@ CK_RV key_ec_public_create(struct libobj_obj *obj, struct libattr_list *attrs)
 	ret = attr_get_value(&new_key->params, &attr_key_ec_public[PUB_PARAMS],
 			     attrs, NO_OVERWRITE);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
 	ret = attr_get_value(&new_key->point_q, &attr_key_ec_public[PUB_POINT],
 			     attrs, NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = libdev_import_key(hsession, obj);
+	DBG_TRACE("Public Key ID 0x%llX", new_key->key_id);
+
+end:
+	if (ret != CKR_OK)
+		key_ec_public_free(obj);
 
 	return ret;
 }
 
-CK_RV key_ec_private_create(struct libobj_obj *obj, struct libattr_list *attrs)
+CK_RV key_ec_private_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
+			    struct libattr_list *attrs)
 {
 	CK_RV ret;
 	struct libobj_key_ec_pair *new_key;
@@ -170,15 +195,31 @@ CK_RV key_ec_private_create(struct libobj_obj *obj, struct libattr_list *attrs)
 			     &attr_key_ec_private[PRIV_PARAMS], attrs,
 			     NO_OVERWRITE);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
 	ret = attr_get_value(&new_key->value_d,
 			     &attr_key_ec_private[PRIV_VALUE], attrs,
 			     NO_OVERWRITE);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
-	return CKR_OK;
+	/*
+	 * Private key required the public point to be imported
+	 * in token.
+	 */
+	ret = attr_get_value(new_key, &attr_key_ec_private[PRIV_PUB_POINT],
+			     attrs, NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = libdev_import_key(hsession, obj);
+	DBG_TRACE("Private Key ID 0x%llX", new_key->key_id);
+
+end:
+	if (ret != CKR_OK)
+		key_ec_private_free(obj);
+
+	return ret;
 }
 
 CK_RV key_ec_keypair_generate(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
@@ -202,28 +243,34 @@ CK_RV key_ec_keypair_generate(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
 	ret = attr_get_value(&keypair->params, &attr_key_ec_public[PUB_PARAMS],
 			     pub_attrs, NO_OVERWRITE);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
 	ret = attr_get_value(&keypair->point_q, &attr_key_ec_public[PUB_POINT],
 			     pub_attrs, MUST_NOT);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
 	/* Verify the private key attributes */
 	ret = attr_get_value(&keypair->params,
 			     &attr_key_ec_private[PRIV_PARAMS], priv_attrs,
 			     MUST_NOT);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
 	ret = attr_get_value(&keypair->value_d,
 			     &attr_key_ec_private[PRIV_VALUE], priv_attrs,
 			     MUST_NOT);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
 	ret = libdev_operate_mechanism(hsession, mech, priv_obj);
 	DBG_TRACE("Key Pair ID 0x%llX", keypair->key_id);
+
+end:
+	if (ret != CKR_OK) {
+		key_ec_public_free(pub_obj);
+		key_ec_private_free(priv_obj);
+	}
 
 	return ret;
 }

pkcs11/src/ifsmw/key_desc.c
@@ -133,6 +133,23 @@ static CK_RV ec_key_desc(struct smw_key_descriptor *desc,
 		desc->security_size = curve->dev->security_size;
 	}
 
+	/*
+	 * If SMW key's descriptor buffer field is set, setup it
+	 * with the EC key object's buffer
+	 */
+	if (desc->buffer) {
+		/*
+		 * Remove the first byte of the Public Buffer
+		 * DER ANSI X9.62 uncompress code byte
+		 */
+		if (key->point_q.array) {
+			desc->buffer->public_data = key->point_q.array + 1;
+			desc->buffer->public_length = key->point_q.number - 1;
+		}
+		desc->buffer->private_data = key->value_d.value;
+		desc->buffer->private_length = key->value_d.length;
+	}
+
 	return ret;
 }
 
@@ -171,6 +188,15 @@ static CK_RV cipher_key_desc(struct smw_key_descriptor *desc,
 	};
 
 	desc->type_name = cipher->smw_name;
+
+	/*
+	 * If SMW key's descriptor buffer field is set, setup it
+	 * with the Cipher key object's buffer
+	 */
+	if (desc->buffer) {
+		desc->buffer->private_data = key->value.array;
+		desc->buffer->private_length = key->value.number;
+	}
 
 	return CKR_OK;
 }

pkcs11/src/ifsmw/mechanims.c
@@ -502,6 +502,64 @@ CK_RV libdev_operate_mechanism(CK_SESSION_HANDLE hsession,
 	return ret;
 }
 
+CK_RV libdev_import_key(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
+{
+	CK_RV ret;
+	int status;
+	CK_SLOT_ID slotid;
+	const struct libdev *devinfo;
+	struct smw_tlv key_attr = { 0 };
+	struct smw_import_key_args imp_args = { 0 };
+	struct smw_key_descriptor key = { 0 };
+	struct smw_keypair_buffer keypair_buffer = { 0 };
+
+	DBG_TRACE("Import a Key");
+	ret = libsess_get_slotid(hsession, &slotid);
+	if (ret != CKR_OK)
+		return ret;
+
+	devinfo = libdev_get_devinfo(slotid);
+	if (!devinfo)
+		return CKR_SLOT_ID_INVALID;
+
+	/*
+	 * Set the key's buffer field to get the
+	 * object key's buffer(s) to import.
+	 */
+	key.buffer = &keypair_buffer;
+
+	ret = key_desc_setup(&key, obj);
+	if (ret != CKR_OK)
+		return ret;
+
+	imp_args.subsystem_name = devinfo->name;
+	imp_args.key_descriptor = &key;
+
+	if (is_token_obj(obj, storage)) {
+		DBG_TRACE("Import Persistent Key");
+		ret = tlv_encode_boolean(&key_attr, "PERSISTENT");
+		if (ret != CKR_OK)
+			goto end;
+
+		imp_args.key_attributes_list =
+			(const unsigned char *)key_attr.string;
+		imp_args.key_attributes_list_length = key_attr.length;
+	}
+
+	status = smw_import_key(&imp_args);
+
+	DBG_TRACE("Import Key on %s status %d", devinfo->name, status);
+	if (status != SMW_STATUS_OK)
+		ret = CKR_FUNCTION_FAILED;
+	else
+		key_desc_copy_key_id(obj, &key);
+
+end:
+	tlv_encode_free(&key_attr);
+
+	return ret;
+}
+
 CK_RV libdev_delete_key(unsigned long long key_id)
 {
 	int status;

pkcs11/src/include/lib_device.h
@@ -244,6 +244,26 @@ CK_RV libdev_operate_mechanism(CK_SESSION_HANDLE hsession,
 			       CK_MECHANISM_PTR mech, void *args);
 
 /**
+ * libdev_import_key() - Call SMW import key API
+ * @hsession: Session handle
+ * @obj: Key object to import
+ *
+ * Function prepare the SMW API argument for the API key import.
+ *
+ * Return:
+ * CKR_CRYPTOKI_NOT_INITIALIZED  - Context not initialized
+ * CKR_GENERAL_ERROR             - No slot defined
+ * CKR_SESSION_HANDLE_INVALID    - Session Handle invalid
+ * CKR_FUNCTION_FAILED           - Operation failed
+ * CKR_SLOT_ID_INVALID           - Slot ID is not valid
+ * CKR_CURVE_NOT_SUPPORTED       - Curve is not supported
+ * CKR_ATTRIBUTE_TYPE_INVALID    - Attribute type is not valid
+ * CKR_HOST_MEMORY               - Out of memory
+ * CKR_OK                        - Success
+ */
+CK_RV libdev_import_key(CK_SESSION_HANDLE hsession, struct libobj_obj *obj);
+
+/**
  * libdev_delete_key() - Call SMW delete key API
  * @key_id: Key id to deleete
  *

pkcs11/src/objects/key.c
@@ -692,12 +692,17 @@ static CK_RV key_public_new(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 
 /**
  * subkey_secret_create() - Create a secret subkey object
+ * @hsession: Session handle
  * @obj: Key object
  * @attrs: List of object attributes
  *
  * Call the key object type creation function.
  *
  * return:
+ * CKR_CRYPTOKI_NOT_INITIALIZED  - Context not initialized
+ * CKR_GENERAL_ERROR             - No slot defined
+ * CKR_SESSION_HANDLE_INVALID    - Session Handle invalid
+ * CKR_SLOT_ID_INVALID           - Slot ID is not valid
  * CKR_ATTRIBUTE_VALUE_INVALID   - Attribute value is not valid
  * CKR_FUNCTION_FAILED           - Function failure
  * CKR_TEMPLATE_INCOMPLETE       - Attribute template incomplete
@@ -707,7 +712,8 @@ static CK_RV key_public_new(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
  * CKR_FUNCTION_FAILED           - Function failure
  * CKR_OK                        - Success
  */
-static CK_RV subkey_secret_create(struct libobj_obj *obj,
+static CK_RV subkey_secret_create(CK_SESSION_HANDLE hsession,
+				  struct libobj_obj *obj,
 				  struct libattr_list *attrs)
 {
 	CK_RV ret;
@@ -716,7 +722,7 @@ static CK_RV subkey_secret_create(struct libobj_obj *obj,
 	case CKK_AES:
 	case CKK_DES:
 	case CKK_DES3:
-		ret = key_cipher_create(obj, attrs);
+		ret = key_cipher_create(hsession, obj, attrs);
 		break;
 
 	default:
@@ -728,6 +734,7 @@ static CK_RV subkey_secret_create(struct libobj_obj *obj,
 
 /**
  * subkey_private_create() - Create a private subkey object
+ * @hsession: Session handle
  * @obj: Key object
  * @attrs: List of object attributes
  *
@@ -735,6 +742,10 @@ static CK_RV subkey_secret_create(struct libobj_obj *obj,
  *
  * return:
  * CKR_CRYPTOKI_NOT_INITIALIZED  - Context not initialized
+ * CKR_GENERAL_ERROR             - No slot defined
+ * CKR_SESSION_HANDLE_INVALID    - Session Handle invalid
+ * CKR_SLOT_ID_INVALID           - Slot ID is not valid
+ * CKR_CURVE_NOT_SUPPORTED       - Curve is not supported
  * CKR_ATTRIBUTE_VALUE_INVALID   - Attribute value is not valid
  * CKR_FUNCTION_FAILED           - Function failure
  * CKR_TEMPLATE_INCOMPLETE       - Attribute template incomplete
@@ -744,14 +755,15 @@ static CK_RV subkey_secret_create(struct libobj_obj *obj,
  * CKR_FUNCTION_FAILED           - Function failure
  * CKR_OK                        - Success
  */
-static CK_RV subkey_private_create(struct libobj_obj *obj,
+static CK_RV subkey_private_create(CK_SESSION_HANDLE hsession,
+				   struct libobj_obj *obj,
 				   struct libattr_list *attrs)
 {
 	CK_RV ret;
 
 	switch (get_key_type(obj)) {
 	case CKK_EC:
-		ret = key_ec_private_create(obj, attrs);
+		ret = key_ec_private_create(hsession, obj, attrs);
 		break;
 
 	default:
@@ -763,6 +775,7 @@ static CK_RV subkey_private_create(struct libobj_obj *obj,
 
 /**
  * subkey_public_create() - Create a public subkey object
+ * @hsession: Session handle
  * @obj: Key object
  * @attrs: List of object attributes
  *
@@ -771,6 +784,10 @@ static CK_RV subkey_private_create(struct libobj_obj *obj,
  * return:
  * CKR_CRYPTOKI_NOT_INITIALIZED  - Context not initialized
  * CKR_GENERAL_ERROR             - No slot defined
+ * CKR_SESSION_HANDLE_INVALID    - Session Handle invalid
+ * CKR_SLOT_ID_INVALID           - Slot ID is not valid
+ * CKR_CURVE_NOT_SUPPORTED       - Curve is not supported
+ * CKR_ATTRIBUTE_VALUE_INVALID   - Attribute value is not valid
  * CKR_FUNCTION_FAILED           - Function failure
  * CKR_TEMPLATE_INCOMPLETE       - Attribute template incomplete
  * CKR_TEMPLATE_INCONSISTENT     - One of the attribute is not valid
@@ -779,14 +796,15 @@ static CK_RV subkey_private_create(struct libobj_obj *obj,
  * CKR_FUNCTION_FAILED           - Function failure
  * CKR_OK                        - Success
  */
-static CK_RV subkey_public_create(struct libobj_obj *obj,
+static CK_RV subkey_public_create(CK_SESSION_HANDLE hsession,
+				  struct libobj_obj *obj,
 				  struct libattr_list *attrs)
 {
 	CK_RV ret;
 
 	switch (get_key_type(obj)) {
 	case CKK_EC:
-		ret = key_ec_public_create(obj, attrs);
+		ret = key_ec_public_create(hsession, obj, attrs);
 		break;
 
 	default:
@@ -997,19 +1015,22 @@ CK_RV key_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 		case CKO_PUBLIC_KEY:
 			ret = key_public_new(hsession, obj, attrs);
 			if (ret == CKR_OK)
-				ret = subkey_public_create(obj, attrs);
+				ret = subkey_public_create(hsession, obj,
+							   attrs);
 			break;
 
 		case CKO_PRIVATE_KEY:
 			ret = key_private_new(obj, attrs);
 			if (ret == CKR_OK)
-				ret = subkey_private_create(obj, attrs);
+				ret = subkey_private_create(hsession, obj,
+							    attrs);
 			break;
 
 		case CKO_SECRET_KEY:
 			ret = key_secret_new(obj, attrs);
 			if (ret == CKR_OK)
-				ret = subkey_secret_create(obj, attrs);
+				ret = subkey_secret_create(hsession, obj,
+							   attrs);
 			break;
 
 		default:

pkcs11/src/objects/key_cipher.h
@@ -16,12 +16,17 @@ void key_cipher_free(struct libobj_obj *obj);
 
 /*
  * key_cipher_create() - Creates a Cipher key object
- * @obj: Cipher Key
+ * @hsession: Session handle
+ * @obj: Cipher Key object
  * @attrs: List of object attributes
  *
  * If key attributes are corrects, create a new Cipher key object.
  *
  * return:
+ * CKR_CRYPTOKI_NOT_INITIALIZED  - Context not initialized
+ * CKR_GENERAL_ERROR             - No slot defined
+ * CKR_SESSION_HANDLE_INVALID    - Session Handle invalid
+ * CKR_SLOT_ID_INVALID           - Slot ID is not valid
  * CKR_ATTRIBUTE_VALUE_INVALID   - Attribute value is not valid
  * CKR_FUNCTION_FAILED           - Function failure
  * CKR_TEMPLATE_INCOMPLETE       - Attribute template incomplete
@@ -30,7 +35,8 @@ void key_cipher_free(struct libobj_obj *obj);
  * CKR_GENERAL_ERROR             - General error defined
  * CKR_OK                        - Success
  */
-CK_RV key_cipher_create(struct libobj_obj *obj, struct libattr_list *attrs);
+CK_RV key_cipher_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
+			struct libattr_list *attrs);
 
 /*
  * key_cipher_generate() - Generates a Cipher object

pkcs11/src/objects/key_ec.h
@@ -22,12 +22,17 @@ void key_ec_private_free(struct libobj_obj *obj);
 
 /*
  * key_ec_public_create() - Creates an EC public key object
+ * @hsession: Session handle
  * @obj: EC Public Key object
  * @attrs: List of object attributes
  *
  * If key attributes are corrects, create a new EC Public key object.
  *
  * return:
+ * CKR_CRYPTOKI_NOT_INITIALIZED  - Context not initialized
+ * CKR_GENERAL_ERROR             - No slot defined
+ * CKR_SESSION_HANDLE_INVALID    - Session Handle invalid
+ * CKR_SLOT_ID_INVALID           - Slot ID is not valid
  * CKR_CURVE_NOT_SUPPORTED       - Curve is not supported
  * CKR_ATTRIBUTE_VALUE_INVALID   - Attribute value is not valid
  * CKR_FUNCTION_FAILED           - Function failure
@@ -37,16 +42,22 @@ void key_ec_private_free(struct libobj_obj *obj);
  * CKR_GENERAL_ERROR             - General error defined
  * CKR_OK                        - Success
  */
-CK_RV key_ec_public_create(struct libobj_obj *obj, struct libattr_list *attrs);
+CK_RV key_ec_public_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
+			   struct libattr_list *attrs);
 
 /*
  * key_ec_private_create() - Creates an EC private key object
+ * @hsession: Session handle
  * @obj: EC Private Key object
  * @attrs: List of object attributes
  *
  * If key attributes are corrects, create a new EC Private key object.
  *
  * return:
+ * CKR_CRYPTOKI_NOT_INITIALIZED  - Context not initialized
+ * CKR_GENERAL_ERROR             - No slot defined
+ * CKR_SESSION_HANDLE_INVALID    - Session Handle invalid
+ * CKR_SLOT_ID_INVALID           - Slot ID is not valid
  * CKR_CURVE_NOT_SUPPORTED       - Curve is not supported
  * CKR_ATTRIBUTE_VALUE_INVALID   - Attribute value is not valid
  * CKR_FUNCTION_FAILED           - Function failure
@@ -56,7 +67,8 @@ CK_RV key_ec_public_create(struct libobj_obj *obj, struct libattr_list *attrs);
  * CKR_GENERAL_ERROR             - General error defined
  * CKR_OK                        - Success
  */
-CK_RV key_ec_private_create(struct libobj_obj *obj, struct libattr_list *attrs);
+CK_RV key_ec_private_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
+			    struct libattr_list *attrs);
 
 /*
  * key_ec_keypair_generate() - Generates an EC keypair object

commit 762d1184a51e3ce933aad30854eb6d6585054526

SSMW-749 pkcs11: Import key to subsystem only if CKA_TOKEN is true
Import only token keys to the subsystem when C_CreateObject() is called.

Fixes: 695a85270478 ("SSMW-130 pkcs11: add SMW's import key when key created")

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

pkcs11/src/objects/key_cipher.c
@@ -97,9 +97,11 @@ CK_RV key_cipher_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 	/* Set the Key object value length equal to the buffer value length */
 	new_key->value_len = new_key->value.number;
 
-	/* Import the secret key in the SMW library session's subsystem */
-	ret = libdev_import_key(hsession, obj);
-	DBG_TRACE("Cipher Key ID 0x%X", new_key->key_id);
+	/* Import only the token key to the subsystem */
+	if (is_token_obj(obj, storage)) {
+		ret = libdev_import_key(hsession, obj);
+		DBG_TRACE("Cipher Key ID 0x%X", new_key->key_id);
+	}
 
 end:
 	if (ret != CKR_OK)

pkcs11/src/objects/key_ec.c
@@ -184,8 +184,11 @@ CK_RV key_ec_public_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 	if (ret != CKR_OK)
 		goto end;
 
-	ret = libdev_import_key(hsession, obj);
-	DBG_TRACE("Public Key ID 0x%X", new_key->key_id);
+	/* Import only the token key to the subsystem */
+	if (is_token_obj(obj, storage)) {
+		ret = libdev_import_key(hsession, obj);
+		DBG_TRACE("Public Key ID 0x%X", new_key->key_id);
+	}
 
 end:
 	if (ret != CKR_OK)
@@ -287,8 +290,11 @@ CK_RV key_ec_private_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 	if (ret != CKR_OK)
 		goto end;
 
-	ret = libdev_import_key(hsession, obj);
-	DBG_TRACE("Private Key ID 0x%X", new_key->key_id);
+	/* Import only the token key to the subsystem */
+	if (is_token_obj(obj, storage)) {
+		ret = libdev_import_key(hsession, obj);
+		DBG_TRACE("Private Key ID 0x%X", new_key->key_id);
+	}
 
 end:
 	if (ret != CKR_OK)
nxp-imx/imx-smw

commit 62cd70c47b092142505b88272950406deffb640a

SSMW-510 subsystems: ele: Add support of hash multi-part
- Add support of hash multi-part operation with ELE subsystem

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/subsystems/ele/hash.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2022-2023 NXP
+ * Copyright 2022-2024 NXP
  */
 
 #include "smw_status.h"
@@ -13,35 +13,56 @@
 
 #include "common.h"
 
-static int hash(struct hdl *hdl, void *args)
+/**
+ * set_hash_context() - Initialize the operation context
+ * @op_context: Pointer to operation context arguments structure
+ * @op_args: Pointer to ELE Hash operation arguments
+ * @digest_length: Digest length
+ * This function initializes the members of operation context structure.
+ *
+ * Return:
+ * SMW_STATUS_OK            - Success
+ * SMW_STATUS_INVALID_PARAM - One of the parameters is invalid
+ */
+static int set_hash_context(struct smw_op_context *op_context,
+			    op_hash_one_go_args_t *op_args,
+			    uint32_t digest_length)
 {
-	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+	int status = SMW_STATUS_INVALID_PARAM;
 
-	hsm_err_t err;
-	op_hash_one_go_args_t op_args = { 0 };
+	struct hash_context *hash_ctx = NULL;
 
-	struct smw_crypto_hash_args *hash_args = args;
-	const struct ele_hash_algo *hash_algo = NULL;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	hash_algo = ele_get_hash_algo(hash_args->algo_id);
-	if (!hash_algo)
+	if (!op_context)
 		goto end;
 
-	op_args.svc_flags = HSM_HASH_FLAG_ONE_SHOT;
-	op_args.input = smw_crypto_get_hash_input_data(hash_args);
-	op_args.output = smw_crypto_get_hash_output_data(hash_args);
-	op_args.input_size = smw_crypto_get_hash_input_length(hash_args);
-	op_args.output_size = smw_crypto_get_hash_output_length(hash_args);
-	op_args.algo = hash_algo->ele_algo;
+	op_context->op_id = SMW_CRYPTO_OP_ID_HASH_MULTI_PART;
 
-	/* Get output length feature */
-	if (!op_args.output) {
-		smw_crypto_set_hash_output_length(hash_args, hash_algo->length);
-		status = SMW_STATUS_OK;
+	hash_ctx = SMW_UTILS_MALLOC(sizeof(*hash_ctx));
+	if (!hash_ctx) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		SMW_DBG_PRINTF(DEBUG,
+			       "Hash subsystem context allocation failure\n");
 		goto end;
 	}
+
+	hash_ctx->ele_ctx = op_args->ctx;
+	hash_ctx->ele_ctx_size = op_args->ctx_size;
+	hash_ctx->ele_algo = op_args->algo;
+	hash_ctx->digest_length = digest_length;
+
+	op_context->subsystem_context = hash_ctx;
+	op_context->op_state = CTX_OP_STATE_INIT;
+
+	status = SMW_STATUS_OK;
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int hash(struct hdl *hdl, op_hash_one_go_args_t *op_args)
+{
+	hsm_err_t err = HSM_NO_ERROR;
 
 	SMW_DBG_PRINTF(VERBOSE,
 		       "[%s (%d)] Call hsm_do_hash()\n"
@@ -53,20 +74,206 @@ static int hash(struct hdl *hdl, void *args)
 		       "      - size: %d\n"
 		       "    Output\n"
 		       "      - buffer: %p\n"
+		       "      - size: %d\n"
+		       "    Context\n"
+		       "      - buffer: %p\n"
 		       "      - size: %d\n",
-		       __func__, __LINE__, op_args.algo, op_args.svc_flags,
-		       op_args.input, op_args.input_size, op_args.output,
-		       op_args.output_size);
+		       __func__, __LINE__, op_args->algo, op_args->svc_flags,
+		       op_args->input, op_args->input_size, op_args->output,
+		       op_args->output_size, op_args->ctx, op_args->ctx_size);
 
-	err = hsm_do_hash(hdl->session, &op_args);
+	err = hsm_do_hash(hdl->session, op_args);
 	SMW_DBG_PRINTF(DEBUG, "hsm_do_hash returned %d\n", err);
 
-	status = ele_convert_err(err);
+	return ele_convert_err(err);
+}
 
-	/* Update Digest size */
+static int hash_one_shot(struct hdl *hdl, void *args)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	op_hash_one_go_args_t op_args = { 0 };
+	struct smw_crypto_hash_args *hash_args = args;
+	const struct ele_hash_algo *hash_algo = NULL;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	hash_algo = ele_get_hash_algo(hash_args->algo_id);
+	if (!hash_algo)
+		goto end;
+
+	op_args.input = smw_crypto_get_hash_input_data(hash_args);
+	op_args.output = smw_crypto_get_hash_output_data(hash_args);
+	op_args.input_size = smw_crypto_get_hash_input_length(hash_args);
+	op_args.output_size = smw_crypto_get_hash_output_length(hash_args);
+	op_args.algo = hash_algo->ele_algo;
+	op_args.svc_flags = HSM_HASH_FLAG_ONE_SHOT;
+
+	/* Get output length feature */
+	if (!op_args.output) {
+		smw_crypto_set_hash_output_length(hash_args, hash_algo->length);
+		status = SMW_STATUS_OK;
+		goto end;
+	}
+
+	status = hash(hdl, &op_args);
+
+	/* Update digest size */
 	smw_crypto_set_hash_output_length(hash_args, op_args.exp_output_size);
 
 end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int hash_init(struct hdl *hdl, struct smw_crypto_hash_args *hash_args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	op_hash_one_go_args_t op_args = { 0 };
+	const struct ele_hash_algo *hash_algo = NULL;
+	struct smw_op_context *op_context = NULL;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	op_context = smw_crypto_get_hash_op_context(hash_args);
+	if (!op_context)
+		goto end;
+
+	hash_algo = ele_get_hash_algo(hash_args->algo_id);
+	if (!hash_algo)
+		goto end;
+
+	op_args.algo = hash_algo->ele_algo;
+	op_args.svc_flags = HSM_HASH_FLAG_GET_CONTEXT;
+
+	status = hash(hdl, &op_args);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	op_args.ctx = SMW_UTILS_MALLOC(op_args.context_size);
+	if (!op_args.ctx) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		SMW_DBG_PRINTF(DEBUG, "ELE Hash context allocation failure\n");
+		goto end;
+	}
+
+	op_args.input = smw_crypto_get_hash_input_data(hash_args),
+	op_args.input_size = smw_crypto_get_hash_input_length(hash_args),
+	op_args.svc_flags = HSM_HASH_FLAG_INIT;
+	op_args.ctx_size = op_args.context_size;
+
+	status = hash(hdl, &op_args);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = set_hash_context(op_context, &op_args, hash_algo->length);
+
+end:
+	if (status != SMW_STATUS_OK && op_args.ctx)
+		SMW_UTILS_FREE(op_args.ctx);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int hash_update(struct hdl *hdl, struct smw_crypto_hash_args *hash_args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	op_hash_one_go_args_t op_args = { 0 };
+	struct hash_context *hash_ctx = NULL;
+	struct smw_op_context *op_context = NULL;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	op_context = smw_crypto_get_hash_op_context(hash_args);
+	if (!op_context || !op_context->subsystem_context)
+		goto end;
+
+	hash_ctx = op_context->subsystem_context;
+
+	op_args.ctx = hash_ctx->ele_ctx;
+	op_args.input = smw_crypto_get_hash_input_data(hash_args);
+	op_args.output = smw_crypto_get_hash_output_data(hash_args);
+	op_args.input_size = smw_crypto_get_hash_input_length(hash_args);
+	op_args.output_size = smw_crypto_get_hash_output_length(hash_args);
+	op_args.algo = hash_ctx->ele_algo;
+	op_args.svc_flags = HSM_HASH_FLAG_UPDATE;
+	op_args.ctx_size = hash_ctx->ele_ctx_size;
+
+	status = hash(hdl, &op_args);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int hash_final(struct hdl *hdl, struct smw_crypto_hash_args *hash_args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	op_hash_one_go_args_t op_args = { 0 };
+	struct hash_context *hash_ctx = NULL;
+	struct smw_op_context *op_context = NULL;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	op_context = smw_crypto_get_hash_op_context(hash_args);
+	if (!op_context || !op_context->subsystem_context)
+		goto end;
+
+	hash_ctx = op_context->subsystem_context;
+
+	op_args.ctx = hash_ctx->ele_ctx;
+	op_args.input = smw_crypto_get_hash_input_data(hash_args);
+	op_args.output = smw_crypto_get_hash_output_data(hash_args);
+	op_args.input_size = smw_crypto_get_hash_input_length(hash_args);
+	op_args.output_size = smw_crypto_get_hash_output_length(hash_args);
+	op_args.algo = hash_ctx->ele_algo;
+	op_args.svc_flags = HSM_HASH_FLAG_FINAL;
+	op_args.ctx_size = hash_ctx->ele_ctx_size;
+
+	/* Get output length feature */
+	if (!op_args.output) {
+		smw_crypto_set_hash_output_length(hash_args,
+						  hash_ctx->digest_length);
+		status = SMW_STATUS_OK;
+		goto end;
+	}
+
+	status = hash(hdl, &op_args);
+
+	/* Update digest size */
+	smw_crypto_set_hash_output_length(hash_args, op_args.exp_output_size);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int hash_multi_part(struct hdl *hdl, void *args)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+	struct smw_crypto_hash_args *hash_args = args;
+
+	switch (hash_args->op_step) {
+	case SMW_OP_STEP_INIT:
+		status = hash_init(hdl, hash_args);
+		break;
+
+	case SMW_OP_STEP_UPDATE:
+		status = hash_update(hdl, hash_args);
+		break;
+
+	case SMW_OP_STEP_FINAL:
+		status = hash_final(hdl, hash_args);
+		break;
+
+	default:
+		break;
+	}
+
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }
@@ -76,7 +283,10 @@ bool ele_hash_handle(struct hdl *hdl, enum operation_id operation_id,
 {
 	switch (operation_id) {
 	case OPERATION_ID_HASH:
-		*status = hash(hdl, args);
+		*status = hash_one_shot(hdl, args);
+		break;
+	case OPERATION_ID_HASH_MULTI_PART:
+		*status = hash_multi_part(hdl, args);
 		break;
 	default:
 		return false;

core/subsystems/ele/CMakeLists.txt
@@ -18,7 +18,7 @@ message(STATUS "    ELE  Headers  : ${ELE_INCLUDE_DIRS}")
 add_library(ele OBJECT "")
 set_target_properties(ele PROPERTIES C_VISIBILITY_PRESET hidden)
 
-if (ENABLE_CIPHER OR ENABLE_AEAD)
+if (ENABLE_CIPHER OR ENABLE_AEAD OR ENABLE_HASH)
     set(ENABLE_CONTEXT ON)
 endif()
 

core/subsystems/ele/common.c
@@ -87,3 +87,12 @@ __weak int ele_get_device_info(struct subsystem_context *ele_ctx)
 
 	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
 }
+
+void ele_free_hash_context(struct hash_context *ctx)
+{
+	if (ctx->ele_ctx) {
+		SMW_UTILS_FREE(ctx->ele_ctx);
+		ctx->ele_ctx = NULL;
+		ctx->ele_ctx_size = 0;
+	}
+}

core/subsystems/ele/common.h
@@ -18,6 +18,20 @@
 #define ELE_UID_SIZE	(ELE_NB_UID_WORD * sizeof(uint32_t))
 
 /**
+ * struct hash_context - Hash context
+ * @ele_algo: ELE Hash algorithm
+ * @ele_ctx: ELE operation context
+ * @ele_ctx_size: ELE operation context size
+ * @digest_length: Digest length
+ */
+struct hash_context {
+	hsm_hash_algo_t ele_algo;
+	uint8_t *ele_ctx;
+	uint16_t ele_ctx_size;
+	uint32_t digest_length;
+};
+
+/**
  * struct hdl - ELE handles
  * @session: Session handle
  * @key_store: Key store service flow handle
@@ -76,7 +90,7 @@ struct ele_hash_algo {
 };
 
 /**
- * get_hash_algo() - Get the ELE hash algorithm information
+ * ele_get_hash_algo() - Get the ELE hash algorithm information
  * @algo_id: SMW Hash algorithm id.
  *
  * Return:
@@ -452,5 +466,14 @@ int open_key_mgmt_service(struct hdl *hdl, hsm_hdl_t *key_management_hdl);
  * SMW_STATUS_SUBSYSTEM_FAILURE    - Subsystem failure
  */
 int close_key_mgt_service(hsm_hdl_t key_management_hdl);
+
+/**
+ * ele_free_hash_context() - Free the ELE Hash context
+ * @ctx: Hash context
+ *
+ * Return:
+ * None.
+ */
+void ele_free_hash_context(struct hash_context *ctx);
 
 #endif /* __COMMON_H__ */

core/subsystems/ele/operation_context.c
@@ -3,6 +3,8 @@
  * Copyright 2024 NXP
  */
 
+#include "utils.h"
+#include "common.h"
 #include "operation_context.h"
 
 static int cancel_operation(struct smw_op_context *ctx)
@@ -19,9 +21,24 @@ static int copy_context(struct smw_op_context *src_ctx,
 	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
 }
 
-static void free_context(struct smw_op_context **args)
+static void free_context(struct smw_op_context *ctx)
 {
-	(void)args;
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!ctx->subsystem_context)
+		return;
+
+	switch (ctx->op_id) {
+	case SMW_CRYPTO_OP_ID_HASH_MULTI_PART:
+		ele_free_hash_context(ctx->subsystem_context);
+		break;
+
+	default:
+		break;
+	}
+
+	SMW_UTILS_FREE(ctx->subsystem_context);
+	ctx->subsystem_context = NULL;
 }
 
 /* ELE context operations structure */

commit 41cb972482770fe1fa27f1fbb9b2e63387dd98ac

SSMW-657 subsystems: ele: Store subsystem ID in hash multi-part context
In case of multi-part operation, the subsystem ID must be stored
in the multi-part context at initialisation.
Next steps of the multi-part operation must use the same subsystem.

Fixes: 62cd70c47b09 ("SSMW-510 subsystems: ele: Add support of hash multi-part")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/subsystems/ele/hash.c
@@ -140,6 +140,8 @@ static int hash_init(struct hdl *hdl, struct smw_crypto_hash_args *hash_args)
 	if (!op_context)
 		goto end;
 
+	smw_crypto_set_ctx_subsystem_id(op_context, SUBSYSTEM_ID_ELE);
+
 	hash_algo = ele_get_hash_algo(hash_args->algo_id);
 	if (!hash_algo)
 		goto end;
nxp-imx/imx-smw

commit a27dc30063238b22161414fd1c02280530e267d2

SSMW-115 pkcs11: Add support to messsage-based Sign/Verify
Implement:
- C_MessageSignInit()
- C_SignMessage()
- C_MessageVerifyInit()
- C_VerifyMessage()

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

pkcs11/src/objects/key_hmac.c
@@ -1,0 +1,196 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2024 NXP
+ */
+
+#include <stdlib.h>
+
+#include "attributes.h"
+#include "key_hmac.h"
+
+#include "lib_device.h"
+#include "libobj_types.h"
+#include "util.h"
+
+#include "trace.h"
+
+enum attr_key_hmac_list {
+	SEC_VALUE = 0,
+	SEC_VALUE_LEN,
+};
+
+const struct template_attr attr_key_hmac[] = {
+	[SEC_VALUE] = TATTR_P(key_hmac, value, VALUE, 0, MUST, byte_array),
+	[SEC_VALUE_LEN] = TATTR(key_hmac, value_len, VALUE_LEN,
+				sizeof(CK_ULONG), MUST_NOT, ulong),
+};
+
+/**
+ * key_hmac_allocate() - Allocate and initialize HMAC secret key
+ * @obj: HMAC key object
+ *
+ * return:
+ * CKR_OK             - Success
+ * CKR_HOST_MEMORY    - Out of memory
+ */
+static CK_RV key_hmac_allocate(struct libobj_obj *obj)
+{
+	CK_RV ret = CKR_HOST_MEMORY;
+	struct libobj_key_hmac *key = NULL;
+
+	key = calloc(1, sizeof(*key));
+	if (key) {
+		set_subkey_to(obj, key);
+		ret = CKR_OK;
+	}
+
+	DBG_TRACE("Allocated a new HMAC secret key (%p) (ret=%ld)", key, ret);
+
+	return ret;
+}
+
+void key_hmac_free(struct libobj_obj *obj)
+{
+	struct libobj_key_hmac *key = get_subkey_from(obj);
+
+	if (!key)
+		return;
+
+	(void)libdev_delete_key(key->key_id);
+
+	if (key->value.array)
+		free(key->value.array);
+
+	free(key);
+
+	set_subkey_to(obj, NULL);
+}
+
+CK_RV key_hmac_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
+		      struct libattr_list *attrs)
+{
+	CK_RV ret = CKR_OK;
+	struct libobj_key_hmac *new_key = NULL;
+
+	ret = key_hmac_allocate(obj);
+	if (ret != CKR_OK)
+		goto end;
+
+	new_key = get_subkey_from(obj);
+
+	DBG_TRACE("Create a new HMAC secret key (%p)", new_key);
+
+	ret = attr_get_value(new_key, &attr_key_hmac[SEC_VALUE], attrs,
+			     NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	/* Verify the key size value is not defined */
+	ret = attr_get_value(new_key, &attr_key_hmac[SEC_VALUE_LEN], attrs,
+			     NO_OVERWRITE);
+	if (ret != CKR_OK)
+		goto end;
+
+	/* Set the Key object value length equal to the buffer value length */
+	new_key->value_len = new_key->value.number;
+
+	/* Import the secret key in the SMW library session's subsystem */
+	ret = libdev_import_key(hsession, obj);
+	DBG_TRACE("HMAC Key ID 0x%X", new_key->key_id);
+
+end:
+	if (ret != CKR_OK)
+		key_hmac_free(obj);
+
+	return ret;
+}
+
+CK_RV key_hmac_get_attribute(CK_ATTRIBUTE_PTR attr,
+			     const struct libobj_obj *obj, bool protect)
+{
+	CK_RV ret = CKR_OK;
+
+	DBG_TRACE("Get attribute type=%#lx protected=%s", attr->type,
+		  protect ? "YES" : "NO");
+
+	ret = attr_get_obj_prot_value(attr, attr_key_hmac,
+				      ARRAY_SIZE(attr_key_hmac),
+				      get_subkey_from(obj), protect);
+	if (ret == CKR_ATTRIBUTE_TYPE_INVALID)
+		attr->ulValueLen = CK_UNAVAILABLE_INFORMATION;
+
+	DBG_TRACE("Get attribute type=%#lx ret %ld", attr->type, ret);
+	return ret;
+}
+
+CK_RV key_hmac_modify_attribute(CK_ATTRIBUTE_PTR attr, struct libobj_obj *obj)
+{
+	CK_RV ret = CKR_OK;
+
+	DBG_TRACE("Modify attribute type=%#lx", attr->type);
+
+	ret = attr_modify_obj_value(attr, attr_key_hmac,
+				    ARRAY_SIZE(attr_key_hmac),
+				    get_subkey_from(obj));
+
+	DBG_TRACE("Modify attribute type=%#lx ret %ld", attr->type, ret);
+	return ret;
+}
+
+CK_RV key_hmac_generate(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
+			struct libobj_obj *obj, struct libattr_list *attrs)
+{
+	CK_RV ret = CKR_OK;
+	struct libobj_key_hmac *new_key = NULL;
+
+	ret = key_hmac_allocate(obj);
+	if (ret != CKR_OK)
+		goto end;
+
+	new_key = get_subkey_from(obj);
+
+	DBG_TRACE("Generate a HMAC key (%p)", new_key);
+
+	/* Verify the key attributes */
+	ret = attr_get_value(new_key, &attr_key_hmac[SEC_VALUE], attrs,
+			     MUST_NOT);
+	if (ret != CKR_OK)
+		goto end;
+
+	ret = attr_get_value(new_key, &attr_key_hmac[SEC_VALUE_LEN], attrs,
+			     MUST);
+	if (ret != CKR_OK)
+		goto end;
+
+	/* Generate the secret key with SMW library */
+	ret = libdev_operate_mechanism(hsession, mech, obj);
+	DBG_TRACE("HMAC Key ID 0x%X", new_key->key_id);
+
+end:
+	if (ret != CKR_OK)
+		key_hmac_free(obj);
+
+	return ret;
+}
+
+CK_RV key_hmac_get_id(struct libbytes *id, struct libobj_obj *obj,
+		      size_t prefix_len)
+{
+	struct libobj_key_hmac *key_hmac = NULL;
+
+	if (!obj || !id)
+		return CKR_GENERAL_ERROR;
+
+	key_hmac = get_subkey_from(obj);
+
+	id->number = prefix_len + sizeof(key_hmac->key_id);
+	id->array = malloc(id->number);
+	if (!id->array)
+		return CKR_HOST_MEMORY;
+
+	DBG_TRACE("HMAC Key ID 0x%X", key_hmac->key_id);
+
+	TO_CK_BYTES(&id->array[prefix_len], key_hmac->key_id);
+
+	return CKR_OK;
+}

pkcs11/src/ifsmw/args_attr.c
@@ -262,6 +262,16 @@ CK_RV args_attrs_key_policy(struct smw_tlv *attr, struct libobj_obj *obj,
 	case CKK_AES:
 	case CKK_DES:
 	case CKK_DES3:
+	case CKK_MD5_HMAC:
+	case CKK_SHA_1_HMAC:
+	case CKK_SHA224_HMAC:
+	case CKK_SHA256_HMAC:
+	case CKK_SHA384_HMAC:
+	case CKK_SHA512_HMAC:
+	case CKK_SHA3_224_HMAC:
+	case CKK_SHA3_256_HMAC:
+	case CKK_SHA3_384_HMAC:
+	case CKK_SHA3_512_HMAC:
 		ret = set_secret_key_usage(&policy, obj, allowed_algos);
 		break;
 

pkcs11/src/ifsmw/key_desc.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021, 2023 NXP
+ * Copyright 2021, 2023-2024 NXP
  */
 
 #include "asn1_ec_curve.h"
@@ -10,9 +10,9 @@
 
 #include "key_desc.h"
 
-#define EC_ASN1_CURVE(name)                                                    \
+#define EC_ASN1_CURVE(_name)                                                   \
 	{                                                                      \
-#name, name                                                    \
+		.name = #_name, .oid = _name                                   \
 	}
 
 const CK_BYTE prime192v1[] = ASN1_OID_PRIME192;
@@ -32,25 +32,23 @@ const CK_BYTE brainpoolP384t1[] = ASN1_OID_BRAINPOOL_P384T1;
 const CK_BYTE brainpoolP512r1[] = ASN1_OID_BRAINPOOL_P512R1;
 const CK_BYTE brainpoolP512t1[] = ASN1_OID_BRAINPOOL_P512T1;
 
-const struct asn1_curve_def ec_asn1_curves[] = {
-	EC_ASN1_CURVE(prime192v1),
-	EC_ASN1_CURVE(prime256v1),
-	EC_ASN1_CURVE(brainpoolP160r1),
-	EC_ASN1_CURVE(brainpoolP160t1),
-	EC_ASN1_CURVE(brainpoolP192r1),
-	EC_ASN1_CURVE(brainpoolP192t1),
-	EC_ASN1_CURVE(brainpoolP224r1),
-	EC_ASN1_CURVE(brainpoolP224t1),
-	EC_ASN1_CURVE(brainpoolP256r1),
-	EC_ASN1_CURVE(brainpoolP256t1),
-	EC_ASN1_CURVE(brainpoolP320r1),
-	EC_ASN1_CURVE(brainpoolP320t1),
-	EC_ASN1_CURVE(brainpoolP384r1),
-	EC_ASN1_CURVE(brainpoolP384t1),
-	EC_ASN1_CURVE(brainpoolP512r1),
-	EC_ASN1_CURVE(brainpoolP512t1),
-	{ 0 },
-};
+const struct asn1_curve_def ec_asn1_curves[] = { EC_ASN1_CURVE(prime192v1),
+						 EC_ASN1_CURVE(prime256v1),
+						 EC_ASN1_CURVE(brainpoolP160r1),
+						 EC_ASN1_CURVE(brainpoolP160t1),
+						 EC_ASN1_CURVE(brainpoolP192r1),
+						 EC_ASN1_CURVE(brainpoolP192t1),
+						 EC_ASN1_CURVE(brainpoolP224r1),
+						 EC_ASN1_CURVE(brainpoolP224t1),
+						 EC_ASN1_CURVE(brainpoolP256r1),
+						 EC_ASN1_CURVE(brainpoolP256t1),
+						 EC_ASN1_CURVE(brainpoolP320r1),
+						 EC_ASN1_CURVE(brainpoolP320t1),
+						 EC_ASN1_CURVE(brainpoolP384r1),
+						 EC_ASN1_CURVE(brainpoolP384t1),
+						 EC_ASN1_CURVE(brainpoolP512r1),
+						 EC_ASN1_CURVE(brainpoolP512t1),
+						 { 0 } };
 
 /**
  * struct dev_curve_def - Definition of SMW curve
@@ -64,48 +62,57 @@ struct dev_curve_def {
 	const unsigned int security_size;
 };
 
-const struct dev_curve_def ec_smw_curves[] = {
-	{ "NIST", 192 },
-	{ "NIST", 256 },
-	{ "BRAINPOOL_R1", 160 },
-	{ "BRAINPOOL_T1", 160 },
-	{ "BRAINPOOL_R1", 192 },
-	{ "BRAINPOOL_T1", 192 },
-	{ "BRAINPOOL_R1", 224 },
-	{ "BRAINPOOL_T1", 224 },
-	{ "BRAINPOOL_R1", 256 },
-	{ "BRAINPOOL_T1", 256 },
-	{ "BRAINPOOL_R1", 320 },
-	{ "BRAINPOOL_T1", 320 },
-	{ "BRAINPOOL_R1", 384 },
-	{ "BRAINPOOL_T1", 384 },
-	{ "BRAINPOOL_R1", 512 },
-	{ "BRAINPOOL_T1", 512 },
-	{ 0 },
-};
+#define EC_SMW_CURVE(_name, _size)                                             \
+	{                                                                      \
+		.name = #_name, .security_size = _size                         \
+	}
+
+const struct dev_curve_def ec_smw_curves[] = { EC_SMW_CURVE(NIST, 192),
+					       EC_SMW_CURVE(NIST, 256),
+					       EC_SMW_CURVE(BRAINPOOL_R1, 160),
+					       EC_SMW_CURVE(BRAINPOOL_T1, 160),
+					       EC_SMW_CURVE(BRAINPOOL_R1, 192),
+					       EC_SMW_CURVE(BRAINPOOL_T1, 192),
+					       EC_SMW_CURVE(BRAINPOOL_R1, 224),
+					       EC_SMW_CURVE(BRAINPOOL_T1, 224),
+					       EC_SMW_CURVE(BRAINPOOL_R1, 256),
+					       EC_SMW_CURVE(BRAINPOOL_T1, 256),
+					       EC_SMW_CURVE(BRAINPOOL_R1, 320),
+					       EC_SMW_CURVE(BRAINPOOL_T1, 320),
+					       EC_SMW_CURVE(BRAINPOOL_R1, 384),
+					       EC_SMW_CURVE(BRAINPOOL_T1, 384),
+					       EC_SMW_CURVE(BRAINPOOL_R1, 512),
+					       EC_SMW_CURVE(BRAINPOOL_T1, 512),
+					       { 0 } };
 
 /*
  * Definition of the ASN1 EC Curves supported (function )
  */
+
+#define EC_CURVE(_asn1, _smw)                                                  \
+	{                                                                      \
+		.asn1 = _asn1, .dev = _smw                                     \
+	}
+
 const struct curve_def ec_curves[] = {
-	{ &ec_asn1_curves[0], &ec_smw_curves[0] },
-	{ &ec_asn1_curves[1], &ec_smw_curves[1] },
-	{ &ec_asn1_curves[2], &ec_smw_curves[2] },
-	{ &ec_asn1_curves[3], &ec_smw_curves[3] },
-	{ &ec_asn1_curves[4], &ec_smw_curves[4] },
-	{ &ec_asn1_curves[5], &ec_smw_curves[5] },
-	{ &ec_asn1_curves[6], &ec_smw_curves[6] },
-	{ &ec_asn1_curves[7], &ec_smw_curves[7] },
-	{ &ec_asn1_curves[8], &ec_smw_curves[8] },
-	{ &ec_asn1_curves[9], &ec_smw_curves[9] },
-	{ &ec_asn1_curves[10], &ec_smw_curves[10] },
-	{ &ec_asn1_curves[11], &ec_smw_curves[11] },
-	{ &ec_asn1_curves[12], &ec_smw_curves[12] },
-	{ &ec_asn1_curves[13], &ec_smw_curves[13] },
-	{ &ec_asn1_curves[14], &ec_smw_curves[14] },
-	{ &ec_asn1_curves[15], &ec_smw_curves[15] },
-	{ &ec_asn1_curves[16], &ec_smw_curves[16] },
-	{ 0 },
+	EC_CURVE(&ec_asn1_curves[0], &ec_smw_curves[0]),
+	EC_CURVE(&ec_asn1_curves[1], &ec_smw_curves[1]),
+	EC_CURVE(&ec_asn1_curves[2], &ec_smw_curves[2]),
+	EC_CURVE(&ec_asn1_curves[3], &ec_smw_curves[3]),
+	EC_CURVE(&ec_asn1_curves[4], &ec_smw_curves[4]),
+	EC_CURVE(&ec_asn1_curves[5], &ec_smw_curves[5]),
+	EC_CURVE(&ec_asn1_curves[6], &ec_smw_curves[6]),
+	EC_CURVE(&ec_asn1_curves[7], &ec_smw_curves[7]),
+	EC_CURVE(&ec_asn1_curves[8], &ec_smw_curves[8]),
+	EC_CURVE(&ec_asn1_curves[9], &ec_smw_curves[9]),
+	EC_CURVE(&ec_asn1_curves[10], &ec_smw_curves[10]),
+	EC_CURVE(&ec_asn1_curves[11], &ec_smw_curves[11]),
+	EC_CURVE(&ec_asn1_curves[12], &ec_smw_curves[12]),
+	EC_CURVE(&ec_asn1_curves[13], &ec_smw_curves[13]),
+	EC_CURVE(&ec_asn1_curves[14], &ec_smw_curves[14]),
+	EC_CURVE(&ec_asn1_curves[15], &ec_smw_curves[15]),
+	EC_CURVE(&ec_asn1_curves[16], &ec_smw_curves[16]),
+	{ 0 }
 };
 
 struct cipher_def {
@@ -113,12 +120,25 @@ struct cipher_def {
 	const char *smw_name;
 };
 
-const struct cipher_def ciphers[] = {
-	{ CKK_AES, "AES" },
-	{ CKK_DES, "DES" },
-	{ CKK_DES3, "DES3" },
-	{ 0 },
-};
+#define CIPHERS(_type, _name)                                                  \
+	{                                                                      \
+		.ck_key_type = _type, .smw_name = #_name                       \
+	}
+
+const struct cipher_def ciphers[] = { CIPHERS(CKK_AES, AES),
+				      CIPHERS(CKK_DES, DES),
+				      CIPHERS(CKK_DES3, DES3),
+				      CIPHERS(CKK_MD5_HMAC, HMAC_MD5),
+				      CIPHERS(CKK_SHA_1_HMAC, HMAC_SHA1),
+				      CIPHERS(CKK_SHA224_HMAC, HMAC_SHA224),
+				      CIPHERS(CKK_SHA256_HMAC, HMAC_SHA256),
+				      CIPHERS(CKK_SHA384_HMAC, HMAC_SHA384),
+				      CIPHERS(CKK_SHA512_HMAC, HMAC_SHA512),
+				      CIPHERS(CKK_SHA3_224_HMAC, HMAC_SHA3_224),
+				      CIPHERS(CKK_SHA3_256_HMAC, HMAC_SHA3_256),
+				      CIPHERS(CKK_SHA3_384_HMAC, HMAC_SHA3_384),
+				      CIPHERS(CKK_SHA3_512_HMAC, HMAC_SHA3_512),
+				      { 0 } };
 
 static CK_RV ec_key_desc(struct smw_key_descriptor *desc,
 			 struct libobj_obj *obj)
@@ -183,6 +203,20 @@ static CK_RV cipher_key_desc(struct smw_key_descriptor *desc,
 		if (key_length != 16 && key_length != 24 && key_length != 32)
 			return CKR_ATTRIBUTE_VALUE_INVALID;
 		desc->security_size = key_length * 8;
+		break;
+
+	case CKK_MD5_HMAC:
+	case CKK_SHA_1_HMAC:
+	case CKK_SHA224_HMAC:
+	case CKK_SHA256_HMAC:
+	case CKK_SHA384_HMAC:
+	case CKK_SHA512_HMAC:
+	case CKK_SHA3_224_HMAC:
+	case CKK_SHA3_256_HMAC:
+	case CKK_SHA3_384_HMAC:
+	case CKK_SHA3_512_HMAC:
+		if (MUL_OVERFLOW(key_length, 8, &desc->security_size))
+			return CKR_ARGUMENTS_BAD;
 		break;
 
 	default:
@@ -269,6 +303,16 @@ CK_RV key_desc_setup(struct smw_key_descriptor *desc, struct libobj_obj *obj)
 	case CKK_AES:
 	case CKK_DES:
 	case CKK_DES3:
+	case CKK_MD5_HMAC:
+	case CKK_SHA_1_HMAC:
+	case CKK_SHA224_HMAC:
+	case CKK_SHA256_HMAC:
+	case CKK_SHA384_HMAC:
+	case CKK_SHA512_HMAC:
+	case CKK_SHA3_224_HMAC:
+	case CKK_SHA3_256_HMAC:
+	case CKK_SHA3_384_HMAC:
+	case CKK_SHA3_512_HMAC:
 		ret = cipher_key_desc(desc, obj);
 		break;
 
@@ -293,7 +337,17 @@ void key_desc_copy_key_id(struct libobj_obj *obj,
 	switch (get_key_type(obj)) {
 	case CKK_AES:
 	case CKK_DES:
-	case CKK_DES3: {
+	case CKK_DES3:
+	case CKK_MD5_HMAC:
+	case CKK_SHA_1_HMAC:
+	case CKK_SHA224_HMAC:
+	case CKK_SHA256_HMAC:
+	case CKK_SHA384_HMAC:
+	case CKK_SHA512_HMAC:
+	case CKK_SHA3_224_HMAC:
+	case CKK_SHA3_256_HMAC:
+	case CKK_SHA3_384_HMAC:
+	case CKK_SHA3_512_HMAC: {
 		struct libobj_key_cipher *key = get_subkey_from(obj);
 
 		key->key_id = desc->id;

pkcs11/src/ifsmw/mechanism.c
@@ -37,6 +37,7 @@
 #define AES_STR	 "AES"
 #define DES_STR	 "DES"
 #define DES3_STR "DES3"
+#define HMAC_STR "HMAC"
 
 struct mgroup;
 struct mentry;
@@ -95,6 +96,21 @@ static CK_RV info_mcipher_des3(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
 			       CK_MECHANISM_INFO_PTR info);
 static CK_RV op_mcipher_des3(CK_SLOT_ID slotid, struct mentry *entry,
 			     void *args);
+static void check_mcmac_aes(CK_SLOT_ID slotid, const char *subsystem,
+			    struct mgroup *mgroup);
+static CK_RV info_mcmac_aes(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			    struct mentry *entry, CK_MECHANISM_INFO_PTR info);
+static CK_RV op_mcmac_aes(CK_SLOT_ID slotid, struct mentry *entry, void *args);
+static void check_mcmac_des3(CK_SLOT_ID slotid, const char *subsystem,
+			     struct mgroup *mgroup);
+static CK_RV info_mcmac_des3(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			     struct mentry *entry, CK_MECHANISM_INFO_PTR info);
+static CK_RV op_mcmac_des3(CK_SLOT_ID slotid, struct mentry *entry, void *args);
+static void check_mhmac(CK_SLOT_ID slotid, const char *subsystem,
+			struct mgroup *mgroup);
+static CK_RV info_mhmac(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			struct mentry *entry, CK_MECHANISM_INFO_PTR info);
+static CK_RV op_mhmac(CK_SLOT_ID slotid, struct mentry *entry, void *args);
 
 const char *smw_ec_name[] = { "NIST", "BRAINPOOL_R1", "BRAINPOOL_T1" };
 
@@ -165,9 +181,11 @@ struct mgroup {
  * Digest mechanisms
  */
 static struct mentry mdigest[] = {
-	M_ALGO_NO_HASH(SHA1, SHA_1),	M_ALGO_NO_HASH(SHA224, SHA224),
-	M_ALGO_NO_HASH(SHA256, SHA256), M_ALGO_NO_HASH(SHA384, SHA384),
-	M_ALGO_NO_HASH(SHA512, SHA512),
+	M_ALGO_NO_HASH(MD5, MD5),	    M_ALGO_NO_HASH(SHA1, SHA_1),
+	M_ALGO_NO_HASH(SHA224, SHA224),	    M_ALGO_NO_HASH(SHA256, SHA256),
+	M_ALGO_NO_HASH(SHA384, SHA384),	    M_ALGO_NO_HASH(SHA512, SHA512),
+	M_ALGO_NO_HASH(SHA3_224, SHA3_224), M_ALGO_NO_HASH(SHA3_256, SHA3_256),
+	M_ALGO_NO_HASH(SHA3_384, SHA3_384), M_ALGO_NO_HASH(SHA3_512, SHA3_512),
 };
 
 /*
@@ -179,12 +197,13 @@ static struct mentry meckeygen[] = {
 
 /*
  * Key Generate mechanism
- * Cipher and RSA keys
+ * Cipher, HMAC and RSA keys
  */
 static struct mentry mkeygen[] = {
 	M_ALGO_NO_HASH(AES, AES_KEY_GEN),
 	M_ALGO_NO_HASH(DES, DES_KEY_GEN),
 	M_ALGO_NO_HASH(DES3, DES3_KEY_GEN),
+	M_ALGO_NO_HASH(HMAC, GENERIC_SECRET_KEY_GEN),
 	M_ALGO_NO_HASH(RSA, RSA_PKCS_KEY_PAIR_GEN),
 };
 
@@ -222,19 +241,60 @@ static struct mentry msign_rsa_pss[] = {
  * Cipher mechanisms
  */
 static struct mentry mcipher_aes[] = {
-	M_ALGO_NO_HASH(AES, AES_CBC), M_ALGO_NO_HASH(AES, AES_CTR),
-	M_ALGO_NO_HASH(AES, AES_CTS), M_ALGO_NO_HASH(AES, AES_ECB),
-	M_ALGO_NO_HASH(AES, AES_XTS),
+	M_ALGO_NO_HASH(CBC_NO_PADDING, AES_CBC),
+	M_ALGO_NO_HASH(CTR, AES_CTR),
+	M_ALGO_NO_HASH(CTS, AES_CTS),
+	M_ALGO_NO_HASH(ECB_NO_PADDING, AES_ECB),
+	M_ALGO_NO_HASH(XTS, AES_XTS),
 };
 
 static struct mentry mcipher_des[] = {
-	M_ALGO_NO_HASH(DES, DES_CBC),
-	M_ALGO_NO_HASH(DES, DES_ECB),
+	M_ALGO_NO_HASH(CBC_NO_PADDING, DES_CBC),
+	M_ALGO_NO_HASH(ECB_NO_PADDING, DES_ECB),
 };
 
 static struct mentry mcipher_des3[] = {
-	M_ALGO_NO_HASH(DES3, DES3_CBC),
-	M_ALGO_NO_HASH(DES3, DES3_ECB),
+	M_ALGO_NO_HASH(CBC_NO_PADDING, DES3_CBC),
+	M_ALGO_NO_HASH(ECB_NO_PADDING, DES3_ECB),
+};
+
+/*
+ * CMAC mechanisms
+ */
+static struct mentry mcmac_aes[] = {
+	M_ALGO_NO_HASH(CMAC, AES_CMAC),
+	M_ALGO_NO_HASH(CMAC, AES_CMAC_GENERAL),
+};
+
+static struct mentry mcmac_des3[] = {
+	M_ALGO_NO_HASH(CMAC, DES3_CMAC),
+	M_ALGO_NO_HASH(CMAC, DES3_CMAC_GENERAL),
+};
+
+/*
+ * HMAC mechanisms
+ */
+static struct mentry mhmac[] = {
+	M_ALGO_HASH(HMAC, MD5, MD5_HMAC),
+	M_ALGO_HASH(HMAC, MD5, MD5_HMAC_GENERAL),
+	M_ALGO_HASH(HMAC, SHA1, SHA_1_HMAC),
+	M_ALGO_HASH(HMAC, SHA1, SHA_1_HMAC_GENERAL),
+	M_ALGO_HASH(HMAC, SHA224, SHA224_HMAC),
+	M_ALGO_HASH(HMAC, SHA224, SHA224_HMAC_GENERAL),
+	M_ALGO_HASH(HMAC, SHA256, SHA256_HMAC),
+	M_ALGO_HASH(HMAC, SHA256, SHA256_HMAC_GENERAL),
+	M_ALGO_HASH(HMAC, SHA384, SHA384_HMAC),
+	M_ALGO_HASH(HMAC, SHA384, SHA384_HMAC_GENERAL),
+	M_ALGO_HASH(HMAC, SHA512, SHA512_HMAC),
+	M_ALGO_HASH(HMAC, SHA512, SHA512_HMAC_GENERAL),
+	M_ALGO_HASH(HMAC, SHA3_224, SHA3_224_HMAC),
+	M_ALGO_HASH(HMAC, SHA3_224, SHA3_224_HMAC_GENERAL),
+	M_ALGO_HASH(HMAC, SHA3_256, SHA3_256_HMAC),
+	M_ALGO_HASH(HMAC, SHA3_256, SHA3_256_HMAC_GENERAL),
+	M_ALGO_HASH(HMAC, SHA3_384, SHA3_384_HMAC),
+	M_ALGO_HASH(HMAC, SHA3_384, SHA3_384_HMAC_GENERAL),
+	M_ALGO_HASH(HMAC, SHA3_512, SHA3_512_HMAC),
+	M_ALGO_HASH(HMAC, SHA3_512, SHA3_512_HMAC_GENERAL),
 };
 
 /*
@@ -250,6 +310,9 @@ static struct mgroup smw_mechanims[] = {
 	M_GROUP(ARRAY_SIZE(mcipher_aes), mcipher_aes),
 	M_GROUP(ARRAY_SIZE(mcipher_des), mcipher_des),
 	M_GROUP(ARRAY_SIZE(mcipher_des3), mcipher_des3),
+	M_GROUP(ARRAY_SIZE(mcmac_aes), mcmac_aes),
+	M_GROUP(ARRAY_SIZE(mcmac_des3), mcmac_des3),
+	M_GROUP(ARRAY_SIZE(mhmac), mhmac),
 	{ 0 }
 };
 
@@ -267,6 +330,12 @@ static struct mgroup smw_mechanims[] = {
 		.cipher_mode = #_cipher_mode_                                  \
 	}
 
+#define CMAC_ALGO(_key_type_, _cmac_algo_, _smw_algo_)                         \
+	{                                                                      \
+		.mech_type = CKM_##_key_type_##_##_cmac_algo_,                 \
+		.smw_algo = #_smw_algo_                                        \
+	}
+
 /**
  * struct cipher_algo_info - Information about cipher algorithm
  * @mech_type: Cipher mechanism
@@ -281,6 +350,23 @@ static struct cipher_algo_info cipher_algos[] = {
 	CIPHER_ALGO(AES, ECB), CIPHER_ALGO(AES, CBC),  CIPHER_ALGO(AES, CTR),
 	CIPHER_ALGO(AES, CTS), CIPHER_ALGO(AES, XTS),  CIPHER_ALGO(DES, ECB),
 	CIPHER_ALGO(DES, CBC), CIPHER_ALGO(DES3, ECB), CIPHER_ALGO(DES3, CBC),
+};
+
+/**
+ * struct cmac_algo_info - Information about CMAC algorithm
+ * @mech_type: CMAC mechanism
+ * @mac_algo: CMAC algo
+ */
+struct cmac_algo_info {
+	CK_MECHANISM_TYPE mech_type;
+	smw_mac_algo_t smw_algo;
+};
+
+static struct cmac_algo_info cmac_algos[] = {
+	CMAC_ALGO(AES, CMAC, CMAC),
+	CMAC_ALGO(AES, CMAC_GENERAL, CMAC_TRUNCATED),
+	CMAC_ALGO(DES3, CMAC, CMAC),
+	CMAC_ALGO(DES3, CMAC_GENERAL, CMAC_TRUNCATED),
 };
 
 /**
@@ -421,6 +507,21 @@ static void check_mkeygen(CK_SLOT_ID slotid, const char *subsystem,
 {
 	DBG_TRACE("Check Key generate");
 	check_keygen_common(slotid, subsystem, mgroup);
+}
+
+static smw_hash_algo_t get_hash_algo(CK_MECHANISM_TYPE mech_type)
+{
+	smw_hash_algo_t hash_algo = NULL;
+	unsigned int i = 0;
+
+	for (; i < ARRAY_SIZE(mdigest); i++) {
+		if (mech_type == mdigest[i].type) {
+			hash_algo = (smw_hash_algo_t)mdigest[i].smw_algo;
+			break;
+		}
+	}
+
+	return hash_algo;
 }
 
 static CK_RV info_mdigest(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
@@ -910,7 +1011,6 @@ static CK_RV op_msign_common(struct smw_sign_verify_args *smw_args,
 {
 	CK_RV ret = CKR_ARGUMENTS_BAD;
 	enum smw_status_code status = SMW_STATUS_OK;
-	unsigned int i = 0;
 	struct smw_tlv attr = { 0 };
 	struct lib_signature_ctx *ctx = (struct lib_signature_ctx *)params->ctx;
 
@@ -923,17 +1023,10 @@ static CK_RV op_msign_common(struct smw_sign_verify_args *smw_args,
 		return ret;
 
 	/* Get hash algorithm */
-	if (entry->smw_hash) {
+	if (entry->smw_hash)
 		smw_args->algo_name = entry->smw_hash;
-	} else if (ctx->hash_mech) {
-		for (; i < ARRAY_SIZE(mdigest); i++) {
-			if (ctx->hash_mech == mdigest[i].type) {
-				smw_args->algo_name =
-					(char *)mdigest[i].smw_algo;
-				break;
-			}
-		}
-	}
+	else if (ctx->hash_mech)
+		smw_args->algo_name = get_hash_algo(ctx->hash_mech);
 
 	/* Build attribute list */
 	ret = args_attr_sign_verify(&attr, signature_type, ctx->salt_len);
@@ -1691,6 +1784,282 @@ static CK_RV op_mcipher_des3(CK_SLOT_ID slotid, struct mentry *entry,
 {
 	(void)entry;
 	return op_mcipher_common(slotid, args);
+}
+
+static smw_mac_algo_t get_cmac_algo(CK_MECHANISM_TYPE mech_type)
+{
+	smw_mac_algo_t algo = NULL;
+	unsigned int i = 0;
+
+	for (; i < ARRAY_SIZE(cmac_algos); i++) {
+		if (mech_type == cmac_algos[i].mech_type) {
+			algo = cmac_algos[i].smw_algo;
+			break;
+		}
+	}
+
+	return algo;
+}
+
+static CK_RV info_mcmac_common(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			       struct mentry *entry, CK_MECHANISM_INFO_PTR info,
+			       struct smw_mac_info mac_info)
+{
+	enum smw_status_code status = SMW_STATUS_OK;
+	CK_RV ret = CKR_OK;
+	const struct libdev *devinfo = NULL;
+
+	DBG_TRACE("info of 0x%lx MAC mechanism", type);
+
+	devinfo = libdev_get_devinfo(slotid);
+	if (!devinfo)
+		return CKR_SLOT_ID_INVALID;
+
+	/*
+	 * Global settings.
+	 */
+	info->ulMaxKeySize = 0;
+	info->ulMinKeySize = 0;
+	info->flags = 0;
+
+	mac_info.mac_algo = get_cmac_algo(entry->type);
+
+	status = smw_config_check_mac(devinfo->name, &mac_info);
+	if (status == SMW_STATUS_OK)
+		info->flags |= CKF_SIGN | CKF_VERIFY;
+
+	/*
+	 * Call specific device mechanism information function
+	 * to complete the global setting.
+	 */
+	if (dev_mech_info[slotid])
+		ret = dev_mech_info[slotid](type, info);
+
+	return ret;
+}
+
+static void check_mcmac_common(CK_SLOT_ID slotid, const char *subsystem,
+			       struct mgroup *mgroup, struct smw_mac_info info)
+{
+	enum smw_status_code status = SMW_STATUS_OK;
+	unsigned int idx;
+	CK_FLAGS slot_flag = 0;
+	struct mentry *entry = NULL;
+
+	slot_flag = BIT(slotid);
+
+	for (idx = 0, entry = mgroup->mechanism; idx < mgroup->number;
+	     idx++, entry++) {
+		info.mac_algo = get_cmac_algo(entry->type);
+		status = smw_config_check_mac(subsystem, &info);
+		if (status == SMW_STATUS_OK)
+			SET_BITS(entry->slot_flag, slot_flag);
+	}
+}
+
+static CK_RV op_mmac_common(CK_SLOT_ID slotid, struct mentry *entry, void *args)
+{
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	enum smw_status_code status = SMW_STATUS_OK;
+	const struct libdev *devinfo = NULL;
+	struct lib_signature_ctx *ctx = NULL;
+	struct lib_signature_params *params = NULL;
+	struct smw_key_descriptor key_desc = { 0 };
+	struct smw_mac_args smw_args = { 0 };
+
+	DBG_TRACE("MAC mechanism");
+
+	devinfo = libdev_get_devinfo(slotid);
+	if (!devinfo)
+		return CKR_SLOT_ID_INVALID;
+
+	params = args;
+	ctx = params->ctx;
+
+	key_desc.id = get_key_id_from((struct libobj_obj *)ctx->hkey, cipher);
+
+	smw_args.subsystem_name = devinfo->name;
+	smw_args.key_descriptor = &key_desc;
+
+	smw_args.input = params->pdata;
+	if (SET_OVERFLOW(params->uldatalen, smw_args.input_length))
+		return ret;
+
+	smw_args.mac = params->psignature;
+	if (SET_OVERFLOW(params->ulsignaturelen, smw_args.mac_length))
+		return ret;
+
+	smw_args.algo_name = GET_ALGO_NAME(entry, 0);
+
+	/* Get hash algorithm */
+	if (entry->smw_hash)
+		smw_args.hash_name = entry->smw_hash;
+	else if (ctx->hash_mech)
+		smw_args.hash_name = get_hash_algo(ctx->hash_mech);
+
+	if (params->op_flag == CKF_SIGN) {
+		status = smw_mac(&smw_args);
+
+		/* Update MAC length */
+		if (status == SMW_STATUS_OK ||
+		    status == SMW_STATUS_OUTPUT_TOO_SHORT)
+			params->ulsignaturelen = smw_args.mac_length;
+	} else {
+		status = smw_mac_verify(&smw_args);
+	}
+
+	ret = smw_status_to_ck_rv(status);
+
+	DBG_TRACE("%s on %s status %d return %ld",
+		  params->op_flag == CKF_SIGN ? "Sign" : "Verify",
+		  smw_args.subsystem_name, status, ret);
+
+	return ret;
+}
+
+static CK_RV info_mcmac_aes(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			    struct mentry *entry, CK_MECHANISM_INFO_PTR info)
+{
+	CK_RV ret = CKR_OK;
+	struct smw_mac_info mac_info = { 0 };
+
+	DBG_TRACE("Return info of 0x%lx MAC mechanism", type);
+
+	mac_info.key_type_name = AES_STR;
+
+	ret = info_mcmac_common(slotid, type, entry, info, mac_info);
+
+	return ret;
+}
+
+static void check_mcmac_aes(CK_SLOT_ID slotid, const char *subsystem,
+			    struct mgroup *mgroup)
+{
+	struct smw_mac_info info = { 0 };
+
+	info.key_type_name = AES_STR;
+
+	check_mcmac_common(slotid, subsystem, mgroup, info);
+}
+
+static CK_RV op_mcmac_aes(CK_SLOT_ID slotid, struct mentry *entry, void *args)
+{
+	return op_mmac_common(slotid, entry, args);
+}
+
+static CK_RV info_mcmac_des3(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			     struct mentry *entry, CK_MECHANISM_INFO_PTR info)
+{
+	CK_RV ret = CKR_OK;
+	struct smw_mac_info mac_info = { 0 };
+
+	DBG_TRACE("Return info of 0x%lx MAC mechanism", type);
+
+	mac_info.key_type_name = DES3_STR;
+
+	ret = info_mcmac_common(slotid, type, entry, info, mac_info);
+
+	return ret;
+}
+
+static void check_mcmac_des3(CK_SLOT_ID slotid, const char *subsystem,
+			     struct mgroup *mgroup)
+{
+	struct smw_mac_info info = { 0 };
+
+	info.key_type_name = DES3_STR;
+
+	check_mcmac_common(slotid, subsystem, mgroup, info);
+}
+
+static CK_RV op_mcmac_des3(CK_SLOT_ID slotid, struct mentry *entry, void *args)
+{
+	return op_mmac_common(slotid, entry, args);
+}
+
+static void check_mhmac(CK_SLOT_ID slotid, const char *subsystem,
+			struct mgroup *mgroup)
+{
+	enum smw_status_code status = SMW_STATUS_OK;
+	unsigned int idx = 0;
+	struct smw_mac_info info = { 0 };
+	struct mentry *entry = NULL;
+	CK_FLAGS slot_flag = 0;
+
+	DBG_TRACE("Check HMAC mechanism");
+
+	/*
+	 * smw_config_check_mac() checks the key type, the MAC algorithm
+	 * (optional) and the hash algorithm (optional).
+	 *
+	 * Slot flag is set if:
+	 *  - sign or verify or both operations are supported
+	 */
+
+	info.key_type_name = HMAC_STR;
+
+	slot_flag = BIT(slotid);
+	for (entry = mgroup->mechanism; idx < mgroup->number; idx++, entry++) {
+		if (entry->smw_algo)
+			info.mac_algo = entry->smw_algo;
+
+		if (entry->smw_hash)
+			info.hash_algo = entry->smw_hash;
+
+		status = smw_config_check_mac(subsystem, &info);
+		DBG_TRACE("%s MAC mechanism %lu: %d", subsystem, entry->type,
+			  status);
+		if (status == SMW_STATUS_OK)
+			SET_BITS(entry->slot_flag, slot_flag);
+	}
+}
+
+static CK_RV info_mhmac(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			struct mentry *entry, CK_MECHANISM_INFO_PTR info)
+{
+	enum smw_status_code status = SMW_STATUS_OK;
+	CK_RV ret = CKR_OK;
+	struct smw_mac_info mac_info = { 0 };
+	const struct libdev *devinfo = NULL;
+
+	DBG_TRACE("Return info of 0x%lx MAC mechanism", type);
+
+	devinfo = libdev_get_devinfo(slotid);
+	if (!devinfo)
+		return CKR_SLOT_ID_INVALID;
+
+	/*
+	 * MAC global settings.
+	 */
+	info->ulMaxKeySize = 0;
+	info->ulMinKeySize = 0;
+	info->flags = 0;
+
+	mac_info.key_type_name = HMAC_STR;
+
+	if (entry->smw_algo)
+		mac_info.mac_algo = entry->smw_algo;
+
+	if (entry->smw_hash)
+		mac_info.hash_algo = entry->smw_hash;
+
+	status = smw_config_check_mac(devinfo->name, &mac_info);
+	if (status == SMW_STATUS_OK)
+		info->flags |= CKF_SIGN | CKF_VERIFY;
+
+	/*
+	 * Call specific device mechanism information function
+	 * to complete the global setting.
+	 */
+	if (dev_mech_info[slotid])
+		ret = dev_mech_info[slotid](type, info);
+
+	return ret;
+}
+
+static CK_RV op_mhmac(CK_SLOT_ID slotid, struct mentry *entry, void *args)
+{
+	return op_mmac_common(slotid, entry, args);
 }
 
 CK_RV libdev_cancel_operation(void **context)

commit cdf37801ee03f26c86e296f43ee56e09fc7a46b2

SSMW-749 pkcs11: Import key to subsystem only if CKA_TOKEN is true
Import only token keys to the subsystem when C_CreateObject() is called.

Fixes: a27dc3006323 ("SSMW-115 pkcs11: Add support to messsage-based Sign/Verify")

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

pkcs11/src/objects/key_hmac.c
@@ -96,9 +96,11 @@ CK_RV key_hmac_create(CK_SESSION_HANDLE hsession, struct libobj_obj *obj,
 	/* Set the Key object value length equal to the buffer value length */
 	new_key->value_len = new_key->value.number;
 
-	/* Import the secret key in the SMW library session's subsystem */
-	ret = libdev_import_key(hsession, obj);
-	DBG_TRACE("HMAC Key ID 0x%X", new_key->key_id);
+	/* Import only the token key to the subsystem */
+	if (is_token_obj(obj, storage)) {
+		ret = libdev_import_key(hsession, obj);
+		DBG_TRACE("HMAC Key ID 0x%X", new_key->key_id);
+	}
 
 end:
 	if (ret != CKR_OK)
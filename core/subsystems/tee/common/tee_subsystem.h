nxp-imx/imx-smw

commit 913dbc86882a4a0d654d99bb9e03791685dfd70b

SSMW-301 subsystems: tee: Add support for HKDF based key derivation
Add support for HKDF based key derivation.

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

core/subsystems/tee/common/tee_subsystem.h
@@ -17,6 +17,10 @@
 #define IMP_MOD_PARAM_IDX		  3
 #define EXP_PUB_KEY_PARAM_IDX		  1
 #define EXP_MOD_PARAM_IDX		  2
+#define DER_SHARED_PARAM_IDX		  0
+#define DER_BASE_KEY_PARAM_IDX		  1
+#define DER_DERIVED_KEY_PARAM_IDX	  2
+#define DER_SHARED_MEM_IDX		  3
 #define GET_KEY_LENGTHS_KEY_ID_IDX	  0
 #define GET_KEY_LENGTHS_PUBKEYS_IDX	  1
 #define GET_KEY_LENGTHS_PRIVKEY_IDX	  2
@@ -33,6 +37,10 @@ enum tee_key_privacy {
 	TEE_KEY_PUBLIC = 0,
 	TEE_KEY_PRIVATE,
 	TEE_KEY_PAIR,
+	/* This type is intended for secret data that has been derived from a
+	 * key derivation scheme.
+	 */
+	TEE_KEY_SHARED_SECRET
 };
 
 /* TEE key type */
@@ -51,6 +59,7 @@ enum tee_key_type {
 	TEE_KEY_TYPE_ID_HMAC_SHA512,
 	TEE_KEY_TYPE_ID_HMAC_SM3,
 	TEE_KEY_TYPE_ID_RSA,
+	TEE_KEY_TYPE_ID_GENERIC_SECRET,
 	TEE_KEY_TYPE_ID_NB,
 	TEE_KEY_TYPE_ID_INVALID
 };
@@ -100,6 +109,7 @@ enum ta_commands {
 	CMD_DELETE_KEY,
 	CMD_IMPORT_KEY,
 	CMD_EXPORT_KEY,
+	CMD_DERIVE_KEY,
 	CMD_HASH,
 	CMD_SIGN,
 	CMD_VERIFY,
@@ -190,6 +200,34 @@ struct aead_shared_params {
  */
 struct shared_context {
 	void *handle;
+};
+
+/**
+ * struct key_derive_shared_params - Derive key operation shared parameters.
+ * @salt_length: Length of salt buffer in bytes.
+ * @info_length: Length of info buffer in bytes.
+ * @key_type: Key type.
+ * @key_usage: Key usage.
+ * @persistent: Use persistent subsystem storage or not.
+ * @hash_algo: Hash algorithm.
+ * @base_key_id: Key ID of base key.
+ * @derived_key_id: Key ID of the derived key.
+ * @base_key_sec_size: Base key security size in bits.
+ * @derived_key_sec_size: Derived key security size in bits.
+ * @derived_key_len: Derived key length in bytes.
+ */
+struct key_derive_shared_params {
+	uint32_t salt_length;
+	uint32_t info_length;
+	enum tee_key_type key_type;
+	unsigned int key_usage;
+	bool persistent;
+	uint32_t hash_algo;
+	uint32_t base_key_id;
+	uint32_t derived_key_id;
+	unsigned int base_key_sec_size;
+	unsigned int derived_key_sec_size;
+	unsigned int derived_key_len;
 };
 
 #endif /* TEE_SUBSYSTEM_H */

core/subsystems/tee/keymgr.c
@@ -48,6 +48,7 @@ RANGE_DEF(HMAC_SHA384, 256, 1024, 8);
 RANGE_DEF(HMAC_SHA512, 256, 1024, 8);
 RANGE_DEF(HMAC_SM3, 80, 1024, 8);
 RANGE_DEF(RSA, 256, 4096, 2);
+RANGE_DEF(GENERIC_SECRET, 8, 4096, 8);
 
 /* This alias is aimed to simplify the definitions of the macros below. */
 #define TEE_KEY_TYPE_ID_ECDSA_NIST TEE_KEY_TYPE_ID_ECDSA
@@ -124,6 +125,7 @@ static const struct key_def {
 	KEY_DEF_RANGE_SYM(HMAC_SHA512),
 	KEY_DEF_RANGE_SYM(HMAC_SM3),
 	KEY_DEF_RANGE_ASYM(RSA),
+	KEY_DEF_RANGE_ASYM(GENERIC_SECRET),
 };
 
 /**
@@ -194,7 +196,7 @@ key_type_tee_to_smw(enum tee_key_type key_type)
 	return ret_type;
 }
 
-static void key_usage_to_tee(smw_attr_usage_t smw, unsigned int *tee)
+void key_usage_to_tee(smw_attr_usage_t smw, unsigned int *tee)
 {
 	unsigned int i = 0;
 
@@ -213,7 +215,7 @@ static void key_usage_to_tee(smw_attr_usage_t smw, unsigned int *tee)
 	SMW_DBG_PRINTF(DEBUG, "TEE key usage: 0x%" PRIx32 "\n", *tee);
 }
 
-static void key_usage_to_smw(unsigned int tee, smw_attr_usage_t *smw)
+void key_usage_to_smw(unsigned int tee, smw_attr_usage_t *smw)
 {
 	unsigned int i = 0;
 
@@ -302,33 +304,6 @@ find_check_key_def(enum smw_config_key_type_id key_type_id,
 	}
 
 	return NULL;
-}
-
-/**
- * set_tmpref_buffer() - Set a shared tmpref buffer parameter.
- * @buffer_type: TEEC memory type.
- * @param_idx: Index of the parameter in @op structure.
- * @buffer: Pointer to the buffer.
- * @buffer_len: @buffer length in bytes.
- * @op: Pointer to operation structure to update.
- *
- * Return:
- * SMW_STATUS_OK		- Success.
- * SMW_STATUS_INVALID_PARAM	- Invalid index.
- */
-static inline int set_tmpref_buffer(unsigned int mem_type,
-				    unsigned int param_idx,
-				    unsigned char *buffer,
-				    unsigned int buffer_len, TEEC_Operation *op)
-{
-	if (param_idx > (TEE_NUM_PARAMS - 1))
-		return SMW_STATUS_INVALID_PARAM;
-
-	SET_TEEC_PARAMS_TYPE(op->paramTypes, mem_type, param_idx);
-	op->params[param_idx].tmpref.buffer = buffer;
-	op->params[param_idx].tmpref.size = buffer_len;
-
-	return SMW_STATUS_OK;
 }
 
 /**
@@ -651,12 +626,17 @@ static int generate_key(void *args)
 	key_attrs = key_args->key_attributes;
 
 	if (key_attrs) {
-		key_usage_to_tee(key_attrs->usage_flags,
-				 &shared_params.key_usage);
-		key_usage_to_smw(shared_params.key_usage, &actual_usage_flags);
-
 		if (SMW_ATTR_IS_PERSISTENT(key_attrs->attributes))
 			shared_params.persistent_storage = true;
+
+		if (key_attrs->usage_flags == SMW_ATTR_USAGE_NONE) {
+			shared_params.key_usage = TEE_KEY_USAGE_ALL;
+		} else {
+			key_usage_to_tee(key_attrs->usage_flags,
+					 &shared_params.key_usage);
+			key_usage_to_smw(shared_params.key_usage,
+					 &actual_usage_flags);
+		}
 	} else {
 		shared_params.key_usage = TEE_KEY_USAGE_ALL;
 	}
@@ -866,51 +846,6 @@ static int check_export_key_config(struct smw_keymgr_descriptor *key_descriptor)
 }
 
 /**
- * set_hex_imp_buffer() - Set HEX buffer for import key operation.
- * @format_id: Format of the input buffer.
- * @buffer: Pointer to the input buffer.
- * @buffer_len: @buffer length in bytes.
- * @hex_buffer: Pointer to the HEX buffer to update.
- * @hex_buffer_len: Pointer @hex_buffer length to update.
- *
- * If format id is BASE64, the input buffer in converted in HEX format. In this
- * case @hex_buffer is allocated in smw_utils_base64_decode() and will be freed
- * at the end of import_key().
- *
- * Return:
- * SMW_STATUS_OK	- Success.
- * Error code from smw_utils_base64_decode().
- */
-static int set_hex_imp_buffer(enum smw_keymgr_format_id format_id,
-			      unsigned char *buffer, unsigned int buffer_len,
-			      unsigned char **hex_buffer,
-			      unsigned int *hex_buffer_len)
-{
-	int status = SMW_STATUS_OK;
-
-	SMW_DBG_TRACE_FUNCTION_CALL;
-
-	if (format_id == SMW_KEYMGR_FORMAT_ID_BASE64) {
-		/* Convert buffer in hex format */
-		status = smw_utils_base64_decode(buffer, buffer_len, hex_buffer,
-						 hex_buffer_len);
-		if (status != SMW_STATUS_OK) {
-			SMW_DBG_PRINTF(ERROR, "%s: Failed to decode base64\n",
-				       __func__);
-			goto exit;
-		}
-	} else {
-		*hex_buffer = buffer;
-		*hex_buffer_len = buffer_len;
-		status = SMW_STATUS_OK;
-	}
-
-exit:
-	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
-	return status;
-}
-
-/**
  * set_params_import_pub_key() - Set public key buffer shared parameter for
  *                               import operation.
  * @key_descriptor: Pointer to key descriptor.
@@ -922,7 +857,7 @@ exit:
  * Return:
  * SMW_STATUS_OK		- Success.
  * SMW_STATUS_INVALID_PARAM	- Private key buffer length invalid.
- * Error code from set_hex_imp_buffer().
+ * Error code from set_hex_buffer().
  * Error code from set_tmpref_buffer().
  */
 static int
@@ -946,8 +881,8 @@ set_params_import_pub_key(struct smw_keymgr_descriptor *key_descriptor,
 		goto exit;
 	}
 
-	status = set_hex_imp_buffer(key_descriptor->format_id, pub_data,
-				    pub_data_len, &hex_pub_data, &hex_pub_len);
+	status = set_hex_buffer(key_descriptor->format_id, pub_data,
+				pub_data_len, &hex_pub_data, &hex_pub_len);
 	if (status != SMW_STATUS_OK)
 		goto exit;
 
@@ -993,7 +928,7 @@ exit:
  * Return:
  * SMW_STATUS_OK		- Success.
  * SMW_STATUS_INVALID_PARAM	- Private key buffer length invalid.
- * Error code from set_hex_imp_buffer().
+ * Error code from set_hex_buffer().
  * Error code from set_tmpref_buffer().
  */
 static int
@@ -1013,9 +948,8 @@ set_params_import_priv_key(enum tee_key_type key_type,
 	if (!priv_data_len)
 		goto exit;
 
-	status = set_hex_imp_buffer(key_descriptor->format_id, priv_data,
-				    priv_data_len, &hex_priv_data,
-				    &hex_priv_len);
+	status = set_hex_buffer(key_descriptor->format_id, priv_data,
+				priv_data_len, &hex_priv_data, &hex_priv_len);
 	if (status != SMW_STATUS_OK)
 		goto exit;
 
@@ -1055,7 +989,7 @@ exit:
  * Return:
  * SMW_STATUS_OK		- Success.
  * SMW_STATUS_INVALID_PARAM	- Modulus buffer length invalid.
- * Error code from set_hex_imp_buffer().
+ * Error code from set_hex_buffer().
  * Error code from set_tmpref_buffer().
  */
 static int
@@ -1071,9 +1005,8 @@ set_params_import_modulus(struct smw_keymgr_descriptor *key_descriptor,
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	status =
-		set_hex_imp_buffer(key_descriptor->format_id, modulus,
-				   modulus_len, &hex_modulus, &hex_modulus_len);
+	status = set_hex_buffer(key_descriptor->format_id, modulus, modulus_len,
+				&hex_modulus, &hex_modulus_len);
 	if (status != SMW_STATUS_OK)
 		goto exit;
 
@@ -1234,12 +1167,18 @@ static int import_key(void *args)
 	key_attrs = key_args->key_attributes;
 
 	if (key_attrs) {
-		key_usage_to_tee(key_attrs->usage_flags,
-				 &shared_params.key_usage);
-		key_usage_to_smw(shared_params.key_usage, &actual_usage_flags);
-
 		if (SMW_ATTR_IS_PERSISTENT(key_attrs->attributes))
 			shared_params.persistent_storage = true;
+
+		if (key_attrs->usage_flags == SMW_ATTR_USAGE_NONE) {
+			shared_params.key_usage = TEE_KEY_USAGE_ALL;
+		} else {
+			key_usage_to_tee(key_attrs->usage_flags,
+					 &shared_params.key_usage);
+			key_usage_to_smw(shared_params.key_usage,
+					 &actual_usage_flags);
+		}
+
 	} else {
 		shared_params.key_usage = TEE_KEY_USAGE_ALL;
 	}
@@ -1289,8 +1228,9 @@ exit:
 	if (status != SMW_STATUS_OK) {
 		if (shared_params.id)
 			(void)tee_delete_key(shared_params.id);
-	} else if (key_attrs->permitted_algo ||
-		   key_attrs->usage_flags != actual_usage_flags) {
+	} else if (key_attrs &&
+		   (key_attrs->permitted_algo ||
+		    key_attrs->usage_flags != actual_usage_flags)) {
 		key_attrs->permitted_algo = 0;
 		key_attrs->usage_flags = actual_usage_flags;
 
@@ -1541,6 +1481,11 @@ static int get_key_attributes(void *args)
 
 	case TEE_KEY_PUBLIC:
 		key_identifier->privacy_id = SMW_KEYMGR_PRIVACY_ID_PUBLIC;
+		break;
+
+	case TEE_KEY_SHARED_SECRET:
+		key_identifier->privacy_id =
+			SMW_KEYMGR_PRIVACY_ID_SHARED_SECRET;
 		break;
 
 	default:
@@ -1818,7 +1763,7 @@ bool tee_key_handle(enum operation_id op_id, void *args, int *status)
 		*status = delete_key(args);
 		break;
 	case OPERATION_ID_DERIVE_KEY:
-		*status = SMW_STATUS_OPERATION_NOT_CONFIGURED;
+		*status = derive_key(args);
 		break;
 	case OPERATION_ID_UPDATE_KEY:
 		*status = SMW_STATUS_OPERATION_NOT_CONFIGURED;

commit 665dffc87bedf6aebfae214f1c0f1f6489c47f93

SSMW-742 subsystems: tee: Remove exporting of base key in key derivation
- Remove exporting of symmetric base key in key derivation using
  HKDF in TA.
- Add support for importing new key type TEE_TYPE_HKDF_IKM.
- Add support for deriving key from a base key of type TEE_TYPE_HKDF_IKM.

Fixes: 913dbc86882a ("SSMW-301 subsystems: tee: Add support for HKDF based key derivation")

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

core/subsystems/tee/common/tee_subsystem.h
@@ -60,6 +60,7 @@ enum tee_key_type {
 	TEE_KEY_TYPE_ID_HMAC_SM3,
 	TEE_KEY_TYPE_ID_RSA,
 	TEE_KEY_TYPE_ID_GENERIC_SECRET,
+	TEE_KEY_TYPE_ID_HKDF_IKM,
 	TEE_KEY_TYPE_ID_NB,
 	TEE_KEY_TYPE_ID_INVALID
 };

core/subsystems/tee/keymgr.c
@@ -49,6 +49,7 @@ RANGE_DEF(HMAC_SHA512, 256, 1024, 8);
 RANGE_DEF(HMAC_SM3, 80, 1024, 8);
 RANGE_DEF(RSA, 256, 4096, 2);
 RANGE_DEF(DERIVE, 8, 4096, 8);
+RANGE_DEF(HKDF_IKM, 8, 4096, 8);
 
 #define KEY_DEF(_key_type, _security_size, _symmetric)                         \
 	{                                                                      \
@@ -91,12 +92,13 @@ RANGE_DEF(DERIVE, 8, 4096, 8);
 		.symmetric = true                                              \
 	}
 
-#define KEY_DEF_DERIVE(_key_type)                                              \
+#define KEY_DEF_DERIVE(_key_type, _tee_key_type)                               \
 	{                                                                      \
 		.key_type_id = SMW_CONFIG_KEY_TYPE_ID_##_key_type,             \
 		.hash_algo_id = SMW_CONFIG_HASH_ALGO_ID_INVALID,               \
 		.hash = SMW_ATTR_HASH_NONE,                                    \
-		.key_type = TEE_KEY_TYPE_ID_GENERIC_SECRET,                    \
+		.key_type = TEE_KEY_TYPE_ID_##_tee_key_type,                   \
+		.security_size = SECURITY_SIZE_RANGE,                          \
 		.security_size_range = security_size_range_##_key_type,        \
 		.symmetric = true                                              \
 	}
@@ -124,15 +126,24 @@ static const struct key_def {
 	struct security_size_range security_size_range;
 	bool symmetric;
 } key_def_list[] = {
-	KEY_DEF_RANGE_ASYM(SECP_R1), KEY_DEF_ASYM(SECP_R1, 384),
-	KEY_DEF_ASYM(SECP_R1, 521),  KEY_DEF_ASYM(ED25519, 256),
-	KEY_DEF_RANGE_SYM(AES),	     KEY_DEF_SYM(DES, 56),
-	KEY_DEF_RANGE_SYM(DES3),     KEY_DEF_SYM(SM4, 128),
-	KEY_DEF_HMAC(MD5),	     KEY_DEF_HMAC(SHA1),
-	KEY_DEF_HMAC(SHA224),	     KEY_DEF_HMAC(SHA256),
-	KEY_DEF_HMAC(SHA384),	     KEY_DEF_HMAC(SHA512),
-	KEY_DEF_HMAC(SM3),	     KEY_DEF_RANGE_ASYM(RSA),
-	KEY_DEF_DERIVE(DERIVE),
+	KEY_DEF_RANGE_ASYM(SECP_R1),
+	KEY_DEF_ASYM(SECP_R1, 384),
+	KEY_DEF_ASYM(SECP_R1, 521),
+	KEY_DEF_ASYM(ED25519, 256),
+	KEY_DEF_RANGE_SYM(AES),
+	KEY_DEF_SYM(DES, 56),
+	KEY_DEF_RANGE_SYM(DES3),
+	KEY_DEF_SYM(SM4, 128),
+	KEY_DEF_HMAC(MD5),
+	KEY_DEF_HMAC(SHA1),
+	KEY_DEF_HMAC(SHA224),
+	KEY_DEF_HMAC(SHA256),
+	KEY_DEF_HMAC(SHA384),
+	KEY_DEF_HMAC(SHA512),
+	KEY_DEF_HMAC(SM3),
+	KEY_DEF_RANGE_ASYM(RSA),
+	KEY_DEF_DERIVE(DERIVE, GENERIC_SECRET),
+	KEY_DEF_DERIVE(HKDF_IKM, HKDF_IKM),
 };
 
 /**
@@ -843,6 +854,7 @@ static int check_import_key_buffers_presence(enum tee_key_type key_type,
 	case TEE_KEY_TYPE_ID_HMAC_SHA384:
 	case TEE_KEY_TYPE_ID_HMAC_SHA512:
 	case TEE_KEY_TYPE_ID_HMAC_SM3:
+	case TEE_KEY_TYPE_ID_HKDF_IKM:
 		/* Symmetric key cases */
 		if (!priv_data) {
 			SMW_DBG_PRINTF(ERROR,

core/subsystems/tee/keymgr_derive.c
@@ -93,13 +93,8 @@ static bool is_base_key_type_supported(enum smw_config_key_type_id type_id,
 
 	if (kdf_id == SMW_CONFIG_KDF_ID_HKDF) {
 		switch (type_id) {
-		case SMW_CONFIG_KEY_TYPE_ID_AES:
-		case SMW_CONFIG_KEY_TYPE_ID_DES:
-		case SMW_CONFIG_KEY_TYPE_ID_DES3:
-		case SMW_CONFIG_KEY_TYPE_ID_SM4:
-		case SMW_CONFIG_KEY_TYPE_ID_HMAC:
 		case SMW_CONFIG_KEY_TYPE_ID_RAW:
-		case SMW_CONFIG_KEY_TYPE_ID_DERIVE:
+		case SMW_CONFIG_KEY_TYPE_ID_HKDF_IKM:
 			status = true;
 			break;
 

core/subsystems/tee/lib_ta/core/keymgr.c
@@ -116,6 +116,7 @@ struct {
 	KEY_DEF_RANGE(HMAC_SM3, HMAC_SM3),
 	KEY_DEF_RANGE(RSA, RSA_KEYPAIR),
 	KEY_DEF_RANGE(GENERIC_SECRET, GENERIC_SECRET),
+	KEY_DEF_RANGE(HKDF_IKM, HKDF_IKM),
 };
 
 static TEE_Result roundup_even_size(size_t *size)
@@ -709,6 +710,29 @@ static TEE_Result set_import_key_private_attributes(TEE_Attribute **attr,
 	return TEE_SUCCESS;
 }
 
+static TEE_Result set_import_ikm_attributes(TEE_Attribute **attr,
+					    uint32_t attr_count,
+					    unsigned char *priv_key,
+					    unsigned int priv_key_len)
+{
+	size_t attr_size = 0;
+
+	FMSG("Executing %s", __func__);
+
+	if (MUL_OVERFLOW(attr_count, sizeof(TEE_Attribute), &attr_size))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	*attr = TEE_Malloc(attr_size, TEE_USER_MEM_HINT_NO_FILL_ZERO);
+	if (!*attr) {
+		EMSG("TEE_Malloc failed");
+		return TEE_ERROR_OUT_OF_MEMORY;
+	}
+
+	TEE_InitRefAttribute(*attr, TEE_ATTR_HKDF_IKM, priv_key, priv_key_len);
+
+	return TEE_SUCCESS;
+}
+
 /**
  * set_import_public_ed25519_attrs() - Set import attributes for public key.
  * @attr: TEE Attribute structure to allocate and set.
@@ -959,6 +983,12 @@ set_import_key_attributes(TEE_Attribute **attr, uint32_t *attr_count,
 		return set_import_key_private_attributes(attr, NB_ATTR_SYMM_KEY,
 							 priv_key,
 							 priv_key_len);
+		break;
+
+	case TEE_TYPE_HKDF_IKM:
+		*attr_count = NB_ATTR_SYMM_KEY;
+		return set_import_ikm_attributes(attr, NB_ATTR_SYMM_KEY,
+						 priv_key, priv_key_len);
 
 	default:
 		return res;

core/subsystems/tee/lib_ta/core/keymgr_derive.c
@@ -265,10 +265,10 @@ static TEE_Result hkdf_derive_key(uint32_t param_types,
 	struct key_handle imported_key_handle = { 0 };
 	struct obj_data derived_key_obj = { 0 };
 	struct obj_data new_key_object = { 0 };
-	unsigned int sec_size = 0;
 
 	size_t base_key_len = 0;
 	size_t derived_key_len = 0;
+	unsigned int key_len = 0;
 	unsigned char *base_key = NULL;
 	unsigned char *derived_key = NULL;
 	unsigned char *base_key_priv_data = NULL;
@@ -329,30 +329,7 @@ static TEE_Result hkdf_derive_key(uint32_t param_types,
 		if (res != TEE_SUCCESS)
 			goto exit;
 
-		res = get_obj_buffer_attr(imported_key_handle.handle,
-					  &base_key_priv_data, &base_key_len,
-					  true);
-		if (res) {
-			EMSG("Failed to get the derived key attribute: 0x%x",
-			     res);
-			goto exit;
-		}
-
-		/* If the base key already exists in TEE storage, it must be of type
-		 * TEE_TYPE_HKDF_IKM to derive a new key from it using HKDF.
-		 * Therefore, first fetch the private key buffer of the base key and
-		 * import it again with object type set to TEE_TYPE_HKDF_IKM.
-		 */
-		sec_size = shared_params->base_key_sec_size;
-		res = TEE_AllocateTransientObject(TEE_TYPE_HKDF_IKM, sec_size,
-						  &base_key_handle);
-		if (res) {
-			EMSG("Failed to allocate transient object: 0x%x", res);
-			goto exit;
-		}
-
-		set_attr_buffer(attr_count, key_attr, TEE_ATTR_HKDF_IKM,
-				base_key_priv_data, base_key_len);
+		base_key_handle = imported_key_handle.handle;
 
 	} else if ((shared_params->base_key_id == INVALID_KEY_ID) && base_key) {
 		if (MUL_OVERFLOW(base_key_len, 8, &max_key_size))
@@ -368,11 +345,13 @@ static TEE_Result hkdf_derive_key(uint32_t param_types,
 
 		set_attr_buffer(attr_count, key_attr, TEE_ATTR_HKDF_IKM,
 				base_key, base_key_len);
-	}
 
-	res = TEE_PopulateTransientObject(base_key_handle, key_attr, 1);
-	if (res) {
-		EMSG("Failed to populate transient object: 0x%x", res);
+		res = TEE_PopulateTransientObject(base_key_handle, key_attr, 1);
+		if (res) {
+			EMSG("Failed to populate transient object: 0x%x", res);
+			goto exit;
+		}
+	} else {
 		goto exit;
 	}
 
@@ -385,13 +364,8 @@ static TEE_Result hkdf_derive_key(uint32_t param_types,
 	}
 
 	/* Check if "derive" usage is set for base key */
-	if (base_key_exists)
-		res = is_derive_usage_set(shared_params->base_key_id,
-					  imported_key_handle.handle,
-					  op_handle);
-	else
-		res = is_derive_usage_set(shared_params->base_key_id,
-					  base_key_handle, op_handle);
+	res = is_derive_usage_set(shared_params->base_key_id, base_key_handle,
+				  op_handle);
 	if (res)
 		goto exit;
 
@@ -453,13 +427,18 @@ static TEE_Result hkdf_derive_key(uint32_t param_types,
 
 	params[DER_DERIVED_KEY_PARAM_IDX].memref.size = derived_key_len;
 
+	if (ADD_OVERFLOW(derived_key_len, 0, &key_len)) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto exit;
+	}
+
 	/* If user has requested to store the derived key,
 	 * derived key buffer will be imported and user defined key type will be
 	 * set and the ID would be returned.
 	 */
 	if (shared_params->store_derived_key) {
 		res = import_derived_key(shared_params, &new_key_object,
-					 derived_key, derived_key_len);
+					 derived_key, key_len);
 		if (res)
 			goto exit;
 	}
nxp-imx/imx-smw

commit 2d0ebeeebbf0359b431c7278045122e240037904

SSMW-510 subsystems: tee: Add support of hash multi-part
- Add support of hash multi-part operation with TEE subsystem
- Simplify the cryptographic operation context structure

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/subsystems/tee/operation_context.c
@@ -15,7 +15,7 @@
 
 #include "smw_status.h"
 
-static void *get_subsystem_context_handle(struct smw_op_context *context,
+static void *get_subsystem_context_handle(struct smw_op_context *ctx,
 					  int *status)
 {
 	*status = SMW_STATUS_INVALID_PARAM;
@@ -25,18 +25,18 @@ static void *get_subsystem_context_handle(struct smw_op_context *context,
 	struct aead_context *aead_ctx = NULL;
 	struct cipher_context *cipher_ctx = NULL;
 
-	if (!context->subsystem_context)
+	if (!ctx->subsystem_context)
 		goto end;
 
-	switch (context->op_id) {
+	switch (ctx->op_id) {
 	case SMW_CRYPTO_OP_ID_AEAD_MULTI_PART:
-		aead_ctx = context->subsystem_context;
+		aead_ctx = ctx->subsystem_context;
 		handle = aead_ctx->tee_handle;
 
 		break;
 
 	case SMW_CRYPTO_OP_ID_CIPHER_MULTI_PART:
-		cipher_ctx = context->subsystem_context;
+		cipher_ctx = ctx->subsystem_context;
 		handle = cipher_ctx->tee_handle;
 
 		break;
@@ -54,18 +54,18 @@ end:
 
 /**
  * free_context() - Free all the memory allocated to operation context
- * @args: Pointer to SMW operation context arguments structure
+ * @ctx: Pointer to SMW operation context arguments structure
  *
  * Return:
  * None.
  */
-static void free_context(struct smw_op_context **args)
+static void free_context(struct smw_op_context *ctx)
 {
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	if ((*args)->subsystem_context) {
-		SMW_UTILS_FREE((*args)->subsystem_context);
-		(*args)->subsystem_context = NULL;
+	if (ctx->subsystem_context) {
+		SMW_UTILS_FREE(ctx->subsystem_context);
+		ctx->subsystem_context = NULL;
 	}
 }
 
@@ -141,24 +141,16 @@ static int allocate_copy_subsystem_context(struct smw_op_context *src_context,
 	case SMW_CRYPTO_OP_ID_AEAD_MULTI_PART:
 		src_aead_ctx = src_context->subsystem_context;
 
-		if (src_context->op_type_id == SMW_CRYPTO_OP_TYPE_ID_ENCRYPT) {
-			if (!src_aead_ctx || !src_aead_ctx->iv_len)
-				goto end;
-		}
-
 		dst_aead_ctx = SMW_UTILS_MALLOC(sizeof(*dst_aead_ctx));
 		if (!dst_aead_ctx) {
 			status = SMW_STATUS_ALLOC_FAILURE;
 			goto end;
 		}
 
-		if (src_context->op_type_id == SMW_CRYPTO_OP_TYPE_ID_ENCRYPT) {
-			dst_aead_ctx->iv_len = src_aead_ctx->iv_len;
+		dst_aead_ctx->iv_len = src_aead_ctx->iv_len;
+		if (dst_aead_ctx->iv_len)
 			SMW_UTILS_MEMCPY(dst_aead_ctx->iv, src_aead_ctx->iv,
 					 src_aead_ctx->iv_len);
-		} else {
-			dst_aead_ctx->iv_len = 0;
-		}
 
 		dst_aead_ctx->tee_handle = tee_dst_ctx->handle;
 		dst_context->subsystem_context = dst_aead_ctx;

core/subsystems/tee/CMakeLists.txt
@@ -24,7 +24,7 @@ set_target_properties(tee PROPERTIES C_VISIBILITY_PRESET hidden)
 # If at least one of the operation that supports MULTI_PART is enabled, context
 # features are enabled
 #
-if (ENABLE_CIPHER OR ENABLE_AEAD)
+if (ENABLE_CIPHER OR ENABLE_AEAD OR ENABLE_HASH)
     set(ENABLE_CONTEXT ON)
 endif()
 

core/subsystems/tee/aead.c
@@ -119,10 +119,6 @@ static int set_aead_context(struct smw_op_context *op_context,
 
 		if (!iv_len)
 			goto end;
-
-		op_context->op_type_id = SMW_CRYPTO_OP_TYPE_ID_ENCRYPT;
-	} else {
-		op_context->op_type_id = SMW_CRYPTO_OP_TYPE_ID_DECRYPT;
 	}
 
 	aead_ctx = SMW_UTILS_CALLOC(1, sizeof(*aead_ctx));

core/subsystems/tee/cipher.c
@@ -58,8 +58,7 @@ static int get_tee_cipher_algo_id(enum smw_config_key_type_id key_type,
 /**
  * set_cipher_context() - Allocate and initialize cipher subsystem specific ctx
  * @op_context: Pointer to operation context arguments structure
- * @args: Pointer to internal cipher arguments structure
- * @context: Pointer to TEE context operation handle structure
+ * @handle: Pointer to TEE context operation handle structure
  *
  * This function initializes the members of operation context structure. It also
  * allocates memory to cipher subsystem specific context and initializes it's
@@ -70,9 +69,7 @@ static int get_tee_cipher_algo_id(enum smw_config_key_type_id key_type,
  * SMW_STATUS_INVALID_PARAM - One of the parameters is invalid
  * SMW_STATUS_ALLOC_FAILURE - Memory allocation failure
  */
-static int set_cipher_context(struct smw_op_context *op_context,
-			      struct smw_crypto_cipher_args *args,
-			      struct shared_context *context)
+static int set_cipher_context(struct smw_op_context *op_context, void *handle)
 {
 	int status = SMW_STATUS_INVALID_PARAM;
 
@@ -91,15 +88,9 @@ static int set_cipher_context(struct smw_op_context *op_context,
 		goto end;
 	}
 
-	cipher_ctx->tee_handle = context->handle;
+	cipher_ctx->tee_handle = handle;
 
 	op_context->subsystem_context = cipher_ctx;
-
-	if (args->op_type_id == SMW_CONFIG_CIPHER_OP_TYPE_ID_ENCRYPT)
-		op_context->op_type_id = SMW_CRYPTO_OP_TYPE_ID_ENCRYPT;
-	else
-		op_context->op_type_id = SMW_CRYPTO_OP_TYPE_ID_DECRYPT;
-
 	op_context->op_state = CTX_OP_STATE_INIT;
 
 	status = SMW_STATUS_OK;
@@ -220,7 +211,7 @@ static int cipher_init(struct smw_op_context *op_context,
 			    "%s: Operation failed\n", __func__);
 
 	if (status == SMW_STATUS_OK)
-		status = set_cipher_context(op_context, args, &context);
+		status = set_cipher_context(op_context, context.handle);
 
 	/* Delete imported ephemeral keys */
 	for (key_idx = 0; key_idx < args->nb_keys; key_idx++) {

core/subsystems/tee/common/tee_subsystem.h
@@ -133,6 +133,9 @@ enum ta_commands {
 	CMD_STORAGE_RETRIEVE,
 	CMD_STORAGE_DELETE,
 	CMD_STORAGE_GET_DATA_INFO,
+	CMD_HASH_INIT,
+	CMD_HASH_UPDATE,
+	CMD_HASH_FINAL,
 	CMD_INVALID,
 };
 

core/subsystems/tee/hash.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2021, 2023 NXP
+ * Copyright 2020-2021, 2023-2024 NXP
  */
 
 #include <tee_client_api.h>
@@ -12,6 +12,52 @@
 #include "hash.h"
 #include "tee.h"
 #include "smw_status.h"
+
+/**
+ * set_hash_context() - Allocate and initialize Hash subsystem specific ctx
+ * @op_context: Pointer to operation context arguments structure
+ * @context: Pointer to TEE shared context structure
+ *
+ * This function initializes the members of operation context structure. It also
+ * allocates memory to Hash subsystem specific context and initializes it's
+ * members.
+ *
+ * Return:
+ * SMW_STATUS_OK            - Success
+ * SMW_STATUS_INVALID_PARAM - One of the parameters is invalid
+ * SMW_STATUS_ALLOC_FAILURE - Memory allocation failure
+ */
+static int set_hash_context(struct smw_op_context *op_context,
+			    struct shared_context *context)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct hash_context *hash_ctx = NULL;
+
+	if (!op_context)
+		goto end;
+
+	op_context->op_id = SMW_CRYPTO_OP_ID_HASH_MULTI_PART;
+
+	hash_ctx = SMW_UTILS_MALLOC(sizeof(*hash_ctx));
+	if (!hash_ctx) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		SMW_DBG_PRINTF(DEBUG,
+			       "Hash subsystem context allocation failure\n");
+		goto end;
+	}
+
+	hash_ctx->tee_handle = context->handle;
+
+	op_context->subsystem_context = hash_ctx;
+	op_context->op_state = CTX_OP_STATE_INIT;
+
+	status = SMW_STATUS_OK;
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
 
 /**
  * hash() - Call TA hash operation.
@@ -27,47 +73,112 @@ static int hash(void *args)
 	TEEC_Operation op = { 0 };
 	int status = SMW_STATUS_INVALID_PARAM;
 	struct smw_crypto_hash_args *hash_args = args;
-	enum tee_algorithm_id tee_algorithm_id = 0;
+	enum tee_algorithm_id tee_algorithm_id = TEE_ALGORITHM_ID_INVALID;
 	unsigned int output_length = 0;
+	enum ta_commands cmd_id = CMD_HASH;
+	struct smw_op_context *op_context = NULL;
+	struct shared_context context = { 0 };
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	if (!hash_args)
 		goto exit;
 
-	/* Convert smw algorithm ID to tee algorithm ID */
-	status = tee_convert_hash_algorithm_id(hash_args->algo_id,
-					       &tee_algorithm_id);
-	if (status != SMW_STATUS_OK) {
-		SMW_DBG_PRINTF(ERROR, "%s: Algorithm ID not supported\n",
-			       __func__);
-		goto exit;
-	}
-
 	/*
 	 * params[0] = Algorithm ID
 	 * params[1] = Message
 	 * params[2] = Digest
+	 * params[3] = Shared context
 	 */
-	op.paramTypes =
-		TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_MEMREF_TEMP_INPUT,
-				 TEEC_MEMREF_TEMP_OUTPUT, TEEC_NONE);
+	switch (hash_args->op_step) {
+	case SMW_OP_STEP_ONESHOT:
+		op.paramTypes =
+			TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,
+					 TEEC_MEMREF_TEMP_INPUT,
+					 TEEC_MEMREF_TEMP_OUTPUT, TEEC_NONE);
+		break;
+
+	case SMW_OP_STEP_INIT:
+		cmd_id = CMD_HASH_INIT;
+		op.paramTypes =
+			TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,
+					 TEEC_MEMREF_TEMP_INPUT, TEEC_NONE,
+					 TEEC_MEMREF_TEMP_OUTPUT);
+		break;
+
+	case SMW_OP_STEP_UPDATE:
+		cmd_id = CMD_HASH_UPDATE;
+		op.paramTypes =
+			TEEC_PARAM_TYPES(TEEC_NONE, TEEC_MEMREF_TEMP_INPUT,
+					 TEEC_NONE, TEEC_MEMREF_TEMP_INPUT);
+		break;
+
+	case SMW_OP_STEP_FINAL:
+		cmd_id = CMD_HASH_FINAL;
+		op.paramTypes =
+			TEEC_PARAM_TYPES(TEEC_NONE, TEEC_MEMREF_TEMP_INPUT,
+					 TEEC_MEMREF_TEMP_OUTPUT,
+					 TEEC_MEMREF_TEMP_INPUT);
+		break;
+
+	default:
+		goto exit;
+	}
+
+	/* Convert smw algorithm ID to tee algorithm ID */
+	if (hash_args->op_step == SMW_OP_STEP_ONESHOT ||
+	    hash_args->op_step == SMW_OP_STEP_INIT) {
+		status = tee_convert_hash_algorithm_id(hash_args->algo_id,
+						       &tee_algorithm_id);
+		if (status != SMW_STATUS_OK) {
+			SMW_DBG_PRINTF(ERROR,
+				       "%s: Algorithm ID not supported\n",
+				       __func__);
+			goto exit;
+		}
+	}
+
+	op_context = smw_crypto_get_hash_op_context(hash_args);
+	if ((hash_args->op_step == SMW_OP_STEP_UPDATE ||
+	     hash_args->op_step == SMW_OP_STEP_FINAL)) {
+		if (!op_context || !op_context->subsystem_context) {
+			SMW_DBG_PRINTF(ERROR, "%s: Invalid Hash handle\n",
+				       __func__);
+
+			goto exit;
+		}
+
+		context.handle =
+			((struct hash_context *)op_context->subsystem_context)
+				->tee_handle;
+	}
+
 	op.params[0].value.a = tee_algorithm_id;
 	op.params[1].tmpref.buffer = smw_crypto_get_hash_input_data(hash_args);
 	op.params[1].tmpref.size = smw_crypto_get_hash_input_length(hash_args);
 	op.params[2].tmpref.buffer = smw_crypto_get_hash_output_data(hash_args);
 	op.params[2].tmpref.size = smw_crypto_get_hash_output_length(hash_args);
+	op.params[3].tmpref.buffer = &context;
+	op.params[3].tmpref.size = sizeof(context);
+
+	smw_crypto_set_ctx_subsystem_id(op_context, SUBSYSTEM_ID_TEE);
 
 	/* Invoke TA */
-	status = execute_tee_cmd(CMD_HASH, &op);
+	status = execute_tee_cmd(cmd_id, &op);
 
-	if (status == SMW_STATUS_OK || status == SMW_STATUS_OUTPUT_TOO_SHORT) {
+	if ((hash_args->op_step == SMW_OP_STEP_ONESHOT ||
+	     hash_args->op_step == SMW_OP_STEP_FINAL) &&
+	    (status == SMW_STATUS_OK ||
+	     status == SMW_STATUS_OUTPUT_TOO_SHORT)) {
 		if (!SET_OVERFLOW(op.params[2].tmpref.size, output_length))
 			smw_crypto_set_hash_output_length(hash_args,
 							  output_length);
 		else
 			status = SMW_STATUS_OPERATION_FAILURE;
 	}
+
+	if (hash_args->op_step == SMW_OP_STEP_INIT && status == SMW_STATUS_OK)
+		set_hash_context(op_context, &context);
 
 exit:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
@@ -80,6 +191,7 @@ bool tee_hash_handle(enum operation_id op_id, void *args, int *status)
 
 	switch (op_id) {
 	case OPERATION_ID_HASH:
+	case OPERATION_ID_HASH_MULTI_PART:
 		*status = hash(args);
 		break;
 	default:

core/subsystems/tee/lib_ta/core/entry.c
@@ -65,6 +65,18 @@ TEE_Result libsmw_dispatcher(uint32_t cmd_id, uint32_t param_types,
 		res = hash(param_types, params);
 		break;
 
+	case CMD_HASH_INIT:
+		res = hash_init(param_types, params);
+		break;
+
+	case CMD_HASH_UPDATE:
+		res = hash_update(param_types, params);
+		break;
+
+	case CMD_HASH_FINAL:
+		res = hash_final(param_types, params);
+		break;
+
 	case CMD_SIGN:
 	case CMD_VERIFY:
 		res = sign_verify(param_types, params, cmd_id);

core/subsystems/tee/lib_ta/core/hash.c
@@ -109,7 +109,7 @@ TEE_Result ta_compute_digest(enum tee_algorithm_id tee_algorithm_id,
 
 	/* Get TEE algorithm ID */
 	res = get_algorithm_info(tee_algorithm_id, &algorithm_info);
-	if (res) {
+	if (res != TEE_SUCCESS || !algorithm_info) {
 		EMSG("Failed to get algorithm info: 0x%x", res);
 		return res;
 	}
@@ -121,14 +121,14 @@ TEE_Result ta_compute_digest(enum tee_algorithm_id tee_algorithm_id,
 
 	res = TEE_AllocateOperation(&operation, algorithm_info->ta_id,
 				    TEE_MODE_DIGEST, 0);
-	if (res) {
+	if (res != TEE_SUCCESS) {
 		EMSG("Failed to alloc operation: 0x%x", res);
 		return res;
 	}
 
 	/* Compute digest */
 	res = TEE_DigestDoFinal(operation, chunk, chunk_len, hash, hash_len);
-	if (res)
+	if (res != TEE_SUCCESS)
 		EMSG("Failed to compute digest: 0x%x", res);
 
 	TEE_FreeOperation(operation);
@@ -139,7 +139,9 @@ TEE_Result ta_compute_digest(enum tee_algorithm_id tee_algorithm_id,
 TEE_Result hash(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS])
 {
 	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
-	enum tee_algorithm_id algo = 0;
+
+	int tee_algorithm_id = TEE_ALGORITHM_ID_INVALID;
+
 	FMSG("Executing %s", __func__);
 
 	/*
@@ -153,14 +155,127 @@ TEE_Result hash(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS])
 					   TEE_PARAM_TYPE_NONE))
 		return res;
 
-	if (params[0].value.a < TEE_ALGORITHM_ID_INVALID) {
-		algo = params[0].value.a;
+	if (ADD_OVERFLOW(params[0].value.a, 0, &tee_algorithm_id))
+		return res;
 
-		res = ta_compute_digest(algo, params[1].memref.buffer,
-					params[1].memref.size,
-					params[2].memref.buffer,
-					&params[2].memref.size);
+	res = ta_compute_digest(tee_algorithm_id, params[1].memref.buffer,
+				params[1].memref.size, params[2].memref.buffer,
+				&params[2].memref.size);
+
+	return res;
+}
+
+TEE_Result hash_init(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS])
+{
+	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
+
+	TEE_OperationHandle operation = TEE_HANDLE_NULL;
+	int tee_algorithm_id = TEE_ALGORITHM_ID_INVALID;
+	const struct algorithm_info *algorithm_info = NULL;
+	struct shared_context *context = NULL;
+
+	FMSG("Executing %s", __func__);
+
+	/*
+	 * params[0] = Algorithm ID
+	 * params[1] = Message
+	 * params[3] = Shared context
+	 */
+	if (param_types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+					   TEE_PARAM_TYPE_MEMREF_INPUT,
+					   TEE_PARAM_TYPE_NONE,
+					   TEE_PARAM_TYPE_MEMREF_OUTPUT) ||
+	    !params[1].memref.buffer != !params[1].memref.size ||
+	    params[3].memref.size != sizeof(*context) ||
+	    !params[3].memref.buffer)
+		return res;
+
+	if (ADD_OVERFLOW(params[0].value.a, 0, &tee_algorithm_id))
+		return res;
+
+	/* Get TEE algorithm ID */
+	res = get_algorithm_info(tee_algorithm_id, &algorithm_info);
+	if (res != TEE_SUCCESS || !algorithm_info) {
+		EMSG("!Failed to get algorithm info: 0x%x", res);
+		return res;
 	}
+
+	res = TEE_AllocateOperation(&operation, algorithm_info->ta_id,
+				    TEE_MODE_DIGEST, 0);
+	if (res != TEE_SUCCESS) {
+		EMSG("Failed to alloc operation: 0x%x", res);
+		return res;
+	}
+
+	/* Compute digest */
+	if (params[1].memref.buffer && params[1].memref.size)
+		TEE_DigestUpdate(operation, params[1].memref.buffer,
+				 params[1].memref.size);
+
+	/* Share operation handle */
+	context = params[3].memref.buffer;
+	context->handle = operation;
+
+	return res;
+}
+
+TEE_Result hash_update(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS])
+{
+	struct shared_context *context = NULL;
+
+	FMSG("Executing %s", __func__);
+
+	/*
+	 * params[1] = Message
+	 * params[3] = Shared context
+	 */
+	if (param_types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_NONE,
+					   TEE_PARAM_TYPE_MEMREF_INPUT,
+					   TEE_PARAM_TYPE_NONE,
+					   TEE_PARAM_TYPE_MEMREF_INPUT) ||
+	    !params[1].memref.buffer != !params[1].memref.size ||
+	    params[3].memref.size != sizeof(*context) ||
+	    !params[3].memref.buffer)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	context = params[3].memref.buffer;
+
+	/* Compute digest */
+	TEE_DigestUpdate(context->handle, params[1].memref.buffer,
+			 params[1].memref.size);
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result hash_final(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS])
+{
+	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
+
+	struct shared_context *context = NULL;
+
+	FMSG("Executing %s", __func__);
+
+	/*
+	 * params[1] = Message
+	 * params[2] = Digest
+	 * params[3] = Shared context
+	 */
+	if (param_types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_NONE,
+					   TEE_PARAM_TYPE_MEMREF_INPUT,
+					   TEE_PARAM_TYPE_MEMREF_OUTPUT,
+					   TEE_PARAM_TYPE_MEMREF_INPUT) ||
+	    !params[1].memref.buffer != !params[1].memref.size ||
+	    params[3].memref.size != sizeof(*context) ||
+	    !params[3].memref.buffer)
+		return res;
+
+	context = params[3].memref.buffer;
+
+	res = TEE_DigestDoFinal(context->handle, params[1].memref.buffer,
+				params[1].memref.size, params[2].memref.buffer,
+				&params[2].memref.size);
+	if (res != TEE_SUCCESS)
+		EMSG("Failed to compute digest: 0x%x", res);
 
 	return res;
 }

commit c58f993e1eb4de64c19c08ace713990f08d8fd93

SSMW-655 subsystems: tee: fix copying context for hash multi-part
Properly handle SMW_CRYPTO_OP_ID_HASH_MULTI_PART in the context
functions.

Fixes: 2d0ebeeebbf0 ("SSMW-510 subsystems: tee: Add support of hash multi-part")
Signed-off-by: Ilie Halip <ilie.halip@nxp.com>

core/subsystems/tee/operation_context.c
@@ -15,15 +15,13 @@
 
 #include "smw_status.h"
 
-static void *get_subsystem_context_handle(struct smw_op_context *ctx,
-					  int *status)
+static int get_subsystem_context_handle(struct smw_op_context *ctx,
+					void **handle)
 {
-	*status = SMW_STATUS_INVALID_PARAM;
-
-	void *handle = NULL;
-
 	struct aead_context *aead_ctx = NULL;
 	struct cipher_context *cipher_ctx = NULL;
+	struct hash_context *hash_ctx = NULL;
+	enum smw_status_code status = SMW_STATUS_INVALID_PARAM;
 
 	if (!ctx->subsystem_context)
 		goto end;
@@ -31,25 +29,33 @@ static void *get_subsystem_context_handle(struct smw_op_context *ctx,
 	switch (ctx->op_id) {
 	case SMW_CRYPTO_OP_ID_AEAD_MULTI_PART:
 		aead_ctx = ctx->subsystem_context;
-		handle = aead_ctx->tee_handle;
+		*handle = aead_ctx->tee_handle;
 
+		status = SMW_STATUS_OK;
 		break;
 
 	case SMW_CRYPTO_OP_ID_CIPHER_MULTI_PART:
 		cipher_ctx = ctx->subsystem_context;
-		handle = cipher_ctx->tee_handle;
+		*handle = cipher_ctx->tee_handle;
 
+		status = SMW_STATUS_OK;
+		break;
+
+	case SMW_CRYPTO_OP_ID_HASH_MULTI_PART:
+		hash_ctx = ctx->subsystem_context;
+		*handle = hash_ctx->tee_handle;
+
+		status = SMW_STATUS_OK;
 		break;
 
 	default:
+		status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
 		break;
 	}
 
-	*status = SMW_STATUS_OK;
-
 end:
-	SMW_DBG_PRINTF(EXTRA, "%s returned %d\n", __func__, *status);
-	return handle;
+	SMW_DBG_PRINTF(EXTRA, "%s returned %d\n", __func__, status);
+	return status;
 }
 
 /**
@@ -74,19 +80,21 @@ static void free_context(struct smw_op_context *ctx)
  * @ctx: Pointer to SMW operation context arguments structure
  *
  * Return:
- * SMW_STATUS_OK            - Success
- * SMW_STATUS_INVALID_PARAM - One of the parameters is invalid
+ * SMW_STATUS_OK                      - Success
+ * SMW_STATUS_INVALID_PARAM           - One of the parameters is invalid
+ * SMW_STATUS_SUBSYSTEM_FAILURE       - Subsystem failure
+ * SMW_STATUS_OPERATION_NOT_SUPPORTED - Operation not supported by subsystem
  */
 static int cancel_operation(struct smw_op_context *ctx)
 {
-	int status = SMW_STATUS_INVALID_PARAM;
+	int status = SMW_STATUS_OK;
 
 	struct shared_context shared_ctx = { 0 };
 	TEEC_Operation op = { 0 };
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	shared_ctx.handle = get_subsystem_context_handle(ctx, &status);
+	status = get_subsystem_context_handle(ctx, &shared_ctx.handle);
 	if (status != SMW_STATUS_OK || !shared_ctx.handle)
 		goto end;
 
@@ -136,6 +144,7 @@ static int allocate_copy_subsystem_context(struct smw_op_context *src_context,
 	struct aead_context *src_aead_ctx = NULL;
 	struct aead_context *dst_aead_ctx = NULL;
 	struct cipher_context *dst_cipher_ctx = NULL;
+	struct hash_context *dst_hash_ctx = NULL;
 
 	switch (src_context->op_id) {
 	case SMW_CRYPTO_OP_ID_AEAD_MULTI_PART:
@@ -169,6 +178,18 @@ static int allocate_copy_subsystem_context(struct smw_op_context *src_context,
 
 		break;
 
+	case SMW_CRYPTO_OP_ID_HASH_MULTI_PART:
+		dst_hash_ctx = SMW_UTILS_MALLOC(sizeof(*dst_hash_ctx));
+		if (!dst_hash_ctx) {
+			status = SMW_STATUS_ALLOC_FAILURE;
+			goto end;
+		}
+
+		dst_hash_ctx->tee_handle = tee_dst_ctx->handle;
+		dst_context->subsystem_context = dst_hash_ctx;
+
+		break;
+
 	default:
 		goto end;
 	}
@@ -196,7 +217,7 @@ end:
 static int copy_context(struct smw_op_context *src_ctx,
 			struct smw_op_context *dst_ctx)
 {
-	int status = SMW_STATUS_INVALID_PARAM;
+	int status = SMW_STATUS_OK;
 
 	struct shared_context src_shared_ctx = { 0 };
 	struct shared_context dst_shared_ctx = { 0 };
@@ -204,7 +225,7 @@ static int copy_context(struct smw_op_context *src_ctx,
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	src_shared_ctx.handle = get_subsystem_context_handle(src_ctx, &status);
+	status = get_subsystem_context_handle(src_ctx, &src_shared_ctx.handle);
 	if (status != SMW_STATUS_OK || !src_shared_ctx.handle)
 		goto end;
 
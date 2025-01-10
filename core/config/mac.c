nxp-imx/imx-smw

commit c0da8723117c1ae70b855c616d20d61d8fbda2a5

SSMW-371 core: config: Add MAC configuration
Add the definition of the MAC in the configuration file.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/config/mac.c
@@ -1,0 +1,156 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ */
+
+#include "smw_crypto.h"
+
+#include "compiler.h"
+#include "debug.h"
+#include "mac.h"
+#include "name.h"
+#include "tag.h"
+#include "utils.h"
+
+#include "common.h"
+
+static const char *const mac_algo_names[] = {
+	[SMW_CONFIG_MAC_ALGO_ID_CMAC] = "CMAC",
+	[SMW_CONFIG_MAC_ALGO_ID_CMAC_TRUNCATED] = "CMAC_TRUNCATED",
+	[SMW_CONFIG_MAC_ALGO_ID_HMAC] = "HMAC",
+	[SMW_CONFIG_MAC_ALGO_ID_HMAC_TRUNCATED] = "HMAC_TRUNCATED",
+};
+
+static int read_mac_algo_names(char **start, char *end, unsigned long *bitmap)
+{
+	return read_names(start, end, bitmap, mac_algo_names,
+			  SMW_CONFIG_MAC_ALGO_ID_NB);
+}
+
+static int mac_read_params(char **start, char *end, void **params)
+{
+	int status = SMW_STATUS_OK;
+	char *cur = *start;
+
+	char buffer[SMW_CONFIG_MAX_PARAMS_NAME_LENGTH + 1];
+	unsigned int length;
+
+	struct mac_params *p;
+	unsigned long key_size_range_bitmap = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	p = SMW_UTILS_CALLOC(1, sizeof(*p));
+	if (!p) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		goto end;
+	}
+
+	init_key_params(&p->key);
+
+	while ((cur < end) && (open_square_bracket != *cur)) {
+		status = read_params_name(&cur, end, buffer);
+		if (status != SMW_STATUS_OK)
+			goto end;
+		SMW_DBG_PRINTF(INFO, "Parameter: %s\n", buffer);
+		length = SMW_UTILS_STRLEN(buffer);
+
+		skip_insignificant_chars(&cur, end);
+
+		if (!SMW_UTILS_STRNCMP(buffer, mac_algo_values, length)) {
+			status =
+				read_mac_algo_names(&cur, end, &p->algo_bitmap);
+			if (status != SMW_STATUS_OK)
+				goto end;
+
+		} else if (!SMW_UTILS_STRNCMP(buffer, hash_algo_values,
+					      length)) {
+			status = read_hash_algo_names(&cur, end,
+						      &p->hash_bitmap);
+			if (status != SMW_STATUS_OK)
+				goto end;
+
+		} else if (read_key(buffer, length, &cur, end,
+				    &key_size_range_bitmap, &p->key, &status)) {
+			if (status != SMW_STATUS_OK)
+				goto end;
+		} else {
+			status = skip_param(&cur, end);
+			if (status != SMW_STATUS_OK)
+				goto end;
+		}
+
+		skip_insignificant_chars(&cur, end);
+	}
+
+	if (!p->algo_bitmap)
+		p->algo_bitmap = SMW_ALL_ONES;
+
+	if (!p->hash_bitmap)
+		p->hash_bitmap = SMW_ALL_ONES;
+
+	if (!p->key.type_bitmap)
+		p->key.type_bitmap = SMW_ALL_ONES;
+
+	*params = p;
+
+	*start = cur;
+
+end:
+	if (p && status != SMW_STATUS_OK)
+		SMW_UTILS_FREE(p);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static void mac_merge_params(void *caps, void *params)
+{
+	struct mac_params *mac_caps = caps;
+	struct mac_params *mac_params = params;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	mac_caps->algo_bitmap |= mac_params->algo_bitmap;
+	merge_key_params(&mac_caps->key, &mac_params->key);
+}
+
+__weak void mac_print_params(void *params)
+{
+	(void)params;
+}
+
+static int mac_check_subsystem_caps(void *args, void *params)
+{
+	int status = SMW_STATUS_OPERATION_NOT_CONFIGURED;
+	struct smw_crypto_mac_args *mac_args = args;
+	struct mac_params *mac_params = params;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (check_id(mac_args->algo_id, mac_params->algo_bitmap) &&
+	    check_key(&mac_args->key_descriptor.identifier, &mac_params->key))
+		status = SMW_STATUS_OK;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+DEFINE_CONFIG_OPERATION_FUNC(mac);
+
+int smw_config_get_mac_algo_id(const char *name,
+			       enum smw_config_mac_algo_id *id)
+{
+	int status = SMW_STATUS_OK;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+	if (!name)
+		*id = SMW_CONFIG_MAC_ALGO_ID_INVALID;
+	else
+		status = smw_utils_get_string_index(name, mac_algo_names,
+						    SMW_CONFIG_MAC_ALGO_ID_NB,
+						    id);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}

CMakeLists.txt
@@ -58,6 +58,7 @@ cmake_dependent_option(ENABLE_HASH "Enable Hash" ON "ENABLE_KEYMGR_MODULE" OFF)
 cmake_dependent_option(ENABLE_SIGN_VERIFY "Enable Sign and Verify" ON "ENABLE_KEYMGR_MODULE" OFF)
 cmake_dependent_option(ENABLE_HMAC "Enable HMAC" ON "ENABLE_KEYMGR_MODULE" OFF)
 cmake_dependent_option(ENABLE_CIPHER "Enable Cipher" ON "ENABLE_KEYMGR_MODULE" OFF)
+cmake_dependent_option(ENABLE_MAC "Enable MAC" ON "ENABLE_KEYMGR_MODULE" OFF)
 option(ENABLE_RNG "Enable Random number generator" ON)
 option(ENABLE_TLS12 "Enable TLS 1.2 features" OFF)
 

core/config/CMakeLists.txt
@@ -14,6 +14,7 @@ target_sources(config PRIVATE
                $<$<BOOL:${ENABLE_HMAC}>:hmac.c>
                $<$<BOOL:${ENABLE_RNG}>:rng.c>
                $<$<BOOL:${ENABLE_CIPHER}>:cipher.c>
+               $<$<BOOL:${ENABLE_MAC}>:mac.c>
                $<$<BOOL:${ENABLE_PSA_DEFAULT_ALT}>:parser_psa_alt.c>)
 if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND VERBOSE AND NOT VERBOSE EQUAL 0)
 target_sources(config PRIVATE
@@ -23,7 +24,8 @@ target_sources(config PRIVATE
                $<$<BOOL:${ENABLE_SIGN_VERIFY}>:sign_verify_debug.c>
                $<$<BOOL:${ENABLE_HMAC}>:hmac_debug.c>
                $<$<BOOL:${ENABLE_RNG}>:rng_debug.c>
-               $<$<BOOL:${ENABLE_CIPHER}>:cipher_debug.c>)
+               $<$<BOOL:${ENABLE_CIPHER}>:cipher_debug.c>
+               $<$<BOOL:${ENABLE_MAC}>:mac_debug.c>)
 endif()
 target_include_directories(config PRIVATE ${INCLUDE_DIR} ${CMAKE_CURRENT_BINARY_DIR})
 

core/config/common.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2022 NXP
+ * Copyright 2020-2023 NXP
  */
 
 #ifndef __COMMON_H__
@@ -63,6 +63,12 @@ struct hash_params {
 
 struct hmac_params {
 	unsigned long algo_bitmap;
+	struct op_key key;
+};
+
+struct mac_params {
+	unsigned long algo_bitmap;
+	unsigned long hash_bitmap;
 	struct op_key key;
 };
 

core/config/mac_debug.c
@@ -1,0 +1,26 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ */
+
+#include "debug.h"
+
+#include "common.h"
+
+void mac_print_params(void *params)
+{
+	struct mac_params *p = params;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!p)
+		return;
+
+	SMW_DBG_PRINTF(DEBUG,
+		       "Params:\n"
+		       "    algo_bitmap: 0x%.8lX\n"
+		       "    hash_bitmap: 0x%.8lX\n",
+		       p->algo_bitmap, p->hash_bitmap);
+
+	print_key_params(&p->key);
+}

core/config/tag.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021 NXP
+ * Copyright 2021-2023 NXP
  */
 
 /* Specified separators */
@@ -27,6 +27,7 @@ const char *alt_tag = "ALT";
 const char *key_type_values = "KEY_TYPE_VALUES";
 const char *hash_algo_values = "HASH_ALGO_VALUES";
 const char *hmac_algo_values = "HMAC_ALGO_VALUES";
+const char *mac_algo_values = "MAC_ALGO_VALUES";
 const char *sign_type_values = "SIGN_TYPE_VALUES";
 const char *op_type_values = "OP_TYPE_VALUES";
 const char *mode_values = "MODE_VALUES";

core/config/tag.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2021 NXP
+ * Copyright 2021-2023 NXP
  */
 
 #ifndef __TAG_H__
@@ -30,6 +30,7 @@ extern const char *alt_tag;
 extern const char *key_type_values;
 extern const char *hash_algo_values;
 extern const char *hmac_algo_values;
+extern const char *mac_algo_values;
 extern const char *sign_type_values;
 extern const char *op_type_values;
 extern const char *mode_values;

core/crypto/CMakeLists.txt
@@ -22,6 +22,10 @@ add_operation(CIPHER)
 add_operation(CIPHER_MULTI_PART)
 endif()
 
+if(ENABLE_MAC)
+add_operation(MAC)
+endif()
+
 #
 # If at least one of the operation that supports MULTI_PART is enabled, context
 # features are enabled

commit 935eb4d73573d599178be78d474967e5054443f5

SSMW-803 config: Fix MAC hash bitmap
Hash bitmap during MAC parsing not merged in the operation parameters
node.
MAC configuration wrongly check hash algorithm.

Fixes: c0da8723117c ("SSMW-371 core: config: Add MAC configuration")
Fixes: c6185fb03eff ("SSMW-115 core: config: Add MAC configuration information API")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/config/mac.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2023-2024 NXP
+ * Copyright 2023-2025 NXP
  */
 
 #include "smw_config.h"
@@ -127,6 +127,8 @@ static void mac_merge_params(void *caps, void *params)
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	mac_caps->algo_bitmap |= mac_params->algo_bitmap;
+	mac_caps->hash_bitmap |= mac_params->hash_bitmap;
+
 	merge_key_params(&mac_caps->key, &mac_params->key);
 }
 
@@ -245,7 +247,7 @@ __export enum smw_status_code smw_config_check_mac(smw_subsystem_t subsystem,
 		if (status != SMW_STATUS_OK)
 			return status;
 
-		if (!check_id(hash_id, params.algo_bitmap))
+		if (!check_id(hash_id, params.hash_bitmap))
 			return SMW_STATUS_OPERATION_NOT_CONFIGURED;
 	}
 
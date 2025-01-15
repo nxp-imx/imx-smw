adc12d8ef6fa2577def176e6a389d177e3da68ed
Cedric Neveux <cedric.neveux@nxp.com>
2025-01-15 16:13:08 +0100

    SSMW-799 core: storage: Add weak functions when storage disabled.
    Add weak functions of the API when module is disabled.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- core/storage/storage_weak.c
+++ core/storage/storage_weak.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2023-2024 NXP
+ * Copyright 2023-2025 NXP
  */
 
 #include "smw_storage.h"
@@ -28,6 +28,15 @@ smw_retrieve_data(struct smw_retrieve_data_args *args)
 }
 
 __weak enum smw_status_code smw_delete_data(struct smw_delete_data_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_API_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
+__weak enum smw_status_code smw_get_data_info(struct smw_data_info_args *args)
 {
 	(void)args;
 

nxp-imx/imx-smw

commit 9348f641e426a1465ee9682933a11beb3a504730

SSMW-719 core: check key type/algo on generate
There may be multiple subsystems which support an operation, but not
necessarily with the same key types/modes. Consider this example:

[SECURE_SUBSYSTEM]
    TEE;
    [SECURITY_OPERATION]
        GENERATE_KEY;
    [SECURITY_OPERATION]
        AEAD;
        KEY_TYPE_VALUES=AES;
        MODE_VALUES=CCM;
[SECURE_SUBSYSTEM]
    ELE;
    [SECURITY_OPERATION]
        GENERATE_KEY;
    [SECURITY_OPERATION]
    AEAD;
        KEY_TYPE_VALUES=AES;
        MODE_VALUES=CCM:GCM:CHACHA20_POLY1305;

Currently, when a key with type=AES and mode=POLY1305 is being
generated, it would be assigned to TEE, even though TEE does not support
it. GENERATE_KEY can only restrict based on key type, not key mode/algo.

So, during key generation, also search through a subsystem's operations
to see if there is one that uses the key mode/algo explicitly.

Signed-off-by: Ilie Halip <ilie.halip@nxp.com>

core/config/aead.c
@@ -12,6 +12,7 @@
 #include "utils.h"
 #include "operations.h"
 #include "subsystems.h"
+#include "list.h"
 
 #include "aead.h"
 #include "tag.h"
@@ -22,6 +23,13 @@ static const char *const aead_mode_strings[] = {
 	[SMW_CONFIG_AEAD_MODE_ID_CCM] = "CCM",
 	[SMW_CONFIG_AEAD_MODE_ID_CHACHA20_POLY1305] = "CHACHA20_POLY1305",
 	[SMW_CONFIG_AEAD_MODE_ID_GCM] = "GCM",
+};
+
+static unsigned int aead_mode_attrs[] = {
+	[SMW_CONFIG_AEAD_MODE_ID_CCM] = SMW_ATTR_MODE_CCM,
+	[SMW_CONFIG_AEAD_MODE_ID_GCM] = SMW_ATTR_MODE_GCM,
+	[SMW_CONFIG_AEAD_MODE_ID_CHACHA20_POLY1305] = SMW_ATTR_MODE_POLY1305,
+	[SMW_CONFIG_AEAD_MODE_ID_NB] = 0,
 };
 
 static const char *const aead_op_type_strings[] = {
@@ -189,25 +197,11 @@ static void aead_multi_part_print_params(void *params)
 	aead_common_print_params(params);
 }
 
-/**
- * check_common_subsystem_caps() - Check subsystem capabilities for AEAD
- *                                 one-shot and multi-part operations
- * @args: Internal AEAD arguments.
- * @params: AEAD operation subsystem parameters.
- *
- * This function checks if mode and operation type set in @args are supported
- * by the subsystem.
- * It also check that the keysset in @args is supported.
- *
- * Return:
- * SMW_STATUS_OK			- Success
- * SMW_STATUS_OPERATION_NOT_CONFIGURED	- Operation not configured
- */
-static int check_common_subsystem_caps(void *args, void *params)
+static int check_common_subsystem_caps(void *args, void *node)
 {
 	int status = SMW_STATUS_OPERATION_NOT_CONFIGURED;
 	struct smw_crypto_aead_args *aead_args = args;
-	struct aead_params *aead_params = params;
+	struct aead_params *aead_params = smw_utils_list_get_data(node);
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -225,16 +219,70 @@ end:
 	return status;
 }
 
-static int aead_check_subsystem_caps(void *args, void *params)
+static int aead_check_subsystem_caps(void *args, void *node)
 {
-	return check_common_subsystem_caps(args, params);
+	return check_common_subsystem_caps(args, node);
 }
 
-static int aead_multi_part_check_subsystem_caps(void *args, void *params)
+static int aead_multi_part_check_subsystem_caps(void *args, void *node)
 {
 	/* This function is only called by AEAD initialization */
 
-	return check_common_subsystem_caps(args, params);
+	return check_common_subsystem_caps(args, node);
+}
+
+static int check_common_key_usable(enum operation_id operation_id,
+				   unsigned int *ref,
+				   enum smw_config_key_type_id key_type_id,
+				   smw_attr_algo_t permitted_algo)
+{
+	int status = SMW_STATUS_OK;
+	struct aead_params params = { 0 };
+	smw_attr_algo_t mode = SMW_ATTR_MODE_NONE;
+	size_t idx = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	status = get_operation_params(operation_id, ref, &params);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	mode = SMW_ATTR_GET_MODE(permitted_algo);
+
+	status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	if (!check_id(key_type_id, params.key.type_bitmap))
+		goto end;
+
+	for (; idx < ARRAY_SIZE(aead_mode_attrs); idx++) {
+		if ((mode == SMW_ATTR_MODE_NONE ||
+		     mode == aead_mode_attrs[idx]) &&
+		    check_id(idx, params.mode_bitmap)) {
+			status = SMW_STATUS_OK;
+			break;
+		}
+	}
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int aead_check_key_usable(unsigned int *ref,
+				 enum smw_config_key_type_id key_type_id,
+				 smw_attr_algo_t permitted_algo)
+{
+	return check_common_key_usable(OPERATION_ID_AEAD, ref, key_type_id,
+				       permitted_algo);
+}
+
+static int
+aead_multi_part_check_key_usable(unsigned int *ref,
+				 enum smw_config_key_type_id key_type_id,
+				 smw_attr_algo_t permitted_algo)
+{
+	return check_common_key_usable(OPERATION_ID_AEAD_MULTI_PART, ref,
+				       key_type_id, permitted_algo);
 }
 
 DEFINE_CONFIG_OPERATION_FUNC(aead);
@@ -279,7 +327,7 @@ __export enum smw_status_code smw_config_check_aead(smw_subsystem_t subsystem,
 	if (info->multipart)
 		op_id = OPERATION_ID_AEAD_MULTI_PART;
 
-	status = get_operation_params(op_id, id, &params);
+	status = get_operation_params_lock(op_id, id, &params);
 	if (status != SMW_STATUS_OK)
 		return status;
 

core/config/cipher.c
@@ -14,6 +14,7 @@
 #include "subsystems.h"
 #include "cipher.h"
 #include "tag.h"
+#include "list.h"
 
 #include "common.h"
 
@@ -24,6 +25,16 @@ static const char *const cipher_mode_strings[] = {
 	[SMW_CONFIG_CIPHER_MODE_ID_CTS] = "CTS",
 	[SMW_CONFIG_CIPHER_MODE_ID_ECB] = "ECB",
 	[SMW_CONFIG_CIPHER_MODE_ID_XTS] = "XTS"
+};
+
+static unsigned int cipher_mode_attrs[] = {
+	[SMW_CONFIG_CIPHER_MODE_ID_CBC] = SMW_ATTR_MODE_CBC_NO_PAD,
+	[SMW_CONFIG_CIPHER_MODE_ID_CFB] = SMW_ATTR_MODE_CFB,
+	[SMW_CONFIG_CIPHER_MODE_ID_CTR] = SMW_ATTR_MODE_CTR,
+	[SMW_CONFIG_CIPHER_MODE_ID_CTS] = SMW_ATTR_MODE_CTS,
+	[SMW_CONFIG_CIPHER_MODE_ID_ECB] = SMW_ATTR_MODE_ECB_NO_PAD,
+	[SMW_CONFIG_CIPHER_MODE_ID_XTS] = SMW_ATTR_MODE_XTS,
+	[SMW_CONFIG_CIPHER_MODE_ID_NB] = 0,
 };
 
 int read_cipher_mode_strings(char **start, char *end, unsigned long *bitmap)
@@ -186,26 +197,12 @@ static void cipher_multi_part_print_params(void *params)
 	cipher_common_print_params(params);
 }
 
-/**
- * check_common_subsystem_caps() - Check subsystem capabilities for cipher
- *                                 one-shot and multi-part operations
- * @args: Internal cipher arguments.
- * @params: Cipher operation subsystem parameters.
- *
- * This function checks if mode and operation type set in @args are supported
- * by the subsystem.
- * It also checks that all keys set in @args are supported.
- *
- * Return:
- * SMW_STATUS_OK			- Success
- * SMW_STATUS_OPERATION_NOT_CONFIGURED	- Operation not configured
- */
-static int check_common_subsystem_caps(void *args, void *params)
+static int check_common_subsystem_caps(void *args, void *node)
 {
 	int status = SMW_STATUS_OPERATION_NOT_CONFIGURED;
 	unsigned int i = 0;
 	struct smw_crypto_cipher_args *cipher_args = args;
-	struct cipher_params *cipher_params = params;
+	struct cipher_params *cipher_params = smw_utils_list_get_data(node);
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -225,16 +222,71 @@ end:
 	return status;
 }
 
-static int cipher_check_subsystem_caps(void *args, void *params)
+static int cipher_check_subsystem_caps(void *args, void *node)
 {
-	return check_common_subsystem_caps(args, params);
+	return check_common_subsystem_caps(args, node);
 }
 
-static int cipher_multi_part_check_subsystem_caps(void *args, void *params)
+static int cipher_multi_part_check_subsystem_caps(void *args, void *node)
 {
 	/* This function is only called by cipher initialization */
 
-	return check_common_subsystem_caps(args, params);
+	return check_common_subsystem_caps(args, node);
+}
+
+static int check_common_key_usable(enum operation_id operation_id,
+				   unsigned int *ref,
+				   enum smw_config_key_type_id key_type_id,
+				   smw_attr_algo_t permitted_algo)
+{
+	int status = SMW_STATUS_OK;
+
+	struct cipher_params params = { 0 };
+	smw_attr_algo_t mode = SMW_ATTR_MODE_NONE;
+	size_t idx = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	status = get_operation_params(operation_id, ref, &params);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	mode = SMW_ATTR_GET_MODE(permitted_algo);
+
+	status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	if (!check_id(key_type_id, params.key.type_bitmap))
+		goto end;
+
+	for (; idx < ARRAY_SIZE(cipher_mode_attrs); idx++) {
+		if ((mode == SMW_ATTR_MODE_NONE ||
+		     mode == cipher_mode_attrs[idx]) &&
+		    check_id(idx, params.mode_bitmap)) {
+			status = SMW_STATUS_OK;
+			break;
+		}
+	}
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int cipher_check_key_usable(unsigned int *ref,
+				   enum smw_config_key_type_id key_type_id,
+				   smw_attr_algo_t permitted_algo)
+{
+	return check_common_key_usable(OPERATION_ID_CIPHER, ref, key_type_id,
+				       permitted_algo);
+}
+
+static int
+cipher_multi_part_check_key_usable(unsigned int *ref,
+				   enum smw_config_key_type_id key_type_id,
+				   smw_attr_algo_t permitted_algo)
+{
+	return check_common_key_usable(OPERATION_ID_CIPHER, ref, key_type_id,
+				       permitted_algo);
 }
 
 DEFINE_CONFIG_OPERATION_FUNC(cipher);
@@ -281,7 +333,7 @@ smw_config_check_cipher(smw_subsystem_t subsystem, struct smw_cipher_info *info)
 	if (info->multipart)
 		op_id = OPERATION_ID_CIPHER_MULTI_PART;
 
-	status = get_operation_params(op_id, id, &params);
+	status = get_operation_params_lock(op_id, id, &params);
 	if (status != SMW_STATUS_OK)
 		return status;
 

core/config/common.h
@@ -11,6 +11,8 @@
 #include "config.h"
 #include "keymgr.h"
 
+struct node;
+
 enum subsystem_state { SUBSYSTEM_STATE_UNLOADED, SUBSYSTEM_STATE_LOADED };
 
 #define SMW_CONFIG_MAX_STRING_LENGTH 256
@@ -29,7 +31,8 @@ enum subsystem_state { SUBSYSTEM_STATE_UNLOADED, SUBSYSTEM_STATE_LOADED };
 		.read = operation##_read_params,                               \
 		.merge = operation##_merge_params,                             \
 		.print = operation##_print_params,                             \
-		.check_subsystem_caps = operation##_check_subsystem_caps       \
+		.check_subsystem_caps = operation##_check_subsystem_caps,      \
+		.check_key_usable = operation##_check_key_usable               \
 	};                                                                     \
 	struct operation_func *smw_##operation##_get_func(void)                \
 	{                                                                      \
@@ -447,18 +450,30 @@ int store_operation_params(enum operation_id operation_id, void *params,
 /**
  * get_operation_params() - Get an operation parameters.
  * @operation_id: Security Operation ID.
- * @subsystem_id: Secure Subsystem ID.
- * @params: Pointer to the data structure
- *          that describes the parameters.
+ * @ref: Pointer to the reference associated to the operation nodes.
+ * @params: Pointer to the data structure describing the parameters.
  *
- * This function gets the parameters configured for
- * this Security Operation.
+ * This function gets the parameters configured for subsystem's operation.
  *
  * Return:
  * error code.
  */
-int get_operation_params(enum operation_id operation_id,
-			 enum subsystem_id subsystem_id, void *params);
+int get_operation_params(enum operation_id operation_id, unsigned int *ref,
+			 void *params);
+
+/**
+ * get_operation_params_lock() - Get an operation parameters with mutex.
+ * @operation_id: Security Operation ID.
+ * @subsystem_id: Secure Subsystem ID.
+ * @params: Pointer to the data structure describing the parameters.
+ *
+ * This function gets the parameters configured for subsystem's operation.
+ *
+ * Return:
+ * error code.
+ */
+int get_operation_params_lock(enum operation_id operation_id,
+			      enum subsystem_id subsystem_id, void *params);
 
 /**
  * get_operation_func() - Get the Security Operation functions.

commit 6d23cb6090b20a9b2f72632d58aee53d8e5131af

SSMW-799 core: config: Fix check_key_attributes to build modularly
Ensure library can be built removing cryptographic support.
Fix check_key_attributes function.

Fixes: 9348f641e426 ("SSMW-719 core: check key type/algo on generate")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/config/aead.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2023-2024 NXP
+ * Copyright 2023-2025 NXP
  */
 
 #include "smw_config.h"
@@ -272,25 +272,23 @@ end:
 	return status;
 }
 
-static int aead_check_key_usable(unsigned int *ref,
-				 enum smw_config_key_type_id key_type_id,
-				 smw_attr_algo_t permitted_algo)
-{
-	return check_common_key_usable(OPERATION_ID_AEAD, ref, key_type_id,
-				       permitted_algo);
-}
-
-static int
-aead_multi_part_check_key_usable(unsigned int *ref,
-				 enum smw_config_key_type_id key_type_id,
-				 smw_attr_algo_t permitted_algo)
-{
-	return check_common_key_usable(OPERATION_ID_AEAD_MULTI_PART, ref,
-				       key_type_id, permitted_algo);
-}
-
 DEFINE_CONFIG_OPERATION_FUNC(aead);
 DEFINE_CONFIG_OPERATION_FUNC(aead_multi_part);
+
+int aead_key_usable(unsigned int *ref, enum smw_config_key_type_id key_type_id,
+		    struct smw_key_attributes *attributes)
+{
+	int status =
+		check_common_key_usable(OPERATION_ID_AEAD, ref, key_type_id,
+					attributes->permitted_algo);
+
+	if (status == SMW_STATUS_OPERATION_NOT_CONFIGURED)
+		status = check_common_key_usable(OPERATION_ID_AEAD_MULTI_PART,
+						 ref, key_type_id,
+						 attributes->permitted_algo);
+
+	return status;
+}
 
 __export enum smw_status_code smw_config_check_aead(smw_subsystem_t subsystem,
 						    struct smw_aead_info *info)

core/config/cipher.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021, 2023-2024 NXP
+ * Copyright 2021, 2023-2025 NXP
  */
 
 #include "smw_config.h"
@@ -276,25 +276,24 @@ end:
 	return status;
 }
 
-static int cipher_check_key_usable(unsigned int *ref,
-				   enum smw_config_key_type_id key_type_id,
-				   smw_attr_algo_t permitted_algo)
-{
-	return check_common_key_usable(OPERATION_ID_CIPHER, ref, key_type_id,
-				       permitted_algo);
-}
-
-static int
-cipher_multi_part_check_key_usable(unsigned int *ref,
-				   enum smw_config_key_type_id key_type_id,
-				   smw_attr_algo_t permitted_algo)
-{
-	return check_common_key_usable(OPERATION_ID_CIPHER, ref, key_type_id,
-				       permitted_algo);
-}
-
 DEFINE_CONFIG_OPERATION_FUNC(cipher);
 DEFINE_CONFIG_OPERATION_FUNC(cipher_multi_part);
+
+int cipher_key_usable(unsigned int *ref,
+		      enum smw_config_key_type_id key_type_id,
+		      struct smw_key_attributes *attributes)
+{
+	int status =
+		check_common_key_usable(OPERATION_ID_CIPHER, ref, key_type_id,
+					attributes->permitted_algo);
+
+	if (status == SMW_STATUS_OPERATION_NOT_CONFIGURED)
+		status = check_common_key_usable(OPERATION_ID_CIPHER_MULTI_PART,
+						 ref, key_type_id,
+						 attributes->permitted_algo);
+
+	return status;
+}
 
 __export enum smw_status_code
 smw_config_check_cipher(smw_subsystem_t subsystem, struct smw_cipher_info *info)

core/config/common.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2024 NXP
+ * Copyright 2020-2025 NXP
  */
 
 #ifndef __COMMON_H__
@@ -32,7 +32,6 @@ enum subsystem_state { SUBSYSTEM_STATE_UNLOADED, SUBSYSTEM_STATE_LOADED };
 		.merge = operation##_merge_params,                             \
 		.print = operation##_print_params,                             \
 		.check_subsystem_caps = operation##_check_subsystem_caps,      \
-		.check_key_usable = operation##_check_key_usable               \
 	};                                                                     \
 	struct operation_func *smw_##operation##_get_func(void)                \
 	{                                                                      \

core/config/device.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2023-2024 NXP
+ * Copyright 2023-2025 NXP
  */
 
 #include "smw_status.h"
@@ -59,18 +59,6 @@ static int device_attestation_check_subsystem_caps(void *args, void *node)
 	return SMW_STATUS_OK;
 }
 
-static int
-device_attestation_check_key_usable(unsigned int *ref,
-				    enum smw_config_key_type_id key_type_id,
-				    smw_attr_algo_t permitted_algo)
-{
-	(void)ref;
-	(void)key_type_id;
-	(void)permitted_algo;
-
-	return SMW_STATUS_OK;
-}
-
 DEFINE_CONFIG_OPERATION_FUNC(device_attestation);
 
 static int device_lifecycle_read_params(char **start, char *end, void **params)
@@ -118,18 +106,6 @@ static int device_lifecycle_check_subsystem_caps(void *args, void *node)
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, SMW_STATUS_OK);
-	return SMW_STATUS_OK;
-}
-
-static int
-device_lifecycle_check_key_usable(unsigned int *ref,
-				  enum smw_config_key_type_id key_type_id,
-				  smw_attr_algo_t permitted_algo)
-{
-	(void)ref;
-	(void)key_type_id;
-	(void)permitted_algo;
-
 	return SMW_STATUS_OK;
 }
 
@@ -181,18 +157,6 @@ static int device_reprovision_check_subsystem_caps(void *args, void *node)
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, SMW_STATUS_OK);
-	return SMW_STATUS_OK;
-}
-
-static int
-device_reprovision_check_key_usable(unsigned int *ref,
-				    enum smw_config_key_type_id key_type_id,
-				    smw_attr_algo_t permitted_algo)
-{
-	(void)ref;
-	(void)key_type_id;
-	(void)permitted_algo;
-
 	return SMW_STATUS_OK;
 }
 

core/config/hash.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2021, 2023-2024 NXP
+ * Copyright 2020-2021, 2023-2025 NXP
  */
 
 #include "smw_status.h"
@@ -167,32 +167,6 @@ static int hash_check_subsystem_caps(void *args, void *node)
 static int hash_multi_part_check_subsystem_caps(void *args, void *node)
 {
 	return hash_common_check_subsystem_caps(args, node);
-}
-
-static int hash_common_check_key_usable(unsigned int *ref,
-					enum smw_config_key_type_id key_type_id,
-					smw_attr_algo_t permitted_algo)
-{
-	(void)ref;
-	(void)key_type_id;
-	(void)permitted_algo;
-
-	return SMW_STATUS_OK;
-}
-
-static int hash_check_key_usable(unsigned int *ref,
-				 enum smw_config_key_type_id key_type_id,
-				 smw_attr_algo_t permitted_algo)
-{
-	return hash_common_check_key_usable(ref, key_type_id, permitted_algo);
-}
-
-static int
-hash_multi_part_check_key_usable(unsigned int *ref,
-				 enum smw_config_key_type_id key_type_id,
-				 smw_attr_algo_t permitted_algo)
-{
-	return hash_common_check_key_usable(ref, key_type_id, permitted_algo);
 }
 
 DEFINE_CONFIG_OPERATION_FUNC(hash);

core/config/keymgr.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2024 NXP
+ * Copyright 2020-2025 NXP
  */
 
 #include "smw_config.h"
@@ -278,9 +278,9 @@ static int derive_key_read_params(char **start, char *end, void **params)
 	return read_params(start, end, OPERATION_ID_DERIVE_KEY, params);
 }
 
-static int derive_check_key_usable(unsigned int *ref,
-				   enum smw_config_key_type_id key_type_id,
-				   smw_attr_algo_t permitted_algo)
+static int derive_key_usable(unsigned int *ref,
+			     enum smw_config_key_type_id key_type_id,
+			     struct smw_key_attributes *attributes)
 {
 	int status = SMW_STATUS_OK;
 
@@ -294,7 +294,7 @@ static int derive_check_key_usable(unsigned int *ref,
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	algo = SMW_ATTR_GET_ALGO(permitted_algo);
+	algo = SMW_ATTR_GET_ALGO(attributes->permitted_algo);
 
 	status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
 
@@ -373,24 +373,48 @@ static int check_key_descriptor(struct smw_keymgr_descriptor *key_descriptor,
 	return status;
 }
 
-static int check_key_usable(enum operation_id operation_id, unsigned int ref,
-			    enum smw_config_key_type_id key_type_id,
-			    smw_attr_algo_t permitted_algo)
+__weak int cipher_key_usable(unsigned int *ref,
+			     enum smw_config_key_type_id key_type_id,
+			     struct smw_key_attributes *attributes)
 {
-	int status = SMW_STATUS_OK;
-	struct operation_func *op_func = NULL;
+	(void)ref;
+	(void)key_type_id;
+	(void)attributes;
 
-	SMW_DBG_TRACE_FUNCTION_CALL;
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
 
-	op_func = get_operation_func(operation_id);
-	SMW_DBG_ASSERT(op_func);
+__weak int sign_key_usable(unsigned int *ref,
+			   enum smw_config_key_type_id key_type_id,
+			   struct smw_key_attributes *attributes)
+{
+	(void)ref;
+	(void)key_type_id;
+	(void)attributes;
 
-	SMW_DBG_ASSERT(op_func->check_key_usable);
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
 
-	status = op_func->check_key_usable(&ref, key_type_id, permitted_algo);
+__weak int aead_key_usable(unsigned int *ref,
+			   enum smw_config_key_type_id key_type_id,
+			   struct smw_key_attributes *attributes)
+{
+	(void)ref;
+	(void)key_type_id;
+	(void)attributes;
 
-	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
-	return status;
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
+__weak int mac_key_usable(unsigned int *ref,
+			  enum smw_config_key_type_id key_type_id,
+			  struct smw_key_attributes *attributes)
+{
+	(void)ref;
+	(void)key_type_id;
+	(void)attributes;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
 }
 
 static int check_key_attributes(struct smw_keymgr_descriptor *key_desc,
@@ -436,48 +460,28 @@ static int check_key_attributes(struct smw_keymgr_descriptor *key_desc,
 
 	switch (class) {
 	case SMW_ATTR_CLASS_SYMMETRIC_ENCRYPTION:
-		status = check_key_usable(OPERATION_ID_CIPHER, ref,
-					  key_desc->identifier.type_id,
-					  attributes->permitted_algo);
-		if (status == SMW_STATUS_OPERATION_NOT_SUPPORTED)
-			status =
-				check_key_usable(OPERATION_ID_CIPHER_MULTI_PART,
-						 ref,
-						 key_desc->identifier.type_id,
-						 attributes->permitted_algo);
+		status = cipher_key_usable(&ref, key_desc->identifier.type_id,
+					   attributes);
 		break;
 
 	case SMW_ATTR_CLASS_ASYMMETRIC_SIGNATURE:
-		status = check_key_usable(OPERATION_ID_SIGN, ref,
-					  key_desc->identifier.type_id,
-					  attributes->permitted_algo);
-		if (status == SMW_STATUS_OPERATION_NOT_SUPPORTED)
-			status = check_key_usable(OPERATION_ID_VERIFY, ref,
-						  key_desc->identifier.type_id,
-						  attributes->permitted_algo);
+		status = sign_key_usable(&ref, key_desc->identifier.type_id,
+					 attributes);
 		break;
 
 	case SMW_ATTR_CLASS_AEAD:
-		status = check_key_usable(OPERATION_ID_AEAD, ref,
-					  key_desc->identifier.type_id,
-					  attributes->permitted_algo);
-		if (status == SMW_STATUS_OPERATION_NOT_SUPPORTED)
-			status = check_key_usable(OPERATION_ID_AEAD_MULTI_PART,
-						  ref,
-						  key_desc->identifier.type_id,
-						  attributes->permitted_algo);
+		status = aead_key_usable(&ref, key_desc->identifier.type_id,
+					 attributes);
 		break;
 
 	case SMW_ATTR_CLASS_MAC:
-		status = check_key_usable(OPERATION_ID_MAC, ref,
-					  key_desc->identifier.type_id,
-					  attributes->permitted_algo);
+		status = mac_key_usable(&ref, key_desc->identifier.type_id,
+					attributes);
 		break;
 
 	case SMW_ATTR_CLASS_KEY_DERIVATION:
-		status = derive_check_key_usable(&ref,
-						 key_desc->identifier.type_id,
-						 attributes->permitted_algo);
+		status = derive_key_usable(&ref, key_desc->identifier.type_id,
+					   attributes);
 		break;
 
 	case SMW_ATTR_CLASS_ASYMMETRIC_ENCRYPTION:

core/config/mac.c
@@ -153,9 +153,10 @@ static int mac_check_subsystem_caps(void *args, void *node)
 	return status;
 }
 
-static int mac_check_key_usable(unsigned int *ref,
-				enum smw_config_key_type_id key_type_id,
-				smw_attr_algo_t permitted_algo)
+DEFINE_CONFIG_OPERATION_FUNC(mac);
+
+int mac_key_usable(unsigned int *ref, enum smw_config_key_type_id key_type_id,
+		   struct smw_key_attributes *attributes)
 {
 	int status = SMW_STATUS_OK;
 	struct mac_params params = { 0 };
@@ -169,13 +170,13 @@ static int mac_check_key_usable(unsigned int *ref,
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	mode = SMW_ATTR_GET_MODE(permitted_algo);
+	mode = SMW_ATTR_GET_MODE(attributes->permitted_algo);
 	if (mode == SMW_ATTR_MODE_NONE || mode == SMW_ATTR_MODE_ANY) {
 		status = SMW_STATUS_OK;
 		goto end;
 	}
 
-	algo = SMW_ATTR_GET_ALGO(permitted_algo);
+	algo = SMW_ATTR_GET_ALGO(attributes->permitted_algo);
 	status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
 
 	if (!check_id(key_type_id, params.key.type_bitmap))
@@ -194,8 +195,6 @@ end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }
-
-DEFINE_CONFIG_OPERATION_FUNC(mac);
 
 __export enum smw_status_code smw_config_check_mac(smw_subsystem_t subsystem,
 						   struct smw_mac_info *info)

core/config/rng.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021, 2023-2024 NXP
+ * Copyright 2021, 2023-2025 NXP
  */
 
 #include "smw_status.h"
@@ -108,17 +108,6 @@ static int rng_check_subsystem_caps(void *args, void *node)
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
-}
-
-static int rng_check_key_usable(unsigned int *ref,
-				enum smw_config_key_type_id key_type_id,
-				smw_attr_algo_t permitted_algo)
-{
-	(void)ref;
-	(void)key_type_id;
-	(void)permitted_algo;
-
-	return SMW_STATUS_OK;
 }
 
 DEFINE_CONFIG_OPERATION_FUNC(rng);

core/config/sign_verify.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2021, 2023-2024 NXP
+ * Copyright 2020-2021, 2023-2025 NXP
  */
 
 #include "smw_status.h"
@@ -346,24 +346,56 @@ end:
 	return status;
 }
 
-static int sign_check_key_usable(unsigned int *ref,
-				 enum smw_config_key_type_id key_type_id,
-				 smw_attr_algo_t permitted_algo)
-{
-	return check_common_key_usable(OPERATION_ID_SIGN, ref, key_type_id,
-				       permitted_algo);
-}
-
-static int verify_check_key_usable(unsigned int *ref,
-				   enum smw_config_key_type_id key_type_id,
-				   smw_attr_algo_t permitted_algo)
-{
-	return check_common_key_usable(OPERATION_ID_VERIFY, ref, key_type_id,
-				       permitted_algo);
-}
-
 DEFINE_CONFIG_OPERATION_FUNC(sign);
 DEFINE_CONFIG_OPERATION_FUNC(verify);
+
+int sign_key_usable(unsigned int *ref, enum smw_config_key_type_id key_type_id,
+		    struct smw_key_attributes *attributes)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+	bool check_sign = false;
+	bool check_verify = false;
+	smw_attr_usage_t usages = attributes->usage_flags;
+	smw_attr_usage_t mask_usages =
+		SMW_ATTR_USAGE_CACHE | SMW_ATTR_USAGE_COPY |
+		SMW_ATTR_USAGE_EXPORT | SMW_ATTR_USAGE_DERIVE;
+
+	/*
+	 * If key usage is not set check both sign and verify operations,
+	 * else check operation(s) corresponding to the key usage set.
+	 */
+	CLEAR_BITS(usages, mask_usages);
+
+	if (usages) {
+		if (SMW_ATTR_USAGE_IS_SIGN_MESSAGE(usages) ||
+		    SMW_ATTR_USAGE_IS_SIGN_HASH(usages))
+			check_sign = true;
+
+		if (SMW_ATTR_USAGE_IS_VERIFY_MESSAGE(usages) ||
+		    SMW_ATTR_USAGE_IS_VERIFY_HASH(usages))
+			check_verify = true;
+	} else if (!usages) {
+		check_sign = true;
+		check_verify = true;
+	}
+
+	if (check_sign) {
+		status = check_common_key_usable(OPERATION_ID_SIGN, ref,
+						 key_type_id,
+						 attributes->permitted_algo);
+
+		if (usages && status == SMW_STATUS_OPERATION_NOT_SUPPORTED)
+			goto end;
+	}
+
+	if (check_verify)
+		status = check_common_key_usable(OPERATION_ID_VERIFY, ref,
+						 key_type_id,
+						 attributes->permitted_algo);
+
+end:
+	return status;
+}
 
 int smw_config_get_signature_algo_id(smw_signature_algo_t name,
 				     enum smw_config_sign_algo_id *id)

core/config/storage.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2023-2024 NXP
+ * Copyright 2023-2025 NXP
  */
 
 #include "smw_status.h"
@@ -253,42 +253,6 @@ static int storage_delete_check_subsystem_caps(void *args, void *node)
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, SMW_STATUS_OK);
-	return SMW_STATUS_OK;
-}
-
-static int
-storage_store_check_key_usable(unsigned int *ref,
-			       enum smw_config_key_type_id key_type_id,
-			       smw_attr_algo_t permitted_algo)
-{
-	(void)ref;
-	(void)key_type_id;
-	(void)permitted_algo;
-
-	return SMW_STATUS_OK;
-}
-
-static int
-storage_retrieve_check_key_usable(unsigned int *ref,
-				  enum smw_config_key_type_id key_type_id,
-				  smw_attr_algo_t permitted_algo)
-{
-	(void)ref;
-	(void)key_type_id;
-	(void)permitted_algo;
-
-	return SMW_STATUS_OK;
-}
-
-static int
-storage_delete_check_key_usable(unsigned int *ref,
-				enum smw_config_key_type_id key_type_id,
-				smw_attr_algo_t permitted_algo)
-{
-	(void)ref;
-	(void)key_type_id;
-	(void)permitted_algo;
-
 	return SMW_STATUS_OK;
 }
 
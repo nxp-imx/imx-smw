nxp-imx/imx-smw

commit 36f420e8c02088346ae0609c31577a9a4e7db30e

SSMW-498 core: devmgr: Implement device reprovisioning APIs
Implementation of the device storage reprovisioing APIs.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/devmgr/reprovision.c
@@ -1,0 +1,113 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2024 NXP
+ */
+
+#include "smw_device.h"
+
+#include "config.h"
+#include "debug.h"
+#include "devmgr.h"
+#include "exec.h"
+#include "subsystems.h"
+
+unsigned char *smw_devmgr_get_reprovision_data(struct smw_devmgr_args *args)
+{
+	unsigned char *buffer = NULL;
+
+	if ((args->op == SMW_OP_DEVMGR_REPROVISION_PREP ||
+	     args->op == SMW_OP_DEVMGR_REPROVISION) &&
+	    args->pub.reprovision)
+		buffer = args->pub.reprovision->data;
+
+	return buffer;
+}
+
+unsigned int smw_devmgr_get_reprovision_length(struct smw_devmgr_args *args)
+{
+	unsigned int length = 0;
+
+	if ((args->op == SMW_OP_DEVMGR_REPROVISION_PREP ||
+	     args->op == SMW_OP_DEVMGR_REPROVISION) &&
+	    args->pub.reprovision)
+		length = args->pub.reprovision->data_length;
+
+	return length;
+}
+
+void smw_devmgr_set_reprovision_length(struct smw_devmgr_args *args,
+				       unsigned int length)
+{
+	if ((args->op == SMW_OP_DEVMGR_REPROVISION_PREP ||
+	     args->op == SMW_OP_DEVMGR_REPROVISION) &&
+	    args->pub.reprovision)
+		args->pub.reprovision->data_length = length;
+}
+
+enum smw_status_code
+smw_device_reprovision_prepare(struct smw_device_reprovision_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_devmgr_args repro_args = { 0 };
+	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!args)
+		goto end;
+
+	if (args->version != 0) {
+		status = SMW_STATUS_INVALID_VERSION;
+		goto end;
+	}
+
+	status = smw_config_get_subsystem_id(args->subsystem_name,
+					     &subsystem_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	repro_args.op = SMW_OP_DEVMGR_REPROVISION_PREP;
+	repro_args.pub.reprovision = args;
+
+	status = smw_utils_execute_operation(OPERATION_ID_DEVICE_REPROVISION,
+					     &repro_args, subsystem_id);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+enum smw_status_code
+smw_device_reprovision(struct smw_device_reprovision_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_devmgr_args repro_args = { 0 };
+	enum subsystem_id subsystem_id = SUBSYSTEM_ID_INVALID;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!args)
+		goto end;
+
+	if (args->version != 0) {
+		status = SMW_STATUS_INVALID_VERSION;
+		goto end;
+	}
+
+	status = smw_config_get_subsystem_id(args->subsystem_name,
+					     &subsystem_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	repro_args.op = SMW_OP_DEVMGR_REPROVISION;
+	repro_args.pub.reprovision = args;
+
+	status = smw_utils_execute_operation(OPERATION_ID_DEVICE_REPROVISION,
+					     &repro_args, subsystem_id);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}

core/devmgr/CMakeLists.txt
@@ -13,13 +13,18 @@ if(ENABLE_DEVICE_LIFECYCLE)
    add_operation(DEVICE_LIFECYCLE)
 endif()
 
+if(ENABLE_DEVICE_REPROVISION)
+   add_operation(DEVICE_REPROVISION)
+endif()
+
 add_library(devmgr OBJECT "")
 target_sources(devmgr PRIVATE
                devmgr_weak.c
                lifecycle_weak.c
                $<$<BOOL:${ENABLE_DEVMGR_MODULE}>:devmgr.c>
                $<$<BOOL:${ENABLE_DEVICE_ATTESTATION}>:attestation.c>
-               $<$<BOOL:${ENABLE_DEVICE_LIFECYCLE}>:lifecycle.c>)
+               $<$<BOOL:${ENABLE_DEVICE_LIFECYCLE}>:lifecycle.c>
+               $<$<BOOL:${ENABLE_DEVICE_REPROVISION}>:reprovision.c>)
 
 target_include_directories(devmgr PRIVATE ${INCLUDE_DIR})
 

core/devmgr/devmgr_weak.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2023 NXP
+ * Copyright 2023-2024 NXP
  */
 
 #include "smw_device.h"
@@ -44,6 +44,26 @@ smw_device_attestation(struct smw_device_attestation_args *args)
 
 __weak enum smw_status_code
 smw_device_get_uuid(struct smw_device_uuid_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
+__weak enum smw_status_code
+smw_device_reprovision_prepare(struct smw_device_reprovision_args *args)
+{
+	(void)args;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
+}
+
+__weak enum smw_status_code
+smw_device_reprovision(struct smw_device_reprovision_args *args)
 {
 	(void)args;
 

core/inc/devmgr.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2023 NXP
+ * Copyright 2023-2024 NXP
  */
 
 #ifndef __DEVMGR_H___
@@ -12,7 +12,9 @@ enum smw_op_devmgr {
 	SMW_OP_DEVMGR_ATTESTATION,
 	SMW_OP_DEVMGR_UUID,
 	SMW_OP_DEVMGR_SET_LIFECYCLE,
-	SMW_OP_DEVMGR_GET_LIFECYCLE
+	SMW_OP_DEVMGR_GET_LIFECYCLE,
+	SMW_OP_DEVMGR_REPROVISION_PREP,
+	SMW_OP_DEVMGR_REPROVISION
 };
 
 /**
@@ -26,6 +28,7 @@ struct smw_devmgr_args {
 	union {
 		struct smw_device_attestation_args *attestation;
 		struct smw_device_uuid_args *uuid;
+		struct smw_device_reprovision_args *reprovision;
 	} pub;
 };
 
@@ -140,4 +143,46 @@ unsigned int smw_devmgr_get_uuid_length(struct smw_devmgr_args *args);
  */
 void smw_devmgr_set_uuid_length(struct smw_devmgr_args *args,
 				unsigned int length);
+
+/**
+ * smw_devmgr_get_reprovisioning_data() - Return the address of the
+ *                                        reprovisioning buffer.
+ * @args: Pointer to the internal device args structure.
+ *
+ * This function returns the address of the device storage reprovisioning
+ * buffer.
+ *
+ * Return:
+ * NULL
+ * address of the device reprovisioning buffer
+ */
+unsigned char *smw_devmgr_get_reprovision_data(struct smw_devmgr_args *args);
+
+/**
+ * smw_devmgr_get_reprovisiong_length() - Return the length of the device
+ *                                        reprovisioning buffer.
+ * @args: Pointer to the internal device args structure.
+ *
+ * This function returns the length of the device storage reprovisioning buffer.
+ *
+ * Return:
+ * 0
+ * length of the device reprovisioning buffer.
+ */
+unsigned int smw_devmgr_get_reprovision_length(struct smw_devmgr_args *args);
+
+/**
+ * smw_devmgr_set_reprovisiong_length() - Set the length of the device
+ *                                        reprovisioning buffer.
+ * @args: Pointer to the internal device args structure.
+ * @length: Length of the reprovisioning buffer.
+ *
+ * This function sets the length of the device storage reprovisioning buffer.
+ *
+ * Return:
+ * None.
+ */
+void smw_devmgr_set_reprovision_length(struct smw_devmgr_args *args,
+				       unsigned int length);
+
 #endif /* __DEVMGR_H___ */

commit 09d3c120ffb6010160944201dfb2023132a3c298

SSMW-862 core: devmgr: Return correct status code
If the arguments version is not supported,
the device manager returns SMW_STATUS_VERSION_NOT_SUPPORTED.

Fixes: 36f420e8c020 ("SSMW-498 core: devmgr: Implement device reprovisioning APIs")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/devmgr/reprovision.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2024 NXP
+ * Copyright 2024-2025 NXP
  */
 
 #include "smw_device.h"
@@ -58,7 +58,7 @@ smw_device_reprovision_prepare(struct smw_device_reprovision_args *args)
 		goto end;
 
 	if (args->version != 0) {
-		status = SMW_STATUS_INVALID_VERSION;
+		status = SMW_STATUS_VERSION_NOT_SUPPORTED;
 		goto end;
 	}
 
@@ -92,7 +92,7 @@ smw_device_reprovision(struct smw_device_reprovision_args *args)
 		goto end;
 
 	if (args->version != 0) {
-		status = SMW_STATUS_INVALID_VERSION;
+		status = SMW_STATUS_VERSION_NOT_SUPPORTED;
 		goto end;
 	}
 
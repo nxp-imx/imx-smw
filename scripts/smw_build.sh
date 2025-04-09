nxp-imx/imx-smw

commit 36d27ee0de4f9f1c963be2c3bcaad898788a1a21

SSMW-174 scripts: add code coverage build and lava
Add code coverage build in te build script.
Add code coverage in lava definition and execution.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

scripts/smw_build.sh
@@ -2,17 +2,77 @@
 set -eE
 trap 'error ${LINENO}' ERR
 
+#
+# Default build options
+#
+opt_build=all
+opt_jsonc_lib=
+opt_coverage="-DCODE_COVERAGE=OFF"
+opt_buildtype="-DCMAKE_BUILD_TYPE=Release"
+opt_verbose="-DVERBOSE=0"
+
+#
+# Get script name and path
+#
 script_name=$0
 script_full=$(realpath "${script_name}")
 script_dir=$(dirname "${script_full}")
 
+function pr_err()
+{
+    printf "\033[1;31m\n"
+    printf "%s" "$@"
+    printf "\033[0m\n"
+}
+
+function get_cmakecache()
+{
+    local pattern="$2"
+
+    while read -r line
+    do
+        case $line in
+            ${pattern}*)
+                IFS='=' read -ra split_line <<< "$line"
+                break
+                ;;
+        esac
+    done < CMakeCache.txt
+
+    if [[ ${#split_line[@]} -ge 2 ]]; then
+        eval "$1=${split_line[1]}"
+    else
+        pr_err "${pattern} not found in CMakeCache.txt"
+    fi
+}
+
+function get_cmakecache_err()
+{
+    get_cmakecache "$1" "$2"
+
+    if [[ -z "${!1}" ]]; then
+         exit 1
+    fi
+}
+
+function check_directory()
+{
+    declare -n mydir=$1
+    mydir="${mydir/\~/$HOME}"
+
+    if [[ ! -d "${mydir}" ]]; then
+        pr_err "${mydir} is not a directory"
+    fi
+}
+
 function toolchain()
 {
-    cmd_script="cmake -DFORCE_TOOLCHAIN_INSTALL=True ${opt_toolpath} ${opt_toolname}"
+    cmd_script="cmake -DFORCE_TOOLCHAIN_INSTALL=True"
+    cmd_script="${cmd_script} ${opt_toolpath} ${opt_toolname}"
 
     printf "\033[0;32m\n"
     printf "***************************************\n"
-    printf " Install toolchain %s \n" "${opt_arch}"
+    printf " Install toolchain %s\n" "${opt_arch}"
     printf "***************************************\n"
     printf "\033[0m\n"
 
@@ -28,7 +88,7 @@ function zlib()
 
     printf "\033[0;32m\n"
     printf "***************************************\n"
-    printf " Install zlib to %s \n" "${opt_export}"
+    printf " Install zlib to %s\n" "${opt_export}"
     printf "***************************************\n"
     printf "\033[0m\n"
 
@@ -54,7 +114,7 @@ function seco()
 
     printf "\033[0;32m\n"
     printf "***************************************\n"
-    printf " Install SECO to %s \n" "${opt_export}"
+    printf " Install SECO to %s\n" "${opt_export}"
     printf "***************************************\n"
     printf "\033[0m\n"
 
@@ -63,7 +123,8 @@ function seco()
         exit 1
     fi
 
-    cmd_script="${cmd_script} -DSECO_SRC_PATH=${opt_src} -DSECO_ROOT=${opt_export} -P ${seco_script}"
+    cmd_script="${cmd_script} -DSECO_SRC_PATH=${opt_src}"
+    cmd_script="${cmd_script} -DSECO_ROOT=${opt_export} -P ${seco_script}"
 
     printf "Execute %s\n" "${cmd_script}"
     eval "${cmd_script}"
@@ -76,7 +137,7 @@ function teec()
 
     printf "\033[0;32m\n"
     printf "***************************************\n"
-    printf " Install OPTEE Client to %s \n" "${opt_export}"
+    printf " Install OPTEE Client to %s\n" "${opt_export}"
     printf "***************************************\n"
     printf "\033[0m\n"
 
@@ -85,7 +146,8 @@ function teec()
         exit 1
     fi
 
-    cmd_script="${cmd_script} -DTEEC_SRC_PATH=${opt_src} -DTEEC_ROOT=${opt_export} -P ${teec_script}"
+    cmd_script="${cmd_script} -DTEEC_SRC_PATH=${opt_src}"
+    cmd_script="${cmd_script} -DTEEC_ROOT=${opt_export} -P ${teec_script}"
 
     printf "Execute %s\n" "${cmd_script}"
     eval "${cmd_script}"
@@ -98,7 +160,7 @@ function tadevkit()
 
     printf "\033[0;32m\n"
     printf "***************************************************\n"
-    printf " Install OPTEE TA Development Kit to %s \n" "${opt_export}"
+    printf " Install OPTEE TA Development Kit to %s\n" "${opt_export}"
     printf "***************************************************\n"
     printf "\033[0m\n"
 
@@ -107,41 +169,151 @@ function tadevkit()
         exit 1
     fi
 
-    cmd_script="${cmd_script} -DOPTEE_OS_SRC_PATH=${opt_src} -DTA_DEV_KIT_ROOT=${opt_export} -P ${tadevkit_script}"
+    cmd_script="${cmd_script} -DOPTEE_OS_SRC_PATH=${opt_src}"
+    cmd_script="${cmd_script} -DTA_DEV_KIT_ROOT=${opt_export}"
+    cmd_script="${cmd_script} -P ${tadevkit_script}"
 
     printf "Execute %s\n" "${cmd_script}"
     eval "${cmd_script}"
 }
 
-function smw()
+function configure()
 {
     printf "\033[0;32m\n"
     printf "***************************************\n"
-    printf " Build SMW to %s \n" "${opt_out}"
+    printf " Configure SMW to %s\n" "${opt_out}"
     printf "***************************************\n"
     printf "\033[0m\n"
 
+    if [[ -z ${opt_out} ]]; then
+        usage_configure
+        exit 1
+    fi
+
     cmd_script="cmake .. ${opt_toolchain}"
+    cmd_script="${cmd_script} ${opt_coverage}"
     cmd_script="${cmd_script} ${opt_buildtype} ${opt_verbose}"
     cmd_script="${cmd_script} ${opt_zlib} ${opt_seco}"
     cmd_script="${cmd_script} ${opt_teec} ${opt_tadevkit}"
-    cmd_script="${cmd_script} ${opt_test} ${opt_jsonc}"
+    cmd_script="${cmd_script} ${opt_jsonc}"
 
     mkdir -p "${opt_out}"
     cd "${opt_out}"
     printf "Execute %s\n" "${cmd_script}"
     eval "${cmd_script}"
-    eval "make"
-    if ${opt_package}; then
-        package_name="libsmw_package.tar.gz"
-        tmp_install_dir="tmp_install"
+}
 
-        mkdir -p "${tmp_install_dir}"
-        eval "make install DESTDIR=${tmp_install_dir} > /dev/null"
-        eval "cp -P ../${opt_jsonc_lib}/libjson-c.so.* ${tmp_install_dir}/usr/lib/."
-        eval "cd ${tmp_install_dir} && tar -czf ../${package_name} . && cd .."
-        rm -rf "${tmp_install_dir}"
+function build_tests()
+{
+    if [[ ! -z ${opt_jsonc} ]]; then
+        eval "cmake .. ${opt_jsonc}"
+    else
+        get_cmakecache_err opt_jsonc_lib "JSONC_LIBRARY"
     fi
+
+    eval "make build_tests"
+}
+
+function build()
+{
+    printf "\033[0;32m\n"
+    printf "***************************************\n"
+    printf " Build SMW (%s) to %s\n" "${opt_build}" "${opt_out}"
+    printf "***************************************\n"
+    printf "\033[0m\n"
+
+    if [[ -z ${opt_out} ]]; then
+        usage_build
+        exit 1
+    fi
+
+    cd "${opt_out}"
+
+    case ${opt_build} in
+        all)
+            eval "make"
+            eval "make smw_pkcs11"
+            build_tests
+            ;;
+        smw)
+            eval "make"
+            ;;
+        pkcs11)
+            eval "make smw_pkcs11"
+            ;;
+        tests)
+            build_tests
+            ;;
+        *)
+            pr_err "Unknwon build option: \"${opt_build}\""
+            usage_build
+            exit 1
+            ;;
+    esac
+}
+
+function install()
+{
+    printf "\033[0;32m\n"
+    printf "***************************************\n"
+    printf " Install SMW to %s\n" "${opt_dest}"
+    printf "***************************************\n"
+    printf "\033[0m\n"
+
+    if [[ -z ${opt_out} ]]; then
+        usage_install
+        exit 1
+    fi
+
+    cd "${opt_out}"
+
+    get_cmakecache opt_jsonc_lib "JSONC_LIBRARY"
+
+    cmd_script=""
+    if [[ ! -z ${opt_dest} ]]; then
+        mkdir -p "${opt_dest}"
+        cmd_script="DESTDIR=${opt_dest}"
+    fi
+    eval "make install ${cmd_script}"
+
+    if [[ ! -z ${opt_jsonc_lib} ]]; then
+    	eval "make install_tests ${cmd_script}"
+    fi
+}
+
+function package()
+{
+    local package_name="libsmw_package.tar.gz"
+    local tmp_inst_dir="tmp_install"
+
+    printf "\033[0;32m\n"
+    printf "***************************************\n"
+    printf " Package SMW in %s\n" "${opt_out}/${package_name}"
+    printf "***************************************\n"
+    printf "\033[0m\n"
+
+    if [[ -z ${opt_out} ]]; then
+        usage_package
+        exit 1
+    fi
+
+    cd "${opt_out}"
+
+    get_cmakecache opt_jsonc_lib "JSONC_LIBRARY"
+
+    cmd_script="DESTDIR=${tmp_inst_dir}"
+    eval "make install ${cmd_script}"
+
+    if [[ ! -z ${opt_jsonc_lib} ]]; then
+    	eval "make install_tests ${cmd_script}"
+    fi
+
+    if [[ ! -z ${opt_jsonc_lib} ]]; then
+        eval "cp -P ${opt_jsonc_lib}.* ${tmp_inst_dir}/usr/lib/."
+    fi
+
+    eval "cd ${tmp_inst_dir} && tar -czf ../${package_name} . && cd .."
+    rm -rf "${tmp_inst_dir}"
 }
 
 function jsonc()
@@ -151,7 +323,7 @@ function jsonc()
 
     printf "\033[0;32m\n"
     printf "***************************************\n"
-    printf " Install json-c to %s \n" "${opt_export}"
+    printf " Install json-c to %s\n" "${opt_export}"
     printf "***************************************\n"
     printf "\033[0m\n"
 
@@ -160,10 +332,12 @@ function jsonc()
         exit 1
     fi
 
-    cmd_script="${cmd_script} -DJSONC_SRC_PATH=${opt_src} -DJSONC_ROOT=${opt_export} -P ${jsonc_script}"
+    cmd_script="${cmd_script} -DJSONC_SRC_PATH=${opt_src}"
+    cmd_script="${cmd_script} -DJSONC_ROOT=${opt_export} -P ${jsonc_script}"
 
     if [[ ${opt_version} && ${opt_hash} ]]; then
-        cmd_script="${cmd_script} -DJSONC_VERSION=${opt_version} -DJSONC_HASH=${opt_hash}"
+        cmd_script="${cmd_script} -DJSONC_VERSION=${opt_version}"
+        cmd_script="${cmd_script} -DJSONC_HASH=${opt_hash}"
     fi
 
     printf "Execute %s\n" "${cmd_script}"
@@ -174,7 +348,8 @@ function usage_toolchain()
 {
     printf "\n"
     printf "To install the toolchain aarch32 or aarch64\n"
-    printf "  %s toolchain arch=[arch] toolpath=[dir] toolname=[name]\n" "${script_name}"
+    printf "  %s toolchain arch=[arch] toolpath=[dir] " "${script_name}"
+    printf "toolname=[name]\n"
     printf "    arch     = Toolchain architecture (aarch32|aarch64)\n"
     printf "    toolpath = [optional] Toolchain path where installed\n"
     printf "    toolname = [optional] Toolchain name\n"
@@ -185,7 +360,8 @@ function usage_zlib()
 {
     printf "\n"
     printf "To build and install the ZLIB Library\n"
-    printf "  %s zlib export=[dir] src=[dir] arch=[arch] toolpath=[dir] toolname=[name]\n" "${script_name}"
+    printf "  %s zlib export=[dir] src=[dir] arch=[arch] " "${script_name}"
+    printf "toolpath=[dir] toolname=[name]\n"
     printf "    export   = Export directory\n"
     printf "    src      = [optional] Temporary directory where install sources\n"
     printf "    arch     = [optional] Toolchain architecture (aarch32|aarch64)\n"
@@ -198,7 +374,8 @@ function usage_seco()
 {
     printf "\n"
     printf "To build and install the SECO libraries\n"
-    printf "  %s seco export=[dir] src=[dir] zlib=[root] arch=[arch] toolpath=[dir] toolname=[name]\n" "${script_name}"
+    printf "  %s seco export=[dir] src=[dir] zlib=[root] " "${script_name}"
+    printf "arch=[arch] toolpath=[dir] toolname=[name]\n"
     printf "    export   = Export directory\n"
     printf "    src      = Source directory\n"
     printf "    zlib     = [optional] ZLIB library root directory\n"
@@ -212,7 +389,8 @@ function usage_teec()
 {
     printf "\n"
     printf "To build and install the OPTEE Client libraries\n"
-    printf "  %s teec export=[dir] src=[dir] out=[dir] arch=[arch] toolpath=[dir] toolname=[name]\n" "${script_name}"
+    printf "  %s teec export=[dir] src=[dir] out=[dir] " "${script_name}"
+    printf "arch=[arch] toolpath=[dir] toolname=[name]\n"
     printf "    export   = Export directory\n"
     printf "    src      = Source directory\n"
     printf "    out      = [optional] Build root directory\n"
@@ -226,7 +404,8 @@ function usage_tadevkit()
 {
     printf "\n"
     printf "To build and install the OPTEE TA Development Kit\n"
-    printf "  %s tadevkit export=[dir] src=[dir] out=[dir] platform=[platforn] arch=[arch] toolpath=[dir] toolname=[name]\n" "${script_name}"
+    printf "  %s tadevkit export=[dir] src=[dir] out=[dir] " "${script_name}"
+    printf "platform=[platforn] arch=[arch] toolpath=[dir] toolname=[name]\n"
     printf "    export   = Export directory\n"
     printf "    src      = Source directory\n"
     printf "    out      = [optional] Build root directory\n"
@@ -237,15 +416,17 @@ function usage_tadevkit()
     printf "\n"
 }
 
-function usage_smw()
+function usage_configure()
 {
     printf "\n"
-    printf "To build the Secure Middleware\n"
+    printf "To configure the Secure Middleware\n"
     printf " - Note: all dependencies must be present\n"
-    printf "  %s smw out=[dir] debug package verbose=[lvl] zlib=[dir] seco=[dir] teec=[dir] tadevkit=[dir] arch=[arch] toolpath=[dir] toolname=[name]\n" "${script_name}"
+    printf "  %s configure out=[dir] coverage debug " "${script_name}"
+    printf "verbose=[lvl] zlib=[dir] seco=[dir] teec=[dir] tadevkit=[dir] "
+    printf "arch=[arch] toolpath=[dir] toolname=[name] json=[dir]\n"
     printf "    out      = Build directory\n"
+    printf "    coverage = [optional] if set enable code coverage tool\n"
     printf "    debug    = [optional] if set build type to debug\n"
-    printf "    package  = [optional] if set build package\n"
     printf "    arch     = [optional] Toolchain architecture (aarch32|aarch64)\n"
     printf "    toolpath = [optional] Toolchain path where installed\n"
     printf "    toolname = [optional] Toolchain name\n"
@@ -256,8 +437,31 @@ function usage_smw()
     printf "    teec     = OPTEE Client export directory\n"
     printf "    tadevkit = OPTEE TA Development Kit export directory\n"
     printf "  To enable tests [optionnal]\n"
-    printf "    test\n"
     printf "    jsonc = JSON-C export directory\n"
+    printf "\n"
+}
+
+function usage_build()
+{
+    printf "\n"
+    printf "To build the Secure Middleware\n"
+    printf " - Note: Project must have been configure first\n"
+    printf " (ref. %s configure)\n" "${script_name}"
+    printf "\n"
+    printf "  %s build [option] out=[dir] jsonc=[dir]\n" "${script_name}"
+    printf "    out   = Build directory\n"
+    printf "    jsonc = [optional] JSON-C export directory (tests build)\n"
+    printf "\n"
+    printf "Note:\n"
+    printf "  - If no [option] specified, build all SMW component\n"
+    printf "  - the JSON-C export directory could have been set "
+    printf "while configuring the Security Middleware\n"
+    printf "\n"
+    printf " Option:\n"
+    printf "  smw      Build SMW library only\n"
+    printf "  pkcs11   Build PKCS11 library\n"
+    printf "  tests    Build all tests (smw and pkcs11)\n"
+    printf "  all      Build all projects including pksc11 and tests (default)\n"
     printf "\n"
 }
 
@@ -265,7 +469,8 @@ function usage_jsonc()
 {
     printf "\n"
     printf "To build and install the JSON-C Library\n"
-    printf "  %s jsonc export=[dir] src=[dir] arch=[arch] toolpath=[dir] toolname=[name]\n" "${script_name}"
+    printf "  %s jsonc export=[dir] src=[dir] arch=[arch] " "${script_name}"
+    printf "toolpath=[dir] toolname=[name]\n"
     printf "    export   = Export directory\n"
     printf "    src      = Temporary directory where install sources\n"
     printf "    arch     = [optional] Toolchain architecture (aarch32|aarch64)\n"
@@ -273,6 +478,25 @@ function usage_jsonc()
     printf "    toolname = [optional] Toolchain name\n"
     printf "    version  = [optional] JSON-C Version to upload\n"
     printf "    hash     = [optional] JSON-C Hash of archive to upload\n"
+    printf "\n"
+}
+
+function usage_install()
+{
+    printf "\n"
+    printf "To install the Security Middleware objects\n"
+    printf "  %s install out=[dir] dest=[dir]\n" "${script_name}"
+    printf "    out      = Build directory\n"
+    printf "    dest     = [optional] Installation directory\n"
+    printf "\n"
+}
+
+function usage_package()
+{
+    printf "\n"
+    printf "To package the Security Middleware objects\n"
+    printf "  %s package out=[dir]\n" "${script_name}"
+    printf "    out      = Build directory\n"
     printf "\n"
 }
 
@@ -288,28 +512,30 @@ function usage()
     usage_teec
     usage_tadevkit
     usage_jsonc
-    usage_smw
+    usage_configure
+    usage_build
+    usage_install
+    usage_package
+
+    exit 1
 }
 
 if [[ $# -eq 0 ]]; then
     usage
-    exit 0
 fi
 
-opt_action=$1
-opt_package=false
+opt_action="$1"
 shift
 
 for arg in "$@"
 do
-    case $arg in
+    case ${arg} in
         arch=*)
             opt_arch="${arg#*=}"
             toolchain_script="${script_dir}/${opt_arch}_toolchain.cmake"
             if [[ ! -e "$toolchain_script" ]]; then
-                printf "Unknown toolchain %s \n" "${opt_arch}"
+                pr_err "Unknown toolchain ${opt_arch}"
                 usage
-                exit 1
             fi
 
             opt_toolscript="-DCMAKE_TOOLCHAIN_FILE=${toolchain_script}"
@@ -317,6 +543,7 @@ do
 
         toolpath=*)
             opt_toolpath="${arg#*=}"
+            check_directory opt_toolpath
             opt_toolpath="-DTOOLCHAIN_PATH=${opt_toolpath}"
             ;;
 
@@ -327,14 +554,17 @@ do
 
         export=*)
             opt_export="${arg#*=}"
+            check_directory opt_export
             ;;
 
         src=*)
             opt_src="${arg#*=}"
+            check_directory opt_src
             ;;
 
         zlib=*)
             opt_zlib="${arg#*=}"
+            check_directory opt_zlib
             opt_zlib="-DZLIB_ROOT=${opt_zlib}"
             ;;
 
@@ -348,6 +578,10 @@ do
             opt_builddir="-DBUILD_DIR=${opt_out}"
             ;;
 
+        dest=*)
+            opt_dest="${arg#*=}"
+            ;;
+
         seco=*)
             opt_seco="${arg#*=}"
             opt_seco="-DSECO_ROOT=${opt_seco}"
@@ -355,48 +589,62 @@ do
 
         teec=*)
             opt_teec="${arg#*=}"
+            check_directory opt_teec
             opt_teec="-DTEEC_ROOT=${opt_teec}"
             ;;
 
         tadevkit=*)
-             opt_tadevkit="${arg#*=}"
-             opt_tadevkit="-DTA_DEV_KIT_ROOT=${opt_tadevkit}"
-             ;;
+            opt_tadevkit="${arg#*=}"
+            check_directory opt_tadevkit
+            opt_tadevkit="-DTA_DEV_KIT_ROOT=${opt_tadevkit}"
+            ;;
+
+        coverage)
+            opt_coverage="-DCODE_COVERAGE=ON"
+            ;;
 
         debug)
-             opt_buildtype="-DCMAKE_BUILD_TYPE=Debug"
-             ;;
-
-        package)
-             opt_package=true
-             ;;
+            opt_buildtype="-DCMAKE_BUILD_TYPE=Debug"
+            ;;
 
         verbose=*)
-             opt_verbose="${arg#*=}"
-             opt_verbose="-DVERBOSE=${opt_verbose}"
-             ;;
-
-        test)
-             opt_test="-DBUILD_TEST=ON"
-             ;;
+            opt_verbose="${arg#*=}"
+            opt_verbose="-DVERBOSE=${opt_verbose}"
+            ;;
 
         jsonc=*)
-             opt_jsonc="${arg#*=}"
-             opt_jsonc_lib="${opt_jsonc}/usr/lib"
-             opt_jsonc="-DJSONC_ROOT=${opt_jsonc}"
-             ;;
+            opt_jsonc="${arg#*=}"
+            check_directory opt_jsonc
+            opt_jsonc="-DJSONC_ROOT=${opt_jsonc}"
+            ;;
 
         version=*)
-             opt_version="${arg#*=}"
-             ;;
+            opt_version="${arg#*=}"
+            ;;
 
         hash=*)
-             opt_hash="${arg#*=}"
-             ;;
+            opt_hash="${arg#*=}"
+            ;;
+
+        #
+        # Build option
+        #
+        all)
+            opt_build="all"
+            ;;
+        smw)
+            opt_build="smw"
+            ;;
+        pkcs11)
+            opt_build="pkcs11"
+            ;;
+        tests)
+            opt_build="tests"
+            ;;
 
         *)
+            pr_err "Unknown argument \"${arg}\""
             usage
-            exit 1
             ;;
     esac
 
@@ -405,7 +653,7 @@ done
 
 opt_toolchain="${opt_toolname} ${opt_toolpath} ${opt_toolscript}"
 
-case $opt_action in
+case ${opt_action} in
     toolchain)
         toolchain
         ;;
@@ -426,12 +674,24 @@ case $opt_action in
         tadevkit
         ;;
 
-    smw)
-        smw
+    configure)
+        configure
+        ;;
+
+    build)
+        build
         ;;
 
     jsonc)
         jsonc
+        ;;
+
+    install)
+        install
+        ;;
+
+    package)
+        package
         ;;
 
     *)

scripts/smw_squad.sh
@@ -10,6 +10,7 @@ smw_setup_yaml="smw_setup.yaml"
 smw_package_yaml="smw_package.yaml"
 smw_ctest_yaml="smw_ctest.yaml"
 pkcs11_ctest_yaml="pkcs11_ctest.yaml"
+code_coverage_yaml="code_coverage.yaml"
 
 devops_script="nexus-find-latest-db.sh"
 lavacli_tool="lavacli"
@@ -17,7 +18,7 @@ def_squad_token="b786b334a22cd4734d4f180813f75bb7d725d491"
 
 export PATH=/home/bamboo/.local/bin:$PATH
 
-lava_url="${bamboo_lava_url:-https://lava.sw.nxp.com}"
+lava_url="${bamboo_lava_url:-"https://lava.sw.nxp.com"}"
 lava_backend="${bamboo_lava_backend:-lava-https}"
 lava_token="${bamboo_lava_token_secret:-}"
 lava_user="${bamboo_lava_user:-squad-mougins}"
@@ -30,8 +31,12 @@ prefix_boot="${bamboo_prefix_boot:-"imx-boot-"}"
 suffix_boot="${bamboo_suffix_boot:-"-sd.bin-flash_spl"}"
 image_folder="${bamboo_image_folder:-"fsl-imx-internal-wayland"}"
 nexus_repo="${bamboo_nexus_repo:-"IMX-raw_Linux_Internal_Daily_Build"}"
+nexus_url="${bamboo_nexus_url:-"https://nl-nxrm.sw.nxp.com/repository"}"
+nexus_dir="${bamboo_nexus_lava_dir:-"mougins-raw-public-artifacts/LAVA/smw"}"
+bamboo_plan="${bamboo_planKey:-"misc"}"
 
-use_new_lava=0
+opt_coverage=0
+ctest_label=""
 
 function usage_lavacli()
 {
@@ -50,12 +55,17 @@ function usage_submit()
 {
     printf "\n"
     printf "To submit a squad job\n"
-    printf "  %s [action] platform script_dir yaml_dir squad_id package_url=[url] ctest_label=[label] job_name=[name]\n" "${script_name}"
-    printf "    action      = [submit] boot with flashing MMC or [submit_uuu] boot with UUU\n"
+    printf "  %s [action] platform script_dir yaml_dir"  "${script_name}"
+    printf " squad_id coverage package_url=[url] ctest_label=[label]"
+    printf " job_name=[name]\n"
+    printf "    action      = [submit] boot with flashing MMC or [submit_uuu]"
+    printf " boot with UUU\n"
     printf "    platform    = Platform name\n"
     printf "    script_dir  = Directory where %s is located\n" "${devops_script}"
-    printf "    yaml_dir    = Directory where Lava jobs descriptions for Security Middleware are located\n"
+    printf "    yaml_dir    = Directory where Lava jobs descriptions for"
+    printf " Security Middleware are located\n"
     printf "    squad_id    = Suffix of squad tests, can contains more than one job\n"
+    printf "    coverage    = [optional} Upload Code Coverage result\n"
     printf "    package_url = [optional] URL to custom Security Middleware package\n"
     printf "    ctest_label = [optional] CTest label\n"
     printf "    job_name    = [optional] Job name added in the job description\n"
@@ -82,6 +92,8 @@ function usage()
     usage_lavacli
     usage_submit
     usage_result
+
+    exit 1
 }
 
 function parse_parameters
@@ -107,6 +119,10 @@ function parse_parameters
 
         job_name=*)
           opt_job_name="${arg#*=}"
+          ;;
+
+        coverage)
+          opt_coverage=1
           ;;
 
         *)
@@ -181,7 +197,6 @@ function squad_submit
     #
     if [[ $# -lt 4 ]]; then
       usage
-      exit 0
     fi
 
     platform=$1
@@ -206,14 +221,9 @@ function squad_submit
         filename_dtb=imx8qxp-mek.dtb
         filename_kernel=${prefix_kernel}${platform}.bin
         rootfs_name=${prefix_rootfs}${platform}
-        BOARD_ID=imx8qxpc0mek
-        if [[ ${use_new_lava} -eq 1 ]]; then
-            LAVA_DEVICE_TYPE=imx8qxp-mek
-        else
-            LAVA_DEVICE_TYPE=fsl-imx8qxp-c0-mek-linux
-        fi
-        UBOOT_MMC_BLK=0x40
-        UBOOT_MMC_CNT=0x1000
+        lava_device_type=imx8qxp-mek
+        uboot_mmc_blk=0x40
+        uboot_mmc_cnt=0x1000
         ;;
 
       *)
@@ -221,7 +231,6 @@ function squad_submit
         printf "Supported platforms: %s " "imx8qxpc0mek"
         printf "\n"
         usage
-        exit 1
         ;;
     esac
 
@@ -230,43 +239,54 @@ function squad_submit
 
     check_file "${script_dir}" "${devops_script}"
 
-    PACKAGE_URL=${opt_package_url}
-
     if [[ ! -z ${opt_ctest_label} ]]; then
-      CTEST_LABEL="-L ${opt_ctest_label}"
+      ctest_label="-L ${opt_ctest_label}"
     fi
 
-    cat "${yaml_dir}"/"${smw_setup_yaml}" > "${yaml_dir}"/"${smw_tmp_yaml}"
+    cat "${yaml_dir}/${smw_setup_yaml}" > "${yaml_dir}/${smw_tmp_yaml}"
     if [[ ! -z ${opt_package_url} ]]; then
       check_url "${opt_package_url}/libsmw_package.tar.gz"
 
-      cat "${yaml_dir}"/"${smw_package_yaml}" >> "${yaml_dir}"/"${smw_tmp_yaml}"
+      cat "${yaml_dir}/${smw_package_yaml}" >> "${yaml_dir}/${smw_tmp_yaml}"
     fi
 
-    # SMW Ctest execution
-    cat "${yaml_dir}"/"${smw_ctest_yaml}" >> "${yaml_dir}"/"${smw_tmp_yaml}"
+    {
+        # SMW ctest execution
+        cat "${yaml_dir}/${smw_ctest_yaml}"
+        # PKCS11 ctest execution
+        cat "${yaml_dir}/${pkcs11_ctest_yaml}"
+    } >> "${yaml_dir}/${smw_tmp_yaml}"
 
-    # PKCS11 Ctest execution
-    cat "${yaml_dir}"/"${pkcs11_ctest_yaml}" >> "${yaml_dir}"/"${smw_tmp_yaml}"
+    if [[ ${opt_coverage} -eq 1 ]]; then
+      # If code coverage enabled
+      gcda_tarball="${platform}_${opt_job_name}.tar.gz"
+      gcda_tarball="${gcda_tarball// /_}"
+      cat "${yaml_dir}/${code_coverage_yaml}" >> "${yaml_dir}/${smw_tmp_yaml}"
+    fi
 
     filename_job="${yaml_dir}"/"${smw_tmp_yaml}"
     check_file "${yaml_dir}" "${smw_tmp_yaml}"
 
-    sed -i "s|REPLACE_UBOOT_MMC_BLK|$UBOOT_MMC_BLK|" "${filename_job}"
-    sed -i "s|REPLACE_UBOOT_MMC_CNT|$UBOOT_MMC_CNT|" "${filename_job}"
+    sed -i "s|REPLACE_UBOOT_MMC_BLK|${uboot_mmc_blk}|" "${filename_job}"
+    sed -i "s|REPLACE_UBOOT_MMC_CNT|${uboot_mmc_cnt}|" "${filename_job}"
 
     if [[ ! -z ${opt_package_url} ]]; then
-      sed -i "s|REPLACE_PACKAGE_URL|$PACKAGE_URL|" "${filename_job}"
+      sed -i "s|REPLACE_PACKAGE_URL|${opt_package_url}|" "${filename_job}"
     fi
-    sed -i "s|REPLACE_CTEST_LABEL|$CTEST_LABEL|" "${filename_job}"
+    sed -i "s|REPLACE_CTEST_LABEL|${ctest_label}|" "${filename_job}"
 
-    SQUAD_GROUP="mougins-devops"
-    if [[ ${use_new_lava} -eq 1 ]]; then
-      JOB_TAG=mougins-public
-    else
-      JOB_TAG=daas_mougins
+    if [[ ${opt_coverage} -eq 1 ]]; then
+      upload_url="${nexus_url}/${nexus_dir}"
+      if [[ ! -z ${bamboo_plan} ]]; then
+          upload_url="${upload_url}/${bamboo_plan}"
+      fi
+      sed -i "s|REPLACE_GCDA_FILE|${gcda_tarball}|" "${filename_job}"
+      sed -i "s|REPLACE_UPLOAD_URL|${upload_url}|" "${filename_job}"
     fi
-    SQUAD_SLUG=SMW
+
+    squad_group="mougins-devops"
+    job_tag=mougins-public
+    squad_slug=SMW
 
     "${script_dir}"/"${devops_script}" \
               -l nl \
@@ -279,12 +299,12 @@ function squad_submit
               -b "${rootfs_name}" \
               -y "${filename_job}" \
               -o "${opt_job_name}" \
-              -v "$LAVA_DEVICE_TYPE" \
-              -t "$JOB_TAG" \
+              -v "${lava_device_type}" \
+              -t "${job_tag}" \
               -n "${daily_build_version}"
 
     if [[ ! -z ${opt_package_url} ]]; then
-        printf "PACKAGE_URL = %s\n" "$PACKAGE_URL"
+        printf "PACKAGE_URL = %s\n" "${opt_package_url}"
     fi
 
     if [[ ! -x "$(command -v ${lavacli_tool})" ]]; then
@@ -303,7 +323,7 @@ function squad_submit
       exit 1
     fi
 
-    squad_url_suffix="${SQUAD_GROUP}"/"${SQUAD_SLUG}"/SMW_"${squad_id}"/"${BOARD_ID}"
+    squad_url_suffix="${squad_group}"/"${squad_slug}"/SMW_"${squad_id}"/"${platform}"
 
     curl --noproxy "*" \
          --header "Auth-Token: ${squad_token}" \
@@ -382,15 +402,10 @@ function squad_result
 
 if [[ $# -eq 0 ]]; then
   usage
-  exit 0
 fi
 
 opt_action=$1
 shift
-
-if [[ ${lava_url} == "https://lava.sw.nxp.com" ]]; then
-  use_new_lava=1
-fi
 
 case $opt_action in
   install)

tests/lava/code_coverage.yaml
@@ -1,0 +1,31 @@
+
+- test:
+    namespace: smw_test
+    timeout:
+      minutes: 60
+    definitions:
+    - from: inline
+      name: smw-gcov
+      path: inline/smw-cov.yaml
+      repository:
+        metadata:
+          name: smw-gcov
+          description: Upload Code Coverage data
+          format: Lava-Test Test Definition 1.0
+        run:
+          steps:
+          - lava-test-case tar_cov --shell 'cd /usr/share/smw && tar -czf REPLACE_GCDA_FILE /bamboo'
+
+- test:
+    namespace: smw_test
+    timeout:
+      minutes: 20
+    definitions:
+    - repository: ssh://git@bitbucket.sw.nxp.com/~nxa06745/lava-test-definitions-nxp.git
+      from: git
+      branch: nexus-upload
+      path: linux/nexus-upload/upload.yaml
+      parameters:
+        UPLOAD_URL: "REPLACE_UPLOAD_URL"
+        FILE_TO_UPLOAD: "/usr/share/smw/REPLACE_GCDA_FILE"
+      name: nexus-upload-file

commit a08bf97f94f20ed12b91e7e1004f8b70f0440e12

SSMW-869 scripts: Remove default configuration build value
Remove the default configuration build value to not overwrite the
value set but the first configuration action when a second configuration
action is performed just to enable additional option.

Fixes: 36d27ee0de4f ("SSMW-174 scripts: add code coverage build and lava")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

scripts/smw_build.sh
@@ -7,12 +7,12 @@ trap 'error ${LINENO}' ERR
 #
 opt_build=all
 opt_jsonc_lib=
-opt_coverage="-DCODE_COVERAGE=OFF"
-opt_buildtype="-DCMAKE_BUILD_TYPE=Release"
-opt_verbose="-DVERBOSE=0"
-opt_format="-DFORMAT=html"
-opt_psa="-DENABLE_PSA_DEFAULT_ALT=OFF"
-opt_tls="-DENABLE_TLS=OFF"
+opt_coverage=
+opt_buildtype=
+opt_verbose=
+opt_format=
+opt_psa=
+opt_tls=
 opt_cmake_ver="3.13"
 
 #
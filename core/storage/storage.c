4b3469a51e1b5ff74afc2452b3980d9a694517a0
Cedric Neveux <cedric.neveux@nxp.com>
2025-05-20 16:01:47 +0200

    SSMW-839 core: storage: Query all subsystems if data unknown
    Call generic function query all subsystems if data or key is not
    known.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- core/storage/storage.c
+++ core/storage/storage.c
@@ -12,6 +12,7 @@
 #include "constants.h"
 #include "exec.h"
 #include "object_db.h"
+#include "object_query.h"
 #include "storage.h"
 
 static void set_data_identifier(struct smw_storage_data_descriptor *descriptor,
@@ -186,11 +187,9 @@ static int query_subsystem_data(struct smw_storage_data_descriptor *desc)
 {
 	int status = SMW_STATUS_OK;
 
-	enum subsystem_id subsystem_id = 0;
-	enum subsystem_id max_subsystem_id = SUBSYSTEM_ID_NB;
+	struct smw_object_query obj_query = { 0 };
 	enum operation_id op_ids[] = { OPERATION_ID_STORAGE_STORE,
 				       OPERATION_ID_STORAGE_RETRIEVE };
-	enum operation_id op_get_info = OPERATION_ID_STORAGE_IS_DATA_PRESENT;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -199,39 +198,14 @@ static int query_subsystem_data(struct smw_storage_data_descriptor *desc)
 		goto end;
 	}
 
-	subsystem_id = desc->subsystem_id;
+	obj_query.subsystem_id = desc->subsystem_id;
+	obj_query.type = SMW_QUERY_TYPE_DATA;
+	obj_query.data = desc;
 
-	/*
-	 * If the subsystem is not specified, try to get data information
-	 * querying each subsystem supporting either store or retrieve data.
-	 */
-	if (subsystem_id == SUBSYSTEM_ID_INVALID) {
-		subsystem_id = 0;
-	} else if (subsystem_id < SUBSYSTEM_ID_NB) {
-		max_subsystem_id = subsystem_id + 1;
-	} else {
-		status = SMW_STATUS_INVALID_PARAM;
-		goto end;
-	}
-
-	for (; subsystem_id < max_subsystem_id; subsystem_id++) {
-		status = smw_config_is_operations_supported(op_ids,
-							    ARRAY_SIZE(op_ids),
-							    subsystem_id);
-		if (status == SMW_STATUS_OPERATION_NOT_SUPPORTED ||
-		    status == SMW_STATUS_SUBSYSTEM_NOT_CONFIGURED) {
-			status = SMW_STATUS_UNKNOWN_ID;
-			continue;
-		}
-
-		status = smw_utils_execute_implicit(op_get_info, desc,
-						    subsystem_id);
-		if (status != SMW_STATUS_UNKNOWN_ID)
-			break;
-	}
-
+	status = util_object_query_subsystem(&obj_query, op_ids,
+					     ARRAY_SIZE(op_ids));
 	if (status == SMW_STATUS_OK)
-		desc->subsystem_id = subsystem_id;
+		desc->subsystem_id = obj_query.subsystem_id;
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);

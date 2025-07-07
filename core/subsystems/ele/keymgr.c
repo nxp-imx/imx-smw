nxp-imx/imx-smw

commit 155591a2fce6ffb9b9ee8b989b1b9af1edb9441a

SSMW-906 subsystems: ele: Rename internal key identifier
Rename the struct smw_keymgr_identifier field `id` to `s_id`
to better understanding the id value meaning.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/keymgr.c
@@ -355,7 +355,7 @@ static int delete_key_operation(hsm_hdl_t key_mgt_hdl,
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	op_args.key_identifier = key_identifier->id;
+	op_args.key_identifier = key_identifier->s_id;
 	if (SMW_ATTR_IS_PERSISTENT(key_identifier->key_attributes.attributes) ||
 	    SMW_ATTR_IS_PERMANENT(key_identifier->key_attributes.attributes))
 		op_args.flags = HSM_OP_DEL_KEY_FLAGS_STRICT_OPERATION;
@@ -531,7 +531,7 @@ static int export_key_operation(struct hdl *hdl,
 		op_args.out_key = tmp_key;
 	}
 
-	op_args.key_identifier = key_identifier->id;
+	op_args.key_identifier = key_identifier->s_id;
 
 	SMW_DBG_PRINTF(VERBOSE,
 		       "[%s (%d)] Call hsm_pub_key_recovery()\n"
@@ -698,7 +698,7 @@ static int generate_key(struct subsystem_context *ele_ctx, void *args)
 		 * NVM storage group full. ELE erases the key identifier of
 		 * the operation argument.
 		 */
-		key_id = key_identifier->id;
+		key_id = key_identifier->s_id;
 
 		status = ele_get_key_group(ele_ctx, persistent_grp, &key_group);
 		if (status != SMW_STATUS_OK)
@@ -762,7 +762,7 @@ static int generate_key(struct subsystem_context *ele_ctx, void *args)
 		goto end;
 
 	key_identifier->subsystem_id = SUBSYSTEM_ID_ELE;
-	key_identifier->id = key_id;
+	key_identifier->s_id = key_id;
 	key_identifier->group = key_group;
 
 	SMW_DBG_PRINTF(DEBUG, "Key identifier: 0x%08X\n", key_id);
@@ -878,7 +878,7 @@ static int import_key(struct hdl *hdl, void *args)
 	if (status == SMW_STATUS_OK) {
 		SMW_DBG_PRINTF(DEBUG, "hsm_import_key key id 0x%08X\n",
 			       op_args.key_identifier);
-		key_desc->identifier.id = op_args.key_identifier;
+		key_desc->identifier.s_id = op_args.key_identifier;
 
 		/*
 		 * In case of key importation, the key group is unknown.
@@ -975,7 +975,7 @@ static int get_key_lengths(struct hdl *hdl, void *args)
 
 	key_desc = args;
 
-	key_attrs.key_identifier = key_desc->identifier.id;
+	key_attrs.key_identifier = key_desc->identifier.s_id;
 
 	status = get_key_attributes_operation(hdl, &key_attrs);
 
@@ -1031,7 +1031,7 @@ static int get_key_attributes(struct hdl *hdl, void *args)
 	key_identifier = &key_args->key_descriptor.identifier;
 	key_attributes = &key_identifier->key_attributes;
 
-	op_key_attrs.key_identifier = key_identifier->id;
+	op_key_attrs.key_identifier = key_identifier->s_id;
 
 	status = get_key_attributes_operation(hdl, &op_key_attrs);
 	if (status != SMW_STATUS_OK)
@@ -1117,8 +1117,8 @@ static int key_attestation(struct hdl *hdl, void *args)
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	op_args.key_identifier = attest_args->key_descriptor.identifier.id;
-	op_args.key_attestation_id = attest_key_descriptor->identifier.id;
+	op_args.key_identifier = attest_args->key_descriptor.identifier.s_id;
+	op_args.key_attestation_id = attest_key_descriptor->identifier.s_id;
 
 	status = set_sign_algo(&attest_args->sign_attributes,
 			       &op_args.attest_algo);
@@ -1216,7 +1216,7 @@ int ele_export_public_key(struct hdl *hdl,
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	/* First get the key attributes */
-	key_attrs.key_identifier = key_desc->identifier.id;
+	key_attrs.key_identifier = key_desc->identifier.s_id;
 	status = get_key_attributes_operation(hdl, &key_attrs);
 	if (status != SMW_STATUS_OK)
 		goto end;

core/subsystems/ele/aead.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2023-2024 NXP
+ * Copyright 2023-2025 NXP
  */
 
 #include "smw_status.h"
@@ -605,7 +605,7 @@ static int aead(struct hdl *hdl, void *args)
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	op_args.key_identifier = key_identifier->id;
+	op_args.key_identifier = key_identifier->s_id;
 
 	/* Get output length feature */
 	if (!smw_crypto_get_aead_output(aead_args)) {

core/subsystems/ele/keymgr_derive_oem_mk.c
@@ -142,8 +142,8 @@ derive_oem_master_key_prepare(struct subsystem_context *ele_ctx,
 	payload->derived_key_security_size = 256;
 	payload->derived_key_usage = HSM_KEY_USAGE_DERIVE;
 	payload->derived_key_permitted_algo = PERMITTED_ALGO_HMAC_KDF_SHA256;
-	payload->derived_key_id = args->key_derived.identifier.id;
-	payload->private_key_id = args->key_base.identifier.id;
+	payload->derived_key_id = args->key_derived.identifier.s_id;
+	payload->private_key_id = args->key_base.identifier.s_id;
 
 	status = set_key_group_lifetime(ele_ctx,
 					args->key_attributes->attributes,
@@ -282,9 +282,9 @@ static int derive_oem_master_key(struct subsystem_context *ele_ctx,
 		goto end;
 
 	args->key_derived.identifier.group = payload->derived_key_group;
-	args->key_derived.identifier.id = key_ex_args.out_derived_key_id;
+	args->key_derived.identifier.s_id = key_ex_args.out_derived_key_id;
 	SMW_DBG_PRINTF(DEBUG, "OEM Master key created id=0x%08X\n",
-		       args->key_derived.identifier.id);
+		       args->key_derived.identifier.s_id);
 
 end:
 	if (key_mgt_hdl) {

core/subsystems/ele/keymgr_derive_tls12.c
@@ -148,7 +148,7 @@ static int tls12_store_key_id(struct smw_keymgr_derive_key_args *args,
 		return SMW_STATUS_OK;
 
 	key_identifier.type_id = type_id;
-	key_identifier.id = *key_id;
+	key_identifier.s_id = *key_id;
 	key_identifier.subsystem_id = SUBSYSTEM_ID_ELE;
 	key_identifier.security_size = bits;
 	key_attributes->attributes =
@@ -353,7 +353,7 @@ tls12_op_derive_master_secret(struct smw_keymgr_derive_key_args *args,
 		goto end;
 
 	payload->ver = 1;
-	payload->key_id = args->key_base.identifier.id;
+	payload->key_id = args->key_base.identifier.s_id;
 
 	status = ele_get_key_store_id(&payload->keystore_id);
 	if (status != SMW_STATUS_OK)
@@ -429,7 +429,7 @@ tls12_op_derive_master_secret(struct smw_keymgr_derive_key_args *args,
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	args->key_derived.identifier.id = key_ex_args.out_derived_key_id;
+	args->key_derived.identifier.s_id = key_ex_args.out_derived_key_id;
 	args->key_derived.identifier.security_size =
 		TLS12_MASTER_SECRET_SEC_SIZE;
 	args->key_derived.identifier.subsystem_id = SUBSYSTEM_ID_ELE;
@@ -478,7 +478,7 @@ tls12_op_derive_key_expansion(struct smw_keymgr_derive_key_args *args,
 		goto end;
 
 	payload->ver = 1;
-	payload->key_id = args->key_base.identifier.id;
+	payload->key_id = args->key_base.identifier.s_id;
 
 	status = ele_get_key_store_id(&payload->keystore_id);
 	if (status != SMW_STATUS_OK)
@@ -576,7 +576,7 @@ static int tls12_op_derive_ivs(struct smw_keymgr_derive_key_args *args,
 		goto end;
 
 	payload->ver = 1;
-	payload->key_id = args->key_base.identifier.id;
+	payload->key_id = args->key_base.identifier.s_id;
 
 	status = ele_get_key_store_id(&payload->keystore_id);
 	if (status != SMW_STATUS_OK)
@@ -663,7 +663,7 @@ int derive_tls12_op(struct hdl *hdl, struct smw_keymgr_derive_key_args *args)
 	if (!args || !args->kdf_args)
 		goto end;
 
-	if (args->key_base.identifier.id == INVALID_KEY_ID)
+	if (args->key_base.identifier.s_id == INVALID_KEY_ID)
 		goto end;
 
 	tls12_args = args->kdf_args;

core/subsystems/ele/keymgr_derive_tls13.c
@@ -231,18 +231,18 @@ static int tls13_set_payload_args(struct smw_keymgr_derive_key_args *args,
 	if (!payload)
 		goto end;
 
-	if (psk->identifier.id) {
+	if (psk->identifier.s_id) {
 		if (psk->identifier.subsystem_id != SUBSYSTEM_ID_ELE) {
 			status = SMW_STATUS_KEY_INVALID;
 			goto end;
 		}
 
-		payload->psk_id = psk->identifier.id;
+		payload->psk_id = psk->identifier.s_id;
 	}
 
 	payload->ver = 1;
 	payload->rsv = 0;
-	payload->key_id = args->key_base.identifier.id;
+	payload->key_id = args->key_base.identifier.s_id;
 
 	status = ele_get_key_store_id(&payload->keystore_id);
 	if (status != SMW_STATUS_OK)
@@ -270,7 +270,7 @@ static int tls13_set_payload_args(struct smw_keymgr_derive_key_args *args,
 	/* Handshake/Master secrets need both the base key and peer public key */
 	if (is_handshake_secret(payload->tls1_3_algo) ||
 	    is_master_secret(payload->tls1_3_algo)) {
-		if (!args->key_base.identifier.id) {
+		if (!args->key_base.identifier.s_id) {
 			status = SMW_STATUS_INVALID_PARAM;
 			goto end;
 		}
@@ -338,7 +338,7 @@ tls13_set_derived_identifier(struct smw_keymgr_derive_key_args *args,
 	if (is_iv(payload->tls1_3_algo))
 		return;
 
-	args->key_derived.identifier.id = key_id;
+	args->key_derived.identifier.s_id = key_id;
 	args->key_derived.identifier.security_size = payload->key_bits;
 	args->key_derived.identifier.subsystem_id = SUBSYSTEM_ID_ELE;
 

core/subsystems/ele/mac.c
@@ -149,8 +149,8 @@ static int mac(struct hdl *hdl, void *args)
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	if (key_identifier->id) {
-		op_args.key_identifier = key_identifier->id;
+	if (key_identifier->s_id) {
+		op_args.key_identifier = key_identifier->s_id;
 	} else {
 		/* MAC using plaintext key buffer */
 		op_args.flags = HSM_OP_MAC_FLAGS_PLAINTEXT_KEY;

core/subsystems/ele/sign_verify.c
@@ -296,8 +296,8 @@ static int sign(struct hdl *hdl, void *args)
 		goto end;
 	}
 
-	if (key_identifier->id) {
-		op_args.key_identifier = key_identifier->id;
+	if (key_identifier->s_id) {
+		op_args.key_identifier = key_identifier->s_id;
 	} else {
 		/* Sign using plaintext key buffer */
 		op_args.flags = HSM_OP_GENERATE_SIGN_FLAGS_PLAINTEXT_KEY;
@@ -430,7 +430,7 @@ static int verify(struct hdl *hdl, void *args)
 	format_id = key_desc->format_id;
 
 	if (format_id == SMW_KEYMGR_FORMAT_ID_INVALID) {
-		export_key_desc.identifier.id = key_desc->identifier.id;
+		export_key_desc.identifier.s_id = key_desc->identifier.s_id;
 
 		status = ele_export_public_key(hdl, &export_key_desc);
 		if (status != SMW_STATUS_OK)

core/subsystems/ele/sign_verify_tls12.c
@@ -125,7 +125,7 @@ int tls_mac_finish(struct hdl *hdl, void *args)
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	payload.ver = 1;
-	payload.key_id = key_identifier->id;
+	payload.key_id = key_identifier->s_id;
 
 	status = ele_get_key_store_id(&payload.keystore_id);
 	if (status != SMW_STATUS_OK)

commit a3645ec4b42e8256cf967a78732cfa4ad6679b35

SSMW-963 subsystems: ele: Fix key import subsystem identifier
When key imported with success, the key identifier's subsystem field
must be set before updating the database.

Fixes: 155591a2fce6 ("SSMW-906 subsystems: ele: Rename internal key identifier")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/keymgr.c
@@ -879,6 +879,7 @@ static int import_key(struct hdl *hdl, void *args)
 		SMW_DBG_PRINTF(DEBUG, "hsm_import_key key id 0x%08X\n",
 			       op_args.key_identifier);
 		key_desc->identifier.s_id = op_args.key_identifier;
+		key_desc->identifier.subsystem_id = SUBSYSTEM_ID_ELE;
 
 		/*
 		 * In case of key importation, the key group is unknown.
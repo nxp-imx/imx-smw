nxp-imx/imx-smw

commit 4ba4e840cf0b1988220cd1a06018c1f05f63b679

SSMW-662 Public: Define new status and cleanup
Remove no more used OSAL API and status code.
Add new status code handling system configuration file.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

public/smw_status.h
@@ -89,12 +89,6 @@
  * @SMW_STATUS_OBJ_DB_DELETE: Object database delete error.
  * @SMW_STATUS_OBJ_DB_GET_INFO: Object database get information error.
  *
- * @SMW_STATUS_KEY_DB_INIT: Initialization error of the key database.
- * @SMW_STATUS_KEY_DB_CREATE: Key database creation error.
- * @SMW_STATUS_KEY_DB_UPDATE: Key database update error.
- * @SMW_STATUS_KEY_DB_DELETE: Key database delete error.
- * @SMW_STATUS_KEY_DB_GET_INFO: Key database get information error.
- *
  * @SMW_STATUS_KEY_POLICY_WARNING_IGNORED: At least one element of the key policy is ignored.
  * @SMW_STATUS_UNKNOWN_KEY_OP_NAME: Key operation name provided by the user or
  * set in the user configuration is not recognized by SMW.
@@ -108,6 +102,7 @@
  * provided by the user is not recognized by SMW.
  * @SMW_STATUS_UNKNOWN_TLS12_ENC_NAME: TLS 1.2 encryption algorithm name
  * provided by the user is not recognized by SMW.
+ * @SMW_STATUS_CONFIGURATION_FAILURE: Library configuration failure.
  *
  * Status code classification:
  ** Common return codes
@@ -152,6 +147,7 @@
  *	- SMW_STATUS_MUTEX_UNLOCK_FAILURE
  *	- SMW_STATUS_INVALID_LIBRARY_CONTEXT
  *	- SMW_STATUS_INVALID_CONFIG_DATABASE
+ *	- SMW_STATUS_CONFIGURATION_FAILURE
  *
  ** Specific return codes - Configuration file
  *
@@ -180,13 +176,6 @@
  *	- SMW_STATUS_ERROR_OBJ_DB_UPDATE
  *	- SMW_STATUS_ERROR_OBJ_DB_DELETE
  *	- SMW_STATUS_ERROR_OBJ_DB_GET_INFO
- *
- ** Specific return codes - Key database
- *	- SMW_STATUS_ERROR_KEY_DB_INIT
- *	- SMW_STATUS_ERROR_KEY_DB_CREATE
- *	- SMW_STATUS_ERROR_KEY_DB_UPDATE
- *	- SMW_STATUS_ERROR_KEY_DB_DELETE
- *	- SMW_STATUS_ERROR_KEY_DB_GET_INFO
  *
  ** Specific return codes - Key manager
  *	- SMW_STATUS_KEY_POLICY_WARNING_IGNORED
@@ -246,11 +235,6 @@ enum smw_status_code {
 	SMW_STATUS_OBJ_DB_UPDATE, /* 40 */
 	SMW_STATUS_OBJ_DB_DELETE,
 	SMW_STATUS_OBJ_DB_GET_INFO,
-	SMW_STATUS_KEY_DB_INIT = SMW_STATUS_OBJ_DB_INIT,
-	SMW_STATUS_KEY_DB_CREATE = SMW_STATUS_OBJ_DB_CREATE,
-	SMW_STATUS_KEY_DB_UPDATE = SMW_STATUS_OBJ_DB_UPDATE, /* 40 */
-	SMW_STATUS_KEY_DB_DELETE = SMW_STATUS_OBJ_DB_DELETE,
-	SMW_STATUS_KEY_DB_GET_INFO = SMW_STATUS_OBJ_DB_GET_INFO,
 	SMW_STATUS_KEY_POLICY_WARNING_IGNORED,
 	SMW_STATUS_KEY_INVALID,
 	SMW_STATUS_MUTEX_LOCK_FAILURE, /* 45 */
@@ -272,7 +256,8 @@ enum smw_status_code {
 	SMW_STATUS_UNKNOWN_KDF_NAME,
 	SMW_STATUS_UNKNOWN_TLS12_KEA_NAME,
 	SMW_STATUS_UNKNOWN_TLS12_ENC_NAME,
-	SMW_STATUS_OEM_SRKH_NOT_FUSED,
+	SMW_STATUS_OEM_SRKH_NOT_FUSED, /* 65 */
+	SMW_STATUS_CONFIGURATION_FAILURE,
 };
 
 #endif /* __SMW_STATUS_H__ */

public/smw/attr.h
@@ -93,13 +93,13 @@ typedef uint32_t smw_attr_usage_t;
  *
  * The attributes bitfield is described below.
  *
- * +--------------------------------------------------------+
- * | Bits                                                   |
- * +---------+---+---+-----------+-----------+--------------+
- * | [31:18] | 17| 16|   [15:8]  |   [7:4]   |    [3:0]     |
- * +---------+---+---+-----------+-----------+--------------+
- * |         | Rl| Rc| Lifecycle |           | Persisitence |
- * +---------+---+---+-----------+-----------+--------------+
+ * +-------------------------------------------------------+
+ * | Bits                                                  |
+ * +---------+---+---+-----------+-----------+-------------+
+ * | [31:18] | 17| 16|   [15:8]  |   [7:4]   |    [3:0]    |
+ * +---------+---+---+-----------+-----------+-------------+
+ * |         | Rl| Rc| Lifecycle |           | Persistence |
+ * +---------+---+---+-----------+-----------+-------------+
  */
 typedef uint32_t smw_attr_attributes_t;
 

public/smw_osal.h
@@ -142,21 +142,6 @@ enum smw_status_code smw_osal_set_subsystem_info(smw_subsystem_t subsystem,
 						 void *info, size_t info_size);
 
 /**
- * smw_osal_open_key_db() - Open a key database file
- * @file: Fullname of the key database
- * @len: Length of the @file string
- *
- * **Deprecated. Will be removed in library version 3.x.**
- * Use smw_osal_open_obj_db().
- *
- * Return:
- * See &enum smw_status_code
- *  - SMW_STATUS_OK                - Success
- *  - SMW_STATUS_KEY_DB_INIT       - Initialization error of the database
- */
-enum smw_status_code smw_osal_open_key_db(const char *file, size_t len);
-
-/**
  * smw_osal_open_obj_db() - Open a object database file
  * @file: Fullname of the object database
  * @len: Length of the @file string
@@ -167,4 +152,5 @@ enum smw_status_code smw_osal_open_key_db(const char *file, size_t len);
  *  - SMW_STATUS_OBJ_DB_INIT       - Initialization error of the database
  */
 enum smw_status_code smw_osal_open_obj_db(const char *file, size_t len);
+
 #endif /* __SMW_OSAL_H__ */

commit 35b3f3d3f8d166d9ff24ce3b62efbefb4869055c

SSMW-184 public: minor fix in status comments
Fix incorrect value in comments.

Fixes: 4ba4e840 ("SSMW-662 Public: Define new status and cleanup")

Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

public/smw_status.h
@@ -269,12 +269,12 @@ enum smw_status_code {
 	SMW_STATUS_UNKNOWN_KDF_NAME,
 	SMW_STATUS_UNKNOWN_TLS12_KEA_NAME,
 	SMW_STATUS_UNKNOWN_TLS12_ENC_NAME,
-	SMW_STATUS_OEM_SRKH_NOT_FUSED, /* 65 */
-	SMW_STATUS_CONFIGURATION_FAILURE,
+	SMW_STATUS_OEM_SRKH_NOT_FUSED,
+	SMW_STATUS_CONFIGURATION_FAILURE, /* 65 */
 	SMW_STATUS_INVALID_IV_SIZE,
 	SMW_STATUS_UNKNOWN_KEY_PRIVACY_NAME,
 	SMW_STATUS_OBJ_DB_FIND,
-	SMW_STATUS_PUBLIC_EXPONENT_NOT_SUPPORTED /* 70 */
+	SMW_STATUS_PUBLIC_EXPONENT_NOT_SUPPORTED /* 69 */
 };
 
 #endif /* __SMW_STATUS_H__ */
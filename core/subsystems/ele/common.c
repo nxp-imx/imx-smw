nxp-imx/imx-smw

commit 692502b5d3ac52bc262c28e985241fc91b279c2b

SSMW-571 core: Add ED25519 Key generate and Signature with ELE
Support ED25519 Key generate and Signature

Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

core/subsystems/ele/keymgr.c
@@ -91,8 +91,10 @@ static const struct key_def {
 } key_def_list[] = {
 	KEY_DEF(SECP_R1, ECC_NIST, ecc_public_key_length, NULL),
 	KEY_DEF(BRAINPOOL_R1, ECC_BP_R1, ecc_public_key_length, NULL),
-	KEY_DEF(AES, AES, NULL, NULL), KEY_DEF(HMAC, HMAC, NULL, NULL),
-	KEY_DEF(RSA, RSA, rsa_public_key_length, rsa_modulus_length)
+	KEY_DEF(AES, AES, NULL, NULL),
+	KEY_DEF(HMAC, HMAC, NULL, NULL),
+	KEY_DEF(RSA, RSA, rsa_public_key_length, rsa_modulus_length),
+	KEY_DEF(ED25519, ECC_TWISTED_EDWARDS, ecc_public_key_length, NULL),
 };
 
 #define SIGN_ALGO(_algo_id, _type_id, _hash_id, _sign_algo)                    \
@@ -278,6 +280,7 @@ static int check_export_key_config(struct smw_keymgr_descriptor *key_descriptor)
 	case SMW_CONFIG_KEY_TYPE_ID_SECP_R1:
 	case SMW_CONFIG_KEY_TYPE_ID_BRAINPOOL_R1:
 	case SMW_CONFIG_KEY_TYPE_ID_BRAINPOOL_T1:
+	case SMW_CONFIG_KEY_TYPE_ID_ED25519:
 		if (smw_keymgr_get_public_data(key_descriptor) &&
 		    !smw_keymgr_get_private_data(key_descriptor)) {
 			status = SMW_STATUS_OK;

core/subsystems/ele/sign_verify.c
@@ -36,6 +36,7 @@
 static const unsigned int secp_r1_key_sizes[] = { 224, 256, 384, 521, 0 };
 static const unsigned int brainpool_r1_key_sizes[] = { 224, 256, 384, 0 };
 static const unsigned int rsa_key_sizes[] = { 2048, 3072, 4096, 0 };
+static const unsigned int ed25519_key_sizes[] = { 255, 0 };
 
 static const struct signature_scheme {
 	enum smw_config_key_type_id key_type_id;
@@ -78,6 +79,10 @@ static const struct signature_scheme {
 			    RSA_PKCS1_PSS_MGF1_SHA384),
 	SIGNATURE_SCHEME_ID(RSA, rsa_key_sizes, PSS, SHA512,
 			    RSA_PKCS1_PSS_MGF1_SHA512),
+	SIGNATURE_SCHEME_ID(ED25519, ed25519_key_sizes, DEFAULT, INVALID,
+			    PURE_EDDSA),
+	SIGNATURE_SCHEME_ID(ED25519, ed25519_key_sizes, DEFAULT, SHA512,
+			    ED25519PH),
 };
 
 static bool check_security_size(unsigned int security_size,
@@ -110,7 +115,8 @@ static int set_signature_scheme(enum smw_config_key_type_id key_type_id,
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	if (hash_id == SMW_CONFIG_HASH_ALGO_ID_INVALID)
+	if (key_type_id != SMW_CONFIG_KEY_TYPE_ID_ED25519 &&
+	    hash_id == SMW_CONFIG_HASH_ALGO_ID_INVALID)
 		hash_id = ele_get_hash_algo_id(message_size);
 
 	for (; i < ARRAY_SIZE(signature_schemes); i++, scheme++) {
@@ -189,10 +195,16 @@ static int sign(struct hdl *hdl, void *args)
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	if (sign_args->attributes.hash_id != SMW_CONFIG_HASH_ALGO_ID_INVALID)
-		op_args.flags = HSM_OP_GENERATE_SIGN_FLAGS_INPUT_MESSAGE;
-	else
+	op_args.flags = HSM_OP_GENERATE_SIGN_FLAGS_INPUT_MESSAGE;
+
+	if (key_identifier->type_id == SMW_CONFIG_KEY_TYPE_ID_ED25519) {
+		if (sign_args->attributes.hash_id !=
+		    SMW_CONFIG_HASH_ALGO_ID_INVALID)
+			op_args.flags = HSM_OP_GENERATE_SIGN_FLAGS_INPUT_DIGEST;
+	} else if (sign_args->attributes.hash_id ==
+		   SMW_CONFIG_HASH_ALGO_ID_INVALID) {
 		op_args.flags = HSM_OP_GENERATE_SIGN_FLAGS_INPUT_DIGEST;
+	}
 
 	SMW_DBG_PRINTF(VERBOSE,
 		       "[%s (%d)] Call hsm_do_sign()\n"
@@ -310,10 +322,16 @@ static int verify(struct hdl *hdl, void *args)
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	if (verify_args->attributes.hash_id != SMW_CONFIG_HASH_ALGO_ID_INVALID)
-		op_args.flags = HSM_OP_GENERATE_SIGN_FLAGS_INPUT_MESSAGE;
-	else
+	op_args.flags = HSM_OP_GENERATE_SIGN_FLAGS_INPUT_MESSAGE;
+
+	if (key_type_id == SMW_CONFIG_KEY_TYPE_ID_ED25519) {
+		if (verify_args->attributes.hash_id !=
+		    SMW_CONFIG_HASH_ALGO_ID_INVALID)
+			op_args.flags = HSM_OP_GENERATE_SIGN_FLAGS_INPUT_DIGEST;
+	} else if (verify_args->attributes.hash_id ==
+		   SMW_CONFIG_HASH_ALGO_ID_INVALID) {
 		op_args.flags = HSM_OP_GENERATE_SIGN_FLAGS_INPUT_DIGEST;
+	}
 
 	SMW_DBG_PRINTF(VERBOSE,
 		       "[%s (%d)] Call hsm_verify_signature()\n"

core/subsystems/ele/key_policy.c
@@ -100,6 +100,10 @@ static const struct {
 			     ASYMMETRIC_SIGNATURE),
 	PERMITTED_ALGO_CURVE(ECDSA_SHA512, ECDSA, NONE, SHA512,
 			     ASYMMETRIC_SIGNATURE),
+	PERMITTED_ALGO_CURVE(ED25519PH, EDDSA, ED25519, SHA512,
+			     ASYMMETRIC_SIGNATURE),
+	PERMITTED_ALGO_CURVE(PURE_EDDSA, EDDSA, ED25519, NONE,
+			     ASYMMETRIC_SIGNATURE),
 	PERMITTED_ALGO(HMAC_KDF_SHA256, HKDF, NONE, SHA256, KEY_DERIVATION),
 	PERMITTED_ALGO(ALL_CIPHER, DEFAULT, ANY, NONE, SYMMETRIC_ENCRYPTION),
 	PERMITTED_ALGO(ALL_AEAD, DEFAULT, ANY, NONE, AEAD),

commit 47acba66290b98d2bff8fc0d75b21bfec7e977e4

SSMW-959 subsystems: ele: Fix endianness handling for signature and key buffers
On i.MX91 and i.MX93 platforms,
- Internally convert EdDSA signature to little-endian format
  during signature generation.
- During signature verification, internally convert EdDSA signature
  and public key buffer to big-endian format, removing the need for
  user-side conversion.
- Return the exported public keys for ECC Edwards and X25519 key pairs
  in little-endian format.

Fixes: 692502b5d3ac ("SSMW-571 core: Add ED25519 Key generate and Signature with ELE")
Fixes: d754519df773 ("SSMW-570 subsystems: ele: add support for X25519")
Fixes: ec0671bd0157 ("SSMW-532: subsystems: ele: Support signature generation using plaintext key")

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

core/subsystems/ele/common.c
@@ -115,3 +115,118 @@ end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }
+
+static bool is_imx91_or_imx93(void)
+{
+	bool is_imx91_or_imx93 = false;
+
+	uint16_t soc_id = se_get_soc_id();
+
+	SMW_DBG_PRINTF(DEBUG, "soc_id = 0X%x\n", soc_id);
+
+	if (soc_id == SOC_IMX91 || soc_id == SOC_IMX93)
+		is_imx91_or_imx93 = true;
+
+	return is_imx91_or_imx93;
+}
+
+static int convert_endian(unsigned char *src, unsigned char *dst,
+			  unsigned int size)
+{
+	unsigned int i = 0;
+
+	if (!src || size == 0)
+		return SMW_STATUS_INVALID_PARAM;
+
+	if (dst) {
+		for (; i < size; i++)
+			dst[i] = src[size - 1 - i];
+	} else {
+		for (; i < size / 2; i++) {
+			src[i] ^= src[size - 1 - i];
+			src[size - 1 - i] ^= src[i];
+			src[i] ^= src[size - 1 - i];
+		}
+	}
+
+	return SMW_STATUS_OK;
+}
+
+static bool is_conversion_req(enum smw_config_key_type_id type_id)
+{
+	bool is_conversion_req = false;
+
+	switch (type_id) {
+	case SMW_CONFIG_KEY_TYPE_ID_ED25519:
+	case SMW_CONFIG_KEY_TYPE_ID_X25519:
+		is_conversion_req = true;
+		break;
+
+	default:
+		break;
+	}
+
+	return is_conversion_req;
+}
+
+int check_and_convert_endian(unsigned char *src, unsigned char *dst,
+			     unsigned int size,
+			     enum smw_config_key_type_id type_id)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	if (!src || size == 0)
+		return status;
+
+	status = SMW_STATUS_OK;
+
+	if (!is_imx91_or_imx93() || !is_conversion_req(type_id))
+		return status;
+
+	status = convert_endian(src, dst, size);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+
+	return status;
+}
+
+int check_and_convert_sign_endian(unsigned char *sign,
+				  unsigned char *converted_sign,
+				  unsigned int sign_len,
+				  enum smw_config_key_type_id type_id)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+	unsigned int part_size = 0;
+
+	if (!sign || sign_len == 0)
+		return status;
+
+	status = SMW_STATUS_OK;
+
+	if (!is_imx91_or_imx93() || !is_conversion_req(type_id))
+		return status;
+
+	part_size = sign_len / 2;
+
+	/*
+	 * The signature is a concatenation of two components: R and S.
+	 * Convert the endianness of each component (R and S) individually,
+	 * then concatenate the results to form the final converted signature.
+	 */
+	if (converted_sign) {
+		status = convert_endian(sign, converted_sign, part_size);
+		if (status == SMW_STATUS_OK)
+			status = convert_endian(sign + part_size,
+						converted_sign + part_size,
+						part_size);
+	} else {
+		status = convert_endian(sign, NULL, part_size);
+		if (status == SMW_STATUS_OK)
+			status = convert_endian(sign + part_size, NULL,
+						part_size);
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+
+	return status;
+}

core/subsystems/ele/common.h
@@ -529,4 +529,49 @@ unsigned int ele_get_sign_msg_block_length(void);
 int derive_oem_mk(struct subsystem_context *ele_ctx,
 		  struct smw_keymgr_derive_key_args *args);
 
+/**
+ * check_and_convert_sign_endian() - Converts the endianness of a signature
+ * @sign: Input signature buffer.
+ * @converted_sign: Output buffer for the converted signature.
+ * @sign_len: Length of the @sign buffer.
+ * @type_id: Key type ID.
+ *
+ * This function converts the endianness of signature buffer @sign, but only if
+ * 1. The platform is i.MX91 or i.MX93.
+ * 2. The key type is either ECC key pair Edwards or ECC key pair Montgomery.
+ *
+ * @converted_sign is an optional buffer. If it is NULL, conversion is done
+ * in-place on @sign.
+ *
+ * Return:
+ * SMW_STATUS_OK                       - Success
+ * SMW_STATUS_INVALID_PARAM            - One of the parameter is invalid.
+ */
+int check_and_convert_sign_endian(unsigned char *sign,
+				  unsigned char *converted_sign,
+				  unsigned int sign_len,
+				  enum smw_config_key_type_id type_id);
+
+/**
+ * check_and_convert_endian() - Converts the endianness of a buffer
+ * @src: Input buffer.
+ * @dst: Output buffer for the converted data.
+ * @size: Length of the @src buffer.
+ * @type_id: Key type ID.
+ *
+ * This function converts the endianness of input buffer @src, but only if
+ * 1. The platform is i.MX91 or i.MX93.
+ * 2. The key type is either ECC key pair Edwards or ECC key pair Montgomery.
+ *
+ * @dst is an optional buffer. If it is NULL, conversion is done
+ * in-place on @src.
+ *
+ * Return:
+ * SMW_STATUS_OK                       - Success
+ * SMW_STATUS_INVALID_PARAM            - One of the parameter is invalid.
+ */
+int check_and_convert_endian(unsigned char *src, unsigned char *dst,
+			     unsigned int size,
+			     enum smw_config_key_type_id type_id);
+
 #endif /* __COMMON_H__ */

core/subsystems/ele/keymgr.c
@@ -479,6 +479,7 @@ static int export_key_operation(struct hdl *hdl,
 	op_pub_key_recovery_args_t op_args = { 0 };
 
 	struct smw_keymgr_identifier *key_identifier = &key_desc->identifier;
+	enum smw_config_key_type_id key_type_id = key_identifier->type_id;
 	unsigned char *public_data = NULL;
 	unsigned char *modulus_data = NULL;
 	unsigned char *tmp_key = NULL;
@@ -491,7 +492,7 @@ static int export_key_operation(struct hdl *hdl,
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	if (key_identifier->type_id != SMW_CONFIG_KEY_TYPE_ID_RSA) {
+	if (key_type_id != SMW_CONFIG_KEY_TYPE_ID_RSA) {
 		/* Set the operation output with user public key arguments */
 		public_data = smw_keymgr_get_public_data(key_desc);
 		public_length = smw_keymgr_get_public_length(key_desc);
@@ -552,8 +553,21 @@ static int export_key_operation(struct hdl *hdl,
 
 	public_length = op_args.exp_out_key_size;
 
-	if (key_identifier->type_id != SMW_CONFIG_KEY_TYPE_ID_RSA) {
+	if (key_type_id != SMW_CONFIG_KEY_TYPE_ID_RSA) {
 		if (status == SMW_STATUS_OK) {
+			/*
+			 * On i.MX93 and i.MX91, the exported public key buffer is encoded
+			 * in big-endian format for ECC Edwards and X25519 key pairs. Hence,
+			 * Convert it to little endian format.
+			 */
+			status = check_and_convert_endian(op_args.out_key, NULL,
+							  public_length,
+							  key_type_id);
+			if (status != SMW_STATUS_OK) {
+				status = SMW_STATUS_OPERATION_FAILURE;
+				goto end;
+			}
+
 			status =
 				smw_keymgr_update_public_buffer(key_desc,
 								op_args.out_key,
@@ -768,6 +782,25 @@ static int generate_key(struct subsystem_context *ele_ctx, void *args)
 	SMW_DBG_PRINTF(DEBUG, "Key identifier: 0x%08X\n", key_id);
 
 	if (public_data) {
+		/*
+		 * On i.MX93 and i.MX91, the exported public key buffer is encoded
+		 * in big-endian format for ECC Edwards and X25519 key pairs. Hence,
+		 * Convert it to little endian format.
+		 */
+		status = check_and_convert_endian(op_args.out_key, NULL,
+						  op_args.exp_out_size,
+						  key_identifier->type_id);
+		if (status != SMW_STATUS_OK) {
+			status = SMW_STATUS_OPERATION_FAILURE;
+
+			/*
+			 * Delete the key in subsystem as smw_generate_key()
+			 * is going to remove it from the key database
+			 */
+			(void)delete_key_operation(key_mgt_hdl, key_identifier);
+			goto end;
+		}
+
 		status = smw_keymgr_update_public_buffer(key_desc,
 							 op_args.out_key,
 							 op_args.exp_out_size);

core/subsystems/ele/sign_verify.c
@@ -17,6 +17,12 @@
 
 #include "common.h"
 
+/* Covers both Ed25519 (64) and Ed448 (114) */
+#define MAX_ED_SIGN_SIZE 114
+
+/* Covers both Ed25519 (32) and Ed448 (57) */
+#define MAX_ED_PUB_KEY_SIZE 57
+
 /* Workaround */
 #define HSM_SIGNATURE_SCHEME_ECDSA_ANY 0x06000600
 
@@ -388,6 +394,20 @@ static int sign(struct hdl *hdl, void *args)
 
 	smw_sign_verify_set_sign_len(sign_args, op_args.exp_signature_size);
 
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	/*
+	 * For platforms i.MX91 and i.MX93, signature generated using EDDSA
+	 * algorithm is encoded in big-endian format. Hence, convert it to little
+	 * endian.
+	 */
+	status = check_and_convert_sign_endian(op_args.signature, NULL,
+					       op_args.exp_signature_size,
+					       key_identifier->type_id);
+	if (status != SMW_STATUS_OK)
+		status = SMW_STATUS_OPERATION_FAILURE;
+
 end:
 	if (key_desc->format_id == SMW_KEYMGR_FORMAT_ID_BASE64) {
 		if (hex_private_buffer)
@@ -424,6 +444,8 @@ static int verify(struct hdl *hdl, void *args)
 	unsigned int key_size = 0;
 	unsigned char *hex_key_buf = NULL;
 	unsigned int hex_key_size = 0;
+	unsigned char temp_sign[MAX_ED_SIGN_SIZE] = { 0 };
+	unsigned char temp_pub_key[MAX_ED_PUB_KEY_SIZE] = { 0 };
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -528,6 +550,29 @@ static int verify(struct hdl *hdl, void *args)
 	    smw_sign_verify_get_ed25519ctx_buf(args)) {
 		status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
 		goto end;
+	}
+
+	/*
+	 * On i.MX91 and i.MX93 platforms, EDDSA-based signature verification
+	 * mandates the following input format requirements:
+	 * - The signature must also be encoded in big-endian format to
+	 *   ensure correct cryptographic validation.
+	 * - The public key buffer must be encoded in big-endian format.
+	 */
+	status = check_and_convert_sign_endian(op_args.signature, temp_sign,
+					       op_args.signature_size,
+					       key_type_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = check_and_convert_endian(hex_key_buf, temp_pub_key,
+					  hex_key_size, key_type_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	if (key_type_id == SMW_CONFIG_KEY_TYPE_ID_ED25519) {
+		op_args.signature = temp_sign;
+		op_args.key = temp_pub_key;
 	}
 
 	if (verify_args->attributes.msg_hashed)
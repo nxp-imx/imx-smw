8d966af52cdeafea68e0ceb716aae91bc388bf60
Cedric Neveux <cedric.neveux@nxp.com>
2025-08-07 15:09:59 +0200

    SSMW-569 public: Add support Montgomery 448 key
    Add support Montgomery 448 key, define key name.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- public/smw/names.h
+++ public/smw/names.h
@@ -141,9 +141,10 @@ typedef enum {
  * * SMW_KEY_TYPE_NAME_RAW: Raw key
  * * SMW_KEY_TYPE_NAME_DERIVE: Derived key
  * * SMW_KEY_TYPE_NAME_HKDF_IKM: HKDF IKM key
- * * SMW_KEY_TYPE_NAME_X25519: X25519 key
+ * * SMW_KEY_TYPE_NAME_X25519: Montgomery Elliptic Curve25519 key
  * * SMW_KEY_TYPE_NAME_EL2GO_PROV_OEM_KEY: EdgeLock 2GO Provisioning OEM Key
  * * SMW_KEY_TYPE_NAME_ED448: Twisted Edwards 448 key
+ * * SMW_KEY_TYPE_NAME_X448: Montgomery Elliptic Curve448 key
  * * SMW_KEY_TYPE_NAME_NB: Number of key types
  */
 typedef enum {
@@ -167,6 +168,7 @@ typedef enum {
 	SMW_KEY_TYPE_NAME_X25519,
 	SMW_KEY_TYPE_NAME_EL2GO_PROV_OEM_KEY,
 	SMW_KEY_TYPE_NAME_ED448,
+	SMW_KEY_TYPE_NAME_X448,
 	SMW_KEY_TYPE_NAME_NB
 } smw_key_type_t;
 

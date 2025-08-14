nxp-imx/imx-smw

commit 289024ae43742847642d92b746f3746390a99a9d

SSMW-497 subsystems: ele: Add ELE key import with OEM Master key derivation
Add support for the OEM Master key derivation.
Add support for ELE key import.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/keymgr_derive_oem_mk.c
@@ -1,0 +1,320 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2025 NXP
+ */
+
+#include "compiler.h"
+#include "debug.h"
+#include "hash.h"
+#include "keymgr_derive.h"
+#include "utils.h"
+
+#include "key_group.h"
+
+#define OEM_MK_SLAT_FLAG_PEER_KEY BIT(0)
+#define OEM_MK_SLAT_FLAGS_SRKH	  BIT(1)
+
+struct __packed payload_oem_mk {
+	uint8_t tag;
+	uint8_t version;
+	uint16_t res;
+	uint32_t key_store_id;
+	uint32_t key_exchange_algo;
+	uint16_t derived_key_group;
+	uint16_t salt_flags;
+	uint16_t derived_key_type;
+	uint16_t derived_key_security_size;
+	uint32_t derived_key_lifetime;
+	uint32_t derived_key_usage;
+	uint32_t derived_key_permitted_algo;
+	uint32_t derived_key_lifecycle;
+	uint32_t derived_key_id;
+	uint32_t private_key_id;
+	uint8_t hash_peer_public_key[32];
+	uint8_t hash_user_fixed_info[32];
+};
+
+#define PAYLOAD_LENGTH sizeof(struct payload_oem_mk)
+#define COMMAND	       0x47
+#define VERSION	       0x07
+
+static int set_key_group_lifetime(struct subsystem_context *ele_ctx,
+				  smw_attr_attributes_t attributes,
+				  struct payload_oem_mk *payload)
+{
+	int status = SMW_STATUS_OK;
+
+	smw_attr_attributes_t persistence =
+		SMW_ATTR_GET_PERSISTENCE(attributes);
+	bool persistent_grp = false;
+	unsigned int key_group = 0;
+
+	switch (persistence) {
+	case SMW_ATTR_PERSISTENCE_PERSISTENT:
+		payload->derived_key_lifetime = HSM_SE_KEY_STORAGE_PERSISTENT;
+		key_group = ELE_FIRST_PERSISTENT_KEY_GROUP;
+		persistent_grp = true;
+		break;
+
+	case SMW_ATTR_PERSISTENCE_PERMANENT:
+		payload->derived_key_lifetime = HSM_SE_KEY_STORAGE_PERS_PERM;
+		key_group = ELE_FIRST_PERSISTENT_KEY_GROUP;
+		persistent_grp = true;
+		break;
+
+	case SMW_ATTR_PERSISTENCE_TRANSIENT:
+		payload->derived_key_lifetime = HSM_SE_KEY_STORAGE_VOLATILE;
+		key_group = ELE_FIRST_TRANSIENT_KEY_GROUP;
+		break;
+
+	default:
+		status = SMW_STATUS_INVALID_PARAM;
+		goto end;
+	}
+
+	status = ele_get_key_group(ele_ctx, persistent_grp, &key_group);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	if (SET_OVERFLOW(key_group, payload->derived_key_group))
+		status = SMW_STATUS_OPERATION_FAILURE;
+
+end:
+	// coverity[missing_unlock]
+	return status;
+}
+
+static int
+derive_oem_master_key_prepare(struct subsystem_context *ele_ctx,
+			      struct smw_keymgr_derive_key_args *args)
+{
+	int status = SMW_STATUS_OK;
+
+	unsigned char *msg = NULL;
+	unsigned int msg_block_length = 0;
+	unsigned int msg_length = 0;
+	unsigned int key_store_id = 0;
+	uint16_t key_lc = 0;
+	struct payload_oem_mk *payload = NULL;
+	struct smw_crypto_hash_args hash_args = { 0 };
+	struct smw_hash_args oneshot = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	msg_block_length = ele_get_sign_msg_block_length();
+	msg_length = msg_block_length + PAYLOAD_LENGTH;
+	msg = smw_keymgr_oem_get_payload(args);
+
+	if (!msg) {
+		smw_keymgr_oem_set_payload_len(args, msg_length);
+		goto end;
+	}
+
+	if (smw_keymgr_oem_get_payload_len(args) < msg_length) {
+		smw_keymgr_oem_set_payload_len(args, msg_length);
+		status = SMW_STATUS_OUTPUT_TOO_SHORT;
+		goto end;
+	}
+
+	ele_fill_sign_msg_block(msg, COMMAND, PAYLOAD_LENGTH);
+
+	payload = (void *)msg + msg_block_length;
+
+	SMW_UTILS_MEMSET(payload, 0, PAYLOAD_LENGTH);
+
+	payload->tag = COMMAND;
+	payload->version = VERSION;
+
+	/*
+	 * Set the key store id in the payload. To prevent
+	 * compiler error of unaligned pointer value, need to use
+	 * intermediate variable and do a memory copy.
+	 */
+	status = ele_get_key_store_id(&key_store_id);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	SMW_UTILS_MEMCPY(&payload->key_store_id, &key_store_id,
+			 sizeof(key_store_id));
+
+	payload->key_exchange_algo = HSM_KEY_EXCHANGE_ECDH_HKDF_SHA256;
+	payload->derived_key_type = HSM_KEY_TYPE_OEM_IMPORT_MK_SK;
+	payload->derived_key_security_size = 256;
+	payload->derived_key_usage = HSM_KEY_USAGE_DERIVE;
+	payload->derived_key_permitted_algo = PERMITTED_ALGO_HMAC_KDF_SHA256;
+	payload->derived_key_id = args->key_derived.identifier.id;
+	payload->private_key_id = args->key_base.identifier.id;
+
+	status = set_key_group_lifetime(ele_ctx,
+					args->key_attributes->attributes,
+					payload);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = ele_set_lifecycle_flags(ele_ctx,
+					 args->key_attributes->attributes,
+					 &key_lc);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	payload->derived_key_lifecycle = key_lc;
+
+	if (smw_keymgr_oem_use_srkh(args))
+		payload->salt_flags = OEM_MK_SLAT_FLAGS_SRKH;
+
+	if (smw_keymgr_oem_use_peer_key_digest(args))
+		payload->salt_flags |= OEM_MK_SLAT_FLAG_PEER_KEY;
+
+	/* Digest the peer public key */
+	if (args->ops.get_peer(args) && args->ops.get_peer_len(args)) {
+		hash_args.algo_id = SMW_CONFIG_HASH_ALGO_ID_SHA256;
+		hash_args.op_step = SMW_OP_STEP_ONESHOT;
+		hash_args.oneshot_pub = &oneshot;
+
+		oneshot.algo_name = SMW_HASH_ALGO_NAME_SHA256;
+		oneshot.input = args->ops.get_peer(args);
+		oneshot.input_length = args->ops.get_peer_len(args);
+		oneshot.output = payload->hash_peer_public_key;
+		oneshot.output_length = sizeof(payload->hash_peer_public_key);
+
+		if (!ele_hash_handle(&ele_ctx->hdl, OPERATION_ID_HASH,
+				     &hash_args, &status))
+			status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+		if (status != SMW_STATUS_OK)
+			goto end;
+	}
+
+	/* Digest the user fixed information */
+	if (args->ops.get_info(args) && args->ops.get_info_len(args)) {
+		hash_args.algo_id = SMW_CONFIG_HASH_ALGO_ID_SHA256;
+		hash_args.op_step = SMW_OP_STEP_ONESHOT;
+		hash_args.oneshot_pub = &oneshot;
+
+		oneshot.algo_name = SMW_HASH_ALGO_NAME_SHA256;
+		oneshot.input = args->ops.get_info(args);
+		oneshot.input_length = args->ops.get_info_len(args);
+		oneshot.output = payload->hash_user_fixed_info;
+		oneshot.output_length = sizeof(payload->hash_user_fixed_info);
+
+		if (!ele_hash_handle(&ele_ctx->hdl, OPERATION_ID_HASH,
+				     &hash_args, &status))
+			status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+		if (status != SMW_STATUS_OK)
+			goto end;
+	}
+
+	smw_keymgr_oem_set_payload_len(args, msg_length);
+
+	SMW_DBG_HEX_DUMP(DEBUG, msg, msg_length, 4);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+
+	// coverity[missing_unlock]
+	return status;
+}
+
+static int derive_oem_master_key(struct subsystem_context *ele_ctx,
+				 struct smw_keymgr_derive_key_args *args)
+{
+	int status = SMW_STATUS_OK;
+
+	int tmp_status = SMW_STATUS_OK;
+	bool srkh_fused = false;
+	hsm_err_t err = HSM_NO_ERROR;
+	hsm_hdl_t key_mgt_hdl = 0;
+	op_key_exchange_args_t key_ex_args = { 0 };
+	struct payload_oem_mk *payload = NULL;
+
+	status = ele_is_oem_srkh_fused(ele_ctx, &srkh_fused);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	if (!srkh_fused) {
+		status = SMW_STATUS_OEM_SRKH_NOT_FUSED;
+		goto end;
+	}
+
+	payload = (struct payload_oem_mk *)smw_keymgr_oem_get_payload(args);
+	if (!payload) {
+		status = SMW_STATUS_INVALID_PARAM;
+		goto end;
+	}
+
+	key_ex_args.flags = HSM_OP_KEY_EXCHANGE_FLAGS_INPUT_SIGNED_CONTENT;
+	key_ex_args.in_content = smw_keymgr_oem_get_payload(args);
+	key_ex_args.in_content_sz = smw_keymgr_oem_get_payload_len(args);
+	key_ex_args.in_pub_buffer = args->ops.get_peer(args);
+	key_ex_args.in_pub_buffer_sz = args->ops.get_peer_len(args);
+	key_ex_args.user_fixed_info = args->ops.get_info(args);
+	key_ex_args.user_fixed_info_sz = args->ops.get_info_len(args);
+
+	status = open_key_mgmt_service(&ele_ctx->hdl, &key_mgt_hdl);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	SMW_DBG_PRINTF(VERBOSE,
+		       " Call hsm_key_exchange()\n"
+		       "  OEM Master Key\n"
+		       "  flags: 0x%04X\n"
+		       "  in_pub_buffer_sz: %d\n"
+		       "  user_fixed_info_sz: %d\n"
+		       "  in_content_sz: %d\n"
+		       "  in_content (payload): %p\n"
+		       "  out_derived_key_id: 0x%08X\n"
+		       "  expected_out_sz: %d\n"
+		       "  output_sz: %d\n"
+		       "  output: %p\n",
+		       key_ex_args.flags, key_ex_args.in_pub_buffer_sz,
+		       key_ex_args.user_fixed_info_sz,
+		       key_ex_args.in_content_sz, key_ex_args.in_content,
+		       key_ex_args.out_derived_key_id,
+		       key_ex_args.exp_output_sz, key_ex_args.output_sz,
+		       key_ex_args.output);
+
+	err = hsm_key_exchange(key_mgt_hdl, &key_ex_args);
+	SMW_DBG_PRINTF(DEBUG, "hsm_key_exchange returned %d\n", err);
+
+	status = ele_convert_err(err);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	args->key_derived.identifier.group = payload->derived_key_group;
+	args->key_derived.identifier.id = key_ex_args.out_derived_key_id;
+	SMW_DBG_PRINTF(DEBUG, "OEM Master key created id=0x%08X\n",
+		       args->key_derived.identifier.id);
+
+end:
+	if (key_mgt_hdl) {
+		tmp_status = close_key_mgt_service(key_mgt_hdl);
+		if (status == SMW_STATUS_OK)
+			status = tmp_status;
+	}
+
+	// coverity[missing_unlock]
+	return status;
+}
+
+int derive_oem_mk(struct subsystem_context *ele_ctx,
+		  struct smw_keymgr_derive_key_args *args)
+{
+	int status = SMW_STATUS_UNKNOWN_OP_TYPE_NAME;
+
+	switch (smw_keymgr_oem_get_op(args)) {
+	case SMW_OEM_MK_OP_NAME_PREPARE:
+		status = derive_oem_master_key_prepare(ele_ctx, args);
+		break;
+
+	case SMW_OEM_MK_OP_NAME_DERIVE:
+		status = derive_oem_master_key(ele_ctx, args);
+		break;
+
+	default:
+		break;
+	}
+
+	// coverity[missing_unlock]
+	return status;
+}

core/subsystems/ele/CMakeLists.txt
@@ -25,10 +25,15 @@ endif()
 set(ele_srcs
     entry.c
     common.c
+    sign_message.c
     $<$<BOOL:${ENABLE_AEAD}>:aead.c>
     $<$<BOOL:${ENABLE_CONTEXT}>:operation_context.c>
-    $<$<BOOL:${ENABLE_KEYMGR_MODULE}>:keymgr.c keymgr_derive.c key_policy.c
-        lifecycle.c>
+    $<$<BOOL:${ENABLE_KEYMGR_MODULE}>:keymgr.c
+                                      keymgr_derive.c
+                                      keymgr_derive_oem_mk.c
+                                      key_policy.c
+                                      lifecycle.c
+                                      key_group.c>
     $<$<BOOL:${ENABLE_HASH}>:hash.c>
     $<$<BOOL:${ENABLE_MAC}>:mac.c>
     $<$<BOOL:${ENABLE_SIGN_VERIFY}>:sign_verify.c>

core/subsystems/ele/common.h
@@ -257,13 +257,14 @@ bool ele_aead_handle(struct hdl *hdl, enum operation_id operation_id,
 
 /**
  * ele_derive_key() - ELE key derivation operation.
- * @hdl: Pointer to the ELE handles structure.
+ * @ele_ctx: Pointer to the ELE subsystem context structure.
  * @args: Pointer to the derive key arguments.
  *
  * Return:
  * SMW status
  */
-int ele_derive_key(struct hdl *hdl, struct smw_keymgr_derive_key_args *args);
+int ele_derive_key(struct subsystem_context *ele_ctx,
+		   struct smw_keymgr_derive_key_args *args);
 
 /**
  * ele_convert_err() - Convert ELE error into SMW status.
@@ -497,5 +498,35 @@ int ele_get_key_store_id(uint32_t *keystore_id);
  */
 int ele_get_key_type(enum smw_config_key_type_id key_type_id,
 		     hsm_key_type_t *ele_key_type);
+
+/**
+ * ele_fill_sign_msg_block() - Fill the signed message block
+ * @msg: Message block to be filled
+ * @cmd: Payload command
+ * @payload_length: Length of the signed message payload
+ *
+ * Fill the signed message block fields that are not fixed.
+ */
+void ele_fill_sign_msg_block(void *msg, unsigned char cmd,
+			     unsigned int payload_length);
+
+/**
+ * ele_get_sign_msg_block_length() - Return the signed message block length
+ *
+ * Return:
+ * Length of signed message block
+ */
+unsigned int ele_get_sign_msg_block_length(void);
+
+/**
+ * derive_oem_mk() - ELE OEM Master key derivation operation.
+ * @ele_ctx: Pointer to the ELE subsystem context structure.
+ * @args: Pointer to the derive key arguments.
+ *
+ * Return:
+ * SMW status
+ */
+int derive_oem_mk(struct subsystem_context *ele_ctx,
+		  struct smw_keymgr_derive_key_args *args);
 
 #endif /* __COMMON_H__ */

core/subsystems/ele/devmgr/CMakeLists.txt
@@ -3,7 +3,6 @@ file(TO_NATIVE_PATH "${folder}/" folder)
 
 list(APPEND folder_files
             devmgr.c
-            sign_message.c
             $<$<BOOL:${ENABLE_DEVICE_ATTESTATION}>:attestation.c>
             $<$<BOOL:${ENABLE_DEVICE_LIFECYCLE}>:lifecycle.c>
             $<$<BOOL:${ENABLE_DEVICE_REPROVISION}>:reprovisioning.c>)

core/subsystems/ele/devmgr/local.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2024 NXP
+ * Copyright 2024-2025 NXP
  */
 
 #ifndef __LOCAL_H__
@@ -62,24 +62,5 @@ bool ele_device_lifecycle_handle(struct subsystem_context *ele_ctx,
 bool ele_device_reprovisioning_handle(struct subsystem_context *ele_ctx,
 				      enum operation_id operation_id,
 				      void *args, int *status);
-
-/**
- * ele_devmgr_fill_msg_block() - Fill the signed message block
- * @msg: Message block to be filled
- * @cmd: Payload command
- * @payload_length: Length of the signed message payload
- *
- * Fill the signed message block fields that are not fixed.
- */
-void ele_devmgr_fill_msg_block(void *msg, unsigned char cmd,
-			       unsigned int payload_length);
-
-/**
- * ele_devmgr_get_msg_block_length() - Return the signed message block length
- *
- * Return:
- * Length of signed message block
- */
-unsigned int ele_devmgr_get_msg_block_length(void);
 
 #endif /* __LOCAL_H__ */

core/subsystems/ele/devmgr/reprovisioning.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2024 NXP
+ * Copyright 2024-2025 NXP
  */
 
 #include <time.h>
@@ -52,7 +52,7 @@ static int device_repro_prepare(struct subsystem_context *ele_ctx,
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	msg_block_length = ele_devmgr_get_msg_block_length();
+	msg_block_length = ele_get_sign_msg_block_length();
 	msg_length = msg_block_length + PAYLOAD_LENGTH;
 	msg = smw_devmgr_get_reprovision_data(args);
 	if (!msg) {
@@ -66,7 +66,7 @@ static int device_repro_prepare(struct subsystem_context *ele_ctx,
 		goto end;
 	}
 
-	ele_devmgr_fill_msg_block(msg, COMMAND, PAYLOAD_LENGTH);
+	ele_fill_sign_msg_block(msg, COMMAND, PAYLOAD_LENGTH);
 
 	status = fw_info_operation(&ele_ctx->hdl, &fw_info);
 	if (status == SMW_STATUS_OK) {

core/subsystems/ele/key_group.c
@@ -1,0 +1,136 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2025 NXP
+ */
+
+#include "utils.h"
+
+#include "key_group.h"
+
+struct key_group {
+	bool persistent;
+	bool full;
+};
+
+static int append_key_group(struct smw_utils_list *key_grp_list,
+			    unsigned int grp, bool persistent, bool full)
+{
+	int status = SMW_STATUS_ALLOC_FAILURE;
+	struct key_group *key_grp = NULL;
+
+	key_grp = SMW_UTILS_MALLOC(sizeof(*key_grp));
+	if (key_grp) {
+		key_grp->persistent = persistent;
+		key_grp->full = full;
+		if (!smw_utils_list_append_data(key_grp_list, key_grp, grp,
+						NULL)) {
+			SMW_UTILS_FREE(key_grp);
+			status = SMW_STATUS_ALLOC_FAILURE;
+		} else {
+			status = SMW_STATUS_OK;
+		}
+	}
+
+	return status;
+}
+
+int ele_get_key_group(struct subsystem_context *ele_ctx, bool persistent,
+		      unsigned int *out_grp)
+{
+	int status = SMW_STATUS_OPERATION_FAILURE;
+	int status_mutex = SMW_STATUS_OK;
+
+	struct node *node = NULL;
+	struct key_group *key_grp = NULL;
+	unsigned int grp = 0;
+	unsigned int first_grp = *out_grp;
+	unsigned int last_grp = ELE_LAST_TRANSIENT_KEY_GROUP;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (smw_utils_mutex_lock(ele_ctx->key_grp_mutex)) {
+		status_mutex = SMW_STATUS_MUTEX_LOCK_FAILURE;
+		goto end;
+	}
+
+	if (persistent)
+		last_grp = ELE_LAST_PERSISTENT_KEY_GROUP;
+
+	for (grp = first_grp; grp <= last_grp; grp++) {
+		node = smw_utils_list_find_first(&ele_ctx->key_grp_list, &grp);
+		if (node) {
+			key_grp = smw_utils_list_get_data(node);
+			if (!key_grp)
+				break;
+
+			if (key_grp->persistent == persistent &&
+			    !key_grp->full) {
+				*out_grp = grp;
+				status = SMW_STATUS_OK;
+				break;
+			}
+		} else {
+			/* Create a new node entry in the list */
+			status = append_key_group(&ele_ctx->key_grp_list, grp,
+						  persistent, false);
+			break;
+		}
+	}
+
+	if (smw_utils_mutex_unlock(ele_ctx->key_grp_mutex))
+		status_mutex = SMW_STATUS_MUTEX_UNLOCK_FAILURE;
+
+end:
+	if (status == SMW_STATUS_OK)
+		status = status_mutex;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s key group #%d returned %d\n", __func__,
+		       *out_grp, status);
+	return status;
+}
+
+int ele_set_key_group_state(struct subsystem_context *ele_ctx, unsigned int grp,
+			    bool persistent, bool full)
+{
+	int status = SMW_STATUS_OK;
+	int status_mutex = SMW_STATUS_OK;
+
+	struct node *node = NULL;
+	struct key_group *key_grp = NULL;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (smw_utils_mutex_lock(ele_ctx->key_grp_mutex)) {
+		status_mutex = SMW_STATUS_MUTEX_LOCK_FAILURE;
+		goto end;
+	}
+
+	node = smw_utils_list_find_first(&ele_ctx->key_grp_list, &grp);
+	if (node) {
+		key_grp = smw_utils_list_get_data(node);
+		if (key_grp && key_grp->persistent == persistent) {
+			key_grp->full = full;
+		} else {
+			SMW_DBG_PRINTF(ERROR,
+				       "%s: key group %u list data error (%p)\n",
+				       __func__, grp, key_grp);
+			status = SMW_STATUS_OPERATION_FAILURE;
+		}
+
+	} else {
+		/* Create a new node entry in the list */
+		status = append_key_group(&ele_ctx->key_grp_list, grp,
+					  persistent, full);
+	}
+
+	if (smw_utils_mutex_unlock(ele_ctx->key_grp_mutex))
+		status_mutex = SMW_STATUS_MUTEX_UNLOCK_FAILURE;
+
+end:
+	if (status == SMW_STATUS_OK)
+		status = status_mutex;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s key group #%d returned %d\n", __func__, grp,
+		       status);
+	return status;
+}

core/subsystems/ele/key_group.h
@@ -1,0 +1,23 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2025 NXP
+ */
+
+#ifndef __KEY_GROUP_H__
+#define __KEY_GROUP_H__
+
+#include "common.h"
+
+#define ELE_UNDEFINED_KEY_GROUP	       UINT16_MAX
+#define ELE_MAX_KEY_GROUP	       100U
+#define ELE_FIRST_PERSISTENT_KEY_GROUP 0U
+#define ELE_FIRST_TRANSIENT_KEY_GROUP  (ELE_MAX_KEY_GROUP / 2)
+#define ELE_LAST_PERSISTENT_KEY_GROUP  (ELE_FIRST_TRANSIENT_KEY_GROUP - 1)
+#define ELE_LAST_TRANSIENT_KEY_GROUP   (ELE_MAX_KEY_GROUP - 1)
+
+int ele_get_key_group(struct subsystem_context *ele_ctx, bool persistent,
+		      unsigned int *out_grp);
+int ele_set_key_group_state(struct subsystem_context *ele_ctx, unsigned int grp,
+			    bool persistent, bool full);
+
+#endif /* __KEY_GROUP_H__ */

commit 280a5287de2c8ac86e4f24072b51fa4ea936ad71

SSMW-984 subsystems: ele: Fix OEM Master key synchronization flag
Ensure that the key exchange operation synchronization flag is set when
OEM Master key is persistent.

Fixes: 289024ae4374 ("SSMW-497 subsystems: ele: Add ELE key import with OEM Master key derivation")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/keymgr_derive_oem_mk.c
@@ -226,6 +226,8 @@ static int derive_oem_master_key(struct subsystem_context *ele_ctx,
 	hsm_err_t err = HSM_NO_ERROR;
 	hsm_hdl_t key_mgt_hdl = 0;
 	op_key_exchange_args_t key_ex_args = { 0 };
+
+	unsigned char *msg = NULL;
 	struct payload_oem_mk *payload = NULL;
 
 	status = ele_is_oem_srkh_fused(ele_ctx, &srkh_fused);
@@ -237,13 +239,21 @@ static int derive_oem_master_key(struct subsystem_context *ele_ctx,
 		goto end;
 	}
 
-	payload = (struct payload_oem_mk *)smw_keymgr_oem_get_payload(args);
-	if (!payload) {
+	msg = smw_keymgr_oem_get_payload(args);
+	if (!msg) {
 		status = SMW_STATUS_INVALID_PARAM;
 		goto end;
 	}
 
 	key_ex_args.flags = HSM_OP_KEY_EXCHANGE_FLAGS_INPUT_SIGNED_CONTENT;
+
+	payload = (void *)msg + ele_get_sign_msg_block_length();
+
+	if (payload->derived_key_lifetime & HSM_SE_KEY_STORAGE_PERSISTENT ||
+	    payload->derived_key_lifetime & HSM_SE_KEY_STORAGE_PERS_PERM) {
+		key_ex_args.flags |= HSM_OP_KEY_EXCHANGE_FLAGS_STRICT_OPERATION;
+	}
+
 	key_ex_args.in_content = smw_keymgr_oem_get_payload(args);
 	key_ex_args.in_content_sz = smw_keymgr_oem_get_payload_len(args);
 	key_ex_args.in_pub_buffer = args->ops.get_peer(args);
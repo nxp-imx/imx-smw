nxp-imx/imx-smw

commit 58af47ff666641c24ddf917a0c8422fac04cade1

SSMW-948 pkcs11: Encode or decode Object ID
Support the data CKA_OBJECT_ID encoding/decoding ASN1 base128 format.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/utils/util.c
@@ -26,6 +26,50 @@ static const char encoding_table[] = {
 /* Mask used to avoid encoding table buffer over-read */
 #define ENC_MAX_ARRAY_MASK (ARRAY_SIZE(encoding_table) - 1)
 
+static CK_BYTE divide_by_128(CK_BYTE_PTR bytes, size_t len)
+{
+	unsigned int remainder = 0;
+	unsigned int current = 0;
+	size_t i = 0;
+
+	for (; i < len; i++) {
+		current = (remainder << 8) | bytes[i];
+		bytes[i] = (current >> 7) & UINT8_MAX;
+		remainder = current % 128;
+	}
+
+	return remainder & UINT8_MAX;
+}
+
+static void multiply_add_128(CK_BYTE_PTR bytes, size_t len, CK_BYTE value)
+{
+	unsigned int carry = value;
+	unsigned int tmp = 0;
+	size_t i = len;
+
+	for (; i > 0; i--) {
+		tmp = bytes[i - 1] << 7;
+
+		if (ADD_OVERFLOW(tmp, carry, &tmp))
+			tmp = 0;
+
+		bytes[i - 1] = tmp & 0xFF;
+		carry = tmp >> 8;
+	}
+}
+
+static int is_zero(CK_BYTE_PTR bytes, size_t len)
+{
+	size_t i = 0;
+
+	for (; i < len; i++) {
+		if (bytes[i] != 0)
+			return 0;
+	}
+
+	return 1;
+}
+
 static size_t get_b64_from_hex_len(size_t hex_len)
 {
 	size_t b64_len = 0;
@@ -398,4 +442,96 @@ exit:
 		free(tmp_hex);
 
 	return ret;
+}
+
+CK_RV util_base128_encode(struct libbytes *out, struct libbytes *in)
+{
+	CK_RV ret = CKR_DATA_INVALID;
+	CK_BYTE *work = NULL;
+	CK_BYTE tmp = 0;
+	size_t i = 0;
+
+	if (!in->number)
+		goto end;
+
+	work = malloc(in->number);
+	if (!work) {
+		DBG_TRACE("Allocation error");
+		ret = CKR_HOST_MEMORY;
+		goto end;
+	}
+
+	/* First pass, calculate the out->number */
+	memcpy(work, in->array, in->number);
+
+	out->number = 0;
+	while (!is_zero(work, in->number)) {
+		(void)divide_by_128(work, in->number);
+		if (INC_OVERFLOW(out->number, 1)) {
+			ret = CKR_DATA_INVALID;
+			goto end;
+		}
+	}
+
+	if (!out->number) {
+		ret = CKR_DATA_INVALID;
+		goto end;
+	}
+
+	/* Allocate the out->array buffer */
+	out->array = malloc(out->number);
+	if (!out->array) {
+		DBG_TRACE("Allocation error");
+		ret = CKR_HOST_MEMORY;
+		goto end;
+	}
+
+	/* Second pass, fill the out->array */
+	memcpy(work, in->array, in->number);
+
+	while (!is_zero(work, in->number))
+		out->array[i++] = divide_by_128(work, in->number);
+
+	/* Reverse in-place and apply continuation bits after */
+	for (i = 0; i < out->number / 2; i++) {
+		tmp = out->array[i];
+		out->array[i] = out->array[out->number - i - 1];
+		out->array[out->number - i - 1] = tmp;
+	}
+
+	for (i = 0; i < out->number - 1; i++)
+		out->array[i] |= 0x80;
+
+	ret = CKR_OK;
+
+end:
+	if (work)
+		free(work);
+
+	return ret;
+}
+
+CK_RV util_base128_decode(struct libbytes *out, struct libbytes *in)
+{
+	CK_BYTE byte = 0;
+	size_t i = 0;
+
+	if (!in->number)
+		return CKR_DATA_INVALID;
+
+	out->number = in->number;
+
+	/* Allocate the out->array buffer */
+	out->array = calloc(out->number, 1);
+	if (!out->array) {
+		DBG_TRACE("Allocation error");
+		return CKR_HOST_MEMORY;
+	}
+
+	for (; i < in->number; i++) {
+		byte = in->array[i] & 0x7F;
+		multiply_add_128(out->array, out->number, byte);
+	}
+
+	return CKR_OK;
 }

pkcs11/src/ifsmw/data.c
@@ -11,6 +11,7 @@
 #include "smw/names.h"
 
 #include "util.h"
+#include "util_asn1.h"
 #include "trace.h"
 
 #include "lib_session.h"
@@ -31,19 +32,37 @@ static int store_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
 	struct smw_store_data_args args = { 0 };
 	struct smw_data_descriptor data_descriptor = { 0 };
 	struct libobj_data *data = get_subobj_from(obj, storage);
+	struct libbytes data_id = { 0 };
 
 	ret = libsess_get_slotid(hsession, &slotid);
 	if (ret != CKR_OK)
-		return ret;
+		goto end;
 
 	devinfo = libdev_get_devinfo(slotid);
-	if (!devinfo)
-		return CKR_SLOT_ID_INVALID;
+	if (!devinfo) {
+		ret = CKR_SLOT_ID_INVALID;
+		goto end;
+	}
+
+	if (data->id.array) {
+		/* User defined a data id */
+		ret = util_asn1_decode_object_id(&data_id, &data->id);
+		if (ret != CKR_OK)
+			goto end;
+
+		if (TO_INT_BE(data_descriptor.identifier, data_id.array,
+			      data_id.number)) {
+			ret = CKR_FUNCTION_FAILED;
+			goto end;
+		}
+	}
 
 	data_descriptor.data = data->value.array;
 
-	if (SET_OVERFLOW(data->value.number, data_descriptor.length))
-		return CKR_FUNCTION_FAILED;
+	if (SET_OVERFLOW(data->value.number, data_descriptor.length)) {
+		ret = CKR_FUNCTION_FAILED;
+		goto end;
+	}
 
 	args_attr_obj_storage(&data_descriptor.attributes.attributes, obj);
 
@@ -55,6 +74,10 @@ static int store_data(CK_SESSION_HANDLE hsession, struct libobj_obj *obj)
 
 	if (status == SMW_STATUS_OK)
 		set_data_token_id(obj, data_descriptor.identifier);
+
+end:
+	if (data_id.array)
+		free(data_id.array);
 
 	return ret;
 }

pkcs11/src/ifsmw/object_desc.c
@@ -18,6 +18,7 @@
 
 #include "key_desc.h"
 #include "object_desc.h"
+#include "util_asn1.h"
 
 #include "trace.h"
 
@@ -154,6 +155,7 @@ static CK_RV attrs_to_object_descriptor(struct smw_object_descriptor *desc,
 	struct librfc2279 unique_id = { 0 };
 	struct librfc2279 label = { 0 };
 	struct libbytes user_id = { 0 };
+	struct libbytes obj_id = { 0 };
 	struct libbytes ec_params = { 0 };
 	unsigned int i = 0;
 	size_t len = 0;
@@ -161,6 +163,18 @@ static CK_RV attrs_to_object_descriptor(struct smw_object_descriptor *desc,
 	for (; i < nb_attrs; i++) {
 		switch (attrs[i].type) {
 		case CKA_OBJECT_ID:
+			if (user_id.array) {
+				ret = CKR_ARGUMENTS_BAD;
+				break;
+			}
+
+			ret = attr_to_byte_array(&obj_id, &attrs[i]);
+			if (ret != CKR_OK)
+				break;
+
+			ret = util_asn1_decode_object_id(&user_id, &obj_id);
+			break;
+
 		case CKA_ID:
 			if (user_id.array)
 				ret = CKR_ARGUMENTS_BAD;
@@ -259,6 +273,9 @@ end:
 	if (user_id.array)
 		free(user_id.array);
 
+	if (obj_id.array)
+		free(obj_id.array);
+
 	if (label.string)
 		free(label.string);
 
@@ -286,6 +303,7 @@ static CK_RV object_descriptor_to_attrs(struct smw_object_descriptor *desc,
 	CK_ULONG obj_length = 0;
 	struct libbytes label = { 0 };
 	struct libbytes user_id = { 0 };
+	struct libbytes obj_id = { 0 };
 	CK_ULONG nb_attrs = 0;
 	CK_ATTRIBUTE_PTR p_attr = NULL;
 	struct smw_key_descriptor *key = &desc->key;
@@ -431,20 +449,46 @@ static CK_RV object_descriptor_to_attrs(struct smw_object_descriptor *desc,
 
 	if (desc->user_id) {
 		if (p_attr) {
-			if (object_class == CKO_DATA)
-				p_attr->type = CKA_OBJECT_ID;
-			else
-				p_attr->type = CKA_ID;
-
 			ret = util_base64_decode(&user_id, desc->user_id);
 			if (ret != CKR_OK)
 				goto end;
 
-			if (p_attr->ulValueLen < user_id.number)
-				p_attr->ulValueLen = user_id.number;
-			else
-				memcpy(p_attr->pValue, user_id.array,
-				       p_attr->ulValueLen);
+			switch (object_class) {
+			case CKO_DATA:
+				p_attr->type = CKA_OBJECT_ID;
+				ret = util_asn1_encode_object_id(&obj_id,
+								 &user_id);
+				if (ret != CKR_OK) {
+					if (user_id.array)
+						free(user_id.array);
+
+					goto end;
+				}
+
+				if (p_attr->ulValueLen < obj_id.number) {
+					p_attr->ulValueLen = obj_id.number;
+				} else {
+					memcpy(p_attr->pValue, obj_id.array,
+					       p_attr->ulValueLen);
+				}
+
+				if (obj_id.array)
+					free(obj_id.array);
+
+				break;
+
+			default:
+				p_attr->type = CKA_ID;
+
+				if (p_attr->ulValueLen < user_id.number) {
+					p_attr->ulValueLen = user_id.number;
+				} else {
+					memcpy(p_attr->pValue, user_id.array,
+					       p_attr->ulValueLen);
+				}
+
+				break;
+			}
 
 			if (user_id.array)
 				free(user_id.array);
@@ -632,8 +676,9 @@ CK_RV obj_db_update(struct libobj_obj *obj)
 	case CKO_DATA:
 		data = get_subobj_from(obj, storage);
 		if (data && data->id.number) {
-			user_id.number = data->id.number;
-			user_id.array = data->id.array;
+			ret = util_asn1_decode_object_id(&user_id, &data->id);
+			if (ret != CKR_OK)
+				goto end;
 		}
 
 		break;
@@ -669,6 +714,9 @@ CK_RV obj_db_update(struct libobj_obj *obj)
 end:
 	cleanup_smw_object_descriptor(&descriptor);
 
+	if (obj->class == CKO_DATA && user_id.array)
+		free(user_id.array);
+
 	return ret;
 }
 
@@ -686,8 +734,8 @@ CK_RV obj_db_retrieve_obj(CK_SESSION_HANDLE hsession,
 		return ret;
 
 	/* Build the attributes template to create the PKCS11 object */
-	ret = object_descriptor_to_attrs(descriptor, object_class,
-					 &attributes, &attributes_count);
+	ret = object_descriptor_to_attrs(descriptor, object_class, &attributes,
+					 &attributes_count);
 	if (ret != CKR_OK)
 		return ret;
 

pkcs11/src/include/util.h
@@ -23,19 +23,39 @@
 	({                                                                     \
 		__typeof__(out) _out = 0;                                      \
 		__typeof__(buf) _buf = (buf);                                  \
-		size_t i = len;                                                \
-		int ret = 0;                                                   \
-		if (i > sizeof(_out)) {                                        \
-			ret = 1;                                               \
+		size_t _i = (len);                                             \
+		int _ret = 0;                                                  \
+		if (_i > sizeof(_out)) {                                       \
+			_ret = 1;                                              \
 		} else {                                                       \
-			_out = _buf[--i] & UINT8_MAX;                          \
-			for (; i; i--) {                                       \
+			_out = _buf[--_i] & UINT8_MAX;                         \
+			for (; _i; _i--) {                                     \
 				_out <<= 8;                                    \
-				_out |= _buf[i - 1] & UINT8_MAX;               \
+				_out |= _buf[_i - 1] & UINT8_MAX;              \
 			}                                                      \
 			out = _out;                                            \
 		}                                                              \
-		ret;                                                           \
+		_ret;                                                          \
+	})
+
+#define TO_INT_BE(out, buf, len)                                               \
+	({                                                                     \
+		__typeof__(out) _out = 0;                                      \
+		__typeof__(buf) _buf = (buf);                                  \
+		size_t _i = 0;                                                 \
+		size_t _maxlen = (len);                                        \
+		int _ret = 0;                                                  \
+		if (_maxlen > sizeof(_out)) {                                  \
+			_ret = 1;                                              \
+		} else {                                                       \
+			_out = _buf[_i++] & UINT8_MAX;                         \
+			for (; _i < _maxlen; _i++) {                           \
+				_out <<= 8;                                    \
+				_out |= _buf[_i] & UINT8_MAX;                  \
+			}                                                      \
+			out = _out;                                            \
+		}                                                              \
+		_ret;                                                          \
 	})
 
 /**
@@ -146,5 +166,33 @@ CK_RV util_base64_encode(char **base64, struct libbytes *src);
  * CKR_HOST_MEMORY      - Out of memory
  */
 CK_RV util_base64_decode(struct libbytes *out, const char *base64);
+
+/**
+ * util_base128_encode() - Encode big-endian byte array in base128 input
+ * @out: [out] Base128 byte array
+ * @in: [in] Big-endian byte array
+ *
+ * Function allocates the @out->array, user must free it.
+ *
+ * return:
+ * CKR_OK               - Success
+ * CKR_HOST_MEMORY      - Out of memory
+ * CKR_DATA_INVALID     - Input buffer invalid
+ */
+CK_RV util_base128_encode(struct libbytes *out, struct libbytes *in);
+
+/**
+ * util_base128_decode() - Decode base128 input into big-endian byte array
+ * @out: [out] Big-endian byte array
+ * @in: [in] Base128 byte array
+ *
+ * Function allocates the @out->array, user must free it.
+ *
+ * return:
+ * CKR_OK               - Success
+ * CKR_HOST_MEMORY      - Out of memory
+ * CKR_DATA_INVALID     - Input buffer invalid
+ */
+CK_RV util_base128_decode(struct libbytes *out, struct libbytes *in);
 
 #endif /* __UTIL_H__ */

pkcs11/src/include/util_asn1.h
@@ -48,10 +48,11 @@ struct curve_def {
 #define ASN1_OBJECT_IDENTIFIER 6
 #define ASN1_OCTET_STRING_TAG  0x04
 
-/*
- * DER ANSI X9.62 Uncompress key tag
- */
+/* DER ANSI X9.62 Uncompress key tag */
 #define ANSI_UNCOMPRESS_KEY_TAG 0x04
+
+/* DER OID ISO tag */
+#define ANSI_ISO_MEMBER_BODY_TAG 0x2A
 
 /**
  * util_asn1_ec_params_to_curve() - Convert EC parameters to a defined EC curve
@@ -137,5 +138,29 @@ CK_RV util_asn1_decode_octet_string(uint8_t *in, size_t inlen, uint8_t *out,
  */
 CK_RV util_asn1_get_field_octet_string(uint8_t *in, size_t inlen, uint8_t **out,
 				       size_t *outlen);
+/**
+ * util_asn1_encode_object_id() - Encode an OBJECT IDENTIFIER
+ * @out: [out] Encoded value
+ * @id: [in] Identifier to encode
+ *
+ * return:
+ * CKR_OK                        - Success
+ * CKR_ARGUMENTS_BAD             - Bad arguments
+ * CKR_HOST_MEMORY               - Out of memory
+ */
+CK_RV util_asn1_encode_object_id(struct libbytes *out, struct libbytes *in);
+
+/**
+ * util_asn1_decode_object_id() - Decode an OBJECT IDENTIFIER
+ * @out: [out] Decoded value
+ * @id: [in] ASN1 object identifier to decode
+ *
+ * return:
+ * CKR_OK                        - Success
+ * CKR_ARGUMENTS_BAD             - Bad arguments
+ * CKR_DATA_INVALID              - Input buffer invalid
+ * CKR_HOST_MEMORY               - Out of memory
+ */
+CK_RV util_asn1_decode_object_id(struct libbytes *out, struct libbytes *in);
 
 #endif /* __UTIL_ASN1_H__ */

pkcs11/src/objects/attributes.c
@@ -10,7 +10,6 @@
 
 #include "trace.h"
 #include "util.h"
-#include "util_asn1.h"
 
 /**
  * obj_find_attr() - Find an attribute type in object template

pkcs11/src/utils/asn1.c
@@ -444,3 +444,66 @@ CK_RV util_asn1_get_field_octet_string(uint8_t *in, size_t inlen, uint8_t **out,
 end:
 	return ret;
 }
+
+CK_RV util_asn1_encode_object_id(struct libbytes *out, struct libbytes *in)
+{
+	CK_RV ret = CKR_OK;
+	struct libbytes tmp = { 0 };
+
+	if (!out || !in)
+		return CKR_ARGUMENTS_BAD;
+
+	if (!in->number || !in->array)
+		return CKR_ARGUMENTS_BAD;
+
+	ret = util_base128_encode(&tmp, in);
+	if (ret != CKR_OK)
+		return ret;
+
+	out->number = 1 + tmp.number;
+	out->array = malloc(out->number);
+	if (out->array) {
+		out->array[0] = ANSI_ISO_MEMBER_BODY_TAG;
+		memcpy(&out->array[1], tmp.array, tmp.number);
+	} else {
+		ret = CKR_HOST_MEMORY;
+	}
+
+	free(tmp.array);
+
+	return ret;
+}
+
+CK_RV util_asn1_decode_object_id(struct libbytes *out, struct libbytes *in)
+{
+	CK_RV ret = CKR_OK;
+	unsigned int tag = 0;
+	struct libbytes tmp = { 0 };
+
+	if (!out || !in)
+		return CKR_ARGUMENTS_BAD;
+
+	if (!in->number || !in->array)
+		return CKR_ARGUMENTS_BAD;
+
+	if (in->number < 2)
+		return CKR_DATA_INVALID;
+
+	tag = in->array[0];
+	/* TAG value first byte can be 0, 1 or 2 */
+	tag /= 40;
+	if (tag > 2)
+		return CKR_DATA_INVALID;
+
+	/* TAG value second byte must be equal to 2 */
+	tag = in->array[0] - tag * 40;
+	if (tag != 2)
+		return CKR_DATA_INVALID;
+
+	tmp.array = &in->array[1];
+	tmp.number = in->number - 1;
+
+	ret = util_base128_decode(out, &tmp);
+
+	return ret;
+}

commit 1e2f18eef8be52f9c1343b1ea97b2b75629d3fe2

SSMW-983 pkcs11: Fix CKA_OBJECT_ID base128 decoding
Fix CKA_OBJECT_ID base128 decoding to handle high id.

Fixes: 58af47ff6666 ("SSMW-948 pkcs11: Encode or decode Object ID")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/utils/util.c
@@ -41,20 +41,25 @@ static CK_BYTE divide_by_128(CK_BYTE_PTR bytes, size_t len)
 	return remainder & UINT8_MAX;
 }
 
-static void multiply_add_128(CK_BYTE_PTR bytes, size_t len, CK_BYTE value)
+static void multiply_add_128(CK_BYTE_PTR bytes, size_t *len, CK_BYTE value)
 {
 	unsigned int carry = value;
 	unsigned int tmp = 0;
-	size_t i = len;
+	size_t i = 0;
 
-	for (; i > 0; i--) {
-		tmp = bytes[i - 1] << 7;
+	for (; i < *len; i++) {
+		tmp = bytes[i] << 7;
 
-		if (ADD_OVERFLOW(tmp, carry, &tmp))
-			tmp = 0;
+		if (ADD_OVERFLOW(tmp, carry, &carry))
+			carry = 0;
 
-		bytes[i - 1] = tmp & 0xFF;
-		carry = tmp >> 8;
+		bytes[i] = carry & 0xFF;
+		carry >>= 8;
+	}
+
+	if (carry > 0) {
+		bytes[*len] = carry & 0xFF;
+		(*len)++;
 	}
 }
 
@@ -513,25 +518,54 @@ end:
 
 CK_RV util_base128_decode(struct libbytes *out, struct libbytes *in)
 {
+	CK_RV ret = CKR_DATA_INVALID;
 	CK_BYTE byte = 0;
 	size_t i = 0;
+	CK_BYTE_PTR tmp = NULL;
+	size_t tmp_size = 0;
 
 	if (!in->number)
-		return CKR_DATA_INVALID;
+		goto end;
 
-	out->number = in->number;
+	if (MUL_OVERFLOW(in->number, 2, &tmp_size)) {
+		ret = CKR_FUNCTION_FAILED;
+		goto end;
+	}
+
+	tmp = calloc(1, tmp_size);
+	if (!tmp) {
+		DBG_TRACE("Allocation error");
+		ret = CKR_HOST_MEMORY;
+		goto end;
+	}
+
+	tmp_size = 1;
+	for (; i < in->number; i++) {
+		byte = in->array[i] & 0x7F;
+		multiply_add_128(tmp, &tmp_size, byte);
+
+		if (!(in->array[i] & 0x80))
+			break;
+	}
+
+	out->number = tmp_size;
 
 	/* Allocate the out->array buffer */
 	out->array = calloc(out->number, 1);
 	if (!out->array) {
 		DBG_TRACE("Allocation error");
-		return CKR_HOST_MEMORY;
+		ret = CKR_HOST_MEMORY;
+		goto end;
 	}
 
-	for (; i < in->number; i++) {
-		byte = in->array[i] & 0x7F;
-		multiply_add_128(out->array, out->number, byte);
-	}
+	for (i = 0; i < out->number; i++)
+		out->array[i] = tmp[out->number - i - 1];
 
-	return CKR_OK;
+	ret = CKR_OK;
+
+end:
+	if (tmp)
+		free(tmp);
+
+	return ret;
 }
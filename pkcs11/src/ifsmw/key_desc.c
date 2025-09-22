nxp-imx/imx-smw

commit ad0047fdc2c2b046f51a1956fdca05daa1c47505

SSMW-573 pkcs11: Add support for the edwards 25519 key
Add the support for the edwards 25519 key.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/ifsmw/key_desc.c
@@ -52,18 +52,54 @@ const CK_BYTE brainpoolP384r1[] = ASN1_OID_BRAINPOOL_P384R1;
 const CK_BYTE brainpoolP384t1[] = ASN1_OID_BRAINPOOL_P384T1;
 const CK_BYTE brainpoolP512r1[] = ASN1_OID_BRAINPOOL_P512R1;
 const CK_BYTE brainpoolP512t1[] = ASN1_OID_BRAINPOOL_P512T1;
+const CK_BYTE ed25519[] = ASN1_OID_ED25519;
+
+enum name_ec_key {
+	EC_PRIME192V1,
+	EC_PRIME256V1,
+	EC_SECP224R1,
+	EC_SECP384R1,
+	EC_SECP521R1,
+	EC_BRAINPOOL160R1,
+	EC_BRAINPOOL160T1,
+	EC_BRAINPOOL192R1,
+	EC_BRAINPOOL192T1,
+	EC_BRAINPOOL224R1,
+	EC_BRAINPOOL224T1,
+	EC_BRAINPOOL256R1,
+	EC_BRAINPOOL256T1,
+	EC_BRAINPOOL320R1,
+	EC_BRAINPOOL320T1,
+	EC_BRAINPOOL384R1,
+	EC_BRAINPOOL384T1,
+	EC_BRAINPOOL512R1,
+	EC_BRAINPOOL512T1,
+	EC_ED25519,
+	EC_NB_KEY_NAME
+};
 
 const struct asn1_curve_def ec_asn1_curves[] = {
-	EC_ASN1_CURVE(prime192v1),	EC_ASN1_CURVE(prime256v1),
-	EC_ASN1_CURVE(secp224r1),	EC_ASN1_CURVE(secp384r1),
-	EC_ASN1_CURVE(secp521r1),	EC_ASN1_CURVE(brainpoolP160r1),
-	EC_ASN1_CURVE(brainpoolP160t1), EC_ASN1_CURVE(brainpoolP192r1),
-	EC_ASN1_CURVE(brainpoolP192t1), EC_ASN1_CURVE(brainpoolP224r1),
-	EC_ASN1_CURVE(brainpoolP224t1), EC_ASN1_CURVE(brainpoolP256r1),
-	EC_ASN1_CURVE(brainpoolP256t1), EC_ASN1_CURVE(brainpoolP320r1),
-	EC_ASN1_CURVE(brainpoolP320t1), EC_ASN1_CURVE(brainpoolP384r1),
-	EC_ASN1_CURVE(brainpoolP384t1), EC_ASN1_CURVE(brainpoolP512r1),
-	EC_ASN1_CURVE(brainpoolP512t1), { 0 }
+	[EC_PRIME192V1] = EC_ASN1_CURVE(prime192v1),
+	[EC_PRIME256V1] = EC_ASN1_CURVE(prime256v1),
+	[EC_SECP224R1] = EC_ASN1_CURVE(secp224r1),
+	[EC_SECP384R1] = EC_ASN1_CURVE(secp384r1),
+	[EC_SECP521R1] = EC_ASN1_CURVE(secp521r1),
+	[EC_BRAINPOOL160R1] = EC_ASN1_CURVE(brainpoolP160r1),
+	[EC_BRAINPOOL160T1] = EC_ASN1_CURVE(brainpoolP160t1),
+	[EC_BRAINPOOL192R1] = EC_ASN1_CURVE(brainpoolP192r1),
+	[EC_BRAINPOOL192T1] = EC_ASN1_CURVE(brainpoolP192t1),
+	[EC_BRAINPOOL224R1] = EC_ASN1_CURVE(brainpoolP224r1),
+	[EC_BRAINPOOL224T1] = EC_ASN1_CURVE(brainpoolP224t1),
+	[EC_BRAINPOOL256R1] = EC_ASN1_CURVE(brainpoolP256r1),
+	[EC_BRAINPOOL256T1] = EC_ASN1_CURVE(brainpoolP256t1),
+	[EC_BRAINPOOL320R1] = EC_ASN1_CURVE(brainpoolP320r1),
+	[EC_BRAINPOOL320T1] = EC_ASN1_CURVE(brainpoolP320t1),
+	[EC_BRAINPOOL384R1] = EC_ASN1_CURVE(brainpoolP384r1),
+	[EC_BRAINPOOL384T1] = EC_ASN1_CURVE(brainpoolP384t1),
+	[EC_BRAINPOOL512R1] = EC_ASN1_CURVE(brainpoolP512r1),
+	[EC_BRAINPOOL512T1] = EC_ASN1_CURVE(brainpoolP512t1),
+	[EC_ED25519] = EC_ASN1_CURVE(ed25519),
+	[EC_NB_KEY_NAME] = { 0 }
 };
 
 /**
@@ -84,49 +120,63 @@ struct dev_curve_def {
 	}
 
 const struct dev_curve_def ec_smw_curves[] = {
-	EC_SMW_CURVE(SECP_R1, 192),	 EC_SMW_CURVE(SECP_R1, 256),
-	EC_SMW_CURVE(SECP_R1, 224),	 EC_SMW_CURVE(SECP_R1, 384),
-	EC_SMW_CURVE(SECP_R1, 521),	 EC_SMW_CURVE(BRAINPOOL_R1, 160),
-	EC_SMW_CURVE(BRAINPOOL_T1, 160), EC_SMW_CURVE(BRAINPOOL_R1, 192),
-	EC_SMW_CURVE(BRAINPOOL_T1, 192), EC_SMW_CURVE(BRAINPOOL_R1, 224),
-	EC_SMW_CURVE(BRAINPOOL_T1, 224), EC_SMW_CURVE(BRAINPOOL_R1, 256),
-	EC_SMW_CURVE(BRAINPOOL_T1, 256), EC_SMW_CURVE(BRAINPOOL_R1, 320),
-	EC_SMW_CURVE(BRAINPOOL_T1, 320), EC_SMW_CURVE(BRAINPOOL_R1, 384),
-	EC_SMW_CURVE(BRAINPOOL_T1, 384), EC_SMW_CURVE(BRAINPOOL_R1, 512),
-	EC_SMW_CURVE(BRAINPOOL_T1, 512), { 0 }
+	[EC_PRIME192V1] = EC_SMW_CURVE(SECP_R1, 192),
+	[EC_PRIME256V1] = EC_SMW_CURVE(SECP_R1, 256),
+	[EC_SECP224R1] = EC_SMW_CURVE(SECP_R1, 224),
+	[EC_SECP384R1] = EC_SMW_CURVE(SECP_R1, 384),
+	[EC_SECP521R1] = EC_SMW_CURVE(SECP_R1, 521),
+	[EC_BRAINPOOL160R1] = EC_SMW_CURVE(BRAINPOOL_R1, 160),
+	[EC_BRAINPOOL160T1] = EC_SMW_CURVE(BRAINPOOL_T1, 160),
+	[EC_BRAINPOOL192R1] = EC_SMW_CURVE(BRAINPOOL_R1, 192),
+	[EC_BRAINPOOL192T1] = EC_SMW_CURVE(BRAINPOOL_T1, 192),
+	[EC_BRAINPOOL224R1] = EC_SMW_CURVE(BRAINPOOL_R1, 224),
+	[EC_BRAINPOOL224T1] = EC_SMW_CURVE(BRAINPOOL_T1, 224),
+	[EC_BRAINPOOL256R1] = EC_SMW_CURVE(BRAINPOOL_R1, 256),
+	[EC_BRAINPOOL256T1] = EC_SMW_CURVE(BRAINPOOL_T1, 256),
+	[EC_BRAINPOOL320R1] = EC_SMW_CURVE(BRAINPOOL_R1, 320),
+	[EC_BRAINPOOL320T1] = EC_SMW_CURVE(BRAINPOOL_T1, 320),
+	[EC_BRAINPOOL384R1] = EC_SMW_CURVE(BRAINPOOL_R1, 384),
+	[EC_BRAINPOOL384T1] = EC_SMW_CURVE(BRAINPOOL_T1, 384),
+	[EC_BRAINPOOL512R1] = EC_SMW_CURVE(BRAINPOOL_R1, 512),
+	[EC_BRAINPOOL512T1] = EC_SMW_CURVE(BRAINPOOL_T1, 512),
+	[EC_ED25519] = EC_SMW_CURVE(ED25519, 255),
+	[EC_NB_KEY_NAME] = { 0 }
 };
 
 /*
- * Definition of the ASN1 EC Curves supported (function )
+ * Definition of the ASN1 EC Curves supported
  */
+#define CKK_UNKNOWN_KEY_TYPE CK_UNAVAILABLE_INFORMATION
 
-#define EC_CURVE(_asn1, _smw)                                                  \
+#define EC_CURVE(_key_name, _ck_type)                                          \
 	{                                                                      \
-		.asn1 = _asn1, .dev = _smw                                     \
+		.asn1 = &ec_asn1_curves[EC_##_key_name],                       \
+		.dev = &ec_smw_curves[EC_##_key_name],                         \
+		.ck_key_type = CKK_##_ck_type                                  \
 	}
 
 const struct curve_def ec_curves[] = {
-	EC_CURVE(&ec_asn1_curves[0], &ec_smw_curves[0]),
-	EC_CURVE(&ec_asn1_curves[1], &ec_smw_curves[1]),
-	EC_CURVE(&ec_asn1_curves[2], &ec_smw_curves[2]),
-	EC_CURVE(&ec_asn1_curves[3], &ec_smw_curves[3]),
-	EC_CURVE(&ec_asn1_curves[4], &ec_smw_curves[4]),
-	EC_CURVE(&ec_asn1_curves[5], &ec_smw_curves[5]),
-	EC_CURVE(&ec_asn1_curves[6], &ec_smw_curves[6]),
-	EC_CURVE(&ec_asn1_curves[7], &ec_smw_curves[7]),
-	EC_CURVE(&ec_asn1_curves[8], &ec_smw_curves[8]),
-	EC_CURVE(&ec_asn1_curves[9], &ec_smw_curves[9]),
-	EC_CURVE(&ec_asn1_curves[10], &ec_smw_curves[10]),
-	EC_CURVE(&ec_asn1_curves[11], &ec_smw_curves[11]),
-	EC_CURVE(&ec_asn1_curves[12], &ec_smw_curves[12]),
-	EC_CURVE(&ec_asn1_curves[13], &ec_smw_curves[13]),
-	EC_CURVE(&ec_asn1_curves[14], &ec_smw_curves[14]),
-	EC_CURVE(&ec_asn1_curves[15], &ec_smw_curves[15]),
-	EC_CURVE(&ec_asn1_curves[16], &ec_smw_curves[16]),
-	EC_CURVE(&ec_asn1_curves[17], &ec_smw_curves[17]),
-	EC_CURVE(&ec_asn1_curves[18], &ec_smw_curves[18]),
-	EC_CURVE(&ec_asn1_curves[19], &ec_smw_curves[19]),
-	{ 0 }
+	EC_CURVE(PRIME192V1, EC),
+	EC_CURVE(PRIME256V1, EC),
+	EC_CURVE(SECP224R1, EC),
+	EC_CURVE(SECP384R1, EC),
+	EC_CURVE(SECP521R1, EC),
+	EC_CURVE(BRAINPOOL160R1, EC),
+	EC_CURVE(BRAINPOOL160T1, EC),
+	EC_CURVE(BRAINPOOL192R1, EC),
+	EC_CURVE(BRAINPOOL192T1, EC),
+	EC_CURVE(BRAINPOOL224R1, EC),
+	EC_CURVE(BRAINPOOL224T1, EC),
+	EC_CURVE(BRAINPOOL256R1, EC),
+	EC_CURVE(BRAINPOOL256T1, EC),
+	EC_CURVE(BRAINPOOL320R1, EC),
+	EC_CURVE(BRAINPOOL320T1, EC),
+	EC_CURVE(BRAINPOOL384R1, EC),
+	EC_CURVE(BRAINPOOL384T1, EC),
+	EC_CURVE(BRAINPOOL512R1, EC),
+	EC_CURVE(BRAINPOOL512T1, EC),
+	EC_CURVE(ED25519, EC_EDWARDS),
+	EC_CURVE(NB_KEY_NAME, UNKNOWN_KEY_TYPE),
 };
 
 struct cipher_def {
@@ -261,7 +311,8 @@ static CK_RV get_hmac_type_from_pkcs(smw_key_type_t *key_type_name,
 
 static CK_RV get_ec_curve_from_smw(struct libbytes *ec_params,
 				   smw_key_type_t key_type_name,
-				   unsigned int security_size)
+				   unsigned int security_size,
+				   CK_KEY_TYPE *ck_key_type)
 {
 	CK_RV ret = CKR_KEY_TYPE_INCONSISTENT;
 
@@ -278,6 +329,7 @@ static CK_RV get_ec_curve_from_smw(struct libbytes *ec_params,
 	if (curve->dev->name) {
 		/* Convert the curve to EC params */
 		ret = util_asn1_curve_to_ec_params(curve, ec_params);
+		*ck_key_type = curve->ck_key_type;
 	}
 
 	return ret;
@@ -333,12 +385,9 @@ static CK_RV ec_key_smw_to_pkcs11(unsigned int op, CK_KEY_TYPE *ck_key_type,
 	 * the @obj key type because @obj key structure allocated may not
 	 * an EC key.
 	 */
-	if (op & OP_KEY_DESC_GET_KEY_TYPE) {
+	if (op & OP_KEY_DESC_GET_KEY_TYPE)
 		ret = get_ec_curve_from_smw(&params, desc->type_name,
-					    desc->security_size);
-		if (ret == CKR_OK && ck_key_type)
-			*ck_key_type = CKK_EC;
-	}
+					    desc->security_size, ck_key_type);
 
 	if (ret == CKR_OK && (op & OP_KEY_DESC_GET_SECURITY_SIZE)) {
 		/* Convert the curve to EC params */
@@ -417,6 +466,47 @@ end:
 	return ret;
 }
 
+static CK_RV edwards_key_set_buffer_from_obj(struct smw_key_descriptor *desc,
+					     struct libobj_obj *obj)
+{
+	CK_RV ret = CKR_OK;
+	struct smw_keypair_gen *smw_key = NULL;
+	struct libobj_key_ec_pair *key = NULL;
+
+	/*
+	 * If SMW key's descriptor buffer field is set, setup it
+	 * with the EC key object's buffer
+	 */
+	if (!desc->buffer)
+		goto end;
+
+	if (!obj) {
+		ret = CKR_FUNCTION_FAILED;
+		goto end;
+	}
+
+	key = get_subkey_from(obj);
+	smw_key = &desc->buffer->gen;
+
+	if (key->point_q.array) {
+		smw_key->public_data = key->point_q.array;
+		if (SET_OVERFLOW(key->point_q.number, smw_key->public_length)) {
+			ret = CKR_ARGUMENTS_BAD;
+			goto end;
+		}
+	}
+
+	smw_key->private_data = key->value_d.value;
+
+	if (SET_OVERFLOW(key->value_d.length, smw_key->private_length))
+		ret = CKR_ARGUMENTS_BAD;
+	else
+		ret = CKR_OK;
+
+end:
+	return ret;
+}
+
 static CK_RV ec_key_pkcs11_to_smw(unsigned int op,
 				  struct smw_key_descriptor *desc,
 				  struct libbytes *ec_params,
@@ -442,8 +532,24 @@ static CK_RV ec_key_pkcs11_to_smw(unsigned int op,
 			goto end;
 	}
 
-	if (op & OP_KEY_DESC_SET_BUFFER)
-		ret = ec_key_set_buffer_from_obj(desc, obj);
+	if (op & OP_KEY_DESC_SET_BUFFER) {
+		if (!obj)
+			goto end;
+
+		switch (get_key_type(obj)) {
+		case CKK_EC:
+			ret = ec_key_set_buffer_from_obj(desc, obj);
+			break;
+
+		case CKK_EC_EDWARDS:
+			ret = edwards_key_set_buffer_from_obj(desc, obj);
+			break;
+
+		default:
+			ret = CKR_KEY_TYPE_INCONSISTENT;
+			break;
+		}
+	}
 
 end:
 	return ret;
@@ -830,6 +936,7 @@ static CK_RV op_key_desc_setup(unsigned int op, struct smw_key_descriptor *desc,
 		break;
 
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		if (op & OP_KEY_DESC_GET_ALL)
 			ret = ec_key_smw_to_pkcs11(op, &tmp_key_type, obj,
 						   desc);

pkcs11/src/ifsmw/mechanism.c
@@ -63,6 +63,12 @@ static CK_RV info_msign_ecdsa(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
 			      struct mentry *entry, CK_MECHANISM_INFO_PTR info);
 static CK_RV op_msign_ecdsa(CK_SLOT_ID slotid, struct mentry *entry,
 			    void *args);
+static void check_msign_eddsa(CK_SLOT_ID slotid, smw_subsystem_t subsystem,
+			      struct mgroup *mgroup);
+static CK_RV info_msign_eddsa(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			      struct mentry *entry, CK_MECHANISM_INFO_PTR info);
+static CK_RV op_msign_eddsa(CK_SLOT_ID slotid, struct mentry *entry,
+			    void *args);
 static void check_msign_rsa(CK_SLOT_ID slotid, smw_subsystem_t subsystem,
 			    struct mgroup *mgroup);
 static CK_RV info_msign_rsa(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
@@ -231,6 +237,15 @@ struct mgroup {
 		       SMW_ATTR_CURVE_ANY, SMW_ATTR_HASH_##_hash),             \
 	       _id)
 
+#define M_SIGN_EDDSA_ANY_HASH(_id)                                             \
+	M_ALGO(NONE, SMW_HASH_ALGO_NAME_NONE, SMW_MAC_ALGO_NAME_NONE,          \
+	       SMW_CIPHER_MODE_NAME_NONE, SMW_AEAD_MODE_NAME_NONE,             \
+	       SMW_SIGNATURE_ALGO_NAME_EDDSA, SMW_SIGNATURE_TYPE_NAME_NONE,    \
+	       SMW_KDF_NAME_NONE,                                              \
+	       SMW_ATTR_ALGO_ASYMMETRIC_SIGNATURE_EDDSA(SMW_ATTR_CURVE_ANY,    \
+							SMW_ATTR_HASH_ANY),    \
+	       _id)
+
 #define M_SIGN_RSA_ANY_HASH(_mode, _id)                                        \
 	M_ALGO(NONE, SMW_HASH_ALGO_NAME_NONE, SMW_MAC_ALGO_NAME_NONE,          \
 	       SMW_CIPHER_MODE_NAME_NONE, SMW_AEAD_MODE_NAME_NONE,             \
@@ -307,6 +322,7 @@ static struct mentry mdigest[] = {
  */
 static struct mentry meckeygen[] = {
 	M_ECKEYGEN(smw_ec_name, ARRAY_SIZE(smw_ec_name), EC_KEY_PAIR_GEN),
+	M_KEYGEN(ED25519, EC_EDWARDS_KEY_PAIR_GEN),
 };
 
 /*
@@ -335,6 +351,10 @@ static struct mentry msign_ecdsa[] = {
 	M_SIGN_ECDSA_ANY_HASH(ECDSA),	    M_SIGN_ECDSA(SHA1, ECDSA_SHA1),
 	M_SIGN_ECDSA(SHA224, ECDSA_SHA224), M_SIGN_ECDSA(SHA256, ECDSA_SHA256),
 	M_SIGN_ECDSA(SHA384, ECDSA_SHA384), M_SIGN_ECDSA(SHA512, ECDSA_SHA512),
+};
+
+static struct mentry msign_eddsa[] = {
+	M_SIGN_EDDSA_ANY_HASH(EDDSA),
 };
 
 static struct mentry msign_rsa[] = {
@@ -420,6 +440,7 @@ static struct mgroup smw_mechanims[] = {
 	M_GROUP(ARRAY_SIZE(mkeygen), mkeygen),
 	M_GROUP(ARRAY_SIZE(mkeyderive), mkeyderive),
 	M_GROUP(ARRAY_SIZE(msign_ecdsa), msign_ecdsa),
+	M_GROUP(ARRAY_SIZE(msign_eddsa), msign_eddsa),
 	M_GROUP(ARRAY_SIZE(msign_rsa), msign_rsa),
 	M_GROUP(ARRAY_SIZE(mcipher), mcipher),
 	M_GROUP(ARRAY_SIZE(maead), maead),
@@ -599,7 +620,8 @@ static bool get_sign_mech(smw_attr_algo_t perm_algo, CK_MECHANISM_TYPE *mech)
 
 	algo = SMW_ATTR_GET_ALGO(perm_algo);
 
-	if (algo == SMW_ATTR_ALGO_ECDSA) {
+	switch (algo) {
+	case SMW_ATTR_ALGO_ECDSA:
 		for (; i < ARRAY_SIZE(msign_ecdsa); i++) {
 			if (perm_algo == msign_ecdsa[i].smw_algo_id) {
 				*mech = msign_ecdsa[i].type;
@@ -607,8 +629,19 @@ static bool get_sign_mech(smw_attr_algo_t perm_algo, CK_MECHANISM_TYPE *mech)
 				break;
 			}
 		}
+		break;
 
-	} else if (algo == SMW_ATTR_ALGO_RSA) {
+	case SMW_ATTR_ALGO_EDDSA:
+		for (; i < ARRAY_SIZE(msign_eddsa); i++) {
+			if (perm_algo == msign_eddsa[i].smw_algo_id) {
+				*mech = msign_eddsa[i].type;
+				found = true;
+				break;
+			}
+		}
+		break;
+
+	case SMW_ATTR_ALGO_RSA:
 		for (; i < ARRAY_SIZE(msign_rsa); i++) {
 			if (perm_algo == msign_rsa[i].smw_algo_id) {
 				*mech = msign_rsa[i].type;
@@ -616,6 +649,10 @@ static bool get_sign_mech(smw_attr_algo_t perm_algo, CK_MECHANISM_TYPE *mech)
 				break;
 			}
 		}
+		break;
+
+	default:
+		break;
 	}
 
 	DBG_TRACE("%s mechanism (0x%08lX)", found ? "Found" : "No", *mech);
@@ -1119,6 +1156,46 @@ end:
 	return ret;
 }
 
+static CK_RV export_edwards_public_key(struct smw_key_descriptor *key_desc,
+				       const struct libobj_obj *obj)
+{
+	CK_RV ret = CKR_OK;
+
+	struct libobj_key_ec_pair *key = get_subkey_from(obj);
+	size_t public_length = 0;
+
+	DBG_TRACE("Export Edwards Public Key");
+
+	/* Assign EC public key length */
+	public_length = key_desc->buffer->gen.public_length;
+	if (!public_length) {
+		ret = CKR_ARGUMENTS_BAD;
+		goto end;
+	}
+
+	key->point_q.number = public_length;
+	key->point_q.array = calloc(1, key->point_q.number);
+	if (!key->point_q.array) {
+		ret = CKR_HOST_MEMORY;
+		goto end;
+	}
+
+	ret = op_export_common(key_desc, obj);
+
+end:
+	if (ret != CKR_OK) {
+		if (key->point_q.array)
+			free(key->point_q.array);
+
+		key->point_q.array = NULL;
+		key->point_q.number = 0;
+	}
+
+	DBG_TRACE("return %ld", ret);
+
+	return ret;
+}
+
 static CK_RV export_rsa_public_key(struct smw_key_descriptor *key_desc,
 				   const struct libobj_obj *obj)
 {
@@ -1433,6 +1510,14 @@ static void check_msign_ecdsa(CK_SLOT_ID slotid, smw_subsystem_t subsystem,
 	check_msign_common(slotid, subsystem, mgroup);
 }
 
+static void check_msign_eddsa(CK_SLOT_ID slotid, smw_subsystem_t subsystem,
+			      struct mgroup *mgroup)
+{
+	DBG_TRACE("Check EDDSA Signature mechanism");
+
+	check_msign_common(slotid, subsystem, mgroup);
+}
+
 static void check_msign_rsa(CK_SLOT_ID slotid, smw_subsystem_t subsystem,
 			    struct mgroup *mgroup)
 {
@@ -1486,6 +1571,12 @@ static CK_RV info_msign_common(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
 }
 
 static CK_RV info_msign_ecdsa(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
+			      struct mentry *entry, CK_MECHANISM_INFO_PTR info)
+{
+	return info_msign_common(slotid, type, entry, info);
+}
+
+static CK_RV info_msign_eddsa(CK_SLOT_ID slotid, CK_MECHANISM_TYPE type,
 			      struct mentry *entry, CK_MECHANISM_INFO_PTR info)
 {
 	return info_msign_common(slotid, type, entry, info);
@@ -1749,6 +1840,20 @@ static CK_RV op_msign_ecdsa(CK_SLOT_ID slotid, struct mentry *entry, void *args)
 	unsigned int key_id = 0;
 
 	DBG_TRACE("ECDSA Signature mechanism");
+
+	ctx = ((struct lib_signature_params *)args)->ctx;
+
+	key_id = get_key_token_id((struct libobj_obj *)ctx->hkey);
+
+	return op_msign_common(slotid, entry, args, key_id);
+}
+
+static CK_RV op_msign_eddsa(CK_SLOT_ID slotid, struct mentry *entry, void *args)
+{
+	struct lib_signature_ctx *ctx = NULL;
+	unsigned int key_id = 0;
+
+	DBG_TRACE("EDDSA Signature mechanism");
 
 	ctx = ((struct lib_signature_params *)args)->ctx;
 
@@ -2798,6 +2903,10 @@ CK_RV libdev_export_public_key(const struct libobj_obj *obj)
 	switch (get_key_type(obj)) {
 	case CKK_EC:
 		ret = export_ec_public_key(&key_descriptor, obj);
+		break;
+
+	case CKK_EC_EDWARDS:
+		ret = export_edwards_public_key(&key_descriptor, obj);
 		break;
 
 	case CKK_RSA:

pkcs11/src/ifsmw/args_attr.c
@@ -255,6 +255,7 @@ void args_attrs_key_usage(smw_attr_usage_t *usage_flags, struct libobj_obj *obj)
 		break;
 
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		set_ec_key_usage(usage_flags, obj);
 		break;
 
@@ -293,6 +294,7 @@ void args_attr_get_key_usage(struct libobj_obj *obj,
 		break;
 
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		get_ec_key_usage(obj, usage_flags);
 		break;
 

pkcs11/src/include/asn1_ec_curve.h
@@ -122,4 +122,28 @@
 		0x2B, 0x24, 0x03, 0x03, 0x02, 0x08, 0x01, 0x01, 0x0E           \
 	}
 
+/* x25519: 1.3.101.110 */
+#define ASN1_OID_X25519                                                        \
+	{                                                                      \
+		0x2B, 0x65, 0x6E                                               \
+	}
+
+/* x448: 1.3.101.111 */
+#define ASN1_OID_X448                                                          \
+	{                                                                      \
+		0x2B, 0x65, 0x6F                                               \
+	}
+
+/* ed25519: 1.3.101.112 */
+#define ASN1_OID_ED25519                                                       \
+	{                                                                      \
+		0x2B, 0x65, 0x70                                               \
+	}
+
+/* ed448: 1.3.101.113 */
+#define ASN1_OID_ED448                                                         \
+	{                                                                      \
+		0x2B, 0x65, 0x71                                               \
+	}
+
 #endif /* __ASN1_EC_CURVE_H__ */

pkcs11/src/include/util_asn1.h
@@ -27,13 +27,15 @@ struct dev_curve_def;
 /**
  * struct curve_def - Definition of ASN1 vs SMW curves
  * @asm1: ASN1 curve definition
- * @dev: device curve definition
+ * @dev: Device curve definition
+ * @ck_key_type: PKCS11 Key type
  *
  * Note: The last element must be NULL
  */
 struct curve_def {
 	const struct asn1_curve_def *asn1;
 	const struct dev_curve_def *dev;
+	const CK_KEY_TYPE ck_key_type;
 };
 
 /* ASN1 Long format length encoding tag */

pkcs11/src/objects/key.c
@@ -329,6 +329,7 @@ static void key_private_free(struct libobj_obj *obj)
 
 	switch (get_key_type(obj)) {
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		key_ec_private_free(obj);
 		break;
 
@@ -367,6 +368,7 @@ static void key_public_free(struct libobj_obj *obj)
 
 	switch (get_key_type(obj)) {
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		key_ec_public_free(obj);
 		break;
 
@@ -970,6 +972,7 @@ static CK_RV subkey_private_create(CK_SESSION_HANDLE hsession,
 
 	switch (get_key_type(obj)) {
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		ret = key_ec_private_create(hsession, obj, attrs);
 		break;
 
@@ -1014,6 +1017,7 @@ static CK_RV subkey_private_retrieve(CK_SESSION_HANDLE hsession,
 
 	switch (get_key_type(obj)) {
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		ret = key_ec_private_retrieve(hsession, obj);
 		break;
 
@@ -1069,6 +1073,7 @@ static CK_RV subkey_private_get_attribute(CK_ATTRIBUTE_PTR attr,
 	 */
 	switch (get_key_type(obj)) {
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		ret = key_ec_private_get_attribute(attr, obj, protect);
 		break;
 
@@ -1121,6 +1126,7 @@ static CK_RV subkey_private_modify_attribute(CK_ATTRIBUTE_PTR attr,
 	 */
 	switch (get_key_type(obj)) {
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		ret = key_ec_private_modify_attribute(attr, obj);
 		break;
 
@@ -1167,6 +1173,7 @@ static CK_RV subkey_public_create(CK_SESSION_HANDLE hsession,
 
 	switch (get_key_type(obj)) {
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		ret = key_ec_public_create(hsession, obj, attrs);
 		break;
 
@@ -1211,6 +1218,7 @@ static CK_RV subkey_public_retrieve(CK_SESSION_HANDLE hsession,
 
 	switch (get_key_type(obj)) {
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		ret = key_ec_public_retrieve(hsession, obj);
 		break;
 
@@ -1259,6 +1267,7 @@ static CK_RV subkey_public_get_attribute(CK_ATTRIBUTE_PTR attr,
 	 */
 	switch (get_key_type(obj)) {
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		ret = key_ec_public_get_attribute(attr, obj);
 		break;
 
@@ -1310,6 +1319,7 @@ static CK_RV subkey_public_modify_attribute(CK_ATTRIBUTE_PTR attr,
 	 */
 	switch (get_key_type(obj)) {
 	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		ret = key_ec_public_modify_attribute(attr, obj);
 		break;
 
@@ -1643,12 +1653,20 @@ CK_RV key_keypair_retrieve(CK_SESSION_HANDLE hsession,
 	set_key_token_id(pub_obj, id);
 	set_key_token_id(priv_obj, id);
 
-	if (get_key_type(priv_obj) == CKK_EC)
+	switch (get_key_type(priv_obj)) {
+	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		ret = key_ec_keypair_retrieve(hsession, pub_obj, priv_obj);
-	else if (get_key_type(priv_obj) == CKK_RSA)
+		break;
+
+	case CKK_RSA:
 		ret = key_rsa_keypair_retrieve(hsession, pub_obj, priv_obj);
-	else
+		break;
+
+	default:
 		ret = CKR_FUNCTION_FAILED;
+		break;
+	}
 
 end:
 	DBG_TRACE("Keypair object (pub=%p priv=%p) retrieve return %ld",
@@ -1802,6 +1820,10 @@ CK_RV key_keypair_generate(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
 		key_type = CKK_EC;
 		break;
 
+	case CKM_EC_EDWARDS_KEY_PAIR_GEN:
+		key_type = CKK_EC_EDWARDS;
+		break;
+
 	case CKM_RSA_PKCS_KEY_PAIR_GEN:
 	case CKM_RSA_X9_31_KEY_PAIR_GEN:
 		key_type = CKK_RSA;
@@ -1831,12 +1853,18 @@ CK_RV key_keypair_generate(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
 	if (ret != CKR_OK)
 		goto end;
 
-	if (key_type == CKK_EC)
+	switch (key_type) {
+	case CKK_EC:
+	case CKK_EC_EDWARDS:
 		ret = key_ec_keypair_generate(hsession, mech, pub_key,
 					      pub_attrs, priv_key, priv_attrs);
-	else
+		break;
+
+	case CKK_RSA:
 		ret = key_rsa_keypair_generate(hsession, mech, pub_key,
 					       pub_attrs, priv_key, priv_attrs);
+		break;
+	}
 
 end:
 	DBG_TRACE("Keypair object (pub=%p priv=%p) generate return %ld",

commit 5235f9c2cc62370974ca0d05a8f04436377f68a3

SSMW-1024 pkcs11: Encode and decode eddsa public buffer as octet string
As describe in RFC 8032, all values are coded as octet strings [1].

[1] https://datatracker.ietf.org/doc/html/rfc8032#section-5.2.2

Fixes: ad0047fdc2c2 ("SSMW-573 pkcs11: Add support for the edwards 25519 key")

Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

pkcs11/src/ifsmw/key_desc.c
@@ -465,6 +465,7 @@ static CK_RV edwards_key_set_buffer_from_obj(struct smw_key_descriptor *desc,
 	CK_RV ret = CKR_OK;
 	struct smw_keypair_gen *smw_key = NULL;
 	struct libobj_key_ec_pair *key = NULL;
+	size_t public_length = 0;
 
 	/*
 	 * If SMW key's descriptor buffer field is set, setup it
@@ -482,11 +483,15 @@ static CK_RV edwards_key_set_buffer_from_obj(struct smw_key_descriptor *desc,
 	smw_key = &desc->buffer->gen;
 
 	if (key->point_q.array) {
-		smw_key->public_data = key->point_q.array;
-		if (SET_OVERFLOW(key->point_q.number, smw_key->public_length)) {
-			ret = CKR_ARGUMENTS_BAD;
+		ret = util_asn1_get_field_octet_string(key->point_q.array,
+						       key->point_q.number,
+						       &smw_key->public_data,
+						       &public_length);
+		if (ret != CKR_OK)
 			goto end;
-		}
+
+		if (SET_OVERFLOW(public_length, smw_key->public_length))
+			ret = CKR_ARGUMENTS_BAD;
 	}
 
 	smw_key->private_data = key->value_d.value;

pkcs11/src/ifsmw/mechanism.c
@@ -1587,12 +1587,26 @@ static CK_RV export_edwards_public_key(struct smw_key_descriptor *key_desc,
 		goto end;
 	}
 
-	key->point_q.number = public_length;
+	ret = util_asn1_encode_octet_string(NULL, public_length, NULL,
+					    &key->point_q.number);
+	if (ret != CKR_OK)
+		goto end;
+
 	key->point_q.array = calloc(1, key->point_q.number);
 	if (!key->point_q.array) {
 		ret = CKR_HOST_MEMORY;
 		goto end;
 	}
+
+	/*
+	 * Pre-encode the DER octet string without key, just to get
+	 * the encapsulation in order to set the SMW's key buffer.
+	 */
+	ret = util_asn1_encode_octet_string(NULL, public_length,
+					    key->point_q.array,
+					    &key->point_q.number);
+	if (ret != CKR_OK)
+		goto end;
 
 	ret = op_export_common(key_desc, obj);
 
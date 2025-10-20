nxp-imx/imx-smw

commit 0f14134342087b765dcb90f1009f24d08d73ce67

SSMW-79 pkcs11: implement empty PKCS11 Library interface
Implementation of an empty PKCS11 Library:
- C_GetFunctionList returns list of function
- C_GetInfo returns library information
- C_GetInterfaceList returns all library interfaces
- C_GetInterface returns library interface queried
- Other functions return CKR_FUNCTION_NOT_SUPPORTED

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/get_info.c
@@ -1,0 +1,325 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2020 NXP
+ */
+
+#include <string.h>
+
+#include "pkcs11smw.h"
+#include "pkcs11smw_config.h"
+#include "util.h"
+
+#include "trace.h"
+
+static struct CK_FUNCTION_LIST_3_0 pkcs11smw_v3_functions = {
+	{ 3, 0 },
+	&C_Initialize,
+	&C_Finalize,
+	&C_GetInfo,
+	&C_GetFunctionList,
+	&C_GetSlotList,
+	&C_GetSlotInfo,
+	&C_GetTokenInfo,
+	&C_GetMechanismList,
+	&C_GetMechanismInfo,
+	&C_InitToken,
+	&C_InitPIN,
+	&C_SetPIN,
+	&C_OpenSession,
+	&C_CloseSession,
+	&C_CloseAllSessions,
+	&C_GetSessionInfo,
+	&C_GetOperationState,
+	&C_SetOperationState,
+	&C_Login,
+	&C_Logout,
+	&C_CreateObject,
+	&C_CopyObject,
+	&C_DestroyObject,
+	&C_GetObjectSize,
+	&C_GetAttributeValue,
+	&C_SetAttributeValue,
+	&C_FindObjectsInit,
+	&C_FindObjects,
+	&C_FindObjectsFinal,
+	&C_EncryptInit,
+	&C_Encrypt,
+	&C_EncryptUpdate,
+	&C_EncryptFinal,
+	&C_DecryptInit,
+	&C_Decrypt,
+	&C_DecryptUpdate,
+	&C_DecryptFinal,
+	&C_DigestInit,
+	&C_Digest,
+	&C_DigestUpdate,
+	&C_DigestKey,
+	&C_DigestFinal,
+	&C_SignInit,
+	&C_Sign,
+	&C_SignUpdate,
+	&C_SignFinal,
+	&C_SignRecoverInit,
+	&C_SignRecover,
+	&C_VerifyInit,
+	&C_Verify,
+	&C_VerifyUpdate,
+	&C_VerifyFinal,
+	&C_VerifyRecoverInit,
+	&C_VerifyRecover,
+	&C_DigestEncryptUpdate,
+	&C_DecryptDigestUpdate,
+	&C_SignEncryptUpdate,
+	&C_DecryptVerifyUpdate,
+	&C_GenerateKey,
+	&C_GenerateKeyPair,
+	&C_WrapKey,
+	&C_UnwrapKey,
+	&C_DeriveKey,
+	&C_SeedRandom,
+	&C_GenerateRandom,
+	&C_GetFunctionStatus,
+	&C_CancelFunction,
+	&C_WaitForSlotEvent,
+	&C_GetInterfaceList,
+	&C_GetInterface,
+	&C_LoginUser,
+	&C_SessionCancel,
+	&C_MessageEncryptInit,
+	&C_EncryptMessage,
+	&C_EncryptMessageBegin,
+	&C_EncryptMessageNext,
+	&C_MessageEncryptFinal,
+	&C_MessageDecryptInit,
+	&C_DecryptMessage,
+	&C_DecryptMessageBegin,
+	&C_DecryptMessageNext,
+	&C_MessageDecryptFinal,
+	&C_MessageSignInit,
+	&C_SignMessage,
+	&C_SignMessageBegin,
+	&C_SignMessageNext,
+	&C_MessageSignFinal,
+	&C_MessageVerifyInit,
+	&C_VerifyMessage,
+	&C_VerifyMessageBegin,
+	&C_VerifyMessageNext,
+	&C_MessageVerifyFinal,
+};
+
+static struct CK_FUNCTION_LIST pkcs11smw_v2_functions = {
+	{ 2, 40 },
+	&C_Initialize,
+	&C_Finalize,
+	&C_GetInfo,
+	&C_GetFunctionList,
+	&C_GetSlotList,
+	&C_GetSlotInfo,
+	&C_GetTokenInfo,
+	&C_GetMechanismList,
+	&C_GetMechanismInfo,
+	&C_InitToken,
+	&C_InitPIN,
+	&C_SetPIN,
+	&C_OpenSession,
+	&C_CloseSession,
+	&C_CloseAllSessions,
+	&C_GetSessionInfo,
+	&C_GetOperationState,
+	&C_SetOperationState,
+	&C_Login,
+	&C_Logout,
+	&C_CreateObject,
+	&C_CopyObject,
+	&C_DestroyObject,
+	&C_GetObjectSize,
+	&C_GetAttributeValue,
+	&C_SetAttributeValue,
+	&C_FindObjectsInit,
+	&C_FindObjects,
+	&C_FindObjectsFinal,
+	&C_EncryptInit,
+	&C_Encrypt,
+	&C_EncryptUpdate,
+	&C_EncryptFinal,
+	&C_DecryptInit,
+	&C_Decrypt,
+	&C_DecryptUpdate,
+	&C_DecryptFinal,
+	&C_DigestInit,
+	&C_Digest,
+	&C_DigestUpdate,
+	&C_DigestKey,
+	&C_DigestFinal,
+	&C_SignInit,
+	&C_Sign,
+	&C_SignUpdate,
+	&C_SignFinal,
+	&C_SignRecoverInit,
+	&C_SignRecover,
+	&C_VerifyInit,
+	&C_Verify,
+	&C_VerifyUpdate,
+	&C_VerifyFinal,
+	&C_VerifyRecoverInit,
+	&C_VerifyRecover,
+	&C_DigestEncryptUpdate,
+	&C_DecryptDigestUpdate,
+	&C_SignEncryptUpdate,
+	&C_DecryptVerifyUpdate,
+	&C_GenerateKey,
+	&C_GenerateKeyPair,
+	&C_WrapKey,
+	&C_UnwrapKey,
+	&C_DeriveKey,
+	&C_SeedRandom,
+	&C_GenerateRandom,
+	&C_GetFunctionStatus,
+	&C_CancelFunction,
+	&C_WaitForSlotEvent,
+};
+
+/*
+ * Define the Library information
+ */
+static const CK_INFO pkcs11smw_info = {
+	.cryptokiVersion = { CRYPTOKI_VERSION_MAJOR, CRYPTOKI_VERSION_MINOR },
+	.manufacturerID = MANUFACTURER_ID,
+	.flags = 0,
+	.libraryDescription = LIBRARY_DESCRIPTION,
+	.libraryVersion = { LIB_VER_MAJOR, LIB_VER_MINOR },
+};
+
+/*
+ * Define all library's interfaces supported
+ */
+static const CK_CHAR def_if_name[] = "PKCS 11";
+#define DEFAULT_INTERFACE_ENTRY 0
+
+static struct CK_INTERFACE pkcs11smw_interfaces[] = {
+	{
+		.pInterfaceName = (CK_CHAR *)def_if_name,
+		.pFunctionList = &pkcs11smw_v2_functions,
+		.flags = 0,
+	},
+	{
+		.pInterfaceName = (CK_CHAR *)def_if_name,
+		.pFunctionList = &pkcs11smw_v3_functions,
+		.flags = 0,
+	},
+	{ 0 },
+};
+
+CK_RV C_GetInfo(CK_INFO_PTR pInfo)
+{
+	size_t len;
+
+	if (!pInfo)
+		return CKR_ARGUMENTS_BAD;
+
+	DBG_TRACE("Copy %p to %p (%zu bytes)", pInfo, &pkcs11smw_info,
+		  sizeof(pkcs11smw_info));
+	memcpy(pInfo, &pkcs11smw_info, sizeof(pkcs11smw_info));
+
+	/* Pad manufacturerID and LibraryDescription with blank */
+	len = strlen((const char *)pkcs11smw_info.manufacturerID);
+	DBG_TRACE("Manufacturer (%zu) bytes: %s", len, pInfo->manufacturerID);
+	memset(pInfo->manufacturerID + len, ' ',
+	       sizeof(pInfo->manufacturerID) - len);
+
+	len = strlen((const char *)pkcs11smw_info.libraryDescription);
+	DBG_TRACE("Lib Description (%zu) bytes: %s", len,
+		  pInfo->libraryDescription);
+	memset(pInfo->libraryDescription + len, ' ',
+	       sizeof(pInfo->libraryDescription) - len);
+
+	return CKR_OK;
+}
+
+CK_RV C_GetFunctionList(CK_FUNCTION_LIST_PTR_PTR ppFunctionList)
+{
+	if (!ppFunctionList)
+		return CKR_ARGUMENTS_BAD;
+
+	*ppFunctionList = &pkcs11smw_v2_functions;
+
+	return CKR_OK;
+}
+
+CK_RV C_GetInterfaceList(CK_INTERFACE_PTR pInterfacesList,
+			 CK_ULONG_PTR pulCount)
+{
+	CK_ULONG nb_entries;
+
+	if (!pulCount)
+		return CKR_ARGUMENTS_BAD;
+
+	nb_entries = ARRAY_SIZE(pkcs11smw_interfaces) - 1;
+	if (!pInterfacesList) {
+		*pulCount = nb_entries;
+		return CKR_OK;
+	}
+
+	if (*pulCount < nb_entries)
+		return CKR_BUFFER_TOO_SMALL;
+
+	memcpy(pInterfacesList, pkcs11smw_interfaces,
+	       nb_entries * sizeof(CK_INTERFACE));
+
+	return CKR_OK;
+}
+
+CK_RV C_GetInterface(CK_UTF8CHAR_PTR pInterfaceName, CK_VERSION_PTR pVersion,
+		     CK_INTERFACE_PTR_PTR ppInterface, CK_FLAGS flags)
+{
+	CK_RV ret = CKR_ARGUMENTS_BAD;
+	CK_INTERFACE_PTR entry = pkcs11smw_interfaces;
+	CK_FUNCTION_LIST_PTR func_list;
+
+	if (!ppInterface)
+		goto end;
+
+	*ppInterface = NULL;
+
+	if (!pInterfaceName && !pVersion && !flags) {
+		DBG_TRACE("No criteria, return default entry %d",
+			  DEFAULT_INTERFACE_ENTRY);
+		entry = &entry[DEFAULT_INTERFACE_ENTRY];
+		ret = CKR_OK;
+		goto end;
+	}
+
+	for (; entry->pInterfaceName; entry++) {
+		if (pInterfaceName) {
+			if (strcmp((const char *)pInterfaceName,
+				   (const char *)entry->pInterfaceName))
+				continue;
+		}
+
+		if (pVersion) {
+			func_list = (CK_FUNCTION_LIST_PTR)entry->pFunctionList;
+			if (pVersion->major != func_list->version.major ||
+			    pVersion->minor != func_list->version.minor)
+				continue;
+		}
+
+		if ((entry->flags & flags) == flags) {
+			ret = CKR_OK;
+			break;
+		}
+	}
+
+end:
+	if (ret == CKR_OK) {
+		*ppInterface = entry;
+		func_list = (CK_FUNCTION_LIST_PTR)entry->pFunctionList;
+		DBG_TRACE("Interface:");
+		DBG_TRACE("    Name: %s", entry->pInterfaceName);
+		DBG_TRACE("    Function ver: %01d.%01d",
+			  func_list->version.major, func_list->version.minor);
+		DBG_TRACE("    Flags: 0x%lX", entry->flags);
+	}
+
+	DBG_TRACE("return 0x%08lX", ret);
+	return ret;
+}

.checkpatch.ignore
@@ -1,1 +1,3 @@
 scripts
+pkcs11/import
+pkcs11/src/include/pkcs11smw.h

.gitignore
@@ -1,2 +1,3 @@
 *.swp
 .tmp
+build*/

CMakeLists.txt
@@ -66,6 +66,11 @@ set(INCLUDE_DIR ${CMAKE_SOURCE_DIR}/public ${CMAKE_SOURCE_DIR}/inc)
 add_subdirectory(osal)
 add_subdirectory(core)
 
+#
+# Include PKCS#11 project
+#
+add_subdirectory(pkcs11)
+
 add_library(smw ${OBJ_LIST} ${INCLUDE_DIR})
 set_target_properties(smw PROPERTIES
 	VERSION ${PROJECT_VERSION}

pkcs11/CMakeLists.txt
@@ -1,0 +1,23 @@
+project(smw_pkcs11 VERSION 1.0 LANGUAGES C)
+
+if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
+	add_compile_options(-Wall -Wextra -Werror)
+endif()
+
+add_library(${PROJECT_NAME} SHARED "")
+
+add_subdirectory(src)
+
+# Include pkcs11 headers
+#
+target_include_directories(${PROJECT_NAME} PUBLIC import)
+
+set_target_properties(${PROJECT_NAME} PROPERTIES
+	VERSION ${PROJECT_VERSION}
+	SOVERSION ${PROJECT_VERSION_MAJOR})
+
+set_target_properties(${PROJECT_NAME} PROPERTIES
+	C_VISIBILITY_PRESET hidden
+	VISIBILITY_INLINES_HIDDEN ON)
+
+string(APPEND CMAKE_SHARED_LINKER_FLAGS " -Wl,--no-undefined")

pkcs11/import/.clang-format
@@ -1,0 +1,4 @@
+#
+# Ignore clang-format in this directory
+#
+DisableFormat: true

pkcs11/import/pkcs11.h
@@ -1,0 +1,243 @@
+/* Copyright (c) OASIS Open 2016-2020. All Rights Reserved.
+ * Distributed under the terms of the OASIS IPR Policy,
+ * [http://www.oasis-open.org/policies-guidelines/ipr], AS-IS, WITHOUT ANY
+ * IMPLIED OR EXPRESS WARRANTY; there is no warranty of MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE or NONINFRINGEMENT of the rights of others.
+ */
+
+#ifndef _PKCS11_H_
+#define _PKCS11_H_ 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Before including this file (pkcs11.h) (or pkcs11t.h by
+ * itself), 5 platform-specific macros must be defined.  These
+ * macros are described below, and typical definitions for them
+ * are also given.  Be advised that these definitions can depend
+ * on both the platform and the compiler used (and possibly also
+ * on whether a Cryptoki library is linked statically or
+ * dynamically).
+ *
+ * In addition to defining these 5 macros, the packing convention
+ * for Cryptoki structures should be set.  The Cryptoki
+ * convention on packing is that structures should be 1-byte
+ * aligned.
+ *
+ * If you're using Windows this might be done by using the following
+ * preprocessor directive before including pkcs11.h or pkcs11t.h:
+ *
+ * #pragma pack(push, cryptoki, 1)
+ *
+ * and using the following preprocessor directive after including
+ * pkcs11.h or pkcs11t.h:
+ *
+ * #pragma pack(pop, cryptoki)
+ *
+ * In a UNIX environment, you're on your own for this.  You might
+ * not need to do (or be able to do!) anything.
+ *
+ *
+ * Now for the macros:
+ *
+ *
+ * 1. CK_PTR: The indirection string for making a pointer to an
+ * object.  It can be used like this:
+ *
+ * typedef CK_BYTE CK_PTR CK_BYTE_PTR;
+ *
+ * If you're using windows, it might be defined by:
+ *
+ * #define CK_PTR *
+ *
+ * In a typical UNIX environment, it might be defined by:
+ *
+ * #define CK_PTR *
+ *
+ *
+ * 2. CK_DECLARE_FUNCTION(returnType, name): A macro which makes
+ * an importable Cryptoki library function declaration out of a
+ * return type and a function name.  It should be used in the
+ * following fashion:
+ *
+ * extern CK_DECLARE_FUNCTION(CK_RV, C_Initialize)(
+ *   CK_VOID_PTR pReserved
+ * );
+ *
+ * If you're using Windows to declare a function in a Win32 cryptoki .dll, 
+ * it might be defined by:
+ *
+ * #define CK_DECLARE_FUNCTION(returnType, name) \
+ *   returnType __declspec(dllimport) name
+ *
+ * In a UNIX environment, it might be defined by:
+ *
+ * #define CK_DECLARE_FUNCTION(returnType, name) \
+ *   returnType name
+ *
+ *
+ * 3. CK_DECLARE_FUNCTION_POINTER(returnType, name): A macro
+ * which makes a Cryptoki API function pointer declaration or
+ * function pointer type declaration out of a return type and a
+ * function name.  It should be used in the following fashion:
+ *
+ * // Define funcPtr to be a pointer to a Cryptoki API function
+ * // taking arguments args and returning CK_RV.
+ * CK_DECLARE_FUNCTION_POINTER(CK_RV, funcPtr)(args);
+ *
+ * or
+ *
+ * // Define funcPtrType to be the type of a pointer to a
+ * // Cryptoki API function taking arguments args and returning
+ * // CK_RV, and then define funcPtr to be a variable of type
+ * // funcPtrType.
+ * typedef CK_DECLARE_FUNCTION_POINTER(CK_RV, funcPtrType)(args);
+ * funcPtrType funcPtr;
+ *
+ * If you're using Windows to access
+ * functions in a Win32 Cryptoki .dll, in might be defined by:
+ *
+ * #define CK_DECLARE_FUNCTION_POINTER(returnType, name) \
+ *   returnType __declspec(dllimport) (* name)
+ *
+ * In a UNIX environment, it might be defined by:
+ *
+ * #define CK_DECLARE_FUNCTION_POINTER(returnType, name) \
+ *   returnType (* name)
+ *
+ *
+ * 4. CK_CALLBACK_FUNCTION(returnType, name): A macro which makes
+ * a function pointer type for an application callback out of
+ * a return type for the callback and a name for the callback.
+ * It should be used in the following fashion:
+ *
+ * CK_CALLBACK_FUNCTION(CK_RV, myCallback)(args);
+ *
+ * to declare a function pointer, myCallback, to a callback
+ * which takes arguments args and returns a CK_RV.  It can also
+ * be used like this:
+ *
+ * typedef CK_CALLBACK_FUNCTION(CK_RV, myCallbackType)(args);
+ * myCallbackType myCallback;
+ *
+ * If you're using Windows, it might be defined by:
+ *
+ * #define CK_CALLBACK_FUNCTION(returnType, name) \
+ *   returnType (* name)
+ *
+ * In a UNIX environment, it might be defined by:
+ *
+ * #define CK_CALLBACK_FUNCTION(returnType, name) \
+ *   returnType (* name)
+ *
+ *
+ * 5. NULL_PTR: This macro is the value of a NULL pointer.
+ *
+ * In any ANSI/ISO C environment (and in many others as well),
+ * this should best be defined by
+ *
+ * #ifndef NULL_PTR
+ * #define NULL_PTR 0
+ * #endif
+ */
+
+
+/* All the various Cryptoki types and #define'd values are in the
+ * file pkcs11t.h.
+ */
+#include "pkcs11t.h"
+
+#define __PASTE(x,y)      x##y
+
+
+/* ==============================================================
+ * Define the "extern" form of all the entry points.
+ * ==============================================================
+ */
+
+#define CK_NEED_ARG_LIST  1
+#define CK_PKCS11_FUNCTION_INFO(name) \
+  extern CK_DECLARE_FUNCTION(CK_RV, name)
+
+/* pkcs11f.h has all the information about the Cryptoki
+ * function prototypes.
+ */
+#include "pkcs11f.h"
+
+#undef CK_NEED_ARG_LIST
+#undef CK_PKCS11_FUNCTION_INFO
+
+
+/* ==============================================================
+ * Define the typedef form of all the entry points.  That is, for
+ * each Cryptoki function C_XXX, define a type CK_C_XXX which is
+ * a pointer to that kind of function.
+ * ==============================================================
+ */
+
+#define CK_NEED_ARG_LIST  1
+#define CK_PKCS11_FUNCTION_INFO(name) \
+  typedef CK_DECLARE_FUNCTION_POINTER(CK_RV, __PASTE(CK_,name))
+
+/* pkcs11f.h has all the information about the Cryptoki
+ * function prototypes.
+ */
+#include "pkcs11f.h"
+
+#undef CK_NEED_ARG_LIST
+#undef CK_PKCS11_FUNCTION_INFO
+
+
+/* ==============================================================
+ * Define structed vector of entry points.  A CK_FUNCTION_LIST
+ * contains a CK_VERSION indicating a library's Cryptoki version
+ * and then a whole slew of function pointers to the routines in
+ * the library.  This type was declared, but not defined, in
+ * pkcs11t.h.
+ * ==============================================================
+ */
+
+#define CK_PKCS11_FUNCTION_INFO(name) \
+  __PASTE(CK_,name) name;
+
+/* Create the 3.0 Function list */
+struct CK_FUNCTION_LIST_3_0 {
+
+  CK_VERSION    version;  /* Cryptoki version */
+
+/* Pile all the function pointers into the CK_FUNCTION_LIST. */
+/* pkcs11f.h has all the information about the Cryptoki
+ * function prototypes.
+ */
+#include "pkcs11f.h"
+
+};
+
+#define CK_PKCS11_2_0_ONLY 1
+
+/* Continue to define the old CK_FUNCTION_LIST */
+struct CK_FUNCTION_LIST {
+
+  CK_VERSION    version;  /* Cryptoki version */
+
+/* Pile all the function pointers into the CK_FUNCTION_LIST. */
+/* pkcs11f.h has all the information about the Cryptoki
+ * function prototypes.
+ */
+#include "pkcs11f.h"
+
+};
+
+#undef CK_PKCS11_FUNCTION_INFO
+#undef CK_PKCS11_2_0_ONLY
+
+
+#undef __PASTE
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PKCS11_H_ */
+

commit 33e3f2d4f616a857780637935c0e6d5ec6ecb0cd

SSMW-1084 pkcs11: Fix coverity 2025.6.0
- Change the pkcs11smw_interfaces as constant
Fixes: 0f1413434208 ("SSMW-79 pkcs11: implement empty PKCS11 Library interface")

- Verify if key derivation mechanism is for CKM_TLS12_KEY_AND_MAC_DERIVE
Fixes: 8b6b4108b911 ("SSMW-184 pkcs11: Add TLS 1.2 Master secret and Key block support")

- Verify if IV buffer in the cipher operation context is valid before
  doing the copy.
Fixes: 425db49bdc51 ("SSMW-112 pkcs11: Add support to message based encryption decryption")

- Check cipher mode parameters is not NULL.
Fixes: d6dfb5f04cdc ("SSMW-580 pkcs11: add support for single-part AEAD")
Fixes: 425db49bdc51 ("SSMW-112 pkcs11: Add support to message based encryption decryption")
Fixes: 5bd9b1639e0b ("SSMW-537 pkcs11: SM4 support")
Fixes: 0be15c029a87 ("SSMW-111 pkcs11: encrypt/decrypt apis")

- Validate the attribute pValue in the attr_modify_obj_value()
Fixes: cd495b66d907 ("SSMW-130 pkcs11: implements set/get Attribute value API")

- Validate the attribute pValue in the attr_modify_obj_value()
Fixes: 45f75f786dba ("SSMW-579 pkcs11: Add support for profile object handling")

- Validate the attribute pValue in the libobj_find_init()
Fixes: f11da5f6dac6 ("SSMW-130 pkcs11: implements find objects")

- Check if find_lock_object() obj is not NULL
Fixes: c5bd53d85786 ("SSMW-885 pkcs11: Allow using session object in all application sessions")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

pkcs11/src/get_info.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020, 2023-2024 NXP
+ * Copyright 2020, 2023-2025 NXP
  */
 
 #include <string.h>
@@ -200,7 +200,7 @@ static const CK_INFO pkcs11smw_info = {
 static const CK_CHAR def_if_name[] = "PKCS 11";
 #define DEFAULT_INTERFACE_ENTRY 0
 
-static struct CK_INTERFACE pkcs11smw_interfaces[] = {
+static const struct CK_INTERFACE pkcs11smw_interfaces[] = {
 	{
 		.pInterfaceName = (CK_CHAR *)def_if_name,
 		.pFunctionList = &pkcs11smw_v2_functions,
@@ -280,7 +280,7 @@ CK_RV C_GetInterface(CK_UTF8CHAR_PTR pInterfaceName, CK_VERSION_PTR pVersion,
 		     CK_INTERFACE_PTR_PTR ppInterface, CK_FLAGS flags)
 {
 	CK_RV ret = CKR_ARGUMENTS_BAD;
-	CK_INTERFACE_PTR entry = pkcs11smw_interfaces;
+	const struct CK_INTERFACE *entry = pkcs11smw_interfaces;
 	CK_FUNCTION_LIST_PTR func_list = NULL_PTR;
 
 	if (!ppInterface)
@@ -318,7 +318,7 @@ CK_RV C_GetInterface(CK_UTF8CHAR_PTR pInterfaceName, CK_VERSION_PTR pVersion,
 
 end:
 	if (ret == CKR_OK) {
-		*ppInterface = entry;
+		*ppInterface = (CK_INTERFACE_PTR)entry;
 		func_list = (CK_FUNCTION_LIST_PTR)entry->pFunctionList;
 		DBG_TRACE("Interface:");
 		DBG_TRACE("    Name: %s", entry->pInterfaceName);

pkcs11/src/ifsmw/mechanism.c
@@ -1878,7 +1878,6 @@ static CK_RV set_tls12_args(CK_MECHANISM_TYPE type,
 		return status;
 
 	obj = params->derived_key;
-	mech_list = get_key_mech_list(obj);
 	base_key = derive_args->key_descriptor_base;
 
 	ctx = params->ctx;
@@ -2008,6 +2007,13 @@ static CK_RV set_tls12_args(CK_MECHANISM_TYPE type,
 			goto end;
 
 		ke->random_data = rd;
+
+		mech_list = get_key_mech_list(obj);
+		if (!mech_list || !mech_list->number) {
+			status = CKR_ARGUMENTS_BAD;
+			goto end;
+		}
+
 		ke->encryption_name =
 			get_tls12_encryption_name(mech_list->mech[0],
 						  derived_key->security_size);

pkcs11/src/lib_cipher.c
@@ -83,7 +83,7 @@ static CK_RV set_iv_value(CK_VOID_PTR iv, CK_ULONG ivlen,
 		}
 	}
 
-	if (ctx->iv_length != ivlen)
+	if (!ctx->iv || ctx->iv_length != ivlen)
 		return CKR_MECHANISM_PARAM_INVALID;
 
 	memcpy(ctx->iv, iv, ivlen);
@@ -195,6 +195,11 @@ static CK_RV check_cipher_params(CK_MECHANISM_TYPE mechanism,
 		break;
 
 	case CKM_AES_CTR:
+		if (!pparameter) {
+			DBG_TRACE("CTR mode: Counter Bits is not set");
+			return CKR_MECHANISM_PARAM_INVALID;
+		}
+
 		if (ulparameterlen != sizeof(CK_AES_CTR_PARAMS)) {
 			DBG_TRACE("ulParameterLen error");
 			return CKR_MECHANISM_PARAM_INVALID;
@@ -250,6 +255,11 @@ static CK_RV check_cipher_params(CK_MECHANISM_TYPE mechanism,
 		break;
 
 	case CKM_SM4_CTR:
+		if (!pparameter) {
+			DBG_TRACE("SM4 CTR mode: Counter bits is not set");
+			return CKR_MECHANISM_PARAM_INVALID;
+		}
+
 		if (ulparameterlen != sizeof(CK_SM4_CTR_PARAMS)) {
 			DBG_TRACE("ulParameterLen error");
 			return CKR_MECHANISM_PARAM_INVALID;
@@ -267,6 +277,11 @@ static CK_RV check_cipher_params(CK_MECHANISM_TYPE mechanism,
 		break;
 
 	case CKM_AES_GCM:
+		if (!pparameter) {
+			DBG_TRACE("GCM mode: Parameters not set");
+			return CKR_MECHANISM_PARAM_INVALID;
+		}
+
 		if (op_flag & (CKF_MESSAGE_ENCRYPT | CKF_MESSAGE_DECRYPT)) {
 			if (ulparameterlen != sizeof(CK_GCM_MESSAGE_PARAMS)) {
 				DBG_TRACE("ulParameterLen error");
@@ -325,6 +340,11 @@ static CK_RV check_cipher_params(CK_MECHANISM_TYPE mechanism,
 		break;
 
 	case CKM_AES_CCM:
+		if (!pparameter) {
+			DBG_TRACE("CCM mode: Parameters not set");
+			return CKR_MECHANISM_PARAM_INVALID;
+		}
+
 		if (op_flag & (CKF_MESSAGE_ENCRYPT | CKF_MESSAGE_DECRYPT)) {
 			if (ulparameterlen != sizeof(CK_CCM_MESSAGE_PARAMS)) {
 				DBG_TRACE("ulParameterLen error");
@@ -361,6 +381,11 @@ static CK_RV check_cipher_params(CK_MECHANISM_TYPE mechanism,
 		break;
 
 	case CKM_CHACHA20_POLY1305:
+		if (!pparameter) {
+			DBG_TRACE("ChaCha20-Poly1305 mode: Parameters not set");
+			return CKR_MECHANISM_PARAM_INVALID;
+		}
+
 		if (op_flag & (CKF_MESSAGE_ENCRYPT | CKF_MESSAGE_DECRYPT)) {
 			if (ulparameterlen !=
 			    sizeof(CK_SALSA20_CHACHA20_POLY1305_MSG_PARAMS)) {

pkcs11/src/objects/attributes.c
@@ -618,6 +618,12 @@ CK_RV attr_modify_obj_value(CK_ATTRIBUTE_PTR attr,
 
 	for (; idx < nb_tattrs; idx++, tattr++) {
 		if (attr->type == tattr->type) {
+			if (!attr->pValue) {
+				DBG_TRACE("Attribute Type 0x%lx not defined",
+					  tattr->type);
+				return CKR_ATTRIBUTE_VALUE_INVALID;
+			}
+
 			if (tattr->modify)
 				return tattr->modify(obj + tattr->of_field,
 						     attr);

pkcs11/src/objects/lib_object.c
@@ -247,9 +247,12 @@ static CK_RV find_lock_token_object(CK_SESSION_HANDLE hsession,
 static CK_RV find_lock_object(CK_SESSION_HANDLE hsession,
 			      struct libobj_obj *obj, struct libobj_list **list)
 {
-	CK_RV ret = CKR_OK;
+	CK_RV ret = CKR_OBJECT_HANDLE_INVALID;
 	struct libdevice *dev = NULL;
 	struct libsess *sess = NULL;
+
+	if (!obj)
+		goto end;
 
 	ret = find_lock_token_object(hsession, obj, list);
 	if (ret == CKR_OK)
@@ -1225,11 +1228,15 @@ static CK_RV libobj_get_profile_attribute(struct libobj_obj *libobj,
 					  CK_ATTRIBUTE_PTR attrs,
 					  CK_ULONG nb_attrs)
 {
-	CK_RV ret = CKR_OK;
-
 	unsigned int i = 0;
 
 	for (; i < nb_attrs; i++) {
+		if (!(attrs + i)->pValue) {
+			DBG_TRACE("Attribute Type 0x%lx not defined",
+				  (attrs + i)->type);
+			return CKR_ATTRIBUTE_VALUE_INVALID;
+		}
+
 		switch ((attrs + i)->type) {
 		case CKA_CLASS:
 			*(CK_OBJECT_CLASS *)(attrs + i)->pValue = CKO_PROFILE;
@@ -1245,11 +1252,13 @@ static CK_RV libobj_get_profile_attribute(struct libobj_obj *libobj,
 			break;
 
 		default:
-			ret = CKR_ATTRIBUTE_TYPE_INVALID;
+			DBG_TRACE("Attribute Type 0x%lx not supported",
+				  (attrs + i)->type);
+			return CKR_ATTRIBUTE_TYPE_INVALID;
 		}
 	}
 
-	return ret;
+	return CKR_OK;
 }
 
 CK_RV libobj_get_attribute(CK_SESSION_HANDLE hsession, CK_OBJECT_HANDLE hobject,
@@ -1739,6 +1748,11 @@ CK_RV libobj_find_init(CK_SESSION_HANDLE hsession, CK_ATTRIBUTE_PTR attrs,
 			attrs_tmp[idx].type = attrs[idx].type;
 			attrs_tmp[idx].ulValueLen = attrs[idx].ulValueLen;
 			if (!attrs_tmp[idx].ulValueLen) {
+				ret = CKR_ATTRIBUTE_VALUE_INVALID;
+				goto end;
+			}
+
+			if (!attrs[idx].pValue) {
 				ret = CKR_ATTRIBUTE_VALUE_INVALID;
 				goto end;
 			}
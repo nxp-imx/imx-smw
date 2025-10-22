nxp-imx/imx-smw

commit 842537108b76a5161dd81c8137bdb1584558a6ce

SSMW-353 subsystems: tee: Add support of asymmetric encryption and decryption
Add support of asymmetric encryption and decryption
operations for TEE subsystem.

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

core/subsystems/tee/asymmetric_encryption.c
@@ -1,0 +1,287 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2025 NXP
+ */
+
+#include <tee_client_api.h>
+
+#include "smw_status.h"
+#include "debug.h"
+#include "utils.h"
+#include "base64.h"
+#include "subsystems.h"
+#include "config.h"
+#include "keymgr.h"
+#include "asymmetric_encryption.h"
+#include "tee.h"
+
+#define ASYMM_ENC_MODE_ID(_type, _id)                                          \
+	{                                                                      \
+		.smw_id = SMW_CONFIG_ASYMM_ENC_MODE_ID_##_type,                \
+		.tee_id = TEE_ASYMM_ENC_MODE_##_id                             \
+	}
+
+/**
+ * struct - Asymmetric encryption padding mode IDs
+ * @smw_id: Asymmetric encryption padding mode ID as defined in SMW.
+ * @tee_id: Asymmetric encryption padding mode ID as defined in TEE subsystem.
+ */
+static const struct {
+	enum smw_config_asymm_enc_mode_id smw_id;
+	enum tee_asymm_enc_mode tee_id;
+} asymm_enc_mode_ids[] = { ASYMM_ENC_MODE_ID(PKCS1_1_5, RSAES_PKCS1_V1_5),
+			   ASYMM_ENC_MODE_ID(OAEP, RSAES_PKCS1_OAEP),
+			   { SMW_CONFIG_ASYMM_ENC_MODE_ID_NO_PAD,
+			     TEE_ASYMM_ENC_MODE_RSA_NOPAD } };
+
+static int tee_get_asymm_enc_mode_id(enum smw_config_asymm_enc_mode_id smw_id,
+				     enum tee_asymm_enc_mode *tee_id)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	unsigned int i = 0;
+	unsigned int array_size = ARRAY_SIZE(asymm_enc_mode_ids);
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	for (i = 0; i < array_size; i++) {
+		if (asymm_enc_mode_ids[i].smw_id == smw_id) {
+			*tee_id = asymm_enc_mode_ids[i].tee_id;
+			status = SMW_STATUS_OK;
+			break;
+		}
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int get_pub_key_buffer_len(enum smw_keymgr_format_id format_id,
+				  unsigned char *pub_key_buffer,
+				  unsigned int pub_key_buffer_len,
+				  unsigned int *hex_pub_buffer_len)
+{
+	int status = SMW_STATUS_OK;
+
+	/* Key ID is set */
+	if (format_id == SMW_KEYMGR_FORMAT_ID_INVALID)
+		goto exit;
+
+	status = smw_keymgr_get_hex_key_buffer_len(format_id, pub_key_buffer,
+						   pub_key_buffer_len,
+						   hex_pub_buffer_len);
+
+exit:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+/**
+ * asymm_encrypt_decrypt() - Perform asymmetric encryption or decryption.
+ * @args: Internal asymmetric encryption arguments structure.
+ * @op_id: OPERATION_ID_ASYMM_ENCRYPT or OPERATION_ID_ASYMM_DECRYPT.
+ *
+ * Return:
+ * SMW_STATUS_OK		        - Success.
+ * SMW_STATUS_INVALID_PARAM	    - One of the parameters is invalid.
+ * SMW_STATUS_SUBSYSTEM_FAILURE	- Operation failed.
+ */
+static int asymm_encrypt_decrypt(struct smw_crypto_asymm_enc_args *args,
+				 enum operation_id op_id)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	TEEC_Operation operation = { 0 };
+	TEEC_SharedMemory shm = { 0 };
+
+	struct smw_keymgr_descriptor *key_desc = NULL;
+	struct smw_keymgr_identifier *key_identifier = NULL;
+	struct asymm_enc_shared_params *shared_params = NULL;
+	unsigned int shared_params_size =
+		sizeof(struct asymm_enc_shared_params);
+
+	unsigned int output_len = 0;
+	unsigned int salt_len = 0;
+	unsigned char *salt = NULL;
+	unsigned int pub_key_len = 0;
+	unsigned char *pub_key = NULL;
+
+	uint32_t param0_type = TEEC_NONE;
+
+	enum tee_key_type key_type_id = TEE_KEY_TYPE_ID_INVALID;
+	enum smw_keymgr_privacy_id key_privacy = SMW_KEYMGR_PRIVACY_ID_INVALID;
+
+	enum ta_commands cmd_id = CMD_ASYMM_DECRYPT;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (!args)
+		goto exit;
+
+	key_desc = &args->key_desc;
+	key_identifier = &key_desc->identifier;
+
+	status = tee_convert_key_type(key_identifier,
+				      SMW_CONFIG_HASH_ALGO_ID_INVALID,
+				      &key_type_id);
+	if (status != SMW_STATUS_OK)
+		goto exit;
+
+	if (key_type_id != TEE_KEY_TYPE_ID_RSA) {
+		SMW_DBG_PRINTF(ERROR, "Only RSA key type is supported.\n");
+		status = SMW_STATUS_INVALID_PARAM;
+		goto exit;
+	}
+
+	if (args->attrs.mode_id == SMW_CONFIG_ASYMM_ENC_MODE_ID_OAEP) {
+		salt_len = smw_crypto_get_asymm_enc_salt_len(args);
+		salt = smw_crypto_get_asymm_enc_salt(args);
+
+		if (salt && salt_len) {
+			if (ADD_OVERFLOW(shared_params_size, salt_len,
+					 &shared_params_size)) {
+				status = SMW_STATUS_INVALID_PARAM;
+				goto exit;
+			}
+		}
+	}
+
+	shared_params = SMW_UTILS_CALLOC(1, shared_params_size);
+	if (!shared_params) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		goto exit;
+	}
+
+	shared_params->salt_length = salt_len;
+
+	if (salt && salt_len)
+		SMW_UTILS_MEMCPY(shared_params->salt, salt, salt_len);
+
+	status = tee_convert_hash_algorithm_id(args->attrs.hash_id,
+					       &shared_params->hash_algorithm);
+	if (status != SMW_STATUS_OK)
+		goto exit;
+
+	status = tee_get_asymm_enc_mode_id(args->attrs.mode_id,
+					   &shared_params->mode);
+	if (status != SMW_STATUS_OK)
+		goto exit;
+
+	/*
+	 * params[0] = Key shared memory or none
+	 * params[1] = Pointer to asymmetric encrypt/decrypt shared params structure
+	 * params[2] = Input buffer and length
+	 * params[3] = Output buffer and length
+	 */
+
+	switch (op_id) {
+	case OPERATION_ID_ASYMM_ENCRYPT:
+		if (key_desc->format_id != SMW_KEYMGR_FORMAT_ID_INVALID) {
+			param0_type = TEEC_MEMREF_PARTIAL_INPUT;
+			key_privacy = SMW_KEYMGR_PRIVACY_ID_PUBLIC;
+		}
+
+		cmd_id = CMD_ASYMM_ENCRYPT;
+		break;
+
+	case OPERATION_ID_ASYMM_DECRYPT:
+		if (key_desc->format_id != SMW_KEYMGR_FORMAT_ID_INVALID) {
+			param0_type = TEEC_MEMREF_PARTIAL_INPUT;
+			key_privacy = SMW_KEYMGR_PRIVACY_ID_PAIR;
+		}
+
+		cmd_id = CMD_ASYMM_DECRYPT;
+		break;
+
+	default:
+		goto exit;
+	}
+
+	if (param0_type == TEEC_MEMREF_PARTIAL_INPUT) {
+		pub_key = smw_keymgr_get_public_data(key_desc);
+		pub_key_len = smw_keymgr_get_public_length(key_desc);
+
+		if (!pub_key || !pub_key_len) {
+			status = SMW_STATUS_INVALID_PARAM;
+			goto exit;
+		}
+
+		status = get_pub_key_buffer_len(key_desc->format_id, pub_key,
+						pub_key_len,
+						&shared_params->pub_key_len);
+		if (status != SMW_STATUS_OK)
+			goto exit;
+
+		status = copy_keys_to_shm(&shm, key_desc, key_privacy);
+		if (status != SMW_STATUS_OK)
+			goto exit;
+
+		operation.params[0].memref.parent = &shm;
+		operation.params[0].memref.offset = 0;
+		operation.params[0].memref.size = shm.size;
+	} else {
+		shared_params->id = key_identifier->id;
+	}
+
+	shared_params->key_type = key_type_id;
+	shared_params->security_size = key_identifier->security_size;
+
+	operation.paramTypes =
+		TEEC_PARAM_TYPES(param0_type, TEEC_MEMREF_TEMP_INPUT,
+				 TEEC_MEMREF_TEMP_INPUT,
+				 TEEC_MEMREF_TEMP_OUTPUT);
+
+	operation.params[1].tmpref.buffer = shared_params;
+	operation.params[1].tmpref.size = shared_params_size;
+	operation.params[2].tmpref.buffer =
+		smw_crypto_get_asymm_enc_input(args);
+	operation.params[2].tmpref.size =
+		smw_crypto_get_asymm_enc_input_len(args);
+	operation.params[3].tmpref.buffer =
+		smw_crypto_get_asymm_enc_output(args);
+	operation.params[3].tmpref.size =
+		smw_crypto_get_asymm_enc_output_len(args);
+
+	/* Invoke TA */
+	status = execute_tee_cmd(cmd_id, &operation);
+	SMW_DBG_PRINTF_COND(ERROR, status != SMW_STATUS_OK,
+			    "%s: Operation failed\n", __func__);
+
+	if (status != SMW_STATUS_OK && status != SMW_STATUS_OUTPUT_TOO_SHORT)
+		goto exit;
+
+	if (!SET_OVERFLOW(operation.params[3].tmpref.size, output_len)) {
+		smw_crypto_set_asymm_enc_output_len(args, output_len);
+
+		SMW_DBG_PRINTF(DEBUG, "Output length = %u\n", output_len);
+	} else {
+		status = SMW_STATUS_OPERATION_FAILURE;
+	}
+
+exit:
+	if (shared_params)
+		free(shared_params);
+
+	if (param0_type == TEEC_MEMREF_PARTIAL_INPUT)
+		TEEC_ReleaseSharedMemory(&shm);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+bool tee_asymm_encrypt_decrypt_handle(enum operation_id op_id, void *args,
+				      int *status)
+{
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	switch (op_id) {
+	case OPERATION_ID_ASYMM_ENCRYPT:
+	case OPERATION_ID_ASYMM_DECRYPT:
+		*status = asymm_encrypt_decrypt(args, op_id);
+		break;
+	default:
+		return false;
+	}
+
+	return true;
+}

core/subsystems/tee/lib_ta/core/asymmetric_encryption.c
@@ -1,0 +1,309 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2025 NXP
+ */
+
+#include <util.h>
+#include <string.h>
+#include <tee_internal_api_extensions.h>
+
+#include "common.h"
+#include "tee_subsystem.h"
+#include "keymgr.h"
+#include "asymmetric_encryption.h"
+#include "obj.h"
+
+#define RSA_OAEP_ALGO_ID(_hash_algo)                                           \
+	{                                                                      \
+		.mode = TEE_ASYMM_ENC_MODE_RSAES_PKCS1_OAEP,                   \
+		.hash_algo = TEE_ALGORITHM_ID_##_hash_algo,                    \
+		.tee_algorithm_id = TEE_ALG_RSAES_PKCS1_OAEP_MGF1_##_hash_algo \
+	}
+
+static const struct {
+	enum tee_asymm_enc_mode mode;
+	enum tee_algorithm_id hash_algo;
+	uint32_t tee_algorithm_id;
+} rsa_algorithm_ids[] = { RSA_OAEP_ALGO_ID(SHA1),
+			  RSA_OAEP_ALGO_ID(SHA224),
+			  RSA_OAEP_ALGO_ID(SHA256),
+			  RSA_OAEP_ALGO_ID(SHA384),
+			  RSA_OAEP_ALGO_ID(SHA512),
+			  { TEE_ASYMM_ENC_MODE_RSAES_PKCS1_V1_5,
+			    TEE_ALGORITHM_ID_INVALID,
+			    TEE_ALG_RSAES_PKCS1_V1_5 },
+			  { TEE_ASYMM_ENC_MODE_RSA_NOPAD,
+			    TEE_ALGORITHM_ID_INVALID, TEE_ALG_RSA_NOPAD } };
+
+/**
+ * get_rsa_algo_id() - Get sign verify algorithm ID from RSA key type.
+ * @signature_type: Signature type.
+ * @hash_algo: Hash algorithm used for the operation.
+ * @digest_len: Length of the operation hashed message in bytes.
+ * @algorithm_id: Pointer to the algorithm ID variable to update.
+ *
+ * If @hash_algo is TEE_ALGORITHM_ID_INVALID, @digest_len must be set. Else,
+ * @digest_len is not necessary.
+ *
+ * Return:
+ * TEE_SUCCESS			- Success.
+ * TEE_ERROR_BAD_PARAMETERS	- One of the parameter is invalid.
+ * TEE_ERROR_NOT_SUPPORTED	- Operation not supported.
+ */
+static TEE_Result get_rsa_algo_id(enum tee_asymm_enc_mode mode,
+				  enum tee_algorithm_id hash_algo,
+				  enum tee_algorithm_id *algorithm_id)
+{
+	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
+	unsigned int i = 0;
+	unsigned int size = ARRAY_SIZE(rsa_algorithm_ids);
+
+	FMSG("Executing %s", __func__);
+
+	if (!algorithm_id)
+		goto end;
+
+	for (; i < size; i++) {
+		if (rsa_algorithm_ids[i].mode == mode &&
+		    ((rsa_algorithm_ids[i].mode !=
+			      TEE_ASYMM_ENC_MODE_RSAES_PKCS1_OAEP &&
+		      rsa_algorithm_ids[i].hash_algo ==
+			      TEE_ALGORITHM_ID_INVALID) ||
+		     rsa_algorithm_ids[i].hash_algo == hash_algo)) {
+			*algorithm_id = rsa_algorithm_ids[i].tee_algorithm_id;
+			res = TEE_SUCCESS;
+			break;
+		}
+	}
+
+end:
+	return res;
+}
+
+/**
+ * set_key() - Set key handle for asymmetric encryption and decryption operation
+ * @cmd_id: Command ID.
+ * @ta_param: TA parameter. Contains key buffer or nothing.
+ * @ta_param_type: @ta_param type.
+ * @shared_params: Pointer to Asymmetric encryption shared parameters structure.
+ * @key_handle: Pointer to key handle to update.
+ * @persistent: Pointer to key info to update.
+ * @algo_id: TEE algorithm ID
+ *
+ * If key is defined as buffer, it is imported as a new transient object. This
+ * new key handle is returned and the key is deleted at the end of the
+ * operation.
+ * If key is defined as key ID, key handle is retrieved.
+ *
+ * Return:
+ * TEE_SUCCESS              - Success.
+ * TEE_ERROR_BAD_PARAMETERS	- One of the parameters is invalid.
+ * Error code from ta_import_key().
+ * Error code from ta_get_obj_handle().
+ */
+static TEE_Result set_key(uint32_t cmd_id, TEE_Param ta_param,
+			  uint32_t ta_param_type,
+			  struct asymm_enc_shared_params *shared_params,
+			  TEE_ObjectHandle *key_handle, bool *persistent,
+			  enum tee_algorithm_id algo_id)
+{
+	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
+	unsigned char *pub_key = NULL;
+	unsigned char *priv_key = NULL;
+	unsigned char *modulus = NULL;
+	unsigned char *ptr = NULL;
+	size_t priv_key_len = 0;
+	size_t modulus_len = 0;
+	size_t key_size = BITS_TO_BYTES_SIZE(shared_params->security_size);
+	uint32_t key_usage = 0;
+
+	FMSG("Executing %s", __func__);
+
+	switch (ta_param_type) {
+	case TEE_PARAM_TYPE_MEMREF_INPUT:
+		/* Set key buffers and lengths */
+		pub_key = ta_param.memref.buffer;
+
+		ptr = pub_key + shared_params->pub_key_len;
+
+		if (cmd_id == CMD_ASYMM_ENCRYPT) {
+			key_usage = TEE_USAGE_ENCRYPT;
+
+			if (algo_id == TEE_ALG_RSA_NOPAD)
+				key_usage |= TEE_USAGE_VERIFY;
+		} else {
+			priv_key = ptr;
+			priv_key_len = key_size;
+			ptr = priv_key + priv_key_len;
+			key_usage = TEE_USAGE_DECRYPT;
+
+			if (algo_id == TEE_ALG_RSA_NOPAD)
+				key_usage |= TEE_USAGE_SIGN;
+		}
+
+		if (shared_params->key_type == TEE_KEY_TYPE_ID_RSA) {
+			modulus = ptr;
+			modulus_len = key_size;
+		}
+
+		/* Import key */
+		res = ta_import_key(key_handle, shared_params->key_type,
+				    shared_params->security_size, key_usage,
+				    priv_key, priv_key_len, pub_key,
+				    shared_params->pub_key_len, modulus,
+				    modulus_len);
+		if (res)
+			EMSG("Failed to import key: 0x%x", res);
+
+		return res;
+
+	case TEE_PARAM_TYPE_NONE:
+		EMSG("Retrieve key handle");
+		res = ta_get_obj_handle(key_handle, shared_params->id,
+					persistent);
+		if (res)
+			EMSG("Key not found: 0x%x", res);
+
+		return res;
+
+	default:
+		return res;
+	}
+}
+
+TEE_Result asymm_encrypt_decrypt(uint32_t param_types,
+				 TEE_Param params[TEE_NUM_PARAMS],
+				 uint32_t cmd_id)
+{
+	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
+	TEE_OperationHandle operation = TEE_HANDLE_NULL;
+	TEE_ObjectHandle key_handle = TEE_HANDLE_NULL;
+	TEE_Attribute attr = { 0 };
+	TEE_ObjectInfo key_info = { 0 };
+	uint32_t param0_type = TEE_PARAM_TYPE_GET(param_types, 0);
+	uint32_t mode = 0;
+	enum tee_algorithm_id algorithm_id = 0;
+	bool persistent = false;
+	struct asymm_enc_shared_params *shared_params = NULL;
+	uint32_t op_max_key_size = 0;
+	uint32_t attr_count = 1;
+	size_t size = 0;
+
+	FMSG("Executing %s (%d)", __func__, cmd_id);
+
+	/*
+	 * params[0] = Key buffer or none
+	 * params[1] = Pointer to asymmetric encrypt/decrypt shared params structure
+	 * params[2] = Input buffer and length
+	 * params[3] = Output buffer and length
+	 */
+
+	if (cmd_id == CMD_ASYMM_ENCRYPT)
+		mode = TEE_MODE_ENCRYPT;
+	else
+		mode = TEE_MODE_DECRYPT;
+
+	if ((TEE_PARAM_TYPE_GET(param_types, 1) !=
+	     TEE_PARAM_TYPE_MEMREF_INPUT) ||
+	    params[1].memref.size < sizeof(*shared_params) ||
+	    !params[1].memref.buffer ||
+	    (TEE_PARAM_TYPE_GET(param_types, 2) !=
+	     TEE_PARAM_TYPE_MEMREF_INPUT) ||
+	    (TEE_PARAM_TYPE_GET(param_types, 3) !=
+	     TEE_PARAM_TYPE_MEMREF_OUTPUT))
+		goto err;
+
+	shared_params = params[1].memref.buffer;
+
+	if (ADD_OVERFLOW(sizeof(*shared_params), shared_params->salt_length,
+			 &size))
+		goto err;
+
+	if (params[1].memref.size != size)
+		goto err;
+
+	op_max_key_size = shared_params->security_size;
+
+	/* Get TEE algorithm ID */
+	if (shared_params->key_type == TEE_KEY_TYPE_ID_RSA) {
+		res = get_rsa_algo_id(shared_params->mode,
+				      shared_params->hash_algorithm,
+				      &algorithm_id);
+
+		if (res) {
+			EMSG("Failed to get algorithm ID: 0x%x", res);
+			goto err;
+		}
+
+		/* Set salt length attribute if needed */
+		if (shared_params->salt_length) {
+			TEE_InitRefAttribute(&attr, TEE_ATTR_RSA_OAEP_LABEL,
+					     shared_params->salt,
+					     shared_params->salt_length);
+		}
+	} else {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto err;
+	}
+
+	res = set_key(cmd_id, params[0], param0_type, shared_params,
+		      &key_handle, &persistent, algorithm_id);
+	if (res)
+		goto err;
+
+	res = TEE_AllocateOperation(&operation, algorithm_id, mode,
+				    op_max_key_size);
+	if (res) {
+		EMSG("Failed to alloc operation: 0x%x", res);
+		goto err;
+	}
+
+	res = TEE_GetObjectInfo1(key_handle, &key_info);
+	if (res != TEE_SUCCESS) {
+		EMSG("Failed to get key info (0x%x)", res);
+		goto err;
+	}
+
+	res = check_operation_keys_usage(operation, &key_info, 1);
+	if (res)
+		goto err;
+
+	res = TEE_SetOperationKey(operation, key_handle);
+	if (res) {
+		EMSG("Failed to set operation key: 0x%x", res);
+		goto err;
+	}
+
+	if (cmd_id == CMD_ASYMM_ENCRYPT) {
+		res = TEE_AsymmetricEncrypt(operation, &attr, attr_count,
+					    params[2].memref.buffer,
+					    params[2].memref.size,
+					    params[3].memref.buffer,
+					    &params[3].memref.size);
+		if (res)
+			EMSG("Failed to perform TEE_AsymmetricEncrypt: 0x%x",
+			     res);
+	} else {
+		res = TEE_AsymmetricDecrypt(operation, &attr, attr_count,
+					    params[2].memref.buffer,
+					    params[2].memref.size,
+					    params[3].memref.buffer,
+					    &params[3].memref.size);
+		if (res)
+			EMSG("Failed to perform TEE_AsymmetricDecrypt: 0x%x",
+			     res);
+	}
+
+err:
+	if (key_handle != TEE_HANDLE_NULL) {
+		if (persistent)
+			TEE_CloseObject(key_handle);
+		else if (param0_type == TEE_PARAM_TYPE_MEMREF_INPUT)
+			TEE_FreeTransientObject(key_handle);
+	}
+
+	if (operation != TEE_HANDLE_NULL)
+		TEE_FreeOperation(operation);
+
+	return res;
+}

core/subsystems/tee/CMakeLists.txt
@@ -39,7 +39,8 @@ target_sources(tee PRIVATE
                $<$<BOOL:${ENABLE_CIPHER}>:cipher.c>
                $<$<BOOL:${ENABLE_CONTEXT}>:operation_context.c>
                $<$<BOOL:${ENABLE_AEAD}>:aead.c>
-               $<$<BOOL:${ENABLE_STORAGE_MODULE}>:storage.c>)
+               $<$<BOOL:${ENABLE_STORAGE_MODULE}>:storage.c>
+               $<$<BOOL:${ENABLE_ASYMMETRIC_ENCRYPTION}>:asymmetric_encryption.c>)
 
 target_include_directories(tee PRIVATE
                            ${TA_HOST_INCLUDE_DIR}

core/subsystems/tee/common/tee_subsystem.h
@@ -110,6 +110,13 @@ enum tee_signature_type {
 	TEE_SIGNATURE_TYPE_EDDSA_CTX
 };
 
+/* TEE asymmetric encryption padding mode */
+enum tee_asymm_enc_mode {
+	TEE_ASYMM_ENC_MODE_RSAES_PKCS1_V1_5,
+	TEE_ASYMM_ENC_MODE_RSAES_PKCS1_OAEP,
+	TEE_ASYMM_ENC_MODE_RSA_NOPAD,
+};
+
 /* TA commands */
 enum ta_commands {
 	CMD_GENERATE_KEY,
@@ -143,6 +150,8 @@ enum ta_commands {
 	CMD_HASH_INIT,
 	CMD_HASH_UPDATE,
 	CMD_HASH_FINAL,
+	CMD_ASYMM_ENCRYPT,
+	CMD_ASYMM_DECRYPT,
 	CMD_INVALID,
 };
 
@@ -256,6 +265,31 @@ struct key_derive_shared_params {
 	unsigned int derived_key_sec_size;
 	unsigned int derived_key_len;
 	bool store_derived_key;
+};
+
+/**
+ * struct asymm_enc_shared_params - Asymmetric encryption shared parameters.
+ * @id: Key ID. Not set if a buffer is used.
+ * @key_type: Key type.
+ * @security_size: Key security size.
+ * @hash_algorithm: Hash algorithm.
+ * @mode: Asymmetric encryption padding mode.
+ * @pub_key_len: Public key buffer length in bytes.
+ * @salt_length: Salt buffer length.
+ * @salt: Salt buffer.
+ *
+ * @salt and @salt_length are optional parameters for
+ * TEE_ALG_RSAES_PKCS1_OAEP_MGF1_XXX algorithms.
+ */
+struct asymm_enc_shared_params {
+	uint32_t id;
+	enum tee_key_type key_type;
+	unsigned int security_size;
+	enum tee_algorithm_id hash_algorithm;
+	enum tee_asymm_enc_mode mode;
+	unsigned int pub_key_len;
+	unsigned int salt_length;
+	unsigned char salt[];
 };
 
 #endif /* TEE_SUBSYSTEM_H */

core/subsystems/tee/lib_ta/core/entry.c
@@ -19,6 +19,7 @@
 #include "operation_context.h"
 #include "obj.h"
 #include "storage.h"
+#include "asymmetric_encryption.h"
 
 TEE_Result libsmw_attach(void)
 {
@@ -170,6 +171,11 @@ TEE_Result libsmw_dispatcher(uint32_t cmd_id, uint32_t param_types,
 
 	case CMD_STORAGE_GET_DATA_INFO:
 		res = storage_get_data_info(param_types, params);
+		break;
+
+	case CMD_ASYMM_ENCRYPT:
+	case CMD_ASYMM_DECRYPT:
+		res = asymm_encrypt_decrypt(param_types, params, cmd_id);
 		break;
 
 	default:

core/subsystems/tee/lib_ta/core/inc/asymmetric_encryption.h
@@ -1,0 +1,24 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2025 NXP
+ */
+
+#ifndef TA_ASYMMETRIC_ENCRYPTION_H
+#define TA_ASYMMETRIC_ENCRYPTION_H
+
+/**
+ * asymm_encrypt_decrypt() - Perform asymmetric encrypt or decrypt operation.
+ * @param_types: Parameters types.
+ * @params: Shared parameters between Secure and Normal world.
+ * @cmd_id: CMD_ASYMM_ENCRYPT or CMD_ASYMM_DECRYPT
+ *
+ * Return:
+ * TEE_SUCCESS			    - Success.
+ * TEE_ERROR_BAD_PARAMETERS	- One of the parameters is invalid.
+ * Error code from internal functions.
+ */
+TEE_Result asymm_encrypt_decrypt(uint32_t param_types,
+				 TEE_Param params[TEE_NUM_PARAMS],
+				 uint32_t cmd_id);
+
+#endif /* TA_ASYMMETRIC_ENCRYPTION_H */

core/subsystems/tee/lib_ta/core/sub.mk
@@ -16,3 +16,4 @@ srcs-y += common.c
 srcs-y += aead.c
 srcs-y += obj.c
 srcs-y += storage.c
+srcs-y += asymmetric_encryption.c

core/subsystems/tee/tee.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2024 NXP
+ * Copyright 2020-2025 NXP
  */
 
 #include <tee_client_api.h>
@@ -106,6 +106,17 @@ __weak bool tee_aead_handle(enum operation_id operation_id, void *args,
 
 __weak bool tee_storage_handle(enum operation_id operation_id, void *args,
 			       int *status)
+
+{
+	(void)operation_id;
+	(void)args;
+	(void)status;
+
+	return false;
+}
+
+__weak bool tee_asymm_encrypt_decrypt_handle(enum operation_id operation_id,
+					     void *args, int *status)
 
 {
 	(void)operation_id;
@@ -356,8 +367,10 @@ static int execute(enum operation_id op_id, void *args)
 		goto end;
 	else if (tee_aead_handle(op_id, args, &status))
 		goto end;
+	else if (tee_storage_handle(op_id, args, &status))
+		goto end;
+	tee_asymm_encrypt_decrypt_handle(op_id, args, &status);
 
-	tee_storage_handle(op_id, args, &status);
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;

commit 446285d5ea70e418175745b60b90cf2f7ee8d749

SSMW-1055 subsystems: tee: Validate asymmetric encryption algorithm
- Report error if the main permitted algorithm and mode are not set for
  asymmetric encryption during key generation and import.
- For asymmetric encryption and decryption operation, set the
  output buffer length to 0, if output buffer is NULL.
- Fix the parameter type issues.

Fixes: 842537108b76 ("SSMW-353 subsystems: tee: Add support of asymmetric encryption and decryption")

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

core/subsystems/tee/asymmetric_encryption.c
@@ -242,6 +242,13 @@ static int asymm_encrypt_decrypt(struct smw_crypto_asymm_enc_args *args,
 	operation.params[3].tmpref.size =
 		smw_crypto_get_asymm_enc_output_len(args);
 
+	/*
+	 * TEE requires an output buffer length set to 0 if output
+	 * buffer is NULL.
+	 */
+	if (!operation.params[3].tmpref.buffer)
+		operation.params[3].tmpref.size = 0;
+
 	/* Invoke TA */
 	status = execute_tee_cmd(cmd_id, &operation);
 	SMW_DBG_PRINTF_COND(ERROR, status != SMW_STATUS_OK,

core/subsystems/tee/keymgr.c
@@ -543,6 +543,59 @@ exit:
 }
 
 /**
+ * check_asymm_encryption_algo() - Check if permitted algorithm is set for
+ *                                 asymmetric encryption and update usage flags
+ * @perm_algo: Permitted algorithm attribute
+ * @usage: Key Usage
+ *
+ * For asymmetric encryption operation using RSA no padding algo
+ * (TEE_ALG_RSA_NOPAD) has the below usage restriction:
+ * - For encryption operation, the object usage SHALL contain both the
+ *   encrypt and verify flags.
+ * - For decryption operation, the object usage SHALL contain both the
+ *   decrypt and sign flags.
+ *
+ * Return:
+ * SMW_STATUS_OK                     - Success
+ * SMW_STATUS_PERMITTED_ALGO_INVALID - Missing one or more algorithm parameters
+ */
+static int check_asymm_encryption_algo(smw_attr_algo_t perm_algo,
+				       smw_attr_usage_t *usage)
+{
+	int status = SMW_STATUS_OK;
+	smw_attr_algo_t algo = SMW_ATTR_GET_ALGO(perm_algo);
+	smw_attr_algo_t mode = SMW_ATTR_GET_MODE(perm_algo);
+	smw_attr_algo_t class = SMW_ATTR_GET_CLASS(perm_algo);
+	smw_attr_usage_t usage_flags = *usage;
+
+	if (class == SMW_ATTR_CLASS_NONE)
+		return status;
+
+	if (class == SMW_ATTR_CLASS_ASYMMETRIC_ENCRYPTION) {
+		if (!algo || !mode) {
+			SMW_DBG_PRINTF(ERROR,
+				       "Permitted algo required for asymm encr");
+			return SMW_STATUS_PERMITTED_ALGO_INVALID;
+		}
+
+		if (algo == SMW_ATTR_ALGO_RSA &&
+		    (mode == SMW_ATTR_MODE_NO_PAD ||
+		     mode == SMW_ATTR_MODE_ANY)) {
+			if (SMW_ATTR_USAGE_IS_ENCRYPT(usage_flags))
+				SMW_ATTR_USAGE_SET_VERIFY_MESSAGE(usage_flags);
+
+			if (SMW_ATTR_USAGE_IS_DECRYPT(usage_flags))
+				SMW_ATTR_USAGE_SET_SIGN_MESSAGE(usage_flags);
+		}
+
+		*usage = usage_flags;
+		status = SMW_STATUS_OK;
+	}
+
+	return status;
+}
+
+/**
  * generate_key() - Generate a key.
  * @args: Key generation arguments.
  *
@@ -570,6 +623,7 @@ static int generate_key(void *args)
 	smw_attr_usage_t actual_usage_flags = 0;
 	enum smw_config_hash_algo_id hash_id = SMW_CONFIG_HASH_ALGO_ID_INVALID;
 	smw_attr_algo_t perm_algo = 0;
+	smw_attr_usage_t usage_flags = SMW_ATTR_USAGE_NONE;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -607,7 +661,12 @@ static int generate_key(void *args)
 	if (status != SMW_STATUS_OK)
 		goto exit;
 
-	key_usage_to_tee(key_attrs->usage_flags, &shared_params.key_usage);
+	usage_flags = key_attrs->usage_flags;
+	status = check_asymm_encryption_algo(perm_algo, &usage_flags);
+	if (status != SMW_STATUS_OK)
+		goto exit;
+
+	key_usage_to_tee(usage_flags, &shared_params.key_usage);
 	key_usage_to_smw(shared_params.key_usage, &actual_usage_flags);
 
 	op.params[0].tmpref.buffer = &shared_params;
@@ -1110,6 +1169,7 @@ static int import_key(void *args)
 	smw_attr_usage_t actual_usage_flags = SMW_ATTR_USAGE_NONE;
 	enum smw_config_hash_algo_id hash_id = SMW_CONFIG_HASH_ALGO_ID_INVALID;
 	smw_attr_algo_t perm_algo = 0;
+	smw_attr_usage_t usage_flags = SMW_ATTR_USAGE_NONE;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -1162,7 +1222,12 @@ static int import_key(void *args)
 	if (status != SMW_STATUS_OK)
 		goto exit;
 
-	key_usage_to_tee(key_attrs->usage_flags, &shared_params.key_usage);
+	usage_flags = key_attrs->usage_flags;
+	status = check_asymm_encryption_algo(perm_algo, &usage_flags);
+	if (status != SMW_STATUS_OK)
+		goto exit;
+
+	key_usage_to_tee(usage_flags, &shared_params.key_usage);
 	key_usage_to_smw(shared_params.key_usage, &actual_usage_flags);
 
 	/*

core/subsystems/tee/lib_ta/core/asymmetric_encryption.c
@@ -52,7 +52,7 @@ static const struct {
  */
 static TEE_Result get_rsa_algo_id(enum tee_asymm_enc_mode mode,
 				  enum tee_algorithm_id hash_algo,
-				  enum tee_algorithm_id *algorithm_id)
+				  uint32_t *algorithm_id)
 {
 	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
 	unsigned int i = 0;
@@ -105,7 +105,7 @@ static TEE_Result set_key(uint32_t cmd_id, TEE_Param ta_param,
 			  uint32_t ta_param_type,
 			  struct asymm_enc_shared_params *shared_params,
 			  TEE_ObjectHandle *key_handle, bool *persistent,
-			  enum tee_algorithm_id algo_id)
+			  uint32_t algo_id)
 {
 	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
 	unsigned char *pub_key = NULL;
@@ -182,7 +182,7 @@ TEE_Result asymm_encrypt_decrypt(uint32_t param_types,
 	TEE_ObjectInfo key_info = { 0 };
 	uint32_t param0_type = TEE_PARAM_TYPE_GET(param_types, 0);
 	uint32_t mode = 0;
-	enum tee_algorithm_id algorithm_id = 0;
+	uint32_t algorithm_id = 0;
 	bool persistent = false;
 	struct asymm_enc_shared_params *shared_params = NULL;
 	uint32_t op_max_key_size = 0;
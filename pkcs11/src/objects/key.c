nxp-imx/imx-smw

commit 69f7dfecfe9d3cc644964d27f135f1f9e82e95f9

SSMW-1059 pkcs11: TLS: Free the ECDH peer public buffer
CKM_ECDH1_DERIVE mechanism is allocating a derive context to store the
peer public buffer.
This buffer is used for the CKM_HKDF_DERIVE,
CKM_TLS12_MASTER_KEY_DERIVE_DH and CKM_TLS12_EXTENDED_MASTER_KEY_DERIVE_DH
mechanism.
This buffer is released when the master key is derived.

Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

pkcs11/src/objects/key.c
@@ -4,6 +4,7 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
 
 #include "attributes.h"
 #include "key.h"
@@ -18,8 +19,18 @@
 #include "libobj_types.h"
 
 #include "util.h"
+#include "compiler.h"
 
 #include "trace.h"
+
+/*
+ * TLS 1.3 expanded_label is composed of:
+ * - 2 bytes: args->length as uint16_t
+ * - 1 byte: prefix_length (6) + args->label_length
+ * - 6 bytes: the prefix ("tls13 ")
+ * - `args->label_length` bytes: the input args->label
+ */
+#define TLS13_LABEL_OFFSET (2 + 1 + 6)
 
 enum attr_key_common_list {
 	KEY_TYPE = 0,
@@ -2308,12 +2319,16 @@ CK_RV derive_key(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
 		 struct libattr_list *attrs)
 {
 	CK_RV ret = CKR_GENERAL_ERROR;
+	CK_BYTE_PTR label = NULL;
 	CK_KEY_TYPE key_type = 0;
 	CK_KEY_TYPE base_key_type = 0;
 	CK_MECHANISM find_mech = { 0 };
 	struct lib_derive_ctx *ctx = NULL;
 	struct libdevice *device = NULL;
 	struct libobj_key_derive_params derive_params = { 0 };
+	struct libmech_list *mech_list = NULL;
+	/* ASCII: "finished", in hex for EBCDIC compatibility */
+	static const char finishedlabel[] = "\x66\x69\x6E\x69\x73\x68\x65\x64";
 
 	DBG_TRACE("Derive a secret key from base key object");
 
@@ -2361,27 +2376,41 @@ CK_RV derive_key(CK_SESSION_HANDLE hsession, CK_MECHANISM_PTR mech,
 	if (ret != CKR_OK)
 		goto end;
 
-	if (mech->mechanism == CKM_ECDH1_DERIVE) {
-		/* Remove previous operation context */
-		ret = libdev_find_opctx(device, CKF_DERIVE, &find_mech,
-					(void **)&ctx);
-		if (ret == CKR_OK) {
-			(void)libdev_remove_opctx(device, CKF_DERIVE);
-			destroy_context(ctx);
+	mech_list = get_key_mech_list(derived_key);
+	if (mech_list && mech_list->number) {
+		switch (mech_list->mech[0]) {
+		case CKM_TLS12_MASTER_KEY_DERIVE_DH:
+		case CKM_TLS12_EXTENDED_MASTER_KEY_DERIVE_DH:
+		case CKM_HKDF_DERIVE:
+			if (mech->mechanism == CKM_ECDH1_DERIVE) {
+				/* Remove previous operation context */
+				ret = libdev_find_opctx(device, CKF_DERIVE,
+							&find_mech,
+							(void **)&ctx);
+				if (ret == CKR_OK) {
+					(void)libdev_remove_opctx(device,
+								  CKF_DERIVE);
+					destroy_context(ctx);
+				}
+
+				ctx = calloc(1, sizeof(*ctx));
+				if (!ctx) {
+					ret = CKR_HOST_MEMORY;
+					goto end;
+				}
+
+				/* Set current operation context */
+				ret = libdev_add_opctx(device, CKF_DERIVE, mech,
+						       ctx);
+				if (ret != CKR_OK)
+					goto end;
+
+				derive_params.ctx = ctx;
+			}
+			break;
+		default:
+			break;
 		}
-
-		ctx = calloc(1, sizeof(*ctx));
-		if (!ctx) {
-			ret = CKR_HOST_MEMORY;
-			goto end;
-		}
-
-		/* Set current operation context */
-		ret = libdev_add_opctx(device, CKF_DERIVE, mech, ctx);
-		if (ret != CKR_OK)
-			goto end;
-
-		derive_params.ctx = ctx;
 	}
 
 	derive_params.hsession = hsession;
@@ -2429,6 +2458,36 @@ end:
 		if (mech->mechanism == CKM_ECDH1_DERIVE && ctx) {
 			(void)libdev_remove_opctx(device, CKF_DERIVE);
 			destroy_context(ctx);
+		}
+	} else {
+		switch (mech->mechanism) {
+		case CKM_HKDF_DERIVE:
+			if (!derive_params.hkdf_params.info ||
+			    derive_params.hkdf_params.info_len !=
+				    TLS13_LABEL_OFFSET + sizeof(finishedlabel))
+				break;
+
+			label = &derive_params.hkdf_params
+					 .info[TLS13_LABEL_OFFSET];
+			if (strncmp((char *)label, finishedlabel,
+				    sizeof(finishedlabel)))
+				break;
+
+			__fallthrough;
+
+		case CKM_TLS12_KEY_AND_MAC_DERIVE:
+			if (!ctx)
+				break;
+
+			if (ctx->context)
+				(void)libdev_cancel_operation(&ctx->context);
+
+			(void)libdev_remove_opctx(device, CKF_DERIVE);
+			destroy_context(ctx);
+			break;
+
+		default:
+			break;
 		}
 	}
 

pkcs11/src/ifsmw/mechanism.c
@@ -1984,6 +1984,10 @@ static CK_RV set_ecdh_args(struct libobj_key_derive_params *derive_params,
 	if (!derive_params || !derive_args)
 		return status;
 
+	if (!derive_params->ecdh_params.pPublicData ||
+	    !derive_params->ecdh_params.ulPublicDataLen)
+		return status;
+
 	if (derive_params->ecdh_params.kdf != CKD_NULL ||
 	    derive_params->ecdh_params.pSharedData ||
 	    derive_params->ecdh_params.ulSharedDataLen)
@@ -2370,7 +2374,7 @@ static CK_RV set_tls13_args(struct libobj_key_derive_params *derive_params,
 	/* Check if we are doing early secret derivation */
 	if (!strncmp(derived_secret_label, (char *)label,
 		     strlen(derived_secret_label))) {
-		derive_params->ctx->skipped = true;
+		ctx->skipped = true;
 		return CKR_OK;
 	}
 
@@ -2394,6 +2398,7 @@ static CK_RV op_mkeyderive(CK_SLOT_ID slotid, struct mentry *entry, void *args)
 	struct smw_kdf_tls13_args tls13_args = { 0 };
 	struct smw_kdf_tls12_op_args tls12_args = { 0 };
 	struct libobj_key_derive_params *derive_params = args;
+	struct lib_derive_ctx *ctx = derive_params->ctx;
 	struct libobj_obj *obj = derive_params->derived_key;
 
 	devinfo = libdev_get_devinfo(slotid);
@@ -2429,7 +2434,7 @@ static CK_RV op_mkeyderive(CK_SLOT_ID slotid, struct mentry *entry, void *args)
 
 	switch (entry->type) {
 	case CKM_HKDF_DERIVE:
-		if (derive_params->ctx) {
+		if (ctx) {
 			if (!(derive_params->hkdf_params.extract ^
 			      derive_params->hkdf_params.expand))
 				return CKR_ARGUMENTS_BAD;
@@ -2446,7 +2451,7 @@ static CK_RV op_mkeyderive(CK_SLOT_ID slotid, struct mentry *entry, void *args)
 
 			set_key_tls(obj, TLS13_KEY);
 
-			if (derive_params->ctx->skipped)
+			if (ctx->skipped)
 				return CKR_OK;
 		} else {
 			derive_args.kdf_arguments = &hkdf_args;
@@ -2467,7 +2472,7 @@ static CK_RV op_mkeyderive(CK_SLOT_ID slotid, struct mentry *entry, void *args)
 		case SMW_ATTR_ALGO_HKDF:
 		case SMW_ATTR_ALGO_TLS_1_2:
 			DBG_TRACE("ECDH Derive Key for TLS detected");
-			derive_params->ctx->skipped = true;
+			ctx->skipped = true;
 			return CKR_OK;
 
 		default:
@@ -2510,6 +2515,7 @@ static CK_RV op_mkeyderive(CK_SLOT_ID slotid, struct mentry *entry, void *args)
 	case CKM_HKDF_DERIVE:
 		if (derive_params->ctx && tls13_args.psk)
 			free(tls13_args.psk);
+
 		break;
 
 	case CKM_TLS12_KEY_AND_MAC_DERIVE:

commit eb6e845493d2e315347a75e8079d62cf21f0ad09

SSMW-1091 pkcs11: Remove duplicate cancel operation
TLS 1.2 operation context cancelled only once

Fixes: 69f7dfecfe9d ("SSMW-1059 pkcs11: TLS: Free the ECDH peer public buffer")

Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

pkcs11/src/objects/key.c
@@ -19,7 +19,6 @@
 #include "libobj_types.h"
 
 #include "util.h"
-#include "compiler.h"
 
 #include "trace.h"
 
@@ -2462,6 +2461,9 @@ end:
 	} else {
 		switch (mech->mechanism) {
 		case CKM_HKDF_DERIVE:
+			if (!ctx)
+				break;
+
 			if (!derive_params.hkdf_params.info ||
 			    derive_params.hkdf_params.info_len !=
 				    TLS13_LABEL_OFFSET + sizeof(finishedlabel))
@@ -2473,14 +2475,16 @@ end:
 				    sizeof(finishedlabel)))
 				break;
 
-			__fallthrough;
+			if (ctx->context)
+				(void)libdev_cancel_operation(&ctx->context);
+
+			(void)libdev_remove_opctx(device, CKF_DERIVE);
+			destroy_context(ctx);
+			break;
 
 		case CKM_TLS12_KEY_AND_MAC_DERIVE:
 			if (!ctx)
 				break;
-
-			if (ctx->context)
-				(void)libdev_cancel_operation(&ctx->context);
 
 			(void)libdev_remove_opctx(device, CKF_DERIVE);
 			destroy_context(ctx);
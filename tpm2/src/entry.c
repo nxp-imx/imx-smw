caf18329b4b6338a0ff943671e58f65e2f1a4605
Florian Quinart <florian.quinart@nxp.com>
2026-01-14 16:57:55 +0100

    SSMW-1175 tpm2: Add TPM2_CC_ContextSave support
    Enable context save functionality in the TCTI command dispatcher.
    This allows TPM2 sessions and transient objects to be saved to
    persistent storage through the standard TPM2 context management interface.
    
    Signed-off-by: Florian Quinart <florian.quinart@nxp.com>

--- tpm2/src/entry.c
+++ tpm2/src/entry.c
@@ -101,6 +101,10 @@ static TSS2_RC tcti_smw_transmit(TSS2_TCTI_CONTEXT *tcti_ctx, size_t size,
 		rc = handle_startauthsession(tcti_smwtpm, header.tag, cmd,
 					     header.size);
 		break;
+	case TPM2_CC_ContextSave:
+		rc = handle_contextsave(tcti_smwtpm, header.tag, cmd,
+					header.size);
+		break;
 	default:
 		/* Unsupported command */
 		DBG_TRACE("Unsupported TPM command: 0x%x", header.code);

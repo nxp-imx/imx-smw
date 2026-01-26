5863e4e56b665ff666cbcc67d9b813bef7cb0061
Florian Quinart <florian.quinart@nxp.com>
2026-01-26 18:44:34 +0100

    SSMW-1184 tpm2: Add TPM2_CC_GetCapability support
    Enable capability reporting functionality in the TCTI command dispatcher.
    This allows TPM2 applications to query supported algorithms, commands,
    and properties through the standard TPM2 GetCapability interface.
    
    Signed-off-by: Florian Quinart <florian.quinart@nxp.com>

--- tpm2/src/entry.c
+++ tpm2/src/entry.c
@@ -112,6 +112,10 @@ static TSS2_RC tcti_smw_transmit(TSS2_TCTI_CONTEXT *tcti_ctx, size_t size,
 		rc = handle_flushcontext(tcti_smwtpm, header.tag, cmd,
 					 header.size);
 		break;
+	case TPM2_CC_GetCapability:
+		rc = handle_getcapability(tcti_smwtpm, header.tag, cmd,
+					  header.size);
+		break;
 	default:
 		/* Unsupported command */
 		DBG_TRACE("Unsupported TPM command: 0x%x", header.code);

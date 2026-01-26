0f62d9470af662628bdd26f2e2bf2c51e0401c34
Florian Quinart <florian.quinart@nxp.com>
2026-01-26 17:40:40 +0100

    SSMW-1184 tpm2: Add TPM2_CC_ContextLoad support
    Integrate ContextLoad command into the TCTI command dispatcher.
    This enables the TCTI layer to recognize and route context loading
    commands to the appropriate handler.
    
    Signed-off-by: Florian Quinart <florian.quinart@nxp.com>

--- tpm2/src/entry.c
+++ tpm2/src/entry.c
@@ -120,6 +120,10 @@ static TSS2_RC tcti_smw_transmit(TSS2_TCTI_CONTEXT *tcti_ctx, size_t size,
 		rc = handle_createprimary(tcti_smwtpm, header.tag, cmd,
 					  header.size);
 		break;
+	case TPM2_CC_ContextLoad:
+		rc = handle_contextload(tcti_smwtpm, header.tag, cmd,
+					header.size);
+		break;
 	default:
 		/* Unsupported command */
 		DBG_TRACE("Unsupported TPM command: 0x%x", header.code);

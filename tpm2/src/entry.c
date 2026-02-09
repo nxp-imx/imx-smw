6d37d07f0c964158a473d06d972321cf453d89fa
Florin Pavelescu <florin.pavelescu@nxp.com>
2026-02-09 19:33:58 +0200

    SSMW-1191 tpm2: Add TPM2_CC_VerifySignature support
    Enable signature verification functionality in the TCTI
    command dispatcher.
    
    Signed-off-by: Florin Pavelescu <florin.pavelescu@nxp.com>

--- tpm2/src/entry.c
+++ tpm2/src/entry.c
@@ -141,6 +141,10 @@ static TSS2_RC tcti_smw_transmit(TSS2_TCTI_CONTEXT *tcti_ctx, size_t size,
 	case TPM2_CC_Sign:
 		rc = handle_sign(tcti_smwtpm, header.tag, cmd, header.size);
 		break;
+	case TPM2_CC_VerifySignature:
+		rc = handle_verifysignature(tcti_smwtpm, header.tag, cmd,
+					    header.size);
+		break;
 	default:
 		/* Unsupported command */
 		DBG_TRACE("Unsupported TPM command: 0x%x", header.code);

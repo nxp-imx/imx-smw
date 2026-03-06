461342343f1e2319d5e4c7283cf8f8345d66f9f4
Olivier Masse <olivier.masse@nxp.com>
2026-03-06 10:40:59 +0100

    SSMW-1274 pkcs11: tests: Add specific data OID test
    Create data with CKA_OBJECT_ID equal to 1.2.0
    Find back the object and check attributes.
    
    Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

--- pkcs11/tests/data_storage.c
+++ pkcs11/tests/data_storage.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2024-2025 NXP
+ * Copyright 2024-2026 NXP
  */
 
 #include <stdlib.h>
@@ -94,14 +94,16 @@ static int data_storage_bad_param(CK_FUNCTION_LIST_PTR pfunc)
 		goto end;
 	}
 
-	TEST_OUT("Destroy %sData\n", token ? "Token " : "");
-	ret = pfunc->C_DestroyObject(sess, hdata);
-	if (CHECK_CK_RV(CKR_OK, "C_DestroyObject"))
-		goto end;
-
 	status = TEST_PASS;
 
 end:
+	if (hdata != CK_INVALID_HANDLE) {
+		TEST_OUT("Destroy %sData\n", token ? "Token " : "");
+		ret = pfunc->C_DestroyObject(sess, hdata);
+		if (CHECK_CK_RV(CKR_OK, "C_DestroyObject"))
+			status = TEST_FAIL;
+	}
+
 	util_close_session(pfunc, &sess);
 
 	SUBTEST_END(status);
@@ -196,14 +198,112 @@ static int data_storage_store(CK_FUNCTION_LIST_PTR pfunc)
 		goto end;
 	}
 
-	TEST_OUT("Destroy %sData\n", token ? "Token " : "");
-	ret = pfunc->C_DestroyObject(sess, hdata);
-	if (CHECK_CK_RV(CKR_OK, "C_DestroyObject"))
+	status = TEST_PASS;
+
+end:
+	if (hdata != CK_INVALID_HANDLE) {
+		TEST_OUT("Destroy %sData\n", token ? "Token " : "");
+		ret = pfunc->C_DestroyObject(sess, hdata);
+		if (CHECK_CK_RV(CKR_OK, "C_DestroyObject"))
+			status = TEST_FAIL;
+	}
+
+	util_close_session(pfunc, &sess);
+
+	SUBTEST_END(status);
+	return status;
+}
+
+static int data_storage_store_specific_oid(CK_FUNCTION_LIST_PTR pfunc)
+{
+	int status = TEST_FAIL;
+
+	CK_RV ret = CKR_OK;
+	CK_SESSION_HANDLE sess = 0;
+	CK_OBJECT_HANDLE hdata = CK_INVALID_HANDLE;
+	CK_OBJECT_CLASS data_class = CKO_DATA;
+	CK_BBOOL token = CK_TRUE;
+	CK_UTF8CHAR label[] = "Data";
+	CK_BYTE data[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
+	CK_BYTE retrieved_data[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	CK_BYTE obj_id[] = { 0x2a, 0x00 };
+	CK_BYTE retrieved_obj_id[] = { 0x00, 0x00 };
+	CK_ATTRIBUTE data_template[] = {
+		{ CKA_CLASS, &data_class, sizeof(data_class) },
+		{ CKA_LABEL, label, sizeof(label) - 1 },
+		{ CKA_VALUE, data, sizeof(data) },
+		{ CKA_TOKEN, &token, sizeof(CK_BBOOL) },
+		{ CKA_OBJECT_ID, &obj_id, sizeof(obj_id) }
+	};
+	CK_ATTRIBUTE find_template[] = {
+		{ CKA_TOKEN, &token, sizeof(CK_BBOOL) },
+		{ CKA_OBJECT_ID, &obj_id, sizeof(obj_id) }
+	};
+	CK_ATTRIBUTE retrieve_template[] = {
+		{ CKA_VALUE, retrieved_data, sizeof(retrieved_data) },
+		{ CKA_OBJECT_ID, retrieved_obj_id, sizeof(retrieved_obj_id) }
+	};
+	CK_ULONG nb_match = 0;
+
+	SUBTEST_START();
+
+	if (util_open_rw_session(pfunc, 0, &sess) == TEST_FAIL)
 		goto end;
+
+	TEST_OUT("Create %sData\n", token ? "Token " : "");
+	ret = pfunc->C_CreateObject(sess, data_template,
+				    ARRAY_SIZE(data_template), &hdata);
+	if (CHECK_CK_RV(CKR_OK, "C_CreateObject"))
+		goto end;
+
+	TEST_OUT("Find Data\n");
+	ret = pfunc->C_FindObjectsInit(sess, find_template,
+				       ARRAY_SIZE(find_template));
+	if (CHECK_CK_RV(CKR_OK, "C_FindObjectsInit"))
+		goto end;
+
+	ret = pfunc->C_FindObjects(sess, &hdata, 1, &nb_match);
+	if (CHECK_CK_RV(CKR_OK, "C_FindObjects"))
+		goto end;
+
+	ret = pfunc->C_FindObjectsFinal(sess);
+	if (CHECK_CK_RV(CKR_OK, "C_FindObjectsFinal"))
+		goto end;
+
+	if (CHECK_EXPECTED(nb_match == 1, "Got %lu but expected one object",
+			   nb_match))
+		goto end;
+
+	TEST_OUT("Retrieve %sData\n", token ? "Token " : "");
+	ret = pfunc->C_GetAttributeValue(sess, hdata, retrieve_template,
+					 ARRAY_SIZE(retrieve_template));
+	if (CHECK_CK_RV(CKR_OK, "C_GetAttributeValue"))
+		goto end;
+
+	if (!util_compare_buffers(data, sizeof(data),
+				  retrieve_template[0].pValue,
+				  retrieve_template[0].ulValueLen)) {
+		TEST_OUT("Retrieved Data is not the same\n");
+		goto end;
+	}
+
+	if (!util_compare_buffers(obj_id, sizeof(obj_id),
+				  retrieve_template[1].pValue,
+				  retrieve_template[1].ulValueLen)) {
+		TEST_OUT("Retrieved Object ID is not the same\n");
+		goto end;
+	}
 
 	status = TEST_PASS;
 
 end:
+	if (hdata != CK_INVALID_HANDLE) {
+		TEST_OUT("Destroy %sData\n", token ? "Token " : "");
+		ret = pfunc->C_DestroyObject(sess, hdata);
+		if (CHECK_CK_RV(CKR_OK, "C_DestroyObject"))
+			status = TEST_FAIL;
+	}
+
 	util_close_session(pfunc, &sess);
 
 	SUBTEST_END(status);
@@ -232,7 +332,10 @@ void tests_pkcs11_data_storage(void *lib_hdl, CK_VOID_PTR pfunc)
 	if (data_storage_bad_param(pfunc) == TEST_FAIL)
 		goto end;
 
-	status = data_storage_store(pfunc);
+	if (data_storage_store(pfunc) == TEST_FAIL)
+		goto end;
+
+	status = data_storage_store_specific_oid(pfunc);
 
 end:
 	ret = ((CK_FUNCTION_LIST_PTR)pfunc)->C_Finalize(NULL_PTR);

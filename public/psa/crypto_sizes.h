nxp-imx/imx-smw

commit 34b0d69f2de953cbcda4799004215850e6ae7348

SSMW-260 public: Add support to hash one go in PSA API
- implement macros related to hash
- update functions descriptions

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

public/psa/crypto_sizes.h
@@ -664,11 +664,11 @@
 #define PSA_EXPORT_PUBLIC_KEY_OUTPUT_SIZE(key_type, key_bits)                  \
 	/* implementation-defined value */
 
+size_t psa_hash_block_length(psa_algorithm_t alg);
+
 /**
  * PSA_HASH_BLOCK_LENGTH() - The input block size of a hash algorithm, in bytes.
  * @alg: A hash algorithm (PSA_ALG_XXX value such that PSA_ALG_IS_HASH(alg) is true).
- *
- * **Warning: Not supported**
  *
  * Hash algorithms process their input data in blocks. Hash operations will retain any partial
  * blocks until they have enough input to fill the block or until the operation is finished.
@@ -680,14 +680,14 @@
  * recognized, return 0. An implementation can return either 0 or the correct size for a hash
  * algorithm that it recognizes, but does not support.
  */
-#define PSA_HASH_BLOCK_LENGTH(alg) /* implementation-defined value */
+#define PSA_HASH_BLOCK_LENGTH(alg) psa_hash_block_length(alg)
+
+size_t psa_hash_length(psa_algorithm_t alg);
 
 /**
  * PSA_HASH_LENGTH() - The size of the output of psa_hash_compute() and psa_hash_finish(), in bytes.
  * @alg: A hash algorithm (PSA_ALG_XXX value such that PSA_ALG_IS_HASH(alg) is true), or an HMAC
  * algorithm (PSA_ALG_HMAC(hash_alg) where hash_alg is a hash algorithm).
- *
- * **Warning: Not supported**
  *
  * This is also the hash length that psa_hash_compare() and psa_hash_verify() expect.
  *
@@ -698,13 +698,11 @@
  * 0. An implementation can return either 0 or the correct size for a hash algorithm that it
  * recognizes, but does not support.
  */
-#define PSA_HASH_LENGTH(alg) /* implementation-defined value */
+#define PSA_HASH_LENGTH(alg) psa_hash_length(alg)
 
 /**
  * DOC: PSA_HASH_MAX_SIZE
  * Maximum size of a hash.
- *
- * **Warning: Not supported**
  *
  * This macro must expand to a compile-time constant integer. It is recommended that this value is
  * the maximum size of a hash supported by the implementation, in bytes. The value must not be
@@ -712,7 +710,7 @@
  *
  * See also PSA_HASH_LENGTH().
  */
-#define PSA_HASH_MAX_SIZE /* implementation-defined value */
+#define PSA_HASH_MAX_SIZE 64
 
 /**
  * DOC: PSA_HASH_SUSPEND_ALGORITHM_FIELD_LENGTH

public/psa/crypto.h
@@ -2432,8 +2432,6 @@ psa_status_t psa_hash_clone(const psa_hash_operation_t *source_operation,
  * @hash: Buffer containing the expected hash value.
  * @hash_length: Size of the @hash buffer in bytes.
  *
- * **Warning: Not supported**
- *
  * Return:
  * * PSA_SUCCESS:
  *	The expected hash is identical to the actual hash of the input.
@@ -2465,8 +2463,6 @@ psa_status_t psa_hash_compare(psa_algorithm_t alg, const uint8_t *input,
  * @hash_size: Size of the @hash buffer in bytes. This must be at least PSA_HASH_LENGTH(alg).
  * @hash_length: On success, the number of bytes that make up the hash value. This is always
  *               PSA_HASH_LENGTH(alg).
- *
- * **Warning: Not supported**
  *
  * **Note**:
  *	To verify the hash of a message against an expected value, use psa_hash_compare() instead.

commit 0be3e932c0a40a8d76c22649dd474405099c611c

SSMW-1231 psa: rework PSA hash block/length macros
According to the PSA specifiction, these should evaluate to compile-time
constant expressions when the argument is also a compile-time constant
expression, and are allowed to evaluate the argument any number of
times.

Rework them by removing the function calls.

Fixes: 34b0d69f2de9 ("SSMW-260 public: Add support to hash one go in PSA API")
Signed-off-by: Ilie Halip <ilie.halip@nxp.com>

public/psa/crypto_sizes.h
@@ -773,7 +773,21 @@ size_t psa_hash_block_length(psa_algorithm_t alg);
  * recognized, return 0. An implementation can return either 0 or the correct size for a hash
  * algorithm that it recognizes, but does not support.
  */
-#define PSA_HASH_BLOCK_LENGTH(alg) psa_hash_block_length(alg)
+#define PSA_HASH_BLOCK_LENGTH(alg)                                             \
+	(alg == PSA_ALG_MD5	     ? 64u :                                   \
+	 alg == PSA_ALG_RIPEMD160    ? 64u :                                   \
+	 alg == PSA_ALG_SHA_1	     ? 64u :                                   \
+	 alg == PSA_ALG_SHA_224	     ? 64u :                                   \
+	 alg == PSA_ALG_SHA_256	     ? 64u :                                   \
+	 alg == PSA_ALG_SHA_384	     ? 128u :                                  \
+	 alg == PSA_ALG_SHA_512	     ? 128u :                                  \
+	 alg == PSA_ALG_SHA3_224     ? 144u :                                  \
+	 alg == PSA_ALG_SHA3_256     ? 136u :                                  \
+	 alg == PSA_ALG_SHA3_384     ? 104u :                                  \
+	 alg == PSA_ALG_SHA3_512     ? 72u :                                   \
+	 alg == PSA_ALG_SM3	     ? 64u :                                   \
+	 alg == PSA_ALG_SHAKE256_512 ? 136u :                                  \
+				       0u)
 
 size_t psa_hash_length(psa_algorithm_t alg);
 
@@ -791,7 +805,20 @@ size_t psa_hash_length(psa_algorithm_t alg);
  * 0. An implementation can return either 0 or the correct size for a hash algorithm that it
  * recognizes, but does not support.
  */
-#define PSA_HASH_LENGTH(alg) psa_hash_length(alg)
+#define PSA_HASH_LENGTH(alg)                                                   \
+	(alg == PSA_ALG_MD5	     ? 16u :                                   \
+	 alg == PSA_ALG_SHA_1	     ? 20u :                                   \
+	 alg == PSA_ALG_SHA_224	     ? 28u :                                   \
+	 alg == PSA_ALG_SHA_256	     ? 32u :                                   \
+	 alg == PSA_ALG_SHA_384	     ? 48u :                                   \
+	 alg == PSA_ALG_SHA_512	     ? 64u :                                   \
+	 alg == PSA_ALG_SHA3_224     ? 28u :                                   \
+	 alg == PSA_ALG_SHA3_256     ? 32u :                                   \
+	 alg == PSA_ALG_SHA3_384     ? 48u :                                   \
+	 alg == PSA_ALG_SHA3_512     ? 64u :                                   \
+	 alg == PSA_ALG_SM3	     ? 32u :                                   \
+	 alg == PSA_ALG_SHAKE256_512 ? 64u :                                   \
+				       0u)
 
 /**
  * DOC: PSA_HASH_MAX_SIZE
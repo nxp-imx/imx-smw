nxp-imx/imx-smw

commit 91beaac08b37aebc9bc7a22619aa49f3254d8c20

SSMW-585 core: psa: Add SHA-3 support
Add SHA3_224, SHA3_256, SHA3_384, SHA3_512 to the list of supported hash

Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

core/psa/crypto.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2022-2023 NXP
+ * Copyright 2022-2024 NXP
  */
 
 #include "smw_keymgr.h"
@@ -85,6 +85,10 @@ static const struct hash_algo_info {
 		       HASH_ALGO(SHA_256, "SHA256", 32, 64),
 		       HASH_ALGO(SHA_384, "SHA384", 48, 128),
 		       HASH_ALGO(SHA_512, "SHA512", 64, 128),
+		       HASH_ALGO(SHA3_224, "SHA3_224", 28, 64),
+		       HASH_ALGO(SHA3_256, "SHA3_256", 32, 64),
+		       HASH_ALGO(SHA3_384, "SHA3_384", 48, 128),
+		       HASH_ALGO(SHA3_512, "SHA3_512", 64, 128),
 		       HASH_ALGO(SM3, "SM3", 32, 64),
 		       HASH_ALGO(NONE, NULL, 0, 0) };
 

commit 4892949d77e343e3070a75f99577b335981680f6

SSMW-1231 core: psa: fix SHA3 block sizes
Fix the block sizes for the SHA3 family of digest functions. They are
specified in FIPS 202 (table 3).

Fixes: 91beaac08b37 ("SSMW-585 core: psa: Add SHA-3 support")
Signed-off-by: Ilie Halip <ilie.halip@nxp.com>

core/psa/crypto.c
@@ -130,10 +130,10 @@ static const struct hash_algo_info {
 		       HASH_ALGO(SHA_256, SHA256, 32, 64),
 		       HASH_ALGO(SHA_384, SHA384, 48, 128),
 		       HASH_ALGO(SHA_512, SHA512, 64, 128),
-		       HASH_ALGO(SHA3_224, SHA3_224, 28, 64),
-		       HASH_ALGO(SHA3_256, SHA3_256, 32, 64),
-		       HASH_ALGO(SHA3_384, SHA3_384, 48, 128),
-		       HASH_ALGO(SHA3_512, SHA3_512, 64, 128),
+		       HASH_ALGO(SHA3_224, SHA3_224, 28, 144),
+		       HASH_ALGO(SHA3_256, SHA3_256, 32, 136),
+		       HASH_ALGO(SHA3_384, SHA3_384, 48, 104),
+		       HASH_ALGO(SHA3_512, SHA3_512, 64, 72),
 		       HASH_ALGO(SM3, SM3, 32, 64),
 		       HASH_ALGO(SHAKE256_512, SHAKE256, 64, 136),
 		       { .psa_alg_id = PSA_ALG_NONE,
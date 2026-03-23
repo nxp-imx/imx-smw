nxp-imx/imx-smw

commit 2dc38632ca40b1953666f795cffff51ab11ef6ff

SSMW-1110 subsystems: ele: Open keystore only if needed
Open keystore only if needed by the operation.

Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

core/subsystems/ele/entry.c
@@ -37,21 +37,24 @@ static int open_session(hsm_hdl_t *session_hdl)
 	return status;
 }
 
-static void close_session(hsm_hdl_t session_hdl)
+static void close_session(struct hdl *hdl)
 {
 	hsm_err_t __maybe_unused err = HSM_NO_ERROR;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	SMW_DBG_PRINTF(DEBUG, "session_hdl: %u\n", session_hdl);
-	err = hsm_close_session(session_hdl);
-	SMW_DBG_PRINTF(DEBUG, "hsm_close_session returned %d\n", err);
+	if (hdl->session) {
+		SMW_DBG_PRINTF(DEBUG, "session_hdl: %u\n", hdl->session);
+		err = hsm_close_session(hdl->session);
+		SMW_DBG_PRINTF(DEBUG, "hsm_close_session returned %d\n", err);
+
+		hdl->session = HSM_HANDLE_NONE;
+	}
 }
 
-static int open_key_store_service(hsm_hdl_t session_hdl,
-				  hsm_hdl_t *key_store_hdl)
+int ele_open_key_store_service(struct hdl *hdl)
 {
-	int status = SMW_STATUS_SUBSYSTEM_NOT_CONFIGURED;
+	int status = SMW_STATUS_OK;
 
 	hsm_err_t err = HSM_NO_ERROR;
 	open_svc_key_store_args_t open_svc_key_store_args = { 0 };
@@ -60,8 +63,16 @@ static int open_key_store_service(hsm_hdl_t session_hdl,
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	if (smw_utils_get_subsystem_info(SMW_SUBSYSTEM_NAME_ELE, &info))
+	if (smw_utils_mutex_lock(hdl->key_store_mutex))
+		return SMW_STATUS_MUTEX_LOCK_FAILURE;
+
+	if (hdl->key_store)
 		goto end;
+
+	if (smw_utils_get_subsystem_info(SMW_SUBSYSTEM_NAME_ELE, &info)) {
+		status = SMW_STATUS_SUBSYSTEM_NOT_CONFIGURED;
+		goto end;
+	}
 
 	open_svc_key_store_args.key_store_identifier = info.storage_id;
 	open_svc_key_store_args.authentication_nonce = info.storage_nonce;
@@ -77,13 +88,13 @@ static int open_key_store_service(hsm_hdl_t session_hdl,
 		       "    key_store_identifier: %x\n"
 		       "    authentication_nonce: %x\n"
 		       "    flags: %x\n",
-		       __func__, __LINE__, session_hdl,
+		       __func__, __LINE__, hdl->session,
 		       open_svc_key_store_args.key_store_identifier,
 		       open_svc_key_store_args.authentication_nonce,
 		       open_svc_key_store_args.flags);
 
-	err = hsm_open_key_store_service(session_hdl, &open_svc_key_store_args,
-					 key_store_hdl);
+	err = hsm_open_key_store_service(hdl->session, &open_svc_key_store_args,
+					 &hdl->key_store);
 
 	SMW_DBG_PRINTF(DEBUG, "hsm_open_key_store_service returned %d\n", err);
 
@@ -101,13 +112,13 @@ static int open_key_store_service(hsm_hdl_t session_hdl,
 		       "    key_store_identifier: %x\n"
 		       "    authentication_nonce: %x\n"
 		       "    flags: %x\n",
-		       __func__, __LINE__, session_hdl,
+		       __func__, __LINE__, hdl->session,
 		       open_svc_key_store_args.key_store_identifier,
 		       open_svc_key_store_args.authentication_nonce,
 		       open_svc_key_store_args.flags);
 
-	err = hsm_open_key_store_service(session_hdl, &open_svc_key_store_args,
-					 key_store_hdl);
+	err = hsm_open_key_store_service(hdl->session, &open_svc_key_store_args,
+					 &hdl->key_store);
 
 	SMW_DBG_PRINTF(DEBUG, "hsm_open_key_store_service returned %d\n", err);
 
@@ -124,35 +135,50 @@ static int open_key_store_service(hsm_hdl_t session_hdl,
 		       "    key_store_identifier: %x\n"
 		       "    authentication_nonce: %x\n"
 		       "    flags: %x\n",
-		       __func__, __LINE__, session_hdl,
+		       __func__, __LINE__, hdl->session,
 		       open_svc_key_store_args.key_store_identifier,
 		       open_svc_key_store_args.authentication_nonce,
 		       open_svc_key_store_args.flags);
 
-	err = hsm_open_key_store_service(session_hdl, &open_svc_key_store_args,
-					 key_store_hdl);
+	err = hsm_open_key_store_service(hdl->session, &open_svc_key_store_args,
+					 &hdl->key_store);
 
 	SMW_DBG_PRINTF(DEBUG, "hsm_open_key_store_service returned %d\n", err);
 
 finish:
 	status = ele_convert_err(err);
 
-	SMW_DBG_PRINTF(DEBUG, "key_store_hdl: %u\n", *key_store_hdl);
+	SMW_DBG_PRINTF(DEBUG, "key_store_hdl: %u\n", *&hdl->key_store);
 
 end:
+	(void)smw_utils_mutex_unlock(hdl->key_store_mutex);
+
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }
 
-static void close_key_store_service(hsm_hdl_t key_store_hdl)
+static void close_key_store_service(struct hdl *hdl)
 {
 	hsm_err_t __maybe_unused err = HSM_NO_ERROR;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	SMW_DBG_PRINTF(DEBUG, "key_store_hdl: %u\n", key_store_hdl);
-	err = hsm_close_key_store_service(key_store_hdl);
-	SMW_DBG_PRINTF(DEBUG, "hsm_close_key_store_service returned %d\n", err);
+	if (smw_utils_mutex_lock(hdl->key_store_mutex))
+		return;
+
+	if (hdl->key_store) {
+		SMW_DBG_PRINTF(DEBUG, "key_store_hdl: %u\n", hdl->key_store);
+		err = hsm_close_key_store_service(hdl->key_store);
+		SMW_DBG_PRINTF(DEBUG, "%s - returned: %d\n",
+			       "hsm_close_key_store_service", err);
+
+		hdl->key_store = HSM_HANDLE_NONE;
+	}
+
+	(void)smw_utils_mutex_unlock(hdl->key_store_mutex);
+
+	// coverity[locked_destroy]
+	(void)smw_utils_mutex_destroy(&hdl->key_store_mutex);
 }
 
 static void reset_handles(void)
@@ -161,13 +187,10 @@ static void reset_handles(void)
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	if (hdl->key_store)
-		close_key_store_service(hdl->key_store);
-	if (hdl->session)
-		close_session(hdl->session);
+	close_key_store_service(hdl);
+	close_session(hdl);
 
-	hdl->session = 0;
-	hdl->key_store = 0;
+	// coverity[missing_unlock]
 }
 
 static int unload(void)
@@ -233,9 +256,10 @@ static int load(void)
 	if (status != SMW_STATUS_OK)
 		goto end;
 
-	status = open_key_store_service(hdl->session, &hdl->key_store);
-	if (status != SMW_STATUS_OK)
+	if (smw_utils_mutex_init(&hdl->key_store_mutex)) {
+		status = SMW_STATUS_MUTEX_INIT_FAILURE;
 		goto end;
+	}
 
 	smw_utils_list_init(&ele_ctx.key_grp_list);
 

core/subsystems/ele/aead.c
@@ -422,6 +422,10 @@ static int aead(struct hdl *hdl, void *args)
 		goto end;
 	}
 
+	status = ele_open_key_store_service(hdl);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
 	SMW_DBG_PRINTF(VERBOSE,
 		       "[%s (%d)] Call hsm_do_auth_enc_new()\n"
 		       "op_auth_enc_new_args_t\n"
@@ -533,5 +537,6 @@ bool ele_aead_handle(struct hdl *hdl, enum operation_id operation_id,
 		return false;
 	}
 
+	// coverity[missing_unlock]
 	return true;
 }

core/subsystems/ele/asymmetric_encryption.c
@@ -339,6 +339,10 @@ static int asymm_encrypt_decrypt(struct subsystem_context *ele_ctx,
 
 	set_io_params(operation_id, args, &op_args);
 
+	status = ele_open_key_store_service(&ele_ctx->hdl);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
 	status =
 		open_asymmetric_enc_service(&ele_ctx->hdl, &asymmetric_enc_hdl);
 	if (status != SMW_STATUS_OK)
@@ -416,6 +420,8 @@ end:
 		status = temp_status;
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+
+	// coverity[missing_unlock]
 	return status;
 }
 

core/subsystems/ele/cipher.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2022-2024 NXP
+ * Copyright 2022-2025 NXP
  */
 
 #include "smw_status.h"
@@ -96,6 +96,10 @@ static int cipher(struct hdl *hdl, void *args)
 		goto end;
 	}
 
+	status = ele_open_key_store_service(hdl);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
 	SMW_DBG_PRINTF(VERBOSE,
 		       "[%s (%d)] Call hsm_cipher_one_go()\n"
 		       "op_cipher_one_go_args_t\n"
@@ -126,6 +130,8 @@ static int cipher(struct hdl *hdl, void *args)
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+
+	// coverity[missing_unlock]
 	return status;
 }
 
@@ -141,5 +147,6 @@ bool ele_cipher_handle(struct hdl *hdl, enum operation_id operation_id,
 		return false;
 	}
 
+	// coverity[missing_unlock]
 	return true;
 }

core/subsystems/ele/common.h
@@ -21,12 +21,14 @@
  * struct hdl - ELE handles
  * @session: Session handle
  * @key_store: Key store service flow handle
+ * @key_store_mutex: Mutex of the key store
  *
  * This structure stores the ELE handles managed by the SMW library.
  */
 struct hdl {
 	hsm_hdl_t session;
 	hsm_hdl_t key_store;
+	void *key_store_mutex;
 };
 
 /**
@@ -576,6 +578,13 @@ void ele_fill_sign_msg_block(void *msg, unsigned char cmd,
  * Length of signed message block
  */
 unsigned int ele_get_sign_msg_block_length(void);
+
+/**
+ * ele_open_key_store_service() - Open key store if not already done
+ * @hdl: Pointer to subsystem context handlers
+ *
+ */
+int ele_open_key_store_service(struct hdl *hdl);
 
 /**
  * derive_oem_mk() - ELE OEM Master key derivation operation.

core/subsystems/ele/keymgr.c
@@ -368,10 +368,16 @@ static int check_export_key_config(struct smw_keymgr_descriptor *key_descriptor)
 
 int open_key_mgmt_service(struct hdl *hdl, hsm_hdl_t *key_management_hdl)
 {
+	int status = SMW_STATUS_OK;
+
 	hsm_err_t err = HSM_NO_ERROR;
 	open_svc_key_management_args_t open_svc_key_management_args = { 0 };
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	status = ele_open_key_store_service(hdl);
+	if (status != SMW_STATUS_OK)
+		return status;
 
 	err = hsm_open_key_management_service(hdl->key_store,
 					      &open_svc_key_management_args,
@@ -381,6 +387,7 @@ int open_key_mgmt_service(struct hdl *hdl, hsm_hdl_t *key_management_hdl)
 	SMW_DBG_PRINTF(DEBUG, "Open key_management_hdl: %u\n",
 		       *key_management_hdl);
 
+	// coverity[missing_unlock]
 	return ele_convert_err(err);
 }
 
@@ -471,6 +478,7 @@ end:
 		status = tmp_status;
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	// coverity[missing_unlock]
 	return status;
 }
 
@@ -997,6 +1005,7 @@ end:
 		SMW_UTILS_FREE(hex_priv_key);
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	// coverity[missing_unlock]
 	return status;
 }
 
@@ -1113,6 +1122,7 @@ static int get_key_lengths(struct hdl *hdl, void *args)
 	}
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	// coverity[missing_unlock]
 	return status;
 }
 
@@ -1154,6 +1164,7 @@ end:
 		status = tmp_status;
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	// coverity[missing_unlock]
 	return status;
 }
 
@@ -1236,6 +1247,7 @@ static bool key_is_present(struct hdl *hdl, void *args, int *status)
 
 	SMW_DBG_PRINTF_COND(VERBOSE, handled, "%s returned %d\n", __func__,
 			    *status);
+	// coverity[missing_unlock]
 	return handled;
 }
 
@@ -1289,6 +1301,7 @@ int ele_get_key_attributes(struct hdl *hdl,
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	// coverity[missing_unlock]
 	return status;
 }
 
@@ -1372,6 +1385,7 @@ end:
 		(void)smw_keymgr_free_keypair_buffer(key_desc);
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	// coverity[missing_unlock]
 	return status;
 }
 

core/subsystems/ele/keymgr_derive_tls12.c
@@ -720,5 +720,6 @@ end:
 	}
 
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	// coverity[missing_unlock]
 	return status;
 }

core/subsystems/ele/keymgr_derive_tls13.c
@@ -432,6 +432,7 @@ end:
 	if (payload)
 		SMW_UTILS_FREE(payload);
 
+	// coverity[missing_unlock]
 	return status;
 }
 
@@ -450,5 +451,6 @@ int derive_tls13(struct hdl *hdl, struct smw_keymgr_derive_key_args *args)
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	// coverity[missing_unlock]
 	return status;
 }

commit 18638b7af3f76b054159d1f40d50d373d64d6eff

SSMW-1242 subsystems: ele: Create key store mutex before openning session
Create the key store mutex before trying to open the session.

Fixes: 2dc38632ca40 ("SSMW-1110 subsystems: ele: Open keystore only if needed")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/ele/entry.c
@@ -252,14 +252,14 @@ static int load(void)
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	status = open_session(&hdl->session);
-	if (status != SMW_STATUS_OK)
-		goto end;
-
 	if (smw_utils_mutex_init(&hdl->key_store_mutex)) {
 		status = SMW_STATUS_MUTEX_INIT_FAILURE;
 		goto end;
 	}
+
+	status = open_session(&hdl->session);
+	if (status != SMW_STATUS_OK)
+		goto end;
 
 	smw_utils_list_init(&ele_ctx.key_grp_list);
 
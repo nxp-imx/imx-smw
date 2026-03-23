nxp-imx/imx-smw

commit 544ce96862012ed1d51020e4d8ef0d80dbb97eed

SSMW-924 subsystems: tee: Add support for signature multi-part operation
Add the signature operation multi-part consisting in hashing the
input message and generating or verifying the digest signature at the
final stage.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/subsystems/tee/tee.c
@@ -382,6 +382,11 @@ __weak void *tee_get_ctx_ops(void)
 	return NULL;
 }
 
+__weak void tee_free_sign_context(struct smw_op_context *ctx)
+{
+	(void)ctx;
+}
+
 int tee_convert_result(TEEC_Result result)
 {
 	int status = SMW_STATUS_SUBSYSTEM_FAILURE;

core/subsystems/tee/CMakeLists.txt
@@ -24,7 +24,7 @@ set_target_properties(tee PROPERTIES C_VISIBILITY_PRESET hidden)
 # If at least one of the operation that supports MULTI_PART is enabled, context
 # features are enabled
 #
-if (ENABLE_CIPHER OR ENABLE_AEAD OR ENABLE_HASH)
+if (ENABLE_CIPHER OR ENABLE_AEAD OR ENABLE_HASH OR ENABLE_SIGN_VERIFY)
     set(ENABLE_CONTEXT ON)
 endif()
 

core/subsystems/tee/lib_ta/core/sign_verify.c
@@ -339,9 +339,13 @@ TEE_Result sign_verify(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS],
 			TEE_InitValueAttribute(&sign_verify_attr[attr_count],
 					       TEE_ATTR_EDDSA_PREHASH, 1, 0);
 			attr_count++;
-		} else if (shared_params->signature_type ==
-				   TEE_SIGNATURE_TYPE_EDDSA_CTX &&
-			   shared_params->ctx_length) {
+		}
+
+		if ((shared_params->signature_type ==
+			     TEE_SIGNATURE_TYPE_EDDSA_CTX ||
+		     shared_params->signature_type ==
+			     TEE_SIGNATURE_TYPE_EDDSA_PH) &&
+		    shared_params->ctx_length) {
 			TEE_InitRefAttribute(&sign_verify_attr[attr_count],
 					     TEE_ATTR_EDDSA_CTX,
 					     shared_params->ctx,

core/subsystems/tee/operation_context.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021, 2023-2024 NXP
+ * Copyright 2021, 2023-2025 NXP
  */
 
 #include <tee_client_api.h>
@@ -11,17 +11,17 @@
 #include "utils.h"
 #include "config.h"
 #include "tee.h"
-#include "operation_context.h"
 
 #include "smw_status.h"
 
 static int get_subsystem_context_handle(struct smw_op_context *ctx,
 					void **handle)
 {
+	enum smw_status_code status = SMW_STATUS_INVALID_PARAM;
 	struct aead_context *aead_ctx = NULL;
 	struct cipher_context *cipher_ctx = NULL;
 	struct hash_context *hash_ctx = NULL;
-	enum smw_status_code status = SMW_STATUS_INVALID_PARAM;
+	struct sign_context *sign_ctx = NULL;
 
 	if (!ctx->subsystem_context)
 		goto end;
@@ -48,6 +48,17 @@ static int get_subsystem_context_handle(struct smw_op_context *ctx,
 		status = SMW_STATUS_OK;
 		break;
 
+	case SMW_CRYPTO_OP_ID_SIGN_MULTI_PART:
+		sign_ctx = ctx->subsystem_context;
+		hash_ctx = sign_ctx->hash_ctx.subsystem_context;
+		if (!hash_ctx)
+			break;
+
+		*handle = hash_ctx->tee_handle;
+
+		status = SMW_STATUS_OK;
+		break;
+
 	default:
 		status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
 		break;
@@ -65,14 +76,24 @@ end:
  * Return:
  * None.
  */
-static void free_context(struct smw_op_context *ctx)
+static void tee_free_context(struct smw_op_context *ctx)
 {
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
-	if (ctx->subsystem_context) {
-		SMW_UTILS_FREE(ctx->subsystem_context);
-		ctx->subsystem_context = NULL;
+	if (!ctx->subsystem_context)
+		return;
+
+	switch (ctx->op_id) {
+	case SMW_CRYPTO_OP_ID_SIGN_MULTI_PART:
+		tee_free_sign_context(ctx);
+		break;
+
+	default:
+		break;
 	}
+
+	SMW_UTILS_FREE(ctx->subsystem_context);
+	ctx->subsystem_context = NULL;
 }
 
 /**
@@ -85,7 +106,7 @@ static void free_context(struct smw_op_context *ctx)
  * SMW_STATUS_SUBSYSTEM_FAILURE       - Subsystem failure
  * SMW_STATUS_OPERATION_NOT_SUPPORTED - Operation not supported by subsystem
  */
-static int cancel_operation(struct smw_op_context *ctx)
+static int tee_cancel_operation(struct smw_op_context *ctx)
 {
 	int status = SMW_STATUS_OK;
 
@@ -134,12 +155,13 @@ end:
  * SMW_STATUS_OK                      - Success
  * SMW_STATUS_INVALID_PARAM           - One of the parameters is invalid
  * SMW_STATUS_ALLOC_FAILURE           - Memory allocation failure
+ * SMw_STATUS_OPERATION_NOT_SUPPORTED - Operation is not supported
  */
 static int allocate_copy_subsystem_context(struct smw_op_context *src_context,
 					   struct smw_op_context *dst_context,
 					   struct shared_context *tee_dst_ctx)
 {
-	int status = SMW_STATUS_INVALID_PARAM;
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
 
 	struct aead_context *src_aead_ctx = NULL;
 	struct aead_context *dst_aead_ctx = NULL;
@@ -164,6 +186,7 @@ static int allocate_copy_subsystem_context(struct smw_op_context *src_context,
 		dst_aead_ctx->tee_handle = tee_dst_ctx->handle;
 		dst_context->subsystem_context = dst_aead_ctx;
 
+		status = SMW_STATUS_OK;
 		break;
 
 	case SMW_CRYPTO_OP_ID_CIPHER_MULTI_PART:
@@ -176,6 +199,7 @@ static int allocate_copy_subsystem_context(struct smw_op_context *src_context,
 		dst_cipher_ctx->tee_handle = tee_dst_ctx->handle;
 		dst_context->subsystem_context = dst_cipher_ctx;
 
+		status = SMW_STATUS_OK;
 		break;
 
 	case SMW_CRYPTO_OP_ID_HASH_MULTI_PART:
@@ -188,6 +212,12 @@ static int allocate_copy_subsystem_context(struct smw_op_context *src_context,
 		dst_hash_ctx->tee_handle = tee_dst_ctx->handle;
 		dst_context->subsystem_context = dst_hash_ctx;
 
+		status = SMW_STATUS_OK;
+		break;
+
+	case SMW_CRYPTO_OP_ID_SIGN_MULTI_PART:
+		status = tee_copy_sign_context(src_context, dst_context,
+					       tee_dst_ctx);
 		break;
 
 	default:
@@ -195,8 +225,6 @@ static int allocate_copy_subsystem_context(struct smw_op_context *src_context,
 	}
 
 	smw_crypto_copy_ctx_members(dst_context, src_context);
-
-	status = SMW_STATUS_OK;
 
 end:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
@@ -214,8 +242,8 @@ end:
  * SMW_STATUS_SUBSYSTEM_FAILURE       - Subsystem failure
  * SMW_STATUS_OPERATION_NOT_SUPPORTED - Operation not supported by subsystem
  */
-static int copy_context(struct smw_op_context *src_ctx,
-			struct smw_op_context *dst_ctx)
+static int tee_copy_context(struct smw_op_context *src_ctx,
+			    struct smw_op_context *dst_ctx)
 {
 	int status = SMW_STATUS_OK;
 
@@ -259,9 +287,11 @@ end:
 }
 
 /* TEE context operations structure */
-static struct smw_crypto_context_ops tee_ctx_ops = { .cancel = cancel_operation,
-						     .copy = copy_context,
-						     .free = free_context };
+static struct smw_crypto_context_ops tee_ctx_ops = {
+	.cancel = tee_cancel_operation,
+	.copy = tee_copy_context,
+	.free = tee_free_context
+};
 
 void *tee_get_ctx_ops(void)
 {

core/subsystems/tee/sign_verify.c
@@ -15,6 +15,7 @@
 #include "keymgr.h"
 #include "sign_verify.h"
 #include "tee.h"
+#include "hash.h"
 
 #define SIGNATURE_TYPE_ID(_type, _id)                                          \
 	{                                                                      \
@@ -102,6 +103,30 @@ static int get_pub_key_hex_len(struct smw_keymgr_descriptor *key_desc,
 						 hex_buffer_len);
 }
 
+static void get_eddsa_context(unsigned char **ctx, unsigned int *ctx_length,
+			      struct smw_crypto_sign_verify_args *args)
+{
+	struct smw_eddsa_params *param = NULL;
+	struct smw_op_context *op_context = NULL;
+	struct sign_context *sign_ctx = NULL;
+
+	if (args->op_step == SMW_OP_STEP_FINAL) {
+		op_context = smw_sign_verify_get_op_context(args);
+		if (op_context && op_context->subsystem_context) {
+			sign_ctx = op_context->subsystem_context;
+
+			*ctx = sign_ctx->eddsa_params.context;
+			*ctx_length = sign_ctx->eddsa_params.context_length;
+		}
+	} else {
+		param = smw_sign_verify_get_eddsa_context(args);
+		if (param) {
+			*ctx = param->context;
+			*ctx_length = param->context_length;
+		}
+	}
+}
+
 /**
  * sign_verify() - Generate or verify a signature.
  * @args: Sign or verify arguments.
@@ -173,10 +198,8 @@ static int sign_verify(struct smw_crypto_sign_verify_args *args,
 			status = SMW_STATUS_INVALID_PARAM;
 			goto exit;
 		}
-
 	} else if (sign_attrs->algo_id == SMW_CONFIG_SIGN_ALGO_ID_EDDSA) {
-		ctx = smw_sign_verify_get_eddsactx_buf(args);
-		ctx_length = smw_sign_verify_get_eddsactx_len(args);
+		get_eddsa_context(&ctx, &ctx_length, args);
 
 		if (ctx && ctx_length) {
 			if (ADD_OVERFLOW(shared_params_size, ctx_length,
@@ -313,8 +336,17 @@ static int sign_verify(struct smw_crypto_sign_verify_args *args,
 	operation.params[1].tmpref.size = shared_params_size;
 	operation.params[2].tmpref.buffer = smw_sign_verify_get_msg_buf(args);
 	operation.params[2].tmpref.size = smw_sign_verify_get_msg_len(args);
-	operation.params[3].tmpref.buffer = smw_sign_verify_get_sign_buf(args);
-	operation.params[3].tmpref.size = smw_sign_verify_get_sign_len(args);
+
+	/*
+	 * In case the signature buffer is NULL, the length must be 0 to
+	 * get the signature length.
+	 */
+	if (smw_sign_verify_get_sign_buf(args)) {
+		operation.params[3].tmpref.buffer =
+			smw_sign_verify_get_sign_buf(args);
+		operation.params[3].tmpref.size =
+			smw_sign_verify_get_sign_len(args);
+	}
 
 	/* Invoke TA */
 	status = execute_tee_cmd(cmd_id, &operation);
@@ -357,6 +389,341 @@ exit:
 	return status;
 }
 
+static int alloc_copy_eddsa_params(struct smw_eddsa_params *dst,
+				   struct smw_eddsa_params *src)
+{
+	int status = SMW_STATUS_OK;
+
+	if (!src || !src->context || !src->context_length)
+		goto end;
+
+	dst->context_length = src->context_length;
+
+	dst->context = SMW_UTILS_MALLOC(dst->context_length);
+	if (!dst->context) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		goto end;
+	}
+
+	SMW_UTILS_MEMCPY(dst->context, src->context, dst->context_length);
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+/**
+ * set_sign_context() - Allocate and initialize signature subsystem specific ctx
+ * @op_context: Pointer to operation context arguments structure
+ * @args: Pointer to sign/verify arguments structure
+ *
+ * This function initializes the members of operation context structure. It also
+ * allocates memory to Hash subsystem specific context and initializes it's
+ * members.
+ *
+ * Return:
+ * SMW_STATUS_OK            - Success
+ * SMW_STATUS_INVALID_PARAM - One of the parameters is invalid
+ * SMW_STATUS_ALLOC_FAILURE - Memory allocation failure
+ */
+static int set_sign_context(struct smw_op_context *op_context,
+			    struct smw_crypto_sign_verify_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_eddsa_params *eddsa_params = NULL;
+	struct sign_context *ctx = NULL;
+
+	if (!op_context)
+		goto end;
+
+	op_context->op_id = SMW_CRYPTO_OP_ID_SIGN_MULTI_PART;
+
+	ctx = SMW_UTILS_CALLOC(1, sizeof(*ctx));
+	if (!ctx) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		SMW_DBG_PRINTF(DEBUG,
+			       "Sign subsystem context allocation failure\n");
+		goto end;
+	}
+
+	eddsa_params = smw_sign_verify_get_eddsa_context(args);
+	status = alloc_copy_eddsa_params(&ctx->eddsa_params, eddsa_params);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	ctx->attributes = args->attributes;
+
+	status = smw_keymgr_copy_key(&ctx->key_descriptor,
+				     &args->key_descriptor);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	op_context->subsystem_context = ctx;
+	op_context->op_state = CTX_OP_STATE_INIT;
+
+	smw_crypto_set_ctx_subsystem_id(op_context, SUBSYSTEM_ID_TEE);
+
+	status = SMW_STATUS_OK;
+
+end:
+	if (status != SMW_STATUS_OK && ctx) {
+		if (ctx->eddsa_params.context_length)
+			SMW_UTILS_FREE(ctx->eddsa_params.context);
+
+		SMW_UTILS_FREE(ctx);
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int signature_init(struct smw_crypto_sign_verify_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_crypto_hash_args hash_args = { 0 };
+	struct smw_hash_init_args init_pub = { 0 };
+	struct smw_sign_verify_attributes *sign_attrs = NULL;
+
+	struct smw_op_context *op_context = NULL;
+	struct sign_context *ctx = NULL;
+	enum tee_key_type key_type_id = TEE_KEY_TYPE_ID_INVALID;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	op_context = smw_sign_verify_get_op_context(args);
+	if (!op_context)
+		goto end;
+
+	sign_attrs = &args->attributes;
+
+	if (sign_attrs->algo_id == SMW_CONFIG_SIGN_ALGO_ID_RSA) {
+		/*
+		 * Signature type is mandatory.
+		 * Salt length optional attribute is only for RSASSA-PSS
+		 * signature type.
+		 */
+		if (sign_attrs->type_id == SMW_CONFIG_SIGN_TYPE_ID_DEFAULT) {
+			SMW_DBG_PRINTF(ERROR, "No signature type set\n");
+			goto end;
+		} else if (sign_attrs->type_id ==
+				   SMW_CONFIG_SIGN_TYPE_ID_PKCS1_1_5 &&
+			   sign_attrs->salt_length) {
+			SMW_DBG_PRINTF(ERROR,
+				       "Salt length not supported for %s\n",
+				       "RSA PKCS1_V1_5");
+			goto end;
+		}
+	} else if (sign_attrs->algo_id == SMW_CONFIG_SIGN_ALGO_ID_EDDSA) {
+		status = tee_convert_key_type(&args->key_descriptor.identifier,
+					      SMW_CONFIG_HASH_ALGO_ID_INVALID,
+					      &key_type_id);
+		if (status != SMW_STATUS_OK)
+			goto end;
+
+		if (key_type_id != TEE_KEY_TYPE_ID_ED25519) {
+			status = SMW_STATUS_KEY_INVALID;
+			goto end;
+		}
+
+		/* Force the hash algorithm to be SHA512 */
+		sign_attrs->hash_id = SMW_CONFIG_HASH_ALGO_ID_SHA512;
+	}
+
+	status = set_sign_context(op_context, args);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	ctx = op_context->subsystem_context;
+	if (!ctx) {
+		status = SMW_STATUS_OPERATION_FAILURE;
+		goto end;
+	}
+
+	hash_args.algo_id = sign_attrs->hash_id;
+	hash_args.op_step = SMW_OP_STEP_INIT;
+	hash_args.init_pub = &init_pub;
+
+	init_pub.input = smw_sign_verify_get_msg_buf(args);
+	init_pub.input_length = smw_sign_verify_get_msg_len(args);
+	init_pub.context = &ctx->hash_ctx;
+
+	if (!tee_hash_handle(OPERATION_ID_HASH_MULTI_PART, &hash_args, &status))
+		status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int signature_update(struct smw_crypto_sign_verify_args *args)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_crypto_hash_args hash_args = { 0 };
+	struct smw_hash_update_args update_pub = { 0 };
+
+	struct smw_op_context *op_context = NULL;
+	struct sign_context *ctx = NULL;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	op_context = smw_sign_verify_get_op_context(args);
+	if (!op_context || !op_context->subsystem_context)
+		goto end;
+
+	ctx = op_context->subsystem_context;
+
+	hash_args.op_step = SMW_OP_STEP_UPDATE;
+	hash_args.update_pub = &update_pub;
+
+	update_pub.input = smw_sign_verify_get_msg_buf(args);
+	update_pub.input_length = smw_sign_verify_get_msg_len(args);
+	update_pub.context = &ctx->hash_ctx;
+
+	if (!tee_hash_handle(OPERATION_ID_HASH_MULTI_PART, &hash_args, &status))
+		status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int signature_final(struct smw_crypto_sign_verify_args *args,
+			   enum operation_id op_id)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct smw_sign_verify_final_args pub_args = { 0 };
+	struct smw_crypto_sign_verify_args tmp_args = { 0 };
+
+	struct smw_crypto_hash_args hash_args = { 0 };
+	struct smw_hash_final_args final_pub = { 0 };
+
+	struct smw_op_context *op_context = NULL;
+	struct sign_context *ctx = NULL;
+
+	unsigned char *digest = NULL;
+	unsigned int digest_length = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	op_context = smw_sign_verify_get_op_context(args);
+	if (!op_context || !op_context->subsystem_context)
+		goto end;
+
+	ctx = op_context->subsystem_context;
+
+	hash_args.op_step = SMW_OP_STEP_FINAL;
+	hash_args.final_pub = &final_pub;
+
+	final_pub.input = smw_sign_verify_get_msg_buf(args);
+	final_pub.input_length = smw_sign_verify_get_msg_len(args);
+	final_pub.context = &ctx->hash_ctx;
+
+	/* First get the digest length */
+	if (!tee_hash_handle(OPERATION_ID_HASH_MULTI_PART, &hash_args, &status))
+		status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	if (status != SMW_STATUS_OK && status != SMW_STATUS_OUTPUT_TOO_SHORT)
+		goto end;
+
+	digest_length = smw_crypto_get_hash_output_length(&hash_args);
+	digest = SMW_UTILS_MALLOC(digest_length);
+	if (!digest) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		goto end;
+	}
+
+	if (smw_sign_verify_get_sign_buf(args)) {
+		final_pub.output = digest;
+		final_pub.output_length = digest_length;
+
+		if (!tee_hash_handle(OPERATION_ID_HASH_MULTI_PART, &hash_args,
+				     &status))
+			status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+		if (status != SMW_STATUS_OK)
+			goto end;
+	}
+
+	/* Do digest buffer signature using temporary operation arguments */
+	tmp_args.key_descriptor = ctx->key_descriptor;
+	tmp_args.attributes = ctx->attributes;
+	tmp_args.attributes.msg_hashed = true;
+	tmp_args.op_step = SMW_OP_STEP_FINAL;
+	tmp_args.final_pub = &pub_args;
+
+	pub_args.context = smw_sign_verify_get_op_context(args);
+	pub_args.message = digest;
+	pub_args.message_length = digest_length;
+	pub_args.signature = smw_sign_verify_get_sign_buf(args);
+	pub_args.signature_length = smw_sign_verify_get_sign_len(args);
+
+	status = sign_verify(&tmp_args, op_id);
+
+	if (op_id == OPERATION_ID_SIGN)
+		smw_sign_verify_set_sign_len(args, pub_args.signature_length);
+
+end:
+	if (digest)
+		SMW_UTILS_FREE(digest);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int sign_multipart(struct smw_crypto_sign_verify_args *args)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	switch (args->op_step) {
+	case SMW_OP_STEP_INIT:
+		status = signature_init(args);
+		break;
+
+	case SMW_OP_STEP_UPDATE:
+		status = signature_update(args);
+		break;
+
+	case SMW_OP_STEP_FINAL:
+		status = signature_final(args, OPERATION_ID_SIGN);
+		break;
+
+	default:
+		break;
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+static int verify_multipart(struct smw_crypto_sign_verify_args *args)
+{
+	int status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+	switch (args->op_step) {
+	case SMW_OP_STEP_INIT:
+		status = signature_init(args);
+		break;
+
+	case SMW_OP_STEP_UPDATE:
+		status = signature_update(args);
+		break;
+
+	case SMW_OP_STEP_FINAL:
+		status = signature_final(args, OPERATION_ID_VERIFY);
+		break;
+
+	default:
+		break;
+	}
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
 bool tee_sign_verify_handle(enum operation_id op_id, void *args, int *status)
 {
 	SMW_DBG_TRACE_FUNCTION_CALL;
@@ -366,9 +733,94 @@ bool tee_sign_verify_handle(enum operation_id op_id, void *args, int *status)
 	case OPERATION_ID_VERIFY:
 		*status = sign_verify(args, op_id);
 		break;
+	case OPERATION_ID_SIGN_MULTI_PART:
+		*status = sign_multipart(args);
+		break;
+	case OPERATION_ID_VERIFY_MULTI_PART:
+		*status = verify_multipart(args);
+		break;
 	default:
 		return false;
 	}
 
 	return true;
+}
+
+void tee_free_sign_context(struct smw_op_context *ctx)
+{
+	struct sign_context *sign_ctx = NULL;
+	struct hash_context *hash_ctx = NULL;
+
+	if (ctx && ctx->subsystem_context) {
+		sign_ctx = ctx->subsystem_context;
+
+		if (sign_ctx->eddsa_params.context)
+			SMW_UTILS_FREE(sign_ctx->eddsa_params.context);
+
+		sign_ctx->eddsa_params.context = NULL;
+
+		hash_ctx = sign_ctx->hash_ctx.subsystem_context;
+		if (hash_ctx)
+			SMW_UTILS_FREE(hash_ctx);
+
+		sign_ctx->hash_ctx.subsystem_context = NULL;
+
+		smw_keymgr_free_key(&sign_ctx->key_descriptor);
+	}
+}
+
+int tee_copy_sign_context(struct smw_op_context *src_context,
+			  struct smw_op_context *dst_context,
+			  struct shared_context *tee_dst_ctx)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+
+	struct sign_context *src_sign_ctx = NULL;
+	struct sign_context *dst_sign_ctx = NULL;
+	struct hash_context *dst_hash_ctx = NULL;
+
+	if (!src_context || !dst_context || !src_context->subsystem_context)
+		goto end;
+
+	src_sign_ctx = src_context->subsystem_context;
+
+	/* Allocate the subsystem context for the destination */
+	dst_sign_ctx = SMW_UTILS_CALLOC(1, sizeof(*dst_sign_ctx));
+	if (!dst_sign_ctx) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		goto end;
+	}
+
+	/* Copy and allocate the eddsa parameters */
+	status = alloc_copy_eddsa_params(&dst_sign_ctx->eddsa_params,
+					 &src_sign_ctx->eddsa_params);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	dst_hash_ctx = SMW_UTILS_MALLOC(sizeof(*dst_hash_ctx));
+	if (!dst_hash_ctx) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		goto end;
+	}
+
+	dst_sign_ctx->attributes = src_sign_ctx->attributes;
+	dst_sign_ctx->hash_ctx = src_sign_ctx->hash_ctx;
+
+	dst_hash_ctx->tee_handle = tee_dst_ctx->handle;
+	dst_sign_ctx->hash_ctx.subsystem_context = dst_hash_ctx;
+
+	status = smw_keymgr_copy_key(&dst_sign_ctx->key_descriptor,
+				     &src_sign_ctx->key_descriptor);
+
+end:
+	if (dst_context)
+		dst_context->subsystem_context = dst_sign_ctx;
+
+	if (status != SMW_STATUS_OK && dst_sign_ctx) {
+		tee_free_sign_context(dst_context);
+		SMW_UTILS_FREE(dst_sign_ctx);
+		dst_context->subsystem_context = NULL;
+	}
+
+	return status;
 }

core/subsystems/tee/tee.h
@@ -8,6 +8,8 @@
 
 #include "keymgr.h"
 #include "utils.h"
+#include "operation_context.h"
+#include "sign_verify.h"
 
 #include "tee_subsystem.h"
 
@@ -39,6 +41,20 @@ struct cipher_context {
  */
 struct hash_context {
 	void *tee_handle;
+};
+
+/**
+ * struct sign_context - Signature context
+ * @hash_ctx: Hash context
+ * @attributes: Signature attributes
+ * @eddsa_params: Edwards signature parameters
+ * @key_descriptor: Signature key descriptor
+ */
+struct sign_context {
+	struct smw_op_context hash_ctx;
+	struct smw_sign_verify_attributes attributes;
+	struct smw_eddsa_params eddsa_params;
+	struct smw_keymgr_descriptor key_descriptor;
 };
 
 /*
@@ -136,6 +152,30 @@ bool tee_hash_handle(enum operation_id operation_id, void *args, int *status);
  */
 bool tee_sign_verify_handle(enum operation_id operation_id, void *args,
 			    int *status);
+
+/**
+ * tee_free_sign_context() - Free the signature context
+ * @ctx: Signature context
+ *
+ * Return:
+ * None.
+ */
+void tee_free_sign_context(struct smw_op_context *ctx);
+
+/**
+ * tee_copy_sign_context() - Copy the signature context
+ * @src_context: Pointer to source operation context arguments structure
+ * @dst_context: Pointer to destination operation context arguments structure
+ * @tee_dst_ctx: Pointer to optee context operation handle structure
+ *
+ * Return:
+ * SMW_STATUS_OK                      - Success
+ * SMW_STATUS_INVALID_PARAM           - Parameter invalid
+ * SMW_STATUS_ALLOC_FAILURE           - Memory allocation failure
+ */
+int tee_copy_sign_context(struct smw_op_context *src_context,
+			  struct smw_op_context *dst_context,
+			  struct shared_context *tee_dst_ctx);
 
 /**
  * tee_mac_handle() - Handle the MAC operation.

commit d62b79f3ca4bea28fc2540d835b80937a4596de2

SSMW-1302 subsystems: tee: Add missing weak stubs for tee_copy_sign_context()
Add missing weak stub for tee_copy_sign_context() function.
Build fails when -DENABLE_SIGN_VERIFY=OFF because the weak stub
was not defined in the tee.c file.
The function is called from operation_context.c but only implemented
in sign_verify.c which is conditionally compiled based on the
ENABLE_SIGN_VERIFY build option.

Fixes: 544ce9686201 ("SSMW-924 subsystems: tee: Add support for signature multi-part operation")

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

core/subsystems/tee/tee.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2025 NXP
+ * Copyright 2020-2026 NXP
  */
 
 #include <tee_client_api.h>
@@ -385,6 +385,17 @@ __weak void *tee_get_ctx_ops(void)
 __weak void tee_free_sign_context(struct smw_op_context *ctx)
 {
 	(void)ctx;
+}
+
+__weak int tee_copy_sign_context(struct smw_op_context *src_context,
+				 struct smw_op_context *dst_context,
+				 struct shared_context *tee_dst_ctx)
+{
+	(void)src_context;
+	(void)dst_context;
+	(void)tee_dst_ctx;
+
+	return SMW_STATUS_OPERATION_NOT_SUPPORTED;
 }
 
 int tee_convert_result(TEEC_Result result)
nxp-imx/imx-smw

commit 00f513597494edabd98f255d0b6066dd5e1f9fe8

SSMW-920 public: Support for SHAKE256 algorithm
Support the SHAKE256 algorithm.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

public/smw/names.h
@@ -204,6 +204,7 @@ typedef enum {
  * * SMW_HASH_ALGO_NAME_SHA3_384: Secure Hash Algorithm 3, 384 bits
  * * SMW_HASH_ALGO_NAME_SHA3_512: Secure Hash Algorithm 3, 512 bits
  * * SMW_HASH_ALGO_NAME_SM3: ShangMi 3
+ * * SMW_HASH_ALGO_NAME_SHAKE_256: Secure Hash Algorithm KECCAK, 256 bits
  * * SMW_HASH_ALGO_NAME_NB: Number of Hash algorithms
  */
 typedef enum {
@@ -219,6 +220,7 @@ typedef enum {
 	SMW_HASH_ALGO_NAME_SHA3_384,
 	SMW_HASH_ALGO_NAME_SHA3_512,
 	SMW_HASH_ALGO_NAME_SM3,
+	SMW_HASH_ALGO_NAME_SHAKE256,
 	SMW_HASH_ALGO_NAME_NB
 } smw_hash_algo_t;
 

commit 2ce293bf6b475d5d586aecacf5df3e70a2fd98a0

SSMW-1260 public: Fix hash algorithms description
Fix typo in SHAKE256 description

Fixes: 00f513597494 ("SSMW-920 public: Support for SHAKE256 algorithm")
Signed-off-by: Tommaso Delato <tommaso.delato@nxp.com>

public/smw/names.h
@@ -212,7 +212,7 @@ typedef enum {
  * * SMW_HASH_ALGO_NAME_SHA3_384: Secure Hash Algorithm 3, 384 bits
  * * SMW_HASH_ALGO_NAME_SHA3_512: Secure Hash Algorithm 3, 512 bits
  * * SMW_HASH_ALGO_NAME_SM3: ShangMi 3
- * * SMW_HASH_ALGO_NAME_SHAKE_256: Secure Hash Algorithm KECCAK, 256 bits
+ * * SMW_HASH_ALGO_NAME_SHAKE256: Secure Hash Algorithm KECCAK, 256 bits
  * * SMW_HASH_ALGO_NAME_NB: Number of Hash algorithms
  */
 typedef enum {
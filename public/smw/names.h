2ce293bf6b475d5d586aecacf5df3e70a2fd98a0
Tommaso Delato <tommaso.delato@nxp.com>
2026-04-01 16:51:53 +0200

    SSMW-1260 public: Fix hash algorithms description
    Fix typo in SHAKE256 description
    
    Fixes: 00f513597494 ("SSMW-920 public: Support for SHAKE256 algorithm")
    Signed-off-by: Tommaso Delato <tommaso.delato@nxp.com>

--- public/smw/names.h
+++ public/smw/names.h
@@ -212,7 +212,7 @@ typedef enum {
  * * SMW_HASH_ALGO_NAME_SHA3_384: Secure Hash Algorithm 3, 384 bits
  * * SMW_HASH_ALGO_NAME_SHA3_512: Secure Hash Algorithm 3, 512 bits
  * * SMW_HASH_ALGO_NAME_SM3: ShangMi 3
- * * SMW_HASH_ALGO_NAME_SHAKE_256: Secure Hash Algorithm KECCAK, 256 bits
+ * * SMW_HASH_ALGO_NAME_SHAKE256: Secure Hash Algorithm KECCAK, 256 bits
  * * SMW_HASH_ALGO_NAME_NB: Number of Hash algorithms
  */
 typedef enum {

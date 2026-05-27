d9beb9cbb570bdc30e8b9557a43810ec5cad6f3f
Florian Quinart <florian.quinart@nxp.com>
2026-05-27 16:09:10 +0200

    SSMW-1317 tpm2: Add TPM2_CC_MakeCredential support
    This enables the TCTI layer to process TPM2_MakeCredential
    commands for credential creation in remote attestation workflows.
    
    Signed-off-by: Florian Quinart <florian.quinart@nxp.com>

--- tpm2/src/entry.c
+++ tpm2/src/entry.c
@@ -186,6 +186,10 @@ static TSS2_RC tcti_smw_transmit(TSS2_TCTI_CONTEXT *tcti_ctx, size_t size,
 	case TPM2_CC_Unseal:
 		rc = handle_unseal(tcti_smwtpm, header.tag, cmd, header.size);
 		break;
+	case TPM2_CC_MakeCredential:
+		rc = handle_makecredential(tcti_smwtpm, header.tag, cmd,
+					   header.size);
+		break;
 	default:
 		/* Unsupported command */
 		DBG_TRACE("Unsupported TPM command: 0x%x", header.code);

fd631091984494a9533e965f4e64d9e7f01b1bee
Olivier Masse <olivier.masse@nxp.com>
2026-06-12 15:47:41 +0200

    SSMW-1384 inc: Add macros
    IS_ENABLED(macro) is true if macro is defined, false otherwise.
    __no_optimization disable compiler optimization in a function.
    
    Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

--- inc/builtin_macros.h
+++ inc/builtin_macros.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2023 NXP
+ * Copyright 2023, 2026 NXP
  */
 
 #ifndef __BUILTIN_MACROS_H__
@@ -11,7 +11,9 @@
 #ifndef BIT
 #define BIT(n) (1UL << (n))
 #endif
-#define BIT_MASK(length)      ((1UL << (length)) - 1)
+#ifndef BIT_MASK
+#define BIT_MASK(length) ((1UL << (length)) - 1)
+#endif
 #define SET_BITS(val, mask)   ((val) |= (mask))
 #define CLEAR_BITS(val, mask) ((val) &= ~(mask))
 
@@ -29,7 +31,9 @@
 
 #define STR(x) #x
 
+#ifndef ARRAY_SIZE
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof((array)[0]))
+#endif
 
 #ifndef MIN
 #define MIN(a, b)                                                              \
@@ -115,5 +119,61 @@
 	})
 
 #define SET_CLEAR_MASK(val, set, clear) (((val) & ~(clear)) | (set))
+
+#ifndef IS_ENABLED
+/**
+ * @brief Check for macro definition in compiler-visible expressions
+ *
+ * This trick was pioneered in Linux as the config_enabled() macro.
+ * The madness has the effect of taking a macro value that may be
+ * defined to "1" (e.g. CONFIG_MYFEATURE), or may not be defined at
+ * all and turning it into a literal expression that can be used at
+ * "runtime".  That is, it works similarly to
+ * "defined(CONFIG_MYFEATURE)" does except that it is an expansion
+ * that can exist in a standard expression and be seen by the compiler
+ * and optimizer.  Thus much ifdef usage can be replaced with cleaner
+ * expressions like:
+ *
+ *     if (IS_ENABLED(CONFIG_MYFEATURE))
+ *             myfeature_enable();
+ *
+ * INTERNAL
+ * First pass just to expand any existing macros, we need the macro
+ * value to be e.g. a literal "1" at expansion time in the next macro,
+ * not "(1)", etc...  Standard recursive expansion does not work.
+ */
+#define IS_ENABLED(CONFIG_macro) Z_IS_ENABLED1(CONFIG_macro)
+
+/* Now stick on a "_XXXX" prefix, it will now be "_XXXX1" if CONFIG_macro
+ * is "1", or just "_XXXX" if it's undefined.
+ *   ENABLED:   Z_IS_ENABLED2(_XXXX1)
+ *   DISABLED   Z_IS_ENABLED2(_XXXX)
+ */
+#define Z_IS_ENABLED1(CONFIG_macro) Z_IS_ENABLED2(_XXXX##CONFIG_macro)
+
+/* Here's the core trick, we map "_XXXX1" to "_YYYY," (i.e. a string
+ * with a trailing comma), so it has the effect of making this a
+ * two-argument tuple to the preprocessor only in the case where the
+ * value is defined to "1"
+ *   ENABLED:    _YYYY,    <--- note comma!
+ *   DISABLED:   _XXXX
+ */
+#define _XXXX1 _YYYY,
+
+/* Then we append an extra argument to fool the gcc preprocessor into
+ * accepting it as a varargs macro.
+ *                         arg1   arg2  arg3
+ *   ENABLED:   Z_IS_ENABLED3(_YYYY,    1,    0)
+ *   DISABLED   Z_IS_ENABLED3(_XXXX 1,  0)
+ */
+#define Z_IS_ENABLED2(one_or_two_args)                                         \
+	Z_IS_ENABLED3(one_or_two_args true, false)
+
+/* And our second argument is thus now cooked to be 1 in the case
+ * where the value is defined to 1, and 0 if not:
+ */
+#define Z_IS_ENABLED3(type, val, ...) val
+
+#endif /* IS_ENABLE */
 
 #endif /* __BUILTIN_MACROS_H__ */
--- inc/compiler.h
+++ inc/compiler.h
@@ -6,11 +6,21 @@
 #ifndef __COMPILER_H__
 #define __COMPILER_H__
 
+#ifndef __weak
 #define __weak	       __attribute__((weak))
+#endif
 #define __export       __attribute__((visibility("default")))
+#ifndef __maybe_unused
 #define __maybe_unused __attribute__((unused))
+#endif
+#ifndef __packed
 #define __packed       __attribute__((packed))
+#endif
+#ifndef __fallthrough
 #define __fallthrough  __attribute__((fallthrough))
+#endif
 #define __format_printf(_m, _n) __attribute__((__format__(__printf__, _m, _n)));
+
+#define __no_optimization __attribute__((optimize("O0")))
 
 #endif /* __COMPILER_H__ */

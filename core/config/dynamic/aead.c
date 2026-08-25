nxp-imx/imx-smw

commit 662f7ff743b6096741a44959116ba3d8647916b6

SSMW-1335 core: config: Add `USE_ELA` config tag support
Add support for `USE_ELA` tag in configuration file to
enable runtime selection of ELA for AEAD, CIPHER, HASH,
and MAC operations.

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

core/config/dynamic/aead.c
@@ -131,6 +131,12 @@ static int aead_common_read_params(char **start, char *end, void **params)
 
 	while ((cur < end) && (open_square_bracket != *cur)) {
 		status = read_params_string(&cur, end, buffer);
+		if (check_ela_tag(&cur, end, buffer, &status)) {
+			if (status == SMW_STATUS_OK)
+				p->use_ela = true;
+
+			continue;
+		}
 
 		if (status != SMW_STATUS_OK)
 			goto end;
@@ -206,6 +212,9 @@ static void aead_common_merge_params(void *caps, void *params)
 
 	aead_caps->mode_bitmap |= aead_params->mode_bitmap;
 	aead_caps->op_bitmap |= aead_params->op_bitmap;
+
+	if (aead_params->use_ela)
+		aead_caps->use_ela = true;
 
 	merge_key_params(&aead_caps->key, &aead_params->key);
 }
@@ -384,4 +393,23 @@ __export enum smw_status_code smw_config_check_aead(smw_subsystem_t subsystem,
 		return SMW_STATUS_OPERATION_NOT_CONFIGURED;
 
 	return SMW_STATUS_OK;
+}
+
+bool aead_is_ela_enabled(enum subsystem_id subsystem_id)
+{
+	bool enabled = false;
+	int status = SMW_STATUS_OK;
+	struct aead_params params = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	status = get_operation_params_lock(OPERATION_ID_AEAD, subsystem_id,
+					   &params);
+	if (status == SMW_STATUS_OK)
+		enabled = params.use_ela;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %s\n", __func__,
+		       enabled ? "true" : "false");
+
+	return enabled;
 }

core/config/dynamic/cipher.c
@@ -99,6 +99,13 @@ static int cipher_common_read_params(char **start, char *end, void **params)
 
 	while ((cur < end) && (open_square_bracket != *cur)) {
 		status = read_params_string(&cur, end, buffer);
+		if (check_ela_tag(&cur, end, buffer, &status)) {
+			if (status == SMW_STATUS_OK)
+				p->use_ela = true;
+
+			continue;
+		}
+
 		if (status != SMW_STATUS_OK)
 			goto end;
 		SMW_DBG_PRINTF(INFO, "Parameter: %s\n", buffer);
@@ -169,6 +176,11 @@ static void cipher_common_merge_params(void *caps, void *params)
 
 	cipher_caps->mode_bitmap |= cipher_params->mode_bitmap;
 	cipher_caps->op_bitmap |= cipher_params->op_bitmap;
+
+	/* Merge ELA flag */
+	if (cipher_params->use_ela)
+		cipher_caps->use_ela = true;
+
 	merge_key_params(&cipher_caps->key, &cipher_params->key);
 }
 
@@ -353,4 +365,23 @@ smw_config_check_cipher(smw_subsystem_t subsystem, struct smw_cipher_info *info)
 		return SMW_STATUS_OPERATION_NOT_CONFIGURED;
 
 	return SMW_STATUS_OK;
+}
+
+bool cipher_is_ela_enabled(enum subsystem_id subsystem_id)
+{
+	bool enabled = false;
+	int status = SMW_STATUS_OK;
+	struct cipher_params params = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	status = get_operation_params_lock(OPERATION_ID_CIPHER, subsystem_id,
+					   &params);
+	if (status == SMW_STATUS_OK)
+		enabled = params.use_ela;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %s\n", __func__,
+		       enabled ? "true" : "false");
+
+	return enabled;
 }

core/config/dynamic/hash.c
@@ -23,6 +23,7 @@ static int hash_common_read_params(char **start, char *end, void **params)
 {
 	int status = SMW_STATUS_OK;
 	char *cur = *start;
+	bool use_ela = false;
 
 	char buffer[SMW_CONFIG_MAX_PARAMS_STRING_LENGTH + 1] = { 0 };
 	size_t length = 0;
@@ -35,6 +36,13 @@ static int hash_common_read_params(char **start, char *end, void **params)
 
 	while ((cur < end) && (open_square_bracket != *cur)) {
 		status = read_params_string(&cur, end, buffer);
+		if (check_ela_tag(&cur, end, buffer, &status)) {
+			if (status == SMW_STATUS_OK)
+				use_ela = true;
+
+			continue;
+		}
+
 		if (status != SMW_STATUS_OK)
 			goto end;
 		SMW_DBG_PRINTF(INFO, "Parameter: %s\n", buffer);
@@ -64,6 +72,8 @@ static int hash_common_read_params(char **start, char *end, void **params)
 
 	p->algo_bitmap = algo_bitmap;
 
+	p->use_ela = use_ela;
+
 	*params = p;
 
 	*start = cur;
@@ -91,6 +101,9 @@ static void hash_common_merge_params(void *caps, void *params)
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
 	hash_caps->algo_bitmap |= hash_params->algo_bitmap;
+
+	if (hash_params->use_ela)
+		hash_caps->use_ela = true;
 }
 
 static void hash_merge_params(void *caps, void *params)
@@ -176,4 +189,23 @@ __export enum smw_status_code smw_config_check_digest(smw_subsystem_t subsystem,
 		return SMW_STATUS_OPERATION_NOT_CONFIGURED;
 
 	return SMW_STATUS_OK;
+}
+
+bool hash_is_ela_enabled(enum subsystem_id subsystem_id)
+{
+	bool enabled = false;
+	int status = SMW_STATUS_OK;
+	struct hash_params params = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	status = get_operation_params_lock(OPERATION_ID_HASH, subsystem_id,
+					   &params);
+	if (status == SMW_STATUS_OK)
+		enabled = params.use_ela;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %s\n", __func__,
+		       enabled ? "true" : "false");
+
+	return enabled;
 }

core/config/dynamic/mac.c
@@ -46,6 +46,13 @@ static int mac_read_params(char **start, char *end, void **params)
 
 	while ((cur < end) && (open_square_bracket != *cur)) {
 		status = read_params_string(&cur, end, buffer);
+		if (check_ela_tag(&cur, end, buffer, &status)) {
+			if (status == SMW_STATUS_OK)
+				p->use_ela = true;
+
+			continue;
+		}
+
 		if (status != SMW_STATUS_OK)
 			goto end;
 
@@ -110,6 +117,9 @@ static void mac_merge_params(void *caps, void *params)
 
 	mac_caps->algo_bitmap |= mac_params->algo_bitmap;
 	mac_caps->hash_bitmap |= mac_params->hash_bitmap;
+
+	if (mac_params->use_ela)
+		mac_caps->use_ela = true;
 
 	merge_key_params(&mac_caps->key, &mac_params->key);
 }
@@ -233,4 +243,23 @@ __export enum smw_status_code smw_config_check_mac(smw_subsystem_t subsystem,
 	}
 
 	return SMW_STATUS_OK;
+}
+
+bool mac_is_ela_enabled(enum subsystem_id subsystem_id)
+{
+	bool enabled = false;
+	int status = SMW_STATUS_OK;
+	struct mac_params params = { 0 };
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	status = get_operation_params_lock(OPERATION_ID_MAC, subsystem_id,
+					   &params);
+	if (status == SMW_STATUS_OK)
+		enabled = params.use_ela;
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %s\n", __func__,
+		       enabled ? "true" : "false");
+
+	return enabled;
 }

core/config/dynamic/parser.c
@@ -497,14 +497,15 @@ static bool read_operation(char **start, char *end,
 
 	status = store_operation_params(operation_id, params, func,
 					subsystem_id);
-	if (status != SMW_STATUS_OK) {
-		SMW_UTILS_FREE(params);
+	if (status != SMW_STATUS_OK)
 		goto end;
-	}
 
 	*start = cur;
 
 end:
+	if (status != SMW_STATUS_OK && params)
+		SMW_UTILS_FREE(params);
+
 	if (return_status)
 		*return_status = status;
 
@@ -611,6 +612,26 @@ end:
 
 __weak bool is_psa_default_alt_enabled(void)
 {
+	return false;
+}
+
+__weak bool check_ela_tag(char **start, char *end, const char *buffer,
+			  int *status)
+{
+	char *cur = *start;
+
+	if (!SMW_UTILS_STRNCMP(buffer, use_ela_tag,
+			       SMW_UTILS_STRLEN(use_ela_tag))) {
+		SMW_DBG_PRINTF(DEBUG,
+			       "USE_ELA tag found but ELA is disabled\n");
+		cur++;
+		skip_insignificant_chars(&cur, end);
+		*start = cur;
+
+		*status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+		return true;
+	}
+
 	return false;
 }
 

core/config/common.h
@@ -63,12 +63,14 @@ struct key_operation_params {
 
 struct hash_params {
 	unsigned long algo_bitmap;
+	bool use_ela;
 };
 
 struct mac_params {
 	unsigned long algo_bitmap;
 	unsigned long hash_bitmap;
 	struct op_key key;
+	bool use_ela;
 };
 
 struct sign_verify_params {
@@ -85,6 +87,7 @@ struct cipher_params {
 	unsigned long mode_bitmap;
 	unsigned long op_bitmap;
 	struct op_key key;
+	bool use_ela;
 };
 
 struct storage_store_params {
@@ -98,6 +101,7 @@ struct aead_params {
 	unsigned long mode_bitmap;
 	unsigned long op_bitmap;
 	struct op_key key;
+	bool use_ela;
 };
 
 struct asymmetric_encryption_params {
@@ -575,5 +579,23 @@ void unload_subsystems(void);
  * * false: - ENABLE_PSA_DEFAULT_ALT is not enabled.
  */
 bool is_psa_default_alt_enabled(void);
+
+/**
+ * check_ela_tag() - Check and validate USE_ELA flag tag
+ * @start: Pointer to current position in config buffer
+ * @end: Pointer to the last char of the buffer being parsed
+ * @buffer: Buffer containing the parsed tag string
+ * @status: Pointer to parsing status
+ *          (input from read_params_string(), output after validation)
+ *
+ * Validates USE_ELA flag format. Must be a standalone flag without
+ * value, terminated by semicolon. Updates @status to SMW_STATUS_OK on
+ * success or SMW_STATUS_SYNTAX_ERROR on format error.
+ *
+ * Return:
+ * * true  - USE_ELA tag found
+ * * false - Not a USE_ELA tag or invalid format
+ */
+bool check_ela_tag(char **start, char *end, const char *buffer, int *status);
 
 #endif /* __COMMON_H__ */

core/config/dynamic/CMakeLists.txt
@@ -21,6 +21,7 @@ add_private_sources_ifdef(config ENABLE_DEVMGR_MODULE dynamic/device.c)
 add_private_sources_ifdef(config ENABLE_STORAGE_MODULE dynamic/storage.c)
 add_private_sources_ifdef(config ENABLE_AEAD dynamic/aead.c)
 add_private_sources_ifdef(config ENABLE_ASYMMETRIC_ENCRYPTION dynamic/asymmetric_encryption.c)
+add_private_sources_ifdef(config ENABLE_ELA dynamic/parse_enable_ela.c)
 
 if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND VERBOSE AND NOT VERBOSE EQUAL 0)
         add_private_sources(config database_debug.c)

core/config/dynamic/parse_enable_ela.c
@@ -1,0 +1,47 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2026 NXP
+ */
+
+#include <stdbool.h>
+
+#include "smw_status.h"
+#include "debug.h"
+#include "utils.h"
+#include "common.h"
+#include "tag.h"
+
+bool check_ela_tag(char **start, char *end, const char *buffer, int *status)
+{
+	char *cur = *start;
+
+	/* Check if buffer contains USE_ELA tag */
+	if (SMW_UTILS_STRNCMP(buffer, use_ela_tag,
+			      SMW_UTILS_STRLEN(use_ela_tag)))
+		return false;
+
+	/* USE_ELA tag found - validate it's format */
+	if (*status == SMW_STATUS_OK) {
+		/* read_params_string() found PARAM=VALUE */
+		SMW_DBG_PRINTF(ERROR, "USE_ELA shouldn't have a value\n");
+		*status = SMW_STATUS_SYNTAX_ERROR;
+		return false;
+	}
+
+	if (*status != SMW_STATUS_SYNTAX_ERROR)
+		return false;
+
+	if (*cur != semicolon)
+		return false;
+
+	/* Valid USE_ELA tag found, skip the semicolon */
+	cur++;
+	skip_insignificant_chars(&cur, end);
+
+	SMW_DBG_PRINTF(INFO, "ELA tag enabled\n");
+
+	*start = cur;
+	*status = SMW_STATUS_OK;
+
+	return true;
+}

commit ebc081aca4316af964c40f70367bbf591ea0fb1a

SSMW-1420 core: config: Fix USE_ELA tag handling when ENABLE_ELA is disabled
- When USE_ELA is present in the config file and ENABLE_ELA=OFF, the weak
  check_ela_tag() returns true with SMW_STATUS_OPERATION_NOT_SUPPORTED.
  Config parsers for AEAD, cipher, hash and MAC did not handle this status,
  causing the parse loop to exit with an error and resulting in a double
  free crash. This issue is only triggered when USE_ELA is the sole
  parameter defined for a security operation in the config file; when
  additional parameters are present, the subsequent parsing resets the
  status and the crash is not observed.
  Reset status to SMW_STATUS_OK in this case so that parsing continues
  without error.

- Fix also a double free in read_operation() in parser.c: func->read
  already frees params on failure, so move the params free to the
  store_operation_params() error path only.

Fixes: 662f7ff743b6 ("SSMW-1335 core: config: Add `USE_ELA` config tag support")

Signed-off-by: Rakshata Nayak <rakshata.nayak@nxp.com>

core/config/dynamic/aead.c
@@ -132,8 +132,16 @@ static int aead_common_read_params(char **start, char *end, void **params)
 	while ((cur < end) && (open_square_bracket != *cur)) {
 		status = read_params_string(&cur, end, buffer);
 		if (check_ela_tag(&cur, end, buffer, &status)) {
+			/*
+			 * If ELA is enabled, set use_ela flag.
+			 * If ELA is disabled (weak check_ela_tag returns
+			 * SMW_STATUS_OPERATION_NOT_SUPPORTED), reset status to OK so that
+			 * parsing continues without error.
+			 */
 			if (status == SMW_STATUS_OK)
 				p->use_ela = true;
+			else if (status == SMW_STATUS_OPERATION_NOT_SUPPORTED)
+				status = SMW_STATUS_OK;
 
 			continue;
 		}

core/config/dynamic/cipher.c
@@ -100,8 +100,16 @@ static int cipher_common_read_params(char **start, char *end, void **params)
 	while ((cur < end) && (open_square_bracket != *cur)) {
 		status = read_params_string(&cur, end, buffer);
 		if (check_ela_tag(&cur, end, buffer, &status)) {
+			/*
+			 * If ELA is enabled, set use_ela flag.
+			 * If ELA is disabled (weak check_ela_tag returns
+			 * SMW_STATUS_OPERATION_NOT_SUPPORTED), reset status to OK so that
+			 * parsing continues without error.
+			 */
 			if (status == SMW_STATUS_OK)
 				p->use_ela = true;
+			else if (status == SMW_STATUS_OPERATION_NOT_SUPPORTED)
+				status = SMW_STATUS_OK;
 
 			continue;
 		}

core/config/dynamic/hash.c
@@ -37,8 +37,16 @@ static int hash_common_read_params(char **start, char *end, void **params)
 	while ((cur < end) && (open_square_bracket != *cur)) {
 		status = read_params_string(&cur, end, buffer);
 		if (check_ela_tag(&cur, end, buffer, &status)) {
+			/*
+			 * If ELA is enabled, set use_ela flag.
+			 * If ELA is disabled (weak check_ela_tag returns
+			 * SMW_STATUS_OPERATION_NOT_SUPPORTED), reset status to OK so that
+			 * parsing continues without error.
+			 */
 			if (status == SMW_STATUS_OK)
 				use_ela = true;
+			else if (status == SMW_STATUS_OPERATION_NOT_SUPPORTED)
+				status = SMW_STATUS_OK;
 
 			continue;
 		}

core/config/dynamic/mac.c
@@ -47,8 +47,16 @@ static int mac_read_params(char **start, char *end, void **params)
 	while ((cur < end) && (open_square_bracket != *cur)) {
 		status = read_params_string(&cur, end, buffer);
 		if (check_ela_tag(&cur, end, buffer, &status)) {
+			/*
+			 * If ELA is enabled, set use_ela flag.
+			 * If ELA is disabled (weak check_ela_tag returns
+			 * SMW_STATUS_OPERATION_NOT_SUPPORTED), reset status to OK so that
+			 * parsing continues without error.
+			 */
 			if (status == SMW_STATUS_OK)
 				p->use_ela = true;
+			else if (status == SMW_STATUS_OPERATION_NOT_SUPPORTED)
+				status = SMW_STATUS_OK;
 
 			continue;
 		}

core/config/dynamic/parser.c
@@ -497,14 +497,14 @@ static bool read_operation(char **start, char *end,
 
 	status = store_operation_params(operation_id, params, func,
 					subsystem_id);
-	if (status != SMW_STATUS_OK)
+	if (status != SMW_STATUS_OK) {
+		SMW_UTILS_FREE(params);
 		goto end;
+	}
 
 	*start = cur;
 
 end:
-	if (status != SMW_STATUS_OK && params)
-		SMW_UTILS_FREE(params);
 
 	if (return_status)
 		*return_status = status;
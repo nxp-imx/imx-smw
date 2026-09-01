nxp-imx/imx-smw

commit c02f482e69a696cdc571d97b9101445d156dea84

SSMW-181 public: Add support for SM2 signature
- Add definitions related to SM2
- Rename unused SMW_KEY_TYPE_NAME_DSA_SM2_FP as SMW_KEY_TYPE_NAME_SM2

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

public/smw/crypto/sign.h
@@ -23,6 +23,16 @@ struct smw_eddsa_params {
 };
 
 /**
+ * struct smw_sm2_params - SM2 signature parameters
+ * @identifier: [in] Distinguishable identifier.
+ * @identifier_length: [in] Length of the identifier.
+ */
+struct smw_sm2_params {
+	unsigned char *identifier;
+	unsigned short identifier_length;
+};
+
+/**
  * struct smw_sign_verify_args - Sign or verify arguments
  * @version: [in] Version of this structure.
  * @subsystem_name: [in] Secure Subsystem name. See &typedef smw_subsystem_t.
@@ -38,7 +48,10 @@ struct smw_eddsa_params {
  * @signature_length:
  *  - [in] Length in bytes of the signature buffer.
  *  - [out] Length in bytes of the signature buffer generated.
- * @eddsa_params: [in] (**optional**) Pointer to eddsa parameters.
+ * @eddsa_params: [in] (**optional**) Pointer to edwards signature parameters.
+ *                See &typedef smw_eddsa_params.
+ * @sm2_params: [in] (**optional**) Pointer to SM2 signature parameters.
+ *              See &typedef smw_sm2_params.
  *
  * The @subsystem_name designates the Secure Subsystem to be used.
  * If this field is :ref:`SMW_SUBSYSTEM_NAME_NONE <smw_subsystem_t>`,
@@ -56,6 +69,7 @@ struct smw_sign_verify_args {
 	unsigned int signature_length;
 	union {
 		struct smw_eddsa_params *eddsa_params;
+		struct smw_sm2_params *sm2_params;
 	};
 };
 
@@ -71,6 +85,7 @@ struct smw_sign_verify_args {
  * @message: [in] Pointer to the message to sign or verify.
  * @message_length: [in] Length in bytes of the message.
  * @eddsa_params: [in] (**optional**) Pointer to edwards signature parameters.
+ * @sm2_params: [in] Pointer to SM2 signature parameters.
  * @context: [in/out] Pointer the multipart operation context.
  *
  * Field @context:\
@@ -93,6 +108,7 @@ struct smw_sign_verify_init_args {
 	unsigned int message_length;
 	union {
 		struct smw_eddsa_params *eddsa_params;
+		struct smw_sm2_params *sm2_params;
 	};
 	struct smw_op_context *context;
 };

public/smw/attr.h
@@ -242,6 +242,7 @@ typedef uint32_t smw_attr_storage_id_t;
  * - SMW_ATTR_CURVE_BRAINPOOL_T1: Brainpool T1 curve.
  * - SMW_ATTR_CURVE_ED25519: Twisted Edwards25519.
  * - SMW_ATTR_CURVE_ED448: Twisted Edwards448.
+ * - SMW_ATTR_CURVE_SM2: ShāngMì 2.
  * - SMW_ATTR_CURVE_ANY: Any curve.
  */
 #define SMW_ATTR_CURVE_NONE	    0x00
@@ -250,6 +251,7 @@ typedef uint32_t smw_attr_storage_id_t;
 #define SMW_ATTR_CURVE_BRAINPOOL_T1 0x03
 #define SMW_ATTR_CURVE_ED25519	    0x04
 #define SMW_ATTR_CURVE_ED448	    0x05
+#define SMW_ATTR_CURVE_SM2	    0x06
 #define SMW_ATTR_CURVE_ANY	    0xFF
 
 /*
@@ -890,6 +892,22 @@ typedef uint32_t smw_attr_storage_id_t;
 #define SMW_ATTR_ALGO_ASYMMETRIC_SIGNATURE_TLS_1_2_SERVER(hash)                \
 	(SMW_ATTR_NAME(CLASS, ASYMMETRIC_SIGNATURE) |                          \
 	 SMW_ATTR_NAME(ALGO, TLS_1_2) | SMW_ATTR_NAME(MODE, SERVER) |          \
+	 SMW_ATTR_VALUE(HASH, hash))
+
+/**
+ * SMW_ATTR_ALGO_ASYMMETRIC_SIGNATURE_SM2() - Build an asymmetric signature
+ * SM2 algorithm.
+ * @hash: A valid hash algorithm. See &typedef smw_attr_algo_t.
+ *
+ * This macro builds an asymmetric signature SM2 algorithm given
+ * the @hash algorithm.
+ *
+ * Return:
+ * A valid asymmetric signature SM2 algorithm.
+ */
+#define SMW_ATTR_ALGO_ASYMMETRIC_SIGNATURE_SM2(hash)                           \
+	(SMW_ATTR_NAME(CLASS, ASYMMETRIC_SIGNATURE) |                          \
+	 SMW_ATTR_NAME(ALGO, SM2) | SMW_ATTR_NAME(CURVE, SM2) |                \
 	 SMW_ATTR_VALUE(HASH, hash))
 
 /**

public/smw/names.h
@@ -136,7 +136,7 @@ typedef enum {
  * * SMW_KEY_TYPE_NAME_AES: AES key
  * * SMW_KEY_TYPE_NAME_DES: DES key
  * * SMW_KEY_TYPE_NAME_DES3: Triple DES key
- * * SMW_KEY_TYPE_NAME_DSA_SM2_FP: DSA SM2 key
+ * * SMW_KEY_TYPE_NAME_SM2: SM2 key
  * * SMW_KEY_TYPE_NAME_SM4: SM4 key
  * * SMW_KEY_TYPE_NAME_HMAC: HMAC key
  * * SMW_KEY_TYPE_NAME_RSA: RSA key
@@ -160,7 +160,7 @@ typedef enum {
 	SMW_KEY_TYPE_NAME_AES,	   /* [symmetric] */
 	SMW_KEY_TYPE_NAME_DES,	   /* [symmetric] */
 	SMW_KEY_TYPE_NAME_DES3,	   /* [symmetric] */
-	SMW_KEY_TYPE_NAME_DSA_SM2_FP,
+	SMW_KEY_TYPE_NAME_SM2,
 	SMW_KEY_TYPE_NAME_SM4,	/* [symmetric] */
 	SMW_KEY_TYPE_NAME_HMAC, /* [symmetric] */
 	SMW_KEY_TYPE_NAME_RSA,	/* [asymmetric, signature, encryption] */
@@ -353,11 +353,12 @@ typedef enum {
  * Values:
  * * SMW_SIGNATURE_ALGO_NAME_NONE: No signature algorithm specified
  * * SMW_SIGNATURE_ALGO_NAME_DEFAULT: Signature algorithm is given by the key type
- * * SMW_SIGNATURE_ALGO_NAME_ECDSA: Elliptic Curve Digital Signature Algorithm
- * * SMW_SIGNATURE_ALGO_NAME_EDDSA: Edwards-curve Digital Signature Algorithm
- * * SMW_SIGNATURE_ALGO_NAME_DSA: Digital Signature Algorithm
+ * * SMW_SIGNATURE_ALGO_NAME_ECDSA: Elliptic Curve Digital signature algorithm
+ * * SMW_SIGNATURE_ALGO_NAME_EDDSA: Edwards-curve Digital signature algorithm
+ * * SMW_SIGNATURE_ALGO_NAME_DSA: Digital signature algorithm
  * * SMW_SIGNATURE_ALGO_NAME_RSA: Rivest, Shamir and Adleman signature algorithm
  * * SMW_SIGNATURE_ALGO_NAME_TLS_1_2: Transport Layer Security 1.2 signature algorithm
+ * * SMW_SIGNATURE_ALGO_NAME_SM2: ShāngMì 2 signature algorithm
  * * SMW_SIGNATURE_ALGO_NAME_NB: Number of signature algorithms
  */
 typedef enum {
@@ -368,6 +369,7 @@ typedef enum {
 	SMW_SIGNATURE_ALGO_NAME_DSA,
 	SMW_SIGNATURE_ALGO_NAME_RSA,
 	SMW_SIGNATURE_ALGO_NAME_TLS_1_2,
+	SMW_SIGNATURE_ALGO_NAME_SM2,
 	SMW_SIGNATURE_ALGO_NAME_NB
 } smw_signature_algo_t;
 

commit f7c26553f2bf3e4726c4e2ce4818c16ecbcbf9af

SSMW-1065 public: Add support for SM2 signature
- Clearly state that SM2 parameters must be defined by the user
in Sign/Verify arguments structure

Fixes: c02f482e69a6 ("SSMW-181 public: Add support for SM2 signature")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

public/smw/crypto/sign.h
@@ -50,7 +50,7 @@ struct smw_sm2_params {
  *  - [out] Length in bytes of the signature buffer generated.
  * @eddsa_params: [in] (**optional**) Pointer to edwards signature parameters.
  *                See &typedef smw_eddsa_params.
- * @sm2_params: [in] (**optional**) Pointer to SM2 signature parameters.
+ * @sm2_params: [in] Pointer to SM2 signature parameters.
  *              See &typedef smw_sm2_params.
  *
  * The @subsystem_name designates the Secure Subsystem to be used.
nxp-imx/imx-smw

commit 7129c151d86260df7c129b39b9d4fe328e0b9443

SSMW-181 subsystems: tee: Add support for SM2 signature with TEE
- Add definitions related to SM2.
- Code factorization in keymgr module
- Fix EDDSA key definition in key_def_list[]

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/subsystems/tee/sign_verify.c
@@ -105,7 +105,7 @@ static int get_pub_key_hex_len(struct smw_keymgr_descriptor *key_desc,
 static void get_eddsa_context(unsigned char **ctx, unsigned int *ctx_length,
 			      struct smw_crypto_sign_verify_args *args)
 {
-	struct smw_eddsa_params *param = NULL;
+	struct smw_eddsa_params *params = NULL;
 	struct smw_op_context *op_context = NULL;
 	struct sign_context *sign_ctx = NULL;
 
@@ -118,12 +118,47 @@ static void get_eddsa_context(unsigned char **ctx, unsigned int *ctx_length,
 			*ctx_length = sign_ctx->eddsa_params.context_length;
 		}
 	} else {
-		param = smw_sign_verify_get_eddsa_context(args);
-		if (param) {
-			*ctx = param->context;
-			*ctx_length = param->context_length;
+		params = smw_sign_verify_get_eddsa_context(args);
+		if (params) {
+			*ctx = params->context;
+			*ctx_length = params->context_length;
 		}
 	}
+}
+
+static void get_sm2_identifier(unsigned char **identifier,
+			       unsigned int *identifier_length,
+			       struct smw_crypto_sign_verify_args *args)
+{
+	struct smw_sm2_params *params = smw_sign_verify_get_sm2_params(args);
+
+	if (params) {
+		*identifier = params->identifier;
+		*identifier_length = params->identifier_length;
+	}
+}
+
+static int get_public_key_buffer(struct smw_keymgr_descriptor *export_key_desc,
+				 unsigned char **hex_key_buf,
+				 unsigned int *hex_key_size)
+{
+	int status = SMW_STATUS_OK;
+
+	status = tee_export_public_key(export_key_desc);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	if (export_key_desc->identifier.type_id == SMW_CONFIG_KEY_TYPE_ID_RSA) {
+		*hex_key_size = smw_keymgr_get_modulus_length(export_key_desc);
+		*hex_key_buf = smw_keymgr_get_modulus(export_key_desc);
+
+	} else {
+		*hex_key_size = smw_keymgr_get_public_length(export_key_desc);
+		*hex_key_buf = smw_keymgr_get_public_data(export_key_desc);
+	}
+
+end:
+	return status;
 }
 
 /**
@@ -152,6 +187,8 @@ static int sign_verify(struct smw_crypto_sign_verify_args *args,
 		sizeof(struct sign_verify_shared_params);
 	unsigned char *ctx = NULL;
 	unsigned int ctx_length = 0;
+	unsigned char *identifier = NULL;
+	unsigned int identifier_length = 0;
 	unsigned int sign_length = 0;
 	unsigned char *hex_pub_key = NULL;
 
@@ -202,6 +239,16 @@ static int sign_verify(struct smw_crypto_sign_verify_args *args,
 
 		if (ctx && ctx_length) {
 			if (ADD_OVERFLOW(shared_params_size, ctx_length,
+					 &shared_params_size)) {
+				status = SMW_STATUS_INVALID_PARAM;
+				goto exit;
+			}
+		}
+	} else if (sign_attrs->algo_id == SMW_CONFIG_SIGN_ALGO_ID_SM2) {
+		get_sm2_identifier(&identifier, &identifier_length, args);
+
+		if (identifier && identifier_length) {
+			if (ADD_OVERFLOW(shared_params_size, identifier_length,
 					 &shared_params_size)) {
 				status = SMW_STATUS_INVALID_PARAM;
 				goto exit;
@@ -320,6 +367,20 @@ static int sign_verify(struct smw_crypto_sign_verify_args *args,
 
 	case SMW_CONFIG_SIGN_ALGO_ID_ECDSA:
 		shared_params->sign_algorithm = TEE_ALGORITHM_ID_ECDSA;
+		break;
+
+	case SMW_CONFIG_SIGN_ALGO_ID_SM2:
+		shared_params->sign_algorithm = TEE_ALGORITHM_ID_SM2;
+		if (identifier && identifier_length) {
+			if (SET_OVERFLOW(identifier_length,
+					 shared_params->identifier_length)) {
+				status = SMW_STATUS_INVALID_PARAM;
+				goto exit;
+			}
+
+			SMW_UTILS_MEMCPY(shared_params->identifier, identifier,
+					 identifier_length);
+		}
 		break;
 
 	default:
@@ -468,6 +529,115 @@ end:
 	return status;
 }
 
+static int set_sm2_digest(struct smw_keymgr_descriptor *key_descriptor,
+			  enum smw_config_hash_algo_id hash_id,
+			  unsigned char *msg, unsigned int msg_len,
+			  unsigned char *id, unsigned int id_len,
+			  unsigned char *digest, unsigned int digest_len)
+{
+	int status = SMW_STATUS_OK;
+
+	struct smw_crypto_hash_args hash_args = { 0 };
+	struct smw_hash_args oneshot_pub = { .version = 1 };
+	unsigned char *p = NULL;
+	unsigned char *buf = NULL;
+	unsigned int buf_len = 2;
+	unsigned char *pub_key = NULL;
+	unsigned int pub_key_size = 0;
+	const unsigned char *sm2_a_b_xg_yg = smw_utils_get_sm2_a_b_xg_yg();
+	unsigned int sm2_a_b_xg_yg_size = smw_utils_get_sm2_a_b_xg_yg_size();
+	struct smw_keymgr_descriptor export_key_desc = { 0 };
+	unsigned int tmp = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	if (key_descriptor->identifier.s_id != INVALID_KEY_ID) {
+		export_key_desc.identifier.s_id =
+			key_descriptor->identifier.s_id;
+
+		status = get_public_key_buffer(&export_key_desc, &pub_key,
+					       &pub_key_size);
+		if (status != SMW_STATUS_OK)
+			goto end;
+	} else {
+		pub_key = smw_keymgr_get_public_data(key_descriptor);
+		pub_key_size = smw_keymgr_get_public_length(key_descriptor);
+	}
+
+	if (!pub_key || !pub_key_size) {
+		status = SMW_STATUS_INVALID_PARAM;
+		goto end;
+	}
+
+	/* Concatenate ENTLA || IDA || a || b || xG || yG || xA || yA */
+	if (ADD_OVERFLOW(buf_len, id_len, &buf_len)) {
+		status = SMW_STATUS_INVALID_PARAM;
+		goto end;
+	}
+	if (ADD_OVERFLOW(buf_len, sm2_a_b_xg_yg_size, &buf_len)) {
+		status = SMW_STATUS_INVALID_PARAM;
+		goto end;
+	}
+	if (ADD_OVERFLOW(buf_len, pub_key_size, &buf_len)) {
+		status = SMW_STATUS_INVALID_PARAM;
+		goto end;
+	}
+
+	buf = SMW_UTILS_MALLOC(buf_len);
+	if (!buf) {
+		status = SMW_STATUS_ALLOC_FAILURE;
+		goto end;
+	}
+	p = buf;
+	*p++ = (BYTES_TO_BITS(id_len) >> 8) & 0xFF;
+	*p++ = BYTES_TO_BITS(id_len) & 0xFF;
+	SMW_UTILS_MEMCPY(p, id, id_len);
+	p += id_len;
+	SMW_UTILS_MEMCPY(p, sm2_a_b_xg_yg, sm2_a_b_xg_yg_size);
+	p += sm2_a_b_xg_yg_size;
+	SMW_UTILS_MEMCPY(p, pub_key, pub_key_size);
+
+	hash_args.algo_id = hash_id;
+	hash_args.op_step = SMW_OP_STEP_ONESHOT;
+	hash_args.oneshot_pub = &oneshot_pub;
+
+	oneshot_pub.input = buf;
+	oneshot_pub.input_length = buf_len;
+	oneshot_pub.output = digest;
+	oneshot_pub.output_length = digest_len;
+
+	/* Compute ZA = H256(ENTLA || IDA || a || b || xG || yG || xA || yA) */
+	if (!tee_hash_handle(OPERATION_ID_HASH, &hash_args, &status))
+		status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+	if (status != SMW_STATUS_OK) {
+		status = SMW_STATUS_INVALID_PARAM;
+		goto end;
+	}
+
+	if (ADD_OVERFLOW(oneshot_pub.output_length, msg_len, &tmp)) {
+		status = SMW_STATUS_INVALID_PARAM;
+		goto end;
+	}
+
+	if (digest_len != tmp) {
+		status = SMW_STATUS_INVALID_PARAM;
+		goto end;
+	}
+
+	/* Concatenate ZA || M */
+	SMW_UTILS_MEMCPY(digest + oneshot_pub.output_length, msg, msg_len);
+
+end:
+	if (export_key_desc.pub)
+		(void)smw_keymgr_free_keypair_buffer(&export_key_desc);
+
+	if (buf)
+		SMW_UTILS_FREE(buf);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
 static int signature_init(struct smw_crypto_sign_verify_args *args)
 {
 	int status = SMW_STATUS_INVALID_PARAM;
@@ -479,6 +649,12 @@ static int signature_init(struct smw_crypto_sign_verify_args *args)
 	struct smw_op_context *op_context = NULL;
 	struct sign_context *ctx = NULL;
 	enum tee_key_type key_type_id = TEE_KEY_TYPE_ID_INVALID;
+	unsigned char *msg_buf = smw_sign_verify_get_msg_buf(args);
+	unsigned int msg_len = smw_sign_verify_get_msg_len(args);
+	unsigned char *identifier = NULL;
+	unsigned int identifier_length = 0;
+	unsigned char *input = NULL;
+	unsigned int input_length = 0;
 
 	SMW_DBG_TRACE_FUNCTION_CALL;
 
@@ -519,6 +695,37 @@ static int signature_init(struct smw_crypto_sign_verify_args *args)
 
 		/* Force the hash algorithm to be SHA512 */
 		sign_attrs->hash_id = SMW_CONFIG_HASH_ALGO_ID_SHA512;
+	} else if (sign_attrs->algo_id == SMW_CONFIG_SIGN_ALGO_ID_SM2) {
+		get_sm2_identifier(&identifier, &identifier_length, args);
+
+		if (!identifier || !identifier_length) {
+			status = SMW_STATUS_INVALID_PARAM;
+			goto end;
+		}
+
+		status = tee_get_digest_length(sign_attrs->hash_id,
+					       &input_length);
+		if (status != SMW_STATUS_OK)
+			goto end;
+
+		if (INC_OVERFLOW(input_length, msg_len)) {
+			status = SMW_STATUS_OPERATION_FAILURE;
+			goto end;
+		}
+
+		input = SMW_UTILS_MALLOC(input_length);
+		if (!input) {
+			status = SMW_STATUS_ALLOC_FAILURE;
+			goto end;
+		}
+
+		status = set_sm2_digest(&args->key_descriptor,
+					sign_attrs->hash_id, msg_buf, msg_len,
+					identifier, identifier_length, input,
+					input_length);
+
+		if (status != SMW_STATUS_OK)
+			goto end;
 	}
 
 	status = set_sign_context(op_context, args);
@@ -535,14 +742,23 @@ static int signature_init(struct smw_crypto_sign_verify_args *args)
 	hash_args.op_step = SMW_OP_STEP_INIT;
 	hash_args.init_pub = &init_pub;
 
-	init_pub.input = smw_sign_verify_get_msg_buf(args);
-	init_pub.input_length = smw_sign_verify_get_msg_len(args);
+	if (input || input_length) {
+		init_pub.input = input;
+		init_pub.input_length = input_length;
+	} else {
+		init_pub.input = msg_buf;
+		init_pub.input_length = msg_len;
+	}
+
 	init_pub.context = &ctx->hash_ctx;
 
 	if (!tee_hash_handle(OPERATION_ID_HASH_MULTI_PART, &hash_args, &status))
 		status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
 
 end:
+	if (input)
+		SMW_UTILS_FREE(input);
+
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
 	return status;
 }

core/subsystems/tee/lib_ta/core/sign_verify.c
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2021-2025 NXP
+ * Copyright 2021-2026 NXP
  */
 
 #include <util.h>
 #include <string.h>
 #include <tee_internal_api_extensions.h>
+#include <utee_defines.h>
 
 #include "common.h"
 #include "tee_subsystem.h"
@@ -13,6 +14,25 @@
 #include "hash.h"
 #include "sign_verify.h"
 #include "obj.h"
+
+static const uint8_t sm2_a_b_xg_yg[] = {
+	/* a */
+	0xFF, 0xFF, 0xFF, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00,
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFC,
+	/* b */
+	0x28, 0xE9, 0xFA, 0x9E, 0x9D, 0x9F, 0x5E, 0x34, 0x4D, 0x5A, 0x9E, 0x4B,
+	0xCF, 0x65, 0x09, 0xA7, 0xF3, 0x97, 0x89, 0xF5, 0x15, 0xAB, 0x8F, 0x92,
+	0xDD, 0xBC, 0xBD, 0x41, 0x4D, 0x94, 0x0E, 0x93,
+	/* xg */
+	0x32, 0xC4, 0xAE, 0x2C, 0x1F, 0x19, 0x81, 0x19, 0x5F, 0x99, 0x04, 0x46,
+	0x6A, 0x39, 0xC9, 0x94, 0x8F, 0xE3, 0x0B, 0xBF, 0xF2, 0x66, 0x0B, 0xE1,
+	0x71, 0x5A, 0x45, 0x89, 0x33, 0x4C, 0x74, 0xC7,
+	/* yg */
+	0xBC, 0x37, 0x36, 0xA2, 0xF4, 0xF6, 0x77, 0x9C, 0x59, 0xBD, 0xCE, 0xE3,
+	0x6B, 0x69, 0x21, 0x53, 0xD0, 0xA9, 0x87, 0x7C, 0xC6, 0x2A, 0x47, 0x40,
+	0x02, 0xDF, 0x32, 0xE5, 0x21, 0x39, 0xF0, 0xA0
+};
 
 #define ECDSA_ALGORITHM_ID(_security_size, _hash_algo)                         \
 	{                                                                      \
@@ -199,7 +219,7 @@ static TEE_Result set_key(uint32_t cmd_id, TEE_Param ta_param,
 				    priv_key, priv_key_len, pub_key,
 				    shared_params->pub_key_len, modulus,
 				    modulus_len);
-		if (res)
+		if (res != TEE_SUCCESS)
 			EMSG("Failed to import key: 0x%x", res);
 
 		return res;
@@ -208,7 +228,7 @@ static TEE_Result set_key(uint32_t cmd_id, TEE_Param ta_param,
 		/* Retrieve key handle */
 		res = ta_get_obj_handle(key_handle, shared_params->id,
 					persistent);
-		if (res)
+		if (res != TEE_SUCCESS)
 			EMSG("Key not found: 0x%x", res);
 
 		return res;
@@ -216,6 +236,108 @@ static TEE_Result set_key(uint32_t cmd_id, TEE_Param ta_param,
 	default:
 		return res;
 	}
+}
+
+static TEE_Result set_sm2_digest(TEE_ObjectHandle key_handle,
+				 enum tee_algorithm_id hash_algo, void *msg,
+				 size_t msg_len, uint8_t *id, uint16_t id_len,
+				 uint8_t *digest, size_t *digest_len)
+{
+	TEE_Result res = TEE_ERROR_BAD_PARAMETERS;
+	uint8_t *p = NULL;
+	uint8_t *buf = NULL;
+	size_t buf_len = 2;
+	unsigned char *pub_key = NULL;
+	size_t pub_key_size = 0;
+	uint8_t *za = NULL;
+	size_t za_len = TEE_SM3_HASH_SIZE;
+
+	FMSG("Executing %s", __func__);
+
+	if (ADD_OVERFLOW(za_len, msg_len, &za_len))
+		goto err;
+
+	res = get_ecc_public_key_size(key_handle, &pub_key_size);
+	if (res != TEE_SUCCESS)
+		return res;
+
+	pub_key = TEE_Malloc(pub_key_size, TEE_USER_MEM_HINT_NO_FILL_ZERO);
+	if (!pub_key) {
+		EMSG("TEE_Malloc failed");
+		res = TEE_ERROR_OUT_OF_MEMORY;
+		goto err;
+	}
+
+	/* Get public key buffer */
+	res = export_public_key(key_handle, pub_key, &pub_key_size, NULL, NULL);
+	if (res != TEE_SUCCESS || !pub_key || !pub_key_size)
+		goto err;
+
+	/* Concatenate ENTLA || IDA || a || b || xG || yG || xA || yA */
+	if (ADD_OVERFLOW(buf_len, id_len, &buf_len)) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto err;
+	}
+	if (ADD_OVERFLOW(buf_len, ARRAY_SIZE(sm2_a_b_xg_yg), &buf_len)) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto err;
+	}
+	if (ADD_OVERFLOW(buf_len, pub_key_size, &buf_len)) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto err;
+	}
+
+	buf = TEE_Malloc(buf_len, TEE_USER_MEM_HINT_NO_FILL_ZERO);
+	if (!buf) {
+		EMSG("TEE_Malloc failed");
+		res = TEE_ERROR_OUT_OF_MEMORY;
+		goto err;
+	}
+
+	p = buf;
+	*p++ = ((id_len << 3) >> 8) & 0xFF;
+	*p++ = (id_len << 3) & 0xFF;
+	TEE_MemMove(p, id, id_len);
+	p += id_len;
+	TEE_MemMove(p, sm2_a_b_xg_yg, ARRAY_SIZE(sm2_a_b_xg_yg));
+	p += ARRAY_SIZE(sm2_a_b_xg_yg);
+	TEE_MemMove(p, pub_key, pub_key_size);
+
+	/* Compute ZA = H256(ENTLA || IDA || a || b || xG || yG || xA || yA) */
+	za = TEE_Malloc(za_len, TEE_USER_MEM_HINT_NO_FILL_ZERO);
+	if (!za) {
+		EMSG("TEE_Malloc failed");
+		res = TEE_ERROR_OUT_OF_MEMORY;
+		goto err;
+	}
+
+	res = ta_compute_digest(hash_algo, buf, buf_len, za, &za_len);
+	if (res != TEE_SUCCESS)
+		goto err;
+
+	if (za_len != TEE_SM3_HASH_SIZE) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto err;
+	}
+
+	/* Concatenate ZA || M */
+	TEE_MemMove(za + za_len, msg, msg_len);
+
+	/* Compute e = H256(ZA || M) */
+	res = ta_compute_digest(hash_algo, za, za_len + msg_len, digest,
+				digest_len);
+
+err:
+	if (pub_key)
+		TEE_Free(pub_key);
+
+	if (buf)
+		TEE_Free(buf);
+
+	if (za)
+		TEE_Free(za);
+
+	return res;
 }
 
 TEE_Result sign_verify(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS],
@@ -231,13 +353,15 @@ TEE_Result sign_verify(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS],
 	uint32_t mode = 0;
 	enum tee_algorithm_id algorithm_id = 0;
 	enum tee_algorithm_id hash_algo = TEE_ALGORITHM_ID_INVALID;
-	void *digest = NULL;
+	uint8_t *digest = NULL;
 	size_t digest_len = 0;
+	uint8_t *buf = NULL;
 	size_t sign_len = 0;
 	bool persistent = false;
 	uint32_t attr_count = 0;
 	struct sign_verify_shared_params *shared_params = NULL;
 	uint32_t op_max_key_size = 0;
+	bool sm2_compute_digest = false;
 
 	FMSG("Executing %s (%d)", __func__, cmd_id);
 
@@ -274,32 +398,69 @@ TEE_Result sign_verify(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS],
 		    sizeof(*shared_params) + shared_params->ctx_length)
 		return res;
 
+	if (shared_params->sign_algorithm == TEE_ALGORITHM_ID_SM2) {
+		if (params[1].memref.size !=
+		    sizeof(*shared_params) + shared_params->identifier_length) {
+			return res;
+		} else if (shared_params->identifier_length != 0) {
+			if (shared_params->msg_hashed)
+				return res;
+
+			sm2_compute_digest = true;
+		} else if (!shared_params->msg_hashed) {
+			return res;
+		}
+	}
+
 	res = set_key(cmd_id, params[0], param0_type, shared_params,
 		      &key_handle, &persistent);
-	if (res)
+	if (res != TEE_SUCCESS)
 		goto err;
 
 	if (!shared_params->msg_hashed)
 		hash_algo = shared_params->hash_algorithm;
 
-	if (hash_algo != TEE_ALGORITHM_ID_INVALID) {
+	if (sm2_compute_digest) {
 		res = ta_get_digest_length(hash_algo, &digest_len);
-		if (res)
+		if (res != TEE_SUCCESS)
 			goto err;
 
-		digest = TEE_Malloc(digest_len, TEE_USER_MEM_HINT_NO_FILL_ZERO);
-		if (digest) {
-			res = ta_compute_digest(hash_algo,
-						params[2].memref.buffer,
-						params[2].memref.size, digest,
-						&digest_len);
-		} else {
+		buf = TEE_Malloc(digest_len, TEE_USER_MEM_HINT_NO_FILL_ZERO);
+		if (!buf) {
 			EMSG("TEE_Malloc failed");
 			res = TEE_ERROR_OUT_OF_MEMORY;
+			goto err;
 		}
 
-		if (res)
+		res = set_sm2_digest(key_handle, hash_algo,
+				     params[2].memref.buffer,
+				     params[2].memref.size,
+				     shared_params->identifier,
+				     shared_params->identifier_length, buf,
+				     &digest_len);
+		if (res != TEE_SUCCESS)
 			goto err;
+
+		digest = buf;
+	} else if (hash_algo != TEE_ALGORITHM_ID_INVALID) {
+		res = ta_get_digest_length(hash_algo, &digest_len);
+		if (res != TEE_SUCCESS)
+			goto err;
+
+		buf = TEE_Malloc(digest_len, TEE_USER_MEM_HINT_NO_FILL_ZERO);
+		if (!buf) {
+			EMSG("TEE_Malloc failed");
+			res = TEE_ERROR_OUT_OF_MEMORY;
+			goto err;
+		}
+
+		res = ta_compute_digest(hash_algo, params[2].memref.buffer,
+					params[2].memref.size, buf,
+					&digest_len);
+		if (res != TEE_SUCCESS)
+			goto err;
+
+		digest = buf;
 	} else {
 		digest = params[2].memref.buffer;
 		digest_len = params[2].memref.size;
@@ -357,7 +518,7 @@ TEE_Result sign_verify(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS],
 		if (cmd_id == CMD_SIGN && params[3].memref.buffer) {
 			res = ta_get_digest_length(TEE_ALGORITHM_ID_SHA512,
 						   &sign_len);
-			if (res)
+			if (res != TEE_SUCCESS)
 				goto err;
 
 			if (params[3].memref.size < sign_len) {
@@ -373,19 +534,24 @@ TEE_Result sign_verify(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS],
 					&algorithm_id);
 		break;
 
+	case TEE_ALGORITHM_ID_SM2:
+		if (shared_params->hash_algorithm == TEE_ALGORITHM_ID_SM3)
+			algorithm_id = TEE_ALG_SM2_DSA_SM3;
+		break;
+
 	default:
 		res = TEE_ERROR_NOT_SUPPORTED;
 		break;
 	}
 
-	if (res) {
+	if (res != TEE_SUCCESS) {
 		EMSG("Failed to get algorithm ID: 0x%x", res);
 		goto err;
 	}
 
 	res = TEE_AllocateOperation(&operation, algorithm_id, mode,
 				    op_max_key_size);
-	if (res) {
+	if (res != TEE_SUCCESS) {
 		EMSG("Failed to alloc operation: 0x%x", res);
 		goto err;
 	}
@@ -397,11 +563,11 @@ TEE_Result sign_verify(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS],
 	}
 
 	res = check_operation_keys_usage(operation, &key_info, 1);
-	if (res)
+	if (res != TEE_SUCCESS)
 		goto err;
 
 	res = TEE_SetOperationKey(operation, key_handle);
-	if (res) {
+	if (res != TEE_SUCCESS) {
 		EMSG("Failed to set operation key: 0x%x", res);
 		goto err;
 	}
@@ -411,14 +577,14 @@ TEE_Result sign_verify(uint32_t param_types, TEE_Param params[TEE_NUM_PARAMS],
 					       attr_count, digest, digest_len,
 					       params[3].memref.buffer,
 					       &params[3].memref.size);
-		if (res)
+		if (res != TEE_SUCCESS)
 			EMSG("Failed to sign digest: 0x%x", res);
 	} else { /* CMD_VERIFY */
 		res = TEE_AsymmetricVerifyDigest(operation, sign_verify_attr,
 						 attr_count, digest, digest_len,
 						 params[3].memref.buffer,
 						 params[3].memref.size);
-		if (res)
+		if (res != TEE_SUCCESS)
 			EMSG("Failed to verify digest: 0x%x", res);
 	}
 
@@ -430,8 +596,8 @@ err:
 			TEE_FreeTransientObject(key_handle);
 	}
 
-	if (hash_algo != TEE_ALGORITHM_ID_INVALID && digest)
-		TEE_Free(digest);
+	if (buf)
+		TEE_Free(buf);
 
 	if (operation != TEE_HANDLE_NULL)
 		TEE_FreeOperation(operation);

core/subsystems/tee/common.c
@@ -67,6 +67,27 @@ end:
 	return status;
 }
 
+int tee_get_digest_length(enum smw_config_hash_algo_id smw_id,
+			  unsigned int *digest_len)
+{
+	int status = SMW_STATUS_INVALID_PARAM;
+	const struct tee_hash_algo *hash_algo = NULL;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	hash_algo = tee_get_hash_algo(smw_id);
+	if (!hash_algo)
+		goto end;
+
+	*digest_len = hash_algo->length;
+
+	status = SMW_STATUS_OK;
+
+end:
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
 int set_tmpref_buffer(unsigned int mem_type, unsigned int param_idx,
 		      unsigned char *buffer, unsigned int buffer_len,
 		      TEEC_Operation *op)

core/subsystems/tee/common/tee_subsystem.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2025 NXP
+ * Copyright 2020-2026 NXP
  */
 
 #ifndef TEE_SUBSYSTEM_H
@@ -61,6 +61,7 @@ enum tee_key_type {
 	TEE_KEY_TYPE_ID_RSA,
 	TEE_KEY_TYPE_ID_GENERIC_SECRET,
 	TEE_KEY_TYPE_ID_HKDF_IKM,
+	TEE_KEY_TYPE_ID_SM2,
 	TEE_KEY_TYPE_ID_NB,
 	TEE_KEY_TYPE_ID_INVALID
 };
@@ -98,6 +99,7 @@ enum tee_algorithm_id {
 	TEE_ALGORITHM_ID_EDDSA,
 	TEE_ALGORITHM_ID_RSA,
 	TEE_ALGORITHM_ID_SHAKE256,
+	TEE_ALGORITHM_ID_SM2,
 	TEE_ALGORITHM_ID_INVALID
 };
 
@@ -208,6 +210,10 @@ struct sign_verify_shared_params {
 		struct {
 			uint8_t ctx_length;
 			uint8_t ctx[];
+		};
+		struct {
+			uint16_t identifier_length;
+			uint8_t identifier[];
 		};
 	};
 };

core/subsystems/tee/keymgr.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright 2020-2025 NXP
+ * Copyright 2020-2026 NXP
  */
 
 #include <inttypes.h>
@@ -60,10 +60,17 @@ RANGE_DEF(HKDF_IKM, 8, 4096, 8);
 		.security_size = _security_size, .symmetric = _symmetric       \
 	}
 
-#define KEY_DEF_ASYM(_key_type, _security_size, _hash)                         \
+#define KEY_DEF_ECDSA(_key_type, _security_size, _hash)                        \
 	KEY_DEF(_key_type, _security_size,                                     \
 		SMW_ATTR_ALGO_ASYMMETRIC_SIGNATURE_ECDSA(                      \
 			SMW_ATTR_CURVE_##_key_type, SMW_ATTR_HASH_##_hash),    \
+		false)
+
+#define KEY_DEF_EDDSA(_key_type, _security_size, _hash)                        \
+	KEY_DEF(_key_type, _security_size,                                     \
+		SMW_ATTR_ALGO_ASYMMETRIC_SIGNATURE_EDDSA(                      \
+			SMW_ATTR_CURVE_##_key_type, SMW_ATTR_HASH_##_hash,     \
+			SMW_ATTR_SIGN_PARAM_EDDSA_NONE),                       \
 		false)
 
 #define KEY_DEF_SYM(_key_type, _security_size)                                 \
@@ -107,6 +114,11 @@ RANGE_DEF(HKDF_IKM, 8, 4096, 8);
 		.symmetric = true                                              \
 	}
 
+#define KEY_DEF_SM2(_security_size, _hash)                                     \
+	KEY_DEF(SM2, _security_size,                                           \
+		SMW_ATTR_ALGO_ASYMMETRIC_SIGNATURE_SM2(SMW_ATTR_HASH_##_hash), \
+		false)
+
 /**
  * struct key_def - TEE Key definition
  * @key_type_id: SMW key type ID
@@ -132,12 +144,12 @@ static const struct key_def {
 	struct security_size_range security_size_range;
 	bool symmetric;
 } key_def_list[] = {
-	KEY_DEF_ASYM(SECP_R1, 192, ANY),
-	KEY_DEF_ASYM(SECP_R1, 224, ANY),
-	KEY_DEF_ASYM(SECP_R1, 256, ANY),
-	KEY_DEF_ASYM(SECP_R1, 384, ANY),
-	KEY_DEF_ASYM(SECP_R1, 521, ANY),
-	KEY_DEF_ASYM(ED25519, 255, SHA512),
+	KEY_DEF_ECDSA(SECP_R1, 192, ANY),
+	KEY_DEF_ECDSA(SECP_R1, 224, ANY),
+	KEY_DEF_ECDSA(SECP_R1, 256, ANY),
+	KEY_DEF_ECDSA(SECP_R1, 384, ANY),
+	KEY_DEF_ECDSA(SECP_R1, 521, ANY),
+	KEY_DEF_EDDSA(ED25519, 255, SHA512),
 	KEY_DEF_RANGE_SYM(AES),
 	KEY_DEF_SYM(DES, 56),
 	KEY_DEF_RANGE_SYM(DES3),
@@ -152,6 +164,7 @@ static const struct key_def {
 	KEY_DEF_RANGE_ASYM(RSA, 0),
 	KEY_DEF_DERIVE(DERIVE, GENERIC_SECRET),
 	KEY_DEF_DERIVE(HKDF_IKM, HKDF_IKM),
+	KEY_DEF_SM2(256, SM3),
 };
 
 /**
@@ -831,6 +844,7 @@ static int check_import_key_buffers_presence(enum tee_key_type key_type,
 	case TEE_KEY_TYPE_ID_SECP_R1:
 	case TEE_KEY_TYPE_ID_ED25519:
 	case TEE_KEY_TYPE_ID_RSA:
+	case TEE_KEY_TYPE_ID_SM2:
 		/*
 		 * OPTEE does not support import of private key only for
 		 * asymmetric key type.
@@ -869,6 +883,7 @@ static int check_export_key_config(struct smw_keymgr_descriptor *key_descriptor)
 	case SMW_CONFIG_KEY_TYPE_ID_SECP_R1:
 	case SMW_CONFIG_KEY_TYPE_ID_ED25519:
 	case SMW_CONFIG_KEY_TYPE_ID_RSA:
+	case SMW_CONFIG_KEY_TYPE_ID_SM2:
 		/*
 		 * For RSA key type, modulus presence is already check in
 		 * core/keymgr.c file
@@ -2045,6 +2060,62 @@ int tee_delete_key(uint32_t id)
 
 exit:
 	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	return status;
+}
+
+int tee_export_public_key(struct smw_keymgr_descriptor *key_desc)
+{
+	int status = SMW_STATUS_OK;
+
+	struct smw_keymgr_get_key_attributes_args key_attrs_args = { 0 };
+	struct smw_keymgr_identifier *key_identifier =
+		&key_attrs_args.key_descriptor.identifier;
+	unsigned int public_length = 0;
+	unsigned int modulus_length = 0;
+
+	SMW_DBG_TRACE_FUNCTION_CALL;
+
+	/* First get the key attributes */
+	key_identifier->s_id = key_desc->identifier.s_id;
+	status = get_key_attributes(&key_attrs_args);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	key_desc->identifier.type_id = key_identifier->type_id;
+	key_desc->identifier.security_size = key_identifier->security_size;
+	key_desc->format_id = SMW_KEYMGR_FORMAT_ID_HEX;
+
+	status = smw_keymgr_alloc_keypair_buffer(key_desc, 0, 0, 0);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	status = get_key_lengths(key_desc);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	public_length = smw_keymgr_get_public_length(key_desc);
+	modulus_length = smw_keymgr_get_modulus_length(key_desc);
+
+	status = smw_keymgr_free_keypair_buffer(key_desc);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	/* Allocate key descriptor's keypair buffer and its public data */
+	status = smw_keymgr_alloc_keypair_buffer(key_desc, public_length, 0,
+						 modulus_length);
+	if (status != SMW_STATUS_OK)
+		goto end;
+
+	/* Export the public key */
+	if (!tee_key_handle(OPERATION_ID_EXPORT_KEY, key_desc, &status))
+		status = SMW_STATUS_OPERATION_NOT_SUPPORTED;
+
+end:
+	if (status != SMW_STATUS_OK && key_desc->pub)
+		(void)smw_keymgr_free_keypair_buffer(key_desc);
+
+	SMW_DBG_PRINTF(VERBOSE, "%s returned %d\n", __func__, status);
+	// coverity[missing_unlock]
 	return status;
 }
 

core/subsystems/tee/lib_ta/core/inc/keymgr.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2025 NXP
+ * Copyright 2020-2026 NXP
  */
 
 #ifndef TA_KEYMGR_H
@@ -13,6 +13,22 @@
 		__typeof__(size) _bits = 0;                                    \
 		ADD_OVERFLOW((size), 7, &_bits) ? 0 : _bits / 8;               \
 	})
+
+/**
+ * export_public_key() - Export the public key.
+ * @handle: Key handle.
+ * @pub_data: Pointer to public key buffer.
+ * @pub_len: @pub_data length in bytes.
+ * @modulus: Modulus buffer.
+ * @modulus_len: @modulus length in bytes.
+ *
+ * Return:
+ * TEE_SUCCESS			- Success.
+ * Error code from internal functions.
+ */
+TEE_Result export_public_key(TEE_ObjectHandle handle, unsigned char *pub_data,
+			     size_t *pub_len, unsigned char *modulus,
+			     size_t *modulus_len);
 
 /**
  * generate_key() - Generate a key.
@@ -154,6 +170,18 @@ TEE_Result key_usage_to_tee(unsigned int key_usage, uint32_t *tee_key_usage);
  * Error code from TEE_RestrictObjectUsage1().
  */
 TEE_Result set_key_usage(uint32_t key_usage, TEE_ObjectHandle key_handle);
+
+/**
+ * get_ecc_public_key_size() - Get the asymmetric public key size.
+ * @handle: Key handle.
+ * @size: Public key size retrieved in bytes.
+ *
+ * Return:
+ * TEE_SUCCESS        - Success.
+ * TEE_ERROR_GENERIC  - Unexpected success.
+ * Error code from TEE_GetObjectBufferAttribute().
+ */
+TEE_Result get_ecc_public_key_size(TEE_ObjectHandle handle, size_t *size);
 
 /**
  * get_key_obj_type() - Get key's object type.

commit bc5c20826df4250732303f35316b1cf87ccbc451

SSMW-1065 subsystems: tee: Use INC_OVERFLOW instead of ADD_OVERFLOW
Cosmetic change
Use INC_OVERFLOW instead of ADD_OVERFLOW
when the destination variable is one of the source variable.

Fixes: 7129c151d862 ("SSMW-181 subsystems: tee: Add support for SM2 signature with TEE")

Signed-off-by: Bruno De Smet <bruno.de.smet@nxp.com>

core/subsystems/tee/sign_verify.c
@@ -238,8 +238,7 @@ static int sign_verify(struct smw_crypto_sign_verify_args *args,
 		get_eddsa_context(&ctx, &ctx_length, args);
 
 		if (ctx && ctx_length) {
-			if (ADD_OVERFLOW(shared_params_size, ctx_length,
-					 &shared_params_size)) {
+			if (INC_OVERFLOW(shared_params_size, ctx_length)) {
 				status = SMW_STATUS_INVALID_PARAM;
 				goto exit;
 			}
@@ -248,8 +247,8 @@ static int sign_verify(struct smw_crypto_sign_verify_args *args,
 		get_sm2_identifier(&identifier, &identifier_length, args);
 
 		if (identifier && identifier_length) {
-			if (ADD_OVERFLOW(shared_params_size, identifier_length,
-					 &shared_params_size)) {
+			if (INC_OVERFLOW(shared_params_size,
+					 identifier_length)) {
 				status = SMW_STATUS_INVALID_PARAM;
 				goto exit;
 			}
@@ -570,15 +569,15 @@ static int set_sm2_digest(struct smw_keymgr_descriptor *key_descriptor,
 	}
 
 	/* Concatenate ENTLA || IDA || a || b || xG || yG || xA || yA */
-	if (ADD_OVERFLOW(buf_len, id_len, &buf_len)) {
+	if (INC_OVERFLOW(buf_len, id_len)) {
 		status = SMW_STATUS_INVALID_PARAM;
 		goto end;
 	}
-	if (ADD_OVERFLOW(buf_len, sm2_a_b_xg_yg_size, &buf_len)) {
+	if (INC_OVERFLOW(buf_len, sm2_a_b_xg_yg_size)) {
 		status = SMW_STATUS_INVALID_PARAM;
 		goto end;
 	}
-	if (ADD_OVERFLOW(buf_len, pub_key_size, &buf_len)) {
+	if (INC_OVERFLOW(buf_len, pub_key_size)) {
 		status = SMW_STATUS_INVALID_PARAM;
 		goto end;
 	}